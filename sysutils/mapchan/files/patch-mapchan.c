c75d8e508b31b50ce2efbe840f733e57090c9c17
Automaton <nobody@home.ok>
2012-12-09 05:37:20 +0100

    Import sysutils/mapchan version 2.0

---
+++ sysutils/mapchan/files/patch-mapchan.c
@@ -0,0 +1,11 @@
+--- mapchan.c.orig	Sat Oct 25 09:21:42 2003
++++ mapchan.c	Sun Feb 27 15:27:01 2005
+@@ -19,7 +19,7 @@
+ #endif
+ 
+ #ifndef CPATH
+-  #define CPATH "/etc/mapchan"
++  #define CPATH "/usr/local/etc/mapchan"
+ #endif
+ 
+ #define BUFSIZE 4096
