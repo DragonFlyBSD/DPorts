3d47b50e8c6fd53dbf7823c7361defde0452683a
Automaton <nobody@home.ok>
2012-12-09 06:49:38 +0100

    Import net-mgmt/cdpr version 2.2.1

---
+++ net-mgmt/cdpr/files/patch-conffile.c
@@ -0,0 +1,22 @@
+
+$FreeBSD: ports/net-mgmt/cdpr/files/patch-conffile.c,v 1.2 2012/11/17 06:00:31 svnexp Exp $
+
+--- conffile.c.orig
++++ conffile.c
+@@ -57,6 +57,7 @@
+ 	struct hostent *h;
+ 	char *addy;
+ 	int port;
++	char *tport;
+ 
+ 	if (ip && url)
+ 	{
+@@ -70,7 +71,7 @@
+ 		else
+ 		{
+ 			strtok(ip,":");
+-			char *tport = strtok(NULL,":");
++			tport = strtok(NULL,":");
+ 			port = atoi(tport);
+ 		}
+ /*
