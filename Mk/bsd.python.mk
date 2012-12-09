56da2bc93caf54a6c92bb045e4e628b67d097e42
John Marino <draco@marino.st>
2012-12-09 15:12:34 +0100

    bsd.python.mk: restore cvsid

--- Mk/bsd.python.mk
+++ Mk/bsd.python.mk
@@ -1,7 +1,7 @@
 # -*- tab-width: 4; -*-
 # ex: ts=4
 #
-# $FreeBSD$
+# $FreeBSD: ports/Mk/bsd.python.mk,v 1.155 2012/11/17 05:54:18 svnexp Exp $
 #
 
 .if !defined(_POSTMKINCLUDED) && !defined(Python_Pre_Include)
