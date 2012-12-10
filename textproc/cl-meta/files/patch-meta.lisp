9d8e04b390a844ee28f779f9563717f1bbafc95f
Automaton <nobody@home.ok>
2012-12-10 20:38:14 +0100

    Import textproc/cl-meta version 0.1_3,1

---
+++ textproc/cl-meta/files/patch-meta.lisp
@@ -0,0 +1,14 @@
+
+$FreeBSD: ports/textproc/cl-meta/files/patch-meta.lisp,v 1.2 2012/11/17 06:01:53 svnexp Exp $
+
+--- meta.lisp.orig
++++ meta.lisp
+@@ -47,7 +47,7 @@
+ ;;;;       Proc. 19'th Nat'l. Conf. of the ACM (Aug. 1964),D1.3-1-D1.3-11.
+ ;;;;
+ ;;;;
+-;;;; Nürnberg, 01.Jul.2001 Jochen Schmidt
++;;;; Nuremberg, 01.Jul.2001 Jochen Schmidt
+ 
+ 
+ (in-package :meta)
