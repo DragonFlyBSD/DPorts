73e854dd885926e855f91416d37163c1d618cc11
Automaton <nobody@home.ok>
2012-12-10 09:34:39 +0100

    Import devel/shmap version 0.3.2_1

---
+++ devel/shmap/files/patch-shmap_shmap.c
@@ -0,0 +1,14 @@
+
+$FreeBSD: ports/devel/shmap/files/patch-shmap_shmap.c,v 1.2 2012/11/17 05:57:03 svnexp Exp $
+
+--- shmap/shmap.c.orig	Mon Dec 30 22:56:29 2002
++++ shmap/shmap.c	Mon Dec 30 22:59:36 2002
+@@ -706,7 +706,7 @@
+ /*
+         returns the amount of items in hashmap
+ */
+-unsigned int shmap_items ( shmap * map )
++size_t shmap_items ( shmap * map )
+ {
+         CHECK_NULL0(map)
+ 
