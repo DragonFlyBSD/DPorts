1573f7732cb788f9516b25f9e644f005f5e046f8
Automaton <nobody@home.ok>
2012-12-11 01:39:56 +0100

    Import misc/dvorak7min version 1.6.1

---
+++ misc/dvorak7min/files/patch-dvorak7min.c
@@ -0,0 +1,10 @@
+--- dvorak7min.c.orig	Mon Mar 15 20:41:06 2004
++++ dvorak7min.c	Mon Mar 15 20:41:25 2004
+@@ -41,7 +41,6 @@
+  *  
+  */
+ 
+-#include <strings.h>
+ #include <string.h>
+ #include <stdlib.h>
+ #include <ctype.h>
