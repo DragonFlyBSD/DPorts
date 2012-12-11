8d506d465000cd2af74946d4d7e9363c283bad70
Automaton <nobody@home.ok>
2012-12-11 14:21:45 +0100

    Import graphics/jasper version 1.900.1_10

---
+++ graphics/jasper/files/patch-jas_types.h
@@ -0,0 +1,20 @@
+$FreeBSD: ports/graphics/jasper/files/patch-jas_types.h,v 1.2 2012/11/17 05:58:10 svnexp Exp $
+
+--- src/libjasper/include/jasper/jas_types.h	Sat Oct  4 12:57:58 2003
++++ src/libjasper/include/jasper/jas_types.h	Wed Dec 17 10:29:46 2003
+@@ -223,13 +223,13 @@
+ #endif
+ /**********/
+ #if !defined(INT_FAST64_MIN)
+-typedef longlong int_fast64_t;
++typedef long long int_fast64_t;
+ #define INT_FAST64_MIN	LLONG_MIN
+ #define INT_FAST64_MAX	LLONG_MAX
+ #endif
+ /**********/
+ #if !defined(UINT_FAST64_MAX)
+-typedef ulonglong uint_fast64_t;
++typedef unsigned long long uint_fast64_t;
+ #define UINT_FAST64_MAX	ULLONG_MAX
+ #endif
+ /**********/
