7547afaa9e0823bf898cd05923f430e28ce48a02
Automaton <nobody@home.ok>
2012-12-12 00:08:21 +0100

    Import net-mgmt/aspathtree version 4.2_3

---
+++ net-mgmt/aspathtree/files/patch-lib_elaborate-aspaths.pl
@@ -0,0 +1,23 @@
+
+$FreeBSD: ports/net-mgmt/aspathtree/files/patch-lib_elaborate-aspaths.pl,v 1.2 2012/11/17 06:00:30 svnexp Exp $
+
+--- lib/elaborate-aspaths.pl.orig
++++ lib/elaborate-aspaths.pl
+@@ -364,7 +364,7 @@
+ 							if ($value1 > 64511 && $value1 < 65535) {
+ 								push (@asspriv,$value1);
+ 								push (@{$paths_withasspriv{$value1}}, $key);
+-							} elsif ($value1 == 65535 || ($value1 > 32767 && $value1 < 64512)) {
++							} elsif ($value1 == 65535 ) {
+ 								push (@assreserved,$value1);
+ 								push (@{$paths_withassreserved{$value1}}, $key);
+ 							}
+@@ -386,7 +386,7 @@
+ 						if ($value > 64511 && $value < 65535) {
+ 							push (@asspriv,$value);
+ 							push (@{$paths_withasspriv{$value}}, $key);
+-						} elsif ($value == 65535 || ($value > 32767 && $value < 64512)) {
++						} elsif ($value == 65535 ) {
+ 							push (@assreserved,$value);
+ 							push (@{$paths_withassreserved{$value}}, $key);
+ 						}
