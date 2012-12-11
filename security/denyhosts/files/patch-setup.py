553c3382ebc4f6d6076104e6eafff9b5c3378cec
Automaton <nobody@home.ok>
2012-12-11 09:25:28 +0100

    Import security/denyhosts version 2.6_4

---
+++ security/denyhosts/files/patch-setup.py
@@ -0,0 +1,27 @@
+
+$FreeBSD: ports/security/denyhosts/files/patch-setup.py,v 1.3 2012/11/17 06:01:03 svnexp Exp $
+
+--- setup.py.orig
++++ setup.py
+@@ -8,7 +8,7 @@
+ from glob import glob
+ 
+ 
+-libpath = "/usr/share/denyhosts"
++libpath = "%%PREFIX%%/share/denyhosts"
+ scriptspath = "%s/scripts" % libpath
+ pluginspath = "%s/plugins" % libpath
+ 
+@@ -26,11 +26,8 @@
+       data_files=[(libpath, glob("denyhosts.cfg-dist")),
+                   (libpath, glob("setup.py")),
+                   (libpath, glob("daemon-control-dist")),
+-                  (libpath, glob("CHANGELOG.txt")),
+-                  (libpath, glob("README.txt")),
+                   (scriptspath, glob("scripts/*")),
+-                  (pluginspath, glob("plugins/*")),
+-                  (libpath, glob("LICENSE.txt"))],
++                  (pluginspath, glob("plugins/*"))],
+       license="GPL v2",
+       ##extra_path='denyhosts',
+       long_description="""
