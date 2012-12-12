1b6076a2646b2846ef47ed74fb2d17042457d018
Automaton <nobody@home.ok>
2012-12-12 04:08:19 +0100

    Import cad/astk-serveur version 1.10.4

---
+++ cad/astk-serveur/files/patch-asrun_config.py
@@ -0,0 +1,13 @@
+--- ./asrun/config.py.orig	2010-07-07 18:18:08.000000000 +0200
++++ ./asrun/config.py	2010-08-11 19:03:21.000000000 +0200
+@@ -66,8 +66,8 @@
+             'SRCHIST'         : ['histor'],
+             # name of "binaries" (as results of a make)
+             'MAKE'            : ['debug nodebug'],
+-            'BIN_NODBG'       : ['asteru.exe'],
+-            'BIN_DBG'         : ['asterd.exe'],
++            'BIN_NODBG'       : ['asteru'],
++            'BIN_DBG'         : ['asterd'],
+             'BINCMDE'         : ['commande'],
+             'BINELE'          : ['elements'],
+             'BINPICKLED'      : ['cata_ele.pickled'],
