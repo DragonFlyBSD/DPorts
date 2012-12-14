af229acfbb7619ba4196528275c38ebab723a92b
Automaton <nobody@home.ok>
2012-12-14 05:51:06 +0100

    Import devel/py-dialog version 2.7_3

---
+++ devel/py-dialog/files/patch-dialog.py
@@ -0,0 +1,24 @@
+
+$FreeBSD: ports/devel/py-dialog/files/patch-dialog.py,v 1.4 2012/11/17 05:56:46 svnexp Exp $
+
+--- dialog.py.orig
++++ dialog.py
+@@ -214,6 +214,9 @@
+     "create_rc": lambda file: ("--create-rc", file),
+     "defaultno": lambda enable: _simple_option("--defaultno", enable),
+     "default_item": lambda string: ("--default-item", string),
++    "yes_label": lambda string: ("--yes-label", string),
++    "no_label": lambda string: ("--no-label", string),
++    "extra_button": lambda enable: _simple_option("--extra-button", enable),
+     "help": lambda enable: _simple_option("--help", enable),
+     "help_button": lambda enable: _simple_option("--help-button", enable),
+     "help_label": lambda string: ("--help-label", string),
+@@ -524,7 +527,7 @@
+ 
+     """
+ 
+-    def __init__(self, dialog="dialog", DIALOGRC=None, compat="dialog",
++    def __init__(self, dialog="cdialog", DIALOGRC=None, compat="dialog",
+                  use_stdout=None):
+         """Constructor for Dialog instances.
+ 
