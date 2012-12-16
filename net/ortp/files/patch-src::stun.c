ae1e412baa11721bee781645e08eca5e8e9c0d14
Automaton <nobody@home.ok>
2012-12-16 11:27:48 +0100

    Import net/ortp version 0.13.0_1

---
+++ net/ortp/files/patch-src::stun.c
@@ -0,0 +1,14 @@
+
+$FreeBSD: ports/net/ortp/files/patch-src::stun.c,v 1.2 2012/11/17 06:00:09 svnexp Exp $
+
+--- src/stun.c
++++ src/stun.c
+@@ -768,7 +768,7 @@
+ 	read(fd,&tick,sizeof(tick));
+ 	closesocket(fd);
+       }
+-#elif defined(__linux) 
++#elif defined(__linux) || defined (__FreeBSD__) 
+       {
+  	fd_set fdSet;
+ 	int maxFd=0;
