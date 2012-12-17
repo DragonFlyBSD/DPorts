f46b5bfaa0f9034d61d1272be721e315064023e5
Automaton <nobody@home.ok>
2012-12-17 21:58:26 +0100

    Import irc/slirc version 0.12_4

---
+++ irc/slirc/files/patch-lru.c
@@ -0,0 +1,25 @@
+
+$FreeBSD: ports/irc/slirc/files/patch-lru.c,v 1.3 2012/11/17 05:58:30 svnexp Exp $
+
+--- lru.c.orig	Sun Aug 22 13:14:28 1999
++++ lru.c	Sun Aug 22 17:15:12 2004
+@@ -253,8 +253,9 @@
+ 	if (el) {
+ 		do {
+ 			if (!(p = index(el->str, '!')))
+-				p = "NoBang";
+-			len = p - el->str;
++				len = strlen(el->str);
++			else
++				len = p - el->str;
+ 			if (ix + len > SLtt_Screen_Cols - 2)
+ 				break;
+ 			if (ix) {
+@@ -283,6 +284,7 @@
+ 					Col = ColLRUmesg;
+ 					break;
+ 				case 'm':
++				break;
+ 				}
+ 				SLsmg_set_color(Col);
+ 			}
