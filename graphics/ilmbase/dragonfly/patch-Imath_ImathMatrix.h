de9dae8685d7873514accaadf1ae8dbf14bb2409
Automaton <nobody@home.ok>
2012-12-18 17:24:52 +0100

    Update graphics/ilmbase to version 1.0.2

---
+++ graphics/ilmbase/dragonfly/patch-Imath_ImathMatrix.h
@@ -0,0 +1,10 @@
+--- Imath/ImathMatrix.h.orig	2010-07-17 00:48:40.000000000 +0200
++++ Imath/ImathMatrix.h	2012-12-18 17:15:19.705334000 +0100
+@@ -49,6 +49,7 @@
+ #include "ImathVec.h"
+ #include "ImathShear.h"
+ 
++#include <cstring>
+ #include <iostream>
+ #include <iomanip>
+ 
