532e6fd2ed794737827d825252d63c246b7d17d0
Automaton <nobody@home.ok>
2012-12-19 06:28:45 +0100

    Import www/testlink version 1.9.5

---
+++ www/testlink/files/patch-install-index.php
@@ -0,0 +1,18 @@
+--- install/index.php.orig	2012-12-08 23:09:18.000000000 +0900
++++ install/index.php	2012-12-09 16:54:13.000000000 +0900
+@@ -39,7 +39,7 @@
+ 		<p>Please read Section on README file or go to www.teamst.org (Forum: TestLink 1.9.5 News,changes, etc) </p>
+ 		<p>Open <a target="_blank" href="../docs/testlink_installation_manual.pdf">Installation manual</a>
+ 		for more information or troubleshooting. You could also look at
+-		<a href="../README">README</a> or <a href="../CHANGELOG">Changes Log</a>.
++		<a href="../docs/README">README</a> or <a href="../docs/CHANGELOG">Changes Log</a>.
+ 		You are welcome to visit our <a target="_blank" href="http://www.teamst.org">
+ 		forum</a> to browse or discuss.
+ 		</p>
+@@ -51,4 +51,4 @@
+ 
+ </div>
+ </body>
+-</html>
+\ No newline at end of file
++</html>
