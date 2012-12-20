93c22a9b1940141a54d531a3a8757e5eb829c340
Automaton <nobody@home.ok>
2012-12-20 14:26:55 +0100

    Import databases/p5-SQL-Translator version 0.11010_1

---
+++ databases/p5-SQL-Translator/files/patch-lib::SQL::Translator::Parser::xSV.pm
@@ -0,0 +1,20 @@
+--- ./lib/SQL/Translator/Parser/xSV.pm.orig	2009-02-13 13:35:07.000000000 +0100
++++ ./lib/SQL/Translator/Parser/xSV.pm	2009-03-10 15:19:30.000000000 +0100
+@@ -64,7 +64,7 @@ C<SQL::Translator::Utils::normalize_name
+ # -------------------------------------------------------------------
+ 
+ use strict;
+-use vars qw($VERSION @EXPORT);
++use vars qw($VERSION @EXPORT_OK);
+ $VERSION = '1.59';
+ 
+ use Exporter;
+@@ -73,7 +73,7 @@ use Text::RecordParser;
+ use SQL::Translator::Utils qw(debug normalize_name);
+ 
+ use base qw(Exporter);
+-@EXPORT = qw(parse);
++@EXPORT_OK = qw(parse);
+ 
+ #
+ # Passed a SQL::Translator instance and a string containing the data
