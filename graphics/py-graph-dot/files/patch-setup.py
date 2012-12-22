f918303a050810c15b3c245eeb5d8376b5e2eaff
Automaton <nobody@home.ok>
2012-12-22 07:49:21 +0100

    Import graphics/py-graph-dot version 1.7.0_1

---
+++ graphics/py-graph-dot/files/patch-setup.py
@@ -0,0 +1,27 @@
+
+$FreeBSD: ports/graphics/py-graph-dot/files/patch-setup.py,v 1.6 2012/11/17 05:58:20 svnexp Exp $
+
+--- setup.py.orig
++++ setup.py
+@@ -4,12 +4,7 @@
+ import os
+ 
+ 
+-try:
+-    from setuptools import setup, find_packages
+-except ImportError as ie:
+-    import ez_setup
+-    ez_setup.use_setuptools()
+-    from setuptools import setup, find_packages
++from setuptools import setup, find_packages
+ 
+ # Startup
+ appname = "python-graph-dot"
+@@ -20,7 +15,6 @@
+         version = appversion,
+         namespace_packages = ["pygraph"],
+         packages = ["pygraph"] + [ os.path.join("pygraph", a) for a in find_packages("pygraph") ],
+-        install_requires = [ 'python-graph-core==%s' % appversion, 'pydot' ],
+         author = "Pedro Matiello",
+         author_email = "pmatiello@gmail.com",
+         description = "DOT support for python-graph",
