bba56caa460e8961d3efe999252756c61d31af9a
Automaton IAS <nobody@home.ok>
2012-12-27 16:07:22 +0100

    Import audio/sdl_mixer version 1.2.12_1

---
+++ audio/sdl_mixer/files/patch-SDL_mixer.h
@@ -0,0 +1,14 @@
+
+$FreeBSD: ports/audio/sdl_mixer/files/patch-SDL_mixer.h,v 1.2 2012/11/17 05:54:43 svnexp Exp $
+
+--- SDL_mixer.h	2001/05/08 07:44:56	1.1
++++ SDL_mixer.h	2001/05/08 07:48:13
+@@ -222,6 +222,8 @@
+ /* Close the mixer, halting all playing audio */
+ extern DECLSPEC void Mix_CloseAudio(void);
+ 
++extern DECLSPEC int Mix_GetMixerInfo(SDL_AudioSpec *, char *, int);
++
+ /* We'll use SDL for reporting errors */
+ #define Mix_SetError	SDL_SetError
+ #define Mix_GetError	SDL_GetError
