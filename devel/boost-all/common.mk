f29e7dd54af8e33d5a97417172569e9bdded4e03
Automaton IAS <nobody@home.ok>
2012-12-27 20:02:18 +0100

    Boost libs needs these make files

---
+++ devel/boost-all/common.mk
@@ -0,0 +1,10 @@
+
+PORTVERSION=	1.48.0
+PORTVERSUFFIX=	${PORTVERSION:C/\.[0-9]+$//}
+DISTNAME=	boost_${PORTVERSION:S/./_/g}
+
+CATEGORIES=	devel
+MAINTAINER=	churanov.port.maintainer@gmail.com
+
+CONFLICTS+=	boost-python-1* boost-1*
+MASTER_SITES=	SF/boost/boost/${PORTVERSION}
