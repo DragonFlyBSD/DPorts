f6bd186329bc93fe136b46145306162086d251dd
Automaton IAS <nobody@home.ok>
2012-12-28 13:57:19 +0100

    Import x11-toolkits/tkshape version 0.4_2

---
+++ x11-toolkits/tkshape/files/patch-unix-pkgIndex.tcl
@@ -0,0 +1,9 @@
+$FreeBSD: ports/x11-toolkits/tkshape/files/patch-unix-pkgIndex.tcl,v 1.3 2012/11/17 06:03:39 svnexp Exp $
+--- unix/pkgIndex.tcl	Mon Sep 11 16:27:28 2000
++++ unix/pkgIndex.tcl.new	Sun Mar 14 13:29:27 2004
+@@ -5,4 +5,4 @@
+ # broken OSes like SunOS4.  If you have a problem with this, tough!
+ 
+ package ifneeded Shape 0.4 "package require Tk 8\n\
+-	[list tclPkgSetup $dir Shape 0.4 {{libshape04.so.1.0 load shape}}]"
++	[list tclPkgSetup $dir Shape 0.4 {{libshape04.so.1 load shape}}]"
