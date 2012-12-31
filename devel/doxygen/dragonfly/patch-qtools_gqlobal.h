602136032c30b5d96af199e3519f8d47250e7c08
Automaton IAS <nobody@home.ok>
2012-12-31 03:42:59 +0100

    Import devel/doxygen version 1.8.2

---
+++ devel/doxygen/dragonfly/patch-qtools_gqlobal.h
@@ -0,0 +1,15 @@
+$NetBSD: patch-ac,v 1.9 2012/07/03 11:31:35 mef Exp $
+
+Add DragonFly
+
+--- qtools/qglobal.h.orig	2009-10-25 15:44:29.000000000 +0000
++++ qtools/qglobal.h
+@@ -118,7 +121,7 @@
+ #define _OS_RELIANTUNIX_
+ #elif defined(linux) || defined(__linux) || defined(__linux__) || defined(__GNU__) || defined(__GLIBC__)
+ #define _OS_LINUX_
+-#elif defined(__FreeBSD__)
++#elif defined(__FreeBSD__) || defined(__DragonFly__)
+ #define _OS_FREEBSD_
+ #elif defined(__NetBSD__)
+ #define _OS_NETBSD_
