ec331bc662ef04642030b5cbb498d9727b0989eb
Automaton IAS <nobody@home.ok>
2012-12-31 18:21:01 +0100

    Import textproc/plover version 2.2.0

---
+++ textproc/plover/files/patch-setup.py
@@ -0,0 +1,16 @@
+
+$FreeBSD: ports/textproc/plover/files/patch-setup.py,v 1.2 2012/11/17 06:02:16 svnexp Exp $
+
+--- setup.py.orig
++++ setup.py
+@@ -24,8 +24,8 @@
+       package_dir={'plover':'plover'},
+       packages=['plover', 'plover.dictionary', 'plover.machine', 'plover.gui'],
+       package_data={'plover' : ['assets/*']},
+-      data_files=[('/usr/share/applications', ['application/Plover.desktop']),
+-                  ('/usr/share/pixmaps', ['plover/assets/plover_on.png']),],
++      data_files=[('%%PREFIX%%/share/applications', ['application/Plover.desktop']),
++                  ('%%PREFIX%%/share/pixmaps', ['plover/assets/plover_on.png']),],
+       scripts=['application/plover'],
+       requires=['serial', 'Xlib', 'wx', 'lockfile'],
+       platforms=['GNU/Linux'],
