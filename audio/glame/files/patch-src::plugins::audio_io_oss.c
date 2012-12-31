90679e63734fff6d643652bfeb1a425ad2679b8f
Automaton IAS <nobody@home.ok>
2012-12-31 17:10:17 +0100

    Import audio/glame version 2.0.1_22

---
+++ audio/glame/files/patch-src::plugins::audio_io_oss.c
@@ -0,0 +1,17 @@
+
+$FreeBSD: ports/audio/glame/files/patch-src::plugins::audio_io_oss.c,v 1.4 2012/11/17 05:54:32 svnexp Exp $
+
+--- src/plugins/audio_io_oss.c	2003/07/01 12:26:28	1.1
++++ src/plugins/audio_io_oss.c	2003/07/01 12:26:41
+@@ -26,9 +26,9 @@
+ 
+ #ifdef HAVE_OSS_LINUX
+ #include <linux/soundcard.h>
+-#elif HAVE_OSS_SYS
++#elif defined(HAVE_OSS_SYS)
+ #include <sys/soundcard.h>
+-#elif HAVE_OSS_MACHINE
++#elif defined(HAVE_OSS_MACHINE)
+ #include <machine/soundcard.h>
+ #else
+ #error BUG in OSS configuration.
