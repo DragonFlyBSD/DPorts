741e2991ff306f38cc0d70493b3bc1dc9db58a4c
Automaton IAS <nobody@home.ok>
2013-01-06 18:44:25 +0100

    Import www/firefox version 17.0.1,1

---
+++ www/firefox/dragonfly/patch-gfx_angle_src_compiler_osinclude.h
@@ -0,0 +1,12 @@
+$NetBSD: patch-bi,v 1.5 2012/09/06 12:08:51 ryoon Exp $
+
+--- gfx/angle/src/compiler/osinclude.h.orig	2012-08-27 04:49:10.000000000 +0000
++++ gfx/angle/src/compiler/osinclude.h
+@@ -16,6 +16,7 @@
+ #define ANGLE_OS_WIN
+ #elif defined(__APPLE__) || defined(__linux__) || \
+       defined(__FreeBSD__) || defined(__OpenBSD__) || \
++      defined(__NetBSD__) || defined(__DragonFly__) || \
+       defined(__sun) || defined(ANDROID) || \
+       defined(__GLIBC__) || defined(__GNU__)
+ #define ANGLE_OS_POSIX
