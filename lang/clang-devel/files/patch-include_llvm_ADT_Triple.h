c80fa9a6447a744b0d9c39de642dff37ab0a9e23
Automaton IAS <nobody@home.ok>
2013-01-20 20:48:49 +0100

    Update lang/clang-devel to version 3.3.r172076

---
+++ lang/clang-devel/files/patch-include_llvm_ADT_Triple.h
@@ -0,0 +1,17 @@
+
+$FreeBSD: ports/lang/clang-devel/files/patch-include_llvm_ADT_Triple.h,v 1.1 2013/01/10 18:52:06 svnexp Exp $
+
+--- include/llvm/ADT/Triple.h.orig
++++ include/llvm/ADT/Triple.h
+@@ -296,6 +296,11 @@
+     return isMacOSX() || getOS() == Triple::IOS;
+   }
+ 
++  /// isOSFreeBSD - Is this FreeBSD OS
++  bool isOSFreeBSD() const {
++    return getOS() == Triple::FreeBSD;
++  }
++
+   /// \brief Tests for either Cygwin or MinGW OS
+   bool isOSCygMing() const {
+     return getOS() == Triple::Cygwin || getOS() == Triple::MinGW32;
--- lang/clang-devel/files/patch-tools_clang_include_clang_Analysis_Analyses_FormatString.h
+++ lang/clang-devel/files/patch-tools_clang_include_clang_Analysis_Analyses_FormatString.h
@@ -1,16 +1,26 @@
 
-$FreeBSD: ports/lang/clang-devel/files/patch-tools_clang_include_clang_Analysis_Analyses_FormatString.h,v 1.3 2012/11/17 05:58:48 svnexp Exp $
+$FreeBSD: ports/lang/clang-devel/files/patch-tools_clang_include_clang_Analysis_Analyses_FormatString.h,v 1.4 2013/01/10 18:52:06 svnexp Exp $
 
 --- tools/clang/include/clang/Analysis/Analyses/FormatString.h.orig
 +++ tools/clang/include/clang/Analysis/Analyses/FormatString.h
-@@ -141,6 +141,10 @@
+@@ -115,9 +115,10 @@
+   enum Kind {
+     InvalidSpecifier = 0,
+       // C99 conversion specifiers.
++    bArg, // FreeBSD extension
+     cArg,
+     dArg,
+-    DArg, // Apple extension
++    DArg, // Apple or FreeBSD extension
+     iArg,
+     IntArgBeg = dArg, IntArgEnd = iArg,
+ 
+@@ -147,7 +148,7 @@
      SArg,
  
      // ** Printf-specific **
+-
 +    
-+    // FreeBSD specific specifiers
-+    bArg,
-+    DArg,
- 
      // Objective-C specific specifiers.
      ObjCObjArg,  // '@'
+     ObjCBeg = ObjCObjArg, ObjCEnd = ObjCObjArg,
