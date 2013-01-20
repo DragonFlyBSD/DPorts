6b5c8d13e006dfef05d63f24a2f361cf114f8253
Automaton IAS <nobody@home.ok>
2013-01-20 11:30:20 +0100

    www/firefox: Fix firefox 18 for i386

--- www/firefox/dragonfly/patch-gfx_qcms_qcmstypes.h
+++ www/firefox/dragonfly/patch-gfx_qcms_qcmstypes.h
@@ -1,11 +1,11 @@
---- gfx/qcms/qcmstypes.h.intermediate	2013-01-20 04:13:24.534102000 +0100
-+++ gfx/qcms/qcmstypes.h	2013-01-20 04:15:01.354238000 +0100
+--- gfx/qcms/qcmstypes.h.intermediate
++++ gfx/qcms/qcmstypes.h
 @@ -11,6 +11,8 @@
- #if defined (__SVR4) && defined (__sun)
- /* int_types.h gets included somehow, so avoid redefining the types differently */
- #include <sys/int_types.h>
-+#elif defined (__DragonFly__)
-+#include <stdint>
- #elif defined (_AIX)
+ 
+ #if defined (_AIX)
  #include <sys/types.h>
- #elif defined(__OpenBSD__)
++#elif defined (__DragonFly__)
++#include <stdlib.h>
+ #elif __OS2__
+ #include <stdlib.h>
+ #endif
