10c1d910bd9061740cb61748e381b3428bd8094e
Automaton <nobody@home.ok>
2013-03-04 21:57:26 +0100

    Update security/nss to version 3.14.3

--- security/nss/dragonfly/patch-coreconf_config.mk
+++ security/nss/dragonfly/patch-coreconf_config.mk
@@ -1,15 +1,15 @@
---- ../../security/coreconf/config.mk.orig	2012-05-03 11:42:58.000000000 +0200
-+++ ../../security/coreconf/config.mk	2012-12-27 22:48:30.000000000 +0100
-@@ -31,7 +31,7 @@
+--- ../../security/coreconf/config.mk.orig	2013-01-08 16:19:07.000000000 +0000
++++ ../../security/coreconf/config.mk
+@@ -31,7 +31,7 @@ endif
  #######################################################################
  
  TARGET_OSES = FreeBSD BSD_OS NetBSD OpenUNIX OS2 QNX Darwin BeOS OpenBSD \
--              AIX RISCOS WINNT WIN95 Linux
-+              AIX RISCOS WINNT WIN95 Linux DragonFly
+-              AIX RISCOS WINNT WIN95 Linux Android
++              AIX RISCOS WINNT WIN95 Linux Android DragonFly
  
  ifeq (,$(filter-out $(TARGET_OSES),$(OS_TARGET)))
  include $(CORE_DEPTH)/coreconf/$(OS_TARGET).mk
-@@ -175,3 +175,4 @@
+@@ -175,3 +175,4 @@ endif
  DEFINES += -DUSE_UTIL_DIRECTLY
  USE_UTIL_DIRECTLY = 1
  
