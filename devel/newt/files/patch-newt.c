4c5028f1a824233d7340bef51bf5a0aef0ab2862
Automaton <nobody@home.ok>
2013-03-06 10:36:05 +0100

    Import devel/newt version 0.52.14

---
+++ devel/newt/files/patch-newt.c
@@ -0,0 +1,12 @@
+
+$FreeBSD: ports/devel/newt/files/patch-newt.c,v 1.3 2012/11/17 05:56:01 svnexp Exp $
+
+--- newt.c	2009-09-24 11:03:09.000000000 -0400
++++ newt.c	2010-01-06 01:04:01.000000000 -0500
+@@ -99,4 +99,6 @@
+ 	{ "\033V",		NEWT_KEY_PGUP, 		NULL },
+ 	{ "\033v",		NEWT_KEY_PGUP, 		NULL },
++	{ "\033[G",             NEWT_KEY_PGDN,          NULL },
++	{ "\033[I",             NEWT_KEY_PGUP,          NULL },
+         { "\026",		NEWT_KEY_PGDN,		NULL },
+ 
