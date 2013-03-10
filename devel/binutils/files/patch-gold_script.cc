9ab10e50791365fd3075c1d8ba9c54647718ae3f
Automaton <nobody@home.ok>
2013-03-10 22:06:06 +0100

    Tweak devel/binutils version 2.23.1

--- devel/binutils/files/patch-gold_script.cc
+++ devel/binutils/files/patch-gold_script.cc
@@ -1,5 +1,5 @@
 
-$FreeBSD: ports/devel/binutils/files/patch-gold_script.cc,v 1.2 2012/11/17 05:55:35 svnexp Exp $
+$FreeBSD: devel/binutils/files/patch-gold_script.cc 303529 2012-09-02 05:07:01Z linimon $
 
 --- gold/script.cc.dist	2012-09-01 19:52:34.000000000 -0500
 +++ gold/script.cc	2012-09-01 19:53:22.000000000 -0500
