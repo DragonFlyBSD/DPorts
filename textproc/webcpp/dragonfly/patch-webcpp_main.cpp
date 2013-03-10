8638d6b6bc61c56f0e08ee4ef92c2455c518f7ac
Automaton <nobody@home.ok>
2013-03-10 02:06:44 +0100

    Import textproc/webcpp version 0.8.4

---
+++ textproc/webcpp/dragonfly/patch-webcpp_main.cpp
@@ -0,0 +1,10 @@
+--- webcpp/main.cpp.orig	2004-01-05 00:52:47.000000000 +0000
++++ webcpp/main.cpp
+@@ -23,6 +23,7 @@
+ #endif
+ 
+ #include "driver.h"
++#include <cstdlib>
+ #include <fstream>
+ using namespace std;
+ 
