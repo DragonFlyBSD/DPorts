99538a9cf3713e8272fe3a6b5f9465a3400e138e
Automaton <nobody@home.ok>
2013-03-19 19:08:53 +0100

    Tweak www/py-turbogears version 1.5.1_1

---
+++ www/py-turbogears/dragonfly/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2011-11-25 13:22:04.000000000 +0000
++++ setup.py
+@@ -58,7 +58,7 @@ toscawidgets = [
+ testtools = [
+     'nose >= 0.9.3',
+     'WebTest >= 1.2.2',
+-    'python-dateutil >= 1.5, < 2.0a',
++    'python-dateutil >= 1.5',
+ ]
+ 
+ tgtesttools = testtools
