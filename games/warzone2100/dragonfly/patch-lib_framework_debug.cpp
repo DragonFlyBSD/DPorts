ece33342ed2a9c749f77f51481d19881695642e8
Automaton <nobody@home.ok>
2013-03-19 10:41:21 +0100

    Update games/warzone2100 to version 3.1.0

---
+++ games/warzone2100/dragonfly/patch-lib_framework_debug.cpp
@@ -0,0 +1,10 @@
+--- lib/framework/debug.cpp.orig	2013-01-14 23:54:07.000000000 +0000
++++ lib/framework/debug.cpp
+@@ -27,6 +27,7 @@
+ #include "frame.h"
+ 
+ #include <string.h>
++#include <strings.h>
+ #include <stdio.h>
+ #include <time.h>
+ #include "string_ext.h"
