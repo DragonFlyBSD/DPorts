cb765fbd8a224f7e0bead2edec0f00c04b296ef8
Automaton <nobody@home.ok>
2013-03-20 10:17:47 +0100

    Tweak ftp/gwget version 1.0.4_6

---
+++ ftp/gwget/dragonfly/patch-src_gwget-application.h
@@ -0,0 +1,11 @@
+--- src/gwget-application.h.orig	2009-06-16 17:58:06.000000000 +0000
++++ src/gwget-application.h
+@@ -18,7 +18,7 @@
+ #ifndef GWGET_APPLICATION_H
+ #define GWGET_APPLICATION_H
+ 
+-#include <glib/gerror.h>
++#include <glib.h>
+ #include <glib-object.h>
+ 
+ G_BEGIN_DECLS
