035c8924d91e932e752378ae2ec65cea1c82739e
Automaton <nobody@home.ok>
2013-03-20 09:19:26 +0100

    Tweak games/osgg version 0.99_7

---
+++ games/osgg/dragonfly/patch-main.cpp
@@ -0,0 +1,10 @@
+--- main.cpp.orig	2013-03-20 08:14:38.565285000 +0000
++++ main.cpp
+@@ -23,6 +23,7 @@
+ #include <vector>
+ #include <list>
+ #include <math.h>
++#include <unistd.h>
+ #include <fstream>
+ #include <iostream>
+ #include <sstream>
