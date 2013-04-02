a25c78d9c91d5a0e63feb46e399dd87bcbe8ecb7
Automaton <nobody@home.ok>
2013-04-02 20:19:59 +0200

    Update multimedia/mplayer to version 1.1.r20130308

--- multimedia/mplayer/dragonfly/patch-stream_open.c
+++ multimedia/mplayer/dragonfly/patch-stream_open.c
@@ -1,10 +1,10 @@
---- stream/open.c.orig	2011-03-21 20:21:32.000000000 +0100
-+++ stream/open.c	2013-01-16 02:02:21.184937000 +0100
-@@ -43,6 +43,7 @@
+--- stream/open.c.orig	2012-10-18 12:17:58.000000000 +0000
++++ stream/open.c
+@@ -39,6 +39,7 @@
+ 
+ /// We keep these 2 for the gui atm, but they will be removed.
+ char* cdrom_device=NULL;
++int dvd_angle=1;
+ int dvd_chapter=1;
  int dvd_last_chapter=0;
  char* dvd_device=NULL;
- int dvd_title=0;
-+int dvd_angle=1;
- char *bluray_device=NULL;
- 
- // Open a new stream  (stdin/file/vcd/url)
--- multimedia/mplayer/dragonfly/patch-stream_stream__dvd.c
+++ multimedia/mplayer/dragonfly/patch-stream_stream__dvd.c
@@ -1,9 +1,9 @@
---- stream/stream_dvd.c.orig	2012-06-02 18:35:44.000000000 +0200
-+++ stream/stream_dvd.c	2013-01-16 02:03:33.485038000 +0100
-@@ -44,7 +44,6 @@
- #include "libmpdemux/demuxer.h"
+--- stream/stream_dvd.c.orig	2012-10-21 14:56:50.000000000 +0000
++++ stream/stream_dvd.c
+@@ -45,7 +45,6 @@
  
  static char* dvd_device_current;
+ static int dvd_title;
 -int dvd_angle=1;
  
  #define	LIBDVDREAD_VERSION(maj,min,micro)	((maj)*10000 + (min)*100 + (micro))
