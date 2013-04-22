b0758aeb9bdfc619fada1f32e4c922a9dad1c653
Automaton <nobody@home.ok>
2013-04-22 12:54:23 +0200

    Tweak french/aster version 10.8.0.3

--- french/aster/dragonfly/patch-setup.py
+++ french/aster/dragonfly/patch-setup.py
@@ -1,13 +1,16 @@
 --- setup.py.orig	2013-01-28 12:35:55.000000000 +0000
 +++ setup.py
-@@ -345,6 +345,10 @@ def main():
+@@ -345,6 +345,13 @@ def main():
              cfg['ARCH'] = 'x86_64'
        else:
           cfg['IFDEF'] = 'LINUX'
 +   elif plt.startswith('dragonfly'):
 +      common_libs = []
 +      cfg['IFDEF']='FREEBSD'
-+      cfg['ARCH'] = machine
++      if machine == 'x86_64':
++         cfg['ARCH'] = machine
++      else:
++         cfg['ARCH'] = 'x86'
     elif plt.startswith('freebsd'):
        common_libs = []
        cfg['IFDEF']='FREEBSD'
