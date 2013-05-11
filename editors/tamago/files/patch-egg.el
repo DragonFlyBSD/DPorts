476fcb729b582b75d7d7de45e811cab69eece6e7
Automaton <nobody@home.ok>
2013-05-11 11:45:36 +0200

    Update editors/tamago to version 4.0.6.0.20041122.19_6

--- editors/tamago/files/patch-egg.el
+++ editors/tamago/files/patch-egg.el
@@ -1,8 +1,14 @@
---- egg.el.orig	2013-03-30 17:08:47.000000000 +0900
-+++ egg.el	2013-03-30 17:11:22.000000000 +0900
-@@ -169,7 +169,8 @@
+--- egg.el.orig	2013-05-05 14:02:05.000000000 +0900
++++ egg.el	2013-05-05 14:02:05.000000000 +0900
+@@ -167,16 +167,22 @@
+ 	  (setq egg-modefull-mode t)
+ 	  (its-define-select-keys egg-modefull-map))
        (setq egg-modeless-mode t))
-     (setq inactivate-current-input-method-function 'egg-mode)
+-    (setq inactivate-current-input-method-function 'egg-mode)
++    (set (if (boundp 'deactivate-current-input-method-function)
++	     'deactivate-current-input-method-function
++	   'inactivate-current-input-method-function)
++	 'egg-mode)
      (setq describe-current-input-method-function 'egg-help)
 -    (make-local-hook 'input-method-activate-hook)
 +    (if (fboundp 'make-local-hook)
@@ -10,12 +16,25 @@
      (add-hook 'input-method-activate-hook 'its-set-mode-line-title nil t)
      (if (eq (selected-window) (minibuffer-window))
  	(add-hook 'minibuffer-exit-hook 'egg-exit-from-minibuffer))
-@@ -184,7 +185,7 @@
+     (run-hooks 'egg-mode-hook)))
+ 
+ (defun egg-exit-from-minibuffer ()
+-  (inactivate-input-method)
++  (if (boundp 'deactivate-input-method)
++      deactivate-input-method
++    inactivate-input-method)
+   (if (<= (minibuffer-depth) 1)
+       (remove-hook 'minibuffer-exit-hook 'egg-exit-from-minibuffer)))
+ 
+@@ -184,7 +190,10 @@
  
  (defun egg-self-insert-char ()
    (interactive)
 -  (its-start last-command-char (and (eq last-command 'egg-use-context)
-+  (its-start last-command-event (and (eq last-command 'egg-use-context)
++  (its-start (if (boundp 'last-command-event)
++		 last-command-event
++	       last-command-char)
++	     (and (eq last-command 'egg-use-context)
  				    egg-context)))
  
  (defun egg-remove-all-text-properties (from to &optional object)
