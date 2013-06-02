a1a622d4d3ca643f29aa18254768a002894ff346
Automaton <nobody@home.ok>
2013-06-02 16:13:05 +0200

    Import x11/fluxbg version 0.7_12

---
+++ x11/fluxbg/dragonfly/patch-src_fluxbg.cc
@@ -0,0 +1,10 @@
+--- src/fluxbg.cc.orig	2003-10-05 09:14:05.000000000 +0000
++++ src/fluxbg.cc
+@@ -16,6 +16,7 @@
+ 
+ 
+ #include <iostream>
++#include <cstring>
+ #include <string>
+ #include <stdlib.h>
+ #include <gtkmm.h>
