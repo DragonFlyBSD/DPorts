0b9dbd7421239dddd4427febd8443f085690872b
Automaton <nobody@home.ok>
2013-06-07 10:50:54 +0200

    Update www/libxul to version 17.0.6

--- www/libxul/dragonfly/patch-gfx_angle_src_compiler_osinclude.h
+++ www/libxul/dragonfly/patch-gfx_angle_src_compiler_osinclude.h
@@ -1,10 +1,10 @@
---- gfx/angle/src/compiler/osinclude.h.orig	2013-01-03 17:43:09.000000000 +0000
+--- gfx/angle/src/compiler/osinclude.h.orig	2013-05-09 18:12:18.000000000 +0000
 +++ gfx/angle/src/compiler/osinclude.h
 @@ -16,6 +16,7 @@
  #define ANGLE_OS_WIN
  #elif defined(__APPLE__) || defined(__linux__) || \
        defined(__FreeBSD__) || defined(__OpenBSD__) || \
 +      defined(__DragonFly__) || \
-       defined(__sun) || defined(ANDROID)
- #define ANGLE_OS_POSIX
- #else
+       defined(__sun) || defined(ANDROID) || \
+       defined(__GLIBC__) || defined(__GNU__) || \
+       defined(__QNX__)
