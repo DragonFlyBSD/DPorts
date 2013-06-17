d00f613199acdb5a4727e51199d7ce80eb101add
DPorts Builder <nobody@home.ok>
2013-06-17 02:50:18 +0200

    Import net/vnc version 4.1.3_5

---
+++ net/vnc/dragonfly/patch-unix_tx_TXImage.cxx
@@ -0,0 +1,10 @@
+--- tx/TXImage.cxx.orig	2008-10-16 17:16:21.000000000 +0200
++++ tx/TXImage.cxx	2013-01-22 17:26:45.157944000 +0100
+@@ -21,6 +21,7 @@
+ 
+ 
+ #include <stdio.h>
++#include <stdlib.h>
+ #include <strings.h>
+ #include <sys/types.h>
+ #include <sys/ipc.h>
---
+++ net/vnc/dragonfly/patch-unix_x0vncserver_Image.cxx
@@ -0,0 +1,10 @@
+--- x0vncserver/Image.cxx.orig	2008-10-16 17:16:21.000000000 +0200
++++ x0vncserver/Image.cxx	2013-01-22 17:30:03.635038000 +0100
+@@ -21,6 +21,7 @@
+ 
+ 
+ #include <stdio.h>
++#include <stdlib.h>
+ #include <sys/types.h>
+ #include <sys/ipc.h>
+ #include <sys/shm.h>
