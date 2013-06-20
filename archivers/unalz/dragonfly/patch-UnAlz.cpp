8f52f27f88854db88ac61fe6e59b3fda1bcc804d
DPorts Builder <nobody@home.ok>
2013-06-21 01:44:57 +0200

    Tweak archivers/unalz version 0.65_1

--- archivers/unalz/dragonfly/patch-UnAlz.cpp
+++ archivers/unalz/dragonfly/patch-UnAlz.cpp
@@ -1,23 +1,11 @@
---- UnAlz.cpp.orig	2009-04-01 06:00:27.000000000 +0000
+--- UnAlz.cpp.intermediate	2013-06-20 23:42:25.780595000 +0000
 +++ UnAlz.cpp
 @@ -455,7 +455,7 @@ BOOL CUnAlz::ReadLocalFileheader()
  	size_t size;
  	char inbuf[ICONV_BUF_SIZE];
  	char outbuf[ICONV_BUF_SIZE];
 -#if defined(__FreeBSD__) || defined(__CYGWIN__) ||  defined(__NetBSD__)
-+#if defined(__FreeBSD__) || defined(__DragonFly__) ||  defined(__NetBSD__)
++#if defined(__FreeBSD__) || defined(__FreeBSD__) ||  defined(__NetBSD__)
  	const char *inptr = inbuf;
  #else
  	char *inptr = inbuf;
-@@ -1907,7 +1907,11 @@ void CUnAlz::DecryptingData(int nSize, B
- ////////////////////////////////////////////////////////////////////////////////////////////////////
- UINT32 CUnAlz::CRC32(UINT32 l, BYTE c)
- {
-+#ifdef __DragonFly__
-+	const z_crc_t *CRC_TABLE = get_crc_table();
-+#else
- 	const unsigned long *CRC_TABLE = get_crc_table();
-+#endif
- 	return CRC_TABLE[(l ^ c) & 0xff] ^ (l >> 8);
- }
- 
