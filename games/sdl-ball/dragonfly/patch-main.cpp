4048fda877427c2580aa3a52510a419f4b3c2b86
DPorts Builder <nobody@home.ok>
2013-08-17 20:50:18 +0200

    Tweak games/sdl-ball version 1.01_7

---
+++ games/sdl-ball/dragonfly/patch-main.cpp
@@ -0,0 +1,10 @@
+--- main.cpp.intermediate	2013-08-17 18:39:04.042837000 +0000
++++ main.cpp
+@@ -25,6 +25,7 @@
+ #include <SDL/SDL.h>
+ #include <SDL/SDL_ttf.h>
+ #include <SDL/SDL_image.h>
++#include <unistd.h>
+ #include <math.h>
+ #include <string.h>
+ #include <list>
