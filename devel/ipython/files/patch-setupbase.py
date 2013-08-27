a03a58d0afea00bf30b6bbb4c688b38cbe901e60
DPorts Builder <nobody@home.ok>
2013-08-26 20:22:42 -0700

    Update devel/ipython to version 1.0.0

--- devel/ipython/files/patch-setupbase.py
+++ devel/ipython/files/patch-setupbase.py
@@ -1,28 +1,18 @@
 
-$FreeBSD: devel/ipython/files/patch-setupbase.py 313302 2013-03-02 22:24:45Z olgeni $
+$FreeBSD: devel/ipython/files/patch-setupbase.py 325353 2013-08-25 16:13:06Z olgeni $
 
 --- setupbase.py.orig
 +++ setupbase.py
-@@ -201,7 +201,8 @@
+@@ -208,7 +208,7 @@
      """
  
      docdirbase  = pjoin('share', 'doc', 'ipython')
 -    manpagebase = pjoin('share', 'man', 'man1')
 +    manpagebase = pjoin('man', 'man1')
-+    examplebase = pjoin('share', 'examples', 'ipython')
  
      # Simple file lists can be made by hand
      manpages = [f for f in glob(pjoin('docs','man','*.1.gz')) if isfile(f)]
-@@ -215,7 +216,7 @@
-     example_files = make_dir_struct(
-         'data',
-         pjoin('docs','examples'),
--        pjoin(docdirbase,'examples')
-+        pjoin(examplebase)
-     )
-     manual_files = make_dir_struct(
-         'data',
-@@ -225,7 +226,6 @@
+@@ -232,7 +232,6 @@
  
      # And assemble the entire output list
      data_files = [ (manpagebase, manpages),
