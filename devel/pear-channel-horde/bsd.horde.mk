364b21b004c183456420aff8c5c4e911490b8a37
DPorts Builder <nobody@home.ok>
2013-09-10 05:44:30 -0700

    Tweak devel/pear-channel-horde version 20120210

--- devel/pear-channel-horde/bsd.horde.mk
+++ devel/pear-channel-horde/bsd.horde.mk
@@ -1,4 +1,4 @@
-# $FreeBSD: devel/pear-channel-horde/bsd.horde.mk 319936 2013-06-04 22:35:08Z mm $
+# $FreeBSD: devel/pear-channel-horde/bsd.horde.mk 325518 2013-08-28 11:48:25Z mm $
 
 MASTER_SITES?=	http://pear.horde.org/get/
 
@@ -83,6 +83,7 @@ horde-Horde_SessionHandler-DEPEND=	${PEARDIR}/Horde/SessionHandler.php:${PORTSDI
 horde-Horde_Scheduler-DEPEND=	${PEARDIR}/Horde/Scheduler.php:${PORTSDIR}/devel/pear-Horde_Scheduler
 horde-Horde_Scribe-DEPEND=	${PEARDIR}/Horde/Scribe.php:${PORTSDIR}/net/pear-Horde_Scribe
 horde-Horde_Share-DEPEND=	${PEARDIR}/Horde/Share/Base.php:${PORTSDIR}/security/pear-Horde_Share
+horde-Horde_Smtp-DEPEND=	${PEARDIR}/Horde/Smtp.php:${PORTSDIR}/mail/pear-Horde_Smtp
 horde-Horde_SpellChecker-DEPEND=	${PEARDIR}/Horde/SpellChecker.php:${PORTSDIR}/textproc/pear-Horde_SpellChecker
 horde-Horde_Stream-DEPEND=	${PEARDIR}/Horde/Stream.php:${PORTSDIR}/devel/pear-Horde_Stream
 horde-Horde_Stream_Filter-DEPEND=	${PEARDIR}/Horde/Stream/Filter/Crc32.php:${PORTSDIR}/devel/pear-Horde_Stream_Filter
