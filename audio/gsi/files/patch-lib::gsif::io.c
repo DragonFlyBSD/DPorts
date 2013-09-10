66372e940b11ffe974fa49d4fd5472f544885c71
DPorts Builder <nobody@home.ok>
2013-09-10 15:12:06 -0700

    Tweak audio/gsi version 0.9.6_1

--- audio/gsi/files/patch-lib::gsif::io.c
+++ audio/gsi/files/patch-lib::gsif::io.c
@@ -1,4 +1,4 @@
-$FreeBSD: ports/audio/gsi/files/patch-lib::gsif::io.c,v 1.2 2012/11/17 05:54:33 svnexp Exp $
+$FreeBSD: audio/gsi/files/patch-lib::gsif::io.c 300895 2012-07-14 12:56:14Z beat $
 
 --- lib/gsif/io.c.orig	Sat Jan 10 22:22:35 2004
 +++ lib/gsif/io.c	Sat Jan 10 22:21:32 2004
