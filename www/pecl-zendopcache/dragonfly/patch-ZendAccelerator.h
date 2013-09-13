b974d11b367a04fbbd1f27e6973c285263d29c68
DPorts Builder <nobody@home.ok>
2013-09-13 15:39:45 -0700

    Import www/pecl-zendopcache version 7.0.2

---
+++ www/pecl-zendopcache/dragonfly/patch-ZendAccelerator.h
@@ -0,0 +1,11 @@
+--- ZendAccelerator.h.orig	2013-06-05 06:28:53.000000000 +0000
++++ ZendAccelerator.h
+@@ -94,7 +94,7 @@
+ #ifndef ZEND_WIN32
+ extern int lock_file;
+ 
+-# if defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || (defined(__APPLE__) && defined(__MACH__)/* Darwin */) || defined(__OpenBSD__) || defined(__NetBSD__)
++# if defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || (defined(__APPLE__) && defined(__MACH__)/* Darwin */) || defined(__OpenBSD__) || defined(__NetBSD__) || defined(__DragonFly__)
+ #  define FLOCK_STRUCTURE(name, type, whence, start, len) \
+ 		struct flock name = {start, len, -1, type, whence}
+ # elif defined(__svr4__)
