2bc7bfa9ed974e1427c5fad157f280499a05e6c5
DPorts Builder <nobody@home.ok>
2013-09-13 09:17:05 -0700

    Update net-p2p/transmission-cli to version 2.82

--- net-p2p/transmission-cli/dragonfly/patch-libtransmission_platform-quota.c
+++ net-p2p/transmission-cli/dragonfly/patch-libtransmission_platform-quota.c
@@ -1,15 +1,15 @@
---- libtransmission/platform.c.orig	2013-06-26 02:35:25.647458000 +0000
-+++ libtransmission/platform.c
-@@ -14,6 +14,8 @@
+--- libtransmission/platform-quota.c.orig	2013-08-09 02:45:44.822416000 +0000
++++ libtransmission/platform-quota.c
+@@ -20,6 +20,8 @@
   #include <sys/types.h> /* types needed by quota.h */
-  #ifdef __FreeBSD__
+  #if defined(__FreeBSD__) || defined(__OpenBSD__)
    #include <ufs/ufs/quota.h> /* quotactl() */
 + #elif defined (__DragonFly__)
 +  #include <vfs/ufs/quota.h> /* quotactl */
   #elif defined (__sun)
    #include <sys/fs/ufs_quota.h> /* quotactl */
   #else
-@@ -746,12 +748,16 @@ getblkdev (const char * path)
+@@ -198,12 +200,17 @@ getblkdev (const char * path)
  static int64_t
  getquota (const char * device)
  {
@@ -22,17 +22,18 @@
    int64_t freespace;
    int64_t spaceused;
  
--#if defined(__FreeBSD__) || defined(SYS_DARWIN)
-+#if defined(__FreeBSD__) || defined(__DragonFly__) || defined(SYS_DARWIN)
+-#if defined(__FreeBSD__) || defined(__OpenBSD__) || defined(SYS_DARWIN)
++#if defined(__FreeBSD__) || defined(__OpenBSD__) || defined(SYS_DARWIN) \
++ || defined(__DragonFly__)
    if (quotactl(device, QCMD(Q_GETQUOTA, USRQUOTA), getuid(), (caddr_t) &dq) == 0)
      {
  #elif defined(__sun)
-@@ -783,7 +789,7 @@ getquota (const char * device)
+@@ -235,7 +242,7 @@ getquota (const char * device)
            /* No quota enabled for this user */
            return -1;
          }
--#if defined(__FreeBSD__)
-+#if defined(__FreeBSD__) || defined(__DragonFly__)
+-#if defined(__FreeBSD__) || defined(__OpenBSD__)
++#if defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__DragonFly__)
        spaceused = (int64_t) dq.dqb_curblocks >> 1;
  #elif defined(SYS_DARWIN)
        spaceused = (int64_t) dq.dqb_curbytes;
