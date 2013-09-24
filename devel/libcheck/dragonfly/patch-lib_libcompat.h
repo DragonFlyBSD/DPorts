71063859fb43aa81881b3538376051fc647a4ae9
DPorts Builder <nobody@home.ok>
2013-09-24 03:21:18 -0700

    libcheck: manually fix iconv+

--- devel/libcheck/dragonfly/patch-lib_libcompat.h
+++ devel/libcheck/dragonfly/patch-lib_libcompat.h
@@ -1,12 +1,16 @@
 --- lib/libcompat.h.orig	2013-04-18 10:19:02.000000000 +0000
 +++ lib/libcompat.h
-@@ -132,7 +132,9 @@ struct itimerspec
+@@ -132,8 +132,13 @@ struct itimerspec
   */
  typedef int timer_t;
  
-+#ifndef __DragonFly__
++#ifdef __DragonFly__
++int clock_gettime(clockid_t clk_id, struct timespec *ts);
++int timer_create(clockid_t clockid, struct sigevent *sevp, timer_t *timerid);
++#else
  int clock_gettime(int clk_id, struct timespec *ts);
-+#endif
  int timer_create(int clockid, struct sigevent *sevp, timer_t *timerid);
++#endif
  int timer_settime(timer_t timerid, int flags, const struct itimerspec *new_value, struct itimerspec * old_value);
  int timer_delete(timer_t timerid);
+ #endif /* HAVE_LIBRT */
