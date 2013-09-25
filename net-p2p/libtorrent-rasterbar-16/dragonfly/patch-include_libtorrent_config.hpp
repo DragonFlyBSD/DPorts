648b184d69e64c639cbbe9f9acb3af08cc57999e
DPorts Builder <nobody@home.ok>
2013-09-25 08:22:43 -0700

    fix libtorrent-raster-16 manually

--- net-p2p/libtorrent-rasterbar-16/dragonfly/patch-include_libtorrent_config.hpp
+++ net-p2p/libtorrent-rasterbar-16/dragonfly/patch-include_libtorrent_config.hpp
@@ -1,11 +1,19 @@
---- include/libtorrent/config.hpp.orig	2012-11-30 05:29:34.000000000 +0000
+--- include/libtorrent/config.hpp.orig	2013-06-24 06:40:48.000000000 +0000
 +++ include/libtorrent/config.hpp
-@@ -182,6 +182,8 @@ POSSIBILITY OF SUCH DAMAGE.
- #define TORRENT_USE_LOCALE 0
- #define TORRENT_CLOSE_MAY_BLOCK 1
+@@ -37,6 +37,7 @@ POSSIBILITY OF SUCH DAMAGE.
+ #include <boost/version.hpp>
+ #include <stdio.h> // for snprintf
+ #include <limits.h> // for IOV_MAX
++#include <sys/param.h> // for os version
+ 
+ #if defined TORRENT_DEBUG_BUFFERS && !defined TORRENT_DISABLE_POOL_ALLOCATOR
+ #error TORRENT_DEBUG_BUFFERS only works if you also disable pool allocators with TORRENT_DISABLE_POOL_ALLOCATOR
+@@ -184,6 +185,8 @@ POSSIBILITY OF SUCH DAMAGE.
+ // execinfo.h is available in the MacOS X 10.5 SDK.
+ #define TORRENT_USE_EXECINFO MAC_OS_X_VERSION_MIN_REQUIRED >= 1050
  #endif
-+#elif defined __DragonFly__
++#elif defined (__DragonFly__) && __DragonFly_version < 300503
 +#define TORRENT_ICONV_ARG (char**)
  #else
  // FreeBSD has a reasonable iconv signature
- #define TORRENT_ICONV_ARG (const char**)
+ // unless we're on glibc
