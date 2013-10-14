36044e0ac2ca4fa9ea1b1138af5ed976b6d7b012
DPorts Builder <nobody@home.ok>
2013-10-14 14:34:15 -0700

    Tweak devel/flickrnet version 2.2.0,1

--- devel/flickrnet/files/patch-AssemblyInfo.cs
+++ devel/flickrnet/files/patch-AssemblyInfo.cs
@@ -1,5 +1,5 @@
 
-$FreeBSD: ports/devel/flickrnet/files/patch-AssemblyInfo.cs,v 1.2 2012/11/17 05:55:43 svnexp Exp $
+$FreeBSD: devel/flickrnet/files/patch-AssemblyInfo.cs 300896 2012-07-14 13:54:48Z beat $
 
 --- AssemblyInfo.cs.orig
 +++ AssemblyInfo.cs
