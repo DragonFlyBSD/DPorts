b4151e31e8bc50cd5d6e5639393b91ac251ae2ef
DPorts Builder <nobody@home.ok>
2013-10-15 01:56:46 -0700

    Tweak audio/rio500 version 0.7_2

--- audio/rio500/files/patch-src_rio__add__song.c
+++ audio/rio500/files/patch-src_rio__add__song.c
@@ -1,5 +1,5 @@
 
-$FreeBSD: ports/audio/rio500/files/patch-src_rio__add__song.c,v 1.2 2012/11/17 05:54:43 svnexp Exp $
+$FreeBSD: audio/rio500/files/patch-src_rio__add__song.c 300895 2012-07-14 12:56:14Z beat $
 
 --- src/rio_add_song.c.orig	Sun Aug 22 16:53:27 2004
 +++ src/rio_add_song.c	Sun Aug 22 16:55:17 2004
