d49ad6cd113237dc5dbe9b9796392e0520488c40
DPorts Builder <nobody@home.ok>
2013-10-14 23:48:58 -0700

    Tweak net-mgmt/cdpr version 2.2.1

--- net-mgmt/cdpr/files/patch-conffile.c
+++ net-mgmt/cdpr/files/patch-conffile.c
@@ -1,5 +1,5 @@
 
-$FreeBSD: ports/net-mgmt/cdpr/files/patch-conffile.c,v 1.2 2012/11/17 06:00:31 svnexp Exp $
+$FreeBSD: net-mgmt/cdpr/files/patch-conffile.c 300897 2012-07-14 14:29:18Z beat $
 
 --- conffile.c.orig
 +++ conffile.c
