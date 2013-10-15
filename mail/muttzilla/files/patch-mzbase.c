33fa25935e4136ef6db913b8330f0edeecaa43ef
DPorts Builder <nobody@home.ok>
2013-10-15 05:31:09 -0700

    Tweak mail/muttzilla version 0.40

--- mail/muttzilla/files/patch-mzbase.c
+++ mail/muttzilla/files/patch-mzbase.c
@@ -1,5 +1,5 @@
 
-$FreeBSD: ports/mail/muttzilla/files/patch-mzbase.c,v 1.2 2012/11/17 05:59:07 svnexp Exp $
+$FreeBSD: mail/muttzilla/files/patch-mzbase.c 300896 2012-07-14 13:54:48Z beat $
 
 --- mzbase.c.orig	Sun Feb 16 16:35:13 2003
 +++ mzbase.c	Sun Feb 16 16:35:28 2003
