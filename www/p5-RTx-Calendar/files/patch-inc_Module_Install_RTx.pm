57bad6e7ab54e09a6491fe4df014236cc5735d34
DPorts Builder <nobody@home.ok>
2013-10-15 14:26:30 -0700

    Tweak www/p5-RTx-Calendar version 0.17

---
+++ www/p5-RTx-Calendar/files/patch-inc_Module_Install_RTx.pm
@@ -0,0 +1,14 @@
+
+$FreeBSD: www/p5-RTx-Calendar/files/patch-inc_Module_Install_RTx.pm 328879 2013-09-30 21:53:55Z matthew $
+
+--- inc/Module/Install/RTx.pm.orig
++++ inc/Module/Install/RTx.pm
+@@ -101,7 +101,7 @@
+     my %index = map { $_ => 1 } @INDEX_DIRS;
+     $self->no_index( directory => $_ ) foreach grep !$index{$_}, @DIRS;
+ 
+-    my $args = join ', ', map "q($_)", map { ($_, $path{$_}) }
++    my $args = join ', ', map "q($_)", map { ($_, "\${DESTDIR}$path{$_}") }
+         grep $subdirs{$_}, keys %path;
+ 
+     print "./$_\t=> $path{$_}\n" for sort keys %subdirs;
