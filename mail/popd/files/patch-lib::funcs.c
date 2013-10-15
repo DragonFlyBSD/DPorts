866326c11ad4b4d2c565e4ff6768d5e2c771147b
DPorts Builder <nobody@home.ok>
2013-10-15 03:47:10 -0700

    Tweak mail/popd version 2.2.2a_5

--- mail/popd/files/patch-lib::funcs.c
+++ mail/popd/files/patch-lib::funcs.c
@@ -1,4 +1,4 @@
-$FreeBSD: ports/mail/popd/files/patch-lib::funcs.c,v 1.2 2012/11/17 05:59:10 svnexp Exp $
+$FreeBSD: mail/popd/files/patch-lib::funcs.c 300896 2012-07-14 13:54:48Z beat $
 
 --- lib/funcs.c.orig	Mon Mar  3 21:10:19 2003
 +++ lib/funcs.c	Wed Oct 15 11:18:01 2003
--- mail/popd/files/patch-src_authenticate.c
+++ mail/popd/files/patch-src_authenticate.c
@@ -1,5 +1,5 @@
 
-$FreeBSD: ports/mail/popd/files/patch-src_authenticate.c,v 1.2 2012/11/17 05:59:10 svnexp Exp $
+$FreeBSD: mail/popd/files/patch-src_authenticate.c 300896 2012-07-14 13:54:48Z beat $
 
 --- src/authenticate.c.orig	Sun Aug 22 16:49:10 2004
 +++ src/authenticate.c	Sun Aug 22 16:49:28 2004
