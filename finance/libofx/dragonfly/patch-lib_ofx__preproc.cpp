5784563402b5d7dcc192540e56a6da6a01828ed6
DPorts Builder <nobody@home.ok>
2013-10-16 04:20:06 -0700

    Update finance/libofx to version 0.9.9

--- finance/libofx/dragonfly/patch-lib_ofx__preproc.cpp
+++ finance/libofx/dragonfly/patch-lib_ofx__preproc.cpp
@@ -1,20 +1,19 @@
---- lib/ofx_preproc.cpp.intermediate	2013-09-25 21:22:34.577569000 +0000
+--- lib/ofx_preproc.cpp.orig	2013-10-16 11:17:02.048404000 +0000
 +++ lib/ofx_preproc.cpp
 @@ -23,6 +23,7 @@
- #include <stdlib.h>
+ #include <cstdlib>
  #include <stdio.h>
  #include <string>
 +#include <sys/param.h>
  #include "ParserEventGeneratorKit.h"
  #include "libofx.h"
  #include "messages.hh"
-@@ -231,7 +232,8 @@ int ofx_proc_file(LibofxContextPtr ctx,
-           memset(iconv_buffer, 0, READ_BUFFER_SIZE * 2);
-           size_t inbytesleft = strlen(s_buffer.c_str());
-           size_t outbytesleft = READ_BUFFER_SIZE * 2 - 1;
--#if defined(OS_WIN32) || defined(__FreeBSD__)
-+#if defined(OS_WIN32) || defined(__FreeBSD__) || \
-+   (defined(__DragonFly__) && __DragonFly_version > 300502)
-           const char * inchar = (const char *)s_buffer.c_str();
+@@ -310,7 +311,7 @@ int ofx_proc_file(LibofxContextPtr ctx,
+             size_t outbytesleft = inbytesleft * 2 - 1;
+             iconv_buffer = (char*) malloc (inbytesleft * 2);
+             memset(iconv_buffer, 0, inbytesleft * 2);
+-#if defined(OS_WIN32) || defined(__sun) || defined(__FreeBSD__)
++#if defined(OS_WIN32) || defined(__sun) || defined(__FreeBSD__) || defined(__DragonFly__)
+             const char * inchar = (const char *)s_buffer.c_str();
  #else
-           char * inchar = (char *)s_buffer.c_str();
+             char * inchar = (char *)s_buffer.c_str();
