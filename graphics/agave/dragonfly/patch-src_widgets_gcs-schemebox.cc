ab411989b46d7c2127ed5f519d681cef57349aee
DPorts Builder <nobody@home.ok>
2013-10-20 10:12:49 -0700

    Tweak graphics/agave version 0.4.2_10

---
+++ graphics/agave/dragonfly/patch-src_widgets_gcs-schemebox.cc
@@ -0,0 +1,11 @@
+--- src/widgets/gcs-schemebox.cc.orig	2006-12-16 17:57:06.000000000 +0000
++++ src/widgets/gcs-schemebox.cc
+@@ -45,7 +45,7 @@ namespace gcs
+ 
+         SchemeBox::SchemeBox(Scheme schm)
+         {
+-            SchemeBox::SchemeBox();
++            SchemeBox();
+             set_scheme(schm);
+         }
+ 
