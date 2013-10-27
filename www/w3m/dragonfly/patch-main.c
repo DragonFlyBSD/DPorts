262ffe3df27d2edcf5e3a2f43f48850c50e611c8
DPorts Builder <nobody@home.ok>
2013-10-27 15:20:57 -0700

    Tweak www/w3m version 0.5.3_2

--- www/w3m/dragonfly/patch-main.c
+++ www/w3m/dragonfly/patch-main.c
@@ -1,15 +1,6 @@
---- main.c.orig	2011-01-04 09:42:19.000000000 +0000
+--- main.c.intermediate	2013-10-27 08:35:34.177157000 +0000
 +++ main.c
-@@ -833,7 +833,7 @@ main(int argc, char **argv, char **envp)
-     mySignal(SIGPIPE, SigPipe);
- #endif
- 
--    orig_GC_warn_proc = GC_set_warn_proc(wrap_GC_warn_proc);
-+    GC_set_warn_proc(wrap_GC_warn_proc);
-     err_msg = Strnew();
-     if (load_argc == 0) {
- 	/* no URL specified */
-@@ -2261,7 +2261,7 @@ DEFUN(movR1, MOVE_RIGHT1,
+@@ -2263,7 +2263,7 @@ DEFUN(movR1, MOVE_RIGHT1,
  static wc_uint32
  getChar(char *p)
  {
