459a07ed59d4613b5f0d26611376e4bc85590caf
DPorts Builder <nobody@home.ok>
2013-11-05 13:32:44 -0800

    Update www/firefox-esr to version 24.1.0_1,1

--- www/firefox-esr/dragonfly/patch-gfx_qcms_qcmstypes.h
+++ www/firefox-esr/dragonfly/patch-gfx_qcms_qcmstypes.h
@@ -1,10 +1,10 @@
---- gfx/qcms/qcmstypes.h.orig	2013-03-09 10:47:10.080401000 +0000
+--- gfx/qcms/qcmstypes.h.orig	2013-06-18 11:01:20.000000000 +0000
 +++ gfx/qcms/qcmstypes.h
 @@ -11,6 +11,8 @@
  
  #if defined(_AIX)
  #include <sys/types.h>
-+#elif defined (__DragonFly__)
++#elif defined(__DragonFly__)
 +#include <stdlib.h>
  #elif defined(__OS2__)
  #include <stdlib.h>
