18179bfec62eda7e8270bfb184b28997baff1b7d
DPorts Builder <nobody@home.ok>
2013-11-12 08:02:35 -0800

    Update lang/clang33 to version 3.3_3

---
+++ lang/clang33/files/patch-tools_clang_lib_Driver_ToolChains.h
@@ -0,0 +1,20 @@
+
+$FreeBSD: lang/clang33/files/patch-tools_clang_lib_Driver_ToolChains.h 333046 2013-11-06 23:38:36Z brooks $
+
+--- tools/clang/lib/Driver/ToolChains.h.orig
++++ tools/clang/lib/Driver/ToolChains.h
+@@ -515,9 +515,14 @@
+   FreeBSD(const Driver &D, const llvm::Triple &Triple,
+           const ArgList &Args);
+ 
++  virtual CXXStdlibType GetCXXStdlibType(const ArgList &Args) const;
++
+   virtual bool IsMathErrnoDefault() const { return false; }
+   virtual bool IsObjCNonFragileABIDefault() const { return true; }
+ 
++  virtual void AddClangCXXStdlibIncludeArgs(const ArgList &DriverArgs,
++                                            ArgStringList &CC1Args) const;
++
+   virtual bool UseSjLjExceptions() const;
+ protected:
+   virtual Tool *buildAssembler() const;
