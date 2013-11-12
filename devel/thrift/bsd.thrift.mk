c3ef4fade127f05886453f983184d8de5a5186bb
DPorts Builder <nobody@home.ok>
2013-11-12 06:20:25 -0800

    Update devel/thrift to version 0.9.1,1

--- devel/thrift/bsd.thrift.mk
+++ devel/thrift/bsd.thrift.mk
@@ -1,8 +1,12 @@
 #
-# $FreeBSD: devel/thrift/bsd.thrift.mk 312988 2013-02-26 16:17:26Z scheidell $
+# $FreeBSD: devel/thrift/bsd.thrift.mk 333296 2013-11-09 15:09:13Z wg $
 #
 # to use:
 # in your makefile, set:
 # PORTVERSION=	${THRIFT_PORTVERSION}
 # see $PORTSDIR/devel/thrift for examples 
-THRIFT_PORTVERSION=	0.9.0
+THRIFT_PORTVERSION=	0.9.1
+
+CONFIGURE_ARGS+=	\
+		--without-tests
+
