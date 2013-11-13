8997a4e7437d58443d2372277ddc46179f6a6dfb
DPorts Builder <nobody@home.ok>
2013-11-13 02:33:30 -0800

    Update devel/libcheck to version 0.9.11

--- devel/libcheck/dragonfly/patch-lib_libcompat.h
+++ devel/libcheck/dragonfly/patch-lib_libcompat.h
@@ -1,16 +1,22 @@
---- lib/libcompat.h.orig	2013-04-18 10:19:02.000000000 +0000
+--- lib/libcompat.h.orig	2013-11-05 02:02:22.000000000 +0000
 +++ lib/libcompat.h
-@@ -132,8 +132,13 @@ struct itimerspec
+@@ -177,7 +177,9 @@ typedef int timer_t;
+  * As the functions which use clockid_t are not defined on the system,
+  * the clockid_t type probably also is not defined.
   */
- typedef int timer_t;
- 
-+#ifdef __DragonFly__
-+int clock_gettime(clockid_t clk_id, struct timespec *ts);
-+int timer_create(clockid_t clockid, struct sigevent *sevp, timer_t *timerid);
-+#else
- int clock_gettime(int clk_id, struct timespec *ts);
- int timer_create(int clockid, struct sigevent *sevp, timer_t *timerid);
++#ifndef __DragonFly__
+ typedef int clockid_t;
 +#endif
+ 
+ /* 
+  * Do a simple forward declaration in case the struct is not defined.
+@@ -186,7 +188,9 @@ typedef int clockid_t;
+  */
+ struct sigevent;
+ 
++#ifndef __DragonFly__
+ int clock_gettime(clockid_t clk_id, struct timespec *ts);
++#endif
+ int timer_create(int clockid, struct sigevent *sevp, timer_t *timerid);
  int timer_settime(timer_t timerid, int flags, const struct itimerspec *new_value, struct itimerspec * old_value);
  int timer_delete(timer_t timerid);
- #endif /* HAVE_LIBRT */
