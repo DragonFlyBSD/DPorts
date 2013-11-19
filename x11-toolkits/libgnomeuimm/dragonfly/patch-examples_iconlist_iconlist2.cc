fa73302d2b51db8d97df7b315c0ca810f1c0a278
DPorts Builder <nobody@home.ok>
2013-11-19 06:41:00 -0800

    Prune 17 obsolete ports
    FreeBSD Ports maintainers have already deleted the following:
      devel/gconfmm
      devel/libglademm
      devel/lua50-app
      devel/lua50-compat51
      devel/lua50-filename
      devel/lua50-gettext
      devel/lua50-posix
      devel/lua50-pty
      graphics/libgnomecanvasmm
      lang/lua50
      lang/tolua++50
      lang/tolua50
      multimedia/tovid
      net/lua50-luasocket
      security/squidclamav
      x11-toolkits/libgnomeuimm
      x11/libgnomemm

--- x11-toolkits/libgnomeuimm/dragonfly/patch-examples_iconlist_iconlist2.cc
+++
@@ -1,7 +0,0 @@
---- examples/iconlist/iconlist2.cc.orig	2002-07-19 21:17:22.000000000 +0000
-+++ examples/iconlist/iconlist2.cc
-@@ -1,3 +1,4 @@
-+#include <cstring>
- #include <libgnomeuimm.h>
- 
- static GtkTargetEntry target_table[] = {
