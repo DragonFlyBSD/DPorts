5a018920c864485981f8337b8b595be03c8637c3
DPorts Builder <nobody@home.ok>
2013-11-20 19:39:42 -0800

    Update lang/clang-devel to version 3.4.r193887_1

--- lang/clang-devel/files/patch-tools_clang_lib_Basic_Targets.cpp
+++ lang/clang-devel/files/patch-tools_clang_lib_Basic_Targets.cpp
@@ -1,14 +1,14 @@
 
-$FreeBSD: lang/clang-devel/files/patch-tools_clang_lib_Basic_Targets.cpp 318206 2013-05-14 22:10:59Z brooks $
+$FreeBSD: lang/clang-devel/files/patch-tools_clang_lib_Basic_Targets.cpp 332935 2013-11-06 00:13:02Z brooks $
 
 --- tools/clang/lib/Basic/Targets.cpp.orig
 +++ tools/clang/lib/Basic/Targets.cpp
-@@ -271,6 +271,8 @@
-           break;
-         case llvm::Triple::mips:
-         case llvm::Triple::mipsel:
-+        case llvm::Triple::mips64:
-+        case llvm::Triple::mips64el:
-         case llvm::Triple::ppc:
-         case llvm::Triple::ppc64:
-           this->MCountName = "_mcount";
+@@ -4981,6 +4981,8 @@
+     LongDoubleWidth = LongDoubleAlign = 128;
+     LongDoubleFormat = &llvm::APFloat::IEEEquad;
+     if (getTriple().getOS() == llvm::Triple::FreeBSD) {
++      IntMaxType = SignedLong;
++      UIntMaxType = UnsignedLong;
+       LongDoubleWidth = LongDoubleAlign = 64;
+       LongDoubleFormat = &llvm::APFloat::IEEEdouble;
+     }
---
+++ lang/clang-devel/files/patch-tools_clang_lib_Driver_ToolChains.h
@@ -0,0 +1,20 @@
+
+$FreeBSD: lang/clang-devel/files/patch-tools_clang_lib_Driver_ToolChains.h 332935 2013-11-06 00:13:02Z brooks $
+
+--- tools/clang/lib/Driver/ToolChains.h.orig
++++ tools/clang/lib/Driver/ToolChains.h
+@@ -515,9 +515,14 @@
+   FreeBSD(const Driver &D, const llvm::Triple &Triple,
+           const llvm::opt::ArgList &Args);
+ 
++  virtual CXXStdlibType GetCXXStdlibType(const llvm::opt::ArgList &Args) const;
++
+   virtual bool IsMathErrnoDefault() const { return false; }
+   virtual bool IsObjCNonFragileABIDefault() const { return true; }
+ 
++  virtual void AddClangCXXStdlibIncludeArgs(const llvm::opt::ArgList &DriverArgs,
++                                            llvm::opt::ArgStringList &CC1Args) const;
++
+   virtual bool UseSjLjExceptions() const;
+ protected:
+   virtual Tool *buildAssembler() const;
