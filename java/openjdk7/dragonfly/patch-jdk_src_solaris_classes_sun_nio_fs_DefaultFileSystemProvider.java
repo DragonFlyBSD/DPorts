920d1a00f7cfdb860504765ab7ca36a69ad3c04f
DPorts Builder <nobody@home.ok>
2013-12-19 12:54:16 -0800

    Update java/openjdk7 to version 7.45.18

--- java/openjdk7/dragonfly/patch-jdk_src_solaris_classes_sun_nio_fs_DefaultFileSystemProvider.java
+++ java/openjdk7/dragonfly/patch-jdk_src_solaris_classes_sun_nio_fs_DefaultFileSystemProvider.java
@@ -1,11 +1,11 @@
---- jdk/src/solaris/classes/sun/nio/fs/DefaultFileSystemProvider.java.intermediate	2013-05-13 10:51:23.878100000 +0000
+--- jdk/src/solaris/classes/sun/nio/fs/DefaultFileSystemProvider.java.intermediate	2013-12-19 19:02:24.715811000 +0000
 +++ jdk/src/solaris/classes/sun/nio/fs/DefaultFileSystemProvider.java
-@@ -82,6 +82,8 @@ public class DefaultFileSystemProvider {
-             return createProvider("sun.nio.fs.SolarisFileSystemProvider");
-         if (osname.equals("Linux"))
+@@ -84,6 +84,8 @@ public class DefaultFileSystemProvider {
              return createProvider("sun.nio.fs.LinuxFileSystemProvider");
+         if (osname.equals("Darwin") || osname.contains("OS X"))
+             return createProvider("sun.nio.fs.MacOSXFileSystemProvider");
 +        if (osname.equals("DragonFly"))
 +            return createProvider("sun.nio.fs.BsdFileSystemProvider");
-         if (osname.endsWith("BSD") || osname.equals("Darwin") || osname.contains("OS X"))
+         if (osname.endsWith("BSD"))
              return createProvider("sun.nio.fs.BsdFileSystemProvider");
          throw new AssertionError("Platform not recognized");
