ce20d43e090b1d37441c2dca6e95a171cddb81a5
DPorts Builder <nobody@home.ok>
2014-01-26 14:11:46 -0800

    Tweak audio/faad version 2.7_4,1

--- audio/faad/files/patch-frontend_main.c
+++ audio/faad/files/patch-frontend_main.c
@@ -1,5 +1,5 @@
 
-$FreeBSD: audio/faad/files/patch-frontend_main.c 300895 2012-07-14 12:56:14Z beat $
+$FreeBSD: audio/faad/files/patch-frontend_main.c 340725 2014-01-22 17:40:44Z mat $
 
 --- frontend/main.c.orig
 +++ frontend/main.c
