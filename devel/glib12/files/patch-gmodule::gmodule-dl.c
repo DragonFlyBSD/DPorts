b6cfb7d2b1cbb4fecb5ffd64ca7329034387e215
DPorts Builder <nobody@home.ok>
2014-01-26 14:00:19 -0800

    Tweak devel/glib12 version 1.2.10_13

--- devel/glib12/files/patch-gmodule::gmodule-dl.c
+++ devel/glib12/files/patch-gmodule::gmodule-dl.c
@@ -1,5 +1,5 @@
 
-$FreeBSD: devel/glib12/files/patch-gmodule::gmodule-dl.c 300896 2012-07-14 13:54:48Z beat $
+$FreeBSD: devel/glib12/files/patch-gmodule::gmodule-dl.c 340725 2014-01-22 17:40:44Z mat $
 
 --- gmodule/gmodule-dl.c	2001/12/12 04:39:42	1.1
 +++ gmodule/gmodule-dl.c	2001/12/12 04:41:05
