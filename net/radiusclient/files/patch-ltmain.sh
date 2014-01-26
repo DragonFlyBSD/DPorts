271188dd424500ea92e6caa0a9fa52e3ff9ca0e4
DPorts Builder <nobody@home.ok>
2014-01-26 14:42:43 -0800

    Tweak net/radiusclient version 0.5.6_1

--- net/radiusclient/files/patch-ltmain.sh
+++ net/radiusclient/files/patch-ltmain.sh
@@ -1,5 +1,5 @@
 
-$FreeBSD: net/radiusclient/files/patch-ltmain.sh 300897 2012-07-14 14:29:18Z beat $
+$FreeBSD: net/radiusclient/files/patch-ltmain.sh 340725 2014-01-22 17:40:44Z mat $
 
 --- ltmain.sh
 +++ ltmain.sh
