1ae27195cc1e16e4ad3b53008ac8423b2ac632c5
DPorts Builder <nobody@home.ok>
2014-01-27 00:50:26 -0800

    Tweak www/trac-downloads version 0.12.0.3.10047_1

--- www/trac-downloads/files/patch-tracdownloads_consoleadmin.py
+++ www/trac-downloads/files/patch-tracdownloads_consoleadmin.py
@@ -1,5 +1,5 @@
 
-$FreeBSD: www/trac-downloads/files/patch-tracdownloads_consoleadmin.py 300897 2012-07-14 14:29:18Z beat $
+$FreeBSD: www/trac-downloads/files/patch-tracdownloads_consoleadmin.py 340725 2014-01-22 17:40:44Z mat $
 
 --- tracdownloads/consoleadmin.py.orig
 +++ tracdownloads/consoleadmin.py
