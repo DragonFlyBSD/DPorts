68881be37f3dae8918e5eb5b915537b4fd242e35
DPorts Builder <nobody@home.ok>
2014-01-27 11:36:23 -0800

    Tweak devel/libmsocket version 0.4_1

--- devel/libmsocket/files/patch-file.c
+++ devel/libmsocket/files/patch-file.c
@@ -1,5 +1,5 @@
 
-$FreeBSD: devel/libmsocket/files/patch-file.c 300896 2012-07-14 13:54:48Z beat $
+$FreeBSD: devel/libmsocket/files/patch-file.c 340725 2014-01-22 17:40:44Z mat $
 
 --- file.c.orig
 +++ file.c
