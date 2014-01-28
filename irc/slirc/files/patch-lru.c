19f464eecfb829c3cca0861b1b6c3050879f3c94
DPorts Builder <nobody@home.ok>
2014-01-28 13:57:01 -0800

    Tweak irc/slirc version 0.12_4

--- irc/slirc/files/patch-lru.c
+++ irc/slirc/files/patch-lru.c
@@ -1,5 +1,5 @@
 
-$FreeBSD: irc/slirc/files/patch-lru.c 300896 2012-07-14 13:54:48Z beat $
+$FreeBSD: irc/slirc/files/patch-lru.c 340725 2014-01-22 17:40:44Z mat $
 
 --- lru.c.orig	Sun Aug 22 13:14:28 1999
 +++ lru.c	Sun Aug 22 17:15:12 2004
