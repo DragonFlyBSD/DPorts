2d67904ad32a9bf238bd9bbfefa5579403abbb14
DPorts Builder <nobody@home.ok>
2014-01-28 06:57:25 -0800

    Tweak devel/cutter version 1.2.2_1

--- devel/cutter/files/patch-test_run-test.sh
+++ devel/cutter/files/patch-test_run-test.sh
@@ -1,5 +1,5 @@
 
-$FreeBSD: devel/cutter/files/patch-test_run-test.sh 300896 2012-07-14 13:54:48Z beat $
+$FreeBSD: devel/cutter/files/patch-test_run-test.sh 340725 2014-01-22 17:40:44Z mat $
 
 --- test/run-test.sh.orig
 +++ test/run-test.sh
