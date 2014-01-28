16ac3096e86252559e863e2c622f0fee15d1725f
DPorts Builder <nobody@home.ok>
2014-01-28 06:57:33 -0800

    Tweak devel/distel version 2012.05.28_1

--- devel/distel/files/patch-elisp_erl-service.el
+++ devel/distel/files/patch-elisp_erl-service.el
@@ -1,5 +1,5 @@
 
-$FreeBSD: devel/distel/files/patch-elisp_erl-service.el 300896 2012-07-14 13:54:48Z beat $
+$FreeBSD: devel/distel/files/patch-elisp_erl-service.el 340725 2014-01-22 17:40:44Z mat $
 
 --- elisp/erl-service.el.orig
 +++ elisp/erl-service.el
