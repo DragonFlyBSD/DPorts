13edfa69f5134b39acb3542aa578ea07e0ff2fbe
DPorts Builder <nobody@home.ok>
2014-01-28 03:26:27 -0800

    Tweak textproc/scim-bridge version 0.4.16_1

--- textproc/scim-bridge/files/patch-agent_scim-bridge-agent-accept-listener.cpp
+++ textproc/scim-bridge/files/patch-agent_scim-bridge-agent-accept-listener.cpp
@@ -1,5 +1,5 @@
 
-$FreeBSD: textproc/scim-bridge/files/patch-agent_scim-bridge-agent-accept-listener.cpp 300897 2012-07-14 14:29:18Z beat $
+$FreeBSD: textproc/scim-bridge/files/patch-agent_scim-bridge-agent-accept-listener.cpp 340725 2014-01-22 17:40:44Z mat $
 
 --- agent/scim-bridge-agent-accept-listener.cpp.orig
 +++ agent/scim-bridge-agent-accept-listener.cpp
