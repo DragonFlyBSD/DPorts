fd61ea77e49f8e462f9b116a521b63ab2c630c7f
DPorts Builder <nobody@home.ok>
2014-01-27 23:32:31 -0800

    Tweak graphics/libaux version 1.0_5

--- graphics/libaux/files/patch-font.c
+++ graphics/libaux/files/patch-font.c
@@ -1,5 +1,5 @@
 
-$FreeBSD: graphics/libaux/files/patch-font.c 300896 2012-07-14 13:54:48Z beat $
+$FreeBSD: graphics/libaux/files/patch-font.c 340725 2014-01-22 17:40:44Z mat $
 
 --- font.c	2001/03/14 10:51:36	1.1
 +++ font.c	2001/03/14 10:51:48
