d21cec2c145f7a24d0aee39419716e596c6b1b46
DPorts Builder <nobody@home.ok>
2014-01-28 15:28:18 -0800

    Import lang/racket-minimal version 5.92

---
+++ lang/racket-minimal/files/patch-racket_include_scheme.h
@@ -0,0 +1,18 @@
+
+$FreeBSD: lang/racket-minimal/files/patch-racket_include_scheme.h 341390 2014-01-27 12:37:06Z olgeni $
+
+--- racket/include/scheme.h.orig
++++ racket/include/scheme.h
+@@ -63,10 +63,12 @@
+ # define MZ_USE_SINGLE_FLOATS
+ #endif
+ 
++#if (__FreeBSD_version >= 1000100)
+ /* gcc defines __SSE_MATH__ when SSE floating point is enabled: */
+ #ifdef __SSE_MATH__
+ # define C_COMPILER_USES_SSE 1
+ #endif
++#endif
+ 
+ #ifdef C_COMPILER_USES_SSE
+ # if defined(MZ_TRY_EXTFLONUMS) && !defined(MZ_NO_EXTFLONUMS)
