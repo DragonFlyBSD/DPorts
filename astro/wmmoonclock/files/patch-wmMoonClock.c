da7f777561fc4a980f4975cde185b638dccd9715
DPorts Builder <nobody@home.ok>
2014-01-28 06:35:40 -0800

    Tweak astro/wmmoonclock version 1.27_3

--- astro/wmmoonclock/files/patch-wmMoonClock.c
+++ astro/wmmoonclock/files/patch-wmMoonClock.c
@@ -1,5 +1,5 @@
 
-$FreeBSD: astro/wmmoonclock/files/patch-wmMoonClock.c 300895 2012-07-14 12:56:14Z beat $
+$FreeBSD: astro/wmmoonclock/files/patch-wmMoonClock.c 340725 2014-01-22 17:40:44Z mat $
 
 --- wmMoonClock.c.orig	Thu Aug 15 18:15:26 2002
 +++ wmMoonClock.c	Thu Aug 15 18:15:51 2002
