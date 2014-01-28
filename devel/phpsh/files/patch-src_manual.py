7aba0fc1db6890331670d08f982ae61fc5264d46
DPorts Builder <nobody@home.ok>
2014-01-28 07:32:03 -0800

    Import devel/phpsh version 20110513

---
+++ devel/phpsh/files/patch-src_manual.py
@@ -0,0 +1,14 @@
+
+$FreeBSD: devel/phpsh/files/patch-src_manual.py 340725 2014-01-22 17:40:44Z mat $
+
+--- src/manual.py.orig
++++ src/manual.py
+@@ -69,7 +69,7 @@
+     manual_file = "php_manual.db"
+     manual_path = os.path.join(os.getenv("HOME"), ".phpsh", manual_file)
+     if not os.path.exists(manual_path):
+-        manual_path = os.path.join("/etc/phpsh", manual_file)
++        manual_path = os.path.join("%%PREFIX%%/etc/phpsh", manual_file)
+     conn = sqlite.connect(manual_path)
+     cursor = conn.cursor()
+ 
---
+++ devel/phpsh/files/patch-src_phpsh.py
@@ -0,0 +1,14 @@
+
+$FreeBSD: devel/phpsh/files/patch-src_phpsh.py 340725 2014-01-22 17:40:44Z mat $
+
+--- src/phpsh.py.orig
++++ src/phpsh.py
+@@ -274,7 +274,7 @@
+         self.config.add_section("Emacs")
+ 
+     def read(self):
+-        config_files = ["/etc/phpsh/config"]
++        config_files = ["%%PREFIX%%/etc/phpsh/config"]
+         home = os.getenv("HOME")
+         if home:
+             homestr = home.strip()
