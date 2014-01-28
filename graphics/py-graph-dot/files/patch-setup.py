d7482bc3d06f072baca03b521499ac3d81c0bca2
DPorts Builder <nobody@home.ok>
2014-01-28 03:06:03 -0800

    Tweak graphics/py-graph-dot version 1.8.2

--- graphics/py-graph-dot/files/patch-setup.py
+++ graphics/py-graph-dot/files/patch-setup.py
@@ -1,5 +1,5 @@
 
-$FreeBSD: graphics/py-graph-dot/files/patch-setup.py 324859 2013-08-17 17:19:06Z demon $
+$FreeBSD: graphics/py-graph-dot/files/patch-setup.py 340725 2014-01-22 17:40:44Z mat $
 
 --- setup.py.orig
 +++ setup.py
