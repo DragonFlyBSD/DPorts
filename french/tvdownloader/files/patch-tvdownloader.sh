f107ce605cac0a8538eda5880026737d702f40d8
DPorts Builder <nobody@home.ok>
2014-01-28 11:44:53 -0800

    Tweak french/tvdownloader version 0.7.2_4

--- french/tvdownloader/files/patch-tvdownloader.sh
+++ french/tvdownloader/files/patch-tvdownloader.sh
@@ -3,7 +3,7 @@
 @@ -1,20 +1,5 @@
 -#!/bin/bash
 +#! /bin/sh
-+# $FreeBSD: french/tvdownloader/files/patch-tvdownloader.sh 300896 2012-07-14 13:54:48Z beat $
++# $FreeBSD: french/tvdownloader/files/patch-tvdownloader.sh 340725 2014-01-22 17:40:44Z mat $
  
  cd "__DATADIR__/tvdownloader/"
 -
