affb1ee90bd29b2f2ead5a9a5700d364a4b23160
DPorts Builder <nobody@home.ok>
2014-01-28 19:03:56 -0800

    Tweak security/burpsuite version 1.5

--- security/burpsuite/files/burpsuite.sh
+++ security/burpsuite/files/burpsuite.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $FreeBSD: security/burpsuite/files/burpsuite.sh 300897 2012-07-14 14:29:18Z beat $
+# $FreeBSD: security/burpsuite/files/burpsuite.sh 340872 2014-01-24 00:14:07Z mat $
 #
 
 cd %%PATH%%
