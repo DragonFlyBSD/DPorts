94844d42a9729aa472cc2037a734c10e32ec2afd
DPorts Builder <nobody@home.ok>
2014-01-28 16:47:13 -0800

    Tweak misc/metalink-editor version 1.2.0_5

--- misc/metalink-editor/files/setup.py
+++ misc/metalink-editor/files/setup.py
@@ -25,7 +25,7 @@
 # SUCH DAMAGE.
 #
 
-__version__ = "$FreeBSD: misc/metalink-editor/files/setup.py 300896 2012-07-14 13:54:48Z beat $"
+__version__ = "$FreeBSD: misc/metalink-editor/files/setup.py 340872 2014-01-24 00:14:07Z mat $"
 
 try:
 	from distutils import sysconfig
