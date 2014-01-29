67e8cd29cbb0f99a767bd9ba34400a102b1f2587
DPorts Builder <nobody@home.ok>
2014-01-28 23:11:19 -0800

    Tweak x11-toolkits/tkshape version 0.4_2

--- x11-toolkits/tkshape/files/patch-unix-shapeUnixImpl.c
+++ x11-toolkits/tkshape/files/patch-unix-shapeUnixImpl.c
@@ -1,4 +1,4 @@
-$FreeBSD: x11-toolkits/tkshape/files/patch-unix-shapeUnixImpl.c 300897 2012-07-14 14:29:18Z beat $
+$FreeBSD: x11-toolkits/tkshape/files/patch-unix-shapeUnixImpl.c 340725 2014-01-22 17:40:44Z mat $
 diff -ur unix/shapeUnixImpl.c unix/shapeUnixImpl.c.new
 --- unix/shapeUnixImpl.c	Mon Sep 11 16:27:28 2000
 +++ unix/shapeUnixImpl.c	Sat Mar 13 11:25:54 2004
