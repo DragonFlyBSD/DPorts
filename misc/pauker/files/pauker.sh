bcffbabb47a41d918fe9aae7e0d1849b667cff34
DPorts Builder <nobody@home.ok>
2014-01-28 16:47:04 -0800

    Tweak misc/pauker version 1.8.r2_1

--- misc/pauker/files/pauker.sh
+++ misc/pauker/files/pauker.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #
-# $FreeBSD: misc/pauker/files/pauker.sh 300896 2012-07-14 13:54:48Z beat $
+# $FreeBSD: misc/pauker/files/pauker.sh 340872 2014-01-24 00:14:07Z mat $
 
 "%%LOCALBASE%%/bin/java" -jar "%%JAVAJARDIR%%/%%PORTNAME%%.jar" "$@"
