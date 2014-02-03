c69265dc2a5e8e22041072d5b3c1a6348568fa07
DPorts Builder <nobody@home.ok>
2014-02-02 19:15:20 -0800

    Import sysutils/squashfs-tools version 4.2

---
+++ sysutils/squashfs-tools/files/patch-squashfs-tools_pseudo.c
@@ -0,0 +1,15 @@
+$FreeBSD: sysutils/squashfs-tools/files/patch-squashfs-tools_pseudo.c 340725 2014-01-22 17:40:44Z mat $
+
+--- pseudo.c	2009-04-05 04:01:58.000000000 +0200
++++ pseudo.c	2010-04-29 17:45:49.000000000 +0200
+@@ -31,6 +31,10 @@
+ #include <stdlib.h>
+ #include <sys/types.h>
+ 
++#ifdef __FreeBSD__
++#include <sys/stat.h>
++#endif
++
+ #include "pseudo.h"
+ 
+ #ifdef SQUASHFS_TRACE
