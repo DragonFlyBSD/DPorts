05590fc782f3ed6a95dd6dbe68df56c21ed725df
DPorts Builder <nobody@home.ok>
2014-02-10 13:56:29 -0800

    Update devel/icu to version 52.1

--- devel/icu/dragonfly/patch-common_unicode_platform.h
+++ devel/icu/dragonfly/patch-common_unicode_platform.h
@@ -1,17 +1,17 @@
-$NetBSD: patch-common_unicode_platform.h,v 1.3 2012/11/07 20:59:51 adam Exp $
-
---- common/unicode/platform.h.orig	2012-11-05 17:14:34.000000000 +0000
+--- common/unicode/platform.h.orig	2013-10-04 20:49:10.000000000 +0000
 +++ common/unicode/platform.h
-@@ -144,7 +144,7 @@
- #   include <android/api-level.h>
- #elif defined(linux) || defined(__linux__) || defined(__linux)
- #   define U_PLATFORM U_PF_LINUX
+@@ -151,7 +151,9 @@
+ #   else
+ #       define U_PLATFORM U_PF_DARWIN
+ #   endif
 -#elif defined(BSD) || defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__MirBSD__)
-+#elif defined(BSD) || defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__MirBSD__) || defined(__DragonFly__)
++#elif defined(BSD) || defined(__FreeBSD__) || defined(__FreeBSD_kernel__) \
++ || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__MirBSD__) \
++ || defined(__DragonFly__)
  #   define U_PLATFORM U_PF_BSD
  #elif defined(sun) || defined(__sun)
      /* Check defined(__SVR4) || defined(__svr4__) to distinguish Solaris from SunOS? */
-@@ -381,6 +381,13 @@
+@@ -379,6 +381,13 @@
  #   define U_IS_BIG_ENDIAN 1
  #elif defined(__LITTLE_ENDIAN__) || defined(_LITTLE_ENDIAN)
  #   define U_IS_BIG_ENDIAN 0
