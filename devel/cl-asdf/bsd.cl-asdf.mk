385874ae98c230aef57779dd709ac50deacfa5a2
DPorts Builder <nobody@home.ok>
2014-02-10 03:39:35 -0800

    Tweak devel/cl-asdf version 2.017,2

--- devel/cl-asdf/bsd.cl-asdf.mk
+++ devel/cl-asdf/bsd.cl-asdf.mk
@@ -1,4 +1,4 @@
-# $FreeBSD: devel/cl-asdf/bsd.cl-asdf.mk 340722 2014-01-22 17:00:46Z mat $
+# $FreeBSD: head/devel/cl-asdf/bsd.cl-asdf.mk 340722 2014-01-22 17:00:46Z mat $
 #
 # bsd.cl-asdf.mk - Common Lisp related macros
 #
