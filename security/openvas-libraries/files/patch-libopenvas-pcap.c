bf1599aa2b19b4a53b20ca87f2e564c3475436e5
DPorts Builder <nobody@home.ok>
2014-02-10 06:10:23 -0800

    Tweak security/openvas-libraries version 2.0.3_3

--- security/openvas-libraries/files/patch-libopenvas-pcap.c
+++ security/openvas-libraries/files/patch-libopenvas-pcap.c
@@ -1,5 +1,5 @@
 
-$FreeBSD: security/openvas-libraries/files/patch-libopenvas-pcap.c 340725 2014-01-22 17:40:44Z mat $
+$FreeBSD: head/security/openvas-libraries/files/patch-libopenvas-pcap.c 340725 2014-01-22 17:40:44Z mat $
 
 --- libopenvas/pcap.c.orig
 +++ libopenvas/pcap.c
--- security/openvas-libraries/files/patch-libopenvas_openvas__server.c
+++ security/openvas-libraries/files/patch-libopenvas_openvas__server.c
@@ -1,5 +1,5 @@
 
-$FreeBSD: security/openvas-libraries/files/patch-libopenvas_openvas__server.c 340725 2014-01-22 17:40:44Z mat $
+$FreeBSD: head/security/openvas-libraries/files/patch-libopenvas_openvas__server.c 340725 2014-01-22 17:40:44Z mat $
 
 --- libopenvas/openvas_server.c.orig
 +++ libopenvas/openvas_server.c
