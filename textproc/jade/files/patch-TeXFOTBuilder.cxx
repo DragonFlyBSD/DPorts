372ab2a2e9ea73b34d110b9b5df9ad03d41a60e6
DPorts Builder <nobody@home.ok>
2014-02-10 05:22:39 -0800

    Tweak textproc/jade version 1.2.1_9

--- textproc/jade/files/patch-TeXFOTBuilder.cxx
+++ textproc/jade/files/patch-TeXFOTBuilder.cxx
@@ -1,4 +1,4 @@
-$FreeBSD: textproc/jade/files/patch-TeXFOTBuilder.cxx 340725 2014-01-22 17:40:44Z mat $
+$FreeBSD: head/textproc/jade/files/patch-TeXFOTBuilder.cxx 340725 2014-01-22 17:40:44Z mat $
 
 --- jade/TeXFOTBuilder.cxx.orig	2011-09-24 14:17:02.000000000 +0000
 +++ jade/TeXFOTBuilder.cxx	2011-09-24 14:19:42.000000000 +0000
