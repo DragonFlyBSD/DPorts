d373ac9359ce7c01346a84280b5be17bc995d09e
DPorts Builder <nobody@home.ok>
2014-02-11 00:50:24 -0800

    Tweak sysutils/squashfs-tools version 4.2

--- sysutils/squashfs-tools/files/patch-squashfs-tools_pseudo.c
+++ sysutils/squashfs-tools/files/patch-squashfs-tools_pseudo.c
@@ -1,4 +1,4 @@
-$FreeBSD: sysutils/squashfs-tools/files/patch-squashfs-tools_pseudo.c 340725 2014-01-22 17:40:44Z mat $
+$FreeBSD: head/sysutils/squashfs-tools/files/patch-squashfs-tools_pseudo.c 340725 2014-01-22 17:40:44Z mat $
 
 --- pseudo.c	2009-04-05 04:01:58.000000000 +0200
 +++ pseudo.c	2010-04-29 17:45:49.000000000 +0200
--- sysutils/squashfs-tools/files/patch-squashfs-tools_unsquashfs.c
+++ sysutils/squashfs-tools/files/patch-squashfs-tools_unsquashfs.c
@@ -1,4 +1,4 @@
-$FreeBSD: sysutils/squashfs-tools/files/patch-squashfs-tools_unsquashfs.c 340725 2014-01-22 17:40:44Z mat $
+$FreeBSD: head/sysutils/squashfs-tools/files/patch-squashfs-tools_unsquashfs.c 340725 2014-01-22 17:40:44Z mat $
 
 --- unsquashfs.c.orig	2010-09-20 03:14:38.000000000 +0530
 +++ unsquashfs.c	2010-09-29 09:34:43.000000000 +0530
