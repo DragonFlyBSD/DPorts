13c5eacbc2a4d2daeac301a1093b20e32162d7c8
DPorts Builder <nobody@home.ok>
2014-02-11 14:50:48 -0800

    Tweak graphics/sodipodi version 0.34_16

--- graphics/sodipodi/files/patch-src::xml::repr-io.c
+++ graphics/sodipodi/files/patch-src::xml::repr-io.c
@@ -1,5 +1,5 @@
 
-$FreeBSD: graphics/sodipodi/files/patch-src::xml::repr-io.c 340725 2014-01-22 17:40:44Z mat $
+$FreeBSD: head/graphics/sodipodi/files/patch-src::xml::repr-io.c 340725 2014-01-22 17:40:44Z mat $
 
 --- src/xml/repr-io.c	2001/11/22 15:20:50	1.1
 +++ src/xml/repr-io.c	2001/11/22 15:21:18
