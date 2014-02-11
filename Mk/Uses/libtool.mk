8ca8df6f21417dd797fa6826bf8e6d6e3dd06249
DPorts Builder <nobody@home.ok>
2014-02-11 04:40:32 -0800

    Fix libtool.mk

--- Mk/Uses/libtool.mk
+++ Mk/Uses/libtool.mk
@@ -20,6 +20,7 @@ patch-libtool:
 	@${FIND} ${WRKDIR} \( -name configure -or -name ltconfig \)	\
 		-type f | ${XARGS} ${REINPLACE_CMD}			\
 		-e '/link_all_deplibs=/s/=unknown/=no/'			\
+		-e 's,freebsd\*),freebsd\*|dragonfly\*),g'			\
 		-e '/objformat=/s/echo aout/echo elf/'
 
 .if ! ${libtool_ARGS:Moldver}
