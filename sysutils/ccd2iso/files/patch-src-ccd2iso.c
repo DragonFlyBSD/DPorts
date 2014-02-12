ce00e268612326b1cb01fa8181aa8b4028fe2426
DPorts Builder <nobody@home.ok>
2014-02-12 15:53:18 -0800

    Tweak sysutils/ccd2iso version 0.9_1

--- sysutils/ccd2iso/files/patch-src-ccd2iso.c
+++ sysutils/ccd2iso/files/patch-src-ccd2iso.c
@@ -1,5 +1,5 @@
 
-$FreeBSD: sysutils/ccd2iso/files/patch-src-ccd2iso.c 340725 2014-01-22 17:40:44Z mat $
+$FreeBSD: head/sysutils/ccd2iso/files/patch-src-ccd2iso.c 340725 2014-01-22 17:40:44Z mat $
 
 --- src/ccd2iso.c.orig	Thu Nov 13 06:43:41 2003
 +++ src/ccd2iso.c	Thu Jun 24 15:31:04 2004
