b3ca280e4927d04f95b4f38f3e2a83a816f892a3
DPorts Builder <nobody@home.ok>
2014-02-12 14:35:08 -0800

    Tweak devel/antlrworks version 1.4.2_1,1

--- devel/antlrworks/files/antlrworks.sh
+++ devel/antlrworks/files/antlrworks.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 #
-# $FreeBSD: devel/antlrworks/files/antlrworks.sh 340872 2014-01-24 00:14:07Z mat $
+# $FreeBSD: head/devel/antlrworks/files/antlrworks.sh 340872 2014-01-24 00:14:07Z mat $
 
 JAVA_VERSION="%%JAVA_VERSION%%" "%%LOCALBASE%%/bin/java" -classpath "%%JAVAJARDIR%%/%%PORTNAME%%.jar" org.antlr.works.IDE "$@"
  
