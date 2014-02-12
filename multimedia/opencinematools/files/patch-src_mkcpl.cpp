dcbf08dcd4cc5edf6bb186e2ec2b2dd013d648ed
DPorts Builder <nobody@home.ok>
2014-02-12 05:51:25 -0800

    Tweak multimedia/opencinematools version 1.1.2

--- multimedia/opencinematools/files/patch-src_mkcpl.cpp
+++ multimedia/opencinematools/files/patch-src_mkcpl.cpp
@@ -1,5 +1,5 @@
 
-$FreeBSD: multimedia/opencinematools/files/patch-src_mkcpl.cpp 340725 2014-01-22 17:40:44Z mat $
+$FreeBSD: head/multimedia/opencinematools/files/patch-src_mkcpl.cpp 340725 2014-01-22 17:40:44Z mat $
 
 --- src/mkcpl.cpp.orig
 +++ src/mkcpl.cpp
