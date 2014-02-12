2af19228c766e5c1025e68d7a5ceab6faf42c5be
DPorts Builder <nobody@home.ok>
2014-02-12 12:15:12 -0800

    Tweak sysutils/setcdboot version 1.0

--- sysutils/setcdboot/files/setcdboot.c
+++ sysutils/setcdboot/files/setcdboot.c
@@ -28,7 +28,7 @@
  * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- * $FreeBSD: sysutils/setcdboot/files/setcdboot.c 340872 2014-01-24 00:14:07Z mat $
+ * $FreeBSD: head/sysutils/setcdboot/files/setcdboot.c 340872 2014-01-24 00:14:07Z mat $
  */
 
 /*
