fde79f7bc07435abe46370565f80c8f0efe9230a
DPorts Builder <nobody@home.ok>
2014-02-12 05:33:00 -0800

    Tweak security/dirbuster version 1.0r1

--- security/dirbuster/files/dirbuster.sh
+++ security/dirbuster/files/dirbuster.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $FreeBSD: security/dirbuster/files/dirbuster.sh 340872 2014-01-24 00:14:07Z mat $
+# $FreeBSD: head/security/dirbuster/files/dirbuster.sh 340872 2014-01-24 00:14:07Z mat $
 #
 
 cd %%PATH%%
