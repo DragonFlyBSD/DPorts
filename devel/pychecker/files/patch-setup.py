da06bf3a25b30d43f17948630ee439a6c5791138
DPorts Builder <nobody@home.ok>
2014-02-12 14:51:41 -0800

    Tweak devel/pychecker version 0.8.19

--- devel/pychecker/files/patch-setup.py
+++ devel/pychecker/files/patch-setup.py
@@ -1,5 +1,5 @@
 
-$FreeBSD: devel/pychecker/files/patch-setup.py 340725 2014-01-22 17:40:44Z mat $
+$FreeBSD: head/devel/pychecker/files/patch-setup.py 340725 2014-01-22 17:40:44Z mat $
 
 --- setup.py
 +++ setup.py
