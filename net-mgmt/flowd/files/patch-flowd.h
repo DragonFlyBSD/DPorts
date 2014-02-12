28471192a1fe268e2a153ad921d2d336cf44d48d
DPorts Builder <nobody@home.ok>
2014-02-12 12:59:05 -0800

    Tweak net-mgmt/flowd version 0.9.1_2

--- net-mgmt/flowd/files/patch-flowd.h
+++ net-mgmt/flowd/files/patch-flowd.h
@@ -1,5 +1,5 @@
 
-$FreeBSD: net-mgmt/flowd/files/patch-flowd.h 340725 2014-01-22 17:40:44Z mat $
+$FreeBSD: head/net-mgmt/flowd/files/patch-flowd.h 340725 2014-01-22 17:40:44Z mat $
 
 --- flowd.h.orig
 +++ flowd.h
