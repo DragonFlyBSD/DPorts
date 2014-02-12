2a013b6eaea1818c81d42f11da9fa2d11a41d8c5
DPorts Builder <nobody@home.ok>
2014-02-12 09:49:28 -0800

    Tweak graphics/xmorph version 2006.08.17_7

--- graphics/xmorph/files/patch-libmorph_my__malloc.c
+++ graphics/xmorph/files/patch-libmorph_my__malloc.c
@@ -1,5 +1,5 @@
 
-$FreeBSD: graphics/xmorph/files/patch-libmorph_my__malloc.c 340725 2014-01-22 17:40:44Z mat $
+$FreeBSD: head/graphics/xmorph/files/patch-libmorph_my__malloc.c 340725 2014-01-22 17:40:44Z mat $
 
 --- libmorph/my_malloc.c.orig	Sat Jun  7 16:35:29 2003
 +++ libmorph/my_malloc.c	Sat Jun  7 16:35:43 2003
