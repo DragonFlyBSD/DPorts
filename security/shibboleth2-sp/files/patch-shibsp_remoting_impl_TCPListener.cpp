96966c52b338a49cf1305e095a95c08694f3958a
DPorts Builder <nobody@home.ok>
2014-02-12 07:08:46 -0800

    Tweak security/shibboleth2-sp version 2.5.2_1

--- security/shibboleth2-sp/files/patch-shibsp_remoting_impl_TCPListener.cpp
+++ security/shibboleth2-sp/files/patch-shibsp_remoting_impl_TCPListener.cpp
@@ -1,5 +1,5 @@
 
-$FreeBSD: security/shibboleth2-sp/files/patch-shibsp_remoting_impl_TCPListener.cpp 340725 2014-01-22 17:40:44Z mat $
+$FreeBSD: head/security/shibboleth2-sp/files/patch-shibsp_remoting_impl_TCPListener.cpp 340725 2014-01-22 17:40:44Z mat $
 
 --- shibsp/remoting/impl/TCPListener.cpp.orig
 +++ shibsp/remoting/impl/TCPListener.cpp
