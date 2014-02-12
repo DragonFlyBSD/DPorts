af6f81077d4c944c756beb07e68a31540516cc27
DPorts Builder <nobody@home.ok>
2014-02-12 14:45:18 -0800

    Tweak www/webresolve version 0.2.0_1

--- www/webresolve/files/patch-webresolve.c
+++ www/webresolve/files/patch-webresolve.c
@@ -1,5 +1,5 @@
 
-$FreeBSD: www/webresolve/files/patch-webresolve.c 340725 2014-01-22 17:40:44Z mat $
+$FreeBSD: head/www/webresolve/files/patch-webresolve.c 340725 2014-01-22 17:40:44Z mat $
 
 --- webresolve.c	2001/03/14 14:44:11	1.1
 +++ webresolve.c	2001/03/14 14:44:26
