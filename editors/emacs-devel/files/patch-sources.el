ff1fa15e8bcf6806bdbc7e001fbadf31abd071e1
DPorts Builder <nobody@home.ok>
2014-02-12 13:34:47 -0800

    Tweak editors/emacs-devel version 24.3.50.115370,2

--- editors/emacs-devel/files/patch-sources.el
+++ editors/emacs-devel/files/patch-sources.el
@@ -1,5 +1,5 @@
 
-$FreeBSD: editors/emacs-devel/files/patch-sources.el 340725 2014-01-22 17:40:44Z mat $
+$FreeBSD: head/editors/emacs-devel/files/patch-sources.el 340725 2014-01-22 17:40:44Z mat $
 
 --- /dev/null
 +++ sources.el
