4d21215371db1c406310cd83b66c2e617a2a5af4
DPorts Builder <nobody@home.ok>
2014-02-12 04:48:54 -0800

    Tweak net-mgmt/softflowd version 0.9.8_2

--- net-mgmt/softflowd/files/patch-softflowd.c
+++ net-mgmt/softflowd/files/patch-softflowd.c
@@ -1,5 +1,5 @@
 
-$FreeBSD: net-mgmt/softflowd/files/patch-softflowd.c 340725 2014-01-22 17:40:44Z mat $
+$FreeBSD: head/net-mgmt/softflowd/files/patch-softflowd.c 340725 2014-01-22 17:40:44Z mat $
 
 --- softflowd.c.orig
 +++ softflowd.c
