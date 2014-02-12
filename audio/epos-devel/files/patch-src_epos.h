5140b217abb8afd943c238355d7fa16ab50c2bac
DPorts Builder <nobody@home.ok>
2014-02-12 15:15:59 -0800

    Tweak audio/epos-devel version 2.5.37

--- audio/epos-devel/files/patch-src_epos.h
+++ audio/epos-devel/files/patch-src_epos.h
@@ -1,5 +1,5 @@
 
-$FreeBSD: audio/epos-devel/files/patch-src_epos.h 340725 2014-01-22 17:40:44Z mat $
+$FreeBSD: head/audio/epos-devel/files/patch-src_epos.h 340725 2014-01-22 17:40:44Z mat $
 
 --- src/epos.h.orig
 +++ src/epos.h
