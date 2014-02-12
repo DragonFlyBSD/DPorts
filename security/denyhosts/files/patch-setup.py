d3f201c71fe96d8a933896ae7358e2416e086e8e
DPorts Builder <nobody@home.ok>
2014-02-11 17:31:33 -0800

    Tweak security/denyhosts version 2.6_4

--- security/denyhosts/files/patch-setup.py
+++ security/denyhosts/files/patch-setup.py
@@ -1,5 +1,5 @@
 
-$FreeBSD: security/denyhosts/files/patch-setup.py 340725 2014-01-22 17:40:44Z mat $
+$FreeBSD: head/security/denyhosts/files/patch-setup.py 340725 2014-01-22 17:40:44Z mat $
 
 --- setup.py.orig
 +++ setup.py
