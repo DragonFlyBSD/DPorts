6f5395aebf42a491617a812ee54423db5bd6f57b
DPorts Builder <nobody@home.ok>
2014-02-12 06:53:29 -0800

    Tweak security/tinyca version 0.7.5_5

--- security/tinyca/files/patch-lib::CA.pm
+++ security/tinyca/files/patch-lib::CA.pm
@@ -1,5 +1,5 @@
 
-$FreeBSD: security/tinyca/files/patch-lib::CA.pm 340725 2014-01-22 17:40:44Z mat $
+$FreeBSD: head/security/tinyca/files/patch-lib::CA.pm 340725 2014-01-22 17:40:44Z mat $
 
 --- lib/CA.pm.orig
 +++ lib/CA.pm
