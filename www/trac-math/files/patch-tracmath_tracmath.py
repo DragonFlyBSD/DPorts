d44f01cf17df16575870e40c8fe0d1ec48f3cccd
DPorts Builder <nobody@home.ok>
2014-02-11 20:29:54 -0800

    Tweak www/trac-math version 0.1_6

--- www/trac-math/files/patch-tracmath_tracmath.py
+++ www/trac-math/files/patch-tracmath_tracmath.py
@@ -1,5 +1,5 @@
 
-$FreeBSD: www/trac-math/files/patch-tracmath_tracmath.py 340725 2014-01-22 17:40:44Z mat $
+$FreeBSD: head/www/trac-math/files/patch-tracmath_tracmath.py 340725 2014-01-22 17:40:44Z mat $
 
 --- tracmath/tracmath.py.orig
 +++ tracmath/tracmath.py
