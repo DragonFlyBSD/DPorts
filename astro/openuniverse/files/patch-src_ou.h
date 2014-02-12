22c99b56c3d43385f509b06b9932280476256459
DPorts Builder <nobody@home.ok>
2014-02-12 14:22:38 -0800

    Tweak astro/openuniverse version 1.0.b3_8

--- astro/openuniverse/files/patch-src_ou.h
+++ astro/openuniverse/files/patch-src_ou.h
@@ -1,4 +1,4 @@
-$FreeBSD: astro/openuniverse/files/patch-src_ou.h 340725 2014-01-22 17:40:44Z mat $
+$FreeBSD: head/astro/openuniverse/files/patch-src_ou.h 340725 2014-01-22 17:40:44Z mat $
 
 --- src/ou.h.orig	Sun Jun  4 12:35:09 2000
 +++ src/ou.h	Tue Nov  5 12:17:24 2002
