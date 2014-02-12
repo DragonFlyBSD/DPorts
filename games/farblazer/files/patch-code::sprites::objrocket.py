54dc7cbfad5e4613e8ad61ea2346ce144749f43e
DPorts Builder <nobody@home.ok>
2014-02-11 22:23:54 -0800

    Tweak games/farblazer version 0.7_7

--- games/farblazer/files/patch-code::sprites::objrocket.py
+++ games/farblazer/files/patch-code::sprites::objrocket.py
@@ -1,5 +1,5 @@
 
-$FreeBSD: games/farblazer/files/patch-code::sprites::objrocket.py 340725 2014-01-22 17:40:44Z mat $
+$FreeBSD: head/games/farblazer/files/patch-code::sprites::objrocket.py 340725 2014-01-22 17:40:44Z mat $
 
 --- code/sprites/objrocket.py	2002/05/10 00:24:42	1.1
 +++ code/sprites/objrocket.py	2002/05/10 00:24:53
