4f6c7bf3279e0b19b0c15c604c4a7e83e0473e4a
DPorts Builder <nobody@home.ok>
2014-02-12 04:42:32 -0800

    Tweak databases/postgresql-plruby version 0.5.4_2

--- databases/postgresql-plruby/files/createlang.sql
+++ databases/postgresql-plruby/files/createlang.sql
@@ -1,7 +1,7 @@
 --
 -- Execute the following SQL statements to enable PL/Ruby.
 --
--- $FreeBSD: databases/postgresql-plruby/files/createlang.sql 340872 2014-01-24 00:14:07Z mat $
+-- $FreeBSD: head/databases/postgresql-plruby/files/createlang.sql 340872 2014-01-24 00:14:07Z mat $
 --
 
 CREATE FUNCTION plruby_call_handler() RETURNS language_handler
