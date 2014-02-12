7b70ff18997cfdfd8f917e54033d97eba5e853f9
DPorts Builder <nobody@home.ok>
2014-02-12 09:47:33 -0800

    Tweak www/trac-wikinotification version 0.2.1_5

--- www/trac-wikinotification/files/patch-WikiNotification__notification.py
+++ www/trac-wikinotification/files/patch-WikiNotification__notification.py
@@ -1,5 +1,5 @@
 
-$FreeBSD: www/trac-wikinotification/files/patch-WikiNotification__notification.py 340725 2014-01-22 17:40:44Z mat $
+$FreeBSD: head/www/trac-wikinotification/files/patch-WikiNotification__notification.py 340725 2014-01-22 17:40:44Z mat $
 
 --- ./WikiNotification/notification.py.orig
 +++ ./WikiNotification/notification.py
