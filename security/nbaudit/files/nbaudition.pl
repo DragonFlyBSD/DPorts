c04751be1a645c5fba6c0c78d976aa4ebabdf5ca
DPorts Builder <nobody@home.ok>
2014-02-12 08:23:47 -0800

    Tweak security/nbaudit version 1.0_1

--- security/nbaudit/files/nbaudition.pl
+++ security/nbaudit/files/nbaudition.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 use strict;
 
-# $FreeBSD: security/nbaudit/files/nbaudition.pl 340872 2014-01-24 00:14:07Z mat $
+# $FreeBSD: head/security/nbaudit/files/nbaudition.pl 340872 2014-01-24 00:14:07Z mat $
 
 # run nmap -oX logfile <host> -p 139 to create the log for this script
 
