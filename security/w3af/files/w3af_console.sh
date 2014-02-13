2b37f665f9d97cb710d67e75b6bc6b609805e42f
DPorts Builder <nobody@home.ok>
2014-02-12 17:24:19 -0800

    Tweak security/w3af version 1.0.r4_3

--- security/w3af/files/w3af_console.sh
+++ security/w3af/files/w3af_console.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $FreeBSD: security/w3af/files/w3af_console.sh 340872 2014-01-24 00:14:07Z mat $
+# $FreeBSD: head/security/w3af/files/w3af_console.sh 340872 2014-01-24 00:14:07Z mat $
 #
 
 cd %%PATH%%
