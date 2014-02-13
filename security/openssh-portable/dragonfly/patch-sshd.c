7e5fe47fb0335d9b9e31dda502f6ba005ce3fca8
DPorts Builder <nobody@home.ok>
2014-02-12 17:29:52 -0800

    Update security/openssh-portable to version 6.5.p1_1,1

--- security/openssh-portable/dragonfly/patch-sshd.c
+++ security/openssh-portable/dragonfly/patch-sshd.c
@@ -1,12 +1,12 @@
---- sshd.c.intermediate	2013-08-19 17:23:26.507870000 +0000
+--- sshd.c.intermediate	2014-02-12 20:20:33.797717000 +0000
 +++ sshd.c
-@@ -1822,9 +1822,6 @@ main(int ac, char **av)
+@@ -1895,9 +1895,6 @@ main(int ac, char **av)
  	/* Reinitialize the log (because of the fork above). */
  	log_init(__progname, options.log_level, options.log_facility, log_stderr);
  
--	/* Avoid killing the process in high-pressure swapping environments. */
--	if (!inetd_flag && madvise(NULL, 0, MADV_PROTECT) != 0)
--		debug("madvise(): %.200s", strerror(errno));
+- 	/* Avoid killing the process in high-pressure swapping environments. */
+- 	if (!inetd_flag && madvise(NULL, 0, MADV_PROTECT) != 0)
+- 		debug("madvise(): %.200s", strerror(errno));
  
- 	/* Initialize the random number generator. */
- 	arc4random_stir();
+ 	/* Chdir to the root directory so that the current disk can be
+ 	   unmounted if desired. */
