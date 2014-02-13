1de9754035a316d68397fd5e234c05e431831d24
DPorts Builder <nobody@home.ok>
2014-02-12 18:12:05 -0800

    Tweak www/crawl version 0.4_8

--- www/crawl/files/patch-compat::gai-errnos.h
+++ www/crawl/files/patch-compat::gai-errnos.h
@@ -1,4 +1,4 @@
-$FreeBSD: www/crawl/files/patch-compat::gai-errnos.h 340725 2014-01-22 17:40:44Z mat $
+$FreeBSD: head/www/crawl/files/patch-compat::gai-errnos.h 340725 2014-01-22 17:40:44Z mat $
 
 --- compat/gai-errnos.h.orig	Fri Mar 12 07:29:36 2004
 +++ compat/gai-errnos.h	Fri Mar 12 07:29:52 2004
