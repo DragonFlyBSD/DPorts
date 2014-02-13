fbd91d4f9e6120c611103ffed790bb23431427bb
DPorts Builder <nobody@home.ok>
2014-02-12 17:06:06 -0800

    Tweak sysutils/jdiskreport version 1.4.0_1

--- sysutils/jdiskreport/files/jdiskreport.sh
+++ sysutils/jdiskreport/files/jdiskreport.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-# $FreeBSD: sysutils/jdiskreport/files/jdiskreport.sh 340872 2014-01-24 00:14:07Z mat $
+# $FreeBSD: head/sysutils/jdiskreport/files/jdiskreport.sh 340872 2014-01-24 00:14:07Z mat $
 JDISKDIR="%%JDISKDIR%%"
 JAVAVM="%%JAVAVM%%"
 PORTVERSION="%%PORTVERSION%%"
