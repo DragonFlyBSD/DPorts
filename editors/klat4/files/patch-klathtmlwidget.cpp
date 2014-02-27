0e89fa47287164bae99295fca8e18fd899465e05
DPorts Builder <nobody@home.ok>
2014-02-26 23:18:37 -0800

    Update editors/klat4 to version 0.6.1_11

--- editors/klat4/files/patch-klathtmlwidget.cpp
+++ editors/klat4/files/patch-klathtmlwidget.cpp
@@ -1,15 +1,15 @@
---- klathtmlwidget.cpp.orig	2008-10-15 14:20:33.000000000 +0000
+--- klathtmlwidget.cpp.orig
 +++ klathtmlwidget.cpp
-@@ -73,7 +73,7 @@ bool KlatHTMLWidget::urlSelected(const Q
+@@ -73,7 +73,7 @@
  			}
  			if(cURL.protocol() == LTRTOKLATPROT && found == true)
  			{
 -				for (i ,found = false ; !found && i < len ; i++)
-+				for (i=0,found=false ; !found && i < len ; i++)
++				for (i = 0, found = false ; !found && i < len ; i++)
  				{
  					if (code[i] == '}')
  					{
-@@ -85,7 +85,9 @@ bool KlatHTMLWidget::urlSelected(const Q
+@@ -85,7 +85,9 @@
  		}
  		
  		emit signalSymbolCodeSelected(code,advance);
