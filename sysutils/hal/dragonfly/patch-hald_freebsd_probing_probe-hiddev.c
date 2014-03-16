55cf6a16629611438797559cfed0125c27ad20d2
DPorts Builder <nobody@home.ok>
2014-03-16 03:50:48 -0700

    Tweak sysutils/hal version 0.5.14_23

--- sysutils/hal/dragonfly/patch-hald_freebsd_probing_probe-hiddev.c
+++ sysutils/hal/dragonfly/patch-hald_freebsd_probing_probe-hiddev.c
@@ -1,16 +1,27 @@
---- hald/freebsd/probing/probe-hiddev.c.orig	2009-09-17 15:47:14.000000000 +0200
-+++ hald/freebsd/probing/probe-hiddev.c	2012-12-21 19:06:24.661349000 +0100
-@@ -31,8 +31,13 @@
+--- hald/freebsd/probing/probe-hiddev.c.orig	2009-09-17 13:47:14.000000000 +0000
++++ hald/freebsd/probing/probe-hiddev.c
+@@ -31,14 +31,24 @@
  #include <fcntl.h>
  #ifndef HAVE_LIBUSB20
  #include <sys/ioctl.h>
-+#ifdef __DragonFly__
++#  ifdef __DragonFly__
 +#include <bus/usb/usb.h>
 +#include <bus/usb/usbhid.h>
-+#else
++#  else
  #include <dev/usb/usb.h>
  #include <dev/usb/usbhid.h>
-+#endif
++#  endif
  #else
++#  ifdef __DragonFly__
++#include <bus/usb/usb.h>
++#include <bus/usb/usbhid.h>
++#  else
  #if __FreeBSD_version >= 800064
  #include <dev/usb/usbhid.h>
+ #else
+ #include <dev/usb2/include/usb2_hid.h>
+ #endif
++#  endif
+ #endif
+ #include <usbhid.h>
+ 
