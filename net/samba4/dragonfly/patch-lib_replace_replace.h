bfce5b7576ed7afb6751cb0cbd9e09c4cce93b6e
DPorts Builder <nobody@home.ok>
2014-03-22 08:30:18 -0700

    Update net/samba4 to version 4.0.16

---
+++ net/samba4/dragonfly/patch-lib_replace_replace.h
@@ -0,0 +1,10 @@
+--- lib/replace/replace.h.orig	2013-02-05 12:25:25.000000000 +0000
++++ lib/replace/replace.h
+@@ -57,7 +57,6 @@
+ #endif
+ 
+ #ifdef HAVE_MALLOC_H
+-#include <malloc.h>
+ #endif
+ 
+ #ifndef __PRI64_PREFIX
