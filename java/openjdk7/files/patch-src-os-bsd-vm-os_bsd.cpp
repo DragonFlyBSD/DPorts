3b1d086f6a75bb0f26c1b46d752b48b824ca85a8
DPorts Builder <nobody@home.ok>
2014-03-29 16:10:36 -0700

    Update java/openjdk7 to version 7.51.13_2,1

--- java/openjdk7/files/patch-src-os-bsd-vm-os_bsd.cpp
+++ java/openjdk7/files/patch-src-os-bsd-vm-os_bsd.cpp
@@ -1,4 +1,4 @@
-$FreeBSD: java/openjdk7/files/patch-src-os-bsd-vm-os_bsd.cpp 335863 2013-12-08 04:11:54Z glewis $
+$FreeBSD: head/java/openjdk7/files/patch-src-os-bsd-vm-os_bsd.cpp 343555 2014-02-09 23:12:11Z glewis $
 
 --- hotspot/src/os/bsd/vm/os_bsd.cpp.orig	2010-02-21 20:07:54.000000000 -0800
 +++ hotspot/src/os/bsd/vm/os_bsd.cpp	2010-02-21 20:08:41.000000000 -0800
--- java/openjdk7/files/patch-src-solaris-classes-sun-nio-ch-DefaultAsynchronousChanneldProvider.java
+++ java/openjdk7/files/patch-src-solaris-classes-sun-nio-ch-DefaultAsynchronousChanneldProvider.java
@@ -1,4 +1,4 @@
-$FreeBSD: java/openjdk7/files/patch-src-solaris-classes-sun-nio-ch-DefaultAsynchronousChanneldProvider.java 332063 2013-10-30 04:29:14Z glewis $
+$FreeBSD: head/java/openjdk7/files/patch-src-solaris-classes-sun-nio-ch-DefaultAsynchronousChanneldProvider.java 332063 2013-10-30 04:29:14Z glewis $
 
 --- jdk/src/solaris/classes/sun/nio/ch/DefaultAsynchronousChannelProvider.java.orig	2013-10-28 01:26:45.766774262 +0100
 +++ jdk/src/solaris/classes/sun/nio/ch/DefaultAsynchronousChannelProvider.java	2013-10-28 01:27:24.043766301 +0100
