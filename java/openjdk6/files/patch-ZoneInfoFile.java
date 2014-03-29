7aeb929d4a418ce8ece4a04379dea2338f2af756
DPorts Builder <nobody@home.ok>
2014-03-29 15:28:44 -0700

    Update java/openjdk6 to version b30_3,1

--- java/openjdk6/files/patch-ZoneInfoFile.java
+++ java/openjdk6/files/patch-ZoneInfoFile.java
@@ -1,4 +1,4 @@
-$FreeBSD: java/openjdk6/files/patch-ZoneInfoFile.java 300896 2012-07-14 13:54:48Z beat $
+$FreeBSD: head/java/openjdk6/files/patch-ZoneInfoFile.java 340725 2014-01-22 17:40:44Z mat $
 
 --- jdk/src/share/classes/sun/util/calendar/ZoneInfoFile.java.orig	2012-05-01 18:59:17.000000000 -0700
 +++ jdk/src/share/classes/sun/util/calendar/ZoneInfoFile.java	2012-05-01 19:02:16.000000000 -0700
