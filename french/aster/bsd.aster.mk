c4ff6f764b509962d2ae1a9670d3b37ccdbb20da
DPorts Builder <nobody@home.ok>
2014-04-04 06:45:10 -0700

    manually update aster to fix med pors

--- french/aster/bsd.aster.mk
+++ french/aster/bsd.aster.mk
@@ -1,5 +1,5 @@
 # Created by: Thierry Thomas (<thierry@pompo.net>)
-# $FreeBSD: french/aster/bsd.aster.mk 314686 2013-03-19 20:32:32Z thierry $
+# $FreeBSD$
 #
 # Common variables used by ports related to Code_Aster
 
@@ -9,18 +9,22 @@ MASTER_SITES=	http://www.code-aster.org/FICHIERS/
 DISTNAME=	aster-${DISTVERSIONPREFIX}${ASTER_DISTVERSION}${DISTVERSIONSUFFIX}
 DISTINFO_FILE=	${.CURDIR}/../../french/aster/distinfo
 
-ASTER_VER=	10.8.0
-ASTER_DISTVERSION=	${ASTER_VER}-3
+ASTER_VER=	11.5.0
+ASTER_DISTVERSION=	${ASTER_VER}-4
 EXTRACT_WRKSRC=	${WRKDIR}/aster-${DISTVERSIONPREFIX}${ASTER_VER}/SRC
 
 .if ${PORTNAME} == metis-edf
-DVSFX=		${DISTVERSIONSUFFIX}
-.elif ${PORTNAME} == homard
+TARNAME=	${PORTNAME:C|-edf||}
+.else
+TARNAME=	${PORTNAME}
+.endif
+
+.if ${PORTNAME} == homard
 DVSFX=		.all
 .endif
 
 .if ${PORTNAME} != aster
 post-extract:
 	(cd ${WRKDIR} &&	\
-	${EXTRACT_CMD} ${EXTRACT_BEFORE_ARGS} ${EXTRACT_WRKSRC}/${PORTNAME}${DVPFX}-${DISTVERSION}${DVSFX}${EXTRACT_SUFX} ${EXTRACT_AFTER_ARGS})
+	${EXTRACT_CMD} ${EXTRACT_BEFORE_ARGS} ${EXTRACT_WRKSRC}/${TARNAME}${DVPFX}-${DISTVERSION}${DVSFX}${EXTRACT_SUFX} ${EXTRACT_AFTER_ARGS})
 .endif
