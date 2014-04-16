bfc27a4e4d7d79f7d451a4f46985af3f8914d1a9
DPorts Builder <nobody@home.ok>
2014-04-16 13:39:18 -0700

    Update x11-drivers/xf86-input-mouse to version 1.9.0_2

--- x11-drivers/xf86-input-mouse/dragonfly/patch-src_bsd__mouse.c
+++ x11-drivers/xf86-input-mouse/dragonfly/patch-src_bsd__mouse.c
@@ -1,11 +1,16 @@
 --- src/bsd_mouse.c.intermediate	2013-10-16 12:47:20.514634000 +0000
 +++ src/bsd_mouse.c
-@@ -78,7 +78,11 @@
+@@ -78,7 +78,16 @@
  #include "usb.h"
  #endif
  
 +#if defined(__DragonFly__)
++#include <sys/param.h>
++#  if __DragonFly_version < 300703
 +#include <bus/usb/usb.h>
++#  else
++#include <bus/u4b/usb.h>
++#  endif
 +#else
  #include <dev/usb/usb.h>
 +#endif
