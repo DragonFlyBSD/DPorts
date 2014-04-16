40fec77100f5e30e6b422eb0ef37c17b770af74f
DPorts Builder <nobody@home.ok>
2014-04-16 13:58:01 -0700

    Tweak x11-drivers/xf86-input-joystick version 1.6.2

--- x11-drivers/xf86-input-joystick/dragonfly/patch-src_backend__bsd.c
+++ x11-drivers/xf86-input-joystick/dragonfly/patch-src_backend__bsd.c
@@ -1,12 +1,19 @@
---- src/backend_bsd.c.orig	2009-09-13 18:12:58.000000000 +0200
-+++ src/backend_bsd.c	2013-01-03 03:58:43.465654000 +0100
-@@ -41,11 +41,19 @@
+--- src/backend_bsd.c.orig	2011-04-20 18:57:00.000000000 +0000
++++ src/backend_bsd.c
+@@ -42,11 +42,26 @@
  #include <xf86_OSproc.h>
  
  #include <usbhid.h>
 +#ifdef __DragonFly__
++#include <sys/param.h>
++#  if __DragonFly_version < 300703
 +#include <bus/usb/usb.h>
 +#include <bus/usb/usbhid.h>
++#  else
++#include <bus/u4b/usb.h>
++#include <bus/u4b/usbhid.h>
++#include <bus/u4b/usb_ioctl.h>
++#  endif
 +#else
  #include <dev/usb/usb.h>
  #include <dev/usb/usbhid.h>
