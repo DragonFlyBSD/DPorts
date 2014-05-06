17c99a7d41a083e89a228aa9f6e7f102e65ac321
DPorts Builder <nobody@home.ok>
2014-05-06 16:53:41 -0700

    Update java/openjdk7 to version 7.55.13_4,1

--- java/openjdk7/dragonfly/patch-jdk_src_solaris_classes_sun_net_PortConfig.java
+++ java/openjdk7/dragonfly/patch-jdk_src_solaris_classes_sun_net_PortConfig.java
@@ -1,12 +1,10 @@
-$FreeBSD: head/java/openjdk7/files/patch-src-solaris-classes-sun-net-PortConfig.java 344496 2014-02-15 23:31:42Z glewis $
-
---- jdk/src/solaris/classes/sun/net/PortConfig.java	Sat Jan 18 12:20:24 2014 -0800
-+++ jdk/src/solaris/classes/sun/net/PortConfig.java	Sat Feb 15 14:31:26 2014 -0800
-@@ -52,7 +52,7 @@
+--- jdk/src/solaris/classes/sun/net/PortConfig.java.orig	2014-05-06 10:13:26.120233000 +0000
++++ jdk/src/solaris/classes/sun/net/PortConfig.java
+@@ -52,7 +52,7 @@ public final class PortConfig {
                      } else if (os.startsWith("SunOS")) {
                          defaultLower = 32768;
                          defaultUpper = 65535;
--                    } else if (os.contains("OS X")) {
+-                    } else if (os.contains("OS X") || os.endsWith("BSD")) {
 +                    } else if (os.contains("OS X") || os.contains("DragonFly") || os.endsWith("BSD")) {
                          defaultLower = 49152;
                          defaultUpper = 65535;
