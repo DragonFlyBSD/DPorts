96b60885f323a8c0f4feaa93cc02cd0703ea4492
DPorts Builder <nobody@home.ok>
2014-05-09 17:25:24 -0700

    Tweak sysutils/apcupsd version 3.14.10_1

--- sysutils/apcupsd/dragonfly/patch-src_drivers_usb_bsd_bsd-usb.c
+++ sysutils/apcupsd/dragonfly/patch-src_drivers_usb_bsd_bsd-usb.c
@@ -1,18 +1,12 @@
 --- src/drivers/usb/bsd/bsd-usb.c.orig	2011-09-02 15:44:13.000000000 +0000
 +++ src/drivers/usb/bsd/bsd-usb.c
-@@ -27,8 +27,19 @@
+@@ -27,8 +27,13 @@
  #include "apc.h"
  #include "hidutils.h"
  #include "../usb_common.h"
 +#ifdef __DragonFly__
-+#include <sys/param.h>
-+#  if __DragonFly_version < 300703
 +#include <bus/usb/usb.h>
 +#include <bus/usb/usbhid.h>
-+#  else
-+#include <bus/u4b/usb.h>
-+#include <bus/u4b/usbhid.h>
-+#  endif
 +#else
  #include <dev/usb/usb.h>
  #include <dev/usb/usbhid.h>
@@ -20,39 +14,3 @@
  
  /* Compatibility cruft for FreeBSD <= 4.7 */
  #ifndef USB_MAX_DEVNAMES
-@@ -97,6 +108,9 @@ static void reinitialize_private_structu
-  */
- static int init_device(UPSINFO *ups, const char *devname)
- {
-+#if defined(__DragonFly__) && __DragonFly_version >= 300703
-+   return 0;
-+#else
-    int fd, rc, rdesclen;
-    struct usb_device_info devinfo;
-    USB_DATA *my_data = (USB_DATA *)ups->driver_internal_data;
-@@ -182,6 +196,7 @@ static int init_device(UPSINFO *ups, con
-    my_data->intfd = fd;
- 
-    return 1;
-+#endif
- }
- 
- /*
-@@ -192,6 +207,9 @@ static int init_device(UPSINFO *ups, con
-  */
- static int open_usb_device(UPSINFO *ups)
- {
-+#if defined(__DragonFly__) && __DragonFly_version >= 300703
-+   return 0;
-+#else
-    int i, j, k, fd, rc;
-    char busname[] = "/dev/usbN";
-    char devname[USB_MAX_DEVNAMELEN + 5 + 1];
-@@ -284,6 +302,7 @@ static int open_usb_device(UPSINFO *ups)
- 
-    ups->device[0] = 0;
-    return 0;
-+#endif
- }
- 
- /* 
--- sysutils/apcupsd/dragonfly/patch-src_drivers_usb_bsd_hidutils.c
+++ sysutils/apcupsd/dragonfly/patch-src_drivers_usb_bsd_hidutils.c
@@ -1,19 +1,12 @@
 --- src/drivers/usb/bsd/hidutils.c.orig	2009-10-31 14:04:32.000000000 +0000
 +++ src/drivers/usb/bsd/hidutils.c
-@@ -26,8 +26,20 @@
+@@ -26,8 +26,13 @@
  #include "apc.h"
  
  #include "hidutils.h"
 +#ifdef __DragonFly__
-+#include <sys/param.h>
-+#  if __DragonFly_version < 300703
 +#include <bus/usb/usb.h>
 +#include <bus/usb/usbhid.h>
-+#  else
-+#include <bus/u4b/usb.h>
-+#include <bus/u4b/usbhid.h>
-+#include <bus/u4b/usb_ioctl.h>
-+#  endif
 +#else
  #include <dev/usb/usb.h>
  #include <dev/usb/usbhid.h>
@@ -21,55 +14,3 @@
  
  #define MAX_SANE_DESCRIPTOR_LEN 4096
  
-@@ -39,6 +51,9 @@
-  */
- unsigned char *hidu_fetch_report_descriptor(int fd, int *rlen)
- {
-+#if defined(__DragonFly__) && __DragonFly_version >= 300703
-+   return NULL;
-+#else
-    int rc, i, rdesclen;
-    struct usb_config_desc cdesc;
-    struct usb_full_desc fdesc;
-@@ -148,6 +163,7 @@ unsigned char *hidu_fetch_report_descrip
- 
-    *rlen = rdesclen;
-    return (unsigned char *)req.ucr_data;
-+#endif
- }
- 
- /* Push a value onto the collection stack */
-@@ -271,6 +287,9 @@ int hidu_locate_item(report_desc_t rdesc
-  */
- int hidu_get_report(int fd, hid_item_t *item, unsigned char *data, int len)
- {
-+#if defined(__DragonFly__) && __DragonFly_version >= 300703
-+   return -1;
-+#else
-    int rc;
-    struct usb_ctl_request req;
- 
-@@ -299,6 +318,7 @@ int hidu_get_report(int fd, hid_item_t *
-    hex_dump(300, data, req.ucr_actlen);
- 
-    return req.ucr_actlen;
-+#endif
- }
- 
- /*
-@@ -345,6 +365,9 @@ int hidu_set_report(int fd, hid_item_t *
-  */
- const char *hidu_get_string(int fd, int index)
- {
-+#if defined(__DragonFly__) && __DragonFly_version >= 300703
-+   return NULL;
-+#else
-    int rc, i;
-    struct usb_string_desc sd;
-    static char string[128];
-@@ -371,4 +394,5 @@ const char *hidu_get_string(int fd, int
- 
-    string[i] = '\0';
-    return string;
-+#endif
- }
