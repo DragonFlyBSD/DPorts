65204a1cbca08915393238d482caecf966a51545
DPorts Builder <nobody@home.ok>
2014-05-26 06:33:07 -0700

    Update devel/ipython to version 2.1.0_1

--- devel/ipython/files/patch-setupbase.py
+++ devel/ipython/files/patch-setupbase.py
@@ -1,22 +1,14 @@
 
-$FreeBSD: head/devel/ipython/files/patch-setupbase.py 325353 2013-08-25 16:13:06Z olgeni $
+$FreeBSD: head/devel/ipython/files/patch-setupbase.py 354891 2014-05-22 21:05:22Z olgeni $
 
 --- setupbase.py.orig
 +++ setupbase.py
-@@ -208,7 +208,7 @@
+@@ -272,7 +272,7 @@
+     Just man pages at this point.
      """
  
-     docdirbase  = pjoin('share', 'doc', 'ipython')
 -    manpagebase = pjoin('share', 'man', 'man1')
 +    manpagebase = pjoin('man', 'man1')
  
      # Simple file lists can be made by hand
      manpages = [f for f in glob(pjoin('docs','man','*.1.gz')) if isfile(f)]
-@@ -232,7 +232,6 @@
- 
-     # And assemble the entire output list
-     data_files = [ (manpagebase, manpages),
--                   (pjoin(docdirbase, 'extensions'), igridhelpfiles),
-                    ] + manual_files + example_files
- 
-     return data_files
