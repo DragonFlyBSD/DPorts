96b187b967d05e93ecd2fdde8c9d3c7e7d4b0577
DPorts Builder <nobody@home.ok>
2014-05-27 03:30:17 -0700

    Update sysutils/e2fsprogs to version 1.42.10

--- sysutils/e2fsprogs/dragonfly/patch-misc_util.c
+++ sysutils/e2fsprogs/dragonfly/patch-misc_util.c
@@ -1,11 +1,12 @@
---- ./misc/util.c.orig	2014-02-09 16:29:27.105527000 +0000
+--- ./misc/util.c.orig	2014-05-11 22:24:32.000000000 +0000
 +++ ./misc/util.c
-@@ -93,7 +93,7 @@ void check_plausibility(const char *devi
- 				"did you specify it correctly?\n"), stderr);
- 		exit(1);
- 	}
+@@ -218,7 +218,8 @@ int check_plausibility(const char *devic
+ 
+ 	if (S_ISBLK(s.st_mode))
+ 		is_dev = 1;
 -#if defined(__FreeBSD__) || defined(__FreeBSD_kernel__)
-+#if defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || defined(__DragonFly__)
++#if defined(__FreeBSD__) || defined(__FreeBSD_kernel__) \
++ || defined(__DragonFly__)
  	/* On FreeBSD, all disk devices are character specials */
- 	if (!S_ISBLK(s.st_mode) && !S_ISCHR(s.st_mode))
- #else
+ 	if (S_ISCHR(s.st_mode))
+ 		is_dev = 1;
