fdc63ec2478d67897221f2d6f0c65cd59fc6f646
DPorts Builder <nobody@home.ok>
2014-05-27 03:31:26 -0700

    Update databases/mysqltuner to version 1.3.0

--- databases/mysqltuner/dragonfly/patch-mysqltuner.pl
+++ databases/mysqltuner/dragonfly/patch-mysqltuner.pl
@@ -1,10 +1,10 @@
---- mysqltuner.pl.orig	2011-04-02 12:03:32.000000000 +0000
+--- mysqltuner.pl.orig	2014-05-26 20:46:11.623082000 +0000
 +++ mysqltuner.pl
-@@ -219,7 +219,7 @@ sub os_setup {
+@@ -221,7 +221,7 @@ sub os_setup {
  		} elsif ($os =~ /Darwin/) {
  			$physical_memory = `sysctl -n hw.memsize` or memerror;
  			$swap_memory = `sysctl -n vm.swapusage | awk '{print \$3}' | sed 's/\..*\$//'` or memerror;
--		} elsif ($os =~ /NetBSD|OpenBSD/) {
+-		} elsif ($os =~ /NetBSD|OpenBSD|FreeBSD/) {
 +		} elsif ($os =~ /NetBSD|OpenBSD|DragonFly|FreeBSD/) {
  			$physical_memory = `sysctl -n hw.physmem` or memerror;
  			if ($physical_memory < 0) {
