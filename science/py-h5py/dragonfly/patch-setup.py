25044468ebe1cdba42744514bb26ac819a662397
DPorts Builder <nobody@home.ok>
2014-06-21 15:15:50 -0700

    Update science/py-h5py to version 2.3.0

--- science/py-h5py/dragonfly/patch-setup.py
+++ science/py-h5py/dragonfly/patch-setup.py
@@ -1,11 +1,12 @@
---- setup.py.intermediate	2014-03-30 09:28:33.672237000 +0000
+--- setup.py.orig	2014-05-23 21:07:21.000000000 +0000
 +++ setup.py
-@@ -135,7 +135,7 @@ else:
-         COMPILER_SETTINGS['library_dirs'] += ['/opt/local/lib']     # macports
+@@ -164,6 +164,9 @@ else:
+     elif sys.platform.startswith('freebsd'):
          COMPILER_SETTINGS['include_dirs'] += ['/usr/local/include'] # homebrew
          COMPILER_SETTINGS['library_dirs'] += ['/usr/local/lib']     # homebrew
--    elif sys.platform.startswith('freebsd'):
 +    elif sys.platform.startswith('dragonfly'):
-         COMPILER_SETTINGS['include_dirs'] += ['%%LOCALBASE%%/include'] # homebrew
-         COMPILER_SETTINGS['library_dirs'] += ['%%LOCALBASE%%/lib']     # homebrew
++        COMPILER_SETTINGS['include_dirs'] += ['/usr/local/include'] # homebrew
++        COMPILER_SETTINGS['library_dirs'] += ['/usr/local/lib']     # homebrew
      if MPI:
+         COMPILER_SETTINGS['include_dirs'] += [mpi4py.get_include()]
+     COMPILER_SETTINGS['runtime_library_dirs'] = [op.abspath(x) for x in COMPILER_SETTINGS['library_dirs']]
