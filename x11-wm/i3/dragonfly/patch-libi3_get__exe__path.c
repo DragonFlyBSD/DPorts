fa333f37003f9f42925d573103cf2bfe33b32088
DPorts Builder <nobody@home.ok>
2014-06-21 15:15:44 -0700

    Update x11-wm/i3 to version 4.8

--- x11-wm/i3/dragonfly/patch-libi3_get__exe__path.c
+++ x11-wm/i3/dragonfly/patch-libi3_get__exe__path.c
@@ -1,17 +1,17 @@
---- libi3/get_exe_path.c.orig	2014-01-23 21:52:24.000000000 +0000
+--- libi3/get_exe_path.c.orig	2014-06-15 17:12:43.000000000 +0000
 +++ libi3/get_exe_path.c
-@@ -21,11 +21,12 @@ char *get_exe_path(const char *argv0) {
- 	char *tmp = smalloc(tmp_size);
- 
+@@ -20,11 +20,12 @@ char *get_exe_path(const char *argv0) {
+     char *destpath = smalloc(destpath_size);
+     char *tmp = smalloc(tmp_size);
  
 -#if defined(__linux__) || defined(__FreeBSD__) || defined(__FreeBSD_kernel__)
 +#if defined(__linux__) || defined(__DragonFly__) \
 + || defined(__FreeBSD__) || defined(__FreeBSD_kernel__)
- 	/* Linux and Debian/kFreeBSD provide /proc/self/exe */
+ /* Linux and Debian/kFreeBSD provide /proc/self/exe */
  #if defined(__linux__) || defined(__FreeBSD_kernel__)
- 	const char *exepath = "/proc/self/exe";
+     const char *exepath = "/proc/self/exe";
 -#elif defined(__FreeBSD__)
 +#elif defined(__FreeBSD__) || defined(__DragonFly__)
- 	const char *exepath = "/proc/curproc/file";
+     const char *exepath = "/proc/curproc/file";
  #endif
- 	ssize_t linksize;
+     ssize_t linksize;
