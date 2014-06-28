951062b75d5bddcc12fd3ee90661634095f51c15
DPorts Builder <nobody@home.ok>
2014-06-28 01:31:02 -0700

    Tweak lang/python32 version 3.2.5_9

--- lang/python32/dragonfly/patch-setup.py
+++ lang/python32/dragonfly/patch-setup.py
@@ -1,6 +1,17 @@
---- setup.py.intermediate	2014-01-26 22:42:10.000000000 +0000
+--- setup.py.intermediate	2014-06-28 07:44:16.389272000 +0000
 +++ setup.py
-@@ -1423,7 +1423,7 @@ class PyBuildExt(build_ext):
+@@ -1375,6 +1375,10 @@ class PyBuildExt(build_ext):
+             macros = dict()
+             libraries = []
+ 
++        elif platform.startswith('dragonfly'):
++            macros = dict()
++            libraries = []
++
+         elif platform in ('freebsd7', 'freebsd8', 'freebsd9', 'freebsd10'):
+             # FreeBSD's P1003.1b semaphore support is very experimental
+             # and has many known problems. (as of June 2008)
+@@ -1423,7 +1427,7 @@ class PyBuildExt(build_ext):
          # Backport Issue: http://bugs.python.org/issue12326
          #############################
          if any(platform.startswith(prefix)
