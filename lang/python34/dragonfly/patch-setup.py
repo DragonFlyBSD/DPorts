b061efd6bca70663fd95638811340b740a89bf99
DPorts Builder <nobody@home.ok>
2014-06-28 01:51:47 -0700

    Tweak lang/python34 version 3.4.1_1

--- lang/python34/dragonfly/patch-setup.py
+++ lang/python34/dragonfly/patch-setup.py
@@ -1,6 +1,17 @@
---- setup.py.orig	2014-04-17 12:03:40.702726000 +0000
+--- setup.py.orig	2014-06-28 08:44:47.215418000 +0000
 +++ setup.py
-@@ -1515,7 +1515,7 @@ class PyBuildExt(build_ext):
+@@ -1486,6 +1486,10 @@ class PyBuildExt(build_ext):
+             macros = dict()
+             libraries = []
+ 
++        elif host_platform.startswith('dragonfly'):
++            macros = dict()
++            libraries = []
++
+         elif host_platform in ('freebsd4', 'freebsd5', 'freebsd6', 'freebsd7', 'freebsd8'):
+             # FreeBSD's P1003.1b semaphore support is very experimental
+             # and has many known problems. (as of June 2008)
+@@ -1525,7 +1529,7 @@ class PyBuildExt(build_ext):
          # End multiprocessing
  
          # Platform-specific libraries
