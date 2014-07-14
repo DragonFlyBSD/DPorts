5c422a386908aa617a311210cc86140dae86a41d
DPorts Builder <nobody@home.ok>
2014-07-13 18:33:52 -0700

    Update mail/qmail to version 1.06_1

--- mail/qmail/dragonfly/patch-cdb_seek.c
+++
@@ -1,9 +0,0 @@
---- cdb_seek.c.orig	2013-01-19 10:40:00.910379000 +0100
-+++ cdb_seek.c	2013-01-19 10:59:45.742034000 +0100
-@@ -1,6 +1,5 @@
- #include <sys/types.h>
- #include <errno.h>
--extern int errno;
- #include "cdb.h"
- 
- #ifndef SEEK_SET
--- mail/qmail/dragonfly/patch-error.c
+++
@@ -1,7 +0,0 @@
---- error.c.orig	2013-01-19 11:00:48.272121000 +0100
-+++ error.c	2013-01-19 11:03:29.462346000 +0100
-@@ -1,4 +1,3 @@
--#include <errno.h>
- #include "error.h"
- 
- /* warning: as coverage improves here, should update error_{str,temp} */
