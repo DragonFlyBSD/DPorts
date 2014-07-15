db8e8a7e9dbcb5431a61fb555b18a05ec3c14a78
DPorts Builder <nobody@home.ok>
2014-07-15 16:14:19 -0700

    Update net/dhcpd-pools to version 2.25

--- net/dhcpd-pools/dragonfly/patch-lib_fpending.h
+++
@@ -1,9 +0,0 @@
---- lib/fpending.h.orig	2013-10-20 18:53:22.000000000 +0000
-+++ lib/fpending.h
-@@ -24,6 +24,3 @@
- # include <stdio_ext.h>
- #endif
- 
--#ifndef __fpending
--size_t __fpending (FILE *) _GL_ATTRIBUTE_PURE;
--#endif
