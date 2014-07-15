dfb8381216c17cf59f5fb566f9d2c21a2410c810
DPorts Builder <nobody@home.ok>
2014-07-15 07:02:31 -0700

    Update devel/R-cran-Rcpp to version 0.11.2

--- devel/R-cran-Rcpp/dragonfly/patch-src_api.cpp
+++ devel/R-cran-Rcpp/dragonfly/patch-src_api.cpp
@@ -1,8 +1,8 @@
---- src/api.cpp.orig	2014-02-02 14:27:39.000000000 +0000
+--- src/api.cpp.orig	2014-06-06 23:38:58.000000000 +0000
 +++ src/api.cpp
 @@ -34,7 +34,9 @@ using namespace Rcpp ;
  #endif
-      
+ 
  #if defined(__GNUC__)
 -    #if defined(WIN32) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__CYGWIN__) || defined(__sun)
 +    #if defined(WIN32) || defined(__FreeBSD__) || defined(__NetBSD__) \
@@ -10,7 +10,7 @@
 +     || defined(__DragonFly__)
      #else
          #include <execinfo.h>
-         
+ 
 @@ -239,7 +241,9 @@ SEXP rcpp_can_use_cxx0x(){
  // [[Rcpp::register]]
  SEXP stack_trace( const char* file, int line ){
@@ -19,6 +19,6 @@
 +        #if defined(WIN32) || defined(__FreeBSD__) || defined(__NetBSD__) \
 +         || defined(__OpenBSD__) || defined(__CYGWIN__) || defined(__sun) \
 +         || defined(__DragonFly__)
-             // Simpler version for Windows and *BSD 
-             List trace = List::create( 
-                 _[ "file"  ] = file, 
+             // Simpler version for Windows and *BSD
+             List trace = List::create(
+                 _[ "file"  ] = file,
