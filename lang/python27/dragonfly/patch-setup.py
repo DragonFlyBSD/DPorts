7aad5cbd9cb53c4f7e0654f69d1ec23015efcd5a
DPorts Builder <nobody@home.ok>
2014-07-20 11:39:43 -0700

    Update lang/python27 to version 2.7.8

--- lang/python27/dragonfly/patch-setup.py
+++ lang/python27/dragonfly/patch-setup.py
@@ -1,6 +1,6 @@
---- setup.py.intermediate	2014-01-30 14:00:56.179431000 +0000
+--- setup.py.orig	2014-07-20 12:11:37.904857000 +0000
 +++ setup.py
-@@ -1549,6 +1549,10 @@ class PyBuildExt(build_ext):
+@@ -1553,6 +1553,10 @@ class PyBuildExt(build_ext):
              macros = dict()
              libraries = []
  
@@ -11,15 +11,12 @@
          elif host_platform in ('freebsd4', 'freebsd5', 'freebsd6', 'freebsd7', 'freebsd8', 'freebsd9', 'freebsd10', 'freebsd11'):
              # FreeBSD's P1003.1b semaphore support is very experimental
              # and has many known problems. (as of June 2008)
-@@ -1600,8 +1604,9 @@ class PyBuildExt(build_ext):
-         else:
-             missing.append('linuxaudiodev')
+@@ -1607,7 +1611,7 @@ class PyBuildExt(build_ext):
+ # Initial backport of http://hg.python.org/cpython/rev/50f1922bc1d5
  
--        if (host_platform in ('linux2', 'freebsd4', 'freebsd5', 'freebsd6',
--                        'freebsd7', 'freebsd8', 'freebsd9', 'freebsd10', 'freebsd11')
-+        if (host_platform in ('linux2')
-+            or host_platform.startswith("freebsd")
-+            or host_platform.startswith("dragonfly")
-             or host_platform.startswith("gnukfreebsd")):
+         if any(sys.platform.startswith(prefix)
+-               for prefix in ("linux", "freebsd", "gnukfreebsd")):
++               for prefix in ("linux", "dragon", "freebsd", "gnukfreebsd")):
              exts.append( Extension('ossaudiodev', ['ossaudiodev.c']) )
          else:
+             missing.append('ossaudiodev')
