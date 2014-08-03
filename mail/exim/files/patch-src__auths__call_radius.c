1ba9b02549dc4249f910b239bebe6e36226d4cdc
DPorts Builder <nobody@home.ok>
2014-08-02 17:14:38 -0700

    Update mail/exim to version 4.83_3

--- mail/exim/files/patch-src__auths__call_radius.c
+++ mail/exim/files/patch-src__auths__call_radius.c
@@ -1,5 +1,5 @@
 
-$FreeBSD: head/mail/exim/files/patch-src::auths::call_radius.c 340725 2014-01-22 17:40:44Z mat $
+$FreeBSD: head/mail/exim/files/patch-src__auths__call_radius.c 363362 2014-07-29 17:12:47Z adamw $
 
 --- src/auths/call_radius.c
 +++ src/auths/call_radius.c
