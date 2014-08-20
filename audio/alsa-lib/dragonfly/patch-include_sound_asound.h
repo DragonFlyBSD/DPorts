558a68bd2a3f214ea916a7f82b01f8a09caf9305
DPorts Builder <nobody@home.ok>
2014-08-20 13:14:38 -0700

    Update audio/alsa-lib to version 1.0.28

--- audio/alsa-lib/dragonfly/patch-include_sound_asound.h
+++ audio/alsa-lib/dragonfly/patch-include_sound_asound.h
@@ -1,4 +1,4 @@
---- include/sound/asound.h.intermediate	2013-10-14 14:57:44.350599000 +0000
+--- include/sound/asound.h.orig	2014-06-17 12:34:19.000000000 +0000
 +++ include/sound/asound.h
 @@ -23,6 +23,9 @@
  #ifndef _UAPI__SOUND_ASOUND_H
@@ -7,6 +7,6 @@
 +#ifdef __DragonFly__
 +#include <sys/time.h>
 +#endif
- #ifdef __linux__
  #include <linux/types.h>
- #else
+ 
+ 
