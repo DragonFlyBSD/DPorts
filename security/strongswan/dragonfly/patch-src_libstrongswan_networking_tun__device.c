d81e09a16423f9e701f1be566b32ca02dbc7629d
DPorts Builder <nobody@home.ok>
2014-08-22 15:01:43 -0700

    Update security/strongswan to version 5.2.0_1

--- security/strongswan/dragonfly/patch-src_libstrongswan_networking_tun__device.c
+++ security/strongswan/dragonfly/patch-src_libstrongswan_networking_tun__device.c
@@ -1,11 +1,13 @@
---- src/libstrongswan/networking/tun_device.c.orig	2013-04-10 09:10:56.000000000 +0000
+--- src/libstrongswan/networking/tun_device.c.orig	2014-06-05 07:50:30.000000000 +0000
 +++ src/libstrongswan/networking/tun_device.c
-@@ -33,6 +33,8 @@
- #include <sys/kern_control.h>
+@@ -49,6 +49,10 @@ tun_device_t *tun_device_create(const ch
  #elif defined(__linux__)
+ #include <linux/types.h>
  #include <linux/if_tun.h>
 +#elif defined(__DragonFly__)
 +#include <net/tun/if_tun.h>
- #else
++#include <net/if_var.h>
++#include <netinet/in_var.h>
+ #elif __FreeBSD__ >= 10
  #include <net/if_tun.h>
- #endif
+ #include <net/if_var.h>
