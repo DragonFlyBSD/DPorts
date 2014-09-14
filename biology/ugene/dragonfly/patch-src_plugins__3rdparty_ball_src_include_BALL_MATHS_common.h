5ad67957d110b76cddd76444eeadc7045392f8fc
DPorts Builder <nobody@home.ok>
2014-09-14 16:14:39 -0700

    Update biology/ugene to version 1.14.0_1

--- biology/ugene/dragonfly/patch-src_plugins__3rdparty_ball_src_include_BALL_MATHS_common.h
+++ biology/ugene/dragonfly/patch-src_plugins__3rdparty_ball_src_include_BALL_MATHS_common.h
@@ -1,11 +1,11 @@
---- src/plugins_3rdparty/ball/src/include/BALL/MATHS/common.h.orig	2014-02-07 16:52:32.000000000 +0000
+--- src/plugins_3rdparty/ball/src/include/BALL/MATHS/common.h.orig	2014-09-13 18:15:15 UTC
 +++ src/plugins_3rdparty/ball/src/include/BALL/MATHS/common.h
-@@ -180,7 +180,7 @@ namespace BALL
- 			#ifdef BALL_COMPILER_MSVC
- 				return (_isnan(t) != 0);
- 			#else
--				return (isnan(t) != 0);
-+				return (std::isnan(t) != 0);
- 			#endif
- 		}
- 
+@@ -175,7 +175,7 @@ namespace BALL
+                 #ifdef BALL_OS_WINDOWS
+                     return (_isnan(t) != 0);
+                 #else
+-                    return (isnan(t) != 0);
++                    return (std::isnan(t) != 0);
+                 #endif
+             #endif
+         }
