9189132b3ef5158c93dbb4a0de20cc902e3324fc
DPorts Builder <nobody@home.ok>
2014-09-14 04:42:04 -0700

    Update x11-drivers/xf86-video-intel29 to version 2.99.916

--- x11-drivers/xf86-video-intel29/files/patch-src_intel__device.c
+++ x11-drivers/xf86-video-intel29/files/patch-src_intel__device.c
@@ -1,13 +1,13 @@
---- src/intel_device.c.orig	2014-06-06 12:29:52.000000000 +0000
+--- src/intel_device.c.orig	2014-08-31 10:47:52 UTC
 +++ src/intel_device.c
-@@ -159,8 +159,8 @@ static int __intel_open_device(const str
- 			 pci->domain, pci->bus, pci->dev, pci->func);
+@@ -401,8 +401,8 @@ static int __intel_open_device__legacy(c
+ 		 pci->domain, pci->bus, pci->dev, pci->func);
  
- 		ret = drmCheckModesettingSupported(id);
--		if (ret) {
--			if (xf86LoadKernelModule("i915"))
-+		if (ret || 1) {
-+			if (xf86LoadKernelModule("i915kms"))
- 				ret = drmCheckModesettingSupported(id);
- 			if (ret)
- 				return -1;
+ 	ret = drmCheckModesettingSupported(id);
+-	if (ret) {
+-		if (xf86LoadKernelModule("i915"))
++	if (ret || 1) {
++		if (xf86LoadKernelModule("i915kms"))
+ 			ret = drmCheckModesettingSupported(id);
+ 		if (ret)
+ 			return -1;
--- x11-drivers/xf86-video-intel29/files/patch-src_sna_sna__display.c
+++ x11-drivers/xf86-video-intel29/files/patch-src_sna_sna__display.c
@@ -1,11 +1,11 @@
---- src/sna/sna_display.c.orig	2014-03-18 15:55:13.000000000 +0000
+--- src/sna/sna_display.c.orig	2014-09-08 06:31:57 UTC
 +++ src/sna/sna_display.c
-@@ -3297,7 +3297,7 @@ static bool sna_probe_initial_configurat
- 
- 	assert((sna->flags & SNA_IS_HOSTED) == 0);
- 
--	if (xf86ReturnOptValBool(sna->Options, OPTION_REPROBE, FALSE)) {
-+	if (xf86ReturnOptValBool(sna->Options, OPTION_REPROBE, FALSE) || 1) {
- 		DBG(("%s: user requests reprobing\n", __FUNCTION__));
- 		return false;
- 	}
+@@ -5343,7 +5343,7 @@ static bool sna_probe_initial_configurat
+ 			OPTION_ROTATE,
+ 			OPTION_PANNING,
+ 		};
+-		if (xf86ReturnOptValBool(sna->Options, OPTION_REPROBE, FALSE)) {
++		if (xf86ReturnOptValBool(sna->Options, OPTION_REPROBE, FALSE) || 1) {
+ 			DBG(("%s: user requests reprobing\n", __FUNCTION__));
+ 			return false;
+ 		}
