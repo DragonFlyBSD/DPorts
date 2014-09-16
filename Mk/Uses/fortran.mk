431c63cd730a18c7d95c48a3aca87e17bbefe3b3
DPorts Builder <nobody@home.ok>
2014-09-16 01:21:21 -0700

    manually fix fortran ports

--- Mk/Uses/fortran.mk
+++ Mk/Uses/fortran.mk
@@ -18,8 +18,8 @@ fortran_ARGS=	gcc
 .if ${fortran_ARGS} == gcc
 .include "${PORTSDIR}/Mk/bsd.default-versions.mk"
 _GCC_VER=	${GCC_DEFAULT:S/.//}
-BUILD_DEPENDS+=	gfortran${_GCC_VER}:${PORTSDIR}/lang/gcc
-RUN_DEPENDS+=	gfortran${_GCC_VER}:${PORTSDIR}/lang/gcc
+BUILD_DEPENDS+=	gfortran${_GCC_VER}:${PORTSDIR}/lang/gcc${_GCC_VER}
+RUN_DEPENDS+=	gfortran${_GCC_VER}:${PORTSDIR}/lang/gcc${_GCC_VER}
 USE_BINUTILS=	yes
 F77=		gfortran${_GCC_VER}
 FC=		gfortran${_GCC_VER}
