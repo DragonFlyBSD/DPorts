c8d887b2e90b2969ff52d03cd5306508dc651f0c
DPorts Builder <nobody@home.ok>
2014-09-21 04:15:42 -0700

    Update www/chromium to version 37.0.2062.120_3

---
+++ www/chromium/dragonfly/patch-third__party_webrtc_system__wrappers_source_spreadsortlib_spreadsort.hpp
@@ -0,0 +1,13 @@
+--- third_party/webrtc/system_wrappers/source/spreadsortlib/spreadsort.hpp.orig	2014-09-09 23:47:56.000000000 +0000
++++ third_party/webrtc/system_wrappers/source/spreadsortlib/spreadsort.hpp
+@@ -21,6 +21,10 @@ Scott McMurray
+ #include <vector>
+ #include "webrtc/system_wrappers/source/spreadsortlib/constants.hpp"
+ 
++#ifdef __DragonFly__
++#  define getchar boost_getchar
++#endif
++
+ namespace boost {
+   namespace detail {
+   	//This only works on unsigned data types
