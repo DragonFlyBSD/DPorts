78d860cbbb6b05d4c77e8a1412c06a359f86800b
DPorts Builder <nobody@home.ok>
2014-09-21 01:40:53 -0700

    Update dns/dualserver to version 6.94,1

--- dns/dualserver/dragonfly/patch-dualserverd.cpp
+++ dns/dualserver/dragonfly/patch-dualserverd.cpp
@@ -1,4 +1,4 @@
---- dualserverd.cpp.intermediate	2013-08-19 09:58:19.800566000 +0000
+--- dualserverd.cpp.orig	2014-09-21 08:13:19.230298000 +0000
 +++ dualserverd.cpp
 @@ -48,6 +48,11 @@
  #ifdef __FreeBSD__
@@ -12,61 +12,61 @@
  using namespace std;
  #include "dualserverd.h"
  
-@@ -99,6 +104,8 @@ const char send404[] = "HTTP/1.1 404 Not
- const char td200[] = "<td>%s</td>";
+@@ -95,6 +100,8 @@ const char send200[] = "HTTP/1.1 200 OK\
+ const char line200[] = "<td>%s</td>";
  #ifdef __FreeBSD__
- const char sVersion[] = "Dual DHCP DNS Server Version 7.02 for FreeBSD Build 7019";
+ const char sVersion[] = "Dual DHCP DNS Server Version 6.94 for FreeBSD";
 +#elif defined __DragonFly__
-+const char sVersion[] = "Dual DHCP DNS Server Version 7.02 for DragonFly Build 7019";
++const char sVersion[] = "Dual DHCP DNS Server Version 6.94 for DragonFly";
  #else
- const char sVersion[] = "Dual DHCP DNS Server Version 7.02 Linux Build 7019";
+ const char sVersion[] = "Dual DHCP DNS Server Version 6.94 Linux Build 7011";
  #endif
-@@ -294,7 +301,7 @@ int main(int argc, char **argv)
-         strcpy(leaFile, "/tmp/dualserver.state");
- 
-     if (!iniFile[0])
--#ifdef __FreeBSD__
-+#if defined(__FreeBSD__) || defined(__DragonFly__)
-         strcpy(iniFile, "/usr/local/etc/dualserver.conf");
- #else
-         strcpy(iniFile, "/etc/dualserver.ini");
-@@ -4285,7 +4292,7 @@ MYDWORD sdmess(data9 *req)
+@@ -4015,7 +4022,7 @@ MYDWORD sdmess(data9 *req)
  	else if (req->dhcpp.header.bp_broadcast || !req->remote.sin_addr.s_addr || req->reqIP)
  	{
  		req->remote.sin_port = htons(IPPORT_DHCPC);
 -#ifdef __FreeBSD__
-+#if defined(__FreeBSD__) || defined(__DragonFly)
-         req->remote.sin_addr.s_addr = network.dhcpConn[req->sockInd].addr.sin_addr.s_addr | (~network.dhcpConn[req->sockInd].mask);
-         //printf("broadcast dhcp response to %s\n",inet_ntoa(req->remote.sin_addr));
- #else
-@@ -9532,7 +9539,7 @@ void *init(void *lparam)
++#if defined(__FreeBSD__) || defined(__DragonFly__)
+     req->remote.sin_addr.s_addr = network.dhcpConn[req->sockInd].addr.sin_addr.s_addr | 
+     (~network.dhcpConn[req->sockInd].mask);
+     //printf("broadcast dhcp response to %s\n",inet_ntoa(req->remote.sin_addr));
+@@ -9057,7 +9064,7 @@ void *init(void *lparam)
  					setsockopt(newNetwork.dhcpListener.sock, SOL_SOCKET, SO_REUSEADDR, (char*)&newNetwork.dhcpListener.reUseVal, newNetwork.dhcpListener.reUseSize);
  					newNetwork.dhcpListener.pktinfoVal = true;
  					newNetwork.dhcpListener.pktinfoSize = sizeof(newNetwork.dhcpListener.pktinfoVal);
 -#ifdef __FreeBSD__
 +#if defined(__FreeBSD__) || defined(__DragonFly__)
-                     setsockopt(newNetwork.dhcpListener.sock, IPPROTO_IP, IP_RECVIF, &newNetwork.dhcpListener.pktinfoVal, newNetwork.dhcpListener.pktinfoSize);
+ 					setsockopt(newNetwork.dhcpListener.sock, IPPROTO_IP, IP_RECVIF, &newNetwork.dhcpListener.pktinfoVal, newNetwork.dhcpListener.pktinfoSize);
  #else
  					setsockopt(newNetwork.dhcpListener.sock, IPPROTO_IP, IP_PKTINFO, &newNetwork.dhcpListener.pktinfoVal, newNetwork.dhcpListener.pktinfoSize);
-@@ -10179,7 +10186,7 @@ void *updateStateFile(void *lparam)
+@@ -9523,7 +9530,7 @@ void getInterfaces(data1 *network)
+ 
+ 			if (addr && !(flags & IFF_LOOPBACK))
+ 				addServer(network->allServers, addr);
+-#ifdef __FreeBSD__
++#if defined(__FreeBSD__) || defined(__DragonFly__)
+ 			if (addr && mask && !(flags & IFF_POINTOPOINT) && !(flags & IFF_LOOPBACK))
+ #else
+ 			if (addr && mask && !(flags & IFF_POINTOPOINT) && !(flags & IFF_LOOPBACK) && !(flags & IFF_DYNAMIC))
+@@ -9686,7 +9693,7 @@ void *updateStateFile(void *lparam)
  
  MYWORD gdmess(data9 *req, MYBYTE sockInd)
  {
 -#ifdef __FreeBSD__
 +#if defined(__FreeBSD__) || defined(__DragonFly__)
-   struct cmsghdr *cmsg;
-   struct sockaddr_dl * isdl;
-   struct sockaddr_in *isin;
-@@ -10200,7 +10207,7 @@ MYWORD gdmess(data9 *req, MYBYTE sockInd
+     struct cmsghdr *cmsg;
+     struct sockaddr_dl * isdl;
+     struct sockaddr_in *isin;
+@@ -9707,7 +9714,7 @@ MYWORD gdmess(data9 *req, MYBYTE sockInd
          req->msg.msg_name = &req->remote;
          req->msg.msg_namelen = sizeof(sockaddr_in);
          req->msg.msg_control = &req->msgcontrol;
 -#ifdef __FreeBSD__
-+#if defined(__FreeBSD__) || defined(__DragonFly)
++#if defined(__FreeBSD__) || defined(__DragonFly__)
          req->msg.msg_controllen = sizeof(req->msgcontrol);
  #else
          req->msg.msg_controllen = sizeof(msg_control);
-@@ -10213,7 +10220,7 @@ MYWORD gdmess(data9 *req, MYBYTE sockInd
+@@ -9720,7 +9727,7 @@ MYWORD gdmess(data9 *req, MYBYTE sockInd
          if (errno || req->bytes <= 0)
              return 0;
  
--- dns/dualserver/dragonfly/patch-dualserverd.h
+++ dns/dualserver/dragonfly/patch-dualserverd.h
@@ -1,28 +1,24 @@
---- dualserverd.h.intermediate	2013-08-19 09:58:19.810566000 +0000
+--- dualserverd.h.intermediate	2014-09-21 08:13:19.240298000 +0000
 +++ dualserverd.h
-@@ -623,14 +623,14 @@ struct data20
- 
- struct msg_control
- {
--#ifdef __FreeBSD__
-+#if defined(__FreeBSD__) || defined(__DragonFly__)
-   u_long cmsg_len;
- #else
- 	ulong cmsg_len;
+@@ -53,10 +53,12 @@ typedef struct in_pktinfo IN_PKTINFO;
  #endif
+ 
+ #ifndef __FreeBSD__
++#ifndef __DragonFly__
+ #ifndef IFF_DYNAMIC
+ #define IFF_DYNAMIC 0x8000
+ #endif
+ #endif
++#endif
+ 
+ #define MYWORD unsigned short
+ #define MYBYTE unsigned char
+@@ -599,7 +601,7 @@ struct msg_control
+ 	u_long cmsg_len;
  	int cmsg_level;
  	int cmsg_type;
--#ifdef __FreeBSD__
-+#if defined(__FreeBSD__) || defined(__DragonFly__)
-     // Arbitrary size, when the size is required we use sizeof
-     unsigned char pktinfo[100];
- #else
-@@ -753,7 +753,7 @@ struct DhcpConnType
- 	union
- 	{
- 		int broadCastVal;
--#ifdef __FreeBSD__
-+#if defined(__FreeBSD__) | defined(__DragonFly__)
-         int pktinfoVal;
- #else
- 		bool pktinfoVal;
+-  #ifdef __FreeBSD__
++  #if defined(__FreeBSD__) || defined(__DragonFly__)
+   // Arbitrary size, when the size is required we use sizeof
+   unsigned char pktinfo[100];
+   #else
