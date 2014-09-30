373dd42e663cfedb667f1f9e710839749477e412
DPorts Builder <nobody@home.ok>
2014-09-29 18:28:27 -0700

    Tweak devel/llvm34 version 3.4.2

--- devel/llvm34/files/patch-utils_llvm-build_llvmbuild_main.py
+++ devel/llvm34/files/patch-utils_llvm-build_llvmbuild_main.py
@@ -1,18 +1,22 @@
 
-$FreeBSD: head/devel/llvm34/files/patch-utils_llvm-build_llvmbuild_main.py 340725 2014-01-22 17:40:44Z mat $
+$FreeBSD: head/devel/llvm34/files/patch-utils_llvm-build_llvmbuild_main.py 369259 2014-09-25 14:30:24Z brooks $
 
 --- utils/llvm-build/llvmbuild/main.py.orig
 +++ utils/llvm-build/llvmbuild/main.py
-@@ -633,7 +633,13 @@
+@@ -660,7 +660,17 @@
  
      # We handle a few special cases of target names here for historical
      # reasons, as these are the names configure currently comes up with.
 -    native_target_name = { 'x86' : 'X86',
 +    native_target_name = { 'amd64' : 'X86',
 +			   'arm' : 'ARM',
++			   'armeb' : 'ARM',
++			   'armv6' : 'ARM',
++			   'armv6hf' : 'ARM',
 +			   'i386' : 'X86',
 +			   'mips' : 'Mips',
 +			   'powerpc' : 'PowerPC',
++			   'powerpc64' : 'PowerPC',
 +			   'sparc64' : 'Sparc',
 +                           'x86' : 'X86',
                             'x86_64' : 'X86',
