d7db3bc88cbfaa54ac6ad0ddd44f4d7ecede2402
DPorts Builder <nobody@home.ok>
2014-09-30 21:35:39 -0700

    Update lang/clang-devel to version 3.6.r218451_1

--- lang/clang-devel/files/patch-tools_clang_include_clang_Analysis_Analyses_FormatString.h
+++ lang/clang-devel/files/patch-tools_clang_include_clang_Analysis_Analyses_FormatString.h
@@ -1,9 +1,9 @@
 
-$FreeBSD: head/lang/clang-devel/files/patch-tools_clang_include_clang_Analysis_Analyses_FormatString.h 340725 2014-01-22 17:40:44Z mat $
+$FreeBSD: head/lang/clang-devel/files/patch-tools_clang_include_clang_Analysis_Analyses_FormatString.h 369283 2014-09-25 19:10:19Z brooks $
 
 --- tools/clang/include/clang/Analysis/Analyses/FormatString.h.orig
 +++ tools/clang/include/clang/Analysis/Analyses/FormatString.h
-@@ -115,9 +115,10 @@
+@@ -122,9 +122,10 @@
    enum Kind {
      InvalidSpecifier = 0,
        // C99 conversion specifiers.
@@ -15,12 +15,3 @@ $FreeBSD: head/lang/clang-devel/files/patch-tools_clang_include_clang_Analysis_A
      iArg,
      IntArgBeg = dArg, IntArgEnd = iArg,
  
-@@ -147,7 +148,7 @@
-     SArg,
- 
-     // ** Printf-specific **
--
-+    
-     // Objective-C specific specifiers.
-     ObjCObjArg,  // '@'
-     ObjCBeg = ObjCObjArg, ObjCEnd = ObjCObjArg,
