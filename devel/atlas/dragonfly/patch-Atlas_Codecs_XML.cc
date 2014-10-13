6eb45e519e560a0dd6969dda4b60e693dbee36ee
DPorts Builder <nobody@home.ok>
2014-10-12 23:02:54 -0700

    Import devel/atlas version 0.5.2_2

---
+++ devel/atlas/dragonfly/patch-Atlas_Codecs_XML.cc
@@ -0,0 +1,10 @@
+--- Atlas/Codecs/XML.cc.orig	2014-10-12 21:24:33 UTC
++++ Atlas/Codecs/XML.cc
+@@ -3,6 +3,7 @@
+ // Copyright (C) 2000-2001 Michael Day, Stefanus Du Toit
+ 
+ #include <Atlas/Codecs/XML.h>
++#include <stdlib.h>
+ 
+ namespace Atlas { namespace Codecs {
+     
