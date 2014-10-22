fb388b9606fde63146e2a8d17c9299f18f76bee2
DPorts Builder <nobody@home.ok>
2014-10-21 20:11:24 -0700

    Import sysutils/curly version 3.4

---
+++ sysutils/curly/files/patch-machine.h
@@ -0,0 +1,13 @@
+$FreeBSD: head/sysutils/curly/files/patch-machine.h 340725 2014-01-22 17:40:44Z mat $
+
+--- bin/curly/machine.h.orig	Wed Nov 13 08:08:13 2002
++++ bin/curly/machine.h	Wed Nov 13 08:08:48 2002
+@@ -19,7 +19,7 @@
+ #endif
+ 
+ #if !defined(NEED_MALLOC_EXTERN)
+-#define NEED_MALLOC_EXTERN	(!USE_MALLOC_H && !defined(NEXT2) && !defined(IRIX) && !defined(BSDI))
++#define NEED_MALLOC_EXTERN	(!USE_MALLOC_H && !defined(NEXT2) && !defined(IRIX) && !defined(BSDI) && !defined(FREEBSD))
+ #endif
+ 
+ #if !defined(USE_STDLIB)
