6237d6dc2d14ac890b6f5df9c7cedb5ce23bfc45
DPorts Builder <nobody@home.ok>
2014-10-31 17:48:26 -0700

    Tweak net/hping version 2.0.0r3,1

--- net/hping/dragonfly/patch-libpcap_stuff.c
+++ net/hping/dragonfly/patch-libpcap_stuff.c
@@ -1,12 +1,12 @@
 --- libpcap_stuff.c.orig	2002-12-13 14:07:07 UTC
 +++ libpcap_stuff.c
-@@ -16,7 +16,9 @@
+@@ -16,6 +16,9 @@
  #include <string.h>
  #include <stdlib.h>
  #include <sys/ioctl.h>
-+#if !defined(__DragonFly__)
- #include <pcap.h>
++#if defined(__DragonFly__)
++# define PCAP_DONT_INCLUDE_PCAP_BPF_H
 +#endif
+ #include <pcap.h>
  #include <net/bpf.h>
  
- #include "globals.h"
