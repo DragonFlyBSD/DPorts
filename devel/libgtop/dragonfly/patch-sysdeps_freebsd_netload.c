0e17bdd2786354fbe043239902489d37dbbeb827
DPorts Builder <nobody@home.ok>
2014-11-03 01:03:55 -0800

    Update devel/libgtop to version 2.28.4_3

--- devel/libgtop/dragonfly/patch-sysdeps_freebsd_netload.c
+++ devel/libgtop/dragonfly/patch-sysdeps_freebsd_netload.c
@@ -1,18 +1,18 @@
 TODO: Implement for DragonFly
 
---- sysdeps/freebsd/netload.c.orig	2011-03-14 23:08:03.000000000 +0100
-+++ sysdeps/freebsd/netload.c	2013-01-11 17:19:14.000000000 +0100
-@@ -91,6 +91,7 @@
+--- sysdeps/freebsd/netload.c.intermediate	2014-11-03 08:58:10 UTC
++++ sysdeps/freebsd/netload.c
+@@ -72,6 +72,7 @@ void
  glibtop_get_netload_p (glibtop *server, glibtop_netload *buf,
                         const char *interface)
  {
 +#ifndef __DragonFly__ /* TODO */
-         struct ifnet ifnet;
-         u_long ifnetaddr, ifnetfound;
-         struct sockaddr *sa = NULL;
-@@ -251,4 +252,5 @@
-                 }
-                 return;
-         }
+ 	static struct ifaddrs *ifap, *ifa;
+ 
+         glibtop_init_p (server, (1L << GLIBTOP_SYSDEPS_NETLOAD), 0);
+@@ -197,4 +198,5 @@ glibtop_get_netload_p (glibtop *server,
+ 	}
+ 
+ 	freeifaddrs(ifap);
 +#endif /* !__DragonFly__ */
  }
