13eb7d7054da6250bcea0107262a9f9a9afa829d
DPorts Builder <nobody@home.ok>
2014-11-12 00:50:55 -0800

    Update java/openjdk8 to version 8.25.17_1

--- java/openjdk8/dragonfly/patch-hotspot_src_os_bsd_vm_os__bsd.cpp
+++ java/openjdk8/dragonfly/patch-hotspot_src_os_bsd_vm_os__bsd.cpp
@@ -1,5 +1,5 @@
---- hotspot/src/os/bsd/vm/os_bsd.cpp.orig	2014-07-09 03:09:28.560950000 +0000
-+++ hotspot/src/os/bsd/vm/os_bsd.cpp	2014-07-09 03:18:02.719599000 +0000
+--- hotspot/src/os/bsd/vm/os_bsd.cpp.orig	2014-11-10 18:55:08.160395000 +0000
++++ hotspot/src/os/bsd/vm/os_bsd.cpp
 @@ -112,7 +112,7 @@
  # include <vm/vm_param.h>
  #endif
@@ -9,7 +9,7 @@
  # include <elf.h>
  #endif
  
-@@ -1197,7 +1197,7 @@ pid_t os::Bsd::gettid() {
+@@ -1236,7 +1236,7 @@ pid_t os::Bsd::gettid() {
    guarantee(retval != 0, "just checking");
    return retval;
  
@@ -18,7 +18,7 @@
  #if __FreeBSD_version > 900030
    return ::pthread_getthreadid_np();
  #else
-@@ -1205,9 +1205,9 @@ pid_t os::Bsd::gettid() {
+@@ -1244,9 +1244,9 @@ pid_t os::Bsd::gettid() {
    thr_self(&tid);
    return (pid_t)tid;
  #endif
@@ -30,7 +30,7 @@
    retval = (pid_t) syscall(SYS__lwp_self);
  #endif
  
-@@ -2811,7 +2811,7 @@ OSReturn os::set_native_priority(Thread*
+@@ -2843,7 +2843,7 @@ OSReturn os::set_native_priority(Thread*
  #ifdef __OpenBSD__
    // OpenBSD pthread_setprio starves low priority threads
    return OS_OK;
@@ -39,7 +39,7 @@
    int ret = pthread_setprio(thread->osthread()->pthread_id(), newpri);
    return (ret == 0) ? OS_OK : OS_ERR;
  #elif defined(__APPLE__) || defined(__NetBSD__)
-@@ -2840,7 +2840,7 @@ OSReturn os::get_native_priority(const T
+@@ -2872,7 +2872,7 @@ OSReturn os::get_native_priority(const T
    }
  
    errno = 0;
@@ -48,12 +48,3 @@
    *priority_ptr = pthread_getprio(thread->osthread()->pthread_id());
  #elif defined(__APPLE__) || defined(__NetBSD__)
    int policy;
-@@ -3303,7 +3303,7 @@ void os::Bsd::set_signal_handler(int sig
-     sigAct.sa_sigaction = signalHandler;
-     sigAct.sa_flags = SA_SIGINFO|SA_RESTART;
-   }
--#if __APPLE__
-+#if defined(__APPLE__)
-   // Needed for main thread as XNU (Mac OS X kernel) will only deliver SIGSEGV
-   // (which starts as SIGBUS) on main thread with faulting address inside "stack+guard pages"
-   // if the signal handler declares it will handle it on alternate stack.
