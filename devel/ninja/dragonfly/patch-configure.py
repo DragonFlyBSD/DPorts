b170a4ab6e1fb324407674d9cff45b10ad5f221c
DPorts Builder <nobody@home.ok>
2014-12-12 08:12:45 -0800

    Update devel/ninja to version 1.5.3,1

--- devel/ninja/dragonfly/patch-configure.py
+++ devel/ninja/dragonfly/patch-configure.py
@@ -1,6 +1,6 @@
---- platform_helper.py.orig	2013-09-12 02:15:28.000000000 +0000
-+++ platform_helper.py
-@@ -29,6 +29,8 @@ class Platform( object ):
+--- configure.py.intermediate	2014-12-12 13:05:20 UTC
++++ configure.py
+@@ -41,6 +41,8 @@ class Platform(object):
          self._platform = sys.platform
          if self._platform.startswith('linux'):
              self._platform = 'linux'
@@ -8,4 +8,4 @@
 +            self._platform = 'freebsd'
          elif self._platform.startswith('freebsd'):
              self._platform = 'freebsd'
-         elif self._platform.startswith('gnukfreebsd8'):
+         elif self._platform.startswith('gnukfreebsd'):
