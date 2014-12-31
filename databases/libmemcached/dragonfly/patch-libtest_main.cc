f237a5d4fb4c0dc53043730314b19f0fff49d23d
DPorts Builder <nobody@home.ok>
2014-12-31 12:39:16 -0800

    Update databases/libmemcached to version 1.0.18

--- databases/libmemcached/dragonfly/patch-libtest_main.cc
+++ databases/libmemcached/dragonfly/patch-libtest_main.cc
@@ -1,9 +1,9 @@
---- libtest/main.cc.orig	2013-10-28 20:44:56.653331000 +0100
+--- libtest/main.cc.orig	2014-02-09 11:52:42 UTC
 +++ libtest/main.cc
-@@ -256,7 +256,10 @@
+@@ -288,7 +288,10 @@ int main(int argc, char *argv[])
+     do
+     {
        exit_code= EXIT_SUCCESS;
-       std::auto_ptr<Framework> frame(new Framework);
- 
 -      fatal_assert(sigignore(SIGPIPE) == 0);
 +      struct sigaction ignore_action;
 +      ignore_action.sa_handler = SIG_IGN;
