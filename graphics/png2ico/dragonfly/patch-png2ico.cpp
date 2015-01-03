3f8739b964510a2c624a5f50e71a1a7c952f3d6f
DPorts Builder <nobody@home.ok>
2015-01-02 17:44:17 -0800

    Update graphics/png2ico to version 2002.12.08_5

---
+++ graphics/png2ico/dragonfly/patch-png2ico.cpp
@@ -0,0 +1,10 @@
+--- png2ico.cpp.intermediate	2015-01-03 01:37:35 UTC
++++ png2ico.cpp
+@@ -33,6 +33,7 @@ Notes about transparent and inverted pix
+ 
+ 
+ #include <cstdio>
++#include <cstring>
+ #include <cstdlib>
+ #include <vector>
+ #include <climits>
