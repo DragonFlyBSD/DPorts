154cb14432db24b1fda256f08624ccd4d046ebd9
DPorts Builder <nobody@home.ok>
2015-01-02 17:17:52 -0800

    Update x11-toolkits/nucleo to version 0.7.6_12

---
+++ x11-toolkits/nucleo/dragonfly/patch-nucleo_image_encoding_PNGenc.cxx
@@ -0,0 +1,10 @@
+--- nucleo/image/encoding/PNGenc.cxx.intermediate	2015-01-03 01:11:16 UTC
++++ nucleo/image/encoding/PNGenc.cxx
+@@ -9,6 +9,7 @@
+  *
+  */
+ 
++#include <cstring>
+ #include <nucleo/config.H>
+ 
+ #include <nucleo/image/Image.H>
