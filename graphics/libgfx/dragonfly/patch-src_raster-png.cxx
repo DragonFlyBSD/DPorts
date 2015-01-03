3b90795a6816e9b87d4aa6cee929b01b3c15e016
DPorts Builder <nobody@home.ok>
2015-01-02 16:58:10 -0800

    Update graphics/libgfx to version 1.1.0_9

---
+++ graphics/libgfx/dragonfly/patch-src_raster-png.cxx
@@ -0,0 +1,10 @@
+--- src/raster-png.cxx.intermediate	2015-01-03 00:52:44 UTC
++++ src/raster-png.cxx
+@@ -9,6 +9,7 @@
+ 
+  ************************************************************************/
+ 
++#include <cstring>
+ #include <vector>
+ #include <gfx/gfx.h>
+ #include <gfx/raster.h>
