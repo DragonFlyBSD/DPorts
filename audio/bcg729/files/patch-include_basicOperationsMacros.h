9bbd7d46c8f0adb6a5cd49e8628dd6f8d9310c0b
DPorts Builder <nobody@home.ok>
2015-01-18 14:36:39 -0800

    Import audio/bcg729 version 1.0.0

---
+++ audio/bcg729/files/patch-include_basicOperationsMacros.h
@@ -0,0 +1,14 @@
+
+$FreeBSD: head/audio/bcg729/files/patch-include_basicOperationsMacros.h 376897 2015-01-13 00:33:06Z sobomax $
+
+--- include/basicOperationsMacros.h
++++ include/basicOperationsMacros.h
+@@ -19,7 +19,7 @@
+  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+  */
+ #ifndef BASICOPERATIONSMACROS_H
+-#define BASICOPERATIONSMACROS__H
++#define BASICOPERATIONSMACROS_H
+ 
+ #include "fixedPointMacros.h"
+ 
