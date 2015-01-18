d31237842f1c8c6870d4df1c7035f4a0599f785d
DPorts Builder <nobody@home.ok>
2015-01-18 09:43:34 -0800

    Import audio/py-pyaudio version 0.2.8

---
+++ audio/py-pyaudio/files/patch-setup.py
@@ -0,0 +1,15 @@
+--- ./setup.py.orig	2012-11-07 00:17:06.624869638 +0100
++++ ./setup.py	2012-11-07 00:37:17.129040078 +0100
+@@ -74,6 +74,12 @@ else:
+     external_libraries = ['portaudio']
+     extra_link_args = []
+ 
++if sys.platform.startswith('dragonfly') or \
++   sys.platform.startswith('freebsd'):
++    include_dirs = ['%%LOCALBASE%%/include/portaudio2']
++    external_libraries = []
++    extra_link_args = ['%%LOCALBASE%%/lib/portaudio2/libportaudio.so']
++
+ if sys.platform == 'darwin':
+     defines += [('MACOSX', '1')]
+ 
