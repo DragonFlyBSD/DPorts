4a60a6f9d5e7e6fad4bf63c9f5344f43d2ca6f52
DPorts Builder <nobody@home.ok>
2015-02-02 09:22:58 -0800

    Update lang/parrot to version 7.0.1

--- lang/parrot/dragonfly/patch-config_auto_warnings.pm
+++ lang/parrot/dragonfly/patch-config_auto_warnings.pm
@@ -1,22 +1,22 @@
---- config/auto/warnings.pm.orig	2012-12-14 02:29:46.000000000 +0000
+--- config/auto/warnings.pm.orig	2015-01-23 10:58:18 UTC
 +++ config/auto/warnings.pm
-@@ -119,7 +119,6 @@ sub _init {
-     my @gcc_or_gpp_basic = qw(
+@@ -120,7 +120,6 @@ sub _init {
+         -fpermissive
          -falign-functions=16
          -funit-at-a-time
 -        -fexcess-precision=standard
-         -fpermissive
          -maccumulate-outgoing-args
          -Wall
-@@ -138,7 +137,6 @@ sub _init {
+         -Wextra
+@@ -135,7 +134,6 @@ sub _init {
          -Winit-self
          -Winline
          -Winvalid-pch
 -        -Wjump-misses-init
          -Wlogical-op
          -Werror=missing-braces
-         -Wmissing-declarations
-@@ -158,7 +156,6 @@ sub _init {
+         -Werror=missing-declarations
+@@ -155,7 +153,6 @@ sub _init {
          -Wno-unused
          -Wvariadic-macros
          -Wwrite-strings
