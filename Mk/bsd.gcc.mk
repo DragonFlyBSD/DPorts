cfbe150c4a1b04ccde47f5c365e7cc35662b964b
DPorts Builder <nobody@home.ok>
2015-02-11 15:51:39 -0800

    Sync Mk with ports

--- Mk/bsd.gcc.mk
+++ Mk/bsd.gcc.mk
@@ -40,21 +40,15 @@ GCCVERSIONS=	040200 040600 040700 040800 040900 050000
 # The first field if the OSVERSION in which it appeared in the base.
 # The second field is the OSVERSION in which it disappeared from the base.
 # The third field is the version as USE_GCC would use.
-GCCVERSION_040200=	     0       0 4.2
-GCCVERSION_040600=	     0       0 4.6
-GCCVERSION_040700=	300400 9999999 4.7
-GCCVERSION_040800=	     0       0 4.8
-GCCVERSION_040900=	     0       0 4.9
-GCCVERSION_050000=	     0       0 5
-
-DFLY_DEFAULT_VERSION=	47
+GCCVERSION_040200=	     0       0 4.2 42
+GCCVERSION_040600=	     0       0 4.6 43
+GCCVERSION_040700=	300400  400102 4.7 47
+GCCVERSION_040800=	     0       0 4.8 48
+GCCVERSION_040900=	     0       0 4.9 49
+GCCVERSION_050000=	400103 9999999 5   50
 
 # No configurable parts below this. ####################################
 #
-
-.if defined(USE_GCC) && ${USE_GCC} == yes
-USE_GCC=	${GCC_DEFAULT}
-.endif
 
 # Extract the fields from GCCVERSION_...
 .for v in ${GCCVERSIONS}
@@ -65,18 +59,32 @@ _GCCVERSION_${v}_L=	${j}
 _GCCVERSION_${v}_R=	${j}
 .  elif !defined(_GCCVERSION_${v}_V)
 _GCCVERSION_${v}_V=	${j}
+.  elif !defined(_GCCVERSION_${v}_VV)
+_GCCVERSION_${v}_VV=	${j}
 .  endif
 . endfor
 .endfor
 
+.if ${DFLYVERSION} >= ${_GCCVERSION_050000_L}
+LASTBASE=		5
+DFLY_DEFAULT_VERSION=	50
+.else
+LASTBASE=		4.7
+DFLY_DEFAULT_VERSION=	47
+.endif
+
+.if ${USE_GCC:Myes}
+USE_GCC=	${LASTBASE}
+.endif
+
 .if defined(USE_GCC) && !defined(FORCE_BASE_CC_FOR_TESTING)
 
-. if empty(USE_GCC) || ${USE_GCC} == any || ${USE_GCC:tl} == "yes"
+. if ${USE_GCC} == any
 
 # Enable the clang-is-cc workaround.  Default to the last GCC imported
 # into base.
-_USE_GCC:=	${GCC_DEFAULT}
-_GCC_ORLATER:=	false
+_USE_GCC:=	${LASTBASE}
+_GCC_ORLATER:=	true
 
 . else # ${USE_GCC} == any
 
@@ -90,11 +98,8 @@ _GCC_ORLATER:=	true
 
 # Initialize _GCC_FOUND${v}.  In parallel, check if USE_GCC points to a
 # valid version to begin with.
-# Set USE_GCC=3.4(4.2, 4.4, 4.6) all to base compiler (4.7)
 .for v in ${GCCVERSIONS}
-. if ${DFLYVERSION} >= ${_GCCVERSION_${v}_L} \
-  && ${DFLYVERSION} <  ${_GCCVERSION_${v}_R} \
-  && ${DFLY_DEFAULT_VERSION} <= ${_GCCVERSION_${v}_V:S/.//}
+. if ${DFLY_DEFAULT_VERSION} >= ${_GCCVERSION_${v}_VV}
 _GCC_FOUND${v}=	base
 . elif exists(${LOCALBASE}/bin/gcc${_GCCVERSION_${v}_V:S/.//})
 _GCC_FOUND${v}=	port
@@ -114,6 +119,11 @@ IGNORE=	Unknown version of GCC specified (USE_GCC=${USE_GCC})
 # get the first available version.
 #
 .if defined(_GCC_ORLATER)
+. if ${_USE_GCC:M5} && ${DFLY_DEFAULT_VERSION:M50}
+#  _USE_GCC is already correct
+. elif ${_USE_GCC:N5} && (${_USE_GCC:S/.//} <= ${DFLY_DEFAULT_VERSION})
+_USE_GCC= ${LASTBASE}
+. else # HEAD ORIG - run on 4.0 and below
 . for v in ${GCCVERSIONS}
 .  if ${_USE_GCC} == ${_GCCVERSION_${v}_V}
 _GCC_MIN1:=	true
@@ -136,6 +146,12 @@ _USE_GCC:=	${_GCC_FOUND}
 . elif ${_USE_GCC} < ${GCC_DEFAULT}
 _USE_GCC:=	${GCC_DEFAULT}
 . endif
+. endif   # TAIL ORIG
+.else  # defined(_GCC_ORLATER)
+. if ${_USE_GCC:M4.[67]}
+_USE_GCC=		4.7
+DFLY_DEFAULT_VERSION=	47
+. endif
 .endif # defined(_GCC_ORLATER)
 
 .endif # defined(USE_GCC)
@@ -147,7 +163,8 @@ _USE_GCC:=	${GCC_DEFAULT}
 # dependencies, CC, CXX, CPP, and flags.
 .for v in ${GCCVERSIONS}
 . if ${_USE_GCC} == ${_GCCVERSION_${v}_V}
-.  if ${DFLYVERSION} < ${_GCCVERSION_${v}_L} || ${DFLYVERSION} > ${_GCCVERSION_${v}_R}
+.  if (${_GCCVERSION_${v}_L} == 0 && ${_GCCVERSION_${v}_R} == 0) || \
+      ${DFLYVERSION} < ${_GCCVERSION_${v}_L}
 V:=			${_GCCVERSION_${v}_V:S/.//}
 _GCC_PORT_DEPENDS:=	gcc${V}
 .   if ${_USE_GCC} == ${LANG_GCC_IS}
@@ -176,11 +193,9 @@ MAKE_ENV+=		CCVER=gcc${DFLY_DEFAULT_VERSION}
 .if defined(_GCC_PORT_DEPENDS)
 BUILD_DEPENDS+=	${_GCC_PORT_DEPENDS}:${PORTSDIR}/lang/${_GCC_PORT}
 RUN_DEPENDS+=	${_GCC_PORT_DEPENDS}:${PORTSDIR}/lang/${_GCC_PORT}
-.  if ${_USE_GCC:S/.//} > ${GCC_DEFAULT}
 # Later GCC ports already depend on binutils; make sure whatever we
 # build leverages this as well.
 USE_BINUTILS=	yes
-.  endif
 .endif
 .endif # defined(_USE_GCC) && !defined(FORCE_BASE_CC_FOR_TESTING)
 
