5ff639c4d44fee79dfefe7de1289f40a64c42cde
DPorts Builder <nobody@home.ok>
2015-02-16 09:57:17 -0800

    Update security/krb5 to version 1.13.1

--- security/krb5/files/patch-lib__krb5__ccache__cc_file.c
+++ security/krb5/files/patch-lib__krb5__ccache__cc_file.c
@@ -1,6 +1,6 @@
---- lib/krb5/ccache/cc_file.c.orig	2014-10-15 23:55:10 UTC
-+++ lib/krb5/ccache/cc_file.c
-@@ -136,6 +136,7 @@ get_size(krb5_context context, krb5_ccac
+--- lib/krb5/ccache/cc_file.c.orig	2014-10-15 16:55:10.000000000 -0700
++++ lib/krb5/ccache/cc_file.c	2015-02-12 13:00:31.967166267 -0800
+@@ -136,6 +136,7 @@
      fcc_data *data = id->data;
      struct stat sb;
  
