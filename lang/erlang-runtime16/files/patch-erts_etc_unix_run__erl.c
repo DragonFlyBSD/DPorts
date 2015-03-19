0b0007ffffdd280755f13ef52474d616dd720ace
DPorts Builder <nobody@home.ok>
2015-03-19 13:40:22 -0700

    Import lang/erlang-runtime16 version 16.b.03.1_4

---
+++ lang/erlang-runtime16/files/patch-erts_etc_unix_run__erl.c
@@ -0,0 +1,15 @@
+
+$FreeBSD: head/lang/erlang-runtime16/files/patch-erts_etc_unix_run__erl.c 340725 2014-01-22 17:40:44Z mat $
+
+--- erts/etc/unix/run_erl.c.orig
++++ erts/etc/unix/run_erl.c
+@@ -69,9 +69,6 @@
+ #ifdef HAVE_UTMP_H
+ #  include <utmp.h>
+ #endif
+-#ifdef HAVE_UTIL_H
+-#  include <util.h>
+-#endif
+ #ifdef HAVE_SYS_IOCTL_H
+ #  include <sys/ioctl.h>
+ #endif
