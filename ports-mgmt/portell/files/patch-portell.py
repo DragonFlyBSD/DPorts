13d8dde5339c69e799fe66638edd24cd9935699a
DPorts Builder <nobody@home.ok>
2015-04-09 11:46:06 -0700

    Tweak ports-mgmt/portell version 0.2_1

--- ports-mgmt/portell/files/patch-portell.py
+++ ports-mgmt/portell/files/patch-portell.py
@@ -1,8 +1,17 @@
 
-$FreeBSD: head/ports-mgmt/portell/files/patch-portell.py 340725 2014-01-22 17:40:44Z mat $
+$FreeBSD: head/ports-mgmt/portell/files/patch-portell.py 383584 2015-04-08 17:19:21Z mich $
 
 --- portell.py.orig
 +++ portell.py
+@@ -6,7 +6,7 @@
+ # Version: 0.2
+ # Usage: portell.py <portname>
+ 
+-import sys, os, shelve
++import sys, os, bsddb
+ 
+ try:
+     PORTELL_DB = os.environ['PORTELL_PATH']
 @@ -14,12 +14,16 @@
      PORTELL_DB = "/var/db/portell.db"
  
@@ -24,3 +33,25 @@ $FreeBSD: head/ports-mgmt/portell/files/patch-portell.py 340725 2014-01-22 17:40
  
  def update_db(msg):
      print msg
+@@ -30,7 +34,7 @@
+         os.unlink(PORTELL_DB + ".db") 
+         
+     try:
+-        d = shelve.open(PORTELL_DB)
++	d = bsddb.btopen(PORTELL_DB, 'c')
+         fix_dbdb()
+         os.chmod(PORTELL_DB, 0666) 
+     except:
+@@ -63,10 +67,10 @@
+     else:
+         portname = sys.argv[1]
+ 
+-    d = shelve.open(PORTELL_DB)
++    d = bsddb.btopen(PORTELL_DB, 'r')
+     fix_dbdb()
+ 
+-    if d.has_key(portname): 
++    if d.has_key(portname):
+         if os.uname()[0].lower() == "freebsd":
+             descr_path = d[portname] + "/pkg-descr"
+         else:
