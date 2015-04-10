142296d1416859dbcae9a01785176819d55a90a5
DPorts Builder <nobody@home.ok>
2015-04-09 20:57:26 -0700

    Update astro/wmsun to version 1.12

--- astro/wmsun/files/patch-wmSun.c
+++ astro/wmsun/files/patch-wmSun.c
@@ -1,12 +1,12 @@
 
-$FreeBSD: head/astro/wmsun/files/patch-wmSun.c 340725 2014-01-22 17:40:44Z mat $
+$FreeBSD: head/astro/wmsun/files/patch-wmSun.c 383467 2015-04-06 18:50:50Z bapt $
 
 --- wmSun.c.orig	Thu Aug 15 18:04:04 2002
 +++ wmSun.c	Thu Aug 15 18:06:24 2002
-@@ -129,7 +129,8 @@
-     int 		Year, Month, DayOfWeek, OldLocalDayOfMonth;
-     int			LocalDayOfMonth,	DayOfMonth;
+@@ -76,7 +76,8 @@
+     int			LocalDayOfMonth, DayOfMonth;
      int			Hours, Mins, Secs, OldSecs, digit, xoff, xsize;
+     int			OldMins;
 -    long		CurrentLocalTime, CurrentGMTTime, date;
 +    time_t		CurrentLocalTime, CurrentGMTTime;
 +    long		date;
