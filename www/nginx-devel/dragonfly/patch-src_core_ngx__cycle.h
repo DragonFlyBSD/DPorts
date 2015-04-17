c3733698e0efab341e6774a80a54d7f483d9dbe5
DPorts Builder <nobody@home.ok>
2015-04-17 09:26:14 -0700

    Update www/nginx-devel to version 1.7.12

--- www/nginx-devel/dragonfly/patch-src_core_ngx__cycle.h
+++ www/nginx-devel/dragonfly/patch-src_core_ngx__cycle.h
@@ -1,4 +1,4 @@
---- src/core/ngx_cycle.h.orig	2013-07-30 13:27:55.000000000 +0000
+--- src/core/ngx_cycle.h.orig	2015-04-07 15:35:35 UTC
 +++ src/core/ngx_cycle.h
 @@ -103,6 +103,8 @@ typedef struct {
       ngx_array_t              env;
@@ -6,6 +6,6 @@
  
 +     unsigned                 so_reuseport:1;
 +
- #if (NGX_THREADS)
+ #if (NGX_OLD_THREADS)
       ngx_int_t                worker_threads;
       size_t                   thread_stack_size;
