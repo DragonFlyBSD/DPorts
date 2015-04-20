cd5e01b04ff506c68a8cba60a8795611d8c2b23f
DPorts Builder <nobody@home.ok>
2015-04-20 03:53:34 -0700

    Update sysutils/apcupsd to version 3.14.13

--- sysutils/apcupsd/dragonfly/patch-src_drivers_usb_bsd_bsd-usb.c
+++ sysutils/apcupsd/dragonfly/patch-src_drivers_usb_bsd_bsd-usb.c
@@ -1,16 +1,13 @@
---- src/drivers/usb/bsd/bsd-usb.c.orig	2011-09-02 15:44:13.000000000 +0000
+--- src/drivers/usb/bsd/bsd-usb.c.orig	2015-01-02 21:34:23 UTC
 +++ src/drivers/usb/bsd/bsd-usb.c
-@@ -27,8 +27,13 @@
+@@ -27,8 +27,8 @@
  #include "apc.h"
  #include "hidutils.h"
- #include "../usb_common.h"
-+#ifdef __DragonFly__
+ #include "bsd-usb.h"
+-#include <dev/usb/usb.h>
+-#include <dev/usb/usbhid.h>
 +#include <bus/usb/usb.h>
 +#include <bus/usb/usbhid.h>
-+#else
- #include <dev/usb/usb.h>
- #include <dev/usb/usbhid.h>
-+#endif
  
  /* Compatibility cruft for FreeBSD <= 4.7 */
  #ifndef USB_MAX_DEVNAMES
