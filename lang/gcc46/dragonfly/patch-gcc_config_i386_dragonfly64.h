9dd4cea4a4fe7419e739ffcf25ef2a8ae104d58b
DPorts Builder <nobody@home.ok>
2015-04-28 09:57:15 -0700

    Prune lang/gcc46

--- lang/gcc46/dragonfly/patch-gcc_config_i386_dragonfly64.h
+++
@@ -1,8 +0,0 @@
---- gcc/config/i386/dragonfly64.h.orig	2013-09-12 14:44:03.000000000 +0000
-+++ gcc/config/i386/dragonfly64.h
-@@ -0,0 +1,5 @@
-+/* Definitions for AMD x86_64 running DragonFly BSD with ELF Format */
-+
-+#undef  TARGET_VERSION
-+#define TARGET_VERSION fprintf (stderr, " (x86-64 DragonFly/ELF)");
-+
