19136526618d8e29a3e9e7fa6dd96b9ad635e38c
DPorts Builder <nobody@home.ok>
2015-05-13 06:27:17 -0700

    Update devel/R-cran-Rcpp to version 0.11.6

--- devel/R-cran-Rcpp/dragonfly/patch-src_api.cpp
+++ devel/R-cran-Rcpp/dragonfly/patch-src_api.cpp
@@ -1,24 +1,24 @@
---- src/api.cpp.orig	2014-06-06 23:38:58.000000000 +0000
+--- src/api.cpp.orig	2015-05-01 14:41:53 UTC
 +++ src/api.cpp
-@@ -34,7 +34,9 @@ using namespace Rcpp ;
+@@ -34,7 +34,9 @@ using namespace Rcpp;
  #endif
  
  #if defined(__GNUC__)
--    #if defined(WIN32) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__CYGWIN__) || defined(__sun)
+-    #if defined(WIN32) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__CYGWIN__) || defined(__sun) || defined(_AIX)
 +    #if defined(WIN32) || defined(__FreeBSD__) || defined(__NetBSD__) \
 +     || defined(__OpenBSD__) || defined(__CYGWIN__) || defined(__sun) \
-+     || defined(__DragonFly__)
++     || defined(_AIX) || defined(__DragonFly__)
+         // do nothing
      #else
          #include <execinfo.h>
- 
-@@ -239,7 +241,9 @@ SEXP rcpp_can_use_cxx0x(){
+@@ -257,7 +259,9 @@ SEXP rcpp_can_use_cxx11() {
  // [[Rcpp::register]]
- SEXP stack_trace( const char* file, int line ){
+ SEXP stack_trace(const char* file, int line) {
      #if defined(__GNUC__)
--        #if defined(WIN32) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__CYGWIN__) || defined(__sun)
+-        #if defined(WIN32) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__CYGWIN__) || defined(__sun) || defined(_AIX)
 +        #if defined(WIN32) || defined(__FreeBSD__) || defined(__NetBSD__) \
 +         || defined(__OpenBSD__) || defined(__CYGWIN__) || defined(__sun) \
-+         || defined(__DragonFly__)
++         || defined(_AIX) || defined(__DragonFly__)
              // Simpler version for Windows and *BSD
-             List trace = List::create(
-                 _[ "file"  ] = file,
+             List trace = List::create(_["file"] = file,
+                                       _[ "line"  ] = line,
