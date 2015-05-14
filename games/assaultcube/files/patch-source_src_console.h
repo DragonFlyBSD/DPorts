794052f314b2c3f944b56add7502945adabe7a29
DPorts Builder <nobody@home.ok>
2015-05-14 09:19:50 -0700

    Import games/assaultcube version 1.2.0.2_4

---
+++ games/assaultcube/files/patch-source_src_console.h
@@ -0,0 +1,17 @@
+--- source/src/console.h.orig	2013-10-09 08:27:31 UTC
++++ source/src/console.h
+@@ -127,7 +127,7 @@
+     }
+ };
+ 
+-/** WIP ALERT */
++/** WIP ALERT *//*
+ struct textinputbuffer_wip
+ {
+     string buf;
+@@ -257,4 +257,4 @@
+         return false;
+     }
+ };
+-
++*/
