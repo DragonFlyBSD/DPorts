11d5610433a4aa21f0d84592637ca39eab8a4fe1
DPorts Builder <nobody@home.ok>
2015-05-25 02:12:50 -0700

    Tweak www/nghttp2 version 0.7.12

--- www/nghttp2/dragonfly/patch-src_shrpx__connection__handler.cc
+++ www/nghttp2/dragonfly/patch-src_shrpx__connection__handler.cc
@@ -1,11 +1,19 @@
 --- src/shrpx_connection_handler.cc.orig	2015-04-19 09:26:53 UTC
 +++ src/shrpx_connection_handler.cc
-@@ -372,7 +372,7 @@ int ConnectionHandler::start_ocsp_update
+@@ -27,6 +27,7 @@
+ #include <unistd.h>
+ #include <sys/types.h>
+ #include <sys/wait.h>
++#include <sys/param.h>
+ 
+ #include <cerrno>
+ #include <thread>
+@@ -372,7 +373,7 @@ int ConnectionHandler::start_ocsp_update
        const_cast<char *>(cert_file), nullptr};
    char *const envp[] = {nullptr};
  
 -#ifdef O_CLOEXEC
-+#if 0
++#if __DragonFly_version >= 400106
    if (pipe2(pfd, O_CLOEXEC) == -1) {
      return -1;
    }
