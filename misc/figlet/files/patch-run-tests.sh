38daef4550c5a9c85424022b2940549c0912caa8
DPorts Builder <nobody@home.ok>
2015-06-03 18:30:28 -0700

    Tweak misc/figlet version 2.2.5

--- misc/figlet/files/patch-run-tests.sh
+++ misc/figlet/files/patch-run-tests.sh
@@ -1,4 +1,3 @@
-$FreeBSD: head/misc/figlet/files/patch-run-tests.sh 308256 2012-12-04 17:34:12Z obrien $
 --- run-tests.sh.orig	2012-06-01 05:51:10.000000000 -0700
 +++ run-tests.sh	2012-12-04 09:28:49.000000000 -0800
 @@ -7 +7 @@ TESTDIR=tests
