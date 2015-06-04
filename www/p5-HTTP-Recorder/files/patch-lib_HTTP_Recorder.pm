f16802fb3232ee6190484e05065eca73e31175aa
DPorts Builder <nobody@home.ok>
2015-06-03 19:20:04 -0700

    Tweak www/p5-HTTP-Recorder version 0.07_1

--- www/p5-HTTP-Recorder/files/patch-lib_HTTP_Recorder.pm
+++ www/p5-HTTP-Recorder/files/patch-lib_HTTP_Recorder.pm
@@ -1,5 +1,3 @@
-$FreeBSD: head/www/p5-HTTP-Recorder/files/patch-lib_HTTP_Recorder.pm 340725 2014-01-22 17:40:44Z mat $
-
 --- lib/HTTP/Recorder.pm.orig	2012-04-23 22:20:16.000000000 +0800
 +++ lib/HTTP/Recorder.pm	2012-04-25 01:11:56.207852080 +0800
 @@ -352,11 +352,12 @@
