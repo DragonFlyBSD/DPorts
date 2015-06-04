8153b1aa23c437224b1e8a254927af0abcd0104d
DPorts Builder <nobody@home.ok>
2015-06-04 14:33:56 -0700

    Tweak net/rdesktop version 1.8.3

--- net/rdesktop/files/patch-xwin.c
+++ net/rdesktop/files/patch-xwin.c
@@ -1,5 +1,3 @@
-$FreeBSD: head/net/rdesktop/files/patch-xwin.c 305566 2012-10-09 02:11:46Z obrien $
-
 --- xwin.c.orig	2011-04-13 04:13:04.000000000 -0700
 +++ xwin.c	2012-10-08 19:06:06.000000000 -0700
 @@ -2150,4 +2150,6 @@ ui_resize_window()
