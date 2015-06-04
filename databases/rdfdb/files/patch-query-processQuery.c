47bf093c8331d803677c3a37190524ecdd08ea25
DPorts Builder <nobody@home.ok>
2015-06-03 20:07:54 -0700

    Tweak databases/rdfdb version 0.46_4

--- databases/rdfdb/files/patch-query-processQuery.c
+++ databases/rdfdb/files/patch-query-processQuery.c
@@ -1,5 +1,3 @@
-$FreeBSD: head/databases/rdfdb/files/patch-query-processQuery.c 340725 2014-01-22 17:40:44Z mat $
-
 --- query/processQuery.c.orig	Mon Sep 18 17:09:01 2000
 +++ query/processQuery.c	Thu Dec 27 21:18:54 2001
 @@ -21,6 +21,7 @@
