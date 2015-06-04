e8ae510bc3e894e8ee7bef1f38f20c94837fd07d
DPorts Builder <nobody@home.ok>
2015-06-03 21:13:09 -0700

    Tweak editors/led version 2.0

--- editors/led/files/patch-src__file.c
+++ editors/led/files/patch-src__file.c
@@ -1,6 +1,3 @@
-
-$FreeBSD: head/editors/led/files/patch-src__file.c 363362 2014-07-29 17:12:47Z adamw $
-
 --- src/file.c	2002/07/15 11:40:21	1.1
 +++ src/file.c	2002/07/15 11:42:31
 @@ -192,6 +192,8 @@
