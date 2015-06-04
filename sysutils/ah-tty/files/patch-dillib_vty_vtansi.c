37a05e6915e94bb0203eda75caa3b6f473b1560e
DPorts Builder <nobody@home.ok>
2015-06-04 01:32:33 -0700

    Tweak sysutils/ah-tty version 0.3.12

--- sysutils/ah-tty/files/patch-dillib_vty_vtansi.c
+++ sysutils/ah-tty/files/patch-dillib_vty_vtansi.c
@@ -1,6 +1,3 @@
-
-$FreeBSD: head/sysutils/ah-tty/files/patch-dillib_vty_vtansi.c 340725 2014-01-22 17:40:44Z mat $
-
 --- dillib/vty/vtansi.c.orig	Mon Aug  9 15:44:41 2004
 +++ dillib/vty/vtansi.c	Mon Aug  9 15:44:56 2004
 @@ -688,6 +688,7 @@
--- sysutils/ah-tty/files/patch-src_psh.h
+++ sysutils/ah-tty/files/patch-src_psh.h
@@ -1,6 +1,3 @@
-
-$FreeBSD: head/sysutils/ah-tty/files/patch-src_psh.h 340725 2014-01-22 17:40:44Z mat $
-
 --- src/psh.h.orig
 +++ src/psh.h
 @@ -52,7 +52,6 @@
