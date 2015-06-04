8681e491058508e277f1ef64c1141f65f3ff0aa6
DPorts Builder <nobody@home.ok>
2015-06-03 18:34:02 -0700

    Tweak multimedia/mpeg_encode version 1.5b

--- multimedia/mpeg_encode/files/patch-opts.c
+++ multimedia/mpeg_encode/files/patch-opts.c
@@ -1,6 +1,3 @@
-
-$FreeBSD: head/multimedia/mpeg_encode/files/patch-opts.c 340725 2014-01-22 17:40:44Z mat $
-
 --- opts.c	2002/10/21 11:32:56	1.1
 +++ opts.c	2002/10/21 11:33:04
 @@ -37,9 +37,10 @@
