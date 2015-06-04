a967eabe18a70b6f5625561e6288d012cab7c867
DPorts Builder <nobody@home.ok>
2015-06-03 19:31:55 -0700

    Tweak converters/aish version 1.13

--- converters/aish/files/patch-ai_unix.c
+++ converters/aish/files/patch-ai_unix.c
@@ -1,5 +1,3 @@
-$FreeBSD: head/converters/aish/files/patch-ai_unix.c 340725 2014-01-22 17:40:44Z mat $
-
 --- ai_unix.c.orig	Fri Jun  7 12:59:05 2002
 +++ ai_unix.c	Fri Jun  7 13:00:49 2002
 @@ -15,7 +15,7 @@
