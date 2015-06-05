289114a0d279acb3923dca903bea6d2a14724716
DPorts Builder <nobody@home.ok>
2015-06-05 05:29:50 -0700

    Tweak audio/trommler version 3.8_4

--- audio/trommler/files/patch-gui.c
+++ audio/trommler/files/patch-gui.c
@@ -1,5 +1,3 @@
-$FreeBSD: head/audio/trommler/files/patch-gui.c 340725 2014-01-22 17:40:44Z mat $
-
 --- gui.c.orig	Thu Feb 20 15:43:08 2003
 +++ gui.c	Thu Feb 20 15:43:21 2003
 @@ -2261,7 +2261,6 @@
