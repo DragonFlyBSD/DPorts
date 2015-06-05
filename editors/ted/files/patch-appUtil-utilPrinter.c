a382e73e910f8b45dd4c50dca35b847722f8931d
DPorts Builder <nobody@home.ok>
2015-06-05 01:59:01 -0700

    Tweak editors/ted version 2.23_3

--- editors/ted/files/patch-appUtil-utilPrinter.c
+++ editors/ted/files/patch-appUtil-utilPrinter.c
@@ -1,5 +1,3 @@
- $FreeBSD: head/editors/ted/files/patch-appUtil-utilPrinter.c 312503 2013-02-18 12:36:09Z pclin $
- 
  --- appUtil/utilPrinter.c	Sun Aug 17 19:14:01 2003
  +++ appUtil/utilPrinter.c	Sun Aug 17 19:15:54 2003
  @@ -331,14 +331,14 @@
