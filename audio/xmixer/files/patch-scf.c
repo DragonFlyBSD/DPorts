7e40c9506f6a2458a09d77ed3a0dcedbb1fde8c2
DPorts Builder <nobody@home.ok>
2015-06-05 05:22:52 -0700

    Tweak audio/xmixer version 0.9.4_7

--- audio/xmixer/files/patch-scf.c
+++ audio/xmixer/files/patch-scf.c
@@ -1,6 +1,3 @@
-
-$FreeBSD: head/audio/xmixer/files/patch-scf.c 340725 2014-01-22 17:40:44Z mat $
-
 --- scf.c.orig	Mon Sep  6 11:02:44 2004
 +++ scf.c	Mon Sep  6 11:03:05 2004
 @@ -1009,7 +1009,7 @@
