f3d9770502578332d0f8185d88a13e5f351ab6f6
DPorts Builder <nobody@home.ok>
2015-06-04 17:47:14 -0700

    Tweak audio/vorbis-tools version 1.4.0_8,3

--- audio/vorbis-tools/files/patch-ogg123_vorbis_comments.c
+++ audio/vorbis-tools/files/patch-ogg123_vorbis_comments.c
@@ -1,4 +1,3 @@
-$FreeBSD: head/audio/vorbis-tools/files/patch-ogg123_vorbis_comments.c 340725 2014-01-22 17:40:44Z mat $
 --- ogg123/vorbis_comments.c.orig	2010-06-05 16:48:23.000000000 +0200
 +++ ogg123/vorbis_comments.c	2010-06-05 16:55:21.000000000 +0200
 @@ -72,7 +72,7 @@ char *lookup_comment_prettyprint (char *
--- audio/vorbis-tools/files/patch-oggenc_lyrics.c
+++ audio/vorbis-tools/files/patch-oggenc_lyrics.c
@@ -1,4 +1,3 @@
-$FreeBSD: head/audio/vorbis-tools/files/patch-oggenc_lyrics.c 340725 2014-01-22 17:40:44Z mat $
 --- oggenc/lyrics.c.orig	2010-06-05 17:00:07.000000000 +0200
 +++ oggenc/lyrics.c	2010-06-05 17:00:17.000000000 +0200
 @@ -16,7 +16,7 @@
