36170532a2befabcb730ab647fa3ce10f0ed1ad4
DPorts Builder <nobody@home.ok>
2015-06-05 01:55:04 -0700

    Tweak audio/glame version 2.0.1_27

--- audio/glame/files/patch-src__plugins__audio_io_oss.c
+++ audio/glame/files/patch-src__plugins__audio_io_oss.c
@@ -1,6 +1,3 @@
-
-$FreeBSD: head/audio/glame/files/patch-src__plugins__audio_io_oss.c 363362 2014-07-29 17:12:47Z adamw $
-
 --- src/plugins/audio_io_oss.c	2003/07/01 12:26:28	1.1
 +++ src/plugins/audio_io_oss.c	2003/07/01 12:26:41
 @@ -26,9 +26,9 @@
