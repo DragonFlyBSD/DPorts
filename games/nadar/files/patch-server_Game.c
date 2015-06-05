50c3e27e78e0d9eb7f10827d7391e21844de8f72
DPorts Builder <nobody@home.ok>
2015-06-05 07:45:48 -0700

    Tweak games/nadar version 2.0_2

--- games/nadar/files/patch-server_Game.c
+++ games/nadar/files/patch-server_Game.c
@@ -1,6 +1,3 @@
-
-$FreeBSD: head/games/nadar/files/patch-server_Game.c 340725 2014-01-22 17:40:44Z mat $
-
 --- server/Game.c.orig	Sun Aug 22 16:35:40 2004
 +++ server/Game.c	Sun Aug 22 16:35:50 2004
 @@ -226,7 +226,7 @@
