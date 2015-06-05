9ce8e9c8f6c944e2b9de1c32c739f5af88186f22
DPorts Builder <nobody@home.ok>
2015-06-04 17:36:17 -0700

    Tweak astro/gpsman version 6.4.4.2

--- astro/gpsman/files/patch-gpsman.tcl
+++ astro/gpsman/files/patch-gpsman.tcl
@@ -1,5 +1,3 @@
-$FreeBSD: head/astro/gpsman/files/patch-gpsman.tcl 340725 2014-01-22 17:40:44Z mat $
-
 diff -ruN gpsman.tcl gpsman.tcl
 --- gpsman.tcl	Tue May  9 02:37:24 2006
 +++ gpsman.tcl	Wed May 10 17:56:02 2006
