180ad6d4d5866876287a266fac6a89a8ab10c8cb
DPorts Builder <nobody@home.ok>
2015-06-06 08:19:14 -0700

    Update lang/mono-basic to version 4.0.1

--- lang/mono-basic/files/patch-vbnc_vbnc_setversion.sh
+++ lang/mono-basic/files/patch-vbnc_vbnc_setversion.sh
@@ -1,6 +1,3 @@
-
-$FreeBSD: head/lang/mono-basic/files/patch-vbnc_vbnc_setversion.sh 340725 2014-01-22 17:40:44Z mat $
-
 --- vbnc/vbnc/setversion.sh.orig
 +++ vbnc/vbnc/setversion.sh
 @@ -1,4 +1,4 @@
