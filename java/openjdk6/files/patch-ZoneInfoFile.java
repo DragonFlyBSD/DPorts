bfa24aa2f2ce6e871cebc6b8f3c767d80de6e495
DPorts Builder <nobody@home.ok>
2015-06-05 23:44:44 -0700

    Tweak java/openjdk6 version b34_2,1

--- java/openjdk6/files/patch-ZoneInfoFile.java
+++ java/openjdk6/files/patch-ZoneInfoFile.java
@@ -1,5 +1,3 @@
-$FreeBSD: head/java/openjdk6/files/patch-ZoneInfoFile.java 340725 2014-01-22 17:40:44Z mat $
-
 --- jdk/src/share/classes/sun/util/calendar/ZoneInfoFile.java.orig	2012-05-01 18:59:17.000000000 -0700
 +++ jdk/src/share/classes/sun/util/calendar/ZoneInfoFile.java	2012-05-01 19:02:16.000000000 -0700
 @@ -476,7 +476,8 @@
