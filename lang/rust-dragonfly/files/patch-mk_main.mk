d7e1762621f069a6ee8c41bbfd64ce03d26643b8
DPorts Builder <nobody@home.ok>
2015-06-15 09:46:46 -0700

    Update lang/rust-dragonfly to version 1.2.0.20150613

--- lang/rust-dragonfly/files/patch-mk_main.mk
+++ lang/rust-dragonfly/files/patch-mk_main.mk
@@ -1,18 +1,20 @@
---- mk/main.mk.old	2015-04-16 20:36:39.000000000 +0200
-+++ mk/main.mk	2015-05-01 12:38:46.600173000 +0200
-@@ -289,8 +289,8 @@
+--- mk/main.mk.old	2015-06-12 04:56:19.000000000 +0200
++++ mk/main.mk	2015-06-13 13:14:29.406997000 +0200
+@@ -287,9 +287,9 @@
  endif
  
  # Any rules that depend on LLVM should depend on LLVM_CONFIG
 -LLVM_CONFIG_$(1):=$$(CFG_LLVM_INST_DIR_$(1))/bin/llvm-config$$(X_$(1))
 -LLVM_MC_$(1):=$$(CFG_LLVM_INST_DIR_$(1))/bin/llvm-mc$$(X_$(1))
+-LLVM_AR_$(1):=$$(CFG_LLVM_INST_DIR_$(1))/bin/llvm-ar$$(X_$(1))
 +LLVM_CONFIG_$(1):=$$(CFG_LLVM_INST_DIR_$(1))/bin/llvm-config36$$(X_$(1))
 +LLVM_MC_$(1):=$$(CFG_LLVM_INST_DIR_$(1))/bin/llvm-mc36$$(X_$(1))
++LLVM_AR_$(1):=$$(CFG_LLVM_INST_DIR_$(1))/bin/llvm-ar36$$(X_$(1))
  LLVM_VERSION_$(1)=$$(shell "$$(LLVM_CONFIG_$(1))" --version)
  LLVM_BINDIR_$(1)=$$(shell "$$(LLVM_CONFIG_$(1))" --bindir)
  LLVM_INCDIR_$(1)=$$(shell "$$(LLVM_CONFIG_$(1))" --includedir)
-@@ -303,8 +303,8 @@
- LLVM_CXXFLAGS_$(1)=$$(subst -I, -iquote , $$(shell "$$(LLVM_CONFIG_$(1))" --cxxflags))
+@@ -306,8 +306,8 @@
+ endif
  LLVM_HOST_TRIPLE_$(1)=$$(shell "$$(LLVM_CONFIG_$(1))" --host-target)
  
 -LLVM_AS_$(1)=$$(CFG_LLVM_INST_DIR_$(1))/bin/llvm-as$$(X_$(1))
