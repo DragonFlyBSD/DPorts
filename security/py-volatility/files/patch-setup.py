55cd66fd83270409894f90f675bc260ce38c1eef
DPorts Builder <nobody@home.ok>
2015-07-02 08:16:25 -0700

    Import security/py-volatility version 2.4_3

---
+++ security/py-volatility/files/patch-setup.py
@@ -0,0 +1,14 @@
+--- setup.py.orig	2014-08-03 18:09:32 UTC
++++ setup.py
+@@ -44,7 +44,10 @@ def find_files(topdirs, py = False):
+     ret = []
+     for topdir in topdirs:
+         for r, _ds, fs in os.walk(topdir):
+-            ret.append((r, [ os.path.join(r, f) for f in fs if (f.endswith('.py') or not py)]))
++            fl = [ os.path.join(r, f) for f in fs if (f.endswith('.py') or not py)]
++            # Do not record directories (with no regular files inside)
++            if len(fl) > 0:
++                ret.append((r, fl))
+     return ret
+ 
+ opts = {}
