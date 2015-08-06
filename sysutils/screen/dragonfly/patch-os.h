c034bc11cf765a930fd1acd3e41de0c80936a2a0
DPorts Builder <nobody@home.ok>
2015-08-05 23:51:32 -0700

    Update sysutils/screen to version 4.3.1_1

--- sysutils/screen/dragonfly/patch-os.h
+++ sysutils/screen/dragonfly/patch-os.h
@@ -1,26 +1,35 @@
---- os.h.intermediate	2012-12-22 00:10:21.000000000 +0100
-+++ os.h	2012-12-22 00:12:45.000000000 +0100
-@@ -260,13 +260,18 @@
+--- os.h.orig	2015-08-05 16:00:47 UTC
++++ os.h
+@@ -250,11 +250,8 @@ extern int errno;
  #endif
  
  #if defined(UTMPOK) || defined(BUGGYGETLOGIN)
--# if defined(SVR4) && !defined(DGUX) && !defined(__hpux) && !defined(linux)
-+# if (defined(SVR4) && !defined(DGUX) && !defined(__hpux) && !defined(linux)) || \
-+      defined(__DragonFly__)
+-# if defined(SVR4) && !defined(DGUX) && !defined(__hpux)
++# if (defined(SVR4) && !defined(DGUX)) || defined(__DragonFly__)
  #  include <utmpx.h>
--#  ifdef UTMPX_FILE
+-#  ifdef UTMPX_FILE /* GNU extension */
 -#   define UTMPFILE	UTMPX_FILE
-+#  if defined(__DragonFly__)
-+#    define UTMPFILE	_PATH_UTMPX
- #  else
--#   define UTMPFILE	"/nonexistent"
 -#  endif
-+#    ifdef UTMPX_FILE
-+#      define UTMPFILE	UTMPX_FILE
-+#    else
-+#      define UTMPFILE	"/nonexistent"
-+#    endif
-+#  endif /* __DragonFly__ */
  #  define utmp		utmpx
  #  define getutent	getutxent
  #  define getutid	getutxid
+@@ -276,15 +273,11 @@ extern int errno;
+ # endif /* apollo */
+ 
+ # ifndef UTMPFILE
+-#  ifdef UTMP_FILE
+-#   define UTMPFILE	UTMP_FILE
+-#  else
+-#   ifdef _PATH_UTMP
+-#    define UTMPFILE	_PATH_UTMP
++#   ifdef UTMPX_FILE
++#     define UTMPFILE	UTMPX_FILE
+ #   else
+-#    define UTMPFILE	"/etc/utmp"
+-#   endif /* _PATH_UTMP */
+-#  endif
++#     define UTMPFILE	_PATH_UTMPX
++#   endif
+ # endif
+ 
+ #endif /* UTMPOK || BUGGYGETLOGIN */
