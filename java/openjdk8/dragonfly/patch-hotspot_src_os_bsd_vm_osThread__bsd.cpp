29811a82f145175df07ec55cc5a0e5c3c617a6d9
DPorts Builder <nobody@home.ok>
2015-08-06 01:45:43 -0700

    Update java/openjdk8 to version 8.51.16

--- java/openjdk8/dragonfly/patch-hotspot_src_os_bsd_vm_osThread__bsd.cpp
+++ java/openjdk8/dragonfly/patch-hotspot_src_os_bsd_vm_osThread__bsd.cpp
@@ -1,11 +1,12 @@
---- hotspot/src/os/bsd/vm/osThread_bsd.cpp.orig	2015-03-18 10:07:31 UTC
+--- hotspot/src/os/bsd/vm/osThread_bsd.cpp.orig	2015-08-05 07:39:27 UTC
 +++ hotspot/src/os/bsd/vm/osThread_bsd.cpp
-@@ -30,7 +30,7 @@
+@@ -30,7 +30,8 @@
  
  void OSThread::pd_initialize() {
    assert(this != NULL, "check");
--#if defined(__APPLE__) || defined(__FreeBSD__) || defined(__NetBSD__)
-+#if defined(__APPLE__) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__DragonFly__)
+-#if defined(__APPLE__) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__)
++#if defined(__APPLE__) || defined(__FreeBSD__) || defined(__NetBSD__) \
++ || defined(__OpenBSD__) || defined(__DragonFly__)
    _thread_id        = 0;
  #else
    _thread_id        = NULL;
