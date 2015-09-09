c19ba2881c46311815b2b7f7d9c53a1a375f0937
DPorts Builder <nobody@home.ok>
2015-09-09 13:04:27 -0700

    Update devel/libgtop to version 2.31.90

--- devel/libgtop/dragonfly/patch-sysdeps_freebsd_netload.c
+++ devel/libgtop/dragonfly/patch-sysdeps_freebsd_netload.c
@@ -1,16 +1,16 @@
---- sysdeps/freebsd/netload.c.orig	2014-11-21 12:23:34 UTC
+--- sysdeps/freebsd/netload.c.orig	2015-09-09 18:40:09 UTC
 +++ sysdeps/freebsd/netload.c
-@@ -67,6 +67,7 @@ void
+@@ -70,6 +70,7 @@ void
  glibtop_get_netload_p (glibtop *server, glibtop_netload *buf,
                         const char *interface)
  {
 +#ifndef __DragonFly__ /* TODO */
- 	struct ifaddrs *ifap, *ifa;
+         struct ifaddrs *ifap, *ifa;
  
-         glibtop_init_p (server, (1L << GLIBTOP_SYSDEPS_NETLOAD), 0);
-@@ -192,4 +193,5 @@ glibtop_get_netload_p (glibtop *server,
- 	}
- 
- 	freeifaddrs(ifap);
+         memset (buf, 0, sizeof (glibtop_netload));
+@@ -191,4 +192,5 @@ glibtop_get_netload_p (glibtop *server,
+                 } // switch() end
+         }
+         freeifaddrs(ifap);
 +#endif /* !__DragonFly__ */
  }
--- devel/libgtop/dragonfly/patch-sysdeps_freebsd_proctime.c
+++ devel/libgtop/dragonfly/patch-sysdeps_freebsd_proctime.c
@@ -1,28 +1,26 @@
-TODO: Implement for DragonFly
-
---- sysdeps/freebsd/proctime.c.orig	2011-03-14 23:08:03.000000000 +0100
-+++ sysdeps/freebsd/proctime.c	2013-01-11 17:23:40.000000000 +0100
-@@ -80,7 +80,9 @@
+--- sysdeps/freebsd/proctime.c.orig	2015-08-17 18:59:37 UTC
++++ sysdeps/freebsd/proctime.c
+@@ -92,7 +92,9 @@ glibtop_get_proc_time_p (glibtop *server
+ 	}
  
- 	glibtop_suid_leave (server);
- 
+ 	buf->frequency = (ci.stathz ? ci.stathz : ci.hz);
 +#ifndef __DragonFly__ /* TODO */
- 	buf->rtime = pinfo [0].ki_runtime * 1e-6;
+ 	buf->rtime = pinfo [0].ki_runtime * buf->frequency / 1000000;
 +#endif
+ 	buf->flags = _glibtop_sysdeps_proc_time;
  
- 	len = sizeof (ci);
- 	if (sysctlbyname ("kern.clockrate", &ci, &len, NULL, 0)) {
-@@ -94,6 +96,7 @@
- 
- 	buf->rtime *= buf->frequency;
- 
+ 	/*
+@@ -101,6 +103,7 @@ glibtop_get_proc_time_p (glibtop *server
+ 	  I have no idea what this PS_INMEM is, but it works perfectly
+ 	  without this check.
+ 	 */
 +#ifndef __DragonFly__ /* TODO */
-        if ((pinfo [0].ki_flag & PS_INMEM)) {
-            buf->utime = (pinfo [0].ki_runtime * 1e-6) * buf->frequency;
- 	   buf->stime = tv2sec (pinfo [0].ki_rusage.ru_stime) * buf->frequency;
-@@ -106,4 +109,5 @@
+ 	buf->utime = tv2sec_freq (pinfo [0].ki_rusage.ru_utime, buf->frequency);
+ 	buf->stime = tv2sec_freq (pinfo [0].ki_rusage.ru_stime, buf->frequency);
+ 	buf->cutime = tv2sec_freq (pinfo [0].ki_childtime, buf->frequency);
+@@ -111,4 +114,5 @@ glibtop_get_proc_time_p (glibtop *server
+ #endif
             buf->start_time = tv2sec (pinfo [0].ki_start);
             buf->flags |= _glibtop_sysdeps_proc_time_user;
-        }
 +#endif
  }
