e31ee2b9bdea12299251feb57baae7ce21a103d2
DPorts Builder <nobody@home.ok>
2015-09-20 11:43:51 -0700

    Import textproc/permute version 1.0

---
+++ textproc/permute/dragonfly/patch-permute.cpp
@@ -0,0 +1,11 @@
+--- permute.cpp.bak	2015-09-20 16:08:24.000000000 +0300
++++ permute.cpp
+@@ -19,7 +19,7 @@
+  */
+ #include <assert.h>
+ #include <iostream>
+-#include <string>
++#include <cstring>
+ 
+ using namespace std;
+ 
