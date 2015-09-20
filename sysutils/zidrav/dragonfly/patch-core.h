91251fe14fda1828f16c61d5e21b123117d77617
DPorts Builder <nobody@home.ok>
2015-09-20 11:43:58 -0700

    Import sysutils/zidrav version 1.2.0_1

---
+++ sysutils/zidrav/dragonfly/patch-core.h
@@ -0,0 +1,11 @@
+--- core.h.bak	2015-09-20 14:56:46.000000000 +0300
++++ core.h
+@@ -21,7 +21,7 @@
+ 
+ #include <iostream>
+ #include <fstream>
+-#include <string>
++#include <cstring>
+ 
+ using namespace std;
+ 
