d385bb26cc0c1b6360bfc2e262eabc4abfc9c599
DPorts Builder <nobody@home.ok>
2015-10-01 09:07:26 -0700

    Update devel/R-cran-Rcpp to version 0.12.1_1

--- devel/R-cran-Rcpp/dragonfly/patch-src_api.cpp
+++ devel/R-cran-Rcpp/dragonfly/patch-src_api.cpp
@@ -1,24 +1,20 @@
---- src/api.cpp.orig	2015-05-01 14:41:53 UTC
+--- src/api.cpp.orig	2015-09-10 13:17:01.000000000 +0300
 +++ src/api.cpp
-@@ -34,7 +34,9 @@ using namespace Rcpp;
+@@ -34,7 +34,7 @@ using namespace Rcpp;
  #endif
  
  #if defined(__GNUC__)
--    #if defined(WIN32) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__CYGWIN__) || defined(__sun) || defined(_AIX)
-+    #if defined(WIN32) || defined(__FreeBSD__) || defined(__NetBSD__) \
-+     || defined(__OpenBSD__) || defined(__CYGWIN__) || defined(__sun) \
-+     || defined(_AIX) || defined(__DragonFly__)
+-    #if defined(_WIN32) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__CYGWIN__) || defined(__sun) || defined(_AIX)
++    #if defined(_WIN32) || defined(__DragonFly__) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__CYGWIN__) || defined(__sun) || defined(_AIX)
          // do nothing
      #else
          #include <execinfo.h>
-@@ -257,7 +259,9 @@ SEXP rcpp_can_use_cxx11() {
+@@ -257,7 +257,7 @@ SEXP rcpp_can_use_cxx11() {
  // [[Rcpp::register]]
  SEXP stack_trace(const char* file, int line) {
      #if defined(__GNUC__)
--        #if defined(WIN32) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__CYGWIN__) || defined(__sun) || defined(_AIX)
-+        #if defined(WIN32) || defined(__FreeBSD__) || defined(__NetBSD__) \
-+         || defined(__OpenBSD__) || defined(__CYGWIN__) || defined(__sun) \
-+         || defined(_AIX) || defined(__DragonFly__)
+-        #if defined(_WIN32) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__CYGWIN__) || defined(__sun) || defined(_AIX)
++        #if defined(_WIN32) || defined(__DragonFly__) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__CYGWIN__) || defined(__sun) || defined(_AIX)
              // Simpler version for Windows and *BSD
              List trace = List::create(_["file"] = file,
                                        _[ "line"  ] = line,
