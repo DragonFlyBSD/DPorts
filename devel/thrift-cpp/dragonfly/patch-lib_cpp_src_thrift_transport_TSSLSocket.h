d75d9093dc046b7978b6a54d2347aeaa576d14a4
DPorts Builder <nobody@home.ok>
2015-11-01 17:22:24 -0800

    Update devel/thrift-cpp to version 0.9.3_2,1

--- devel/thrift-cpp/dragonfly/patch-lib_cpp_src_thrift_transport_TSSLSocket.h
+++ devel/thrift-cpp/dragonfly/patch-lib_cpp_src_thrift_transport_TSSLSocket.h
@@ -1,11 +1,10 @@
---- lib/cpp/src/thrift/transport/TSSLSocket.h.orig	2013-08-15 14:04:29.000000000 +0000
+--- lib/cpp/src/thrift/transport/TSSLSocket.h.orig	2015-08-18 18:49:52 UTC
 +++ lib/cpp/src/thrift/transport/TSSLSocket.h
-@@ -26,6 +26,8 @@
+@@ -25,6 +25,7 @@
+ #include <openssl/ssl.h>
  #include <thrift/concurrency/Mutex.h>
  #include <thrift/transport/TSocket.h>
- 
 +#include <sys/socket.h>
-+
- namespace apache { namespace thrift { namespace transport {
  
- class AccessManager;
+ namespace apache {
+ namespace thrift {
--- devel/thrift-cpp/dragonfly/patch-lib_cpp_src_thrift_transport_TSocket.cpp
+++ devel/thrift-cpp/dragonfly/patch-lib_cpp_src_thrift_transport_TSocket.cpp
@@ -1,20 +1,11 @@
---- lib/cpp/src/thrift/transport/TSocket.cpp.orig	2013-08-15 14:04:29.000000000 +0000
+--- lib/cpp/src/thrift/transport/TSocket.cpp.orig	2015-08-18 18:49:52 UTC
 +++ lib/cpp/src/thrift/transport/TSocket.cpp
-@@ -158,7 +158,7 @@ bool TSocket::peek() {
+@@ -214,7 +214,7 @@ bool TSocket::peek() {
    int r = static_cast<int>(recv(socket_, cast_sockopt(&buf), 1, MSG_PEEK));
    if (r == -1) {
      int errno_copy = THRIFT_GET_SOCKET_ERROR;
--    #if defined __FreeBSD__ || defined __MACH__
-+    #if defined __FreeBSD__ || defined __MACH__ || defined __DragonFly__
+-#if defined __FreeBSD__ || defined __MACH__
++#if defined __FreeBSD__ || defined __MACH__ || defined __DragonFly__
      /* shigin:
       * freebsd returns -1 and THRIFT_ECONNRESET if socket was closed by
       * the other side
-@@ -476,7 +476,7 @@ uint32_t TSocket::read(uint8_t* buf, uin
-       goto try_again;
-     }
- 
--    #if defined __FreeBSD__ || defined __MACH__
-+    #if defined __FreeBSD__ || defined __MACH__ || defined __DragonFly__
-     if (errno_copy == THRIFT_ECONNRESET) {
-       /* shigin: freebsd doesn't follow POSIX semantic of recv and fails with
-        * THRIFT_ECONNRESET if peer performed shutdown
