2d1f7e05edc9b21cbb57e1a16c30f0fd8f6b432c
DPorts Builder <nobody@home.ok>
2015-11-13 00:47:09 -0800

    Update editors/texstudio to version 2.10.4

--- editors/texstudio/dragonfly/patch-debughelper.cpp
+++ editors/texstudio/dragonfly/patch-debughelper.cpp
@@ -1,24 +1,33 @@
---- debughelper.cpp.orig	2015-10-08 23:03:09.674861000 +0200
-+++ debughelper.cpp	2015-10-08 23:04:34.916507000 +0200
-@@ -474,7 +474,7 @@
+--- debughelper.cpp.orig	2015-10-25 15:22:30 UTC
++++ debughelper.cpp
+@@ -27,7 +27,7 @@
+ #endif
+ 
+ 
+-#if (defined(__unix__) || defined(unix) || defined(__linux__) || defined(linux) || defined(__gnu_hurd__) || defined(__FreeBSD__) || defined(__FreeBSD_kernel__))
++#if (defined(__unix__) || defined(unix) || defined(__linux__) || defined(linux) || defined(__gnu_hurd__) || defined(__FreeBSD__) || defined(__DragonFly__))
+ #define OS_IS_LINUX_LIKE
+ #endif
+ 
+@@ -486,7 +486,7 @@ QString print_backtrace(const QString &m
  
  #define USE_SIGNAL_HANDLER
  #ifdef CPU_IS_X86_64
-- #if defined(__FreeBSD__) 
-+ #if defined(__FreeBSD__) || defined(__DragonFly__)
-   #define PC_FROM_UCONTEXT(context) (context)->uc_mcontext.mc_rip
-   #define STACK_FROM_UCONTEXT(context) (context)->uc_mcontext.mc_rsp
-   #define FRAME_FROM_UCONTEXT(context) (context)->uc_mcontext.mc_rbp
-@@ -484,7 +484,7 @@
-   #define FRAME_FROM_UCONTEXT(context) (context)->uc_mcontext.gregs[REG_RBP]
-  #endif
+-#if defined(__FreeBSD__)
++#if defined(__FreeBSD__) || defined(__DragonFly__)
+ #define PC_FROM_UCONTEXT(context) (context)->uc_mcontext.mc_rip
+ #define STACK_FROM_UCONTEXT(context) (context)->uc_mcontext.mc_rsp
+ #define FRAME_FROM_UCONTEXT(context) (context)->uc_mcontext.mc_rbp
+@@ -496,7 +496,7 @@ QString print_backtrace(const QString &m
+ #define FRAME_FROM_UCONTEXT(context) (context)->uc_mcontext.gregs[REG_RBP]
+ #endif
  #elif defined(CPU_IS_X86_32)
-- #if defined(__FreeBSD__)
-+ #if defined(__FreeBSD__) || defined(__DragonFly__)
-   #define PC_FROM_UCONTEXT(context) (context)->uc_mcontext.mc_eip
-   #define STACK_FROM_UCONTEXT(context) (context)->uc_mcontext.mc_esp
-   #define FRAME_FROM_UCONTEXT(context) (context)->uc_mcontext.mc_ebp
-@@ -533,8 +533,8 @@
+-#if defined(__FreeBSD__)
++#if defined(__FreeBSD__) || defined(__DragonFly__)
+ #define PC_FROM_UCONTEXT(context) (context)->uc_mcontext.mc_eip
+ #define STACK_FROM_UCONTEXT(context) (context)->uc_mcontext.mc_esp
+ #define FRAME_FROM_UCONTEXT(context) (context)->uc_mcontext.mc_ebp
+@@ -545,8 +545,8 @@ QString print_backtrace(const QString &m
  #error Unknown cpu architecture
  #endif
  
