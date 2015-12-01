ae279acb300ec95e2e6391468ee021a054dbac97
DPorts Builder <nobody@home.ok>
2015-11-30 23:47:45 -0800

    Update databases/mysqltuner to version 1.6.0

--- databases/mysqltuner/dragonfly/patch-mysqltuner.pl
+++ databases/mysqltuner/dragonfly/patch-mysqltuner.pl
@@ -1,11 +1,11 @@
---- mysqltuner.pl.orig	2014-05-26 20:46:11.623082000 +0000
+--- mysqltuner.pl.orig	2015-11-30 14:31:48 UTC
 +++ mysqltuner.pl
-@@ -221,7 +221,7 @@ sub os_setup {
- 		} elsif ($os =~ /Darwin/) {
- 			$physical_memory = `sysctl -n hw.memsize` or memerror;
- 			$swap_memory = `sysctl -n vm.swapusage | awk '{print \$3}' | sed 's/\..*\$//'` or memerror;
--		} elsif ($os =~ /NetBSD|OpenBSD|FreeBSD/) {
-+		} elsif ($os =~ /NetBSD|OpenBSD|DragonFly|FreeBSD/) {
- 			$physical_memory = `sysctl -n hw.physmem` or memerror;
- 			if ($physical_memory < 0) {
- 				$physical_memory = `sysctl -n hw.physmem64` or memerror;
+@@ -794,7 +794,7 @@ sub os_setup {
+               `sysctl -n vm.swapusage | awk '{print \$3}' | sed 's/\..*\$//'`
+               or memerror;
+         }
+-        elsif ( $os =~ /NetBSD|OpenBSD|FreeBSD/ ) {
++        elsif ( $os =~ /NetBSD|OpenBSD|DragonFly|FreeBSD/ ) {
+             $physical_memory = `sysctl -n hw.physmem` or memerror;
+             if ( $physical_memory < 0 ) {
+                 $physical_memory = `sysctl -n hw.physmem64` or memerror;
