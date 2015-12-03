2ba6de71f4bf8fbc2ded0ba306ebb87a40464314
DPorts Builder <nobody@home.ok>
2015-12-03 08:18:13 -0800

    Import games/mkhexgrid version 0.1.1_7

---
+++ games/mkhexgrid/dragonfly/patch-png.cpp
@@ -0,0 +1,11 @@
+--- png.cpp.orig	2007-01-10 15:37:42.000000000 +0200
++++ png.cpp
+@@ -23,7 +23,7 @@
+ #include <iomanip>
+ #include <exception>
+ #include <stdexcept>
+-#include <string>
++#include <cstring>
+ #include <sstream>
+ using namespace std;
+ 
