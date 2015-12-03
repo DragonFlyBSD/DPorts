df348648e6be7ef4049bc5a706a66dd40e693e63
DPorts Builder <nobody@home.ok>
2015-12-03 08:17:41 -0800

    Import devel/love5 version 0.5.0_15

---
+++ devel/love5/dragonfly/patch-src_liblove_include_love_File.h
@@ -0,0 +1,11 @@
+--- src/liblove/include/love/File.h.orig	2008-09-20 17:40:17.000000000 +0300
++++ src/liblove/include/love/File.h
+@@ -12,7 +12,7 @@
+ #include "constants.h"
+ 
+ // STL
+-#include <string>
++#include <cstring>
+ 
+ // Boost
+ #include <boost/shared_ptr.hpp>
