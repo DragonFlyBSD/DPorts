f7c8b490a2ec2c15f6153ab1788e4a2f7d0b8e57
DPorts Builder <nobody@home.ok>
2015-12-04 11:27:56 -0800

    Import games/xspacewarp version 1.2_2

---
+++ games/xspacewarp/dragonfly/patch-c_blackhole.hh
@@ -0,0 +1,11 @@
+--- c_blackhole.hh.bak	2015-12-04 20:45:47.000000000 +0200
++++ c_blackhole.hh
+@@ -14,7 +14,7 @@
+ #ifndef _BLACKHOLE_
+ #define _BLACKHOLE_
+ 
+-
++#include <cstdlib>
+ #include "c_celestial.hh"
+ #include <X11/Xlib.h>
+ #include "space_objects.hh"
