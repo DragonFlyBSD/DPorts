f91a15f1414112f2308a31470b5795bfb6e84c5d
DPorts Builder <nobody@home.ok>
2015-12-05 11:27:55 -0800

    Import games/tsito version 0.8.4

---
+++ games/tsito/dragonfly/patch-src_Board.cpp
@@ -0,0 +1,10 @@
+--- src/Board.cpp.orig	2003-04-23 02:00:34.000000000 +0300
++++ src/Board.cpp
+@@ -2,6 +2,7 @@
+ #include	"Move.h"
+ 
+ #include	<string>
++#include	<cstring>
+ #include	<cctype>
+ #include	<cstdlib>
+ #include	<ctime>
