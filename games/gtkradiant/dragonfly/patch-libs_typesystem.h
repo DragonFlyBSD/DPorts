d96aed100ad4eb18e06c40b5094d50ed36f6e6aa
DPorts Builder <nobody@home.ok>
2015-12-05 07:05:23 -0800

    Import games/gtkradiant version 1.5.0_14

---
+++ games/gtkradiant/dragonfly/patch-libs_typesystem.h
@@ -0,0 +1,10 @@
+--- libs/typesystem.h.orig	2006-02-11 00:01:20.000000000 +0200
++++ libs/typesystem.h
+@@ -23,6 +23,7 @@ Foundation, Inc., 51 Franklin St, Fifth
+ #define INCLUDED_TYPESYSTEM_H
+ 
+ 
++#include <memory>
+ #include <list>
+ #include "generic/callback.h"
+ #include "generic/static.h"
