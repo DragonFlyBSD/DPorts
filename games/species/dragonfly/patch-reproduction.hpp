87723cacc49a0567e9350b447fd1d5181a0d2ef7
DPorts Builder <nobody@home.ok>
2015-12-05 11:27:59 -0800

    Import games/species version 1.2d_1

---
+++ games/species/dragonfly/patch-reproduction.hpp
@@ -0,0 +1,10 @@
+--- reproduction.hpp.bak	2015-12-05 20:29:30.000000000 +0200
++++ reproduction.hpp
+@@ -20,6 +20,7 @@
+ 
+ #include "ini.hpp"
+ #include <iostream>
++#include <cstring>
+ 
+ // a forward
+ class CWarrior;
