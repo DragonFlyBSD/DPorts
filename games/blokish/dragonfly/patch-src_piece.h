19bc1d5d7f42a391fc532336cb7f46df273d63c0
DPorts Builder <nobody@home.ok>
2015-12-07 04:03:05 -0800

    Import games/blokish version 0.9.4_8

---
+++ games/blokish/dragonfly/patch-src_piece.h
@@ -0,0 +1,11 @@
+--- src/piece.h.orig	2006-01-08 21:56:22.000000000 +0200
++++ src/piece.h
+@@ -26,6 +26,8 @@
+ #include <iostream>
+ #include <vector>
+ #include <string>
++#include <algorithm>
++#include <cstdlib>
+ 
+ #include "base.h"
+ 
