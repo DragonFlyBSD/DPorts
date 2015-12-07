2cb1db96fa01361c31a2203607c8ce77f7a5fdaa
DPorts Builder <nobody@home.ok>
2015-12-07 10:52:00 -0800

    Import games/jfk version 0.1.1_12

---
+++ games/jfk/dragonfly/patch-src_lib_network.cc
@@ -0,0 +1,10 @@
+--- src/lib/network.cc.bak	2015-12-07 16:59:39.000000000 +0200
++++ src/lib/network.cc
+@@ -7,6 +7,7 @@
+ #include <cerrno>
+ #include <csignal>
+ #include <cstdlib>
++#include <cstddef>
+ #include <cstring>
+ #include <new>
+ #include <string>
