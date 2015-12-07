4a5d1df46bee7287860d691bb6e33f4e1ec85146
DPorts Builder <nobody@home.ok>
2015-12-07 10:51:27 -0800

    Import games/pouetchess version 0.1.1_13

---
+++ games/pouetchess/dragonfly/patch-src_gui.h
@@ -0,0 +1,10 @@
+--- src/gui.h.orig	2015-12-07 15:50:18.000000000 +0200
++++ src/gui.h
+@@ -10,6 +10,7 @@
+ #include <SDL/SDL_opengl.h>
+ #include <list>
+ #include <string>
++#include <cstring>
+ 
+ 
+ using namespace std;
