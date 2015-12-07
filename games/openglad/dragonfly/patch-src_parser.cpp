759c4aa4b321ac0b4e319baefb38eca0405cdaaa
DPorts Builder <nobody@home.ok>
2015-12-07 02:02:35 -0800

    Import games/openglad version 0.98_10

---
+++ games/openglad/dragonfly/patch-src_parser.cpp
@@ -0,0 +1,11 @@
+--- src/parser.cpp.orig	2002-10-01 06:39:36.000000000 +0200
++++ src/parser.cpp
+@@ -23,6 +23,8 @@
+ #include <iostream>
+ #include <fstream>
+ #include <string>
++#include <cstring>
++#include <cstdlib>
+ #include "parser.h"
+ 
+ using namespace std;
