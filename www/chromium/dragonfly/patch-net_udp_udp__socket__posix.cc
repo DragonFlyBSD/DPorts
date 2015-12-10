4668a9da5e34506ff31fced30e56235e5b929c27
DPorts Builder <nobody@home.ok>
2015-12-10 04:30:00 -0800

    Update www/chromium to version 47.0.2526.73

--- www/chromium/dragonfly/patch-net_udp_udp__socket__posix.cc
+++ www/chromium/dragonfly/patch-net_udp_udp__socket__posix.cc
@@ -1,6 +1,6 @@
---- net/udp/udp_socket_libevent.cc.orig	2014-05-14 19:22:14.000000000 +0000
-+++ net/udp/udp_socket_libevent.cc
-@@ -36,7 +36,7 @@ const int kBindRetries = 10;
+--- net/udp/udp_socket_posix.cc.orig	2015-11-24 20:00:55 UTC
++++ net/udp/udp_socket_posix.cc
+@@ -37,7 +37,7 @@ const int kBindRetries = 10;
  const int kPortStart = 1024;
  const int kPortEnd = 65535;
  
@@ -9,7 +9,7 @@
  
  // Returns IPv4 address in network order.
  int GetIPv4AddressFromIndex(int socket, uint32 index, uint32* address){
-@@ -569,7 +569,7 @@ int UDPSocketLibevent::SetSocketOptions(
+@@ -549,7 +549,7 @@ int UDPSocketPosix::SetMulticastOptions(
    if (multicast_interface_ != 0) {
      switch (addr_family_) {
        case AF_INET: {
@@ -18,7 +18,7 @@
          ip_mreqn mreq;
          mreq.imr_ifindex = multicast_interface_;
          mreq.imr_address.s_addr = htonl(INADDR_ANY);
-@@ -644,7 +644,7 @@ int UDPSocketLibevent::JoinGroup(const I
+@@ -624,7 +624,7 @@ int UDPSocketPosix::JoinGroup(const IPAd
        if (addr_family_ != AF_INET)
          return ERR_ADDRESS_INVALID;
  
