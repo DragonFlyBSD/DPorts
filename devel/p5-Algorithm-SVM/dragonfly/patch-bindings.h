64ec57efeadb7d7d8221e4ed08e54adf88206c1e
DPorts Builder <nobody@home.ok>
2015-12-13 10:19:15 -0800

    Import devel/p5-Algorithm-SVM version 0.13_1

---
+++ devel/p5-Algorithm-SVM/dragonfly/patch-bindings.h
@@ -0,0 +1,10 @@
+--- bindings.h.orig	2008-01-22 23:13:57.000000000 +0200
++++ bindings.h
+@@ -3,6 +3,7 @@
+ 
+ using namespace std;
+ 
++#include <cstring>
+ #include <vector>
+ #include <map>
+ #include <assert.h>
