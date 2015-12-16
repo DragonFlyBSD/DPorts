94638a12a1e1673c7668cab56c1644bf83e2e81d
DPorts Builder <nobody@home.ok>
2015-12-16 12:13:13 -0800

    Update databases/cego to version 2.27.2

---
+++ databases/cego/dragonfly/patch-src_Cego.cc
@@ -0,0 +1,10 @@
+--- src/Cego.cc.orig	2015-12-04 19:04:42 UTC
++++ src/Cego.cc
+@@ -3,6 +3,7 @@
+ // This code was produced by the dragon parser generator
+ //
+ #include "Cego.h"
++#include <stdlib.h>
+ #include <lfcbase/Exception.h>
+ #include <lfcbase/StackT.h>
+ 
