413749d03c86bd1dfa847667e7b3fbdd9634aeb7
DPorts Builder <nobody@home.ok>
2015-12-16 14:31:48 -0800

    Tweak emulators/qemu-devel version 2.4.0

---
+++ emulators/qemu-devel/dragonfly/patch-coroutine-ucontext.c
@@ -0,0 +1,19 @@
+--- coroutine-ucontext.c.orig	2015-12-15 23:02:28 +0100
++++ coroutine-ucontext.c
+@@ -122,7 +122,15 @@
+                 2, arg.i[0], arg.i[1]);
+ 
+     /* swapcontext() in, siglongjmp() back out */
+-    if (!sigsetjmp(old_env, 0)) {
++    /* Save signal mask in this sigsetjmp, because makecontext on DragonFly
++     * leaves all signals blocked when entering the new context with
++     * swapcontext.
++     * Workaround this, by just having the signal mask restored by the
++     * siglongjmp that brings us back from qemu_coroutine_new().
++     * XXX Remove this workaround when the makecontext behaviour is fixed
++     *     on DragonFly.
++     */
++    if (!sigsetjmp(old_env, 1)) {
+         swapcontext(&old_uc, &uc);
+     }
+     return &co->base;
