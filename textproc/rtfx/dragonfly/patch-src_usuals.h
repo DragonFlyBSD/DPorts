3247f6b3fe3561e75ce2c7f72bdb0cfc5ec68a8e
DPorts Builder <nobody@home.ok>
2015-12-20 00:09:53 -0800

    Import textproc/rtfx version 1.1_1

---
+++ textproc/rtfx/dragonfly/patch-src_usuals.h
@@ -0,0 +1,10 @@
+--- src/usuals.h.orig	2007-05-27 03:53:31.000000000 +0300
++++ src/usuals.h
+@@ -66,6 +66,7 @@
+ 
+ #include <wchar.h>
+ #include <string>
++#include <cstring>
+ using std::string;
+ 
+ #if HAVE_WSTRING
