30fc9a242adcf3cdcd9186a53505d4ef33798538
DPorts Builder <nobody@home.ok>
2015-12-20 02:02:40 -0800

    Tweak textproc/aiksaurus version 1.2.1_3

---
+++ textproc/aiksaurus/dragonfly/patch-gtk_src_gtkAiksaur.t.cpp
@@ -0,0 +1,10 @@
+--- gtk/src/gtkAiksaur.t.cpp.orig	2002-01-12 04:02:12.000000000 +0200
++++ gtk/src/gtkAiksaur.t.cpp
+@@ -21,6 +21,7 @@
+ #include <AiksaurusGTK.h>
+ #include "config.h"
+ #include <gtk/gtk.h>
++#include <cstdlib>
+ #include <iostream>
+ #include <string>
+ using namespace std;
