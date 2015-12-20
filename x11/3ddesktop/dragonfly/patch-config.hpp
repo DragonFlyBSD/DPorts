b07388e44c06080b1e2136ce1d5627e7577c86e9
DPorts Builder <nobody@home.ok>
2015-12-20 00:09:57 -0800

    Import x11/3ddesktop version 0.2.9_13

---
+++ x11/3ddesktop/dragonfly/patch-config.hpp
@@ -0,0 +1,10 @@
+--- config.hpp.orig	2005-06-20 14:28:29.000000000 +0300
++++ config.hpp
+@@ -24,6 +24,7 @@
+ 
+ #include <stdio.h>
+ #include <stdlib.h>
++#include <cstring>
+ 
+ #include <errno.h>
+ #include <unistd.h>
