6a3757f62df643528521362012d4cd18f8844749
DPorts Builder <nobody@home.ok>
2015-12-28 20:29:08 -0800

    Update lang/python33 to version 3.3.6_2

--- lang/python33/files/patch-Lib_distutils_unixccompiler.py
+++ lang/python33/files/patch-Lib_distutils_unixccompiler.py
@@ -2,9 +2,9 @@
 # Issue ID: http://bugs.python.org/issue20767
 # Submitted by: antoine
 
---- ./Lib/distutils/unixccompiler.py.orig	2014-03-09 15:25:15.921204441 +1100
-+++ ./Lib/distutils/unixccompiler.py	2014-03-09 15:25:50.643809649 +1100
-@@ -225,6 +225,8 @@
+--- Lib/distutils/unixccompiler.py.orig	2014-10-12 07:03:52 UTC
++++ Lib/distutils/unixccompiler.py
+@@ -225,6 +225,8 @@ class UnixCCompiler(CCompiler):
          if sys.platform[:6] == "darwin":
              # MacOSX's linker doesn't understand the -R flag at all
              return "-L" + dir
