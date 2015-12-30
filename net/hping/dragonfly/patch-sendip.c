603761c67e1f37dcb6c34be5b223036b2ce74943
DPorts Builder <nobody@home.ok>
2015-12-30 11:40:33 -0800

    Update net/hping to version 2.0.0r3_1,1

--- net/hping/dragonfly/patch-sendip.c
+++ net/hping/dragonfly/patch-sendip.c
@@ -1,20 +1,20 @@
---- sendip.c.orig	2014-10-06 04:48:35 UTC
+--- sendip.c.intermediate	2014-10-06 04:48:35 UTC
 +++ sendip.c
 @@ -48,7 +48,7 @@
  	ip->ihl		= (IPHDR_SIZE + optlen + 3) >> 2;
  	ip->tos		= ip_tos;
  
--#if defined OSTYPE_FREEBSD || defined OSTYPE_NETBSD || defined OSTYPE_BSDI
-+#if defined OSTYPE_FREEBSD || defined OSTYPE_DRAGONFLY || defined OSTYPE_NETBSD || defined OSTYPE_BSDI
- /* FreeBSD */
+-#if defined OSTYPE_NETBSD || defined OSTYPE_BSDI
++#if defined OSTYPE_DRAGONFLY || defined OSTYPE_NETBSD || defined OSTYPE_BSDI
  /* NetBSD */
  	ip->tot_len	= packetsize;
+ #else
 @@ -73,7 +73,7 @@
  			htons((unsigned short) src_id);
  	}
  
--#if defined OSTYPE_FREEBSD || defined OSTYPE_NETBSD | defined OSTYPE_BSDI
-+#if defined OSTYPE_FREEBSD || defined OSTYPE_DRAGONFLY || defined OSTYPE_NETBSD | defined OSTYPE_BSDI
- /* FreeBSD */
+-#if defined OSTYPE_NETBSD | defined OSTYPE_BSDI
++#if defined OSTYPE_DRAGONFLY || defined OSTYPE_NETBSD | defined OSTYPE_BSDI
  /* NetBSD */
  	ip->frag_off	|= more_fragments;
+ 	ip->frag_off	|= fragoff >> 3;
