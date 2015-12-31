797c918924a18c3919ab242bddcfb9d79317ae07
DPorts Builder <nobody@home.ok>
2015-12-31 00:20:51 -0800

    Update multimedia/audacious-plugins to version 3.7.1

--- multimedia/audacious-plugins/dragonfly/patch-src_gio_gio.cc
+++ multimedia/audacious-plugins/dragonfly/patch-src_gio_gio.cc
@@ -1,12 +1,13 @@
---- src/gio/gio.cc.orig	2015-11-07 23:04:08 UTC
+--- src/gio/gio.cc.orig	2015-12-29 02:29:53 UTC
 +++ src/gio/gio.cc
-@@ -29,6 +29,9 @@
+@@ -29,6 +29,10 @@
  #include <libaudcore/plugin.h>
  #include <libaudcore/runtime.h>
  
 +#undef getc
 +#undef feof
 +#undef ungetc
- 
++
  static const char gio_about[] =
   N_("GIO Plugin for Audacious\n"
+     "Copyright 2009-2012 John Lindgren");
