dadaf006c4fbe5abae0d29d099d7098017925c94
DPorts Builder <nobody@home.ok>
2016-01-01 13:53:29 -0800

    Import graphics/darknock version 0.3.0_5

---
+++ graphics/darknock/dragonfly/patch-src_net_maxnet.cpp
@@ -0,0 +1,10 @@
+--- src/net/maxnet.cpp.orig	2007-10-17 03:11:58.000000000 +0300
++++ src/net/maxnet.cpp
+@@ -19,6 +19,7 @@
+  ***************************************************************************/
+ 
+ #include <iostream>
++#include <cstdlib>
+ // #include <QtDebug>
+ #include "net.h"
+ #include "node.h"
---
+++ graphics/darknock/dragonfly/patch-src_net_pattern.h
@@ -0,0 +1,10 @@
+--- src/net/pattern.h.orig	2007-10-16 23:55:25.000000000 +0300
++++ src/net/pattern.h
+@@ -24,6 +24,7 @@
+ #include <vector>
+ #include <string>
+ #include <iostream>
++#include <cstdlib>
+ 
+ namespace darknocksBrain {
+ 
