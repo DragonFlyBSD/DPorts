9991f8ea894806b25db79ceb4dab569221d374c8
DPorts Builder <nobody@home.ok>
2016-01-01 13:53:20 -0800

    Import graphics/gimmage version 0.2.3_11

---
+++ graphics/gimmage/dragonfly/patch-src_FileManager.h
@@ -0,0 +1,10 @@
+--- src/FileManager.h.orig	2007-06-26 16:51:15.000000000 +0300
++++ src/FileManager.h
+@@ -21,6 +21,7 @@ Copyright 2006 Bartek Kostrzewa
+ 
+ #include <iostream>
+ #include <list>
++#include <cstring>
+ // #include <string>
+ #include <glibmm.h>
+ 
