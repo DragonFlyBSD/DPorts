2f74f4af531025a7deb5c91c67a0c655497e88d8
DPorts Builder <nobody@home.ok>
2016-01-01 13:52:28 -0800

    Import graphics/metacam version 1.2

---
+++ graphics/metacam/dragonfly/patch-metatiff.h
@@ -0,0 +1,11 @@
+--- metatiff.h.orig	2004-08-21 20:56:53.000000000 +0300
++++ metatiff.h
+@@ -29,6 +29,8 @@ $Id: metatiff.h,v 1.3 2002/09/02 19:20:0
+ #include <vector>
+ #include <string>
+ #include <iostream>
++#include <cstring>
++#include <cstdlib>
+ 
+ using namespace std;
+ /* Classes for access to meta-infomation from tiff files */
