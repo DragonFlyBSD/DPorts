3bc555e657b1940578bcc817b3b7fe4d0e58ec3a
DPorts Builder <nobody@home.ok>
2016-01-01 14:30:17 -0800

    Import x11-toolkits/gtkmathview version 0.8.0_9

---
+++ x11-toolkits/gtkmathview/dragonfly/patch-src_common_String.hh
@@ -0,0 +1,10 @@
+--- src/common/String.hh.orig	2007-08-17 13:02:45.000000000 +0300
++++ src/common/String.hh
+@@ -20,6 +20,7 @@
+ #define __String_hh__
+ 
+ #include <string>
++#include <cstring>
+ 
+ #include "gmv_defines.h"
+ #include "Char.hh"
