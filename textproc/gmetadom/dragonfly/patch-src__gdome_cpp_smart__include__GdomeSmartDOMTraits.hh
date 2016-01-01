c37cbcc99d580b4bf1feaf02b05b4178387ae667
DPorts Builder <nobody@home.ok>
2016-01-01 14:27:45 -0800

    Import textproc/gmetadom version 0.2.6_4

---
+++ textproc/gmetadom/dragonfly/patch-src__gdome_cpp_smart__include__GdomeSmartDOMTraits.hh
@@ -0,0 +1,10 @@
+--- src/gdome_cpp_smart/include/GdomeSmartDOMTraits.hh.orig	2007-10-15 12:39:07.000000000 +0300
++++ src/gdome_cpp_smart/include/GdomeSmartDOMTraits.hh
+@@ -26,6 +26,7 @@
+  */
+ 
+ #include <string>
++#include <cstring>
+ 
+ #include "GdomeSmartDOMChar.hh"
+ 
