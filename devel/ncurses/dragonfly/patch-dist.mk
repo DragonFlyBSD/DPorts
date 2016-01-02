1a87d4335255bb89d5fa7b09f41dbc5d60e5c340
DPorts Builder <nobody@home.ok>
2016-01-02 12:08:47 -0800

    Update devel/ncurses to version 6.0_3

---
+++ devel/ncurses/dragonfly/patch-dist.mk
@@ -0,0 +1,11 @@
+--- dist.mk.orig	2015-08-06 23:13:39 UTC
++++ dist.mk
+@@ -36,7 +36,7 @@ SHELL = /bin/sh
+ 
+ # These define the major/minor/patch versions of ncurses.
+ NCURSES_MAJOR = 6
+-NCURSES_MINOR = 0
++NCURSES_MINOR = 01
+ NCURSES_PATCH = 20150808
+ 
+ # We don't append the patch to the version, since this only applies to releases
