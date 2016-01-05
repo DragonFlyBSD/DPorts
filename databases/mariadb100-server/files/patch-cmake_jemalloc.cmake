81e484c4a12cd2d2f8fbe0479dd92a167d482791
DPorts Builder <nobody@home.ok>
2016-01-04 23:41:35 -0800

    Update databases/mariadb100-server to version 10.0.23

--- databases/mariadb100-server/files/patch-cmake_jemalloc.cmake
+++ databases/mariadb100-server/files/patch-cmake_jemalloc.cmake
@@ -1,18 +1,28 @@
---- cmake/jemalloc.cmake.orig	2015-06-17 14:54:11 UTC
+Upstreamed https://github.com/MariaDB/server/pull/139
+
+--- cmake/jemalloc.cmake.orig	2015-12-16 16:05:44 UTC
 +++ cmake/jemalloc.cmake
-@@ -24,12 +24,12 @@ MACRO (CHECK_JEMALLOC)
+@@ -20,11 +20,21 @@ MACRO (CHECK_JEMALLOC)
+       SET(CMAKE_REQUIRED_LIBRARIES pthread dl m)
+       SET(what bundled)
+     ELSE()
+-      SET(libname jemalloc)
++      IF(CMAKE_SYSTEM_NAME STREQUAL "FreeBSD" AND
++        CMAKE_SYSTEM_VERSION STRGREATER "10.0")
++        SET(libname c)
++      ELSE()
++        SET(libname jemalloc)
++      ENDIF()
        SET(what system)
      ENDIF()
  
 -    CHECK_LIBRARY_EXISTS(${libname} malloc_stats_print "" HAVE_JEMALLOC)
-+    CHECK_LIBRARY_EXISTS(c malloc_stats_print "" HAVE_JEMALLOC)
++    IF(CMAKE_SYSTEM_NAME STREQUAL "FreeBSD" AND
++      CMAKE_SYSTEM_VERSION STRGREATER "10.0")
++      SET(HAVE_JEMALLOC ON)
++    ELSE()
++      CHECK_LIBRARY_EXISTS(${libname} malloc_stats_print "" HAVE_JEMALLOC)
++    ENDIF()
      SET(CMAKE_REQUIRED_LIBRARIES)
  
      IF (HAVE_JEMALLOC)
--      SET(LIBJEMALLOC ${libname})
--      SET(MALLOC_LIBRARY "${what} jemalloc")
-+      SET(LIBJEMALLOC c)
-+      SET(MALLOC_LIBRARY "system jemalloc")
-     ELSEIF (NOT WITH_JEMALLOC STREQUAL "auto")
-       MESSAGE(FATAL_ERROR "${libname} is not found")
-     ENDIF()
