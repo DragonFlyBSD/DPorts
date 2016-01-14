1f83709a63a6aaf54ed25855fcfc80f68fc23f37
DPorts Builder <nobody@home.ok>
2016-01-13 18:03:26 -0800

    Update math/octave to version 4.0.0_1

--- math/octave/dragonfly/patch-libgnu_signal.in.h
+++ math/octave/dragonfly/patch-libgnu_signal.in.h
@@ -1,11 +1,11 @@
---- libgnu/signal.in.h.orig	2012-01-06 17:10:38.000000000 +0000
+--- libgnu/signal.in.h.orig	2015-05-23 17:36:17.000000000 +0300
 +++ libgnu/signal.in.h
-@@ -61,7 +61,7 @@
-    pthread_sigmask in <pthread.h>, not in <signal.h>.
+@@ -59,7 +59,7 @@
+    declare pthread_sigmask in <pthread.h>, not in <signal.h>.
     But avoid namespace pollution on glibc systems.*/
  #if (@GNULIB_PTHREAD_SIGMASK@ || defined GNULIB_POSIXCHECK) \
--    && ((defined __APPLE__ && defined __MACH__) || defined __FreeBSD__ || defined __OpenBSD__ || defined __osf__ || defined __sun) \
-+    && ((defined __APPLE__ && defined __MACH__) || defined __DragonFly__ || defined __FreeBSD__ || defined __OpenBSD__ || defined __osf__ || defined __sun) \
+-    && ((defined __APPLE__ && defined __MACH__) \
++    && ((defined __APPLE__ && defined __MACH__) || defined __DragonFly__ \
+         || defined __FreeBSD__ || defined __OpenBSD__ || defined __osf__ \
+         || defined __sun || defined __ANDROID__) \
      && ! defined __GLIBC__
- # include <pthread.h>
- #endif
