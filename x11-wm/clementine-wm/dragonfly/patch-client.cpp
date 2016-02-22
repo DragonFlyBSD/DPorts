844f6471655fbc8e25f8fa9425a7b050722955bc
DPorts Builder <nobody@home.ok>
2016-02-22 10:20:27 -0800

    Import x11-wm/clementine-wm version 0.0.7_7

---
+++ x11-wm/clementine-wm/dragonfly/patch-client.cpp
@@ -0,0 +1,10 @@
+--- client.cpp.orig	2002-03-23 23:08:57 UTC
++++ client.cpp
+@@ -10,6 +10,7 @@
+ #include "painter.h"
+ #include <iostream>
+ #include <assert.h>
++#include <cstdlib>
+ #include <iostream> // ## db more
+ 
+ #define PIXELS 0
