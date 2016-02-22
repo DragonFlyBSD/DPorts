08b3705ae56d81c3cacdf9745fabac50bb250e24
DPorts Builder <nobody@home.ok>
2016-02-22 10:40:08 -0800

    Update databases/cego to version 2.29.1

---
+++ databases/cego/dragonfly/patch-src_CegoAction.cc
@@ -0,0 +1,10 @@
+--- src/CegoAction.cc.orig	2016-02-05 07:42:47 UTC
++++ src/CegoAction.cc
+@@ -48,6 +48,7 @@
+ #include "CegoTypeConverter.h"
+ 
+ #include <ctype.h>
++#include <string.h>
+ #include <stdlib.h>
+ 
+ extern char __quoteEscapeFlag;
