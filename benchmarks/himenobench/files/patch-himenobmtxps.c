436e49e7d6807c9574c5137238de46ce5cf75484
DPorts Builder <nobody@home.ok>
2016-03-09 22:44:34 -0800

    Import benchmarks/himenobench version 2015.07.09

---
+++ benchmarks/himenobench/files/patch-himenobmtxps.c
@@ -0,0 +1,18 @@
+--- himenobmtxps.c.orig	2002-02-21 00:27:48 UTC
++++ himenobmtxps.c
+@@ -37,6 +37,7 @@
+ ********************************************************************/
+ 
+ #include <stdio.h>
++#include <sys/time.h>
+ 
+ #ifdef SSMALL
+ #define MIMAX            33
+@@ -245,7 +246,6 @@ mflops(int nn,double cpu,double flop)
+ double
+ second()
+ {
+-#include <sys/time.h>
+ 
+   struct timeval tm;
+   double t ;
