b9b14b941fca7587d4859ac61fa2b35c74a28fe1
DPorts Builder <nobody@home.ok>
2016-04-08 15:32:41 -0700

    Update devel/sdl20 to version 2.0.4

--- devel/sdl20/dragonfly/patch-src_cpuinfo_SDL__cpuinfo.c
+++ devel/sdl20/dragonfly/patch-src_cpuinfo_SDL__cpuinfo.c
@@ -1,11 +1,11 @@
---- src/cpuinfo/SDL_cpuinfo.c.orig	2014-03-16 02:31:44.000000000 +0000
+--- src/cpuinfo/SDL_cpuinfo.c.orig	2016-01-02 19:56:31 UTC
 +++ src/cpuinfo/SDL_cpuinfo.c
-@@ -665,7 +665,7 @@ SDL_GetSystemRAM(void)
+@@ -735,7 +735,7 @@ SDL_GetSystemRAM(void)
  #endif
  #ifdef HAVE_SYSCTLBYNAME
          if (SDL_SystemRAM <= 0) {
--#if defined(__FreeBSD__) || defined(__FreeBSD_kernel__)
-+#if defined(__FreeBSD__) || defined(__DragonFly__) || defined(__FreeBSD_kernel__)
+-#if defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || defined(__NetBSD__)
++#if defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || defined(__NetBSD__) || defined(__DragonFly__)
  #ifdef HW_REALMEM
              int mib[2] = {CTL_HW, HW_REALMEM};
  #else
