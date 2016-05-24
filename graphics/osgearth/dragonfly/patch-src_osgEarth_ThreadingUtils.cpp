20066a4f54d3c667ce1bcf905f17f802d998b61f
DPorts Builder <nobody@home.ok>
2016-05-24 06:08:22 -0700

    Update graphics/osgearth to version 2.7

--- graphics/osgearth/dragonfly/patch-src_osgEarth_ThreadingUtils.cpp
+++ graphics/osgearth/dragonfly/patch-src_osgEarth_ThreadingUtils.cpp
@@ -1,4 +1,4 @@
---- src/osgEarth/ThreadingUtils.cpp.intermediate	2014-05-09 14:24:31.817118000 +0000
+--- src/osgEarth/ThreadingUtils.cpp.intermediate	2016-05-23 16:05:03 UTC
 +++ src/osgEarth/ThreadingUtils.cpp
 @@ -23,7 +23,6 @@
  #else
@@ -8,10 +8,10 @@
  #endif
  
  using namespace osgEarth::Threading;
-@@ -39,6 +38,8 @@ unsigned osgEarth::Threading::getCurrent
-   return (unsigned)::GetCurrentThreadId();
- #elif __APPLE__
+@@ -41,6 +40,8 @@ unsigned osgEarth::Threading::getCurrent
    return ::syscall(SYS_thread_selfid);
+ #elif __ANDROID__
+   return gettid();
 +#elif __DragonFly__
 +  return (unsigned)::syscall(SYS_lwp_gettid);
  #elif __FreeBSD__
