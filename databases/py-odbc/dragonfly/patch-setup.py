58cad8b88cac8e4d566c02ea999a47d4971275a9
DPorts Builder <nobody@home.ok>
2016-06-01 23:01:29 -0700

    Update databases/py-odbc to version 3.0.10

--- databases/py-odbc/dragonfly/patch-setup.py
+++ databases/py-odbc/dragonfly/patch-setup.py
@@ -1,11 +1,11 @@
---- setup.py.intermediate	2013-10-30 13:25:18.347948000 +0000
+--- setup.py.orig	2016-06-01 23:18:54 UTC
 +++ setup.py
-@@ -156,7 +156,7 @@ def get_compiler_settings(version_str):
+@@ -187,7 +187,7 @@ def get_compiler_settings(version_str):
          # What is the proper way to detect iODBC, MyODBC, unixODBC, etc.?
          settings['libraries'].append('odbc')
  
--        if sys.platform.startswith('freebsd'):
+-	if sys.platform.startswith('freebsd'):
 +        if sys.platform.startswith('freebsd') or sys.platform.startswith('dragonfly'):
-             settings['library_dirs'] = ['/usr/local/lib/']
-             settings['include_dirs'] = ['/usr/local/include/']
-             freebsd_prefix = '/usr/local'
+ 	    settings['library_dirs'] = ['/usr/local/lib/']
+ 	    settings['include_dirs'] = ['/usr/local/include/']
+ 	    freebsd_prefix = '/usr/local'
