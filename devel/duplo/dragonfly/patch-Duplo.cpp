4935dc8987dade0113013659aa78e5000669f0b8
DPorts Builder <nobody@home.ok>
2016-06-14 14:32:36 -0700

    Import devel/duplo version 0.2.0

---
+++ devel/duplo/dragonfly/patch-Duplo.cpp
@@ -0,0 +1,10 @@
+--- Duplo.cpp.orig	2006-05-25 14:49:03.000000000 +0300
++++ Duplo.cpp
+@@ -18,6 +18,7 @@
+ 
+ #include <fstream>
+ #include <time.h>
++#include <cstring>
+ 
+ #include "SourceFile.h"
+ #include "SourceLine.h"
