25f3730c1ca34bd8a5e45d955abaf192a6e2fb43
DPorts Builder <nobody@home.ok>
2016-06-15 09:59:25 -0700

    Import emulators/bsvc version 2.1_3

---
+++ emulators/bsvc/dragonfly/patch-Framework_Interface.cxx
@@ -0,0 +1,10 @@
+--- Framework/Interface.cxx.orig	2016-06-15 18:32:28.000000000 +0300
++++ Framework/Interface.cxx
+@@ -34,6 +34,7 @@
+   #include <iostream>
+   #include <strstream>
+   #include <string>
++  #include <cstring>
+ #endif
+ 
+ #ifdef USE_STD
---
+++ emulators/bsvc/dragonfly/patch-sim68360_devices_M68681.cxx
@@ -0,0 +1,10 @@
+--- sim68360/devices/M68681.cxx.orig	2016-06-15 18:41:31.000000000 +0300
++++ sim68360/devices/M68681.cxx
+@@ -16,6 +16,7 @@
+ #include <iostream>
+ #include <strstream>
+ #include <string>
++#include <cstdlib>
+ 
+ #include <sys/types.h>
+ #include <unistd.h>
