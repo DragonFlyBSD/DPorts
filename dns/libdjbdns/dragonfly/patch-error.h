a37f91421350ac7a58b8118653d5cf6c8162c653
DPorts Builder <nobody@home.ok>
2016-06-16 00:00:53 -0700

    Import dns/libdjbdns version 1.05_2

---
+++ dns/libdjbdns/dragonfly/patch-error.h
@@ -0,0 +1,14 @@
+--- error.h.orig	2002-12-06 17:42:23.000000000 +0200
++++ error.h
+@@ -1,7 +1,11 @@
+ #ifndef ERROR_H
+ #define ERROR_H
+ 
++#ifdef __DragonFly__
++#include <errno.h>
++#else
+ extern int errno;
++#endif
+ 
+ extern int error_intr;
+ extern int error_nomem;
