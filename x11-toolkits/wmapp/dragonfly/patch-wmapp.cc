2af23b72a704986994ac383afca83424a7d0d6e9
DPorts Builder <nobody@home.ok>
2016-06-16 15:29:59 -0700

    Import x11-toolkits/wmapp version 0.0.4.3_2

---
+++ x11-toolkits/wmapp/dragonfly/patch-wmapp.cc
@@ -0,0 +1,8 @@
+--- wmapp.cc.orig	2006-01-09 15:23:35.000000000 +0200
++++ wmapp.cc
+@@ -1,4 +1,5 @@
+ #include <string>
++#include <cstring>
+ #include "wmapp.h"
+ #include "wmwindow.h"
+ 
---
+++ x11-toolkits/wmapp/dragonfly/patch-wmwindow.cc
@@ -0,0 +1,8 @@
+--- wmwindow.cc.orig	2003-01-05 03:17:03.000000000 +0200
++++ wmwindow.cc
+@@ -1,4 +1,5 @@
+ #include <iostream>
++#include <cstring>
+ #include "wmwindow.h"
+ #include "wmapp.h"
+ 
