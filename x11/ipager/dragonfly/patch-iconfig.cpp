cd6ff707a938255f183ee44225c9a386ea9c7507
DPorts Builder <nobody@home.ok>
2016-06-18 12:48:13 -0700

    Import x11/ipager version 1.1.0_9

---
+++ x11/ipager/dragonfly/patch-iconfig.cpp
@@ -0,0 +1,10 @@
+--- iconfig.cpp.orig	2005-06-08 20:41:59.000000000 +0300
++++ iconfig.cpp
+@@ -30,6 +30,7 @@
+ #include <iostream>
+ #include <fstream>
+ #include <sstream>
++#include <cstdlib>
+ 
+ #include <sys/stat.h>
+ #include <sys/types.h>
