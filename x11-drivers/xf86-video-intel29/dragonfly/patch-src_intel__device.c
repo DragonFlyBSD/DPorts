335ba60578b47ce9790c2ec96928fbbba65be4a3
DPorts Builder <nobody@home.ok>
2016-06-22 06:34:55 -0700

    Update x11-drivers/xf86-video-intel29 to version 2.99.2016.06.01,2

--- x11-drivers/xf86-video-intel29/dragonfly/patch-src_intel__device.c
+++ x11-drivers/xf86-video-intel29/dragonfly/patch-src_intel__device.c
@@ -1,11 +1,13 @@
---- src/intel_device.c.orig	2015-09-09 05:41:54 UTC
+Force i915.ko loading?
+
+--- src/intel_device.c.orig	2016-04-02 11:03:57.000000000 +0300
 +++ src/intel_device.c
-@@ -404,7 +404,7 @@ static int __intel_open_device__legacy(c
+@@ -425,7 +425,7 @@ static int __intel_open_device__legacy(c
  		 pci->domain, pci->bus, pci->dev, pci->func);
  
  	ret = drmCheckModesettingSupported(id);
 -	if (ret) {
 +	if (ret || 1) {
- 		if (xf86LoadKernelModule("i915"))
+ 		if (load_i915_kernel_module() == 0)
  			ret = drmCheckModesettingSupported(id);
  		if (ret)
