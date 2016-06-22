0cbe31ec6a090ffbfccb8214f7fc6042750e3408
DPorts Builder <nobody@home.ok>
2016-06-22 08:56:57 -0700

    Tweak security/gnupg1 version 1.4.20

--- security/gnupg1/files/patch-security_gnupg1_files_patch-g10-trustdb.c
+++ security/gnupg1/files/patch-security_gnupg1_files_patch-g10-trustdb.c
@@ -1,10 +1,10 @@
 Index: security/gnupg1/files/patch-g10-trustdb.c
 ===================================================================
---- security/gnupg1/files/patch-g10-trustdb.c	(revision 0)
-+++ security/gnupg1/files/patch-g10-trustdb.c	(working copy)
+--- security/gnupg1/files/patch-g10-trustdb.c.orig	2016-06-17 21:55:32 UTC
++++ security/gnupg1/files/patch-g10-trustdb.c
 @@ -0,0 +1,10 @@
 +--- g10/trustdb.c.orig	2013-12-13 08:28:22.000000000 +0100
-++++ g10/trustdb.c	2014-05-17 19:47:21.125216457 +0200
+++++ g10/trustdb.c
 +@@ -927,6 +927,7 @@
 +   TRUSTREC rec;
 +   int rc;
