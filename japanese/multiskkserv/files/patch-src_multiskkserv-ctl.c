aed24192f92b111ae3b8a611b6558256676d0140
DPorts Builder <nobody@home.ok>
2016-06-23 12:20:18 -0700

    Tweak japanese/multiskkserv version 0.20020201_1

--- japanese/multiskkserv/files/patch-src_multiskkserv-ctl.c
+++ japanese/multiskkserv/files/patch-src_multiskkserv-ctl.c
@@ -1,6 +1,6 @@
---- src/multiskkserv-ctl.c.orig	Sun Jan 13 06:10:13 2002
-+++ src/multiskkserv-ctl.c	Fri Aug 13 19:50:45 2004
-@@ -168,7 +168,7 @@
+--- src/multiskkserv-ctl.c.orig	2002-01-12 21:10:13 UTC
++++ src/multiskkserv-ctl.c
+@@ -168,7 +168,7 @@ show_stat(char *remote, int port, int fa
    char rbuf[SKKSERV_REQUEST_SIZE];
  
    if ((sock = socket_connect(remote, &sstr, port, (char *)SKKSERV_SERVICE, family)) < 0) {
