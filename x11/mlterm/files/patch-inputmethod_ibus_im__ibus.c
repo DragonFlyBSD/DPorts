e25254125d0c63eaa14222a7fa135c884a2e185e
DPorts Builder <nobody@home.ok>
2016-06-23 15:53:50 -0700

    Tweak x11/mlterm version 3.7.0

--- x11/mlterm/files/patch-inputmethod_ibus_im__ibus.c
+++ x11/mlterm/files/patch-inputmethod_ibus_im__ibus.c
@@ -1,6 +1,6 @@
---- inputmethod/ibus/im_ibus.c.orig	2011-05-29 01:07:42.000000000 +0900
-+++ inputmethod/ibus/im_ibus.c	2011-05-29 04:57:48.000000000 +0900
-@@ -631,7 +631,7 @@
+--- inputmethod/ibus/im_ibus.c.orig	2016-03-13 10:57:40 UTC
++++ inputmethod/ibus/im_ibus.c
+@@ -1289,7 +1289,7 @@ im_ibus_get_info(
  	}
  
  	result->id = strdup( "ibus") ;
