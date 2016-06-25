a4ce920ff93f1d9c04965dda5f2ed2115733f1ee
DPorts Builder <nobody@home.ok>
2016-06-25 03:18:01 -0700

    Import audio/spiralloops version 2.0.0_9

---
+++ audio/spiralloops/dragonfly/patch-SpiralSound_Sample.h
@@ -0,0 +1,10 @@
+--- SpiralSound/Sample.h.intermediate	2016-06-24 18:06:27.000000000 +0300
++++ SpiralSound/Sample.h
+@@ -21,6 +21,7 @@
+ 
+ #include <assert.h>
+ #include <iostream>
++#include <cstring>
+ #include "SpiralInfo.h"
+ 
+ static const unsigned short UMAX_LEV  = 65535;
