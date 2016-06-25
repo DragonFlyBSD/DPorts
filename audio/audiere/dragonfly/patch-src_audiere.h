7744e60464b1f3f5bd4ed720469b0938df72a124
DPorts Builder <nobody@home.ok>
2016-06-25 03:18:07 -0700

    Import audio/audiere version 1.9.4_6

---
+++ audio/audiere/dragonfly/patch-src_audiere.h
@@ -0,0 +1,10 @@
+--- src/audiere.h.orig	2006-02-14 06:57:01.000000000 +0200
++++ src/audiere.h
+@@ -28,6 +28,7 @@
+ 
+ #include <vector>
+ #include <string>
++#include <cstring>
+ 
+ #ifdef _MSC_VER
+ #pragma warning(disable : 4786)
