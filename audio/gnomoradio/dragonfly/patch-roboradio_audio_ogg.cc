ce9a04b20a93c600897ca53f577771af98fdb44f
DPorts Builder <nobody@home.ok>
2016-06-25 03:18:27 -0700

    Import audio/gnomoradio version 0.15.1_19

---
+++ audio/gnomoradio/dragonfly/patch-roboradio_audio_ogg.cc
@@ -0,0 +1,10 @@
+--- roboradio/audio/ogg.cc.intermediate	2016-06-24 19:06:15.000000000 +0300
++++ roboradio/audio/ogg.cc
+@@ -27,6 +27,7 @@
+ #include <algorithm>
+ 
+ #include <iostream>
++#include <cstring>
+ 
+ #if USE_VORBIS
+ 
