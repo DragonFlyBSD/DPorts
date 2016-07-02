a5cbe3a172c08a3ce80f89e6c76a4c57a5caca94
DPorts Builder <nobody@home.ok>
2016-07-02 04:39:47 -0700

    Import net-p2p/jigdo version 0.7.3_8

---
+++ net-p2p/jigdo/dragonfly/patch-src_util_md5sum.hh
@@ -0,0 +1,10 @@
+--- src/util/md5sum.hh.orig	2005-04-10 02:09:52.000000000 +0300
++++ src/util/md5sum.hh
+@@ -31,6 +31,7 @@
+ #include <cstdlib>
+ #include <iosfwd>
+ #include <string>
++#include <cstring>
+ 
+ #include <bstream.hh>
+ #include <debug.hh>
---
+++ net-p2p/jigdo/dragonfly/patch-src_util_string.hh
@@ -0,0 +1,10 @@
+--- src/util/string.hh.orig	2005-04-10 02:09:52.000000000 +0300
++++ src/util/string.hh
+@@ -20,6 +20,7 @@
+ #include <config.h>
+ 
+ #include <string>
++#include <cstring>
+ //______________________________________________________________________
+ 
+ /** Convert a number x to characters and append to a string. */
