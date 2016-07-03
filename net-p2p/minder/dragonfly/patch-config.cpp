68e43f0985960fa0409c42f58994c8cd03265bbc
DPorts Builder <nobody@home.ok>
2016-07-02 17:39:40 -0700

    Import net-p2p/minder version 2.0.2_1

---
+++ net-p2p/minder/dragonfly/patch-config.cpp
@@ -0,0 +1,10 @@
+--- config.cpp.orig	2004-04-18 15:29:53.000000000 +0300
++++ config.cpp
+@@ -24,6 +24,7 @@ Foundation, Inc., 59 Temple Place - Suit
+ #pragma warning(disable:4786)
+ #endif
+ #include <stdio.h>
++#include <cstring>
+ #include "Parse.h"
+ 
+ #include "config.h"
