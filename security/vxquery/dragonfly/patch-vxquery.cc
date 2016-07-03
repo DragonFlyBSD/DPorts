f81ba0f3689a0f9533a5505615de4b4296ecbacd
DPorts Builder <nobody@home.ok>
2016-07-02 19:28:19 -0700

    Import security/vxquery version 0.2.1

---
+++ security/vxquery/dragonfly/patch-vxquery.cc
@@ -0,0 +1,10 @@
+--- vxquery.cc	2004-04-06 02:38:29.000000000 +0300
++++ vxquery.cc
+@@ -36,6 +36,7 @@
+ #include <memory>
+ #include <stack>
+ #include <string>
++#include <cstring>
+ #include <vector>
+ 
+ #include <vuxml/vuxml.hh>
