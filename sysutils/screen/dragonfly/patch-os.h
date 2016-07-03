c0d4385fd815908b8ff34ae18193a650974f77ff
DPorts Builder <nobody@home.ok>
2016-07-03 03:51:10 -0700

    Update sysutils/screen to version 4.4.0_1

--- sysutils/screen/dragonfly/patch-os.h
+++ sysutils/screen/dragonfly/patch-os.h
@@ -1,10 +1,10 @@
---- os.h.orig	2015-08-05 16:00:47 UTC
+--- os.h.orig	2016-07-03 10:31:48 UTC
 +++ os.h
 @@ -250,11 +250,8 @@ extern int errno;
  #endif
  
  #if defined(UTMPOK) || defined(BUGGYGETLOGIN)
--# if defined(SVR4) && !defined(DGUX) && !defined(__hpux)
+-# if defined(SVR4) && !defined(DGUX) && !defined(__hpux) && !defined(linux)
 +# if (defined(SVR4) && !defined(DGUX)) || defined(__DragonFly__)
  #  include <utmpx.h>
 -#  ifdef UTMPX_FILE /* GNU extension */
@@ -13,7 +13,7 @@
  #  define utmp		utmpx
  #  define getutent	getutxent
  #  define getutid	getutxid
-@@ -276,15 +273,11 @@ extern int errno;
+@@ -277,15 +274,11 @@ extern int errno;
  # endif /* apollo */
  
  # ifndef UTMPFILE
