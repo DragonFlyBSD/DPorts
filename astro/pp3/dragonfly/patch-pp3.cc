ec9a9ef4cf2815590a1d0a01670c4cfd921fb9a9
DPorts Builder <nobody@home.ok>
2016-07-02 22:48:59 -0700

    Import astro/pp3 version 1.3.3_6

---
+++ astro/pp3/dragonfly/patch-pp3.cc
@@ -0,0 +1,10 @@
+--- pp3.cc.orig	2004-08-14 19:45:08.000000000 +0300
++++ pp3.cc
+@@ -15,6 +15,7 @@
+ #include <cstdlib> 
+ #include <cmath> 
+ #include <cfloat> 
++#include <cstring>
+ 
+ using namespace std;
+ 
