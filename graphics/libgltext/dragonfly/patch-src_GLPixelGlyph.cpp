51ee28b601700a7c0551a6d3303ef4f002094ba8
DPorts Builder <nobody@home.ok>
2016-07-11 09:38:54 -0700

    Import graphics/libgltext version 0.3.1_7

---
+++ graphics/libgltext/dragonfly/patch-src_GLPixelGlyph.cpp
@@ -0,0 +1,10 @@
+--- src/GLPixelGlyph.cpp.orig	2003-03-15 06:52:59.000000000 +0200
++++ src/GLPixelGlyph.cpp
+@@ -29,6 +29,7 @@
+  ************************************************************ gltext-cpr-end */
+ #include "GLPixelGlyph.h"
+ #include "OpenGL.h"
++#include <cstring>
+ 
+ namespace gltext
+ {
