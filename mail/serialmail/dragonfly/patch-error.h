e80a38525ab92a9e75a07694aa6b2860cfd2bd9d
DPorts Builder <nobody@home.ok>
2016-07-11 09:40:11 -0700

    Import mail/serialmail version 0.75_3

---
+++ mail/serialmail/dragonfly/patch-error.h
@@ -0,0 +1,14 @@
+--- error.h.orig	1998-11-15 03:28:23.000000000 +0100
++++ error.h
+@@ -1,7 +1,11 @@
+ #ifndef ERROR_H
+ #define ERROR_H
+ 
++#ifdef __DragonFly__
++#include <errno.h>
++#else
+ extern int errno;
++#endif
+ 
+ extern int error_intr;
+ extern int error_nomem;
---
+++ mail/serialmail/dragonfly/patch-find-systype.sh
@@ -0,0 +1,11 @@
+--- find-systype.sh.orig	1998-11-15 03:28:23.000000000 +0100
++++ find-systype.sh
+@@ -32,7 +32,7 @@ then
+     chip="`sysctl -n hw.model`"
+     kern=""
+     ;;
+-  freebsd)
++  freebsd|dragonfly)
+     # see above about bsd 4.4
+     oper="$sys-$unamer"
+     arch="$unamem"
