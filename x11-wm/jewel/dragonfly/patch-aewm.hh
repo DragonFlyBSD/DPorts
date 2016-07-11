204f755712f5c8d19d7bef004782ff94d8c0807a
DPorts Builder <nobody@home.ok>
2016-07-11 09:36:41 -0700

    Import x11-wm/jewel version 0.12.41_4

---
+++ x11-wm/jewel/dragonfly/patch-aewm.hh
@@ -0,0 +1,10 @@
+--- aewm.hh.orig	2005-01-03 23:54:59.000000000 +0200
++++ aewm.hh
+@@ -34,6 +34,7 @@
+ #include <stdlib.h>
+ #include <iostream>
+ #include <string>
++#include <cstring>
+ 
+ #include <list>
+ #include <algorithm>
