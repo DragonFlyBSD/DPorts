58dada3f42f2a90a4b63debc3ca171c53de627b4
DPorts Builder <nobody@home.ok>
2016-07-11 10:27:00 -0700

    Import japanese/scim-honoka version 0.9.1_12

---
+++ japanese/scim-honoka/dragonfly/patch-src_honokasetupgtk.h
@@ -0,0 +1,10 @@
+--- src/honokasetupgtk.h.orig	2005-12-14 08:14:36.000000000 +0200
++++ src/honokasetupgtk.h
+@@ -29,6 +29,7 @@
+ #include <vector>
+ #include <cstdio>
+ #include <cstdlib>
++#include <cstring>
+ #include <honokasetupcore.h>
+ 
+ 
