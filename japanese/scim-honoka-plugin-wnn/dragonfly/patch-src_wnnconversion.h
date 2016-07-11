2539d8c9a460ea35c1ebf1830564676d79398ee4
DPorts Builder <nobody@home.ok>
2016-07-11 11:46:10 -0700

    Import japanese/scim-honoka-plugin-wnn version 0.9.0_13

---
+++ japanese/scim-honoka-plugin-wnn/dragonfly/patch-src_wnnconversion.h
@@ -0,0 +1,10 @@
+--- src/wnnconversion.h.orig	2006-05-22 06:38:28.000000000 +0300
++++ src/wnnconversion.h
+@@ -26,6 +26,7 @@
+ 
+ #include <cstdio>
+ #include <cstdlib>
++#include <cstring>
+ #include <vector>
+ #include <wnnproto.h>
+ #define Uses_SCIM_ICONV
