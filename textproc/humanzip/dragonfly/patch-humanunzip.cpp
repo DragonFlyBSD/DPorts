e27b0d92b1aefd4f392a7be14f63d9b45979c44c
DPorts Builder <nobody@home.ok>
2016-07-13 11:10:22 -0700

    Import textproc/humanzip version 0.5

---
+++ textproc/humanzip/dragonfly/patch-humanunzip.cpp
@@ -0,0 +1,10 @@
+--- humanunzip.cpp.intermediate	2016-07-13 20:01:01 UTC
++++ humanunzip.cpp
+@@ -22,6 +22,7 @@ using namespace std;
+ #include <fstream>
+ #include <sstream>
+ #include <vector>
++#include <cstdlib>
+ #include <unistd.h>
+ #include <sys/stat.h>
+ #include "humanzip.h"
---
+++ textproc/humanzip/dragonfly/patch-humanzip.cpp
@@ -0,0 +1,11 @@
+--- humanzip.cpp.intermediate	2016-07-13 19:57:50 UTC
++++ humanzip.cpp
+@@ -24,6 +24,8 @@ using namespace std;
+ #include <getopt.h>
+ #include <vector>
+ #include <iomanip>
++#include <cstdlib>
++#include <algorithm>
+ #include <unistd.h>
+ #include <sys/stat.h>
+ #include "humanzip.h"
