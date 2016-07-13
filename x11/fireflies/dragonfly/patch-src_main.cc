ac32934ddc2622bb2f5fbdff31a4eaf314110151
DPorts Builder <nobody@home.ok>
2016-07-13 11:13:11 -0700

    Import x11/fireflies version 2.07_6

---
+++ x11/fireflies/dragonfly/patch-src_main.cc
@@ -0,0 +1,10 @@
+--- src/main.cc.intermediate	2016-07-13 17:04:27 UTC
++++ src/main.cc
+@@ -11,6 +11,7 @@
+ 
+ #include <iostream>
+ #include <stdlib.h>
++#include <cstring>
+ 
+ #ifdef WIN32
+ #include <windows.h>
