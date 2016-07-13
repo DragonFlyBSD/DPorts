8a6c276f2a1a8698fa5d31b98bfbe65889264644
DPorts Builder <nobody@home.ok>
2016-07-13 11:12:36 -0700

    Import biology/chemeq version 1.50_2

---
+++ biology/chemeq/dragonfly/patch-src_chemeq.h
@@ -0,0 +1,11 @@
+--- src/chemeq.h.orig	2003-03-12 00:36:25.000000000 +0200
++++ src/chemeq.h
+@@ -6,6 +6,8 @@
+ #include <iostream>
+ #include <vector>
+ #include <string>
++#include <cstdlib>
++#include <cstring>
+ #include <map>
+ 
+ typedef struct {
