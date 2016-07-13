342fb93e5b7ededb6083c252444e724f91b86c1e
DPorts Builder <nobody@home.ok>
2016-07-13 11:09:09 -0700

    Import devel/qprog version 0.4_4

---
+++ devel/qprog/dragonfly/patch-src_chipinfo.cc
@@ -0,0 +1,10 @@
+--- src/chipinfo.cc.orig	2007-06-17 08:03:19.000000000 +0300
++++ src/chipinfo.cc
+@@ -13,6 +13,7 @@
+ */
+ 
+ #include <iostream>
++#include <cstdlib>
+ 
+ #include "chipinfo.h"
+ 
