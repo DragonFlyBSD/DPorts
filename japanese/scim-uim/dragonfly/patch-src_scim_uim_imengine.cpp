bb70b70a60e3dcc32fbbbde5d49e2c96cb168871
DPorts Builder <nobody@home.ok>
2016-07-13 11:12:17 -0700

    Import japanese/scim-uim version 0.2.0_11

---
+++ japanese/scim-uim/dragonfly/patch-src_scim_uim_imengine.cpp
@@ -0,0 +1,11 @@
+--- src/scim_uim_imengine.cpp.intermediate	2016-07-13 19:19:33 UTC
++++ src/scim_uim_imengine.cpp
+@@ -32,6 +32,8 @@
+   #include <config.h>
+ #endif
+ 
++#include <cstring>
++
+ #include <scim.h>
+ #include "scim_uim_imengine.h"
+ #include <uim/uim-scm.h>
