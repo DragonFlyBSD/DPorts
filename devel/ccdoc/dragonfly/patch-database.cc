655b8974f4e1ef5c6e5e91451f685cf37fa874f2
DPorts Builder <nobody@home.ok>
2016-07-13 11:09:48 -0700

    Import devel/ccdoc version 0.8r41

---
+++ devel/ccdoc/dragonfly/patch-database.cc
@@ -0,0 +1,10 @@
+--- database.cc.orig	2004-10-06 19:35:47.000000000 +0300
++++ database.cc
+@@ -39,6 +39,7 @@ namespace {
+ #include <iomanip>
+ #include <fstream>
+ #include <cstdio>
++#include <cstring>
+ 
+ // ================================================================
+ // Constructor.
---
+++ devel/ccdoc/dragonfly/patch-phase1_scanner.cc
@@ -0,0 +1,10 @@
+--- phase1_scanner.cc.orig	2004-10-06 19:35:47.000000000 +0300
++++ phase1_scanner.cc
+@@ -29,6 +29,7 @@
+ #include "log.h"
+ #include "phase1_scanner.h"
+ #include <cstdio>
++#include <cstring>
+ 
+ // ================================================================
+ // This variable allows the header version
