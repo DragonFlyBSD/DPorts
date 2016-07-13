a151b144b33c99282648371d2bea837f5da78687
DPorts Builder <nobody@home.ok>
2016-07-13 11:13:55 -0700

    Import audio/Maaate version 0.3.1_5

---
+++ audio/Maaate/dragonfly/patch-src_tier2_segmentData.cc
@@ -0,0 +1,11 @@
+--- src/tier2/segmentData.cc.orig	2002-10-09 03:22:06.000000000 +0200
++++ src/tier2/segmentData.cc
+@@ -25,6 +25,8 @@
+ //#include <curses.h> //for getchar()
+ #include <cfloat>
+ #include <iostream>
++#include <cstdlib>
++#include <cstring>
+ #include "segmentData.H"
+ 
+ // constructor
