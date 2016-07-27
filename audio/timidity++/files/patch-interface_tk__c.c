d72b52bc0fab9bee34d967c30bff7a3298aa2b7e
DPorts Builder <nobody@home.ok>
2016-07-27 05:13:47 -0700

    Tweak audio/timidity++ version 2.14.0_2

--- audio/timidity++/files/patch-interface_tk__c.c
+++ audio/timidity++/files/patch-interface_tk__c.c
@@ -1,6 +1,6 @@
---- interface/tk_c.c.orig
+--- interface/tk_c.c.orig	2011-12-03 15:49:19 UTC
 +++ interface/tk_c.c
-@@ -913,7 +913,7 @@
+@@ -913,7 +913,7 @@ static char *v_eval(char *fmt, ...)
  	vsnprintf(buf, sizeof(buf), fmt, ap);
  	Tcl_Eval(my_interp, buf);
  	va_end(ap);
