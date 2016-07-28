586bbcbb38821b7e120566a7f017e19a8cba4473
DPorts Builder <nobody@home.ok>
2016-07-27 17:47:45 -0700

    Tweak audio/extace version 1.9.9_9

--- audio/extace/files/patch-src_color__win.c
+++ audio/extace/files/patch-src_color__win.c
@@ -1,6 +1,6 @@
---- src/color_win.c.orig	Wed Feb 26 01:22:19 2003
-+++ src/color_win.c	Fri May 23 18:26:18 2003
-@@ -579,12 +579,12 @@
+--- src/color_win.c.orig	2008-01-22 23:59:52 UTC
++++ src/color_win.c
+@@ -620,12 +620,12 @@ void grad_win_create()
  		button = gtk_button_new_with_label("Set Color");
  		gtk_box_pack_start(GTK_BOX(hbox),button,TRUE,TRUE,0);
  		gtk_signal_connect(GTK_OBJECT(button), "clicked",
@@ -15,7 +15,7 @@
  
  		hbox = gtk_hbox_new(TRUE,0);
  		gtk_box_pack_start(GTK_BOX(vbox),hbox,FALSE,FALSE,0);
-@@ -592,12 +592,12 @@
+@@ -633,12 +633,12 @@ void grad_win_create()
  		button = gtk_button_new_with_label("Save");
  		gtk_box_pack_start(GTK_BOX(hbox),button,TRUE,TRUE,0);
  		gtk_signal_connect(GTK_OBJECT(button), "clicked",
