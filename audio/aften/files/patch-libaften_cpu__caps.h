44f5e3489db21a327d0ec90cd441000310da6f3c
DPorts Builder <nobody@home.ok>
2016-07-27 20:34:40 -0700

    Tweak audio/aften version 0.0.8

--- audio/aften/files/patch-libaften_cpu__caps.h
+++ audio/aften/files/patch-libaften_cpu__caps.h
@@ -1,5 +1,5 @@
---- ./libaften/cpu_caps.h.orig	2007-08-11 18:13:25.000000000 +0200
-+++ ./libaften/cpu_caps.h	2010-04-03 00:31:40.000000000 +0200
+--- libaften/cpu_caps.h.orig	2007-08-11 16:13:25 UTC
++++ libaften/cpu_caps.h
 @@ -26,6 +26,7 @@
  #include "ppc_cpu_caps.h"
  #else
