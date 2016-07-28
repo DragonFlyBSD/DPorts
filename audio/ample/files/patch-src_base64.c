06856ea1c0c650c2fe714283e47b062bd1e72a82
DPorts Builder <nobody@home.ok>
2016-07-27 20:33:56 -0700

    Tweak audio/ample version 0.5.7

--- audio/ample/files/patch-src_base64.c
+++ audio/ample/files/patch-src_base64.c
@@ -1,5 +1,5 @@
---- src/base64.c.orig	Tue Nov 25 18:28:56 2003
-+++ src/base64.c	Sat Jul  3 22:38:53 2004
+--- src/base64.c.orig	2003-11-25 09:28:56 UTC
++++ src/base64.c
 @@ -18,6 +18,8 @@
   * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
   */
