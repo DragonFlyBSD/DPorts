584d6b1e4a64517b3f71a3fa8b49fc249590d2a0
DPorts Builder <nobody@home.ok>
2016-07-27 18:06:31 -0700

    Tweak audio/lmms version 1.1.3,2

--- audio/lmms/files/patch-plugins_sf2__player_sf2__player.cpp
+++ audio/lmms/files/patch-plugins_sf2__player_sf2__player.cpp
@@ -1,5 +1,5 @@
---- plugins/sf2_player/sf2_player.cpp.orig	2011-07-02 13:14:01.000000000 +0000
-+++ plugins/sf2_player/sf2_player.cpp	2011-09-29 12:18:02.000000000 +0000
+--- plugins/sf2_player/sf2_player.cpp.orig	2015-03-08 02:47:14 UTC
++++ plugins/sf2_player/sf2_player.cpp
 @@ -23,6 +23,10 @@
   *
   */
@@ -9,5 +9,5 @@
 +	#define log2(x)             ( log (x) * (1./M_LN2) )
 +#endif
  #include <QtCore/QDebug>
- #include <QtCore/QTextStream>
  #include <QtGui/QLayout>
+ #include <QtGui/QLabel>
