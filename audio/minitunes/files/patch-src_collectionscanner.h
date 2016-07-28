d508c4b41bbde24a8c6292c959484c9add5f6a78
DPorts Builder <nobody@home.ok>
2016-07-27 19:12:49 -0700

    Tweak audio/minitunes version 0.1.1_3

--- audio/minitunes/files/patch-src_collectionscanner.h
+++ audio/minitunes/files/patch-src_collectionscanner.h
@@ -1,5 +1,5 @@
---- src/collectionscanner.h.orig	2010-07-05 18:02:28.000000000 +0800
-+++ src/collectionscanner.h	2010-07-06 16:08:43.000000000 +0800
+--- src/collectionscanner.h.orig	2010-07-13 10:30:19 UTC
++++ src/collectionscanner.h
 @@ -6,8 +6,8 @@
  #include "model/album.h"
  
