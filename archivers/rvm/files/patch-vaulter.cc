1a8f7a4dd32e0f206c578676cc92591d63bcd2b6
DPorts Builder <nobody@home.ok>
2016-07-27 17:44:55 -0700

    Tweak archivers/rvm version 1.11

--- archivers/rvm/files/patch-vaulter.cc
+++ archivers/rvm/files/patch-vaulter.cc
@@ -1,10 +1,10 @@
---- vaulter.cc.orig	Wed Mar 24 05:03:42 2004
-+++ vaulter.cc	Sat Apr  3 21:24:37 2004
+--- vaulter.cc.orig	2014-11-12 13:09:24 UTC
++++ vaulter.cc
 @@ -4,6 +4,7 @@
  #include <vector>
  #include <map>
  #include <string>
 +#include <cassert>
  
- #include "debug.h"
+ #include "asserts.h"
  #include "error.h"
