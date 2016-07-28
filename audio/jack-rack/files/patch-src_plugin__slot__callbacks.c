6e066a7e62c4b0dc3696d4490caf0f81f9347ab6
DPorts Builder <nobody@home.ok>
2016-07-27 21:05:28 -0700

    Tweak audio/jack-rack version 1.4.7_7

--- audio/jack-rack/files/patch-src_plugin__slot__callbacks.c
+++ audio/jack-rack/files/patch-src_plugin__slot__callbacks.c
@@ -1,6 +1,6 @@
---- src/plugin_slot_callbacks.c.orig	2008-08-27 16:45:43.000000000 +0200
-+++ src/plugin_slot_callbacks.c	2008-08-27 16:49:19.000000000 +0200
-@@ -109,6 +109,7 @@
+--- src/plugin_slot_callbacks.c.orig	2007-11-18 13:13:49 UTC
++++ src/plugin_slot_callbacks.c
+@@ -109,6 +109,7 @@ slot_ablise_cb (GtkWidget * button, GdkE
  	    
  	      ui = plugin_slot->jack_rack->ui;
  	    
@@ -8,7 +8,7 @@
  	      g_object_set_data (G_OBJECT (ui->midi_menu_item), 
  			       "jack-rack-ctrl-type",
  			       GINT_TO_POINTER(2/*PLUGIN_ENABLE_CONTROL*/)); 
-@@ -117,6 +118,7 @@
+@@ -117,6 +118,7 @@ slot_ablise_cb (GtkWidget * button, GdkE
  			       plugin_slot);
  	      gtk_menu_popup (GTK_MENU (ui->midi_menu), NULL, NULL, NULL, NULL, event->button, event->time);
  	    
