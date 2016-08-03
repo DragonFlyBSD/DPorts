982dce9aa9af8024ac1e2c8e6796aeaaac22d516
DPorts Builder <nobody@home.ok>
2016-08-03 10:53:58 -0700

    Import sysutils/lineak-xosdplugin version 0.9_10

---
+++ sysutils/lineak-xosdplugin/dragonfly/patch-xosd__plugin_xosdctrl.h
@@ -0,0 +1,11 @@
+--- xosd_plugin/xosdctrl.h.orig	2006-02-08 20:32:13.000000000 +0200
++++ xosd_plugin/xosdctrl.h
+@@ -28,6 +28,8 @@ extern "C" {
+ }
+ #include <lineak/displayctrl.h>
+ #include <string>
++#include <cstring>
++#include <cstdlib>
+ #include <iostream>
+ 
+ class LConfig;
