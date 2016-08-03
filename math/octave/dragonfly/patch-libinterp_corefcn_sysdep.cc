85a14500493821a056791193f2a7d1d475cf49f4
DPorts Builder <nobody@home.ok>
2016-08-03 15:58:30 -0700

    Update math/octave to version 4.0.3

--- math/octave/dragonfly/patch-libinterp_corefcn_sysdep.cc
+++ math/octave/dragonfly/patch-libinterp_corefcn_sysdep.cc
@@ -1,6 +1,6 @@
---- libinterp/corefcn/sysdep.cc.orig	2013-12-27 21:57:41.000000000 +0000
+--- libinterp/corefcn/sysdep.cc.orig	2016-04-20 18:23:56 UTC
 +++ libinterp/corefcn/sysdep.cc
-@@ -88,7 +88,8 @@ along with Octave; see the file COPYING.
+@@ -92,7 +92,8 @@ along with Octave; see the file COPYING.
  #define STDIN_FILENO 1
  #endif
  
@@ -10,13 +10,12 @@
  static void
  BSD_init (void)
  {
-@@ -256,7 +257,8 @@ same_file_internal (const std::string& f
- void
- sysdep_init (void)
- {
+@@ -318,7 +319,7 @@ sysdep_init (void)
+ #endif
+ #endif
+ 
 -#if defined (__386BSD__) || defined (__FreeBSD__) || defined (__NetBSD__)
-+#if defined (__386BSD__) || defined (__FreeBSD__) \
-+ || defined (__NetBSD__) || defined (__DragonFly__)
++#if defined (__386BSD__) || defined (__FreeBSD__) || defined (__NetBSD__) || defined __DragonFly__
    BSD_init ();
  #elif defined (__MINGW32__)
    MINGW_init ();
