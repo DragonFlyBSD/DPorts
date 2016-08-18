170ee3da23db262e095866e050b61a4fd0e454d2
DPorts Builder <nobody@home.ok>
2016-08-18 13:10:27 -0700

    Import devel/sope3 version 3.1.4

---
+++ devel/sope3/dragonfly/patch-sope-core_NGStreams_NGLocalSocketDomain.m
@@ -0,0 +1,11 @@
+--- sope-core/NGStreams/NGLocalSocketDomain.m.orig	2016-07-07 07:02:47.000000000 +0300
++++ sope-core/NGStreams/NGLocalSocketDomain.m
+@@ -25,7 +25,7 @@
+ #include "NGLocalSocketAddress.h"
+ #include "NGSocket.h"
+ 
+-#if defined(__APPLE__) || defined(__FreeBSD__)
++#if defined(__APPLE__) || defined(__FreeBSD__) || defined(__DragonFly__)
+ #  include <sys/types.h>
+ #  include <sys/socket.h>
+ #else
