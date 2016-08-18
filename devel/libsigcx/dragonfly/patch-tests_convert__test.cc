ba857a6e3057bd15ef32a329bb15d0834ddab780
DPorts Builder <nobody@home.ok>
2016-08-17 23:55:01 -0700

    Import devel/libsigcx version 0.6.6_8

---
+++ devel/libsigcx/dragonfly/patch-tests_convert__test.cc
@@ -0,0 +1,9 @@
+--- tests/convert_test.cc.orig	2002-08-29 19:20:00.000000000 +0200
++++ tests/convert_test.cc
+@@ -1,5 +1,6 @@
+ #include <string>
+ #include <iostream>
++#include <cstring>
+ #include <sigcx/convert.h>
+ 
+ /*
