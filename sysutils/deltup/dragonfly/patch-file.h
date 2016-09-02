3ad6da268c9edfc2c80694b6c0568ebc10ef7e52
DPorts Builder <nobody@home.ok>
2016-09-02 09:55:42 -0700

    Tweak sysutils/deltup version 0.4.4_1

--- sysutils/deltup/dragonfly/patch-file.h
+++ sysutils/deltup/dragonfly/patch-file.h
@@ -1,11 +1,12 @@
 --- file.h.bak	2015-10-03 17:48:42.000000000 +0300
 +++ file.h
-@@ -12,6 +12,8 @@
+@@ -12,6 +12,9 @@
   * Author: John Whitney <jjw@deltup.org>
   */
  
++#include <cstdio>  // for fopen printf stderr etc
 +#include <cstdlib>
-+#include <cstring>
++#include <cstring> // for strlen
  #include <zlib.h>
  
  class IStream {
