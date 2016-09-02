dd6ce9a08b423d37db57a4bcd5571eedf3689e37
DPorts Builder <nobody@home.ok>
2016-09-02 09:57:38 -0700

    Tweak irc/irc2dc version 0.1

---
+++ irc/irc2dc/dragonfly/patch-src_defs.h
@@ -0,0 +1,10 @@
+--- src/defs.h.orig	2009-05-22 16:53:57.000000000 +0300
++++ src/defs.h
+@@ -33,6 +33,7 @@
+ #define __DEFS_H__
+ 
+ #include <iostream>
++#include <cstdio> // for sprintf
+ #include <cstdlib>
+ #include <string>
+ #include <syslog.h>
