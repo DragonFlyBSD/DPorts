5e8b62ca3af03ac3df6810352c5624f0c96868f8
DPorts Builder <nobody@home.ok>
2016-09-02 10:08:18 -0700

    Tweak devel/glrparser version 1.4_1

---
+++ devel/glrparser/dragonfly/patch-glr_glrGrammar.h
@@ -0,0 +1,10 @@
+--- glr/glrGrammar.h.orig	2003-03-26 11:03:08.000000000 +0200
++++ glr/glrGrammar.h
+@@ -6,6 +6,7 @@
+ #include <set>
+ #include <map>
+ #include <deque>
++#include <cstdio> // for EOF
+ 
+ using namespace std;
+ 
