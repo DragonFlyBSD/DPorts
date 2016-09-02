cd63b485541c158a3274a2903d590e8906b6c092
DPorts Builder <nobody@home.ok>
2016-09-02 10:09:22 -0700

    Tweak graphics/panomatic version 0.9.4

---
+++ graphics/panomatic/dragonfly/patch-panomatic_tclap_MultiArg.h
@@ -0,0 +1,10 @@
+--- panomatic/tclap/MultiArg.h.intermediate	2016-09-02 18:20:53 UTC
++++ panomatic/tclap/MultiArg.h
+@@ -25,6 +25,7 @@
+ 
+ #include <string>
+ #include <vector>
++#include <cstdio> // for EOF
+ 
+ #include <tclap/Arg.h>
+ #include <tclap/Constraint.h>
---
+++ graphics/panomatic/dragonfly/patch-panomatic_tclap_ValueArg.h
@@ -0,0 +1,10 @@
+--- panomatic/tclap/ValueArg.h.intermediate	2016-09-02 18:20:53 UTC
++++ panomatic/tclap/ValueArg.h
+@@ -25,6 +25,7 @@
+ 
+ #include <string>
+ #include <vector>
++#include <cstdio> // for EOF
+ 
+ #include <tclap/Arg.h>
+ #include <tclap/Constraint.h>
