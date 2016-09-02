06c4e320eb7c021215bbc1e47a85304b4576f580
DPorts Builder <nobody@home.ok>
2016-09-02 09:58:02 -0700

    Tweak lang/phc version 0.1.7

--- lang/phc/dragonfly/patch-generated_ast.h
+++ lang/phc/dragonfly/patch-generated_ast.h
@@ -1,10 +1,11 @@
 --- generated/ast.h.orig	2006-09-08 15:51:52.000000000 +0300
 +++ generated/ast.h
-@@ -8,6 +8,8 @@
+@@ -8,6 +8,9 @@
  #include <iostream>
  #include <sstream>
  #include <iomanip>
 +#include <cstring>
++#include <cstdio>  // for printf
 +#include <cstdlib>
  #include <phc/lib/error.h>
  #include <phc/lib/Object.h>
