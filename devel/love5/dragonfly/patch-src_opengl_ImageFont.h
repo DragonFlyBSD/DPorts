bc1791008bb46cab5a12094b42c0f8fa00f23804
DPorts Builder <nobody@home.ok>
2016-09-02 09:57:00 -0700

    Tweak devel/love5 version 0.5.0_15

---
+++ devel/love5/dragonfly/patch-src_opengl_ImageFont.h
@@ -0,0 +1,10 @@
+--- src/opengl/ImageFont.h.orig	2008-09-20 17:40:14.000000000 +0300
++++ src/opengl/ImageFont.h
+@@ -14,6 +14,7 @@
+ 
+ // STD
+ #include <string>
++#include <cstdio> // for printf
+ 
+ namespace love_opengl
+ {
