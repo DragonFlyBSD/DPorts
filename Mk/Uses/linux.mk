bd3be2ea60abb4abcca0452e967ec842c5d7a1cc
DPorts Builder <nobody@home.ok>
2016-09-17 13:10:07 -0700

    Sync Mk with ports

--- Mk/Uses/linux.mk
+++ Mk/Uses/linux.mk
@@ -3,13 +3,17 @@
 .ifndef _INCLUDE_USES_LINUX_MK
 _INCLUDE_USES_LINUX_MK=	yes
 
-.if empty(linux_args)
-linux_args=	c6
+.if empty(linux_ARGS)
+linux_ARGSs=	c6
 .endif
 
 IGNORE=	Linux emulation is not supported on DragonFly
 
 LINUX_ARCH=	x86_64
 PKGNAMEPREFIX?=	linux-${linux_ARGS}-
+
+.if ${PORTNAME} != "${linux_ARGS}"
+BUILD_DEPENDS+=	linux_base-${linux_ARGS}>0:emulators/linux_base-${linux_ARGS}
+.endif
 
 .endif
