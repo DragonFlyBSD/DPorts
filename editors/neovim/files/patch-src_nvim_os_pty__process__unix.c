e292b3151b82e4d3f9f3047d1e05c18462380263
DPorts Builder <nobody@home.ok>
2016-10-17 07:52:41 -0700

    Update editors/neovim to version 0.1.5

--- editors/neovim/files/patch-src_nvim_os_pty__process__unix.c
+++ editors/neovim/files/patch-src_nvim_os_pty__process__unix.c
@@ -1,6 +1,6 @@
---- src/nvim/event/pty_process.c.orig	2016-04-25 01:46:34 UTC
-+++ src/nvim/event/pty_process.c
-@@ -10,7 +10,7 @@
+--- src/nvim/os/pty_process_unix.c.orig	2016-08-22 02:14:28 UTC
++++ src/nvim/os/pty_process_unix.c
+@@ -9,7 +9,7 @@
  #include <sys/ioctl.h>
  
  // forkpty is not in POSIX, so headers are platform-specific
