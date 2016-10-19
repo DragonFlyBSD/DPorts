5e96dc03ead031cae04e94dcdd650b14c386cc6a
DPorts Builder <nobody@home.ok>
2016-10-19 15:56:59 -0700

    Tweak print/dymo-cups-drivers version 1.4.0_1

---
+++ print/dymo-cups-drivers/dragonfly/patch-src_common_CupsPrintEnvironment.h
@@ -0,0 +1,10 @@
+--- src/common/CupsPrintEnvironment.h.orig	2012-02-07 13:22:37 UTC
++++ src/common/CupsPrintEnvironment.h
+@@ -21,6 +21,7 @@
+ #ifndef h952b1c81_8931_433a_8479_7ae6d8e85a86
+ #define h952b1c81_8931_433a_8479_7ae6d8e85a86
+ 
++#include <stdio.h>
+ #include "PrinterDriver.h"
+ 
+ namespace DymoPrinterDriver
