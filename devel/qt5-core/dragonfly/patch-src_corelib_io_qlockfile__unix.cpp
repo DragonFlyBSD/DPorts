9ecef9241a6dfa204099a30695951964a3056e35
DPorts Builder <nobody@home.ok>
2016-11-02 17:21:16 -0700

    Update devel/qt5-core to version 5.6.2

--- devel/qt5-core/dragonfly/patch-src_corelib_io_qlockfile__unix.cpp
+++ devel/qt5-core/dragonfly/patch-src_corelib_io_qlockfile__unix.cpp
@@ -1,11 +1,11 @@
---- src/corelib/io/qlockfile_unix.cpp.orig	2015-10-13 04:35:31 UTC
+--- src/corelib/io/qlockfile_unix.cpp.orig	2016-09-16 05:49:42 UTC
 +++ src/corelib/io/qlockfile_unix.cpp
-@@ -239,7 +239,7 @@ QString QLockFilePrivate::processNameByP
+@@ -266,7 +266,7 @@ QString QLockFilePrivate::processNameByP
      }
      buf[len] = 0;
      return QFileInfo(QFile::decodeName(buf)).fileName();
 -#elif defined(Q_OS_BSD4) && !defined(Q_OS_IOS)
 +#elif defined(Q_OS_BSD4) && !defined(Q_OS_IOS) && 0
- # if defined(__GLIBC__) && defined(__FreeBSD_kernel__)
-     int mib[4] = { CTL_KERN, KERN_PROC, KERN_PROC_PID, pid };
-     size_t len = 0;
+ # if defined(Q_OS_NETBSD)
+     struct kinfo_proc2 kp;
+     int mib[6] = { CTL_KERN, KERN_PROC2, KERN_PROC_PID, (int)pid, sizeof(struct kinfo_proc2), 1 };
