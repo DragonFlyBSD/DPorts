cac4666ed0567bf6a0e9f9dc1a0c34ec2ef7a37a
DPorts Builder <nobody@home.ok>
2016-12-07 11:26:39 -0800

    Update devel/boost-libs to version 1.62.0_1

--- devel/boost-libs/dragonfly/patch-boost_math_tools_config.hpp
+++ devel/boost-libs/dragonfly/patch-boost_math_tools_config.hpp
@@ -4,14 +4,14 @@ these functions long double functions wasn't tested so far for c++ in boost
 and previously wasn't packed anyway (orphans on plist).
 For now explicitly disable them (+ they are slow and unreliable anyway).
 
---- boost/math/tools/config.hpp	2013-04-15 11:47:08.000000000 +0300
+--- boost/math/tools/config.hpp.orig	2016-12-07 15:43:17 UTC
 +++ boost/math/tools/config.hpp
-@@ -23,7 +23,7 @@
+@@ -28,7 +28,7 @@
  
  #include <boost/math/tools/user.hpp>
  
--#if (defined(__CYGWIN__) || defined(__FreeBSD__) || defined(__NetBSD__) \
-+#if (defined(__CYGWIN__) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__DragonFly__) \
+-#if (defined(__CYGWIN__) || defined(__NetBSD__) \
++#if (defined(__CYGWIN__) || defined(__NetBSD__) || defined(__DragonFly__) \
     || (defined(__hppa) && !defined(__OpenBSD__)) || (defined(__NO_LONG_DOUBLE_MATH) && (DBL_MANT_DIG != LDBL_MANT_DIG))) \
     && !defined(BOOST_MATH_NO_LONG_DOUBLE_MATH_FUNCTIONS)
  #  define BOOST_MATH_NO_LONG_DOUBLE_MATH_FUNCTIONS
