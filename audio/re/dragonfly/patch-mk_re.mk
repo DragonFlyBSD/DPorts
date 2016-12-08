ea66f16cb7a1dbf00d3acd49f2b7285eb87c32fe
DPorts Builder <nobody@home.ok>
2016-12-07 20:16:49 -0800

    Update audio/re to version 0.5.0

--- audio/re/dragonfly/patch-mk_re.mk
+++ audio/re/dragonfly/patch-mk_re.mk
@@ -1,6 +1,6 @@
---- mk/re.mk.intermediate	2016-06-24 12:04:28.000000000 +0300
+--- mk/re.mk.intermediate	2016-12-07 17:09:47 UTC
 +++ mk/re.mk
-@@ -268,6 +268,16 @@ ifeq ($(OS),netbsd)
+@@ -269,6 +269,16 @@ ifeq ($(OS),netbsd)
  	AFLAGS		:= cru
  	HAVE_KQUEUE	:= 1
  endif
@@ -17,15 +17,3 @@
  ifeq ($(OS),freebsd)
  	CFLAGS		+= -fPIC -DFREEBSD
  	LFLAGS		+= -fPIC
-@@ -536,9 +546,11 @@ endif
- ifneq ($(HAVE_LIBRESOLV),)
- CFLAGS  += -DHAVE_LIBRESOLV
- ifneq ($(OS),freebsd)
-+ifneq ($(OS),dragonfly)
- LIBS    += -lresolv
- endif
- endif
-+endif
- ifneq ($(HAVE_SYSLOG),)
- CFLAGS  += -DHAVE_SYSLOG
- endif
