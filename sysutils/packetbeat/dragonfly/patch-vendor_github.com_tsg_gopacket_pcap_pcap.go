40bf0c141cdd53ad4dc8bd51b3f57105c3d0bb1c
DPorts Builder <nobody@home.ok>
2016-12-12 21:55:48 -0800

    Update sysutils/packetbeat to version 5.1.1

--- sysutils/packetbeat/dragonfly/patch-vendor_github.com_tsg_gopacket_pcap_pcap.go
+++ sysutils/packetbeat/dragonfly/patch-vendor_github.com_tsg_gopacket_pcap_pcap.go
@@ -1,10 +1,10 @@
---- vendor/github.com/tsg/gopacket/pcap/pcap.go.orig	2016-05-18 15:54:42.000000000 +0300
+--- vendor/github.com/tsg/gopacket/pcap/pcap.go.orig	2016-12-06 07:41:08 UTC
 +++ vendor/github.com/tsg/gopacket/pcap/pcap.go
-@@ -10,6 +10,7 @@ package pcap
+@@ -9,6 +9,7 @@ package pcap
+ 
  /*
  #cgo linux LDFLAGS: -lpcap
- #cgo freebsd LDFLAGS: -lpcap
 +#cgo dragonfly LDFLAGS: -lpcap
+ #cgo freebsd LDFLAGS: -lpcap
+ #cgo openbsd LDFLAGS: -lpcap
  #cgo darwin LDFLAGS: -lpcap
- #cgo solaris LDFLAGS: -lpcap
- #cgo windows CFLAGS: -I C:/WpdPack/Include
