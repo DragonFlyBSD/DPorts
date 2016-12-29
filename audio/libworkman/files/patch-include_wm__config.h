991a1210decb17c55f8a10ba7f3ea45798eecc47
DPorts Builder <nobody@home.ok>
2016-12-29 08:18:23 -0800

    Tweak audio/libworkman version 1.4_3

--- audio/libworkman/files/patch-include_wm__config.h
+++ audio/libworkman/files/patch-include_wm__config.h
@@ -1,11 +1,11 @@
---- include/wm_config.h.orig	1999-05-05 18:34:22.000000000 +0200
+--- include/wm_config.h.orig	1999-05-05 16:34:22 UTC
 +++ include/wm_config.h
 @@ -133,7 +133,7 @@
   ***  ******  **  **  ******  ******  **  ******  **  *  **********
   ***  ******  **  **      **      **     ****    ***    ***********
   ******************************************************************/
 -#if defined(__FreeBSD__) || defined(__NetBSD__)
-+#if defined(__DragonFly__) || defined(__FreeBSD__) || defined(__NetBSD__)
++#if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__DragonFly__)
  
  #define DEFAULT_CD_DEVICE	"/dev/rmatcd0c"
  
