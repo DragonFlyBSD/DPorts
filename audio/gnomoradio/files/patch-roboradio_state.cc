bfee418c0e1b56908d3c400ee9c8d432b0f2fb29
DPorts Builder <nobody@home.ok>
2016-12-29 20:23:12 -0800

    Tweak audio/gnomoradio version 0.15.1_19

--- audio/gnomoradio/files/patch-roboradio_state.cc
+++ audio/gnomoradio/files/patch-roboradio_state.cc
@@ -1,4 +1,4 @@
---- roboradio/state.cc.orig	2004-10-24 03:24:34.000000000 +0300
+--- roboradio/state.cc.orig	2004-10-24 00:24:34 UTC
 +++ roboradio/state.cc
 @@ -28,6 +28,7 @@
  #include <sys/stat.h>
