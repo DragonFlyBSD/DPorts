ee7c1324959eb659495629f7db8cf4d69d13041b
DPorts Builder <nobody@home.ok>
2016-12-30 19:34:45 -0800

    Update devel/R-cran-Rcpp to version 0.12.8

--- devel/R-cran-Rcpp/dragonfly/patch-src_api.cpp
+++ devel/R-cran-Rcpp/dragonfly/patch-src_api.cpp
@@ -1,18 +1,18 @@
---- src/api.cpp.orig	2016-05-14 16:04:16 UTC
+--- src/api.cpp.orig	2016-11-16 13:37:45 UTC
 +++ src/api.cpp
-@@ -34,7 +34,7 @@ using namespace Rcpp;
+@@ -33,7 +33,7 @@ using namespace Rcpp;
  #endif
  
- #if defined(__GNUC__)
+ #if defined(__GNUC__) || defined(__clang__)
 -    #if defined(_WIN32) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__CYGWIN__) || defined(__sun) || defined(_AIX) || defined(__MUSL__)
 +    #if defined(_WIN32) || defined(__DragonFly__) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__CYGWIN__) || defined(__sun) || defined(_AIX) || defined(__MUSL__)
          // do nothing
      #else
          #include <execinfo.h>
-@@ -256,7 +256,7 @@ SEXP rcpp_can_use_cxx11() {
+@@ -263,7 +263,7 @@ SEXP rcpp_can_use_cxx11() {
  // [[Rcpp::register]]
  SEXP stack_trace(const char* file, int line) {
-     #if defined(__GNUC__)
+     #if defined(__GNUC__) || defined(__clang__)
 -        #if defined(_WIN32) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__CYGWIN__) || defined(__sun) || defined(_AIX) || defined(__MUSL__)
 +        #if defined(_WIN32) || defined(__DragonFly__) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__CYGWIN__) || defined(__sun) || defined(_AIX) || defined(__MUSL__)
              // Simpler version for Windows and *BSD
