63ffff9608a442e2636f4f29d02474d51b981869
DPorts Builder <nobody@home.ok>
2016-12-31 09:19:36 -0800

    Update games/iortcw to version 1.5.a,1

--- games/iortcw/dragonfly/patch-SP_code_zlib-1.2.8_ioapi.h
+++ games/iortcw/dragonfly/patch-SP_code_zlib-1.2.8_ioapi.h
@@ -1,11 +1,11 @@
---- SP/code/zlib-1.2.8/ioapi.h.orig	2015-12-26 16:21:49.000000000 +0200
+--- SP/code/zlib-1.2.8/ioapi.h.orig	2016-11-19 09:33:57 UTC
 +++ SP/code/zlib-1.2.8/ioapi.h
 @@ -55,7 +55,7 @@
  #define ftello64 ftell
  #define fseeko64 fseek
  #else
--#ifdef __FreeBSD__
-+#if defined(__FreeBSD__) || defined(__DragonFly__)
+-#if defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__)
++#if defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__) || defined(__DragonFly__)
  #define fopen64 fopen
  #define ftello64 ftello
  #define fseeko64 fseeko
