d23376b03715eee3d096d57d30f452c1b69b03f7
DPorts Builder <nobody@home.ok>
2016-12-31 06:29:30 -0800

    Update sysutils/pax-utils to version 1.1.7

--- sysutils/pax-utils/dragonfly/patch-porting.h
+++ sysutils/pax-utils/dragonfly/patch-porting.h
@@ -1,6 +1,6 @@
---- porting.h.orig	2013-04-08 00:01:36.000000000 +0300
+--- porting.h.orig	2016-11-12 07:10:13 UTC
 +++ porting.h
-@@ -36,13 +36,13 @@
+@@ -40,7 +40,7 @@
  #include <sys/stat.h>
  #include <sys/types.h>
  #include "elf.h"
@@ -8,7 +8,9 @@
 +#if !defined(__FreeBSD__) && !defined(__DragonFly__) && !defined(__OpenBSD__)
  # include <alloca.h>
  #endif
- #if defined(__GLIBC__) || defined(__UCLIBC__)
+ #if defined(__linux__)
+@@ -50,7 +50,7 @@
+ #if defined(__GLIBC__) || defined(__UCLIBC__) || defined(__ANDROID__)
  # include <byteswap.h>
  # include <endian.h>
 -#elif defined(__FreeBSD__)
