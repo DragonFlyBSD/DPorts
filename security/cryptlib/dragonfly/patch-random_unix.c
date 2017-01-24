e50c8db907544d8e0a3d7d2d121ff5921862bcb0
DPorts Builder <nobody@home.ok>
2017-01-24 10:38:31 -0800

    Update security/cryptlib to version 3.4.3

--- security/cryptlib/dragonfly/patch-random_unix.c
+++ security/cryptlib/dragonfly/patch-random_unix.c
@@ -1,11 +1,11 @@
---- random/unix.c.orig	2010-06-18 01:51:32.000000000 +0300
+--- random/unix.c.orig	2016-02-25 11:52:52 UTC
 +++ random/unix.c
-@@ -591,7 +591,7 @@ static FILE *my_popen( INOUT DATA_SOURCE
- 				( void ) setegid( gathererGID );
+@@ -1541,7 +1541,7 @@ static FILE *my_popen( INOUT DATA_SOURCE
+ 			( void ) setgid( gathererGID );
+ 			( void ) setegid( gathererGID );
  #else
-   #if( defined( __linux__ ) || ( defined( __FreeBSD__ ) && OSVERSION >= 5 ) || \
--	   ( defined( __hpux ) && OSVERSION >= 11 ) )
-+	   ( defined( __hpux ) && OSVERSION >= 11 ) || defined( __DragonFly__ ) )
- 				( void ) setresuid( gathererUID, gathererUID, gathererUID );
- 				( void ) setresgid( gathererGID, gathererGID, gathererGID );
-   #else
+-  #if( defined( __linux__ ) || ( defined( __FreeBSD__ ) && OSVERSION >= 5 ) || \
++  #if( defined( __linux__ ) || defined __FreeBSD__ || defined __DragonFly__ || \
+ 	   ( defined( __hpux ) && OSVERSION >= 11 ) )
+ 			( void ) setresuid( gathererUID, gathererUID, gathererUID );
+ 			( void ) setresgid( gathererGID, gathererGID, gathererGID );
