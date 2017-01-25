ead5267173766d41b0d1b0b6dc460616de2dea8a
DPorts Builder <nobody@home.ok>
2017-01-25 06:14:41 -0800

    Tweak devel/android-tools-adb version 7.1.1.r4

---
+++ devel/android-tools-adb/dragonfly/patch-base_logging.cpp
@@ -0,0 +1,13 @@
+--- base/logging.cpp.orig	2017-01-25 08:28:58.000000000 +0200
++++ base/logging.cpp
+@@ -69,6 +69,10 @@
+ #elif defined(_WIN32)
+ #include <windows.h>
+ #elif defined(__DragonFly__)
++#include <sys/param.h>
++#if __DragonFly_version >= 400709
++#include <sys/lwp.h>
++#endif
+ #include <unistd.h>
+ #elif defined(__FreeBSD__)
+ #include <pthread_np.h>
