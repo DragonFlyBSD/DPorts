6b56323934fcb398fb5ca1b220303fcb07eba2d1
DPorts Builder <nobody@home.ok>
2017-02-08 15:08:48 -0800

    Update sysutils/e2fsprogs to version 1.43.4

--- sysutils/e2fsprogs/dragonfly/patch-lib_ext2fs_unix__io.c
+++ sysutils/e2fsprogs/dragonfly/patch-lib_ext2fs_unix__io.c
@@ -1,4 +1,4 @@
---- lib/ext2fs/unix_io.c.orig	2016-06-09 00:39:43.000000000 +0300
+--- lib/ext2fs/unix_io.c.orig	2017-01-31 06:08:02 UTC
 +++ lib/ext2fs/unix_io.c
 @@ -15,7 +15,7 @@
   * %End-Header%
@@ -9,12 +9,12 @@
  #define _XOPEN_SOURCE 600
  #define _DARWIN_C_SOURCE
  #define _FILE_OFFSET_BITS 64
-@@ -573,7 +573,7 @@ static errcode_t unix_open(const char *n
- 	}
+@@ -632,7 +632,7 @@ static errcode_t unix_open_channel(const
+ 		io->align = 512;
  #endif
  
--#if defined(__CYGWIN__) || defined(__FreeBSD__) || defined(__FreeBSD_kernel__)
-+#if defined(__CYGWIN__) || defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || defined(__DragonFly__)
- 	/*
- 	 * Some operating systems require that the buffers be aligned,
- 	 * regardless of O_DIRECT
+-#if defined(__FreeBSD__) || defined(__FreeBSD_kernel__)
++#if defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || defined(__DragonFly__)
+ 	if (io->flags & CHANNEL_FLAGS_BLOCK_DEVICE) {
+ 		int dio_align = ext2fs_get_dio_alignment(fd);
+ 
