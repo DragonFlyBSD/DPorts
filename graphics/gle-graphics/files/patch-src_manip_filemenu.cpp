9846592c209edb8fb3bcaf320d6ab449f926c0de
DPorts Builder <nobody@home.ok>
2017-02-20 09:13:02 -0800

    Tweak graphics/gle-graphics version 4.2.4.c_6

--- graphics/gle-graphics/files/patch-src_manip_filemenu.cpp
+++ graphics/gle-graphics/files/patch-src_manip_filemenu.cpp
@@ -1,15 +1,17 @@
---- src/manip/filemenu.cpp.orig	2010-06-14 18:34:39.000000000 +0300
+--- src/manip/filemenu.cpp.orig	2010-06-14 15:34:39 UTC
 +++ src/manip/filemenu.cpp
-@@ -404,10 +404,10 @@ void do_help(const char *k1, const char
+@@ -404,10 +404,12 @@ void do_help(const char *k1, const char 
          gotoxy(1,1);
  	wprintf("Manip help on topic {%s},  sub topic {%s}",k1,k2);
          scr_menubg();
 -        hfile = fopen(gledir(hfilename),"r");
-+        hfile = fopen(gledir(hfilename).c_str(),"r");
++        string fname = gledir(hfilename);
++        hfilename = fname.c_str();
++        hfile = fopen(fname.c_str(),"r");
          if (hfile==NULL) {
  		gotoxy(1,3);
 -		wprintf("Unable to open {%s} \n",gledir(hfilename));
-+		wprintf("Unable to open {%s} \n",gledir(hfilename).c_str());
++		wprintf("Unable to open {%s} \n",hfilename);
  		text_inkey();
  		return;
  	}
