b21a91faf24a76bbaa027378efbed196fbadeb01
DPorts Builder <nobody@home.ok>
2017-02-20 09:45:00 -0800

    Update www/squid-devel to version 4.0.18

--- www/squid-devel/files/patch-src_ssl_bio.h
+++ www/squid-devel/files/patch-src_ssl_bio.h
@@ -1,14 +1,11 @@
-LibreSSL compat
-not sure what are these BIO_*() functions in newer OpenSSL
-
---- src/ssl/bio.h.orig	2016-12-16 12:06:20.000000000 +0200
-+++ src/ssl/bio.h	2017-01-25 17:06:53.000000000 +0200
-@@ -194,7 +194,7 @@ private:
+--- src/ssl/bio.h.orig	2017-02-05 21:15:41 UTC
++++ src/ssl/bio.h
+@@ -205,7 +205,7 @@ private:
  void
  applyTlsDetailsToSSL(SSL *ssl, Security::TlsDetails::Pointer const &details, Ssl::BumpMode bumpMode);
  
 -#if (OPENSSL_VERSION_NUMBER < 0x10100000L)
-+#if (OPENSSL_VERSION_NUMBER < 0x10100000L) || defined(LIBRESSL_VERSION_NUMBER)
++#if (OPENSSL_VERSION_NUMBER < 0x10100000L || defined(LIBRESSL_VERSION_NUMBER))
  // OpenSSL v1.0 bio compatibility functions
  inline void *BIO_get_data(BIO *table) { return table->ptr; }
  inline void BIO_set_data(BIO *table, void *data) { table->ptr = data; }
