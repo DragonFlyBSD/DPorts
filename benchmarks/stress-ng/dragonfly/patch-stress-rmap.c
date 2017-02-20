21ee3c3a99ccb78677020069dedfe19e40b05cd1
DPorts Builder <nobody@home.ok>
2017-02-20 09:17:05 -0800

    Update benchmarks/stress-ng to version 0.07.20

--- benchmarks/stress-ng/dragonfly/patch-stress-rmap.c
+++ benchmarks/stress-ng/dragonfly/patch-stress-rmap.c
@@ -1,15 +1,14 @@
---- stress-rmap.c.orig	2016-11-08 18:02:04.000000000 +0200
+--- stress-rmap.c.orig	2017-02-09 11:08:58 UTC
 +++ stress-rmap.c
-@@ -173,7 +173,12 @@ int stress_rmap(
+@@ -144,7 +144,11 @@ int stress_rmap(const args_t *args)
  	}
  	(void)unlink(filename);
  
 +#ifdef __DragonFly__
-+/* XXX no support for posix_fallocate() */
 +	if (ftruncate(fd, 0 + sz) < 0) {
 +#else
  	if (posix_fallocate(fd, 0, sz) < 0) {
 +#endif
- 		pr_fail_err(name, "posix_fallocate");
+ 		pr_fail_err("posix_fallocate");
  		(void)close(fd);
- 		(void)stress_temp_dir_rm(name, mypid, instance);
+ 		(void)stress_temp_dir_rm_args(args);
