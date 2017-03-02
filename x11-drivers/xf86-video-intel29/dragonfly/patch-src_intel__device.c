f32b2700df6ceb7cb2d9c5852bb054f23231b223
DPorts Builder <nobody@home.ok>
2017-03-02 08:44:17 -0800

    Tweak x11-drivers/xf86-video-intel29 version 2.99.2017.01.06_1,2

--- x11-drivers/xf86-video-intel29/dragonfly/patch-src_intel__device.c
+++ x11-drivers/xf86-video-intel29/dragonfly/patch-src_intel__device.c
@@ -1,13 +1,24 @@
-Force i915.ko loading?
+i915 does not use hw.dri.0.busid
+so simplify to basics.
 
---- src/intel_device.c.orig	2016-04-02 11:03:57.000000000 +0300
+--- src/intel_device.c.orig	2017-01-03 23:03:54.000000000 +0200
 +++ src/intel_device.c
-@@ -425,7 +425,7 @@ static int __intel_open_device__legacy(c
+@@ -424,6 +424,10 @@ static int __intel_open_device__legacy(c
+ 		 "pci:%04x:%02x:%02x.%d",
  		 pci->domain, pci->bus, pci->dev, pci->func);
  
++#if defined(__DragonFly__)
++	/* assume modesetting for i915, allow multiple loads and no fbcon */
++	load_i915_kernel_module();
++#else
  	ret = drmCheckModesettingSupported(id);
--	if (ret) {
-+	if (ret || 1) {
+ 	if (ret) {
  		if (load_i915_kernel_module() == 0)
- 			ret = drmCheckModesettingSupported(id);
- 		if (ret)
+@@ -433,6 +437,7 @@ static int __intel_open_device__legacy(c
+ 		/* Be nice to the user and load fbcon too */
+ 		(void)xf86LoadKernelModule("fbcon");
+ 	}
++#endif
+ 
+ 	return fd_set_nonblock(drmOpen(NULL, id));
+ }
