56169d7eb78def1716b15878a1ef13706a2df10e
DPorts Builder <nobody@home.ok>
2017-04-02 16:22:48 -0700

    Update devel/fpc-fcl-net to version 3.0.2

--- devel/fpc-fcl-net/dragonfly/patch-packages_fcl-net_fpmake.pp
+++ devel/fpc-fcl-net/dragonfly/patch-packages_fcl-net_fpmake.pp
@@ -1,11 +1,11 @@
---- packages/fcl-net/fpmake.pp.orig	2015-11-13 21:15:29 UTC
+--- packages/fcl-net/fpmake.pp.orig	2017-02-04 15:11:06 UTC
 +++ packages/fcl-net/fpmake.pp
 @@ -70,7 +70,7 @@ begin
          end;
      T.ResourceStrings := True;
  
--    T:=P.Targets.AddUnit('cnetdb.pp',[linux,freebsd]);
-+    T:=P.Targets.AddUnit('cnetdb.pp',[linux,dragonfly,freebsd]);
+-    T:=P.Targets.AddUnit('cnetdb.pp',[linux,freebsd,solaris]);
++    T:=P.Targets.AddUnit('cnetdb.pp',[linux,dragonfly,freebsd,solaris]);
  
      P.ExamplePath.Add('examples');
      P.Targets.AddExampleProgram('examples/ip6test.pp');
