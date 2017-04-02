aa900bb458af7bb104d357685866efcbebd9a366
DPorts Builder <nobody@home.ok>
2017-04-02 14:03:17 -0700

    Update net/cjdns to version 19.1

--- net/cjdns/dragonfly/patch-crypto_random_seed_BsdKernArndSysctlRandomSeed.h
+++ net/cjdns/dragonfly/patch-crypto_random_seed_BsdKernArndSysctlRandomSeed.h
@@ -1,13 +1,13 @@
 No KERN_ARND on DragonFly yet?
 
---- crypto/random/seed/BsdKernArndSysctlRandomSeed.h.orig	2016-06-15 00:58:54.000000000 +0300
+--- crypto/random/seed/BsdKernArndSysctlRandomSeed.h.orig	2017-04-02 17:57:23 UTC
 +++ crypto/random/seed/BsdKernArndSysctlRandomSeed.h
 @@ -20,7 +20,7 @@
  #include "memory/Allocator.h"
  #include "util/Linker.h"
  
--#if defined(freebsd) || defined(openbsd)
-+#if (defined(freebsd) || defined(openbsd)) && !defined(__DragonFly__)
+-#if defined(freebsd)
++#if defined(freebsd) && !defined(__DragonFly__)
      Linker_require("crypto/random/seed/BsdKernArndSysctlRandomSeed.c");
      struct RandomSeed* BsdKernArndSysctlRandomSeed_new(struct Allocator* alloc);
      RandomSeedProvider_register(BsdKernArndSysctlRandomSeed_new)
