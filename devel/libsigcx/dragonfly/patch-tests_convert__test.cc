f38fc64f1c6c278b5b54c483d04c81a6c1074895
DPorts Builder <nobody@home.ok>
2017-04-12 16:07:06 -0700

    Prune 48 obsolete ports
    FreeBSD Ports maintainers have already deleted the following:
      accessibility/at-spi
      accessibility/at-spi-reference
      accessibility/dasher
      accessibility/gnome-mag
      accessibility/gnome-speech
      accessibility/gok
      accessibility/java-access-bridge
      archivers/gnome-autoar
      archivers/pecl-snappy
      audio/gnome-audio
      audio/schism
      databases/godis
      databases/libgda4
      databases/libgda4-bdb
      databases/libgda4-ldap
      databases/libgda4-mdb
      databases/libgda4-mysql
      databases/libgda4-postgresql
      databases/libgda4-reference
      databases/libgdamm
      databases/p5-Search-Xapian
      databases/puppetdb-terminus
      databases/rubygem-redis-actionpack4
      deskutils/gnome-blog
      deskutils/gnome-epub-thumbnailer
      devel/go-pretty
      devel/libsigcx
      devel/llvm37
      devel/php5-geshi
      devel/rubygem-nio4r1
      graphics/goocanvasmm
      lang/clang37
      security/openvpn23
      security/openvpn23-polarssl
      sysutils/jail2
      sysutils/puppet38
      textproc/go-text
      textproc/p5-XML-XSH
      www/moodle29
      www/p5-Catalyst-Plugin-Singleton
      www/rubygem-jquery-atwho-rails-rails4
      www/typo3
      www/typo3-lts
      x11-themes/kde4-icons-oxygen
      x11-toolkits/gal2
      x11-toolkits/gal2-reference
      x11-toolkits/libgtksourceviewmm
      x11-toolkits/vte290

--- devel/libsigcx/dragonfly/patch-tests_convert__test.cc
+++
@@ -1,9 +0,0 @@
---- tests/convert_test.cc.orig	2002-08-29 19:20:00.000000000 +0200
-+++ tests/convert_test.cc
-@@ -1,5 +1,6 @@
- #include <string>
- #include <iostream>
-+#include <cstring>
- #include <sigcx/convert.h>
- 
- /*
