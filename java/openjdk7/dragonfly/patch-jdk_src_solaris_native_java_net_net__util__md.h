7ec5b44af376e5c9efbba64f6f9946e2ec72dc30
DPorts Builder <nobody@home.ok>
2017-07-03 22:22:44 -0700

    Update java/openjdk7 to version 7.141.02,1

--- java/openjdk7/dragonfly/patch-jdk_src_solaris_native_java_net_net__util__md.h
+++ java/openjdk7/dragonfly/patch-jdk_src_solaris_native_java_net_net__util__md.h
@@ -1,11 +1,11 @@
---- jdk/src/solaris/native/java/net/net_util_md.h.intermediate	2013-05-13 10:51:23.858101000 +0000
-+++ jdk/src/solaris/native/java/net/net_util_md.h
-@@ -37,7 +37,8 @@
- #endif
- 
- 
--#if defined(__linux__) || defined(__FreeBSD__) || defined(MACOSX)
-+#if defined(__linux__) || defined(__FreeBSD__) || defined(MACOSX) \
+--- jdk/src/solaris/native/java/net/net_util_md.h.orig	2017-07-03 16:34:27.000000000 +0000
++++ jdk/src/solaris/native/java/net/net_util_md.h	2017-07-03 16:35:53.000000000 +0000
+@@ -46,7 +46,8 @@
+    close subroutine does not return until the select call returns.
+    ...
+ */
+-#if defined(__linux__) || defined(__FreeBSD__) || defined(MACOSX) || defined (_AIX)
++#if defined(__linux__) || defined(__FreeBSD__) || defined(MACOSX) || defined (_AIX) \
 + || defined(__DragonFly__)
  extern int NET_Timeout(int s, long timeout);
  extern int NET_Read(int s, void* buf, size_t len);
