a61990a2477a48a8b8c9b5e184cf9643df319cf8
DPorts Builder <nobody@home.ok>
2017-07-06 09:54:47 -0700

    Update databases/galera to version 25.3.20_2

--- databases/galera/dragonfly/patch-galerautils_src_gu__arch.h
+++ databases/galera/dragonfly/patch-galerautils_src_gu__arch.h
@@ -1,11 +1,11 @@
---- galerautils/src/gu_arch.h.orig	2014-03-27 17:49:30.000000000 +0200
-+++ galerautils/src/gu_arch.h
-@@ -41,7 +41,7 @@
- # error "Byte order not defined"
- #endif
- 
--#if defined(__sun__)
-+#if defined(__sun__) || defined(__DragonFly__)
- # define GU_WORDSIZE 64 /* Solaris 11 is only 64-bit ATM */
+--- galerautils/src/gu_arch.h.orig	2017-05-25 17:11:36.000000000 +0000
++++ galerautils/src/gu_arch.h	2017-07-06 16:35:48.000000000 +0000
+@@ -47,6 +47,8 @@
+ # else
+ #  define GU_WORDSIZE 32
+ # endif
++#elif defined __DragonFly__
++# define GU_WORDSIZE 64
  #elif defined(__APPLE__) || defined(__FreeBSD__)
  # include <stdint.h>
+ # define GU_WORDSIZE __WORDSIZE
