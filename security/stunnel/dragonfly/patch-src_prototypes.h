0510e937d46612b6929431a05814aa8e9c76429d
DPorts Builder <nobody@home.ok>
2017-07-25 08:33:52 -0700

    Update security/stunnel to version 5.42,1

--- security/stunnel/dragonfly/patch-src_prototypes.h
+++ security/stunnel/dragonfly/patch-src_prototypes.h
@@ -1,18 +1,11 @@
---- src/prototypes.h.orig	2017-03-26 20:25:00 UTC
+--- src/prototypes.h.orig	2017-06-05 14:44:40.000000000 +0300
 +++ src/prototypes.h
-@@ -657,13 +657,13 @@ typedef enum {
- #endif /* OPENSSL_NO_DH */
-     STUNNEL_LOCKS                           /* number of locks */
- } LOCK_TYPE;
--#if OPENSSL_VERSION_NUMBER < 0x10100004L
-+#if OPENSSL_VERSION_NUMBER < 0x10100004L || defined(LIBRESSL_VERSION_NUMBER)
- typedef int STUNNEL_RWLOCK;
- #else
- typedef CRYPTO_RWLOCK *STUNNEL_RWLOCK;
+@@ -678,7 +678,7 @@ void stunnel_rwlock_destroy_debug(struct
+ #define stunnel_write_unlock(x) stunnel_write_unlock_debug((x),__FILE__,__LINE__)
+ #define stunnel_rwlock_destroy(x) stunnel_rwlock_destroy_debug((x),__FILE__,__LINE__)
+ 
+-#if OPENSSL_VERSION_NUMBER<0x10100004L
++#if OPENSSL_VERSION_NUMBER<0x10100004L || defined(LIBRESSL_VERSION_NUMBER)
+ #define CRYPTO_atomic_add(addr,amount,result,type) \
+     *result = type ? CRYPTO_add(addr,amount,type) : (*addr+=amount)
  #endif
- extern STUNNEL_RWLOCK stunnel_locks[STUNNEL_LOCKS];
--#if OPENSSL_VERSION_NUMBER>=0x10100004L
-+#if OPENSSL_VERSION_NUMBER>=0x10100004L && !defined(LIBRESSL_VERSION_NUMBER)
- #define CRYPTO_THREAD_read_unlock(type) CRYPTO_THREAD_unlock(type)
- #define CRYPTO_THREAD_write_unlock(type) CRYPTO_THREAD_unlock(type)
- #else
--- security/stunnel/dragonfly/patch-src_sthreads.c
+++ security/stunnel/dragonfly/patch-src_sthreads.c
@@ -1,59 +1,51 @@
---- src/sthreads.c.orig	2017-01-19 08:51:32 UTC
+--- src/sthreads.c.orig	2017-06-06 15:46:34.000000000 +0300
 +++ src/sthreads.c
-@@ -47,7 +47,7 @@
- STUNNEL_RWLOCK stunnel_locks[STUNNEL_LOCKS];
+@@ -97,14 +97,14 @@ unsigned long stunnel_thread_id(void) {
+ 
+ #endif /* USE_WIN32 */
+ 
+-#if OPENSSL_VERSION_NUMBER>=0x10000000L && OPENSSL_VERSION_NUMBER<0x10100004L
++#if (OPENSSL_VERSION_NUMBER>=0x10000000L && OPENSSL_VERSION_NUMBER<0x10100004L) || defined(LIBRESSL_VERSION_NUMBER)
+ NOEXPORT void threadid_func(CRYPTO_THREADID *tid) {
+     CRYPTO_THREADID_set_numeric(tid, stunnel_thread_id());
+ }
  #endif
+ 
+ void thread_id_init(void) {
+-#if OPENSSL_VERSION_NUMBER>=0x10000000L && OPENSSL_VERSION_NUMBER<0x10100000L
++#if (OPENSSL_VERSION_NUMBER>=0x10000000L && OPENSSL_VERSION_NUMBER<0x10100000L) || defined(LIBRESSL_VERSION_NUMBER)
+     CRYPTO_THREADID_set_callback(threadid_func);
+ #endif
+ #if OPENSSL_VERSION_NUMBER<0x10000000L || !defined(OPENSSL_NO_DEPRECATED)
+@@ -216,11 +216,11 @@ void stunnel_rwlock_destroy_debug(struct
+ 
+ struct CRYPTO_dynlock_value stunnel_locks[STUNNEL_LOCKS];
  
 -#if OPENSSL_VERSION_NUMBER<0x10100004L
 +#if OPENSSL_VERSION_NUMBER<0x10100004L || defined(LIBRESSL_VERSION_NUMBER)
  #define CRYPTO_THREAD_lock_new() CRYPTO_get_new_dynlockid()
  #endif
  
-@@ -205,7 +205,7 @@ int create_client(SOCKET ls, SOCKET s, C
- 
- #ifdef USE_PTHREAD
- 
 -#if OPENSSL_VERSION_NUMBER<0x10100004L
 +#if OPENSSL_VERSION_NUMBER<0x10100004L || defined(LIBRESSL_VERSION_NUMBER)
  
- struct CRYPTO_dynlock_value {
-     pthread_rwlock_t rwlock;
-@@ -265,16 +265,18 @@ unsigned long stunnel_thread_id(void) {
+ static struct CRYPTO_dynlock_value *lock_cs;
+ 
+@@ -261,7 +261,7 @@ NOEXPORT void locking_callback(int mode,
+ 
+ void locking_init(void) {
+     size_t i;
+-#if OPENSSL_VERSION_NUMBER<0x10100004L
++#if OPENSSL_VERSION_NUMBER<0x10100004L || defined(LIBRESSL_VERSION_NUMBER)
+     size_t num;
  #endif
- }
  
--#if OPENSSL_VERSION_NUMBER>=0x10000000L && OPENSSL_VERSION_NUMBER<0x10100004L
-+#if OPENSSL_VERSION_NUMBER>=0x10000000L
-+#if OPENSSL_VERSION_NUMBER<0x10100004L || defined(LIBRESSL_VERSION_NUMBER)
- NOEXPORT void threadid_func(CRYPTO_THREADID *tid) {
-     CRYPTO_THREADID_set_numeric(tid, stunnel_thread_id());
- }
- #endif
-+#endif
- 
- int sthreads_init(void) {
-     int i;
+@@ -269,7 +269,7 @@ void locking_init(void) {
+     for(i=0; i<STUNNEL_LOCKS; i++) /* all the mutexes */
+         stunnel_rwlock_init(&stunnel_locks[i]);
  
 -#if OPENSSL_VERSION_NUMBER<0x10100004L
 +#if OPENSSL_VERSION_NUMBER<0x10100004L || defined(LIBRESSL_VERSION_NUMBER)
-     /* initialize the OpenSSL dynamic locking */
-     CRYPTO_set_dynlock_create_callback(dyn_create_function);
-     CRYPTO_set_dynlock_lock_callback(dyn_lock_function);
-@@ -347,7 +349,7 @@ int create_client(SOCKET ls, SOCKET s, C
-  * but it is unsupported on Windows XP (and earlier versions of Windows):
-  * https://msdn.microsoft.com/en-us/library/windows/desktop/aa904937%28v=vs.85%29.aspx */
- 
--#if OPENSSL_VERSION_NUMBER<0x10100004L
-+#if OPENSSL_VERSION_NUMBER<0x10100004L || defined(LIBRESSL_VERSION_NUMBER)
- 
- struct CRYPTO_dynlock_value {
-     CRITICAL_SECTION mutex;
-@@ -401,7 +403,7 @@ int sthreads_init(void) {
- #ifndef USE_FORK
-     int i;
- 
--#if OPENSSL_VERSION_NUMBER<0x10100004L
-+#if OPENSSL_VERSION_NUMBER<0x10100004L || defined(LIBRESSL_VERSION_NUMBER)
-     /* initialize the OpenSSL dynamic locking */
-     CRYPTO_set_dynlock_create_callback(dyn_create_function);
-     CRYPTO_set_dynlock_lock_callback(dyn_lock_function);
+     /* initialize the OpenSSL static locking */
+     num=(size_t)CRYPTO_num_locks();
+     lock_cs=str_alloc_detached(num*sizeof(struct CRYPTO_dynlock_value));
