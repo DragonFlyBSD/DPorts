5cd9643120ee657e9cb795a8dd045c5336c735a6
DPorts Builder <nobody@home.ok>
2017-08-03 20:46:54 -0700

    Update ftp/proftpd to version 1.3.6

--- ftp/proftpd/dragonfly/patch-lib__pr_fnmatch.c
+++ ftp/proftpd/dragonfly/patch-lib__pr_fnmatch.c
@@ -1,15 +1,6 @@
---- lib/pr_fnmatch.c.orig	2012-04-24 19:19:01 UTC
+--- lib/pr_fnmatch.c.orig	2017-04-10 02:31:02 UTC
 +++ lib/pr_fnmatch.c
-@@ -250,7 +250,7 @@
- # endif
- # define STRLEN(S) strlen (S)
- # define STRCAT(D, S) strcat (D, S)
--# define MEMPCPY(D, S, N) __mempcpy (D, S, N)
-+# define MEMPCPY(D, S, N) mempcpy (D, S, N)
- # define MEMCHR(S, C, N) memchr (S, C, N)
- # define STRCOLL(S1, S2) strcoll (S1, S2)
- # include "pr_fnmatch_loop.c"
-@@ -274,7 +274,7 @@
+@@ -278,7 +278,7 @@ __wcschrnul (const wchar_t *s, wint_t c)
  #  define BTOWC(C)	(C)
  #  define STRLEN(S) __wcslen (S)
  #  define STRCAT(D, S) __wcscat (D, S)
