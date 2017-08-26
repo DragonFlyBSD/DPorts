9f73159cf00a4af9cb2be2b9e243e8d21b7c52ec
DPorts Builder <nobody@home.ok>
2017-08-26 11:48:13 -0700

    Update x11/terminal.app to version 0.9.9

--- x11/terminal.app/dragonfly/patch-TerminalView.m
+++ x11/terminal.app/dragonfly/patch-TerminalView.m
@@ -1,28 +1,20 @@
---- TerminalView.m.orig	2012-03-06 17:54:33.000000000 -0500
+--- TerminalView.m.orig	2017-08-02 17:17:43 UTC
 +++ TerminalView.m
-@@ -33,7 +33,7 @@ activated */
- #  include <termios.h>
+@@ -34,7 +34,7 @@ activated */
  #  include <pcap.h>
+ #  include <util.h>
  #define TCSETS TIOCSETA
 -#elif defined(__FreeBSD__)
 +#elif defined(__FreeBSD__) || defined(__DragonFly__)
  #  include <sys/types.h>
  #  include <sys/ioctl.h>
  #  include <termios.h>
-@@ -52,7 +52,7 @@ activated */
+@@ -53,7 +53,7 @@ activated */
  #include <sys/types.h>
  #include <unistd.h>
  #include <fcntl.h>
 -#ifndef __FreeBSD__
-+#if !defined(__FreeBSD__) && !defined(__DragonFly__)
++#if !defined __FreeBSD__ && !defined __DragonFly__
  #if !(defined (__NetBSD__)) && !(defined (__SOLARIS__)) && !(defined(__OpenBSD__))
  #  include <pty.h>
  #endif
-@@ -73,6 +73,7 @@ activated */
- #include <AppKit/NSGraphics.h>
- #include <AppKit/NSScroller.h>
- #include <AppKit/DPSOperators.h>
-+#include <AppKit/NSButtonCell.h>
- 
- #include "TerminalView.h"
- 
