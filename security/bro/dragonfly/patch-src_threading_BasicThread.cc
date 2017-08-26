c8db86ae37d6a8f80467a661fc0318b467a7e5a7
DPorts Builder <nobody@home.ok>
2017-08-26 11:22:02 -0700

    Update security/bro to version 2.5.1

--- security/bro/dragonfly/patch-src_threading_BasicThread.cc
+++ security/bro/dragonfly/patch-src_threading_BasicThread.cc
@@ -1,11 +1,11 @@
---- src/threading/BasicThread.cc.orig	2015-09-06 22:43:16.000000000 +0300
+--- src/threading/BasicThread.cc.orig	2017-06-26 23:24:37 UTC
 +++ src/threading/BasicThread.cc
-@@ -59,7 +59,7 @@ void BasicThread::SetOSName(const char*
+@@ -60,7 +60,7 @@ void BasicThread::SetOSName(const char*
  	pthread_setname_np(arg_name);
  #endif
  
 -#ifdef FREEBSD
-+#if defined(FREEBSD) || defined(DRAGONFLY)
- 	pthread_set_name_np(pthread_self(), arg_name, arg_name);
++#if defined FREEBSD || defined DRAGONFLY
+ 	pthread_set_name_np(thread.native_handle(), arg_name, arg_name);
  #endif
  	}
