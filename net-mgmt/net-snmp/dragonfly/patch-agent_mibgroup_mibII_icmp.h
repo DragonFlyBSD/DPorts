201ac4e85b266d6cf4629d3d4812e5d119142d11
DPorts Builder <nobody@home.ok>
2017-10-01 05:33:59 -0700

    Tweak net-mgmt/net-snmp version 5.7.3_17

--- net-mgmt/net-snmp/dragonfly/patch-agent_mibgroup_mibII_icmp.h
+++ net-mgmt/net-snmp/dragonfly/patch-agent_mibgroup_mibII_icmp.h
@@ -1,10 +1,11 @@
---- agent/mibgroup/mibII/icmp.h.intermediate	2015-02-02 17:26:14 UTC
+--- agent/mibgroup/mibII/icmp.h.orig	2017-10-01 12:13:50 UTC
 +++ agent/mibgroup/mibII/icmp.h
-@@ -21,6 +21,7 @@ config_arch_require(openbsd4,  mibII/ker
+@@ -22,6 +22,8 @@ config_arch_require(openbsd4,  mibII/ker
  config_arch_require(openbsd5,  mibII/kernel_sysctl)
  config_arch_require(dragonfly2, mibII/kernel_sysctl)
  config_arch_require(dragonfly3, mibII/kernel_sysctl)
 +config_arch_require(dragonfly4, mibII/kernel_sysctl)
++config_arch_require(dragonfly5, mibII/kernel_sysctl)
  config_arch_require(darwin10,  mibII/kernel_sysctl)
  config_arch_require(darwin11,  mibII/kernel_sysctl)
  config_arch_require(darwin12,  mibII/kernel_sysctl)
