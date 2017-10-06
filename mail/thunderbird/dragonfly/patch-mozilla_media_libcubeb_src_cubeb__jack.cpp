e3c47805947a884e964ab4f5d17106f755d006e8
DPorts Builder <nobody@home.ok>
2017-10-06 14:50:13 -0700

    Update mail/thunderbird to version 52.4.0

---
+++ mail/thunderbird/dragonfly/patch-mozilla_media_libcubeb_src_cubeb__jack.cpp
@@ -0,0 +1,12 @@
+--- mozilla/media/libcubeb/src/cubeb_jack.cpp.intermediate	2017-10-06 21:11:00 UTC
++++ mozilla/media/libcubeb/src/cubeb_jack.cpp
+@@ -8,9 +8,6 @@
+  */
+ #define _DEFAULT_SOURCE
+ #define _BSD_SOURCE
+-#ifndef __FreeBSD__
+-#define _POSIX_SOURCE
+-#endif
+ #include <algorithm>
+ #include <dlfcn.h>
+ #include <limits>
