66070f1781c0120d557ebf5d055d5ef93ff3ab42
DPorts Builder <nobody@home.ok>
2017-11-02 07:15:07 -0700

    Update sysutils/py-psutil to version 5.4.0

--- sysutils/py-psutil/dragonfly/patch-psutil_____init____.py
+++ sysutils/py-psutil/dragonfly/patch-psutil_____init____.py
@@ -1,6 +1,6 @@
---- psutil/__init__.py.orig	2017-08-03 16:14:27 UTC
-+++ psutil/__init__.py
-@@ -78,6 +78,7 @@ from ._common import FREEBSD  # NOQA
+--- psutil/__init__.py.orig	2017-11-02 10:52:12.593964000 +0200
++++ psutil/__init__.py	2017-11-02 10:57:55.867278000 +0200
+@@ -80,6 +80,7 @@
  from ._common import LINUX
  from ._common import NETBSD  # NOQA
  from ._common import OPENBSD  # NOQA
@@ -8,12 +8,12 @@
  from ._common import OSX
  from ._common import POSIX  # NOQA
  from ._common import SUNOS
-@@ -185,7 +186,7 @@ __all__ = [
+@@ -194,7 +195,7 @@
      "POWER_TIME_UNKNOWN", "POWER_TIME_UNLIMITED",
  
      "BSD", "FREEBSD", "LINUX", "NETBSD", "OPENBSD", "OSX", "POSIX", "SUNOS",
--    "WINDOWS",
-+    "WINDOWS", "DRAGONFLY",
+-    "WINDOWS", "AIX",
++    "WINDOWS", "AIX", "DRAGONFLY"
  
      # classes
      "Process", "Popen",
