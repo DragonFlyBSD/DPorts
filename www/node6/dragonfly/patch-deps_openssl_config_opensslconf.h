64d5d585596067f858bb39a2ec2015b94e0047bb
DPorts Builder <nobody@home.ok>
2017-11-02 12:30:48 -0700

    Update www/node6 to version 6.11.5

--- www/node6/dragonfly/patch-deps_openssl_config_opensslconf.h
+++ www/node6/dragonfly/patch-deps_openssl_config_opensslconf.h
@@ -1,11 +1,11 @@
---- deps/openssl/config/opensslconf.h.orig	2015-09-23 03:32:14.000000000 +0300
+--- deps/openssl/config/opensslconf.h.orig	2017-10-24 19:09:59 UTC
 +++ deps/openssl/config/opensslconf.h
-@@ -110,7 +110,7 @@
- # include "./archs/VC-WIN64A/opensslconf.h"
- #elif (defined(__FreeBSD__) || defined(__OpenBSD__)) && defined(__i386__)
- # include "./archs/BSD-x86/opensslconf.h"
--#elif (defined(__FreeBSD__) || defined(__OpenBSD__)) && defined(__x86_64__)
-+#elif (defined(__FreeBSD__) || defined(__DragonFly__) || defined(__OpenBSD__)) && defined(__x86_64__)
- # include "./archs/BSD-x86_64/opensslconf.h"
- #elif defined(__sun) && defined(__i386__)
- # include "./archs/solaris-x86-gcc/opensslconf.h"
+@@ -98,7 +98,7 @@
+ #endif
+ 
+ #undef OPENSSL_BSD
+-#if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__)
++#if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined __DragonFly__
+ # define OPENSSL_BSD 1
+ #endif
+ 
