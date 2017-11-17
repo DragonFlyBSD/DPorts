1262fe40abb3844d3a7cc07d3842ca840616c400
DPorts Builder <nobody@home.ok>
2017-11-17 07:46:15 -0800

    Update sysutils/logrotate to version 3.13.0

--- sysutils/logrotate/dragonfly/patch-config.c
+++ sysutils/logrotate/dragonfly/patch-config.c
@@ -1,8 +1,8 @@
---- config.c.orig	2015-02-13 06:11:21 UTC
+--- config.c.orig	2017-10-12 15:19:41 UTC
 +++ config.c
-@@ -1,8 +1,4 @@
+@@ -1,8 +1,5 @@
  #include "queue.h"
--/* Alloca is defined in stdlib.h in NetBSD */
+ /* Alloca is defined in stdlib.h in NetBSD */
 -#ifndef __NetBSD__
 -#include <alloca.h>
 -#endif
