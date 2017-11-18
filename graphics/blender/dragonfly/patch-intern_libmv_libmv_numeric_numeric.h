3c5bd94888621f9db8a504d4a9bc279626f8a51f
DPorts Builder <nobody@home.ok>
2017-11-18 05:20:46 -0800

    Update graphics/blender to version 2.79_1

--- graphics/blender/dragonfly/patch-intern_libmv_libmv_numeric_numeric.h
+++ graphics/blender/dragonfly/patch-intern_libmv_libmv_numeric_numeric.h
@@ -1,11 +1,11 @@
---- intern/libmv/libmv/numeric/numeric.h.orig	2016-03-23 10:49:49.000000000 +0200
+--- intern/libmv/libmv/numeric/numeric.h.orig	2017-09-11 07:34:59.000000000 +0300
 +++ intern/libmv/libmv/numeric/numeric.h
 @@ -35,7 +35,7 @@
  
  #if !defined(__MINGW64__)
  #  if defined(_WIN32) || defined(__APPLE__) || \
--      (defined(__FreeBSD__) && __FreeBSD_version < 1200032) || defined(__NetBSD__)
-+      (defined(__FreeBSD__) && __FreeBSD_version < 1200032) || defined(__NetBSD__) || defined(__DragonFly__)
- static void sincos(double x, double *sinx, double *cosx) {
+-      defined(__FreeBSD__) || defined(__NetBSD__)
++      defined(__FreeBSD__) || defined(__NetBSD__) || defined(__DragonFly__)
+ inline void sincos(double x, double *sinx, double *cosx) {
    *sinx = sin(x);
    *cosx = cos(x);
