84543b45d6f21a89290193b44510f0e383583c2f
DPorts Builder <nobody@home.ok>
2017-11-19 17:00:18 -0800

    Update x11/xpra to version 2.0.3_3

--- x11/xpra/dragonfly/patch-setup.py
+++ x11/xpra/dragonfly/patch-setup.py
@@ -1,6 +1,6 @@
---- setup.py.intermediate	2017-02-21 20:13:51 UTC
+--- setup.py.orig	2017-11-20 00:56:28 UTC
 +++ setup.py
-@@ -190,7 +190,7 @@ webp_ENABLED            = False
+@@ -185,7 +185,7 @@ jpeg_ENABLED            = DEFAULT and pk
  vpx_ENABLED             = DEFAULT and pkg_config_version("1.3", "vpx", fallback=WIN32)
  enc_ffmpeg_ENABLED      = DEFAULT and pkg_config_version("56", "libavcodec")
  webcam_ENABLED          = DEFAULT and not OSX
@@ -9,3 +9,12 @@
  #ffmpeg 2 onwards:
  dec_avcodec2_ENABLED    = DEFAULT and pkg_config_version("56", "libavcodec", fallback=WIN32)
  # some version strings I found:
+@@ -205,7 +205,7 @@ csc_libyuv_ENABLED      = DEFAULT and pk
+ #Cython / gcc / packaging build options:
+ annotate_ENABLED        = True
+ warn_ENABLED            = True
+-strict_ENABLED          = True
++strict_ENABLED          = False
+ PIC_ENABLED             = not WIN32     #ming32 moans that it is always enabled already
+ debug_ENABLED           = False
+ verbose_ENABLED         = False
