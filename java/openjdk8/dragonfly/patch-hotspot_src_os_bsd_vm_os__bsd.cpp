efb0cc339337e06cb2a9d6c40a73a4dbcc0d0c44
DPorts Builder <nobody@home.ok>
2017-12-06 07:04:49 -0800

    Update java/openjdk8 to version 8.152.16_1

--- java/openjdk8/dragonfly/patch-hotspot_src_os_bsd_vm_os__bsd.cpp
+++ java/openjdk8/dragonfly/patch-hotspot_src_os_bsd_vm_os__bsd.cpp
@@ -1,6 +1,6 @@
---- hotspot/src/os/bsd/vm/os_bsd.cpp.orig	2015-03-18 10:12:26 UTC
+--- hotspot/src/os/bsd/vm/os_bsd.cpp.orig	2017-12-06 14:50:04 UTC
 +++ hotspot/src/os/bsd/vm/os_bsd.cpp
-@@ -113,7 +113,7 @@
+@@ -114,7 +114,7 @@
  # include <vm/vm_param.h>
  #endif
  
@@ -9,7 +9,16 @@
  # include <elf.h>
  #endif
  
-@@ -2850,7 +2850,7 @@ OSReturn os::set_native_priority(Thread*
+@@ -2396,6 +2396,8 @@ bool os::remove_stack_guard_pages(char*
+ 
+ static address _highest_vm_reserved_address = NULL;
+ 
++#define MAP_ALIGNED(n)	((n) << 24)
++
+ // If 'fixed' is true, anon_mmap() will attempt to reserve anonymous memory
+ // at 'requested_addr'. If there are existing memory mappings at the same
+ // location, however, they will be overwritten. If 'fixed' is false,
+@@ -2983,7 +2985,7 @@ OSReturn os::set_native_priority(Thread*
  #ifdef __OpenBSD__
    // OpenBSD pthread_setprio starves low priority threads
    return OS_OK;
@@ -18,7 +27,7 @@
    int ret = pthread_setprio(thread->osthread()->pthread_id(), newpri);
    return (ret == 0) ? OS_OK : OS_ERR;
  #elif defined(__APPLE__) || defined(__NetBSD__)
-@@ -2879,7 +2879,7 @@ OSReturn os::get_native_priority(const T
+@@ -3012,7 +3014,7 @@ OSReturn os::get_native_priority(const T
    }
  
    errno = 0;
