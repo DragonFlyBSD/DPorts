69d9952cb6dc6e7ba2f60c2b69049f6ddddcf5f7
DPorts Builder <nobody@home.ok>
2018-01-03 22:41:39 -0800

    Update graphics/appleseed to version 1.7.1.b

--- graphics/appleseed/dragonfly/patch-src_appleseed_foundation_platform_system.cpp
+++ graphics/appleseed/dragonfly/patch-src_appleseed_foundation_platform_system.cpp
@@ -1,26 +1,25 @@
---- src/appleseed/foundation/platform/system.cpp.orig	2016-09-30 11:36:29.000000000 +0300
+--- src/appleseed/foundation/platform/system.cpp.orig	2017-07-27 18:53:21.000000000 +0300
 +++ src/appleseed/foundation/platform/system.cpp
-@@ -77,6 +77,9 @@
- // FreeBSD.
- #elif defined __FreeBSD__
+@@ -75,7 +75,7 @@
+     #include <unistd.h>
  
-+// DragonFly.
-+#elif defined __DragonFly__
-+
+ // FreeBSD.
+-#elif defined __FreeBSD__
++#elif defined __FreeBSD__ || defined __DragonFly__
+ 
      #include <sys/types.h>
      #include <sys/resource.h>
-     #include <sys/sysctl.h>
-@@ -104,6 +107,9 @@ void System::print_information(Logger& l
+@@ -104,6 +104,9 @@ void System::print_information(Logger& l
          logger,
          "system information:\n"
-         "  logical cores    %s\n"
-+#ifdef __DragonFly__
-+        ,pretty_uint(get_logical_cpu_core_count()).c_str());
+         "  logical cores                 %s\n"
++#if defined __DragonFly__
++        , pretty_uint(get_logical_cpu_core_count()).c_str());
 +#else
-         "  L1 data cache    size %s, line size %s\n"
-         "  L2 cache         size %s, line size %s\n"
-         "  L3 cache         size %s, line size %s\n"
-@@ -118,6 +124,7 @@ void System::print_information(Logger& l
+         "  L1 data cache                 size %s, line size %s\n"
+         "  L2 cache                      size %s, line size %s\n"
+         "  L3 cache                      size %s, line size %s\n"
+@@ -118,6 +121,7 @@ void System::print_information(Logger& l
          pretty_size(get_l3_cache_line_size()).c_str(),
          pretty_size(get_total_physical_memory_size()).c_str(),
          pretty_size(get_total_virtual_memory_size()).c_str());
@@ -28,7 +27,7 @@
  }
  
  size_t System::get_logical_cpu_core_count()
-@@ -349,6 +356,13 @@ uint64 System::get_process_virtual_memor
+@@ -349,6 +353,13 @@ uint64 System::get_process_virtual_memor
      return info.resident_size;
  }
  
