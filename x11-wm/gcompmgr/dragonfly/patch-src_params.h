858eb836efe097d9e2b4ff1b970de098ebff9b4d
DPorts Builder <nobody@home.ok>
2018-02-05 01:32:40 -0800

    Import x11-wm/gcompmgr version 0.21_10

---
+++ x11-wm/gcompmgr/dragonfly/patch-src_params.h
@@ -0,0 +1,10 @@
+--- src/params.h.orig	2005-06-14 19:20:00.000000000 +0300
++++ src/params.h
+@@ -27,6 +27,7 @@
+ #include <cstdlib>
+ #include <cstdio>
+ #include <string>
++#include <cstring>
+ #include <vector>
+ #include <iostream>
+ #include <fstream>
---
+++ x11-wm/gcompmgr/dragonfly/patch-src_stdxinc.h
@@ -0,0 +1,10 @@
+--- src/stdxinc.h.orig	2005-06-14 08:18:34.000000000 +0300
++++ src/stdxinc.h
+@@ -14,6 +14,7 @@
+ #include <cstdlib>
+ #include <cstdio>
+ #include <string>
++#include <cstring>
+ #include <vector>
+ using namespace std;
+ 
