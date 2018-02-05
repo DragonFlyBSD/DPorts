6eed864eb1f89ce9ef961c94550d56729ddeaed4
DPorts Builder <nobody@home.ok>
2018-02-05 10:29:34 -0800

    Update devel/fpc-fcl-xml to version 3.0.4

--- devel/fpc-fcl-xml/dragonfly/patch-packages_fcl-xml_fpmake.pp
+++ devel/fpc-fcl-xml/dragonfly/patch-packages_fcl-xml_fpmake.pp
@@ -1,9 +1,9 @@
---- packages/fcl-xml/fpmake.pp.orig	2015-11-13 21:15:29 UTC
+--- packages/fcl-xml/fpmake.pp.orig	2017-09-02 16:48:24 UTC
 +++ packages/fcl-xml/fpmake.pp
 @@ -22,7 +22,7 @@ begin
      P.Options.Add('-S2h');
      D:=P.Dependencies.Add('fcl-base');
-       D.Version:='3.0.2';
+       D.Version:='3.0.4';
 -    D:=P.Dependencies.Add('iconvenc',[linux,darwin,iphonesim,freebsd,haiku,beos,aix]);
 +    D:=P.Dependencies.Add('iconvenc',[linux,darwin,iphonesim,dragonfly,freebsd,haiku,beos,aix]);
  
