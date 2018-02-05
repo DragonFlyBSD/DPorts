51bc61463f0ce6d233d765e4abf1876083b05062
DPorts Builder <nobody@home.ok>
2018-02-05 08:52:48 -0800

    Update math/z3 to version 4.6.0

--- math/z3/dragonfly/patch-scripts_mk__util.py
+++ math/z3/dragonfly/patch-scripts_mk__util.py
@@ -1,6 +1,6 @@
---- scripts/mk_util.py.intermediate	2016-12-17 16:11:47.000000000 +0200
+--- scripts/mk_util.py.orig	2018-02-05 13:43:46 UTC
 +++ scripts/mk_util.py
-@@ -605,7 +605,7 @@ elif os.name == 'posix':
+@@ -602,7 +602,7 @@ elif os.name == 'posix':
          PREFIX="/usr/local"
      elif os.uname()[0] == 'Linux':
          IS_LINUX=True
@@ -9,21 +9,21 @@
          IS_FREEBSD=True
      elif os.uname()[0] == 'OpenBSD':
          IS_OPENBSD=True
-@@ -1227,7 +1227,7 @@ def get_so_ext():
+@@ -1240,7 +1240,7 @@ def get_so_ext():
      sysname = os.uname()[0]
      if sysname == 'Darwin':
          return 'dylib'
 -    elif sysname == 'Linux' or sysname == 'FreeBSD' or sysname == 'OpenBSD':
 +    elif sysname == 'Linux' or sysname == 'FreeBSD' or sysname == 'OpenBSD' or sysname == 'DragonFly':
          return 'so'
-     elif sysname == 'CYGWIN':
+     elif sysname == 'CYGWIN' or sysname.startswith('MSYS_NT') or sysname.startswith('MINGW'):
          return 'dll'
-@@ -2444,7 +2444,7 @@ def mk_config():
+@@ -2492,7 +2492,7 @@ def mk_config():
              LDFLAGS        = '%s -lrt' % LDFLAGS
              SLIBFLAGS      = '-shared'
              SLIBEXTRAFLAGS = '%s -lrt' % SLIBEXTRAFLAGS
 -        elif sysname == 'FreeBSD':
 +        elif sysname == 'FreeBSD' or sysname == 'DragonFly':
-             CXXFLAGS       = '%s -fno-strict-aliasing -D_FREEBSD_' % CXXFLAGS
+             CXXFLAGS       = '%s -D_FREEBSD_' % CXXFLAGS
              OS_DEFINES     = '-D_FREEBSD_'
              SO_EXT         = '.so'
