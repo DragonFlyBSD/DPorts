321b8d6d70a867d279fea1afd8fac67b0a230047
DPorts Builder <nobody@home.ok>
2018-02-12 08:54:26 -0800

    Update devel/astyle to version 3.1

---
+++ devel/astyle/dragonfly/patch-src_astyle__main.cpp
@@ -0,0 +1,10 @@
+--- ../../src/astyle_main.cpp.orig	2018-01-11 17:18:19 UTC
++++ ../../src/astyle_main.cpp
+@@ -37,6 +37,7 @@
+ 
+ #include <algorithm>
+ #include <cerrno>
++#include <climits>
+ #include <clocale>		// needed by some compilers
+ #include <cstdlib>
+ #include <fstream>
