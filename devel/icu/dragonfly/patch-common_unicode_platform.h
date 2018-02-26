620b6e6d28df42b6043231c30f93956a105551a0
DPorts Builder <nobody@home.ok>
2018-02-26 07:48:30 -0800

    Tweak devel/icu version 60.2_2,1

--- devel/icu/dragonfly/patch-common_unicode_platform.h
+++ devel/icu/dragonfly/patch-common_unicode_platform.h
@@ -1,15 +1,29 @@
---- common/unicode/platform.h.orig	2016-09-30 03:57:59 UTC
+--- common/unicode/platform.h.intermediate	2018-02-26 15:38:13 UTC
 +++ common/unicode/platform.h
-@@ -159,8 +159,10 @@
+@@ -165,8 +165,8 @@
  #   else
  #       define U_PLATFORM U_PF_DARWIN
  #   endif
 -#elif defined(BSD) || defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__MirBSD__)
 -#   if defined(__FreeBSD__)
-+#elif defined(BSD) || defined(__FreeBSD__) || defined(__FreeBSD_kernel__) \
-+ || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__MirBSD__) \
-+ || defined(__DragonFly__)
++#elif defined(BSD) || defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__MirBSD__) || defined(__DragonFly__)
 +#   if defined(__FreeBSD__) || defined(__DragonFly__)
  #       include <sys/endian.h>
  #   endif
  #   define U_PLATFORM U_PF_BSD
+@@ -491,10 +491,14 @@
+ #   define U_CPLUSPLUS_VERSION 1
+ #endif
+ 
+-#if (U_PLATFORM == U_PF_AIX || U_PLATFORM == U_PF_OS390) && defined(__cplusplus) &&(U_CPLUSPLUS_VERSION < 11)
++#if defined(__cplusplus) && (U_CPLUSPLUS_VERSION < 11)
+ // add in std::nullptr_t
+ namespace std {
++#if (U_PLATFORM == U_PF_AIX || U_PLATFORM == U_PF_OS390)
+   typedef decltype(nullptr) nullptr_t;
++#elif defined(__GNUC__) && !defined(__clang__)
++  typedef __decltype(__null) nullptr_t;
++#endif
+ };
+ #endif
+ 
