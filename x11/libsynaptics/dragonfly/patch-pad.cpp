35b5bf1fb518bdea28430e7017431a3c95bdbcbd
DPorts Builder <nobody@home.ok>
2018-03-02 11:17:10 -0800

    Update x11/libsynaptics to version 0.14.6.c

---
+++ x11/libsynaptics/dragonfly/patch-pad.cpp
@@ -0,0 +1,11 @@
+--- pad.cpp.intermediate	2018-03-02 19:09:05 UTC
++++ pad.cpp
+@@ -2,6 +2,8 @@
+ #include "synparam.h"
+ #include "pad.h"
+ #include <pthread.h>
++#include <stdio.h>
++#include <stdlib.h>
+ #include <unistd.h>
+ #include <sys/shm.h>
+ 
