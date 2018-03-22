1609ee7ee8aa590b101c46b24c2a8e5c253e237a
DPorts Builder <nobody@home.ok>
2018-03-21 18:41:07 -0700

    Update devel/nspr to version 4.19

--- devel/nspr/dragonfly/patch-nsprpub_pr_src_pthreads_ptsynch.c
+++ devel/nspr/dragonfly/patch-nsprpub_pr_src_pthreads_ptsynch.c
@@ -1,9 +1,9 @@
---- pr/src/pthreads/ptsynch.c.orig	2014-06-20 12:34:59.616376000 +0000
+--- pr/src/pthreads/ptsynch.c.orig	2018-03-22 01:34:56 UTC
 +++ pr/src/pthreads/ptsynch.c
-@@ -919,7 +919,7 @@ PR_IMPLEMENT(PRStatus) PR_DeleteSemaphor
-  */
+@@ -913,7 +913,7 @@ PR_IMPLEMENT(PRStatus) PR_DeleteSemaphor
  #if (defined(__GNU_LIBRARY__) && !defined(_SEM_SEMUN_UNDEFINED)) \
-     || defined(FREEBSD) || defined(OPENBSD) || defined(BSDI) \
+     || (defined(FREEBSD) && __FreeBSD_version < 1200059) \
+     || defined(OPENBSD) || defined(BSDI) \
 -    || defined(DARWIN) || defined(SYMBIAN)
 +    || defined(DARWIN) || defined(SYMBIAN) || defined(__DragonFly__)
  /* union semun is defined by including <sys/sem.h> */
