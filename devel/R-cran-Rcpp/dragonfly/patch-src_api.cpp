871bf80cad52c716ecabda23201fc6a252bb12e8
DPorts Builder <nobody@home.ok>
2018-04-08 15:20:17 -0700

    Update devel/R-cran-Rcpp to version 0.12.16

--- devel/R-cran-Rcpp/dragonfly/patch-src_api.cpp
+++ devel/R-cran-Rcpp/dragonfly/patch-src_api.cpp
@@ -1,20 +1,20 @@
---- src/api.cpp.orig	2017-07-13 11:40:06 UTC
+--- src/api.cpp.orig	2018-03-08 17:04:51 UTC
 +++ src/api.cpp
 @@ -33,7 +33,7 @@ using namespace Rcpp;
  #endif
  
  #if defined(__GNUC__) || defined(__clang__)
--    #if defined(_WIN32) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__CYGWIN__) || defined(__sun) || defined(_AIX) || defined(__MUSL__) || defined(__HAIKU__)
-+    #if defined(_WIN32) || defined(__DragonFly__) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__CYGWIN__) || defined(__sun) || defined(_AIX) || defined(__MUSL__) || defined(__HAIKU__)
+-    #if defined(_WIN32) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__CYGWIN__) || defined(__sun) || defined(_AIX) || defined(__MUSL__) || defined(__HAIKU__) || defined(__ANDROID__)
++    #if defined(_WIN32) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__CYGWIN__) || defined(__sun) || defined(_AIX) || defined(__MUSL__) || defined(__HAIKU__) || defined(__ANDROID__) || defined(__DragonFly__)
          // do nothing
      #else
          #include <execinfo.h>
-@@ -272,7 +272,7 @@ SEXP rcpp_can_use_cxx11() {				// #nocov
+@@ -267,7 +267,7 @@ SEXP rcpp_can_use_cxx11() {				// #nocov
  // [[Rcpp::register]]
  SEXP stack_trace(const char* file, int line) {
      #if defined(__GNUC__) || defined(__clang__)
--        #if defined(_WIN32) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__CYGWIN__) || defined(__sun) || defined(_AIX) || defined(__MUSL__) || defined(__HAIKU__)
-+        #if defined(_WIN32) || defined(__DragonFly__) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__CYGWIN__) || defined(__sun) || defined(_AIX) || defined(__MUSL__) || defined(__HAIKU__)
+-        #if defined(_WIN32) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__CYGWIN__) || defined(__sun) || defined(_AIX) || defined(__MUSL__) || defined(__HAIKU__) || defined(__ANDROID__)
++        #if defined(_WIN32) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__CYGWIN__) || defined(__sun) || defined(_AIX) || defined(__MUSL__) || defined(__HAIKU__) || defined(__ANDROID__) || defined(__DragonFly__)
              // Simpler version for Windows and *BSD
              List trace = List::create(_["file"] = file,
                                        _[ "line"  ] = line,
