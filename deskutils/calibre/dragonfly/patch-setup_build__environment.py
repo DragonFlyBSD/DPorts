3bd561228fa7ab1ec7cf4540d7651f9b91d298d9
DPorts Builder <nobody@home.ok>
2018-04-09 09:50:12 -0700

    Update deskutils/calibre to version 3.21.0

--- deskutils/calibre/dragonfly/patch-setup_build__environment.py
+++ deskutils/calibre/dragonfly/patch-setup_build__environment.py
@@ -1,14 +1,14 @@
---- setup/build_environment.py.orig	2018-02-12 17:46:47 UTC
+--- setup/build_environment.py.orig	2018-04-09 16:44:19 UTC
 +++ setup/build_environment.py
-@@ -86,10 +86,8 @@ pyqt['sip_flags'] = PYQT_CONFIGURATION['
+@@ -96,10 +96,8 @@ pyqt['sip_flags'] = PYQT_CONFIGURATION['
  def get_sip_dir():
      if iswindows:
-         q = os.environ.get('SIP_DIR', os.path.join(sys.prefix, 'share', 'sip'))
+         q = os.path.join(sys.prefix, 'share', 'sip')
 -    elif isfreebsd:
--        q = os.environ.get('SIP_DIR', os.path.join(sys.prefix, 'share', 'py-sip'))
+-        q = os.path.join(sys.prefix, 'share', 'py-sip')
      else:
 -        q = os.path.join(sys.prefix, 'share', 'sip')
-+        q = os.environ.get('SIP_DIR', os.path.join(sys.prefix, 'share', 'py-sip'))
++        q = os.path.join(sys.prefix, 'share', 'py-sip')
+     q = os.environ.get('SIP_DIR', q)
      for x in ('', 'Py2-PyQt5', 'PyQt5', 'sip/PyQt5'):
          base = os.path.join(q, x)
-         if os.path.exists(os.path.join(base, 'QtWidgets')):
