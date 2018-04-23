c74daab10abe57f8aacb36f0bf1f8f250f6eb9df
DPorts Builder <nobody@home.ok>
2018-04-23 08:45:54 -0700

    Update lang/rust to version 1.25.0

--- lang/rust/dragonfly/patch-cmake_modules_HandleLLVMOptions.cmake
+++ lang/rust/dragonfly/patch-cmake_modules_HandleLLVMOptions.cmake
@@ -1,10 +1,10 @@
---- src/llvm/cmake/modules/HandleLLVMOptions.cmake.orig	2017-06-26 10:55:45.047965000 +0200
-+++ src/llvm/cmake/modules/HandleLLVMOptions.cmake	2017-06-26 10:55:52.978184000 +0200
-@@ -106,6 +106,7 @@
-         ${TARGET_TRIPLE} MATCHES "windows" OR
-         ${TARGET_TRIPLE} MATCHES "mingw" OR
-         ${TARGET_TRIPLE} MATCHES "freebsd" OR
-+        ${TARGET_TRIPLE} MATCHES "dragonfly" OR
-         ${TARGET_TRIPLE} MATCHES "netbsd" OR
-         ${TARGET_TRIPLE} MATCHES "openbsd") AND
+--- src/llvm/cmake/modules/HandleLLVMOptions.cmake.orig	2018-03-10 02:51:13 UTC
++++ src/llvm/cmake/modules/HandleLLVMOptions.cmake
+@@ -146,6 +146,7 @@ endif()
+ # build might work on ELF but fail on MachO/COFF.
+ if(NOT (${CMAKE_SYSTEM_NAME} MATCHES "Darwin" OR WIN32 OR CYGWIN OR
+         ${CMAKE_SYSTEM_NAME} MATCHES "FreeBSD" OR
++        ${CMAKE_SYSTEM_NAME} MATCHES "DragonFly" OR
+         ${CMAKE_SYSTEM_NAME} MATCHES "OpenBSD") AND
     NOT LLVM_USE_SANITIZER)
+   set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -Wl,-z,defs")
