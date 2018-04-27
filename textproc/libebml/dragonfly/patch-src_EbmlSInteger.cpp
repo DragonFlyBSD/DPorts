cb0a4b05c13569132f0affaeb55627c19d0688bf
DPorts Builder <nobody@home.ok>
2018-04-26 21:42:44 -0700

    Update textproc/libebml to version 1.3.6

---
+++ textproc/libebml/dragonfly/patch-src_EbmlSInteger.cpp
@@ -0,0 +1,10 @@
+--- src/EbmlSInteger.cpp.orig	2018-04-20 18:27:20 UTC
++++ src/EbmlSInteger.cpp
+@@ -34,6 +34,7 @@
+ */
+ #include <cassert>
+ #include <climits>
++#include <limits>
+ 
+ #include "ebml/EbmlSInteger.h"
+ 
