f0c6df05f3237afb570e3c9495d354c3be43fedc
DPorts Builder <nobody@home.ok>
2018-05-09 05:17:18 -0700

    Update devel/libvirt to version 4.3.0

--- devel/libvirt/dragonfly/patch-src_security_security__dac.c
+++ devel/libvirt/dragonfly/patch-src_security_security__dac.c
@@ -1,11 +1,11 @@
---- src/security/security_dac.c.orig	2016-01-17 01:24:09 UTC
+--- src/security/security_dac.c.orig	2018-03-27 12:54:04 UTC
 +++ src/security/security_dac.c
-@@ -1425,7 +1425,7 @@ virSecurityDACGetProcessLabelInternal(pi
+@@ -1963,7 +1963,7 @@ virSecurityDACGetProcessLabelInternal(pi
      VIR_FREE(path);
      return ret;
  }
 -#elif defined(__FreeBSD__)
-+#elif defined(__FreeBSD__) && ! defined __DragonFly__
++#elif defined(__FreeBSD__) && !defined(__DragonFly__)
  static int
  virSecurityDACGetProcessLabelInternal(pid_t pid,
                                        virSecurityLabelPtr seclabel)
