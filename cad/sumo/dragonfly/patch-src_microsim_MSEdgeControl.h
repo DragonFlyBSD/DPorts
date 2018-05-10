42b177bbbd855d014cf42af8c246aff0cfb537f7
DPorts Builder <nobody@home.ok>
2018-05-10 02:33:36 -0700

    Tweak cad/sumo version 0.31.0

---
+++ cad/sumo/dragonfly/patch-src_microsim_MSEdgeControl.h
@@ -0,0 +1,10 @@
+--- src/microsim/MSEdgeControl.h.orig	2017-02-17 22:31:23.000000000 +0000
++++ src/microsim/MSEdgeControl.h
+@@ -43,6 +43,7 @@
+ #include <set>
+ #include <utils/common/SUMOTime.h>
+ #include <utils/common/Named.h>
++#include <microsim/MSLane.h>
+ 
+ 
+ // ===========================================================================
