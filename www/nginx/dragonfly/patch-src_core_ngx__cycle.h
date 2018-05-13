b96a3240c2a0fd7d1f4a040146b621bd05072f06
DPorts Builder <nobody@home.ok>
2018-05-13 09:49:13 -0700

    Update www/nginx to version 1.14.0_1,2

--- www/nginx/dragonfly/patch-src_core_ngx__cycle.h
+++ www/nginx/dragonfly/patch-src_core_ngx__cycle.h
@@ -1,10 +1,10 @@
---- src/core/ngx_cycle.h.orig	2016-04-26 13:31:19 UTC
+--- src/core/ngx_cycle.h.orig	2018-05-13 14:48:24 UTC
 +++ src/core/ngx_cycle.h
-@@ -108,6 +108,7 @@ typedef struct {
+@@ -115,6 +115,7 @@ typedef struct {
  
      ngx_array_t               env;
      char                    **environment;
 +    unsigned                  so_reuseport:1;
+ 
+     ngx_uint_t                transparent;  /* unsigned  transparent:1; */
  } ngx_core_conf_t;
- 
- 
