80aec164427e366889d686a8c04a5ff150deb054
DPorts Builder <nobody@home.ok>
2018-06-04 20:02:07 -0700

    Update devel/lxqt-build-tools to version 0.5.0

--- devel/lxqt-build-tools/dragonfly/patch-cmake_modules_LXQtCreatePkgConfigFile.cmake
+++ devel/lxqt-build-tools/dragonfly/patch-cmake_modules_LXQtCreatePkgConfigFile.cmake
@@ -1,9 +1,9 @@
---- cmake/modules/LXQtCreatePkgConfigFile.cmake.orig	2017-01-01 23:46:43.000000000 +0200
+--- cmake/modules/LXQtCreatePkgConfigFile.cmake.orig	2018-05-21 17:06:51 UTC
 +++ cmake/modules/LXQtCreatePkgConfigFile.cmake
 @@ -233,7 +233,7 @@ function(lxqt_create_pkgconfig_file)
      if (DEFINED USER_INSTALL)
          # FreeBSD loves to install files to different locations
-         # http://www.freebsd.org/doc/handbook/dirstructure.html
+         # https://www.freebsd.org/doc/handbook/dirstructure.html
 -        if(${CMAKE_SYSTEM_NAME} STREQUAL "FreeBSD")
 +        if(${CMAKE_SYSTEM_NAME} STREQUAL "FreeBSD" OR ${CMAKE_SYSTEM_NAME} STREQUAL "DragonFly")
              set(_PKGCONFIG_INSTALL_DESTINATION "libdata/pkgconfig")
