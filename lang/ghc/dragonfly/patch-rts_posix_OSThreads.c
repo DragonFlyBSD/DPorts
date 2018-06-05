fd5b03f5de0c9645cae931280c4e09721c1ddfe1
DPorts Builder <nobody@home.ok>
2018-06-04 19:06:40 -0700

    Update lang/ghc to version 8.4.2

--- lang/ghc/dragonfly/patch-rts_posix_OSThreads.c
+++ lang/ghc/dragonfly/patch-rts_posix_OSThreads.c
@@ -1,7 +1,7 @@
---- rts/posix/OSThreads.c.orig	2014-12-23 04:31:10.000000000 +0200
+--- rts/posix/OSThreads.c.orig	2018-02-01 04:51:19 UTC
 +++ rts/posix/OSThreads.c
-@@ -21,6 +21,12 @@
- #define __BSD_VISIBLE   1
+@@ -20,6 +20,12 @@
+ #define _DARWIN_C_SOURCE 1
  #endif
  
 +#if defined(dragonfly_HOST_OS)
@@ -13,7 +13,7 @@
  #include "Rts.h"
  
  #if defined(linux_HOST_OS)
-@@ -31,7 +37,7 @@
+@@ -30,7 +36,7 @@
  
  #if defined(HAVE_PTHREAD_H)
  #include <pthread.h>
@@ -22,7 +22,7 @@
  #include <pthread_np.h>
  #endif
  #endif
-@@ -44,7 +50,7 @@
+@@ -42,7 +48,7 @@
  #include <string.h>
  #endif
  
@@ -31,12 +31,12 @@
  #include <sys/types.h>
  #include <sys/sysctl.h>
  #endif
-@@ -243,7 +249,7 @@ getNumberOfProcessors (void)
-         nproc = sysconf(_SC_NPROCESSORS_ONLN);
- #elif defined(HAVE_SYSCONF) && defined(_SC_NPROCESSORS_CONF)
-         nproc = sysconf(_SC_NPROCESSORS_CONF);
--#elif defined(darwin_HOST_OS) || defined(freebsd_HOST_OS)
-+#elif defined(darwin_HOST_OS) || defined(freebsd_HOST_OS) || defined(dragonfly_HOST_OS)
-         size_t size = sizeof(nat);
-         if(0 != sysctlbyname("hw.ncpu",&nproc,&size,NULL,0))
+@@ -256,7 +262,7 @@ getNumberOfProcessors (void)
+             if(sysctlbyname("hw.ncpu",&nproc,&size,NULL,0) != 0)
+                 nproc = 1;
+         }
+-#elif defined(freebsd_HOST_OS)
++#elif defined(freebsd_HOST_OS) || defined(dragonfly_HOST_OS)
+         size_t size = sizeof(uint32_t);
+         if(sysctlbyname("hw.ncpu",&nproc,&size,NULL,0) != 0)
              nproc = 1;
