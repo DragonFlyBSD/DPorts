6da67845968fdec113f0c415df1028f337495231
DPorts Builder <nobody@home.ok>
2018-07-09 08:23:11 -0700

    Update security/i2pd to version 2.19.0

--- security/i2pd/dragonfly/patch-libi2pd_I2PEndian.h
+++ security/i2pd/dragonfly/patch-libi2pd_I2PEndian.h
@@ -1,11 +1,11 @@
---- libi2pd/I2PEndian.h.orig	2017-12-29 02:41:52 UTC
+--- libi2pd/I2PEndian.h.orig	2018-06-26 17:46:01 UTC
 +++ libi2pd/I2PEndian.h
 @@ -3,7 +3,7 @@
  #include <inttypes.h>
  #include <string.h>
  
--#if defined(__FreeBSD__)
-+#if defined(__FreeBSD__) || defined(__DragonFly__)
+-#if defined(__FreeBSD__) || defined(__NetBSD__)
++#if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__DragonFly__)
  #include <sys/endian.h>
- #elif defined(__linux__) || defined(__FreeBSD_kernel__) || defined(__OpenBSD__)
+ #elif defined(__linux__) || defined(__FreeBSD_kernel__) || defined(__OpenBSD__) || defined(__GLIBC__)
  #include <endian.h>
