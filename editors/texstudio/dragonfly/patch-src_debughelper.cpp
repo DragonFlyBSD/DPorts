f5b4728975b5d1a359f660888e3aa03d04779a04
DPorts Builder <nobody@home.ok>
2018-07-09 09:18:37 -0700

    Update editors/texstudio to version 2.12.8

--- editors/texstudio/dragonfly/patch-src_debughelper.cpp
+++ editors/texstudio/dragonfly/patch-src_debughelper.cpp
@@ -1,15 +1,15 @@
---- debughelper.cpp.orig	2015-10-25 15:22:30 UTC
-+++ debughelper.cpp
-@@ -27,7 +27,7 @@
+--- src/debughelper.cpp.orig	2018-01-29 19:09:36 UTC
++++ src/debughelper.cpp
+@@ -33,7 +33,7 @@
  #endif
  
  
 -#if (defined(__unix__) || defined(unix) || defined(__linux__) || defined(linux) || defined(__gnu_hurd__) || defined(__FreeBSD__) || defined(__FreeBSD_kernel__))
-+#if (defined(__unix__) || defined(unix) || defined(__linux__) || defined(linux) || defined(__gnu_hurd__) || defined(__FreeBSD__) || defined(__DragonFly__))
++#if (defined(__unix__) || defined(unix) || defined(__linux__) || defined(linux) || defined(__gnu_hurd__) || defined(__FreeBSD__) || defined(__FreeBSD_kernel__)) || defined(__DragonFly__)
  #define OS_IS_LINUX_LIKE
  #endif
  
-@@ -486,7 +486,7 @@ QString print_backtrace(const QString &m
+@@ -492,7 +492,7 @@ QString print_backtrace(const QString &m
  
  #define USE_SIGNAL_HANDLER
  #ifdef CPU_IS_X86_64
@@ -18,7 +18,7 @@
  #define PC_FROM_UCONTEXT(context) (context)->uc_mcontext.mc_rip
  #define STACK_FROM_UCONTEXT(context) (context)->uc_mcontext.mc_rsp
  #define FRAME_FROM_UCONTEXT(context) (context)->uc_mcontext.mc_rbp
-@@ -496,7 +496,7 @@ QString print_backtrace(const QString &m
+@@ -502,7 +502,7 @@ QString print_backtrace(const QString &m
  #define FRAME_FROM_UCONTEXT(context) (context)->uc_mcontext.gregs[REG_RBP]
  #endif
  #elif defined(CPU_IS_X86_32)
@@ -27,7 +27,7 @@
  #define PC_FROM_UCONTEXT(context) (context)->uc_mcontext.mc_eip
  #define STACK_FROM_UCONTEXT(context) (context)->uc_mcontext.mc_esp
  #define FRAME_FROM_UCONTEXT(context) (context)->uc_mcontext.mc_ebp
-@@ -545,8 +545,8 @@ QString print_backtrace(const QString &m
+@@ -551,8 +551,8 @@ QString print_backtrace(const QString &m
  #error Unknown cpu architecture
  #endif
  
