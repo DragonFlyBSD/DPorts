bab414cfcd50c0f0f6a6e89c5a35438377b6cda3
DPorts Builder <nobody@home.ok>
2018-07-09 09:04:49 -0700

    Update math/z3 to version 4.7.1

--- math/z3/dragonfly/patch-scripts_mk__util.py
+++ math/z3/dragonfly/patch-scripts_mk__util.py
@@ -1,24 +1,24 @@
---- scripts/mk_util.py.orig	2018-02-05 13:43:46 UTC
+--- scripts/mk_util.py.orig	2018-07-09 15:47:08 UTC
 +++ scripts/mk_util.py
-@@ -602,7 +602,7 @@ elif os.name == 'posix':
+@@ -606,7 +606,7 @@ elif os.name == 'posix':
          PREFIX="/usr/local"
      elif os.uname()[0] == 'Linux':
          IS_LINUX=True
 -    elif os.uname()[0] == 'FreeBSD':
 +    elif os.uname()[0] == 'FreeBSD' or os.uname()[0] == 'DragonFly':
          IS_FREEBSD=True
-     elif os.uname()[0] == 'OpenBSD':
-         IS_OPENBSD=True
-@@ -1240,7 +1240,7 @@ def get_so_ext():
+     elif os.uname()[0] == 'NetBSD':
+         IS_NETBSD=True
+@@ -1251,7 +1251,7 @@ def get_so_ext():
      sysname = os.uname()[0]
      if sysname == 'Darwin':
          return 'dylib'
--    elif sysname == 'Linux' or sysname == 'FreeBSD' or sysname == 'OpenBSD':
-+    elif sysname == 'Linux' or sysname == 'FreeBSD' or sysname == 'OpenBSD' or sysname == 'DragonFly':
+-    elif sysname == 'Linux' or sysname == 'FreeBSD' or sysname == 'NetBSD' or sysname == 'OpenBSD':
++    elif sysname == 'Linux' or sysname == 'FreeBSD' or sysname == 'NetBSD' or sysname == 'OpenBSD' or sysname == 'DragonFly':
          return 'so'
      elif sysname == 'CYGWIN' or sysname.startswith('MSYS_NT') or sysname.startswith('MINGW'):
          return 'dll'
-@@ -2492,7 +2492,7 @@ def mk_config():
+@@ -2511,7 +2511,7 @@ def mk_config():
              LDFLAGS        = '%s -lrt' % LDFLAGS
              SLIBFLAGS      = '-shared'
              SLIBEXTRAFLAGS = '%s -lrt' % SLIBEXTRAFLAGS
