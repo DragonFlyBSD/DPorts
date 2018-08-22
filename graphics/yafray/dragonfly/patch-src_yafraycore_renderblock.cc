ba95a4e10fcb3b859e438a1584ac59e7fc910846
DPorts Builder <nobody@home.ok>
2018-08-22 09:40:42 -0700

    Prune 90 obsolete ports
    FreeBSD Ports maintainers have already deleted the following:
      astro/p5-ZConf-Weather
      audio/libmusicbrainz3
      cad/kicad-library-devel
      comms/xwota
      databases/pgpool-II-33
      databases/rubygem-redis-namespace15
      deskutils/p5-ZConf-BGSet
      deskutils/p5-ZConf-Runner
      deskutils/p5-ZConf-Runner-GUI-GTK
      devel/p5-ZConf-GUI
      devel/py-levenshtein
      devel/rubygem-grape-entity060
      emulators/snes9express
      emulators/snes9x
      graphics/p5-Image-Size-FillFullSelect
      graphics/p5-Imager-AverageGray
      graphics/projectm-libvisual
      graphics/yafray
      lang/clang38
      lang/spec.alpha
      mail/p5-Mail-Cache
      mail/p5-Mail-IMAPTalk-MailCache
      mail/p5-ZConf-Mail
      net-p2p/transmission-qt4
      net-p2p/transmission-qt5
      net/isc-dhcp43-client
      net/isc-dhcp43-relay
      net/isc-dhcp43-server
      net/p5-ZConf
      net/p5-ZConf-Bookmarks
      net/rubygem-asset_sync220
      net/rubygem-gitaly-proto095
      net/rubygem-gitaly-proto096
      net/rubygem-gitaly-proto099
      net/rubygem-omniauth-github11
      net/wpa_gui
      security/libbeid
      security/opensaml2
      security/py-python-social-auth
      security/rubygem-omniauth-shibboleth12
      security/rubygem-rbnacl4
      security/shibboleth2-sp
      sysutils/logstash
      sysutils/p5-ZConf-Cron
      textproc/kibana46
      textproc/p5-Text-SpellChecker-GUI
      textproc/rubygem-html-pipeline-gitlab
      www/p5-ZConf-RSS
      www/p5-ZConf-RSS-GUI-GTK
      www/py-django-keyedcache
      www/py-django-signals-ahoy
      www/py-django-social-auth
      www/py-django-threaded-multihost
      www/rubygem-gollum-lib-gitlab
      www/rubygem-gon61
      x11-fm/p5-PerlFM
      x11-fonts/fontcacheproto
      x11-fonts/fontsproto
      x11-fonts/hasklig
      x11-fonts/xf86bigfontproto
      x11/bigreqsproto
      x11/compositeproto
      x11/damageproto
      x11/dmxproto
      x11/dri2proto
      x11/dri3proto
      x11/evieext
      x11/fixesproto
      x11/glproto
      x11/inputproto
      x11/kbproto
      x11/presentproto
      x11/printproto
      x11/randrproto
      x11/recordproto
      x11/renderproto
      x11/resourceproto
      x11/scrnsaverproto
      x11/trapproto
      x11/videoproto
      x11/xcmiscproto
      x11/xextproto
      x11/xf86dgaproto
      x11/xf86driproto
      x11/xf86miscproto
      x11/xf86rushproto
      x11/xf86vidmodeproto
      x11/xineramaproto
      x11/xproto
      x11/xproxymanagementprotocol

--- graphics/yafray/dragonfly/patch-src_yafraycore_renderblock.cc
+++
@@ -1,9 +0,0 @@
---- src/yafraycore/renderblock.cc.orig	2005-05-30 09:28:19.000000000 +0300
-+++ src/yafraycore/renderblock.cc
-@@ -1,5 +1,6 @@
- 
- #include "renderblock.h"
-+#include <cstdlib> // for rand()
- 
- using namespace std;
- 
