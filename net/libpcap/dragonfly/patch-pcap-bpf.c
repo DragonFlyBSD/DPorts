9ab445aa0c3714ff3766c0cdb26bfc467d82e168
DPorts Builder <nobody@home.ok>
2018-08-26 12:30:44 -0700

    Update net/libpcap to version 1.9.0

--- net/libpcap/dragonfly/patch-pcap-bpf.c
+++ net/libpcap/dragonfly/patch-pcap-bpf.c
@@ -1,6 +1,6 @@
---- pcap-bpf.c.orig	2016-10-26 00:07:59 UTC
+--- pcap-bpf.c.orig	2018-06-24 17:54:20 UTC
 +++ pcap-bpf.c
-@@ -48,7 +48,7 @@
+@@ -45,7 +45,7 @@
  #endif
  #include <sys/utsname.h>
  
@@ -9,7 +9,7 @@
  /*
   * Add support for capturing on FreeBSD usbusN interfaces.
   */
-@@ -1451,7 +1451,7 @@ pcap_cleanup_bpf(pcap_t *p)
+@@ -1459,7 +1459,7 @@ pcap_cleanup_bpf(pcap_t *p)
  		}
  #endif /* HAVE_BSD_IEEE80211 */
  
@@ -18,7 +18,7 @@
  		/*
  		 * Attempt to destroy the usbusN interface that we created.
  		 */
-@@ -1825,7 +1825,7 @@ pcap_activate_bpf(pcap_t *p)
+@@ -1843,7 +1843,7 @@ pcap_activate_bpf(pcap_t *p)
  	 * If this is FreeBSD, and the device name begins with "usbus",
  	 * try to create the interface if it's not available.
  	 */
@@ -27,21 +27,21 @@
  	if (strncmp(p->opt.device, usbus_prefix, USBUS_PREFIX_LEN) == 0) {
  		/*
  		 * Do we already have an interface with that name?
-@@ -2582,7 +2582,7 @@ check_bpf_bindable(const char *name)
+@@ -2639,7 +2639,7 @@ check_bpf_bindable(const char *name)
  	return (1);
  }
  
 -#if defined(__FreeBSD__) && defined(SIOCIFCREATE2)
 +#if (defined(__FreeBSD__) || defined(__DragonFly__)) && defined(SIOCIFCREATE2)
  static int
- finddevs_usb(pcap_if_t **alldevsp, char *errbuf)
+ get_usb_if_flags(const char *name _U_, bpf_u_int32 *flags _U_, char *errbuf _U_)
  {
-@@ -2662,7 +2662,7 @@ pcap_platform_finddevs(pcap_if_t **allde
- 	if (pcap_findalldevs_interfaces(alldevsp, errbuf, check_bpf_bindable) == -1)
+@@ -2835,7 +2835,7 @@ pcap_platform_finddevs(pcap_if_list_t *d
+ 	    get_if_flags) == -1)
  		return (-1);	/* failure */
  
 -#if defined(__FreeBSD__) && defined(SIOCIFCREATE2)
 +#if (defined(__FreeBSD__) || defined(__DragonFly__)) && defined(SIOCIFCREATE2)
- 	if (finddevs_usb(alldevsp, errbuf) == -1)
+ 	if (finddevs_usb(devlistp, errbuf) == -1)
  		return (-1);
  #endif
