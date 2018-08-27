e147fc93739546d9f7a72e32dab5f72bd718b921
DPorts Builder <nobody@home.ok>
2018-08-27 12:43:57 -0700

    Update graphics/opencollada to version 1.6.63_1

--- graphics/opencollada/dragonfly/patch-COLLADABaseUtils_include_COLLADABUhash__map.h
+++ graphics/opencollada/dragonfly/patch-COLLADABaseUtils_include_COLLADABUhash__map.h
@@ -1,11 +1,11 @@
---- COLLADABaseUtils/include/COLLADABUhash_map.h.orig	2014-05-27 23:55:46 UTC
+--- COLLADABaseUtils/include/COLLADABUhash_map.h.orig	2018-08-27 19:38:57 UTC
 +++ COLLADABaseUtils/include/COLLADABUhash_map.h
 @@ -60,7 +60,7 @@
          #define COLLADABU_HASH_NAMESPACE_CLOSE }
          #define COLLADABU_HASH_FUN hash
      #endif
--#elif defined(__FreeBSD__) && defined(_LIBCPP_VERSION)
-+#elif (defined(__FreeBSD__) || defined(__DragonFly__)) && defined(_LIBCPP_VERSION)
+-#elif (defined(__APPLE__) || defined(__FreeBSD__)) && defined(_LIBCPP_VERSION)
++#elif (defined(__APPLE__) || defined(__FreeBSD__) || defined(__DragonFly__)) && defined(_LIBCPP_VERSION)
      #include <unordered_map>
      #include <unordered_set>
      #define COLLADABU_HASH_MAP std::unordered_map
