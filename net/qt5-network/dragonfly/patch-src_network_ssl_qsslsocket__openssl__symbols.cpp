9972bf9b2a83958571ec2fec46cb42305fa3267b
DPorts Builder <nobody@home.ok>
2018-10-24 17:49:59 -0700

    Tweak net/qt5-network version 5.11.2

--- net/qt5-network/dragonfly/patch-src_network_ssl_qsslsocket__openssl__symbols.cpp
+++ net/qt5-network/dragonfly/patch-src_network_ssl_qsslsocket__openssl__symbols.cpp
@@ -1,11 +1,20 @@
 --- src/network/ssl/qsslsocket_openssl_symbols.cpp.intermediate	2018-10-23 13:44:02 UTC
 +++ src/network/ssl/qsslsocket_openssl_symbols.cpp
-@@ -402,7 +402,7 @@ DEFINEFUNC(int, SSL_connect, SSL *a, a,
- DEFINEFUNC(int, SSL_CTX_check_private_key, const SSL_CTX *a, a, return -1, return)
- DEFINEFUNC4(long, SSL_CTX_ctrl, SSL_CTX *a, a, int b, b, long c, c, void *d, d, return -1, return)
- DEFINEFUNC(void, SSL_CTX_free, SSL_CTX *a, a, return, DUMMYARG)
--#if OPENSSL_VERSION_NUMBER >= 0x10000000L
-+#if OPENSSL_VERSION_NUMBER >= 0x10000000L && !defined(LIBRESSL_VERSION_NUMBER)
- DEFINEFUNC(SSL_CTX *, SSL_CTX_new, const SSL_METHOD *a, a, return 0, return)
- #else
- DEFINEFUNC(SSL_CTX *, SSL_CTX_new, SSL_METHOD *a, a, return 0, return)
+@@ -417,7 +417,7 @@ DEFINEFUNC2(int, SSL_CTX_use_PrivateKey,
+ DEFINEFUNC2(int, SSL_CTX_use_RSAPrivateKey, SSL_CTX *a, a, RSA *b, b, return -1, return)
+ DEFINEFUNC3(int, SSL_CTX_use_PrivateKey_file, SSL_CTX *a, a, const char *b, b, int c, c, return -1, return)
+ DEFINEFUNC(X509_STORE *, SSL_CTX_get_cert_store, const SSL_CTX *a, a, return 0, return)
+-#if OPENSSL_VERSION_NUMBER >= 0x10002000L
++#if OPENSSL_VERSION_NUMBER >= 0x10002000L && !defined(LIBRESSL_VERSION_NUMBER)
+ DEFINEFUNC(SSL_CONF_CTX *, SSL_CONF_CTX_new, DUMMYARG, DUMMYARG, return 0, return);
+ DEFINEFUNC(void, SSL_CONF_CTX_free, SSL_CONF_CTX *a, a, return ,return);
+ DEFINEFUNC2(void, SSL_CONF_CTX_set_ssl_ctx, SSL_CONF_CTX *a, a, SSL_CTX *b, b, return, return);
+@@ -1156,7 +1156,7 @@ bool q_resolveOpenSslSymbols()
+     RESOLVEFUNC(SSL_CTX_use_RSAPrivateKey)
+     RESOLVEFUNC(SSL_CTX_use_PrivateKey_file)
+     RESOLVEFUNC(SSL_CTX_get_cert_store);
+-#if OPENSSL_VERSION_NUMBER >= 0x10002000L
++#if OPENSSL_VERSION_NUMBER >= 0x10002000L && !defined(LIBRESSL_VERSION_NUMBER)
+     RESOLVEFUNC(SSL_CONF_CTX_new);
+     RESOLVEFUNC(SSL_CONF_CTX_free);
+     RESOLVEFUNC(SSL_CONF_CTX_set_ssl_ctx);
--- net/qt5-network/dragonfly/patch-src_network_ssl_qsslsocket__openssl__symbols__p.h
+++ net/qt5-network/dragonfly/patch-src_network_ssl_qsslsocket__openssl__symbols__p.h
@@ -1,6 +1,19 @@
 --- src/network/ssl/qsslsocket_openssl_symbols_p.h.orig	2018-09-13 04:25:10 UTC
 +++ src/network/ssl/qsslsocket_openssl_symbols_p.h
-@@ -356,7 +356,7 @@ int q_SSL_CTX_use_PrivateKey(SSL_CTX *a,
+@@ -74,6 +74,12 @@
+ 
+ QT_BEGIN_NAMESPACE
+ 
++#if defined(LIBRESSL_VERSION_NUMBER) && LIBRESSL_VERSION_NUMBER >= 0x20700000L
++# define TLS1_2_VERSION 0x0303
++# define TLS_MAX_VERSION TLS1_2_VERSION
++# define TLS_ANY_VERSION 0x10000
++#endif
++
+ #define DUMMYARG
+ 
+ #if !defined QT_LINKED_OPENSSL
+@@ -356,7 +362,7 @@ int q_SSL_CTX_use_PrivateKey(SSL_CTX *a,
  int q_SSL_CTX_use_RSAPrivateKey(SSL_CTX *a, RSA *b);
  int q_SSL_CTX_use_PrivateKey_file(SSL_CTX *a, const char *b, int c);
  X509_STORE *q_SSL_CTX_get_cert_store(const SSL_CTX *a);
