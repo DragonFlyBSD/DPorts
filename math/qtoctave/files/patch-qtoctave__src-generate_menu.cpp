3e3de777bb26f2a4fc91570a7b8f7ee9d40a4b36
DPorts Builder <nobody@home.ok>
2018-11-25 20:18:31 -0800

    Tweak math/qtoctave version 0.10.1_4

--- math/qtoctave/files/patch-qtoctave__src-generate_menu.cpp
+++ math/qtoctave/files/patch-qtoctave__src-generate_menu.cpp
@@ -1,4 +1,4 @@
---- qtoctave/src/generate_menu.cpp.orig	2011-01-06 21:48:07.000000000 +0000
+--- qtoctave/src/generate_menu.cpp.orig	2018-11-05 13:09:19 UTC
 +++ qtoctave/src/generate_menu.cpp
 @@ -279,7 +279,7 @@ bool GenerateMenu::process_menu_file(QSt
  MenuExtCallBack *GenerateMenu::process_menu_file(QString _file)
