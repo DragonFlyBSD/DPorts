b2b22d99cd603d42f092a4d80faaaf70c102fdf0
DPorts Builder <nobody@home.ok>
2018-11-26 06:54:27 -0800

    Update x11-toolkits/gtk30 to version 3.22.30_3

--- x11-toolkits/gtk30/dragonfly/patch-gdk_wayland_gdkdevice-wayland.c
+++ x11-toolkits/gtk30/dragonfly/patch-gdk_wayland_gdkdevice-wayland.c
@@ -9,7 +9,7 @@
  #include <sys/time.h>
  #include <sys/mman.h>
 -#include <linux/input.h>
-+#include <dev/evdev/input.h>
++#include <dev/misc/evdev/input.h>
  
  #define BUTTON_BASE (BTN_LEFT - 1) /* Used to translate to 1-indexed buttons */
  
