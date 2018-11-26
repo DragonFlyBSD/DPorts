5e5580aaaf805d80848e2de4c3eead236c0675fc
DPorts Builder <nobody@home.ok>
2018-11-25 20:03:36 -0800

    Tweak devel/injeqt version 1.2.0_2

--- devel/injeqt/files/patch-src_internal_containers.h
+++ devel/injeqt/files/patch-src_internal_containers.h
@@ -1,10 +1,10 @@
---- src/internal/containers.h.orig	2017-04-21 09:40:08.000000000 +0000
+--- src/internal/containers.h.orig	2018-10-26 17:05:42 UTC
 +++ src/internal/containers.h
 @@ -23,6 +23,7 @@
  #include "internal.h"
  
  #include <algorithm>
-+#include <functional>  // for std::function
++#include <functional>
  #include <vector>
  
  /**
