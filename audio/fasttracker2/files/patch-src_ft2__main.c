2fc7362ddf013100589d1d12375bbb3db0637f01
DPorts Builder <nobody@home.ok>
2018-11-28 09:24:49 -0800

    Update audio/fasttracker2 to version 0.1.b120

--- audio/fasttracker2/files/patch-src_ft2__main.c
+++ audio/fasttracker2/files/patch-src_ft2__main.c
@@ -1,17 +1,18 @@
---- src/ft2_main.c.orig	2018-08-30 13:44:45 UTC
+--- src/ft2_main.c.orig	2018-11-27 19:40:20 UTC
 +++ src/ft2_main.c
-@@ -234,12 +234,14 @@ int main(int argc, char *argv[])
+@@ -187,15 +187,6 @@ int main(int argc, char *argv[])
+         enterFullscreen();
      }
  
-     /* set up MIDI input (in a thread because it can take quite a while on f.ex. macOS) */
-+    /*
-     initMidiThread = SDL_CreateThread(initMidiFunc, "FT2 Clone MIDI Initialization Thread", NULL);
-     if (initMidiThread == NULL)
-     {
-         cleanUpAndExit();
-         return (1);
-     }
-+    */
+-    //benchmarkAudioChannelMixer(); /* for development testing */
+-
+-    /* set up MIDI input (in a thread because it can take quite a while on f.ex. macOS) */
+-    initMidiThread = SDL_CreateThread(initMidiFunc, "FT2 Clone MIDI Initialization Thread", NULL);
+-    if (initMidiThread == NULL)
+-    {
+-        cleanUpAndExit();
+-        return (1);
+-    }
+     SDL_DetachThread(initMidiThread); /* don't wait for this thread, let it clean up when done */
  
-     /* don't let thread wait for this thread, let it clean up on its own when done */
-     SDL_DetachThread(initMidiThread);
+     setupWaitVBL();
