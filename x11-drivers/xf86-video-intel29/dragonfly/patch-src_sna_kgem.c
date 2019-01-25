332fa5799ee0a44c949f759a2c755937f7550314
DPorts Builder <nobody@home.ok>
2019-01-24 22:22:47 -0800

    Update x11-drivers/xf86-video-intel29 to version 2.99.2018.10.22,2

--- x11-drivers/xf86-video-intel29/dragonfly/patch-src_sna_kgem.c
+++ x11-drivers/xf86-video-intel29/dragonfly/patch-src_sna_kgem.c
@@ -1,22 +1,11 @@
---- src/sna/kgem.c.orig	2014-12-09 16:08:21 UTC
+--- src/sna/kgem.c.orig	2018-12-03 09:01:25 UTC
 +++ src/sna/kgem.c
-@@ -70,7 +70,7 @@ search_snoop_cache(struct kgem *kgem, un
- #define DBG_NO_CREATE2 0
+@@ -71,7 +71,7 @@ search_snoop_cache(struct kgem *kgem, un
  #define DBG_NO_USERPTR 0
  #define DBG_NO_UNSYNCHRONIZED_USERPTR 0
+ #define DBG_NO_COHERENT_MMAP_GTT 0
 -#define DBG_NO_LLC 0
 +#define DBG_NO_LLC 1
  #define DBG_NO_SEMAPHORES 0
  #define DBG_NO_MADV 0
  #define DBG_NO_UPLOAD_CACHE 0
-@@ -3198,7 +3198,9 @@
- 	if (rq) {
- 		struct kgem_request *tmp;
- 
--		if (__kgem_busy(kgem, rq->bo->handle)) {
-+		if (rq->bo == NULL)
-+			fprintf(stderr, "__kgem_ring_is_idle: rq->bo == NULL\n");
-+		if (rq->bo && __kgem_busy(kgem, rq->bo->handle)) {
- 			DBG(("%s: last fence handle=%d still busy\n",
- 			     __FUNCTION__, rq->bo->handle));
- 			return false;
