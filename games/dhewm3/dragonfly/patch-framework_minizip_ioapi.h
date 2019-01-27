51131d1850e36d87768d555d4f4e2c316bde8d80
DPorts Builder <nobody@home.ok>
2019-01-26 20:25:36 -0800

    Update games/dhewm3 to version 1.5.0

--- games/dhewm3/dragonfly/patch-framework_minizip_ioapi.h
+++ games/dhewm3/dragonfly/patch-framework_minizip_ioapi.h
@@ -1,11 +1,11 @@
---- framework/minizip/ioapi.h.orig	2015-10-09 17:06:49.000000000 +0300
+--- framework/minizip/ioapi.h.orig	2018-12-15 04:49:59 UTC
 +++ framework/minizip/ioapi.h
 @@ -59,7 +59,7 @@
  #define ftello64 ftell
  #define fseeko64 fseek
  #else
--#ifdef __FreeBSD__
-+#if defined(__FreeBSD__) || defined(__DragonFly__)
+-#if defined(__FreeBSD__) || defined(__OpenBSD__)
++#if defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__DragonFly__)
  #define fopen64 fopen
  #define ftello64 ftello
  #define fseeko64 fseeko
