e81099b60482632dde7756821642094c82b9ac78
DPorts Builder <nobody@home.ok>
2019-01-30 08:14:41 -0800

    Import devel/sope3 version 3.2.10_4

---
+++ devel/sope3/dragonfly/patch-sope-core_NGStreams_NGInternetSocketAddress.m
@@ -0,0 +1,11 @@
+--- sope-core/NGStreams/NGInternetSocketAddress.m.orig	2016-07-07 07:02:47.000000000 +0300
++++ sope-core/NGStreams/NGInternetSocketAddress.m
+@@ -48,7 +48,7 @@
+ #include "NGInternetSocketDomain.h"
+ #include "common.h"
+ 
+-#if defined(HAVE_GETHOSTBYNAME_R) && !defined(linux) && !defined(__FreeBSD__) && !defined(__GLIBC__)
++#if defined(HAVE_GETHOSTBYNAME_R) && !defined(linux) && !defined(__FreeBSD__) && !defined(__DragonFly__) && !defined(__GLIBC__)
+ #define USE_GETHOSTBYNAME_R 1
+ #endif
+ 
