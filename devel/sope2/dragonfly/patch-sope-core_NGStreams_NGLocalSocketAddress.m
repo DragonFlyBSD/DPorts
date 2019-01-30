78f735f45c89d1932c39c57b66315dec49173447
DPorts Builder <nobody@home.ok>
2019-01-30 08:14:25 -0800

    Import devel/sope2 version 2.3.23_3

---
+++ devel/sope2/dragonfly/patch-sope-core_NGStreams_NGLocalSocketAddress.m
@@ -0,0 +1,11 @@
+--- sope-core/NGStreams/NGLocalSocketAddress.m.orig	2016-07-06 14:34:57.000000000 +0300
++++ sope-core/NGStreams/NGLocalSocketAddress.m
+@@ -28,7 +28,7 @@
+ 
+ #include "config.h"
+ 
+-#if defined(__APPLE__) || defined(__FreeBSD__)
++#if defined(__APPLE__) || defined(__FreeBSD__) || defined(__DragonFly__)
+ #  include <sys/types.h>
+ #else
+ #  include <sys/un.h>
