3849fd599cf33bdf021cf53bf71d054a105e0d66
DPorts Builder <nobody@home.ok>
2019-02-25 16:59:01 -0800

    french/aster - Manual update
    - Hopefully it will allow other packages that depend on some aster
      files to build successfully.
    - This package remains broken.

--- french/aster/dragonfly/patch-as_setup.py
+++ french/aster/dragonfly/patch-as_setup.py
@@ -1,10 +1,18 @@
---- as_setup.py.intermediate	2016-11-22 14:53:10.000000000 +0200
-+++ as_setup.py
-@@ -521,6 +521,32 @@ class SETUP:
+--- as_setup.py.orig	2019-02-21 17:47:39.000000000 +0000
++++ as_setup.py	2019-02-21 17:47:39.000000000 +0000
+@@ -521,6 +521,48 @@
        if iextr_as:
           self.Clean(to_delete=path)
  
 +      # Insert DragonFly patches here
++      file2patch = os.path.join(self.workdir, self.content, 'bibc/include/aster_depend.h')
++      self._print('DragonFly patch: depend include => modify ' + file2patch)
++      for ligne in fileinput.input(file2patch, inplace=1):
++         nl = 0
++         nl = string.find(ligne, "__FreeBSD__")
++         if nl > 0:
++            ligne =ligne.replace("__FreeBSD__", "__DragonFly__")
++         sys.stdout.write(ligne)
 +      file2patch = os.path.join(self.workdir, self.content, 'bibc/utilitai/mempid.c')
 +      self._print('DragonFly patch: no mem counting => modify ' + file2patch)
 +      for ligne in fileinput.input(file2patch, inplace=1):
@@ -29,7 +37,30 @@
 +         if nl > 0:
 +            ligne =ligne.replace("ki_ssize", "kp_vm_ssize")
 +         sys.stdout.write(ligne)
++      file2patch = os.path.join(self.workdir, self.content, 'bibc/renum/onmetl.c')
++      self._print('DragonFly patch: try to remove a left behind function => modify ' + file2patch)
++      for ligne in fileinput.input(file2patch, inplace=1):
++         nl = 0
++         nl = string.find(ligne, "WritePermutation")
++         if nl > 0:
++            ligne =ligne.replace("WritePermutation", "WritePermutation1")
++         sys.stdout.write(ligne)
 +
        # Insert FreeBSD patches here
        file2patch = os.path.join(self.workdir, self.content, 'bibc/wscript')
        self._print('FreeBSD patch: no libdl => modify ' + file2patch)
+@@ -530,14 +572,6 @@
+          if nl > 0:
+             ligne =ligne.replace("self.check_cc", "# self.check_cc")
+          sys.stdout.write(ligne)
+-      file2patch = os.path.join(self.workdir, self.content, 'bibcxx/wscript')
+-      self._print('FreeBSD patch: explicit link with libc++ required since Gcc 4.9 => modify ' + file2patch)
+-      for ligne in fileinput.input(file2patch, inplace=1):
+-         nl = 0
+-         nl = string.find(ligne, "uselib_store='CXX', lib='stdc++'")
+-         if nl > 0:
+-            ligne =ligne.replace("lib='stdc++'", "lib='c++ stdc++'")
+-         sys.stdout.write(ligne)
+       file2patch = os.path.join(self.workdir, self.content, 'waftools/scotch.py')
+       self._print('FreeBSD patch: int64_t missing => modify ' + file2patch)
+       for ligne in fileinput.input(file2patch, inplace=1):
