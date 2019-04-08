8ca49314f14616f1f3a6bff5794a6a1c4404a110
DPorts Builder <nobody@home.ok>
2019-04-08 16:17:24 -0700

    Tweak net-p2p/qtum version 0.17.2

---
+++ net-p2p/qtum/dragonfly/patch-src_cryptopp_config.h
@@ -0,0 +1,20 @@
+--- src/cryptopp/config.h.orig	2019-02-21 22:21:59.00 UTC
++++ src/cryptopp/config.h
+@@ -510,7 +510,7 @@ NAMESPACE_END
+ 	#define CRYPTOPP_MM_MALLOC_AVAILABLE
+ #elif defined(__APPLE__)
+ 	#define CRYPTOPP_APPLE_MALLOC_AVAILABLE
+-#elif defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__)
++#elif defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__DragonFly__)
+ 	#define CRYPTOPP_MALLOC_ALIGNMENT_IS_16
+ #elif defined(__linux__) || defined(__sun__) || defined(__CYGWIN__)
+ 	#define CRYPTOPP_MEMALIGN_AVAILABLE
+@@ -601,7 +601,7 @@ NAMESPACE_END
+ #define CRYPTOPP_UNIX_AVAILABLE
+ #endif
+ 
+-#if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__)
++#if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__DragonFly__)
+ #define CRYPTOPP_BSD_AVAILABLE
+ #endif
+ 
