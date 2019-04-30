d1b5f207d991591241bb5bf7199c7078e0aadb41
DPorts Builder <nobody@home.ok>
2019-04-30 07:28:50 -0700

    Update math/octave to version 5.1.0_3

--- math/octave/dragonfly/patch-libinterp_corefcn_sysdep.cc
+++ math/octave/dragonfly/patch-libinterp_corefcn_sysdep.cc
@@ -1,21 +1,20 @@
---- libinterp/corefcn/sysdep.cc.orig	2016-04-20 18:23:56 UTC
+--- libinterp/corefcn/sysdep.cc.intermediate	2019-04-29 12:53:15 UTC
 +++ libinterp/corefcn/sysdep.cc
-@@ -92,7 +92,8 @@ along with Octave; see the file COPYING.
- #define STDIN_FILENO 1
+@@ -92,7 +92,7 @@ along with Octave; see the file COPYING.
+ #  define STDIN_FILENO 1
  #endif
  
 -#if defined (__386BSD__) || defined (__FreeBSD__) || defined (__NetBSD__)
-+#if defined (__386BSD__) || defined (__FreeBSD__) \
-+ || defined (__NetBSD__) || defined (__DragonFly__)
++#if defined (__386BSD__) || defined (__FreeBSD__) || defined (__NetBSD__) || defined (__DragonFly__)
  static void
  BSD_init (void)
  {
-@@ -318,7 +319,7 @@ sysdep_init (void)
- #endif
+@@ -337,7 +337,7 @@ namespace octave
+     omp_get_num_threads ();
  #endif
  
 -#if defined (__386BSD__) || defined (__FreeBSD__) || defined (__NetBSD__)
-+#if defined (__386BSD__) || defined (__FreeBSD__) || defined (__NetBSD__) || defined __DragonFly__
-   BSD_init ();
++#if defined (__386BSD__) || defined (__FreeBSD__) || defined (__NetBSD__) || defined (__DragonFly__)
+     BSD_init ();
  #elif defined (__MINGW32__)
-   MINGW_init ();
+     MINGW_init ();
