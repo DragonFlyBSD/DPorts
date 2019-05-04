eaa2d321fa947f27a154a3d79fe73840b4cd0790
DPorts Builder <nobody@home.ok>
2019-05-03 21:34:57 -0700

    Update x11/mlterm to version 3.8.8

--- x11/mlterm/dragonfly/patch-baselib_src_bl__pty__streams.c
+++ x11/mlterm/dragonfly/patch-baselib_src_bl__pty__streams.c
@@ -1,11 +1,11 @@
---- baselib/src/bl_pty_streams.c.orig	2018-10-27 14:29:17 UTC
+--- baselib/src/bl_pty_streams.c.orig	2019-03-31 13:10:47 UTC
 +++ baselib/src/bl_pty_streams.c
 @@ -23,7 +23,7 @@
  #include <sys/ioctl.h>
  #include <sys/stat.h>
  #include <unistd.h>
--#ifdef __ANDROID__
-+#if defined(__ANDROID__) || defined(__DragonFly__) || defined(__FreeBSD__)
+-#if defined(__ANDROID__) || defined(__HAIKU__)
++#if defined(__ANDROID__) || defined(__HAIKU__) || defined(__DragonFly__) || defined(__FreeBSD__)
  #include <termios.h>
  #else
  #include <sys/termios.h>
