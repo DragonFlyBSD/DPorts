8c8e8e72daff1f26b6c624d6c63a324160b37d70
DPorts Builder <nobody@home.ok>
2019-05-05 16:03:41 -0700

    Update games/teeworlds to version 0.7.3.1

--- games/teeworlds/dragonfly/patch-src_base_detect.h
+++ games/teeworlds/dragonfly/patch-src_base_detect.h
@@ -1,9 +1,9 @@
---- src/base/detect.h.orig	2013-05-01 14:47:39.000000000 +0300
+--- src/base/detect.h.orig	2019-04-22 18:47:23 UTC
 +++ src/base/detect.h
-@@ -25,6 +25,13 @@
+@@ -32,6 +32,13 @@
+ 	#define CONF_PLATFORM_STRING "freebsd"
  #endif
  
- /* unix family */
 +#if defined(__DragonFly__)
 +	#define CONF_FAMILY_UNIX 1
 +	#define CONF_FAMILY_STRING "unix"
@@ -11,15 +11,15 @@
 +	#define CONF_PLATFORM_STRING "dragonfly"
 +#endif
 +
- #if defined(__FreeBSD__) || defined(__FreeBSD_kernel__)
+ #if defined(__NetBSD__)
  	#define CONF_FAMILY_UNIX 1
  	#define CONF_FAMILY_STRING "unix"
-@@ -78,7 +85,7 @@
+@@ -85,7 +92,7 @@
  
  /* use gcc endianness definitions when available */
  #if defined(__GNUC__) && !defined(__APPLE__) && !defined(__MINGW32__) && !defined(__sun)
--	#if defined(__FreeBSD__) || defined(__OpenBSD__)
-+	#if defined(__DragonFly__) || defined(__FreeBSD__) || defined(__OpenBSD__)
+-	#if defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__)
++	#if defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__) || defined(__DragonFly__)
  		#include <sys/endian.h>
  	#else
  		#include <endian.h>
