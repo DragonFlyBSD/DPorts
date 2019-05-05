ffb7e97c0f5c6499837aa36075b8f1be004422e9
DPorts Builder <nobody@home.ok>
2019-05-05 16:13:28 -0700

    Update databases/mysql-connector-c++ to version 1.1.12

--- databases/mysql-connector-c++/dragonfly/patch-FindMySQL.cmake
+++ databases/mysql-connector-c++/dragonfly/patch-FindMySQL.cmake
@@ -1,11 +1,19 @@
---- FindMySQL.cmake.orig	2016-01-12 18:42:38 UTC
+--- FindMySQL.cmake.orig	2019-01-16 16:19:52 UTC
 +++ FindMySQL.cmake
-@@ -577,7 +577,7 @@ elseif(MYSQL_CONFIG_EXECUTABLE)
+@@ -642,13 +642,15 @@ elseif(MYSQL_CONFIG_EXECUTABLE)
  
-   # This code assumes there is just one "-L...." and that
-   # no space between "-L" and the path
--  _mysql_config(MYSQL_LIB_DIR "(^| )-L" "--libs")
-+  _mysql_config(MYSQL_LIB_DIR "^-L" "--libs")
-   if(NOT MYSQL_LIB_DIR)
-     message(FATAL_ERROR "Could not find the library dir from running "
-                         "\"${MYSQL_CONFIG_EXECUTABLE}\"")
+   else()
+ 
+-    _mysql_config(MYSQL_LIBRARIES "(^| )-l" "--libs")
++    _mysql_config(MYSQL_LIBRARIES "-l" "--libs")
+     FOREACH(__lib IN LISTS MYSQL_LIBRARIES)
+       string(REGEX MATCH "mysqlclient([^ ]*)" _matched_lib __lib)
+       IF(_matched_lib)
+         set(_search_libs ${matched_lib})
+       ENDIF()
+     ENDFOREACH()
++    set(_search_libs "mysqlclient")
++    set(MYSQL_LIB_DIR "/usr/local/lib/mysql")
+     # First library is assumed to be the client library
+     # list(GET MYSQL_LIBRARIES 0 _search_libs)
+     find_library(MYSQL_LIB
