618ca49c45ff9f5a3a31fc99e41d461c27f7d3c1
DPorts Builder <nobody@home.ok>
2019-05-11 09:42:20 -0700

    Import math/xlife++ version 2.0.1_4

---
+++ math/xlife++/dragonfly/patch-src_mathsResources_specialFunctions_erfFunction.cpp
@@ -0,0 +1,10 @@
+--- src/mathsResources/specialFunctions/erfFunction.cpp.orig	2018-05-09 16:11:45 UTC
++++ src/mathsResources/specialFunctions/erfFunction.cpp
+@@ -27,6 +27,7 @@ XLiFE++ is an extended library of finite
+ #include "utils.h"
+ 
+ #include <iostream>
++#include <cmath>
+ 
+ 
+  /*Authors:
