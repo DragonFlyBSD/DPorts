b190bff80e9b099d043db0d35f7485b05ca5fc4e
DPorts Builder <nobody@home.ok>
2019-05-10 18:03:08 -0700

    Update emulators/qemu-devel to version 3.1.0

--- emulators/qemu-devel/dragonfly/patch-util_coroutine-ucontext.c
+++ emulators/qemu-devel/dragonfly/patch-util_coroutine-ucontext.c
@@ -1,6 +1,6 @@
---- util/coroutine-ucontext.c.orig	2015-12-16 22:04:53 UTC
+--- util/coroutine-ucontext.c.orig	2018-12-11 17:44:35 UTC
 +++ util/coroutine-ucontext.c
-@@ -122,7 +122,15 @@ Coroutine *qemu_coroutine_new(void)
+@@ -159,7 +159,15 @@ Coroutine *qemu_coroutine_new(void)
                  2, arg.i[0], arg.i[1]);
  
      /* swapcontext() in, siglongjmp() back out */
@@ -14,6 +14,6 @@
 +     *     on DragonFly.
 +     */
 +    if (!sigsetjmp(old_env, 1)) {
+         start_switch_fiber(&fake_stack_save, co->stack, co->stack_size);
          swapcontext(&old_uc, &uc);
      }
-     return &co->base;
