0a1858171e49f68dfa4fa6fedd37cd3e498567be
DPorts Builder <nobody@home.ok>
2019-05-11 10:27:25 -0700

    Update www/firefox to version 66.0.4,1

--- www/firefox/dragonfly/patch-ipc_chromium_src_base_platform__thread__posix.cc
+++ www/firefox/dragonfly/patch-ipc_chromium_src_base_platform__thread__posix.cc
@@ -1,14 +1,14 @@
---- ipc/chromium/src/base/platform_thread_posix.cc.orig	2016-09-05 23:12:53.000000000 +0300
+--- ipc/chromium/src/base/platform_thread_posix.cc.orig	2019-05-07 13:53:02 UTC
 +++ ipc/chromium/src/base/platform_thread_posix.cc
-@@ -16,6 +16,11 @@
- #elif defined(OS_LINUX)
- #include <sys/syscall.h>
- #include <sys/prctl.h>
-+#elif defined(OS_DRAGONFLY)
-+#include <sys/param.h>
-+#if __DragonFly_version >= 400709
-+#include <sys/lwp.h>
-+#endif
+@@ -18,6 +18,11 @@
+ #  include <sys/prctl.h>
  #endif
  
++#if defined(OS_DRAGONFLY)
++#include <sys/param.h>
++#include <sys/lwp.h>
++#endif
++
  #if !defined(OS_MACOSX)
+ #  include <unistd.h>
+ #endif
