0da0c199603b12165c5c34effa3b053a9e885d26
DPorts Builder <nobody@home.ok>
2019-05-11 17:04:55 -0700

    Update devel/libc++ to version 8.0.0

--- devel/libc++/files/patch-include_stddef.h
+++ devel/libc++/files/patch-include_stddef.h
@@ -1,11 +1,11 @@
---- include/stddef.h.orig	2017-02-10 01:27:42 UTC
+--- include/stddef.h.orig	2018-08-20 22:29:20 UTC
 +++ include/stddef.h
 @@ -54,7 +54,7 @@ using std::nullptr_t;
  
  // Re-use the compiler's <stddef.h> max_align_t where possible.
  #if !defined(__CLANG_MAX_ALIGN_T_DEFINED) && !defined(_GCC_MAX_ALIGN_T) && \
--    !defined(__DEFINED_max_align_t)
-+  !defined(__DEFINED_max_align_t) && !defined(_MAX_ALIGN_T_DECLARED)
+-    !defined(__DEFINED_max_align_t) && !defined(__NetBSD__)
++    !defined(__DEFINED_max_align_t) && !defined(__NetBSD__) && !defined(_MAX_ALIGN_T_DECLARED)
  typedef long double max_align_t;
  #endif
  
