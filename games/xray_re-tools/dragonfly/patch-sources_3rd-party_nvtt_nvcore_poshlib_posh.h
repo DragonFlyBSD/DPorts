4c8c4734551ab3d29a3a81c901de7c0188e542c2
DPorts Builder <nobody@home.ok>
2019-05-13 12:45:11 -0700

    Import games/xray_re-tools version g20180902

---
+++ games/xray_re-tools/dragonfly/patch-sources_3rd-party_nvtt_nvcore_poshlib_posh.h
@@ -0,0 +1,14 @@
+--- sources/3rd-party/nvtt/nvcore/poshlib/posh.h.intermediate	2019-05-13 12:31:54.000000000 +0000
++++ sources/3rd-party/nvtt/nvcore/poshlib/posh.h
+@@ -293,6 +293,11 @@ Metrowerks:
+ #  define POSH_OS_STRING "Linux"
+ #endif
+ 
++#if defined __DragonFly__
++#  define POSH_OS_LINUX 1
++#  define POSH_OS_STRING "DragonFly"
++#endif
++
+ #if defined __FreeBSD__
+ #  define POSH_OS_LINUX 1
+ #  define POSH_OS_STRING "FreeBSD"
---
+++ games/xray_re-tools/dragonfly/patch-sources_utils_converter_ogg__tools.cxx
@@ -0,0 +1,11 @@
+--- sources/utils/converter/ogg_tools.cxx.intermediate	2019-05-13 12:42:29.000000000 +0000
++++ sources/utils/converter/ogg_tools.cxx
+@@ -5,6 +5,8 @@
+ #include <dirent.h>
+ #include <errno.h>
+ #endif
++#include <stdio.h>
++#include <cstring>
+ #include "ogg_tools.h"
+ #include "xr_types.h"
+ #include "xr_file_system.h"
