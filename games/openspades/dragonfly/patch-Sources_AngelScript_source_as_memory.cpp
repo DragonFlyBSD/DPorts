6da57baecc3ce730d4e6b78fd998f6fab541655b
DPorts Builder <nobody@home.ok>
2019-05-13 12:38:01 -0700

    Import games/openspades version 0.1.3

---
+++ games/openspades/dragonfly/patch-Sources_AngelScript_source_as_memory.cpp
@@ -0,0 +1,11 @@
+--- Sources/AngelScript/source/as_memory.cpp.orig	2019-01-04 07:21:49 UTC
++++ Sources/AngelScript/source/as_memory.cpp
+@@ -38,7 +38,7 @@
+ 
+ #include <stdlib.h>
+ 
+-#if !defined(__APPLE__) && !defined(__SNC__) && !defined(__ghs__) && !defined(__FreeBSD__) && !defined(__OpenBSD__)
++#if !defined(__APPLE__) && !defined(__SNC__) && !defined(__ghs__) && !defined(__FreeBSD__) && !defined(__OpenBSD__) && !defined(__DragonFly__)
+ #include <malloc.h>
+ #endif
+ 
