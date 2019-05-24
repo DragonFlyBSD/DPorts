fbd23fed1b6e93a76fbb89550bf2904adbeec622
DPorts Builder <nobody@home.ok>
2019-05-24 16:30:22 -0700

    Tweak audio/lash version 0.5.4_14

--- audio/lash/dragonfly/patch-liblash_socket.c
+++ audio/lash/dragonfly/patch-liblash_socket.c
@@ -1,12 +1,11 @@
---- liblash/socket.c.orig	2006-09-16 22:25:31.000000000 +0300
+--- liblash/socket.c.orig	2006-09-16 21:25:31.000000000 +0200
 +++ liblash/socket.c
-@@ -19,6 +19,9 @@
+@@ -18,8 +18,6 @@
+  *   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
   */
  
- #define _POSIX_SOURCE /* addrinfo */
-+#if defined(__DragonFly__)
-+#define __BSD_VISIBLE 1 /* for NI_MAXHOST, NI_MAXSERV */
-+#endif
- 
+-#define _POSIX_SOURCE /* addrinfo */
+-
  #include <stdint.h>
  #include <sys/types.h>
+ #include <sys/socket.h>
