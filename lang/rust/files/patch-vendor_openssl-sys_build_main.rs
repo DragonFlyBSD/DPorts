1cc1e11144cac1397f006c867d4f501c9b27e019
DPorts Builder <nobody@home.ok>
2019-05-27 07:35:48 -0700

    Update lang/rust to version 1.34.2

--- lang/rust/files/patch-vendor_openssl-sys_build_main.rs
+++ lang/rust/files/patch-vendor_openssl-sys_build_main.rs
@@ -1,10 +1,10 @@
---- src/vendor/openssl-sys/build/main.rs.intermediate	2019-05-09 16:57:01 UTC
-+++ src/vendor/openssl-sys/build/main.rs
-@@ -505,6 +505,7 @@ See rust-openssl README for more informa
+--- vendor/openssl-sys/build/main.rs.orig	2019-05-13 21:50:35 UTC
++++ vendor/openssl-sys/build/main.rs
+@@ -505,6 +505,7 @@ See rust-openssl README for more information:
              (8, 1) => ('8', '1'),
              (8, _) => ('8', 'x'),
              (9, 0) => ('9', '0'),
-+            (9, 1) => ('9', '1'),
++            (9, _) => ('9', 'x'),
              _ => version_error(),
          };
  
@@ -13,7 +13,7 @@
  
  This crate is only compatible with OpenSSL 1.0.1 through 1.1.1, or LibreSSL 2.5
 -through 2.9.0, but a different version of OpenSSL was found. The build is now aborting
-+through 2.9.1, but a different version of OpenSSL was found. The build is now aborting
++through 2.9.x, but a different version of OpenSSL was found. The build is now aborting
  due to this version mismatch.
  
  "
