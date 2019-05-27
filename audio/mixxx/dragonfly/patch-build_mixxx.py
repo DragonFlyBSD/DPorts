349bec885db14127cd49058d641b585a27dcb509
DPorts Builder <nobody@home.ok>
2019-05-27 09:46:33 -0700

    Import audio/mixxx version 2.2.1

---
+++ audio/mixxx/dragonfly/patch-build_mixxx.py
@@ -0,0 +1,11 @@
+--- build/mixxx.py.intermediate	2019-05-26 12:08:37.000000000 +0000
++++ build/mixxx.py
+@@ -262,7 +262,7 @@ class MixxxBuild(object):
+         if os.name == 'nt' or sys.platform == 'win32':
+             return 'windows'
+         # Should cover {Net,Open,Free,DragonFly}BSD, but only tested on OpenBSD
+-        if 'bsd' in sys.platform:
++        if 'bsd' in sys.platform or 'dragonfly' in sys.platform:
+             return 'bsd'
+         if sys.platform.startswith('linux'):
+             return 'linux'
---
+++ audio/mixxx/dragonfly/patch-src_encoder_encodermp3.cpp
@@ -0,0 +1,11 @@
+--- src/encoder/encodermp3.cpp.intermediate	2019-05-26 12:08:37.000000000 +0000
++++ src/encoder/encodermp3.cpp
+@@ -95,7 +95,7 @@ EncoderMp3::EncoderMp3(EncoderCallback*
+      */
+     QStringList libnames;
+     QString libname = "";
+-#if defined(__LINUX__) || defined(__FreeBSD__)
++#if defined(__LINUX__) || defined(__FreeBSD__) || defined(__DragonFly__)
+     libnames << "mp3lame";
+ #elif __WINDOWS__
+     libnames << "lame_enc.dll";
