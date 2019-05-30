0996fe6f5796010a7be0647786f38affa08a2448
DPorts Builder <nobody@home.ok>
2019-05-30 16:44:20 -0700

    Tweak graphics/gegl version 0.4.14_1

--- graphics/gegl/dragonfly/patch-gegl_gegl-config.c
+++ graphics/gegl/dragonfly/patch-gegl_gegl-config.c
@@ -1,11 +1,24 @@
---- gegl/gegl-config.c.orig	2019-05-22 13:05:36 UTC
+--- gegl/gegl-config.c.orig	2019-05-30 09:46:01 UTC
 +++ gegl/gegl-config.c
-@@ -271,7 +271,7 @@ gegl_config_class_init (GeglConfigClass
+@@ -271,6 +271,21 @@ gegl_config_class_init (GeglConfigClass
                               ) * page_size;
        mach_port_deallocate (mach_task_self (), host);
      }
--#elif defined(__FreeBSD__)
-+#elif defined(__FreeBSD__) || defined(__DragonFly__)
++#elif defined(__DragonFly__)
++
++    int64_t usermem, free_pages, inact;
++    size_t len = sizeof usermem;
++
++    if(sysctlbyname("hw.usermem", &usermem, &len, NULL, 0)) {
++      mem_total = usermem;
++    };
++
++    if(sysctlbyname("vm.stats.vm.v_free_count", &free_pages, &len, NULL, 0)) {
++	if(sysctlbyname("vm.stats.vm.v_inactive_count", &inact, &len, NULL, 0)) {
++	    mem_available = (free_pages + inact) * sysconf (_SC_PAGESIZE);
++	}
++    };
++
+ #elif defined(__FreeBSD__)
  
      size_t len = sizeof(unsigned int);
-     unsigned int realmem, physmem;
