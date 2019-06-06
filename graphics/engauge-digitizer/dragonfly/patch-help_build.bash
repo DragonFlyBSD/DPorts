54c6adcf36451c538f3a01927bc80f89035e0b30
DPorts Builder <nobody@home.ok>
2019-06-06 12:42:12 -0700

    Tweak graphics/engauge-digitizer version 11.2_1

---
+++ graphics/engauge-digitizer/dragonfly/patch-help_build.bash
@@ -0,0 +1,9 @@
+Invokded as ./build.bash directly in MF.
+
+--- help/build.bash.orig	2019-05-01 03:52:33 UTC
++++ help/build.bash
+@@ -1,3 +1,4 @@
++#!/usr/bin/env bash
+ # This bash script creates .qch and .qhc output files. Optional argument is target directory.
+ # Since bash has different locations depending on the operating system, no #! line is included.
+ #
