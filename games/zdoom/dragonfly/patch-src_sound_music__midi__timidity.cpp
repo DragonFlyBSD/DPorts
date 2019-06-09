6887b2a00c1fd86873f9d01267e9964d08f75232
DPorts Builder <nobody@home.ok>
2019-06-08 18:46:28 -0700

    Tweak games/zdoom version 2.8.1_4

---
+++ games/zdoom/dragonfly/patch-src_sound_music__midi__timidity.cpp
@@ -0,0 +1,12 @@
+Add missing <sys/select.h> for select() visibility.
+
+--- src/sound/music_midi_timidity.cpp.orig	2016-02-22 02:11:13 UTC
++++ src/sound/music_midi_timidity.cpp
+@@ -8,6 +8,7 @@
+ #include <unistd.h>
+ 
+ #include <sys/types.h>
++#include <sys/select.h>
+ #include <sys/wait.h>
+ #include <wordexp.h>
+ #include <signal.h>
