cefaf842bb39ae924a0aee6a314fe9c617abc9b9
DPorts Builder <nobody@home.ok>
2019-07-17 16:39:46 -0700

    Tweak sysutils/facter version 3.14.1

--- sysutils/facter/dragonfly/patch-lib_src_facts_dragonfly_collection.cc
+++ sysutils/facter/dragonfly/patch-lib_src_facts_dragonfly_collection.cc
@@ -1,6 +1,6 @@
---- lib/src/facts/dragonfly/collection.cc.orig	2019-07-16 10:38:45 UTC
+--- lib/src/facts/dragonfly/collection.cc.orig	2019-07-16 16:31:27 UTC
 +++ lib/src/facts/dragonfly/collection.cc
-@@ -0,0 +1,26 @@
+@@ -0,0 +1,28 @@
 +#include <facter/facts/collection.hpp>
 +#include <internal/facts/bsd/uptime_resolver.hpp>
 +#include <internal/facts/bsd/filesystem_resolver.hpp>
@@ -9,6 +9,7 @@
 +#include <internal/facts/posix/kernel_resolver.hpp>
 +#include <internal/facts/posix/ssh_resolver.hpp>
 +#include <internal/facts/posix/timezone_resolver.hpp>
++#include <internal/facts/posix/operating_system_resolver.hpp>
 +
 +using namespace std;
 +
@@ -22,6 +23,7 @@
 +        add(make_shared<posix::identity_resolver>());
 +        add(make_shared<posix::timezone_resolver>());
 +        add(make_shared<bsd::filesystem_resolver>());
++        add(make_shared<posix::operating_system_resolver>());
 +        add(make_shared<glib::load_average_resolver>());
 +    }
 +
