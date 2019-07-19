42a014d2ea6cad1e02769c7b18927f8c1f6050e3
DPorts Builder <nobody@home.ok>
2019-07-18 23:09:52 -0700

    Update net-mgmt/collectd5 to version 5.9.0_1

--- net-mgmt/collectd5/dragonfly/patch-src_processes.c
+++ net-mgmt/collectd5/dragonfly/patch-src_processes.c
@@ -1,6 +1,6 @@
---- src/processes.c.orig	2018-10-23 06:57:09 UTC
+--- src/processes.c.orig	2019-06-13 09:13:42 UTC
 +++ src/processes.c
-@@ -97,10 +97,13 @@
+@@ -102,10 +102,13 @@
  /* #endif KERNEL_LINUX */
  
  #elif HAVE_LIBKVM_GETPROCS &&                                                  \
@@ -15,18 +15,18 @@
  #include <sys/sysctl.h>
  #include <sys/user.h>
  /* #endif HAVE_LIBKVM_GETPROCS && (HAVE_STRUCT_KINFO_PROC_FREEBSD ||
-@@ -200,7 +203,7 @@ typedef struct process_entry_s {
-   _Bool has_maps;
+@@ -214,7 +217,7 @@ typedef struct process_entry_s {
+   bool has_maps;
  } process_entry_t;
  
 -typedef struct procstat_entry_s {
 +typedef struct collectd_procstat_entry_s {
    unsigned long id;
-   unsigned long age;
+   unsigned char age;
  
-@@ -221,10 +224,10 @@ typedef struct procstat_entry_s {
-   derive_t cswitch_vol;
-   derive_t cswitch_invol;
+@@ -242,10 +245,10 @@ typedef struct procstat_entry_s {
+   value_to_rate_state_t delay_freepages;
+ #endif
  
 -  struct procstat_entry_s *next;
 -} procstat_entry_t;
@@ -38,23 +38,22 @@
    char name[PROCSTAT_NAME_LEN];
  #if HAVE_REGEX_H
    regex_t *re;
-@@ -261,11 +264,11 @@ typedef struct procstat {
-   _Bool report_maps_num;
-   _Bool report_ctx_switch;
+@@ -289,11 +292,11 @@ typedef struct procstat {
+   bool report_ctx_switch;
+   bool report_delay;
  
 -  struct procstat *next;
--  struct procstat_entry_s *instances;
--} procstat_t;
 +  struct collectd_procstat *next;
-+  struct collectd_procstat_entry_s *instances;
+   struct procstat_entry_s *instances;
+-} procstat_t;
 +} collectd_procstat_t;
  
--static procstat_t *list_head_g = NULL;
-+static collectd_procstat_t *list_head_g = NULL;
+-static procstat_t *list_head_g;
++static collectd_procstat_t *list_head_g;
  
- static _Bool want_init = 1;
- static _Bool report_ctx_switch = 0;
-@@ -282,11 +285,12 @@ static mach_msg_type_number_t pset_list_
+ static bool want_init = true;
+ static bool report_ctx_switch;
+@@ -311,11 +314,12 @@ static mach_msg_type_number_t pset_list_
  
  #elif KERNEL_LINUX
  static long pagesize_g;
@@ -69,8 +68,8 @@
  static int pagesize;
  /* #endif HAVE_LIBKVM_GETPROCS && (HAVE_STRUCT_KINFO_PROC_FREEBSD ||
   * HAVE_STRUCT_KINFO_PROC_OPENBSD) */
-@@ -305,11 +309,11 @@ int getargs(void *processBuffer, int buf
- #endif /* HAVE_PROCINFO_H */
+@@ -338,11 +342,11 @@ static ts_t *taskstats_handle;
+ #endif
  
  /* put name of process from config to list_head_g tree
 - * list_head_g is a list of 'procstat_t' structs with
@@ -85,7 +84,7 @@
    int status;
  
    new = calloc(1, sizeof(*new));
-@@ -392,7 +396,7 @@ static procstat_t *ps_list_register(cons
+@@ -426,7 +430,7 @@ static procstat_t *ps_list_register(cons
  
  /* try to match name against entry, returns 1 if success */
  static int ps_list_match(const char *name, const char *cmdline,
@@ -94,7 +93,16 @@
  #if HAVE_REGEX_H
    if (ps->re != NULL) {
      int status;
-@@ -442,12 +446,12 @@ static void ps_update_counter(derive_t *
+@@ -491,7 +495,7 @@ static void ps_update_delay_one(gauge_t
+   }
+ }
+ 
+-static void ps_update_delay(procstat_t *out, procstat_entry_t *prev,
++static void ps_update_delay(collectd_procstat_t *out, procstat_entry_t *prev,
+                             process_entry_t *curr) {
+   cdtime_t now = cdtime();
+ 
+@@ -509,12 +513,12 @@ static void ps_update_delay(procstat_t *
  /* add process entry to 'instances' of process 'name' (or refresh it) */
  static void ps_list_add(const char *name, const char *cmdline,
                          process_entry_t *entry) {
@@ -109,7 +117,7 @@
      if ((ps_list_match(name, cmdline, ps)) == 0)
        continue;
  
-@@ -460,7 +464,7 @@ static void ps_list_add(const char *name
+@@ -527,7 +531,7 @@ static void ps_list_add(const char *name
          break;
  
      if ((pse == NULL) || (pse->id != entry->id)) {
@@ -118,7 +126,7 @@
  
        new = calloc(1, sizeof(*new));
        if (new == NULL)
-@@ -523,10 +527,10 @@ static void ps_list_add(const char *name
+@@ -595,10 +599,10 @@ static void ps_list_add(const char *name
  
  /* remove old entries from instances of processes in list_head_g */
  static void ps_list_reset(void) {
@@ -132,16 +140,16 @@
      ps->num_proc = 0;
      ps->num_lwp = 0;
      ps->num_fd = 0;
-@@ -541,7 +545,7 @@ static void ps_list_reset(void) {
+@@ -618,7 +622,7 @@ static void ps_list_reset(void) {
      pse = ps->instances;
      while (pse != NULL) {
-       if (pse->age > 10) {
+       if (pse->age > 0) {
 -        DEBUG("Removing this procstat entry cause it's too old: "
 +        DEBUG("Removing this collectd_procstat entry cause it's too old: "
                "id = %lu; name = %s;",
                pse->id, ps->name);
  
-@@ -563,7 +567,7 @@ static void ps_list_reset(void) {
+@@ -640,7 +644,7 @@ static void ps_list_reset(void) {
    }   /* for (ps = list_head_g; ps != NULL; ps = ps->next) */
  }
  
@@ -150,7 +158,7 @@
    for (int i = 0; i < ci->children_num; i++) {
      oconfig_item_t *c = ci->children + i;
  
-@@ -583,11 +587,11 @@ static void ps_tune_instance(oconfig_ite
+@@ -667,11 +671,11 @@ static void ps_tune_instance(oconfig_ite
  static int ps_config(oconfig_item_t *ci) {
  #if KERNEL_LINUX
    const size_t max_procname_len = 15;
@@ -164,16 +172,16 @@
  
    for (int i = 0; i < ci->children_num; ++i) {
      oconfig_item_t *c = ci->children + i;
-@@ -600,7 +604,7 @@ static int ps_config(oconfig_item_t *ci)
+@@ -684,7 +688,7 @@ static int ps_config(oconfig_item_t *ci)
          continue;
        }
  
 -#if KERNEL_LINUX || KERNEL_SOLARIS || KERNEL_FREEBSD
 +#if KERNEL_LINUX || KERNEL_SOLARIS || KERNEL_FREEBSD || KERNEL_DRAGONFLY
        if (strlen(c->values[0].value.string) > max_procname_len) {
-         WARNING("processes plugin: this platform has a %zu character limit "
-                 "to process names. The `Process \"%s\"' option will "
-@@ -673,7 +677,8 @@ static int ps_init(void) {
+         WARNING("processes plugin: this platform has a %" PRIsz
+                 " character limit "
+@@ -774,7 +778,8 @@ static int ps_init(void) {
  /* #endif KERNEL_LINUX */
  
  #elif HAVE_LIBKVM_GETPROCS &&                                                  \
@@ -183,7 +191,7 @@
    pagesize = getpagesize();
  /* #endif HAVE_LIBKVM_GETPROCS && (HAVE_STRUCT_KINFO_PROC_FREEBSD ||
   * HAVE_STRUCT_KINFO_PROC_OPENBSD) */
-@@ -700,7 +705,7 @@ static void ps_submit_state(const char *
+@@ -801,7 +806,7 @@ static void ps_submit_state(const char *
  }
  
  /* submit info about specific process (e.g.: memory taken, cpu usage, etc..) */
@@ -192,16 +200,16 @@
    value_list_t vl = VALUE_LIST_INIT;
    value_t values[2];
  
-@@ -1077,7 +1082,7 @@ static int ps_count_fd(int pid) {
-   return (count >= 1) ? count : 1;
- } /* int ps_count_fd (pid) */
+@@ -1253,7 +1258,7 @@ static int ps_delay(process_entry_t *ps)
+ }
+ #endif
  
 -static void ps_fill_details(const procstat_t *ps, process_entry_t *entry) {
 +static void ps_fill_details(const collectd_procstat_t *ps, process_entry_t *entry) {
-   if (entry->has_io == 0) {
+   if (entry->has_io == false) {
      ps_read_io(entry);
-     entry->has_io = 1;
-@@ -1624,7 +1629,7 @@ static int ps_read(void) {
+     entry->has_io = true;
+@@ -1803,7 +1808,7 @@ static int ps_read(void) {
    int stopped = 0;
    int blocked = 0;
  
@@ -210,7 +218,7 @@
    process_entry_t pse;
  
    ps_list_reset();
-@@ -1913,13 +1918,14 @@ static int ps_read(void) {
+@@ -2091,13 +2096,14 @@ static int ps_read(void) {
    ps_submit_state("paging", paging);
    ps_submit_state("blocked", blocked);
  
@@ -227,29 +235,29 @@
    int running = 0;
    int sleeping = 0;
    int zombies = 0;
-@@ -1958,13 +1964,21 @@ static int ps_read(void) {
+@@ -2136,13 +2142,21 @@ static int ps_read(void) {
    for (int i = 0; i < count; i++) {
      /* Create only one process list entry per _process_, i.e.
       * filter out threads (duplicate PID entries). */
 +#ifdef HAVE_STRUCT_KINFO_PROC_FREEBSD
      if ((proc_ptr == NULL) || (proc_ptr->ki_pid != procs[i].ki_pid)) {
 +#else
-+    if ((proc_ptr == NULL) || (proc_ptr->kp_pid != procs[i].kp_pid)) {
++   if ((proc_ptr == NULL) || (proc_ptr->kp_pid != procs[i].kp_pid)) {
 +#endif
        char cmdline[CMDLINE_BUFFER_SIZE] = "";
-       _Bool have_cmdline = 0;
+       bool have_cmdline = 0;
  
        proc_ptr = &(procs[i]);
        /* Don't probe system processes and processes without arguments */
 +#ifdef HAVE_STRUCT_KINFO_PROC_FREEBSD
        if (((procs[i].ki_flag & P_SYSTEM) == 0) && (procs[i].ki_args != NULL)) {
 +#else
-+      if ((procs[i].kp_flags & P_SYSTEM) == 0) {
++     if ((procs[i].kp_flags & P_SYSTEM) == 0) {
 +#endif
          char **argv;
          int argc;
          int status;
-@@ -1985,9 +1999,8 @@ static int ps_read(void) {
+@@ -2163,9 +2177,8 @@ static int ps_read(void) {
        } /* if (process has argument list) */
  
        memset(&pse, 0, sizeof(pse));
@@ -260,7 +268,7 @@
        pse.num_lwp = procs[i].ki_numthreads;
  
        pse.vmem_size = procs[i].ki_size;
-@@ -1997,9 +2010,24 @@ static int ps_read(void) {
+@@ -2175,9 +2188,24 @@ static int ps_read(void) {
        pse.stack_size = procs[i].ki_ssize * pagesize;
        pse.vmem_minflt_counter = procs[i].ki_rusage.ru_minflt;
        pse.vmem_majflt_counter = procs[i].ki_rusage.ru_majflt;
@@ -285,7 +293,7 @@
        /*
         * The u-area might be swapped out, and we can't get
         * at it because we have a crashdump and no swap.
-@@ -2013,7 +2041,7 @@ static int ps_read(void) {
+@@ -2191,7 +2219,7 @@ static int ps_read(void) {
              procs[i].ki_rusage.ru_stime.tv_usec +
              (1000000lu * procs[i].ki_rusage.ru_stime.tv_sec);
        }
@@ -294,7 +302,7 @@
        /* no I/O data */
        pse.io_rchar = -1;
        pse.io_wchar = -1;
-@@ -2032,30 +2060,49 @@ static int ps_read(void) {
+@@ -2210,30 +2238,49 @@ static int ps_read(void) {
        pse.cswitch_vol = -1;
        pse.cswitch_invol = -1;
  
@@ -350,7 +358,7 @@
        }
      } /* if ((proc_ptr == NULL) || (proc_ptr->ki_pid != procs[i].ki_pid)) */
    }
-@@ -2070,7 +2117,7 @@ static int ps_read(void) {
+@@ -2248,7 +2295,7 @@ static int ps_read(void) {
    ps_submit_state("idle", idle);
    ps_submit_state("wait", wait);
  
@@ -359,7 +367,7 @@
      ps_submit_proc_list(ps_ptr);
  /* #endif HAVE_LIBKVM_GETPROCS && HAVE_STRUCT_KINFO_PROC_FREEBSD */
  
-@@ -2214,7 +2261,7 @@ static int ps_read(void) {
+@@ -2392,7 +2439,7 @@ static int ps_read(void) {
    ps_submit_state("idle", idle);
    ps_submit_state("dead", dead);
  
@@ -368,7 +376,7 @@
      ps_submit_proc_list(ps_ptr);
  /* #endif HAVE_LIBKVM_GETPROCS && HAVE_STRUCT_KINFO_PROC_OPENBSD */
  
-@@ -2353,7 +2400,7 @@ static int ps_read(void) {
+@@ -2531,7 +2578,7 @@ static int ps_read(void) {
    ps_submit_state("paging", paging);
    ps_submit_state("blocked", blocked);
  
@@ -377,7 +385,7 @@
      ps_submit_proc_list(ps);
  /* #endif HAVE_PROCINFO_H */
  
-@@ -2449,7 +2496,7 @@ static int ps_read(void) {
+@@ -2627,7 +2674,7 @@ static int ps_read(void) {
    ps_submit_state("system", system);
    ps_submit_state("orphan", orphan);
  
