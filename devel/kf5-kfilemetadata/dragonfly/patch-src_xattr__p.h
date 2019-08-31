b1002a330a668e930b0f52ea17f32b0dc32f02ae
DPorts Builder <nobody@home.ok>
2019-08-31 15:25:57 -0700

    Update devel/kf5-kfilemetadata to version 5.61.0

--- devel/kf5-kfilemetadata/dragonfly/patch-src_xattr__p.h
+++ devel/kf5-kfilemetadata/dragonfly/patch-src_xattr__p.h
@@ -1,6 +1,6 @@
---- src/xattr_p.h.orig	2019-07-07 18:33:26 UTC
+--- src/xattr_p.h.orig	2019-08-03 19:54:00 UTC
 +++ src/xattr_p.h
-@@ -53,7 +53,7 @@
+@@ -52,7 +52,7 @@
  #define ssize_t SSIZE_T
  #endif
  
@@ -9,12 +9,3 @@
  inline ssize_t k_getxattr(const QString& path, const QString& name, QString* value)
  {
      const QByteArray p = QFile::encodeName(path);
-@@ -439,7 +439,7 @@ inline int k_removexattr(const QString&,
- 
- inline bool k_hasAttribute(const QString&, const QString&)
- {
--    return false
-+    return false;
- }
- 
- inline bool k_isSupported(const QString&)
