88803bfa0f3a2c839f6c97be16a48613d95b9ec1
DPorts Builder <nobody@home.ok>
2019-08-31 02:58:44 -0700

    Update devel/nspr to version 4.22

--- devel/nspr/dragonfly/patch-nsprpub_pr_include_md__pth.h
+++ devel/nspr/dragonfly/patch-nsprpub_pr_include_md__pth.h
@@ -1,28 +1,38 @@
---- pr/include/md/_pth.h.orig	2014-06-05 21:42:08.000000000 +0000
+--- pr/include/md/_pth.h.orig	2019-08-19 14:12:12 UTC
 +++ pr/include/md/_pth.h
-@@ -114,7 +114,7 @@
- #define _PT_PTHREAD_COPY_THR_HANDLE(st, dt)   (dt) = (st)
- #elif defined(IRIX) || defined(OSF1) || defined(AIX) || defined(SOLARIS) \
+@@ -86,7 +86,7 @@
+  */
+ #if defined(AIX) || defined(SOLARIS) \
  	|| defined(LINUX) || defined(__GNU__) || defined(__GLIBC__) \
 -	|| defined(HPUX) || defined(FREEBSD) \
 +	|| defined(HPUX) || defined(FREEBSD) || defined(__DragonFly__) \
  	|| defined(NETBSD) || defined(OPENBSD) || defined(BSDI) \
  	|| defined(NTO) || defined(DARWIN) \
- 	|| defined(UNIXWARE) || defined(RISCOS)	|| defined(SYMBIAN)
-@@ -201,7 +201,7 @@
- #endif /* defined(_PR_DCETHREADS) */
+ 	|| defined(UNIXWARE) || defined(RISCOS)
+@@ -116,7 +116,7 @@
+ 	|| defined(LINUX) || defined(__GNU__)|| defined(__GLIBC__) \
+ 	|| defined(FREEBSD) || defined(NETBSD) || defined(OPENBSD) \
+ 	|| defined(BSDI) || defined(UNIXWARE) \
+-	|| defined(DARWIN)
++	|| defined(DARWIN) || defined(__DragonFly__)
+ #define PT_NO_SIGTIMEDWAIT
+ #endif
  
+@@ -133,7 +133,7 @@
+ #define PT_PRIO_MIN            sched_get_priority_min(SCHED_OTHER)
+ #define PT_PRIO_MAX            sched_get_priority_max(SCHED_OTHER)
  #elif defined(LINUX) || defined(__GNU__) || defined(__GLIBC__) \
--	|| defined(FREEBSD) || defined(SYMBIAN)
-+	|| defined(FREEBSD) || defined(SYMBIAN) || defined(__DragonFly__)
+-	|| defined(FREEBSD)
++	|| defined(FREEBSD) || defined(__DragonFly__)
  #define PT_PRIO_MIN            sched_get_priority_min(SCHED_OTHER)
  #define PT_PRIO_MAX            sched_get_priority_max(SCHED_OTHER)
  #elif defined(NTO)
-@@ -260,6 +260,7 @@ extern int (*_PT_aix_yield_fcn)();
- #elif defined(HPUX) || defined(SOLARIS) \
+@@ -177,7 +177,7 @@ extern int (*_PT_aix_yield_fcn)();
  	|| defined(LINUX) || defined(__GNU__) || defined(__GLIBC__) \
  	|| defined(FREEBSD) || defined(NETBSD) || defined(OPENBSD) \
-+	|| defined(__DragonFly__) \
  	|| defined(BSDI) || defined(NTO) || defined(DARWIN) \
- 	|| defined(UNIXWARE) || defined(RISCOS) || defined(SYMBIAN)
+-	|| defined(UNIXWARE) || defined(RISCOS)
++	|| defined(UNIXWARE) || defined(RISCOS) || defined(__DragonFly__)
  #define _PT_PTHREAD_YIELD()            	sched_yield()
+ #else
+ #error "Need to define _PT_PTHREAD_YIELD for this platform"
--- devel/nspr/dragonfly/patch-nsprpub_pr_src_md_unix_uxrng.c
+++ devel/nspr/dragonfly/patch-nsprpub_pr_src_md_unix_uxrng.c
@@ -1,11 +1,11 @@
---- pr/src/md/unix/uxrng.c.orig	2014-06-05 21:42:08.000000000 +0000
+--- pr/src/md/unix/uxrng.c.orig	2019-08-19 14:12:12 UTC
 +++ pr/src/md/unix/uxrng.c
-@@ -86,7 +86,7 @@ GetHighResClock(void *buf, size_t maxbyt
+@@ -64,7 +64,7 @@ GetHighResClock(void *buf, size_t maxbyt
  
  #elif (defined(LINUX) || defined(FREEBSD) || defined(__FreeBSD_kernel__) \
      || defined(NETBSD) || defined(__NetBSD_kernel__) || defined(OPENBSD) \
--    || defined(SYMBIAN) || defined(__GNU__))
-+    || defined(SYMBIAN) || defined(__GNU__)) || defined(__DragonFly__)
+-    || defined(__GNU__))
++    || defined(__GNU__) || defined(__DragonFly__))
  #include <sys/types.h>
  #include <sys/stat.h>
  #include <fcntl.h>
