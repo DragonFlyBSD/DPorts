8aafc4357a417017eafe73d6dc0b71c9ed478cb9
DPorts Builder <nobody@home.ok>
2019-08-31 11:27:34 -0700

    Update multimedia/oggvideotools to version 0.9.1_14

--- multimedia/oggvideotools/files/patch-src_base_test_decoderTest.cpp
+++ multimedia/oggvideotools/files/patch-src_base_test_decoderTest.cpp
@@ -1,10 +1,10 @@
---- src/base/test/decoderTest.cpp.orig	2016-09-05 11:35:14.000000000 +0300
+--- src/base/test/decoderTest.cpp.orig	2019-07-01 07:30:12 UTC
 +++ src/base/test/decoderTest.cpp
 @@ -5,6 +5,7 @@
  #include "oggDecoder.h"
  #include <vector>
  #include <iostream>
-+#include <cstring> // for memset
++#include <cstring>
  
  int main(int argc, char* argv[])
  {
