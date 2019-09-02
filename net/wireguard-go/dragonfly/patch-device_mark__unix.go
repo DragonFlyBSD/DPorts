27ee549015730301e0d50436940bdc6f5187334a
DPorts Builder <nobody@home.ok>
2019-09-02 09:28:48 -0700

    Import net/wireguard-go version 0.0.20190805

---
+++ net/wireguard-go/dragonfly/patch-device_mark__unix.go
@@ -0,0 +1,8 @@
+--- device/mark_unix.go.orig	2019-05-17 08:28:04 UTC
++++ device/mark_unix.go
+@@ -1,4 +1,4 @@
+-// +build android openbsd freebsd
++// +build android openbsd freebsd dragonfly
+ 
+ /* SPDX-License-Identifier: MIT
+  *
---
+++ net/wireguard-go/dragonfly/patch-ipc_uapi__bsd.go
@@ -0,0 +1,8 @@
+--- ipc/uapi_bsd.go.orig	2019-05-17 08:28:04 UTC
++++ ipc/uapi_bsd.go
+@@ -1,4 +1,4 @@
+-// +build darwin freebsd openbsd
++// +build darwin freebsd openbsd dragonfly
+ 
+ /* SPDX-License-Identifier: MIT
+  *
