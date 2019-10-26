d17a8079fb5f69c8631fce85facca16eac90d425
DPorts Builder <nobody@home.ok>
2019-10-26 05:55:57 -0700

    Update net/qt5-network to version 5.13.0_1

--- net/qt5-network/files/patch-src_network_ssl_qsslcontext_openssl.cpp
+++ net/qt5-network/files/patch-src_network_ssl_qsslcontext_openssl.cpp
@@ -1,12 +1,8 @@
-* 
-* Fix for libressl after openssl111 API change
-*
-*
---- src/network/ssl/qsslcontext_openssl.cpp.orig	2018-12-03 11:15:26 UTC
+--- src/network/ssl/qsslcontext_openssl.cpp.orig	2019-10-01 08:05:51 UTC
 +++ src/network/ssl/qsslcontext_openssl.cpp
-@@ -249,7 +249,7 @@ void QSslContext::applyBackendConfig(QSs
-     if (sslContext->sslConfiguration.backendConfiguration().isEmpty())
-         return;
+@@ -265,7 +265,7 @@ void QSslContext::applyBackendConfig(QSslContext *sslC
+     }
+ #endif // ocsp
  
 -#if OPENSSL_VERSION_NUMBER >= 0x10002000L
 +#if OPENSSL_VERSION_NUMBER >= 0x10002000L && !defined(LIBRESSL_VERSION_NUMBER)
--- net/qt5-network/files/patch-src_network_ssl_qsslsocket_openssl_symbols_p.h
+++ net/qt5-network/files/patch-src_network_ssl_qsslsocket_openssl_symbols_p.h
@@ -1,13 +1,8 @@
-Define maximum TLS version as 1.2 so as to not hit any possibly
-unsupported TLS 1.3 symbols.
-
-Also do not define SSL_CONF_CTX symbols absent from LibreSSL.
-
---- src/network/ssl/qsslsocket_openssl_symbols_p.h.orig	2018-12-03 11:15:26 UTC
+--- src/network/ssl/qsslsocket_openssl_symbols_p.h.orig	2019-10-01 07:47:06 UTC
 +++ src/network/ssl/qsslsocket_openssl_symbols_p.h
-@@ -74,6 +74,13 @@
- 
- QT_BEGIN_NAMESPACE
+@@ -72,6 +72,12 @@
+ #include "qsslsocket_openssl_p.h"
+ #include <QtCore/qglobal.h>
  
 +#if defined(LIBRESSL_VERSION_NUMBER) && LIBRESSL_VERSION_NUMBER >= 0x20700000L
 +# define TLS1_2_VERSION 0x0303
@@ -15,16 +10,15 @@ Also do not define SSL_CONF_CTX symbols absent from LibreSSL.
 +# define TLS_ANY_VERSION 0x10000
 +#endif
 +
-+
- #define DUMMYARG
- 
- #if !defined QT_LINKED_OPENSSL
-@@ -359,7 +366,7 @@ int q_SSL_CTX_use_PrivateKey(SSL_CTX *a, EVP_PKEY *b);
+ #if QT_CONFIG(ocsp)
+ #include "qocsp_p.h"
+ #endif
+@@ -372,7 +378,7 @@ int q_SSL_CTX_use_PrivateKey(SSL_CTX *a, EVP_PKEY *b);
  int q_SSL_CTX_use_RSAPrivateKey(SSL_CTX *a, RSA *b);
  int q_SSL_CTX_use_PrivateKey_file(SSL_CTX *a, const char *b, int c);
  X509_STORE *q_SSL_CTX_get_cert_store(const SSL_CTX *a);
 -#if OPENSSL_VERSION_NUMBER >= 0x10002000L
-+#if OPENSSL_VERSION_NUMBER >= 0x10002000L && !defined(LIBRESSL_VERSION_NUMBER)
++#if OPENSSL_VERSION_NUMBER >= 0x10002000L  && !defined(LIBRESSL_VERSION_NUMBER)
  SSL_CONF_CTX *q_SSL_CONF_CTX_new();
  void q_SSL_CONF_CTX_free(SSL_CONF_CTX *a);
  void q_SSL_CONF_CTX_set_ssl_ctx(SSL_CONF_CTX *a, SSL_CTX *b);
