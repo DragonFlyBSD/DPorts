2fc5c5fc57300320633f1ba4d1456c31f7984de8
DPorts Builder <nobody@home.ok>
2019-10-27 09:41:28 -0700

    Sync Mk with ports

--- Mk/bsd.default-versions.mk
+++ Mk/bsd.default-versions.mk
@@ -64,7 +64,7 @@ LINUX_DEFAULT?=		c7
 # Possible values: 60, 70, 80, 90, -devel (to be used when non-base compiler is required)
 # Please give notice to the Graphics Team (x11@FreeBSD.org) in advance before 
 # bumping the LLVM version.
-LLVM_DEFAULT?=		90
+LLVM_DEFAULT?=		80
 # Possible values: 5.1, 5.2, 5.3
 LUA_DEFAULT?=		5.2
 # Possible values: 5.5, 5.6, 5.7, 8.0, 5.5m, 10.0m, 10.1m, 10.2m, 10.3m, 5.5p, 5.6p, 5.7p, 5.6w
