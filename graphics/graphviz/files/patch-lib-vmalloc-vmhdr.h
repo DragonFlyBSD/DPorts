f7d8659b0fd5c5bd280b0a69e990aba4606997eb
DPorts Builder <nobody@home.ok>
2019-10-26 22:04:51 -0700

    Update graphics/graphviz to version 2.42.2_2

--- graphics/graphviz/files/patch-lib-vmalloc-vmhdr.h
+++ graphics/graphviz/files/patch-lib-vmalloc-vmhdr.h
@@ -1,4 +1,4 @@
---- lib/vmalloc/vmhdr.h.orig	2016-12-22 05:44:41 UTC
+--- lib/vmalloc/vmhdr.h.orig	2019-09-12 02:11:30 UTC
 +++ lib/vmalloc/vmhdr.h
 @@ -20,6 +20,10 @@ extern "C" {
  #ifndef _BLD_vmalloc
@@ -6,8 +6,8 @@
  #endif
 +#if defined(__FreeBSD__) && defined(__aarch64__)
 +/* No sbrk on FreeBSD/AArch64 */
-+#define _std_malloc	1
++#define _BLD_INSTRUMENT	1
 +#endif
- #ifdef WIN32
+ #ifdef _WIN32
  #include <io.h>
  #endif
