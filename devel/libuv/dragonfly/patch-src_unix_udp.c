47ea7ca0523dc1f5e9628f9a9bfbcfcfb11d1882
DPorts Builder <nobody@home.ok>
2019-10-28 15:42:40 -0700

    Tweak devel/libuv version 1.33.1

--- devel/libuv/dragonfly/patch-src_unix_udp.c
+++ devel/libuv/dragonfly/patch-src_unix_udp.c
@@ -9,3 +9,12 @@
  static int uv__udp_set_source_membership4(uv_udp_t* handle,
                                            const struct sockaddr_in* multicast_addr,
                                            const char* interface_addr,
+@@ -842,7 +842,7 @@ int uv_udp_set_source_membership(uv_udp_
+                                  const char* interface_addr,
+                                  const char* source_addr,
+                                  uv_membership membership) {
+-#if !defined(__OpenBSD__) && !defined(__NetBSD__)
++#if !defined(__OpenBSD__) && !defined(__NetBSD__) && !defined(__DragonFly__)
+   int err;
+   struct sockaddr_storage mcast_addr;
+   struct sockaddr_in* mcast_addr4;
