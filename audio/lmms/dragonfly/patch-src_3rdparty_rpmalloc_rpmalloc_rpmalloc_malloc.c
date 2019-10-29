9cb6789e6b56ade08001eec3679acca3c0d3b14e
DPorts Builder <nobody@home.ok>
2019-10-29 10:46:46 -0700

    Update audio/lmms to version 1.2.1,2

--- audio/lmms/dragonfly/patch-src_3rdparty_rpmalloc_rpmalloc_rpmalloc_malloc.c
+++ audio/lmms/dragonfly/patch-src_3rdparty_rpmalloc_rpmalloc_rpmalloc_malloc.c
@@ -1,12 +1,11 @@
---- src/3rdparty/rpmalloc/rpmalloc/rpmalloc/malloc.c.orig	2019-05-29 18:34:03 UTC
+--- src/3rdparty/rpmalloc/rpmalloc/rpmalloc/malloc.c.orig	2019-08-08 18:59:16 UTC
 +++ src/3rdparty/rpmalloc/rpmalloc/rpmalloc/malloc.c
-@@ -199,7 +199,8 @@ pthread_create(pthread_t* thread,
+@@ -313,7 +313,7 @@ pthread_create(pthread_t* thread,
                 const pthread_attr_t* attr,
                 void* (*start_routine)(void*),
                 void* arg) {
--#if defined(__linux__) || defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__APPLE__)
-+#if defined(__linux__) || defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__APPLE__) || \
-+    defined(__DragonFly__)
+-#if defined(__linux__) || defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__APPLE__) || defined(__HAIKU__)
++#if defined(__linux__) || defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__APPLE__) || defined(__HAIKU__) || defined(__DragonFly__)
  	char fname[] = "pthread_create";
  #else
  	char fname[] = "_pthread_create";
