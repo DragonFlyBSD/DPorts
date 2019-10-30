7b5ecc1f62fdd9dbe6cb6ca6c67aac3ff98d6283
DPorts Builder <nobody@home.ok>
2019-10-30 10:25:14 -0700

    Update sysutils/screen to version 4.7.0_1

--- sysutils/screen/dragonfly/patch-os.h
+++ sysutils/screen/dragonfly/patch-os.h
@@ -1,35 +1,18 @@
---- os.h.orig	2016-07-03 10:31:48 UTC
+--- os.h.intermediate	2019-10-30 09:38:11 UTC
 +++ os.h
-@@ -250,11 +250,8 @@ extern int errno;
+@@ -250,11 +250,14 @@ extern int errno;
  #endif
  
  #if defined(UTMPOK) || defined(BUGGYGETLOGIN)
--# if defined(SVR4) && !defined(DGUX) && !defined(__hpux) && !defined(linux)
-+# if (defined(SVR4) && !defined(DGUX)) || defined(__DragonFly__)
+-# if (defined(SVR4) && !defined(DGUX) && !defined(__hpux) && !defined(linux)) || defined(__FreeBSD_version)
++# if (defined(SVR4) && !defined(DGUX) && !defined(__hpux) && !defined(linux)) || defined(__FreeBSD_version) || defined(__DragonFly__)
  #  include <utmpx.h>
--#  ifdef UTMPX_FILE /* GNU extension */
--#   define UTMPFILE	UTMPX_FILE
--#  endif
+ #  ifdef UTMPX_FILE /* GNU extension */
+ #   define UTMPFILE	UTMPX_FILE
+ #  endif
++#  ifndef UTMPFILE
++#   define UTMPFILE	_PATH_UTMPX
++#  endif
  #  define utmp		utmpx
  #  define getutent	getutxent
  #  define getutid	getutxid
-@@ -277,15 +274,11 @@ extern int errno;
- # endif /* apollo */
- 
- # ifndef UTMPFILE
--#  ifdef UTMP_FILE
--#   define UTMPFILE	UTMP_FILE
--#  else
--#   ifdef _PATH_UTMP
--#    define UTMPFILE	_PATH_UTMP
-+#   ifdef UTMPX_FILE
-+#     define UTMPFILE	UTMPX_FILE
- #   else
--#    define UTMPFILE	"/etc/utmp"
--#   endif /* _PATH_UTMP */
--#  endif
-+#     define UTMPFILE	_PATH_UTMPX
-+#   endif
- # endif
- 
- #endif /* UTMPOK || BUGGYGETLOGIN */
