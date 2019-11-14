7664592479db3756a8511faab6ef4c446f74a88a
DPorts Builder <nobody@home.ok>
2019-11-14 02:54:20 -0800

    Tweak databases/influxdb version 1.7.6

--- databases/influxdb/dragonfly/patch-src_github.com_pkg_term_termios_termios__bsd.go
+++
@@ -1,8 +0,0 @@
---- src/github.com/pkg/term/termios/termios_bsd.go.orig	2018-07-30 02:16:39 UTC
-+++ src/github.com/pkg/term/termios/termios_bsd.go
-@@ -1,4 +1,4 @@
--// +build darwin freebsd openbsd netbsd
-+// +build darwin freebsd openbsd netbsd dragonfly
- 
- package termios
- 
