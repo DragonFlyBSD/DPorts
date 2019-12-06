379bfafffb8c38d474398e0bb5f6e4c8ce05703e
DPorts Builder <nobody@home.ok>
2019-12-06 12:04:43 -0800

    Tweak devel/autoconf-wrapper version 20131203

--- devel/autoconf-wrapper/files/autotools-wrapper.sh
+++ devel/autoconf-wrapper/files/autotools-wrapper.sh
@@ -25,7 +25,7 @@
 # OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 # SUCH DAMAGE.
 #
-# $FreeBSD: head/devel/autoconf-wrapper/files/autotools-wrapper.sh 340872 2014-01-24 00:14:07Z mat $
+# $FreeBSD$
 #
 
 bindir=%%PREFIX%%/bin
