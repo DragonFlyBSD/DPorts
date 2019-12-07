bc236cf40f9a5821911b33fcbf00e893d405be33
DPorts Builder <nobody@home.ok>
2019-12-07 11:37:31 -0800

    Update lang/perl5-devel to version 5.31.5.287

--- lang/perl5-devel/files/patch-perl.c
+++ lang/perl5-devel/files/patch-perl.c
@@ -1,8 +1,8 @@
 Remove compile time information (reproducible builds)
 
---- perl.c.orig	2019-08-12 09:27:18 UTC
+--- perl.c.orig	2019-11-12 05:02:45 UTC
 +++ perl.c
-@@ -2005,23 +2005,7 @@ S_Internals_V(pTHX_ CV *cv)
+@@ -2007,23 +2007,7 @@ S_Internals_V(pTHX_ CV *cv)
      PUSHs(Perl_newSVpvn_flags(aTHX_ non_bincompat_options,
  			      sizeof(non_bincompat_options) - 1, SVs_TEMP));
  
