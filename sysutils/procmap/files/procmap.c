a1373f34328445964dd04138430b7fe69be01c2a
DPorts Builder <nobody@home.ok>
2019-12-07 19:23:07 -0800

    Tweak sysutils/procmap version 1.0

--- sysutils/procmap/files/procmap.c
+++ sysutils/procmap/files/procmap.c
@@ -31,7 +31,7 @@
  *
  * Reliably print the entire contents of the proc filesystem's "map" files.
  *
- * $FreeBSD: head/sysutils/procmap/files/procmap.c 340872 2014-01-24 00:14:07Z mat $
+ * $FreeBSD$
  *
  ****************************************************************************/
 
