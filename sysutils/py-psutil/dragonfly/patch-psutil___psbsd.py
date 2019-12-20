5dca5d98f7517716231313a8d50698bf2520ea56
DPorts Builder <nobody@home.ok>
2019-12-20 12:44:55 -0800

    Tweak sysutils/py-psutil version 5.6.5

--- sysutils/py-psutil/dragonfly/patch-psutil___psbsd.py
+++ sysutils/py-psutil/dragonfly/patch-psutil___psbsd.py
@@ -1,10 +1,10 @@
---- psutil/_psbsd.py.orig	2017-08-03 16:14:27 UTC
+--- psutil/_psbsd.py.orig	2019-06-28 13:13:13 UTC
 +++ psutil/_psbsd.py
 @@ -23,6 +23,7 @@ from ._common import memoize
  from ._common import memoize_when_activated
  from ._common import NETBSD
  from ._common import OPENBSD
 +from ._common import DRAGONFLY
- from ._common import sockfam_to_enum
- from ._common import socktype_to_enum
  from ._common import usage_percent
+ from ._compat import FileNotFoundError
+ from ._compat import PermissionError
