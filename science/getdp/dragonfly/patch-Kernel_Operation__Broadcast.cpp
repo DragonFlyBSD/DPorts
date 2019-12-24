53aff74f67fd05266f4342f232badacf118cb3df
DPorts Builder <nobody@home.ok>
2019-12-24 11:39:37 -0800

    Update science/getdp to version 3.3.0

--- science/getdp/dragonfly/patch-Kernel_Operation__Broadcast.cpp
+++ science/getdp/dragonfly/patch-Kernel_Operation__Broadcast.cpp
@@ -1,10 +1,10 @@
---- Kernel/Operation_Broadcast.cpp.orig	2019-04-12 10:20:07 UTC
+--- Kernel/Operation_Broadcast.cpp.orig	2019-11-22 11:40:16 UTC
 +++ Kernel/Operation_Broadcast.cpp
 @@ -5,6 +5,7 @@
  
  #include <vector>
  #include <string>
 +#include <cstring>	// for strncpy()
+ #include <sstream>
  #include <stdio.h>
  #include <stdlib.h>
- #include "GetDPConfig.h"
