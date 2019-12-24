70aa99146185cbc4fa9963bc684b4fe976b60dd4
DPorts Builder <nobody@home.ok>
2019-12-24 11:37:43 -0800

    Update sysutils/google-compute-engine-oslogin to version 20191018.00

--- sysutils/google-compute-engine-oslogin/dragonfly/patch-src_include_compat.h
+++ sysutils/google-compute-engine-oslogin/dragonfly/patch-src_include_compat.h
@@ -1,5 +1,5 @@
---- compat.h.orig	2019-04-17 00:46:12 UTC
-+++ compat.h
+--- src/include/compat.h.orig	2019-10-18 20:10:43 UTC
++++ src/include/compat.h
 @@ -15,7 +15,7 @@
  #ifndef OSLOGIN_COMPAT_H
  #define OSLOGIN_COMPAT_H
