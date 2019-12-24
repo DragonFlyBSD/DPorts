b680dcb73c7e474bab50b802c6f385229fa4808e
DPorts Builder <nobody@home.ok>
2019-12-24 08:05:24 -0800

    Import graphics/urho3d version 1.7.1

---
+++ graphics/urho3d/dragonfly/patch-Source_ThirdParty_AngelScript_source_as__memory.cpp
@@ -0,0 +1,11 @@
+--- Source/ThirdParty/AngelScript/source/as_memory.cpp.orig	2019-05-24 15:51:45 UTC
++++ Source/ThirdParty/AngelScript/source/as_memory.cpp
+@@ -38,7 +38,7 @@
+ 
+ #include <stdlib.h>
+ 
+-#if !defined(__APPLE__) && !defined(__SNC__) && !defined(__ghs__) && !defined(__FreeBSD__) && !defined(__OpenBSD__)
++#if !defined(__APPLE__) && !defined(__SNC__) && !defined(__ghs__) && !defined(__FreeBSD__) && !defined(__OpenBSD__) && !defined(__DragonFly__)
+ #include <malloc.h>
+ #endif
+ 
