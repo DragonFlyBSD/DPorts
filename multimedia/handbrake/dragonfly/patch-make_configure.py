9a3284cc70df3d689ba9eccbd6a64082ef044e3e
DPorts Builder <nobody@home.ok>
2019-12-24 17:30:16 -0800

    Update multimedia/handbrake to version 1.3.0_1

--- multimedia/handbrake/dragonfly/patch-make_configure.py
+++ multimedia/handbrake/dragonfly/patch-make_configure.py
@@ -1,11 +1,44 @@
---- make/configure.py.orig	2019-02-22 16:23:51 UTC
+--- make/configure.py.orig	2019-11-09 20:44:32 UTC
 +++ make/configure.py
-@@ -1327,7 +1327,7 @@ def createCLI():
-     grp.add_option( '--disable-ffmpeg-aac', dest="enable_ffmpeg_aac", action='store_false' )
+@@ -1338,7 +1338,7 @@ def createCLI( cross = None ):
+     ## add build options
+     grp = cli.add_argument_group( 'Build Options' )
+     grp.add_argument( '--snapshot', default=False, action='store_true', help='Force a snapshot build' )
+-    h = IfHost( 'Build extra contribs for flatpak packaging', '*-*-linux*', '*-*-freebsd*', none=argparse.SUPPRESS ).value
++    h = IfHost( 'Build extra contribs for flatpak packaging', '*-*-linux*', '*-*-freebsd*', '*-*-dragonfly*', none=argparse.SUPPRESS ).value
+     grp.add_argument( '--flatpak', default=False, action='store_true', help=h )
+     cli.add_argument_group( grp )
  
-     h = IfHost( 'enable Nvidia NVEnc video encoder', '*-*-*', none=optparse.SUPPRESS_HELP ).value
--    grp.add_option( '--enable-nvenc', dest="enable_nvenc", default=not (host.match( '*-*-darwin*' ) or host.match( '*-*-freebsd*' )), action='store_true', help=h )
-+    grp.add_option( '--enable-nvenc', dest="enable_nvenc", default=not (host.match( '*-*-darwin*' ) or host.match( '*-*-dragonfly*' ) or host.match( '*-*-freebsd*' )), action='store_true', help=h )
+@@ -1378,19 +1378,19 @@ def createCLI( cross = None ):
+     h = IfHost( 'enable assembly code in non-contrib modules', 'NOMATCH*-*-darwin*', 'NOMATCH*-*-linux*', none=argparse.SUPPRESS ).value
+     grp.add_argument( '--enable-asm', default=False, action='store_true', help=h )
  
+-    h = IfHost( 'disable GTK GUI', '*-*-linux*', '*-*-freebsd*', none=argparse.SUPPRESS ).value
++    h = IfHost( 'disable GTK GUI', '*-*-linux*', '*-*-freebsd*', '*-*-dragonfly*', none=argparse.SUPPRESS ).value
+     grp.add_argument( '--disable-gtk', default=False, action='store_true', help=h )
  
-     cli.add_option_group( grp )
+-    h = IfHost( 'disable GTK GUI update checks', '*-*-linux*', '*-*-freebsd*', none=argparse.SUPPRESS ).value
++    h = IfHost( 'disable GTK GUI update checks', '*-*-linux*', '*-*-freebsd*', '*-*-dragonfly*', none=argparse.SUPPRESS ).value
+     grp.add_argument( '--disable-gtk-update-checks', default=False, action='store_true', help=h )
+ 
+     h = 'enable GTK GUI for Windows' if (cross is not None and 'mingw' in cross) else argparse.SUPPRESS
+     grp.add_argument( '--enable-gtk-mingw', default=False, action='store_true', help=h )
+ 
+-    h = IfHost( 'Build GUI with GTK4', '*-*-linux*', '*-*-freebsd*', none=argparse.SUPPRESS ).value
++    h = IfHost( 'Build GUI with GTK4', '*-*-linux*', '*-*-freebsd*', '*-*-dragonfly*', none=argparse.SUPPRESS ).value
+     grp.add_argument( '--enable-gtk4', default=False, action='store_true', help=h )
+ 
+-    h = IfHost( 'disable GStreamer (live preview)', '*-*-linux*', '*-*-freebsd*', none=argparse.SUPPRESS ).value
++    h = IfHost( 'disable GStreamer (live preview)', '*-*-linux*', '*-*-freebsd*', '*-*-dragonfly*', none=argparse.SUPPRESS ).value
+     grp.add_argument( '--disable-gst', default=False, action='store_true', help=h )
+ 
+     h = IfHost( 'x265 video encoder', '*-*-*', none=argparse.SUPPRESS ).value
+@@ -2038,6 +2038,8 @@ int main()
+         doc.addBlank()
+         if host_tuple.system == 'freebsd':
+             doc.add( 'HAS.pthread', 1 )
++        if host_tuple.system == 'dragonfly':
++            doc.add( 'HAS.pthread', 1 )
+         if not strerror_r.fail:
+             doc.add( 'HAS.strerror_r', 1 )
+ 
