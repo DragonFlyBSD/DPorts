6b07966d08696700da57489cc0c663b835a0beac
DPorts Builder <nobody@home.ok>
2019-12-25 03:58:43 -0800

    Update security/keepassxc to version 2.5.1

--- security/keepassxc/dragonfly/patch-src_core_Alloc.cpp
+++ security/keepassxc/dragonfly/patch-src_core_Alloc.cpp
@@ -1,19 +1,10 @@
---- src/core/Alloc.cpp.orig	2019-09-07 13:01:04 UTC
+--- src/core/Alloc.cpp.orig	2019-11-11 19:53:43 UTC
 +++ src/core/Alloc.cpp
-@@ -20,6 +20,7 @@
+@@ -21,6 +21,7 @@
  #include <sodium.h>
  #if defined(Q_OS_MACOS)
  #include <malloc/malloc.h>
 +#elif defined(__DragonFly__)
  #elif defined(Q_OS_FREEBSD)
  #include <malloc_np.h>
- #else
-@@ -63,7 +64,7 @@ void operator delete(void* ptr) noexcept
-     ::operator delete(ptr, _msize(ptr));
- #elif defined(Q_OS_MACOS)
-     ::operator delete(ptr, malloc_size(ptr));
--#elif defined(Q_OS_UNIX)
-+#elif defined(Q_OS_UNIX) && !defined(__DragonFly__)
-     ::operator delete(ptr, malloc_usable_size(ptr));
- #else
-     // whatever OS this is, give up and simply free stuff
+ #elif defined(HAVE_MALLOC_H)
