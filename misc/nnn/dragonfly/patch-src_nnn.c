e0986fc1fa1b04cd655d63cd8f39b8b07077a87b
DPorts Builder <nobody@home.ok>
2019-12-27 09:33:43 -0800

    Update misc/nnn to version 2.8.1

--- misc/nnn/dragonfly/patch-src_nnn.c
+++ misc/nnn/dragonfly/patch-src_nnn.c
@@ -1,4 +1,4 @@
---- src/nnn.c.orig	2019-03-18 21:47:22 UTC
+--- src/nnn.c.orig	2019-12-04 23:17:32 UTC
 +++ src/nnn.c
 @@ -44,7 +44,7 @@
  #include <sys/resource.h>
@@ -9,26 +9,18 @@
  #include <sys/types.h>
  #include <sys/event.h>
  #include <sys/time.h>
-@@ -58,7 +58,7 @@
+@@ -58,7 +58,8 @@
  #ifndef NCURSES_WIDECHAR
  #define NCURSES_WIDECHAR 1
  #endif
--#elif defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__) || defined(__APPLE__)
-+#elif defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__) || defined(__APPLE__) || defined(__DragonFly__)
+-#elif defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__) || defined(__APPLE__) || defined(__sun)
++#elif defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__) || defined(__APPLE__) || defined(__sun) || \
++  defined(__DragonFly__)
  #ifndef _XOPEN_SOURCE_EXTENDED
  #define _XOPEN_SOURCE_EXTENDED
  #endif
-@@ -277,7 +277,7 @@ static char g_cppath[PATH_MAX] __attribu
- static char g_tmpfpath[HOME_LEN_MAX] __attribute__ ((aligned));
- 
- /* Replace-str for xargs on different platforms */
--#if defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__) || defined(__APPLE__)
-+#if defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__) || defined(__APPLE__) || defined(__DragonFly__)
- #define REPLACE_STR 'J'
- #elif defined(__linux__) || defined(__CYGWIN__)
- #define REPLACE_STR 'I'
-@@ -319,7 +319,7 @@ static char * const utils[] = {
- 	"vidir",
+@@ -393,7 +394,7 @@ static char * const utils[] = {
+ 	"tar",
  #ifdef __APPLE__
  	"bashlock",
 -#elif defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__)
