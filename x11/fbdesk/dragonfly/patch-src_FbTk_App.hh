d4fae9819828749352fc6cbf7c2c35568a21c868
DPorts Builder <nobody@home.ok>
2019-12-30 07:44:56 -0800

    Import x11/fbdesk version 1.4.1_6

---
+++ x11/fbdesk/dragonfly/patch-src_FbTk_App.hh
@@ -0,0 +1,10 @@
+--- src/FbTk/App.hh.orig	2006-03-25 11:47:09.000000000 +0000
++++ src/FbTk/App.hh
+@@ -23,6 +23,7 @@
+ #define FBTK_APP_HH
+ 
+ #include <X11/Xlib.h>
++#include <cstring>
+ 
+ namespace FbTk {
+ 
