90fb975c71395279061cb45efee9855bcbb01a6d
DPorts Builder <nobody@home.ok>
2020-01-23 09:12:14 -0800

    Sync Mk with ports

--- Mk/bsd.port.mk
+++ Mk/bsd.port.mk
@@ -1043,7 +1043,7 @@ NOTPHONY?=
 FLAVORS?=
 FLAVOR?=
 OVERLAYS?=
-REWARNFILE?=	${WRKDIR}/reinplace_warnings.txt
+REWARNFILE=	${WRKDIR}/reinplace_warnings.txt
 # Disallow forced FLAVOR as make argument since we cannot change it to the
 # proper default.
 .if empty(FLAVOR) && !empty(.MAKEOVERRIDES:MFLAVOR)
