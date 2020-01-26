743b758e6a7d86e7fdbb4b2effbc0b57c4e6a924
DPorts Builder <nobody@home.ok>
2020-01-26 10:55:22 -0800

    Update deskutils/calibre to version 4.8.0_2

--- deskutils/calibre/dragonfly/patch-setup_build__environment.py
+++ deskutils/calibre/dragonfly/patch-setup_build__environment.py
@@ -1,14 +1,20 @@
---- setup/build_environment.py.orig	2018-04-09 16:44:19 UTC
+--- setup/build_environment.py.intermediate	2020-01-26 14:54:08.000000000 +0000
 +++ setup/build_environment.py
-@@ -96,10 +96,8 @@ pyqt['sip_flags'] = PYQT_CONFIGURATION['
- def get_sip_dir():
-     if iswindows:
-         q = os.path.join(sys.prefix, 'share', 'sip')
--    elif isfreebsd:
--        q = os.path.join(sys.prefix, 'share', 'py-sip')
-     else:
--        q = os.path.join(sys.prefix, 'share', 'sip')
-+        q = os.path.join(sys.prefix, 'share', 'py-sip')
-     q = os.environ.get('SIP_DIR', q)
-     for x in ('', 'Py2-PyQt5', 'PyQt5', 'sip/PyQt5'):
-         base = os.path.join(q, x)
+@@ -9,7 +9,7 @@ __docformat__ = 'restructuredtext en'
+ import os, subprocess, re, sys, sysconfig
+ from distutils.spawn import find_executable
+ 
+-from setup import isfreebsd, isosx, iswindows, is64bit, islinux, ishaiku
++from setup import isfreebsd, isosx, iswindows, is64bit, islinux, ishaiku, isdragonflybsd
+ is64bit
+ 
+ NMAKE = RC = msvc = MT = win_inc = win_lib = None
+@@ -103,7 +103,7 @@ def get_sip_dir():
+     if q is None:
+         if iswindows:
+             q = os.path.join(sys.prefix, 'share', 'sip')
+-        elif isfreebsd:
++        elif isfreebsd or isdragonflybsd:
+             q = os.path.join(sys.prefix, 'share', 'py-sip')
+         else:
+             q = os.path.join(os.path.dirname(PyQt5.__file__), 'bindings')
