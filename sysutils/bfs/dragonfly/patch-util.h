316db13ff20bfd066436a4dce0f613162d8cf18a
DPorts Builder <nobody@home.ok>
2020-01-28 08:44:40 -0800

    Update sysutils/bfs to version 1.5.2

--- sysutils/bfs/dragonfly/patch-util.h
+++ sysutils/bfs/dragonfly/patch-util.h
@@ -1,6 +1,6 @@
---- util.h.orig	2019-05-06 03:32:04 UTC
+--- util.h.orig	2020-01-09 15:14:40 UTC
 +++ util.h
-@@ -48,8 +48,10 @@
+@@ -48,16 +48,20 @@
  #endif
  
  #ifndef BFS_HAS_SYS_ACL
@@ -11,3 +11,13 @@
  
  #ifndef BFS_HAS_SYS_CAPABILITY
  #	define BFS_HAS_SYS_CAPABILITY BFS_HAS_INCLUDE(<sys/capability.h>, __linux__)
+ #endif
+ 
+ #ifndef BFS_HAS_SYS_EXTATTR
++#ifndef __DragonFly__
+ #	define BFS_HAS_SYS_EXTATTR BFS_HAS_INCLUDE(<sys/extattr.h>, __FreeBSD__)
+ #endif
++#endif
+ 
+ #ifndef BFS_HAS_SYS_MKDEV
+ #	define BFS_HAS_SYS_MKDEV BFS_HAS_INCLUDE(<sys/mkdev.h>, false)
