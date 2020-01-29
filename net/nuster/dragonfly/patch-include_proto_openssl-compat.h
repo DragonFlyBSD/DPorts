457ee98a52c5ad16d4d6a3645b79d98889a8d972
DPorts Builder <nobody@home.ok>
2020-01-29 02:35:11 -0800

    Tweak net/nuster version 3.0.0.19

---
+++ net/nuster/dragonfly/patch-include_proto_openssl-compat.h
@@ -0,0 +1,11 @@
+--- include/proto/openssl-compat.h.orig	2019-07-29 13:47:40 UTC
++++ include/proto/openssl-compat.h
+@@ -89,7 +89,7 @@ static inline int SSL_SESSION_set1_id_co
+ }
+ #endif
+ 
+-#if (OPENSSL_VERSION_NUMBER < 0x1010000fL) || defined(LIBRESSL_VERSION_NUMBER)
++#if (OPENSSL_VERSION_NUMBER < 0x1010000fL)
+ /*
+  * Functions introduced in OpenSSL 1.1.0 and not yet present in LibreSSL
+  */
