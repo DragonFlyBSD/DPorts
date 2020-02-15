c8c1e35e3c1a3d4469937f7af3a28e2b3f192a84
DPorts Builder <nobody@home.ok>
2020-02-15 13:20:35 -0800

    Import www/minio version 2020.01.03.19.12.21

---
+++ www/minio/dragonfly/patch-cmd_posix-dirent__namelen__bsd.go
@@ -0,0 +1,8 @@
+--- cmd/posix-dirent_namelen_bsd.go.old	2020-02-08 11:54:43 UTC
++++ cmd/posix-dirent_namelen_bsd.go
+@@ -1,4 +1,4 @@
+-// +build darwin freebsd openbsd netbsd
++// +build darwin freebsd openbsd netbsd dragonfly
+ 
+ /*
+  * MinIO Cloud Storage, (C) 2019 MinIO, Inc.
