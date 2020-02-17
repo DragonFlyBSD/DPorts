583bb2ab8204d116fe97817ca69bcc2c79a2f057
DPorts Builder <nobody@home.ok>
2020-02-17 03:19:18 -0800

    Update graphics/embree to version 3.8.0

--- graphics/embree/dragonfly/patch-common_sys_sysinfo.cpp
+++ graphics/embree/dragonfly/patch-common_sys_sysinfo.cpp
@@ -1,6 +1,16 @@
---- common/sys/sysinfo.cpp.bak	2015-10-16 13:01:59.000000000 +0300
+--- common/sys/sysinfo.cpp.orig	2020-02-04 08:01:09 UTC
 +++ common/sys/sysinfo.cpp
-@@ -30,6 +30,8 @@ namespace embree
+@@ -18,6 +18,9 @@
+ #include "intrinsics.h"
+ #include "string.h"
+ #include "ref.h"
++#if defined(__DragonFly__)
++#include <pthread_np.h>
++#endif
+ #if defined(__FREEBSD__)
+ #include <sys/cpuset.h>
+ #include <pthread_np.h>
+@@ -38,6 +41,8 @@ namespace embree
      return "Linux (32bit)";
  #elif defined(__LINUX__) && defined(__X86_64__)
      return "Linux (64bit)";
@@ -9,11 +19,10 @@
  #elif defined(__FREEBSD__) && !defined(__X86_64__)
      return "FreeBSD (32bit)";
  #elif defined(__FREEBSD__) && defined(__X86_64__)
-@@ -410,6 +412,28 @@ namespace embree
-     return std::string(buf);
+@@ -474,6 +479,37 @@ namespace embree
    }
  }
-+
+ 
 +#endif
 +
 +////////////////////////////////////////////////////////////////////////////////
@@ -26,15 +35,25 @@
 +
 +namespace embree
 +{
-+  std::string getExecutableFileName() 
++  std::string getExecutableFileName()
 +  {
 +    const int mib[4] = { CTL_KERN, KERN_PROC, KERN_PROC_PATHNAME, -1 };
 +    char buf[1024];
 +    size_t len = sizeof(buf);
-+    if (sysctl(mib, 4, buf, &len, 0x0, 0) == -1) *buf = '\0';
++    if (sysctl(mib, 4, buf, &len, 0x0, 0) == -1)
++      return std::string();
 +    return std::string(buf);
 +  }
++
++  size_t getVirtualMemoryBytes() {
++    return 0;
++  }
++
++  size_t getResidentMemoryBytes() {
++    return 0;
++  }
 +}
- 
++
  #endif
  
+ ////////////////////////////////////////////////////////////////////////////////
