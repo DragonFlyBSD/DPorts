4369182e05ebdbaad086eb90dc9a0824ae3e6eaa
DPorts Builder <nobody@home.ok>
2020-02-17 13:10:53 -0800

    Update lang/rust to version 1.41.0

--- lang/rust/dragonfly/patch-vendor_libc_src_unix_bsd_freebsdlike_dragonfly_mod.rs
+++ lang/rust/dragonfly/patch-vendor_libc_src_unix_bsd_freebsdlike_dragonfly_mod.rs
@@ -1,20 +1,10 @@
---- vendor/libc/src/unix/bsd/freebsdlike/dragonfly/mod.rs.orig	2019-12-16 17:26:11 UTC
+--- vendor/libc/src/unix/bsd/freebsdlike/dragonfly/mod.rs.orig	2020-01-27 17:21:00 UTC
 +++ vendor/libc/src/unix/bsd/freebsdlike/dragonfly/mod.rs
-@@ -1053,7 +1053,17 @@ f! {
-     }
+@@ -1054,6 +1054,7 @@ f! {
  }
  
-+// DragonFlyBSD's __error function is declared with "static inline", so it must
-+// be implemented in the libc crate, as a pointer to a static thread_local.
-+f! {
-+    pub fn __error() -> *mut ::c_int {
-+        &mut errno
-+    }
-+}
-+
  extern "C" {
-+    #[thread_local]
-+    pub static mut errno: ::c_int;
++    pub fn __errno_location() -> *mut ::c_int;
      pub fn setgrent();
      pub fn mprotect(
          addr: *mut ::c_void,
