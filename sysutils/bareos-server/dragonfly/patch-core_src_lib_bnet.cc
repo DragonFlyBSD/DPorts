a3b470a70edbb93d84899718e64065b05b671312
DPorts Builder <nobody@home.ok>
2020-02-18 07:53:05 -0800

    Update sysutils/bareos-server to version 19.2.6

--- sysutils/bareos-server/dragonfly/patch-core_src_lib_bnet.cc
+++ sysutils/bareos-server/dragonfly/patch-core_src_lib_bnet.cc
@@ -1,20 +1,20 @@
---- core/src/lib/bnet.cc.orig	2019-12-12 17:04:14 UTC
+--- core/src/lib/bnet.cc.orig	2020-02-11 15:58:26 UTC
 +++ core/src/lib/bnet.cc
-@@ -206,7 +206,7 @@ err:
-    return false;
+@@ -201,7 +201,7 @@ err:
+   return false;
  }
  #else
 -bool BnetTlsServer(std::shared_ptr<TlsImplementation> tls_implementation,
 +bool BnetTlsServer(/* std::shared_ptr<TlsImplementation> tls_implementation, */
-                    BareosSocket *bsock,
-                    const std::vector<std::string> &verify_list)
+                    BareosSocket* bsock,
+                    const std::vector<std::string>& verify_list)
  {
-@@ -214,7 +214,7 @@ bool BnetTlsServer(std::shared_ptr<TlsIm
-    return false;
+@@ -209,7 +209,7 @@ bool BnetTlsServer(std::shared_ptr<TlsIm
+   return false;
  }
  
 -bool BnetTlsClient(std::shared_ptr<TLS_IMPLEMENTATION> tls_implementation,
 +bool BnetTlsClient(/* std::shared_ptr<TLS_IMPLEMENTATION> tls_implementation, */
-                    BareosSocket *bsock,
+                    BareosSocket* bsock,
                     bool VerifyPeer,
-                    const std::vector<std::string> &verify_list)
+                    const std::vector<std::string>& verify_list)
--- sysutils/bareos-server/dragonfly/patch-core_src_lib_tls.cc
+++ sysutils/bareos-server/dragonfly/patch-core_src_lib_tls.cc
@@ -1,6 +1,6 @@
 gHackup for LibreSSL.
 
---- core/src/lib/tls.cc.orig	2019-12-12 17:04:14 UTC
+--- core/src/lib/tls.cc.orig	2020-02-11 15:58:26 UTC
 +++ core/src/lib/tls.cc
 @@ -22,6 +22,8 @@
  #include "lib/tls.h"
@@ -8,17 +8,17 @@ gHackup for LibreSSL.
  
 +#include <openssl/opensslv.h>
 +
- Tls::Tls()
+ Tls::Tls() { return; }
+ 
+ Tls::~Tls() { return; }
+@@ -29,8 +31,10 @@ Tls::~Tls() { return; }
+ Tls* Tls::CreateNewTlsContext(Tls::TlsImplementationType type)
  {
-    return;
-@@ -35,8 +37,10 @@ Tls::~Tls()
- Tls *Tls::CreateNewTlsContext(Tls::TlsImplementationType type)
- {
-    switch (type) {
+   switch (type) {
 +#ifndef LIBRESSL_VERSION_NUMBER
-       case Tls::TlsImplementationType::kTlsOpenSsl:
-          return new TlsOpenSsl();
+     case Tls::TlsImplementationType::kTlsOpenSsl:
+       return new TlsOpenSsl();
 +#endif
  
-       case Tls::TlsImplementationType::kTlsUnknown:
-       default:
+     case Tls::TlsImplementationType::kTlsUnknown:
+     default:
