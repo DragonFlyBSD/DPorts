efb4b90c73fa42b466acd718d211a2db4ca3ab50
DPorts Builder <nobody@home.ok>
2020-03-20 07:46:39 -0700

    Update x11/sddm to version 0.18.1_2

--- x11/sddm/files/patch-src_helper_Backend.cpp
+++ x11/sddm/files/patch-src_helper_Backend.cpp
@@ -11,22 +11,37 @@
  
  namespace SDDM {
      Backend::Backend(HelperApp* parent)
-@@ -70,6 +74,19 @@ namespace SDDM {
+@@ -70,6 +74,34 @@ namespace SDDM {
                          .arg(mainConfig.X11.UserAuthFile.get());
                  env.insert(QStringLiteral("XAUTHORITY"), value);
              }
 +#if defined(Q_OS_FREEBSD)
 +	    /* get additional environment variables via setclassenvironment();
 +	       this needs to be done here instead of in UserSession::setupChildProcess
-+	       as the environment for execve() is prepared here */
++	       as the environment for execve() is prepared here;
++	       save and restore SDDM's environment because setclassenvironment() mangles it */
 +	    login_cap_t *lc;
 +
-+	    if ((lc = login_getpwclass(pw)) != 0) {
++        auto savedEnv = QProcessEnvironment::systemEnvironment();
++
++	    if ((lc = login_getpwclass(pw)) != NULL) {
 +		setclassenvironment(lc, pw, 1);		/* path variables */
 +		setclassenvironment(lc, pw, 0);		/* non-path variables */
-+		/* copy all environment variables that are now set */
-+		env.insert(QProcessEnvironment::systemEnvironment());
++		login_close(lc);
 +	    }
++	    if ((lc = login_getuserclass(pw)) != NULL) {
++		setclassenvironment(lc, pw, 1);		/* path variables */
++		setclassenvironment(lc, pw, 0);		/* non-path variables */
++		login_close(lc);
++	    }
++		/* copy all environment variables that are now set */
++        QString savedLang = env.value(QStringLiteral("LANG"));
++		env.insert(QProcessEnvironment::systemEnvironment());
++        if (qobject_cast<HelperApp*>(parent())->user() == QStringLiteral("sddm"))
++            env.insert(QStringLiteral("LANG"), savedLang);
++        /* restore original environment */
++        QProcessEnvironment::systemEnvironment().clear();
++        QProcessEnvironment::systemEnvironment().insert(savedEnv);
 +#endif /* defined(Q_OS_FREEBSD) */
              // TODO: I'm fairly sure this shouldn't be done for PAM sessions, investigate!
              m_app->session()->setProcessEnvironment(env);
