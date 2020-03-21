53fcd1164dd4cbfe76bd3dee21cde33db5d21eb3
DPorts Builder <nobody@home.ok>
2020-03-21 07:32:04 -0700

    Update lang/go to version 1.14.1,1

--- lang/go/dragonfly/patch-src_os_executable__procfs.go
+++ lang/go/dragonfly/patch-src_os_executable__procfs.go
@@ -1,20 +1,11 @@
---- src/os/executable_procfs.go.orig	2019-12-04 22:53:55 UTC
+--- src/os/executable_procfs.go.intermediate	2020-03-21 13:58:14 UTC
 +++ src/os/executable_procfs.go
 @@ -2,7 +2,7 @@
  // Use of this source code is governed by a BSD-style
  // license that can be found in the LICENSE file.
  
--// +build linux netbsd dragonfly nacl js,wasm
-+// +build linux netbsd nacl js,wasm
+-// +build linux netbsd dragonfly js,wasm
++// +build linux netbsd js,wasm
  
  package os
  
-@@ -23,8 +23,6 @@ var executablePath, executablePathErr =
- 		procfn = "/proc/self/exe"
- 	case "netbsd":
- 		procfn = "/proc/curproc/exe"
--	case "dragonfly":
--		procfn = "/proc/curproc/file"
- 	}
- 	return Readlink(procfn)
- }()
