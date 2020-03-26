3d54682e08c859dd40347824901074b3dd7f1218
DPorts Builder <nobody@home.ok>
2020-03-26 04:39:06 -0700

    Tweak sysutils/py-psutil version 5.7.0

--- sysutils/py-psutil/dragonfly/patch-psutil___common.py
+++ sysutils/py-psutil/dragonfly/patch-psutil___common.py
@@ -1,15 +1,15 @@
---- psutil/_common.py.orig	2018-07-28 19:10:45 UTC
+--- psutil/_common.py.orig	2020-02-18 17:46:14 UTC
 +++ psutil/_common.py
-@@ -43,7 +43,7 @@ PY3 = sys.version_info[0] == 3
+@@ -45,7 +45,7 @@ PY3 = sys.version_info[0] == 3
  __all__ = [
-     # constants
+     # OS constants
      'FREEBSD', 'BSD', 'LINUX', 'NETBSD', 'OPENBSD', 'MACOS', 'OSX', 'POSIX',
 -    'SUNOS', 'WINDOWS',
 +    'SUNOS', 'WINDOWS', 'DRAGONFLY',
-     'ENCODING', 'ENCODING_ERRS', 'AF_INET6',
      # connection constants
      'CONN_CLOSE', 'CONN_CLOSE_WAIT', 'CONN_CLOSING', 'CONN_ESTABLISHED',
-@@ -80,7 +80,8 @@ OSX = MACOS  # deprecated alias
+     'CONN_FIN_WAIT1', 'CONN_FIN_WAIT2', 'CONN_LAST_ACK', 'CONN_LISTEN',
+@@ -86,7 +86,8 @@ OSX = MACOS  # deprecated alias
  FREEBSD = sys.platform.startswith("freebsd")
  OPENBSD = sys.platform.startswith("openbsd")
  NETBSD = sys.platform.startswith("netbsd")
