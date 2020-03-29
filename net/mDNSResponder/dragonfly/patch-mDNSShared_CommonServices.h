25fe39b29ba6a9ff41e29270ca24b2ecf0b67401
DPorts Builder <nobody@home.ok>
2020-03-29 14:21:29 -0700

    Update net/mDNSResponder to version 1096.0.2

--- net/mDNSResponder/dragonfly/patch-mDNSShared_CommonServices.h
+++ net/mDNSResponder/dragonfly/patch-mDNSShared_CommonServices.h
@@ -1,4 +1,4 @@
---- mDNSShared/CommonServices.h.orig	2011-12-02 00:39:45.000000000 +0000
+--- mDNSShared/CommonServices.h.orig	2018-09-27 21:05:12 UTC
 +++ mDNSShared/CommonServices.h
 @@ -54,6 +54,16 @@ extern "C" {
      #endif
@@ -17,24 +17,7 @@
  // FreeBSD
  
  #if ( !defined( TARGET_OS_FREEBSD ) )
-@@ -100,7 +110,15 @@ extern "C" {
- 
- // No predefined macro for VxWorks so just assume VxWorks if nothing else is set.
- 
--    #if ( !macintosh && !__MACH__  && !defined( __FreeBSD__ ) && !defined( __linux__ ) && !defined ( __SVR4 ) && !defined ( __sun ) && !defined( __PALMOS_TRAPS__ ) && !defined( __PALMOS_ARMLET__ ) && !defined( _WIN32 ) )
-+    #if ( !macintosh && !__MACH__  \
-+      && !defined(__DragonFly__) \
-+      && !defined( __FreeBSD__ ) \
-+      && !defined( __linux__ ) \
-+      && !defined( __SVR4 ) \
-+      && !defined( __sun ) \
-+      && !defined( __PALMOS_TRAPS__ ) \
-+      && !defined( __PALMOS_ARMLET__ ) \
-+      && !defined( _WIN32 ) )
-         #define TARGET_OS_VXWORKS       1
-     #else
-         #define TARGET_OS_VXWORKS       0
-@@ -189,6 +207,15 @@ extern "C" {
+@@ -176,6 +186,15 @@ extern "C" {
      #include    <libkern/OSTypes.h>
      #include    <sys/types.h>
  
@@ -50,17 +33,12 @@
  #elif ( TARGET_OS_FREEBSD )
  
  // FreeBSD
-@@ -474,7 +501,12 @@ typedef int socklen_t;
+@@ -439,7 +458,7 @@ typedef int socklen_t;
+ // - Mac OS X when not building with BSD headers
  // - Windows
  
- #if ( TARGET_LANGUAGE_C_LIKE )
--    #if ( !defined(_SSIZE_T) && ( TARGET_OS_WIN32 || !defined( _BSD_SSIZE_T_DEFINED_ ) ) && !TARGET_OS_FREEBSD && !TARGET_OS_LINUX && !TARGET_OS_VXWORKS && !TARGET_OS_MAC)
-+    #if ( !defined(_SSIZE_T) && ( TARGET_OS_WIN32 || !defined( _BSD_SSIZE_T_DEFINED_ ) ) \
-+       && !TARGET_OS_DRAGONFLY \
-+       && !TARGET_OS_FREEBSD \
-+       && !TARGET_OS_LINUX \
-+       && !TARGET_OS_VXWORKS \
-+       && !TARGET_OS_MAC)
+-#if ( !defined(_SSIZE_T) && ( TARGET_OS_WIN32 || !defined( _BSD_SSIZE_T_DEFINED_ ) ) && !TARGET_OS_FREEBSD && !TARGET_OS_LINUX && !TARGET_OS_MAC)
++#if ( !defined(_SSIZE_T) && ( TARGET_OS_WIN32 || !defined( _BSD_SSIZE_T_DEFINED_ ) ) && !TARGET_OS_FREEBSD && !TARGET_OS_LINUX && !TARGET_OS_MAC && !TARGET_OS_DRAGONFLY)
  typedef int ssize_t;
-     #endif
  #endif
+ 
