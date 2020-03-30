af0c3a362ca5ae10f1fd327fefbd2c31b3f5d9b7
DPorts Builder <nobody@home.ok>
2020-03-30 06:58:56 -0700

    Update sysutils/facter to version 3.14.8

--- sysutils/facter/dragonfly/patch-lib_src_facts_dragonfly_collection.cc
+++ sysutils/facter/dragonfly/patch-lib_src_facts_dragonfly_collection.cc
@@ -1,4 +1,4 @@
---- lib/src/facts/dragonfly/collection.cc.orig	2019-07-16 16:31:27 UTC
+--- lib/src/facts/dragonfly/collection.cc.orig	2020-03-30 13:02:38 UTC
 +++ lib/src/facts/dragonfly/collection.cc
 @@ -0,0 +1,28 @@
 +#include <facter/facts/collection.hpp>
@@ -7,7 +7,7 @@
 +#include <internal/facts/glib/load_average_resolver.hpp>
 +#include <internal/facts/posix/identity_resolver.hpp>
 +#include <internal/facts/posix/kernel_resolver.hpp>
-+#include <internal/facts/posix/ssh_resolver.hpp>
++#include <internal/facts/ssh_resolver.hpp>
 +#include <internal/facts/posix/timezone_resolver.hpp>
 +#include <internal/facts/posix/operating_system_resolver.hpp>
 +
@@ -19,7 +19,7 @@
 +    {
 +        add(make_shared<posix::kernel_resolver>());
 +        add(make_shared<bsd::uptime_resolver>());
-+        add(make_shared<posix::ssh_resolver>());
++        add(make_shared<ssh_resolver>());
 +        add(make_shared<posix::identity_resolver>());
 +        add(make_shared<posix::timezone_resolver>());
 +        add(make_shared<bsd::filesystem_resolver>());
