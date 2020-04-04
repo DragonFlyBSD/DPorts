0c23061ad2b8813150a7631b6e88dcddeb3c75de
DPorts Builder <nobody@home.ok>
2020-04-03 19:03:29 -0700

    Update www/zola to version 0.10.0_2

--- www/zola/dragonfly/patch-libc_mod.rs
+++ www/zola/dragonfly/patch-libc_mod.rs
@@ -1,10 +1,10 @@
---- cargo-crates/libc-0.2.62/src/unix/bsd/freebsdlike/dragonfly/mod.rs.orig	2019-07-15 09:14:44 UTC
-+++ cargo-crates/libc-0.2.62/src/unix/bsd/freebsdlike/dragonfly/mod.rs
-@@ -1039,6 +1039,7 @@ f! {
+--- cargo-crates/libc-0.2.66/src/unix/bsd/freebsdlike/dragonfly/mod.rs.orig	2019-09-16 18:34:34 UTC
++++ cargo-crates/libc-0.2.66/src/unix/bsd/freebsdlike/dragonfly/mod.rs
+@@ -1054,6 +1054,7 @@ f! {
  }
  
- extern {
+ extern "C" {
 +    pub fn __errno_location() -> *mut ::c_int;
      pub fn setgrent();
-     pub fn mprotect(addr: *mut ::c_void, len: ::size_t, prot: ::c_int)
-                     -> ::c_int;
+     pub fn mprotect(
+         addr: *mut ::c_void,
