320dfc163d419c16611cc113c201cbed2df4997d
DPorts Builder <nobody@home.ok>
2020-04-21 06:07:56 -0700

    Update www/webkit2-gtk3 to version 2.28.0

--- www/webkit2-gtk3/dragonfly/patch-Source_ThirdParty_ANGLE_src_compiler_translator_SymbolTable.h
+++ www/webkit2-gtk3/dragonfly/patch-Source_ThirdParty_ANGLE_src_compiler_translator_SymbolTable.h
@@ -1,9 +1,9 @@
---- Source/ThirdParty/ANGLE/src/compiler/translator/SymbolTable.h.orig	2019-08-28 11:46:28 UTC
+--- Source/ThirdParty/ANGLE/src/compiler/translator/SymbolTable.h.orig	2020-02-04 10:24:05 UTC
 +++ Source/ThirdParty/ANGLE/src/compiler/translator/SymbolTable.h
-@@ -30,6 +30,7 @@
- //   are tracked in the intermediate representation, not the symbol table.
+@@ -31,6 +31,7 @@
  //
  
+ #include <limits>
 +#include <cmath>
  #include <memory>
  #include <set>
