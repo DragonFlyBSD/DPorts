33c0bd3bde96e0fd7ec28483b9ad19672d22dc09
DPorts Builder <nobody@home.ok>
2020-05-15 14:47:35 -0700

    Update multimedia/libv4l to version 1.18.0

--- multimedia/libv4l/dragonfly/patch-lib_include_libv4lconvert.h
+++ multimedia/libv4l/dragonfly/patch-lib_include_libv4lconvert.h
@@ -1,11 +1,12 @@
---- lib/include/libv4lconvert.h.orig	2015-06-16 09:34:09 UTC
+--- lib/include/libv4lconvert.h.orig	2017-01-22 17:33:34 UTC
 +++ lib/include/libv4lconvert.h
-@@ -28,7 +28,7 @@
+@@ -28,7 +28,8 @@
  #include <linux/ioctl.h>
  #endif
  
--#if defined(__FreeBSD__) || defined(__FreeBSD_kernel__)
-+#if defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || defined(__DragonFly__)
+-#if defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || defined(__OpenBSD__)
++#if defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || defined(__OpenBSD__) || \
++  defined(__DragonFly__)
  #include <sys/time.h>
  #include <sys/types.h>
  #include <sys/ioctl.h>
