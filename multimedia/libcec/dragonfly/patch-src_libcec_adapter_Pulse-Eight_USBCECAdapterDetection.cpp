8dc7d4a8f8cc69c0f80879cedfe921534567cc6c
DPorts Builder <nobody@home.ok>
2020-05-16 18:34:33 -0700

    Update multimedia/libcec to version 5.0.0

--- multimedia/libcec/dragonfly/patch-src_libcec_adapter_Pulse-Eight_USBCECAdapterDetection.cpp
+++ multimedia/libcec/dragonfly/patch-src_libcec_adapter_Pulse-Eight_USBCECAdapterDetection.cpp
@@ -1,6 +1,6 @@
---- src/libcec/adapter/Pulse-Eight/USBCECAdapterDetection.cpp.orig	2018-08-05 09:39:43.967618000 +0200
-+++ src/libcec/adapter/Pulse-Eight/USBCECAdapterDetection.cpp	2018-08-05 09:42:12.280808000 +0200
-@@ -64,7 +64,7 @@ static GUID USB_CDC_GUID = { 0x4D36E978,
+--- src/libcec/adapter/Pulse-Eight/USBCECAdapterDetection.cpp.orig	2020-05-17 01:14:14 UTC
++++ src/libcec/adapter/Pulse-Eight/USBCECAdapterDetection.cpp
+@@ -63,7 +63,7 @@ static GUID USB_CDC_GUID = { 0x4D36E978,
  extern "C" {
  #include <libudev.h>
  }
@@ -9,16 +9,16 @@
  #include <sys/param.h>
  #include <sys/sysctl.h>
  #include <stdio.h>
-@@ -133,7 +133,7 @@ bool FindComPort(std::string& strLocatio
+@@ -138,7 +138,7 @@ bool FindComPort(std::string& strLocatio
  
  bool CUSBCECAdapterDetection::CanAutodetect(void)
  {
--#if defined(__APPLE__) || defined(HAVE_LIBUDEV) || defined(__WINDOWS__) || defined(__FreeBSD__)
-+#if defined(__APPLE__) || defined(HAVE_LIBUDEV) || defined(__WINDOWS__) || defined(__FreeBSD__) || defined(__DragonFly__)
+-#if defined(__APPLE__) || defined(HAVE_LIBUDEV) || defined(__WINDOWS__) || defined(__FreeBSD__) || defined(__linux__)
++#if defined(__APPLE__) || defined(HAVE_LIBUDEV) || defined(__WINDOWS__) || defined(__FreeBSD__) || defined(__linux__) || defined(__DragonFly__)
    return true;
  #else
    return false;
-@@ -418,7 +418,7 @@ uint8_t CUSBCECAdapterDetection::FindAda
+@@ -484,7 +484,7 @@ uint8_t CUSBCECAdapterDetection::FindAda
  {
    uint8_t iFound(0);
  
