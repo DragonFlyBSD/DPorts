c34f7169ec182d4f230fcfff98e92c849af62a31
DPorts Builder <nobody@home.ok>
2020-05-20 18:53:07 -0700

    Update security/openconnect to version 8.08

--- security/openconnect/dragonfly/patch-openssl-dtls.c
+++ security/openconnect/dragonfly/patch-openssl-dtls.c
@@ -1,4 +1,4 @@
---- openssl-dtls.c.orig	2019-06-12 09:17:15 UTC
+--- openssl-dtls.c.orig	2020-03-30 23:23:30 UTC
 +++ openssl-dtls.c
 @@ -32,6 +32,10 @@
  
@@ -11,12 +11,3 @@
  /* In the very early days there were cases where this wasn't found in
   * the header files but it did still work somehow. I forget the details
   * now but I was definitely avoiding using the macro. Let's just define
-@@ -315,7 +319,7 @@ static unsigned int psk_callback(SSL *ss
- 
- #endif
- 
--#if OPENSSL_VERSION_NUMBER < 0x10002000L
-+#if OPENSSL_VERSION_NUMBER < 0x10002000L || defined(LIBRESSL_VERSION_NUMBER)
- static const SSL_CIPHER *SSL_CIPHER_find(SSL *ssl, const unsigned char *ptr)
- {
-     return ssl->method->get_cipher_by_char(ptr);
