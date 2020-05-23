135f9fad0473660faab38c2db434a6fa60238779
DPorts Builder <nobody@home.ok>
2020-05-22 19:27:07 -0700

    Update databases/rocksdb to version 6.8.1

--- databases/rocksdb/dragonfly/patch-env_env__test.cc
+++ databases/rocksdb/dragonfly/patch-env_env__test.cc
@@ -1,6 +1,6 @@
---- env/env_test.cc.orig	2019-10-02 00:04:43 UTC
+--- env/env_test.cc.orig	2020-04-16 23:01:15 UTC
 +++ env/env_test.cc
-@@ -1354,7 +1354,9 @@ TEST_P(EnvPosixTestWithParam, Preallocat
+@@ -1376,7 +1376,9 @@ TEST_P(EnvPosixTestWithParam, Preallocat
    std::unique_ptr<WritableFile> srcfile;
    EnvOptions soptions;
    soptions.use_direct_reads = soptions.use_direct_writes = direct_io_;
@@ -9,16 +9,14 @@
 +    !defined(OS_AIX) && !defined(OS_OPENBSD) && !defined(OS_FREEBSD) && \
 +    !defined(OS_DRAGONFLYBSD)
      if (soptions.use_direct_writes) {
-       rocksdb::SyncPoint::GetInstance()->SetCallBack(
+       ROCKSDB_NAMESPACE::SyncPoint::GetInstance()->SetCallBack(
            "NewWritableFile:O_DIRECT", [&](void* arg) {
-@@ -1415,7 +1417,9 @@ TEST_P(EnvPosixTestWithParam, Consistent
-       const std::string path =
-           test::TmpDir(env_) + "/" + "testfile_" + std::to_string(i);
-       std::unique_ptr<WritableFile> file;
+@@ -1437,7 +1439,7 @@ TEST_P(EnvPosixTestWithParam, Consistent
+     const std::string path =
+         test::TmpDir(env_) + "/" + "testfile_" + std::to_string(i);
+     std::unique_ptr<WritableFile> file;
 -#if !defined(OS_MACOSX) && !defined(OS_WIN) && !defined(OS_SOLARIS) && !defined(OS_AIX) && !defined(OS_OPENBSD) && !defined(OS_FREEBSD)
-+#if !defined(OS_MACOSX) && !defined(OS_WIN) && !defined(OS_SOLARIS) && \
-+    !defined(OS_AIX) && !defined(OS_OPENBSD) && !defined(OS_FREEBSD) && \
-+    !defined(OS_DRAGONFLYBSD)
++#if !defined(OS_MACOSX) && !defined(OS_WIN) && !defined(OS_SOLARIS) && !defined(OS_AIX) && !defined(OS_OPENBSD) && !defined(OS_FREEBSD) && !defined(OS_DRAGONFLYBSD)
        if (soptions.use_direct_writes) {
-         rocksdb::SyncPoint::GetInstance()->SetCallBack(
+         ROCKSDB_NAMESPACE::SyncPoint::GetInstance()->SetCallBack(
              "NewWritableFile:O_DIRECT", [&](void* arg) {
