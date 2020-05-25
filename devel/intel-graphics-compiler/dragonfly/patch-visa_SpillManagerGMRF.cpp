92d2a34cfb9af1eeed2335bbcc30e6aea51ab83f
DPorts Builder <nobody@home.ok>
2020-05-24 17:42:33 -0700

    Update devel/intel-graphics-compiler to version 1.0.3899

---
+++ devel/intel-graphics-compiler/dragonfly/patch-visa_SpillManagerGMRF.cpp
@@ -0,0 +1,11 @@
+--- visa/SpillManagerGMRF.cpp.orig	2020-05-04 10:18:54 UTC
++++ visa/SpillManagerGMRF.cpp
+@@ -32,7 +32,7 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ #include "BuildIR.h"
+ #include "DebugInfo.h"
+ 
+-#include <math.h>
++#include <cmath>
+ #include <sstream>
+ #include <fstream>
+ 
