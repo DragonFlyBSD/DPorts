e336e7a6b765cf7b5266be6a6c27999601be4c52
DPorts Builder <nobody@home.ok>
2020-05-25 18:16:04 -0700

    Update astro/kstars to version 3.4.2,1

---
+++ astro/kstars/dragonfly/patch-kstars_fitsviewer_fitssepdetector.cpp
@@ -0,0 +1,10 @@
+--- kstars/fitsviewer/fitssepdetector.cpp.orig	2020-05-25 14:48:57 UTC
++++ kstars/fitsviewer/fitssepdetector.cpp
+@@ -18,6 +18,7 @@
+  ***************************************************************************/
+ 
+ #include <math.h>
++#include <cstddef>
+ 
+ #include "sep/sep.h"
+ #include "fits_debug.h"
