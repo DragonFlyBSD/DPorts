5a012f3a80abdb2f3a8ad438eca21b5f467c4184
DPorts Builder <nobody@home.ok>
2020-05-28 12:30:10 -0700

    Tweak games/veloren version 0.5.0_3

--- games/veloren/dragonfly/patch-cargo-crates_cpal-0.10.0_src_host_mod.rs
+++
@@ -1,8 +0,0 @@
---- cargo-crates/cpal-0.10.0/src/host/mod.rs.orig	2019-07-05 18:30:36 UTC
-+++ cargo-crates/cpal-0.10.0/src/host/mod.rs
-@@ -1,4 +1,4 @@
--#[cfg(any(target_os = "linux", target_os = "freebsd"))]
-+#[cfg(any(target_os = "linux", target_os = "dragonfly", target_os = "freebsd"))]
- pub(crate) mod alsa;
- #[cfg(all(windows, feature = "asio"))]
- pub(crate) mod asio;
