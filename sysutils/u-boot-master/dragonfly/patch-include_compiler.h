2f4338d5616bf02725c1ac315edbb41edd65fd2f
DPorts Builder <nobody@home.ok>
2020-05-28 16:22:10 -0700

    Update sysutils/u-boot-master to version 2020.04

--- sysutils/u-boot-master/dragonfly/patch-include_compiler.h
+++ sysutils/u-boot-master/dragonfly/patch-include_compiler.h
@@ -1,4 +1,4 @@
---- include/compiler.h.orig	2019-10-07 21:14:02 UTC
+--- include/compiler.h.orig	2020-04-13 15:02:18 UTC
 +++ include/compiler.h
 @@ -15,7 +15,7 @@
      defined(__sun__)	 || \
@@ -9,14 +9,13 @@
  # include <stdint.h>
  #endif
  
-@@ -44,11 +44,11 @@
+@@ -44,10 +44,10 @@
  #ifdef __linux__
  # include <endian.h>
  # include <byteswap.h>
 -#elif defined(__MACH__) || defined(__FreeBSD__)
 +#elif defined(__MACH__) || defined(__FreeBSD__) || defined(__DragonFly__)
  # include <machine/endian.h>
- typedef unsigned long ulong;
  #endif
 -#ifdef __FreeBSD__
 +#if defined(__FreeBSD__) || defined(__DragonFly__)
