3999311527f0395801d03724457a3f6ce88bc971
DPorts Builder <nobody@home.ok>
2020-06-16 15:18:02 -0700

    Prune 72 obsolete ports
    FreeBSD Ports maintainers have already deleted the following:
      databases/db48
      databases/rubygem-peek-mysql2-rails5
      databases/rubygem-peek-pg-rails5
      devel/cargo-tree
      devel/graphql.el
      devel/hs-data-default
      devel/hs-data-default-class
      devel/hs-data-default-instances-base
      devel/hs-data-default-instances-containers
      devel/hs-data-default-instances-dlist
      devel/hs-data-default-instances-old-locale
      devel/hs-dlist
      devel/hs-extensible-exceptions
      devel/hs-hashable
      devel/hs-mtl
      devel/hs-old-locale
      devel/hs-old-time
      devel/hs-random
      devel/hs-setlocale
      devel/hs-transformers-compat
      devel/hs-unordered-containers
      devel/hs-utf8-string
      devel/libphutil
      devel/py-django-rq-dashboard
      devel/py-django-tastypie-mongoengine
      devel/py-jupyterlab_server
      devel/rubygem-peek-gc-rails5
      devel/rubygem-peek-rails5
      devel/rubygem-peek-rblineprof-rails5
      devel/rubygem-peek-redis-rails5
      irc/psybnc
      lang/perl5.26
      mail/claws-mail-archiver
      mail/claws-mail-attach_warner
      mail/claws-mail-spamreport
      mail/claws-mail-tnef
      math/hs-semigroups
      math/py-onnx
      misc/onnx
      multimedia/din
      net/freerdp1
      net/get_iplayer
      net/hs-network
      print/frescobaldi
      security/pam_pseudo
      sysutils/ansible23
      sysutils/ansible24
      sysutils/ansible25
      sysutils/ansible26
      sysutils/beats
      sysutils/py-cpuinfo
      sysutils/rdiff-backup
      textproc/simplexml
      www/mediawiki132
      www/moodle36
      www/py-beautifulsoup460
      www/py-dj22-django-formset-js-improved
      www/py-dj22-django-jquery-js
      www/py-dj22-django_csp
      www/py-dj22-djangoql
      www/py-django-annotations
      www/py-django-happenings
      www/py-django-json-rpc
      www/py-django-registration-defaults
      www/py-django_openstack_auth
      www/py-djangotoolbox
      www/py-dojango
      www/py-requests-futures097
      x11-wm/hs-xmonad-contrib
      x11/hs-X11
      x11/hs-X11-xft
      x11/xcolorsel

--- sysutils/beats/dragonfly/patch-src_github.com_elastic_beats_vendor_github.com_docker_docker_client_client__unix.go
+++
@@ -1,8 +0,0 @@
---- ../src/github.com/elastic/beats/vendor/github.com/docker/docker/client/client_unix.go.orig	2018-12-23 01:44:01.707915000 +0100
-+++ ../src/github.com/elastic/beats/vendor/github.com/docker/docker/client/client_unix.go	2018-12-23 01:44:07.488020000 +0100
-@@ -1,4 +1,4 @@
--// +build linux freebsd solaris openbsd darwin netbsd
-+// +build linux dragonfly freebsd solaris openbsd darwin netbsd
- 
- package client
- 
