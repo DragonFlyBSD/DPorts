f94087e9ab5b6dc783528ec1c10a92d2e535bc9a
DPorts Builder <nobody@home.ok>
2020-06-28 17:01:34 -0700

    Update www/node12 to version 12.18.0

--- www/node12/dragonfly/patch-deps_uv_src_unix_udp.c
+++ www/node12/dragonfly/patch-deps_uv_src_unix_udp.c
@@ -1,20 +1,22 @@
---- deps/uv/src/unix/udp.c.orig	2019-12-03 15:10:34 UTC
+--- deps/uv/src/unix/udp.c.orig	2020-06-02 15:46:09 UTC
 +++ deps/uv/src/unix/udp.c
-@@ -653,7 +653,7 @@ static int uv__udp_set_membership6(uv_ud
+@@ -850,7 +850,8 @@ static int uv__udp_set_membership6(uv_ud
  }
  
  
--#if !defined(__OpenBSD__) && !defined(__NetBSD__)
-+#if !defined(__OpenBSD__) && !defined(__NetBSD__) && !defined(__DragonFly__)
+-#if !defined(__OpenBSD__) && !defined(__NetBSD__) && !defined(__ANDROID__)
++#if !defined(__OpenBSD__) && !defined(__NetBSD__) && !defined(__ANDROID__) && \
++  !defined(__DragonFly__)
  static int uv__udp_set_source_membership4(uv_udp_t* handle,
                                            const struct sockaddr_in* multicast_addr,
                                            const char* interface_addr,
-@@ -842,7 +842,7 @@ int uv_udp_set_source_membership(uv_udp_
+@@ -1045,7 +1046,8 @@ int uv_udp_set_source_membership(uv_udp_
                                   const char* interface_addr,
                                   const char* source_addr,
                                   uv_membership membership) {
--#if !defined(__OpenBSD__) && !defined(__NetBSD__)
-+#if !defined(__OpenBSD__) && !defined(__NetBSD__) && !defined(__DragonFly__)
+-#if !defined(__OpenBSD__) && !defined(__NetBSD__) && !defined(__ANDROID__)
++#if !defined(__OpenBSD__) && !defined(__NetBSD__) && !defined(__ANDROID__) && \
++  !defined(__DragonFly__)
    int err;
    struct sockaddr_storage mcast_addr;
    struct sockaddr_in* mcast_addr4;
