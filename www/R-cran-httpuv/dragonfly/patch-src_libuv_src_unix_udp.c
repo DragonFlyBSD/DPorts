aeb20b58c0ff661d9ea3ac27e1166456ca906154
DPorts Builder <nobody@home.ok>
2020-06-30 12:33:55 -0700

    Update www/R-cran-httpuv to version 1.5.4

---
+++ www/R-cran-httpuv/dragonfly/patch-src_libuv_src_unix_udp.c
@@ -0,0 +1,22 @@
+--- src/libuv/src/unix/udp.c.orig	2020-06-05 16:40:07 UTC
++++ src/libuv/src/unix/udp.c
+@@ -850,7 +850,8 @@ static int uv__udp_set_membership6(uv_ud
+ }
+ 
+ 
+-#if !defined(__OpenBSD__) && !defined(__NetBSD__) && !defined(__ANDROID__)
++#if !defined(__OpenBSD__) && !defined(__NetBSD__) && !defined(__ANDROID__) && \
++  !defined(__DragonFly__)
+ static int uv__udp_set_source_membership4(uv_udp_t* handle,
+                                           const struct sockaddr_in* multicast_addr,
+                                           const char* interface_addr,
+@@ -1045,7 +1046,8 @@ int uv_udp_set_source_membership(uv_udp_
+                                  const char* interface_addr,
+                                  const char* source_addr,
+                                  uv_membership membership) {
+-#if !defined(__OpenBSD__) && !defined(__NetBSD__) && !defined(__ANDROID__)
++#if !defined(__OpenBSD__) && !defined(__NetBSD__) && !defined(__ANDROID__) && \
++  !defined(__DragonFly__)
+   int err;
+   struct sockaddr_storage mcast_addr;
+   struct sockaddr_in* mcast_addr4;
