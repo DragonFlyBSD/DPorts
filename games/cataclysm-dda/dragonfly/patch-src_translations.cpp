a753122a868c3e43b18d39a4a6ebbafbd6c8b5b2
DPorts Builder <nobody@home.ok>
2020-06-30 17:58:24 -0700

    Update games/cataclysm-dda to version 0.e,1

--- games/cataclysm-dda/dragonfly/patch-src_translations.cpp
+++ games/cataclysm-dda/dragonfly/patch-src_translations.cpp
@@ -1,11 +1,12 @@
---- src/translations.cpp.intermediate	2019-05-13 15:02:23.000000000 +0000
+--- src/translations.cpp.orig	2020-07-01 00:05:03 UTC
 +++ src/translations.cpp
-@@ -176,7 +176,7 @@ void set_language()
- 
-     // Step 2. Bind to gettext domain.
-     std::string locale_dir;
--#if (defined __linux__ || defined __FreeBSD__ || (defined MACOSX && !defined TILES))
-+#if (defined __linux__ || defined __FreeBSD__ || defined(__DragonFly__) || (defined MACOSX && !defined TILES))
-     if( !FILENAMES["base_path"].empty() ) {
-         locale_dir = FILENAMES["base_path"] + "share/locale";
+@@ -212,7 +212,8 @@ void set_language()
+     auto env = getenv( "LANGUAGE" );
+     locale_dir = std::string( PATH_INFO::base_path() + "lang/mo/" + ( env ? env : "none" ) +
+                               "/LC_MESSAGES/cataclysm-dda.mo" );
+-#elif (defined(__linux__) || defined(__FreeBSD__) || (defined(MACOSX) && !defined(TILES)))
++#elif (defined(__linux__) || defined(__FreeBSD__) || defined(__DragonFly__) \
++       || (defined(MACOSX) && !defined(TILES)))
+     if( !PATH_INFO::base_path().empty() ) {
+         locale_dir = PATH_INFO::base_path() + "share/locale";
      } else {
