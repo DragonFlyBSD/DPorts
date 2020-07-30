a4165a63f878defbfae1ea31974306923a747545
DPorts Builder <nobody@home.ok>
2020-07-30 05:52:09 -0700

    Import devel/lazygit version 0.20.9

---
+++ devel/lazygit/dragonfly/patch-vendor_github.com_cloudfoundry_jibber_jabber_jibber_jabber_unix.go
@@ -0,0 +1,8 @@
+--- vendor/github.com/cloudfoundry/jibber_jabber/jibber_jabber_unix.go.orig	2020-07-28 12:32:23.666618000 +0200
++++ vendor/github.com/cloudfoundry/jibber_jabber/jibber_jabber_unix.go	2020-07-28 12:32:29.926629000 +0200
+@@ -1,4 +1,4 @@
+-// +build darwin freebsd linux netbsd openbsd
++// +build darwin freebsd linux netbsd openbsd dragonfly
+ 
+ package jibber_jabber
+ 
