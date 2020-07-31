436a6b36ddabfa9b7badb40554127bfad995b598
DPorts Builder <nobody@home.ok>
2020-07-30 20:35:56 -0700

    Update devel/meson to version 0.54.3

--- devel/meson/dragonfly/patch-mesonbuild_mesonlib.py
+++ devel/meson/dragonfly/patch-mesonbuild_mesonlib.py
@@ -1,15 +1,15 @@
---- mesonbuild/mesonlib.py.orig	2019-11-28 17:37:44 UTC
+--- mesonbuild/mesonlib.py.orig	2020-06-14 19:40:08 UTC
 +++ mesonbuild/mesonlib.py
-@@ -672,7 +672,7 @@ def default_libdir():
+@@ -729,7 +729,7 @@ def default_libdir() -> str:
                  return 'lib/' + archpath
          except Exception:
              pass
--    if is_freebsd():
-+    if is_freebsd() or is_dragonflybsd:
+-    if is_freebsd() or is_irix():
++    if is_freebsd() or is_irix() or is_dragonflybsd():
          return 'lib'
      if os.path.isdir('/usr/lib64') and not os.path.islink('/usr/lib64'):
          return 'lib64'
-@@ -697,7 +697,7 @@ def get_library_dirs() -> typing.List[st
+@@ -757,7 +757,7 @@ def get_library_dirs() -> T.List[str]:
      # problematic, please raise the issue on the mailing list.
      unixdirs = ['/usr/local/lib', '/usr/lib', '/lib']
  
