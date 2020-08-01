752dc0f3597db8610f9705077e38a745d461caa4
DPorts Builder <nobody@home.ok>
2020-08-01 09:30:07 -0700

    Update net/asterisk13 to version 13.35.0

--- net/asterisk13/files/patch-channels_chan__dahdi.c
+++ net/asterisk13/files/patch-channels_chan__dahdi.c
@@ -1,6 +1,6 @@
---- channels/chan_dahdi.c.orig	2017-12-20 19:05:45 UTC
+--- channels/chan_dahdi.c.orig	2020-07-09 15:33:47 UTC
 +++ channels/chan_dahdi.c
-@@ -4666,6 +4666,8 @@ void dahdi_ec_enable(struct dahdi_pvt *p)
+@@ -4716,6 +4716,8 @@ void dahdi_ec_enable(struct dahdi_pvt *p)
  		return;
  	}
  	if (p->echocancel.head.tap_length) {
@@ -9,7 +9,7 @@
  #if defined(HAVE_PRI) || defined(HAVE_SS7)
  		switch (p->sig) {
  #if defined(HAVE_PRI)
-@@ -4696,7 +4698,8 @@ void dahdi_ec_enable(struct dahdi_pvt *p)
+@@ -4746,7 +4748,8 @@ void dahdi_ec_enable(struct dahdi_pvt *p)
  			break;
  		}
  #endif	/* defined(HAVE_PRI) || defined(HAVE_SS7) */
@@ -19,7 +19,7 @@
  		if (res) {
  			ast_log(LOG_WARNING, "Unable to enable echo cancellation on channel %d (%s)\n", p->channel, strerror(errno));
  		} else {
-@@ -4730,8 +4733,9 @@ void dahdi_ec_disable(struct dahdi_pvt *p)
+@@ -4780,8 +4783,9 @@ void dahdi_ec_disable(struct dahdi_pvt *p)
  
  	if (p->echocanon) {
  		struct dahdi_echocanparams ecp = { .tap_length = 0 };
