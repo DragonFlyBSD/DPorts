c15da6c923a3cb383aeb1e1fc5077c632f098816
DPorts Builder <nobody@home.ok>
2020-08-09 04:45:05 -0700

    Update devel/py-grpcio to version 1.30.0

--- devel/py-grpcio/dragonfly/patch-setup.py
+++ devel/py-grpcio/dragonfly/patch-setup.py
@@ -1,11 +1,11 @@
---- setup.py.orig	2019-02-05 10:01:40 UTC
-+++ setup.py
-@@ -43,6 +43,8 @@ CARES_INCLUDE = (
-     os.path.join('third_party', 'cares', 'cares'),)
+--- setup.py.orig	2020-08-08 19:18:35.281426000 +0200
++++ setup.py	2020-08-08 19:18:52.191508000 +0200
+@@ -49,6 +49,8 @@
+ )
  if 'darwin' in sys.platform:
-   CARES_INCLUDE += (os.path.join('third_party', 'cares', 'config_darwin'),)
+     CARES_INCLUDE += (os.path.join('third_party', 'cares', 'config_darwin'),)
 +if 'dragonfly' in sys.platform:
-+  CARES_INCLUDE += (os.path.join('third_party', 'cares', 'config_freebsd'),)
++    CARES_INCLUDE += (os.path.join('third_party', 'cares', 'config_freebsd'),)
  if 'freebsd' in sys.platform:
-   CARES_INCLUDE += (os.path.join('third_party', 'cares', 'config_freebsd'),)
+     CARES_INCLUDE += (os.path.join('third_party', 'cares', 'config_freebsd'),)
  if 'linux' in sys.platform:
