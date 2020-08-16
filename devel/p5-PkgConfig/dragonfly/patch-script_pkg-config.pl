8d80457e2236aade1882eb43375a56a8996542da
DPorts Builder <nobody@home.ok>
2020-08-16 12:32:59 -0700

    Tweak devel/p5-PkgConfig version 0.24026

---
+++ devel/p5-PkgConfig/dragonfly/patch-script_pkg-config.pl
@@ -0,0 +1,11 @@
+--- script/pkg-config.pl.orig	2020-07-19 12:17:28 UTC
++++ script/pkg-config.pl
+@@ -222,7 +222,7 @@ if($ENV{PKG_CONFIG_NO_OS_CUSTOMIZATION})
+     }
+ 
+ 
+-} elsif($^O eq 'freebsd') {
++} elsif($^O eq 'freebsd' or $^O eq 'dragonfly') {
+ 
+     # TODO: FreeBSD 10-12's version of pkg-config does not
+     # support PKG_CONFIG_DEBUG_SPEW so I can't verify
