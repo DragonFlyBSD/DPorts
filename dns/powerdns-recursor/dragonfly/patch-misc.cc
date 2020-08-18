4a58cdffe63212b408dd280aac56031c8254a376
DPorts Builder <nobody@home.ok>
2020-08-17 23:45:49 -0700

    Update dns/powerdns-recursor to version 4.3.3

--- dns/powerdns-recursor/dragonfly/patch-misc.cc
+++ dns/powerdns-recursor/dragonfly/patch-misc.cc
@@ -1,9 +1,9 @@
---- misc.cc.orig	2019-07-15 08:32:59 UTC
+--- misc.cc.orig	2020-07-15 21:43:28 UTC
 +++ misc.cc
-@@ -57,7 +57,7 @@
- #include <sys/types.h>
+@@ -58,7 +58,7 @@
  #include <pwd.h>
  #include <grp.h>
+ #include <limits.h>
 -#ifdef __FreeBSD__
 +#if defined(__FreeBSD__) || defined(__DragonFly__)
  #  include <pthread_np.h>
