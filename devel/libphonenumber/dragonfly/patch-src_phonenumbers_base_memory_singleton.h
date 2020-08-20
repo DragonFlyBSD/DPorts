4660dc95dc0459fb934a47c565d5a1890541bea8
DPorts Builder <nobody@home.ok>
2020-08-20 04:15:49 -0700

    Update devel/libphonenumber to version 8.12.7

--- devel/libphonenumber/dragonfly/patch-src_phonenumbers_base_memory_singleton.h
+++ devel/libphonenumber/dragonfly/patch-src_phonenumbers_base_memory_singleton.h
@@ -1,11 +1,11 @@
---- src/phonenumbers/base/memory/singleton.h.orig	2020-01-26 19:58:44.000000000 +0000
+--- src/phonenumbers/base/memory/singleton.h.intermediate	2020-08-19 19:58:44.000000000 +0000
 +++ src/phonenumbers/base/memory/singleton.h
-@@ -56,7 +56,7 @@ template <class T> boost::once_flag Sing
- #include "phonenumbers/base/logging.h"
- #include "phonenumbers/base/thread_checker.h"
- 
--#if !defined(__linux__) && !defined(__APPLE__) && !defined(__FreeBSD__)
-+#if !defined(__linux__) && !defined(__APPLE__) && !defined(__FreeBSD__) && !defined(__DragonFly__)
- 
- namespace i18n {
- namespace phonenumbers {
+@@ -22,7 +22,7 @@
+ #elif (__cplusplus >= 201103L) && defined(I18N_PHONENUMBERS_USE_STDMUTEX)
+ // C++11 Lock implementation based on std::mutex.
+ #include "phonenumbers/base/memory/singleton_stdmutex.h"
+-#elif defined(__linux__) || defined(__APPLE__) || defined(I18N_PHONENUMBERS_HAVE_POSIX_THREAD) || defined(__FreeBSD__)
++#elif defined(__linux__) || defined(__APPLE__) || defined(I18N_PHONENUMBERS_HAVE_POSIX_THREAD) || defined(__FreeBSD__) || defined(__DragonFly__)
+ #include "phonenumbers/base/memory/singleton_posix.h"
+ #elif defined(WIN32)
+ #include "phonenumbers/base/memory/singleton_win32.h"
--- devel/libphonenumber/dragonfly/patch-src_phonenumbers_base_synchronization_lock.h
+++ devel/libphonenumber/dragonfly/patch-src_phonenumbers_base_synchronization_lock.h
@@ -1,11 +1,11 @@
---- src/phonenumbers/base/synchronization/lock.h.intermediate	2020-01-26 19:58:44.000000000 +0000
+--- src/phonenumbers/base/synchronization/lock.h.intermediate	2020-09-19 19:58:44.000000000 +0000
 +++ src/phonenumbers/base/synchronization/lock.h
-@@ -63,7 +63,7 @@ private:
- // Dummy lock implementation on non-POSIX platforms. If you are running on a
- // different platform and care about thread-safety, please compile with
- // -DI18N_PHONENUMBERS_USE_BOOST.
--#elif !defined(__linux__) && !defined(__APPLE__) && !defined(__FreeBSD__)
-+#elif !defined(__linux__) && !defined(__APPLE__) && !defined(__FreeBSD__) && !defined(__DragonFly__)
- 
- namespace i18n {
- namespace phonenumbers {
+@@ -22,7 +22,7 @@
+ #elif (__cplusplus >= 201103L) && defined(I18N_PHONENUMBERS_USE_STDMUTEX)
+ // C++11 Lock implementation based on std::mutex.
+ #include "phonenumbers/base/synchronization/lock_stdmutex.h"
+-#elif defined(__linux__) || defined(__APPLE__) || defined(I18N_PHONENUMBERS_HAVE_POSIX_THREAD) || defined(__FreeBSD__)
++#elif defined(__linux__) || defined(__APPLE__) || defined(I18N_PHONENUMBERS_HAVE_POSIX_THREAD) || defined(__FreeBSD__) || defined(__DragonFly__)
+ #include "phonenumbers/base/synchronization/lock_posix.h"
+ #elif defined(WIN32)
+ #include "phonenumbers/base/synchronization/lock_win32.h"
