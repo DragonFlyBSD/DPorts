bb10084bce4969540a89a87206b558027ddedb72
DPorts Builder <nobody@home.ok>
2020-08-20 03:46:38 -0700

    Update audio/supercollider to version 3.11.0

--- audio/supercollider/dragonfly/patch-lang_LangSource_PyrMathSupport.cpp
+++ audio/supercollider/dragonfly/patch-lang_LangSource_PyrMathSupport.cpp
@@ -1,4 +1,4 @@
---- lang/LangSource/PyrMathSupport.cpp.orig	2018-03-28 20:05:07 UTC
+--- lang/LangSource/PyrMathSupport.cpp.orig	2020-04-01 20:16:41 UTC
 +++ lang/LangSource/PyrMathSupport.cpp
 @@ -26,7 +26,7 @@
  #include <stdlib.h>
@@ -6,6 +6,6 @@
  
 -#if defined(__FreeBSD__) || defined(__NetBSD__)
 +#if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__DragonFly__)
- # include <complex.h>
+ #    include <complex.h>
  #endif
  
