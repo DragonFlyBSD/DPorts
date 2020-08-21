67724a55322c82989403d3c013437f45bd7e6e99
DPorts Builder <nobody@home.ok>
2020-08-21 11:59:36 -0700

    Update devel/stlink to version 1.6.1

---
+++ devel/stlink/dragonfly/patch-src_libusb__settings.h
@@ -0,0 +1,17 @@
+--- src/libusb_settings.h.orig	2020-05-31 22:00:20 UTC
++++ src/libusb_settings.h
+@@ -39,8 +39,14 @@
+     #define MINIMAL_API_VERSION 0x01000104 // v1.0.20
+ #endif
+ 
++#if !defined(LIBUSB_API_VERSION) && defined(__DragonFly__)
++#define LIBUSB_API_VERSION 0x01000102
++#endif
++
++#ifndef __DragonFly__
+ #if ( LIBUSB_API_VERSION < MINIMAL_API_VERSION )
+     #error unsupported libusb version
+ #endif
++#endif
+ 
+ #endif // STLINKUSB_H
