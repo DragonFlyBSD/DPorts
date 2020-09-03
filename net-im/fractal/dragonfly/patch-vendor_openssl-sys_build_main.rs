68564aa419d98442277d8978498768efc9055983
DPorts Builder <nobody@home.ok>
2020-09-03 11:50:45 -0700

    Tweak net-im/fractal version 4.2.2_6

--- net-im/fractal/dragonfly/patch-vendor_openssl-sys_build_main.rs
+++ net-im/fractal/dragonfly/patch-vendor_openssl-sys_build_main.rs
@@ -1,6 +1,6 @@
 --- vendor/openssl-sys/build/main.rs.orig	2019-11-30 10:09:17 UTC
 +++ vendor/openssl-sys/build/main.rs
-@@ -183,28 +183,32 @@ See rust-openssl README for more informa
+@@ -183,28 +183,33 @@ See rust-openssl README for more informa
      if let Some(libressl_version) = libressl_version {
          println!("cargo:libressl_version_number={:x}", libressl_version);
  
@@ -22,6 +22,8 @@
 -            (8, _) => ('8', 'x'),
 -            (9, 0) => ('9', '0'),
 -            (9, _) => ('9', 'x'),
+-            _ => version_error(),
+-        };
 +        let (major, minor, fix) = match (major, minor, fix) {
 +            (2, 5, 0) => ('2', '5', '0'),
 +            (2, 5, 1) => ('2', '5', '1'),
@@ -40,21 +42,22 @@
 +            (3, 0, 0) => ('3', '0', '0'),
 +            (3, 0, 1) => ('3', '0', '1'),
 +            (3, 0, _) => ('3', '0', 'x'),
-             _ => version_error(),
-         };
++            (3, 1, _) => ('3', '1', 'x'),
++             _ => version_error(),
++         };
  
          println!("cargo:libressl=true");
 -        println!("cargo:libressl_version=2{}{}", minor, fix);
-+        println!("cargo:libressl_version={}{}{}", major, minor, fix);
++        println!("cargo:libressl_version=i{}{}{}", major, minor, fix);
          println!("cargo:version=101");
          Version::Libressl
      } else {
-@@ -240,7 +244,7 @@ fn version_error() -> ! {
+@@ -240,7 +245,7 @@ fn version_error() -> ! {
          "
  
  This crate is only compatible with OpenSSL 1.0.1 through 1.1.1, or LibreSSL 2.5
 -through 2.9.x, but a different version of OpenSSL was found. The build is now aborting
-+through 3.0.x, but a different version of OpenSSL was found. The build is now aborting
++through 3.1.x, but a different version of OpenSSL was found. The build is now aborting
  due to this version mismatch.
  
  "
