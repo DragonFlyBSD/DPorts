5439fbf9629abac2a3a69686111ddb6adcbd0c7f
DPorts Builder <nobody@home.ok>
2020-09-22 23:48:23 -0700

    Update graphics/alembic to version 1.7.14

--- graphics/alembic/dragonfly/patch-lib_Alembic_Util_Murmur3.cpp
+++ graphics/alembic/dragonfly/patch-lib_Alembic_Util_Murmur3.cpp
@@ -1,11 +1,11 @@
---- lib/Alembic/Util/Murmur3.cpp.intermediate	2016-07-09 17:42:17 UTC
-+++ lib/Alembic/Util/Murmur3.cpp
-@@ -41,7 +41,7 @@
+--- lib/Alembic/Util/Murmur3.cpp.orig	2020-09-22 14:21:24.255889000 +0200
++++ lib/Alembic/Util/Murmur3.cpp	2020-09-22 14:21:38.056208000 +0200
+@@ -39,7 +39,7 @@
+ #include <Alembic/Util/Murmur3.h>
+ #include <Alembic/Util/PlainOldDataType.h>
  
- #ifdef __APPLE__
+-#if defined(__APPLE__) || defined(__FreeBSD__)
++#if defined(__APPLE__) || defined(__FreeBSD__) || defined(__DragonFly__)
  #include <machine/endian.h>
--#elif defined(__FreeBSD__)
-+#elif defined(__FreeBSD__) || defined(__DragonFly__)
- #include <sys/endian.h>
  #elif !defined(_MSC_VER)
  #include <endian.h>
