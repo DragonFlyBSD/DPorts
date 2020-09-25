39a5a9f58ec7a41197412103fcc701b992e703df
DPorts Builder <nobody@home.ok>
2020-09-25 16:00:58 -0700

    Import sysutils/edk2 version g202005

---
+++ sysutils/edk2/dragonfly/patch-BaseTools_Source_C_GenFv_GenFvInternalLib.c
@@ -0,0 +1,11 @@
+--- BaseTools/Source/C/GenFv/GenFvInternalLib.c.orig	2020-06-02 07:05:21 UTC
++++ BaseTools/Source/C/GenFv/GenFvInternalLib.c
+@@ -13,7 +13,7 @@ SPDX-License-Identifier: BSD-2-Clause-Pa
+ // Include files
+ //
+ 
+-#if defined(__FreeBSD__)
++#if defined(__FreeBSD__) || defined(__DragonFly__)
+ #include <uuid.h>
+ #elif defined(__GNUC__)
+ #include <uuid/uuid.h>
