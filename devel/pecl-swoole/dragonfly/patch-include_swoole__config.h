0111de4127704addaed39b907e9caabb49cb6aef
DPorts Builder <nobody@home.ok>
2020-09-25 14:15:44 -0700

    Tweak devel/pecl-swoole version 4.5.3

--- devel/pecl-swoole/dragonfly/patch-include_swoole__config.h
+++ devel/pecl-swoole/dragonfly/patch-include_swoole__config.h
@@ -1,11 +1,11 @@
---- include/swoole_config.h.orig	2019-09-25 07:04:17 UTC
+--- include/swoole_config.h.orig	2020-08-30 04:51:37 UTC
 +++ include/swoole_config.h
-@@ -41,7 +41,7 @@
+@@ -43,7 +43,7 @@
  
- #define SW_SOCKET_OVERFLOW_WAIT    100
- #define SW_SOCKET_MAX_DEFAULT      65536
+ #define SW_SOCKET_OVERFLOW_WAIT 100
+ #define SW_SOCKET_MAX_DEFAULT 65536
 -#if defined(__MACH__) || defined(__FreeBSD__)
 +#if defined(__MACH__) || defined(__FreeBSD__) || defined(__DragonFly__)
- #define SW_SOCKET_BUFFER_SIZE      262144
+ #define SW_SOCKET_BUFFER_SIZE 262144
  #else
- #define SW_SOCKET_BUFFER_SIZE      8388608
+ #define SW_SOCKET_BUFFER_SIZE 8388608
