e59cf658df240b3244c7c4ffae76b32be15593f3
DPorts Builder <nobody@home.ok>
2020-09-25 10:28:31 -0700

    Update devel/libzim to version 6.2.2

--- devel/libzim/dragonfly/patch-src_fs__unix.cpp
+++ devel/libzim/dragonfly/patch-src_fs__unix.cpp
@@ -1,11 +1,12 @@
---- src/fs_unix.cpp.intermediate	2019-05-02 21:07:22 UTC
+--- src/fs_unix.cpp.orig	2020-09-02 16:22:41 UTC
 +++ src/fs_unix.cpp
-@@ -34,7 +34,7 @@ namespace unix {
+@@ -34,7 +34,8 @@ namespace unix {
  
  zsize_t FD::readAt(char* dest, zsize_t size, offset_t offset) const
  {
--#if defined(__APPLE__) || defined(__FreeBSD__)
-+#if defined(__APPLE__) || defined(__FreeBSD__) || defined(__DragonFly__)
+-#if defined(__APPLE__) || defined(__OpenBSD__) || defined(__FreeBSD__)
++#if defined(__APPLE__) || defined(__OpenBSD__) || defined(__FreeBSD__) || \
++  defined(__DragonFly__)
  # define PREAD pread
  #else
  # define PREAD pread64
