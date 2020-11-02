62a339f8f21e6c897820fe7bb644fb0fdab65885
DPorts Builder <nobody@home.ok>
2020-11-02 02:30:01 -0800

    Update devel/py-evdev to version 1.3.0

--- devel/py-evdev/dragonfly/patch-evdev_genecodes.py
+++ devel/py-evdev/dragonfly/patch-evdev_genecodes.py
@@ -1,11 +1,11 @@
---- evdev/genecodes.py.intermediate	2019-06-12 15:05:09.000000000 +0000
+--- evdev/genecodes.py.orig	2020-11-02 00:44:50 UTC
 +++ evdev/genecodes.py
 @@ -20,7 +20,7 @@ if sys.argv[1:]:
  
  
  #-----------------------------------------------------------------------------
--macro_regex = r'#define +((?:KEY|ABS|REL|SW|MSC|LED|BTN|REP|SND|ID|EV|BUS|SYN|FF)_\w+)'
-+macro_regex = r'#define(?: |\t)+((?:KEY|ABS|REL|SW|MSC|LED|BTN|REP|SND|ID|EV|BUS|SYN|FF)_\w+)'
+-macro_regex = r'#define +((?:KEY|ABS|REL|SW|MSC|LED|BTN|REP|SND|ID|EV|BUS|SYN|FF|UI_FF|INPUT_PROP)_\w+)'
++macro_regex = r'#define(?: |\t)+((?:KEY|ABS|REL|SW|MSC|LED|BTN|REP|SND|ID|EV|BUS|SYN|FF|UI_FF|INPUT_PROP)_\w+)'
  macro_regex = re.compile(macro_regex)
  
  uname = list(os.uname()); del uname[1]
