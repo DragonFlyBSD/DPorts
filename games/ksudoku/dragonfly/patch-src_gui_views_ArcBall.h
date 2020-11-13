e9944988f16b32516c29e0fc25e525c3e5c7830c
DPorts Builder <nobody@home.ok>
2020-11-13 14:36:21 -0800

    Update games/ksudoku to version 20.08.2

---
+++ games/ksudoku/dragonfly/patch-src_gui_views_ArcBall.h
@@ -0,0 +1,10 @@
+--- src/gui/views/ArcBall.h.orig	2020-10-03 12:36:35 UTC
++++ src/gui/views/ArcBall.h
+@@ -21,6 +21,7 @@
+ #define _ArcBall_h
+ 
+ #include "math.h"                                               // Needed for sqrtf
++#include "stddef.h"	// for NULL
+ 
+ // 8<--Snip here if you have your own math types/funcs-->8 
+ 
