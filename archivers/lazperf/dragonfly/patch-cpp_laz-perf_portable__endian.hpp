df2105d46086a8be8eb649ca2ffbd6f836d68865
DPorts Builder <nobody@home.ok>
2020-11-15 07:31:18 -0800

    Update archivers/lazperf to version 1.5.0

--- archivers/lazperf/dragonfly/patch-cpp_laz-perf_portable__endian.hpp
+++ archivers/lazperf/dragonfly/patch-cpp_laz-perf_portable__endian.hpp
@@ -1,11 +1,11 @@
---- laz-perf/portable_endian.hpp.orig	2020-04-16 18:53:19 UTC
-+++ laz-perf/portable_endian.hpp
+--- cpp/laz-perf/portable_endian.hpp.orig	2020-10-02 13:47:27 UTC
++++ cpp/laz-perf/portable_endian.hpp
 @@ -43,11 +43,11 @@
  #   define __PDP_ENDIAN    PDP_ENDIAN
  **/
  
 -#elif defined(__OpenBSD__)|| defined(__FreeBSD__) 
-+#elif defined(__OpenBSD__) || defined(__FreeBSD__) || defined(__DragonFly__)
++#elif defined(__OpenBSD__)|| defined(__FreeBSD__) || defined(__DragonFly__)
  
  #   include <sys/endian.h>
  
