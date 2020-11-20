bc7db3e871cee414aceaec7a02894441a1d5b568
DPorts Builder <nobody@home.ok>
2020-11-20 00:37:57 -0800

    Tweak textproc/xlreader version 0.9.0

---
+++ textproc/xlreader/dragonfly/patch-format.c
@@ -0,0 +1,10 @@
+--- format.c.orig	2001-10-01 04:33:46 UTC
++++ format.c
+@@ -46,6 +46,7 @@
+ 
+ #include <stdio.h>
+ #include <stdlib.h>
++#include <string.h>
+ #include <strings.h>
+ #include "biff.h"
+ #include "book.h"
