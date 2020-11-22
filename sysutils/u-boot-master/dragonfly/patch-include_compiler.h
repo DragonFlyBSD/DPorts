621a4665a7a41418e744c392e9684ec7de2313b0
DPorts Builder <nobody@home.ok>
2020-11-22 02:51:54 -0800

    Tweak sysutils/u-boot-master version 2020.07

--- sysutils/u-boot-master/dragonfly/patch-include_compiler.h
+++ sysutils/u-boot-master/dragonfly/patch-include_compiler.h
@@ -1,4 +1,4 @@
---- include/compiler.h.orig	2020-04-13 15:02:18 UTC
+--- include/compiler.h.orig	2020-07-06 19:22:53 UTC
 +++ include/compiler.h
 @@ -15,7 +15,7 @@
      defined(__sun__)	 || \
@@ -9,7 +9,7 @@
  # include <stdint.h>
  #endif
  
-@@ -44,10 +44,10 @@
+@@ -44,14 +44,16 @@
  #ifdef __linux__
  # include <endian.h>
  # include <byteswap.h>
@@ -20,5 +20,11 @@
 -#ifdef __FreeBSD__
 +#if defined(__FreeBSD__) || defined(__DragonFly__)
  # include <sys/endian.h> /* htole32 and friends */
++#ifndef __BYTE_ORDER
  # define __BYTE_ORDER BYTE_ORDER
  # define __LITTLE_ENDIAN LITTLE_ENDIAN
+ # define __BIG_ENDIAN BIG_ENDIAN
++#endif
+ #elif defined(__OpenBSD__)
+ # include <endian.h>
+ # define __BYTE_ORDER BYTE_ORDER
