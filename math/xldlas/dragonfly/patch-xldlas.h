71be34a5107a5339da475bf2dfa6c0c68be84aba
DPorts Builder <nobody@home.ok>
2020-11-27 09:08:27 -0800

    Update math/xldlas to version 0.85_9

---
+++ math/xldlas/dragonfly/patch-xldlas.h
@@ -0,0 +1,11 @@
+--- xldlas.h.intermediate	2020-11-27 14:07:15.000000000 +0000
++++ xldlas.h
+@@ -413,8 +413,6 @@ EXTERN FL_OBJECT	*graph_yrange_auto_butt
+ EXTERN FL_OBJECT	*graph_yrange_from_counter;
+ EXTERN FL_OBJECT	*graph_yrange_to_counter;
+ 
+-EXTERN 
+-
+ EXTERN FL_FORM		*att_window;
+ EXTERN FL_OBJECT	*att_point_counter;
+ EXTERN FL_OBJECT	*att_line_counter;
