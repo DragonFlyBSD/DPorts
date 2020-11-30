d2c987a96b14ff550469fbdd65b9d9ba6e7f1c4a
DPorts Builder <nobody@home.ok>
2020-11-29 16:50:00 -0800

    Tweak multimedia/gmerlin version 1.2.0_17

---
+++ multimedia/gmerlin/dragonfly/patch-lib_visualize.c
@@ -0,0 +1,10 @@
+--- lib/visualize.c.orig	2012-06-08 11:50:32 UTC
++++ lib/visualize.c
+@@ -36,6 +36,7 @@
+ 
+ #include <pthread.h>
+ #include <sys/signal.h>
++#include <signal.h>
+ 
+ #include <gmerlin/log.h>
+ 
