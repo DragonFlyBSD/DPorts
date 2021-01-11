e704afed009f0cb15fa9988ee1a44040cba9cc2c
DPorts Builder <nobody@home.ok>
2021-01-11 14:10:18 -0800

    Update net/traefik to version 2.3.2

--- net/traefik/dragonfly/patch-vendor_github.com_docker_docker_client_client_unix.go
+++ net/traefik/dragonfly/patch-vendor_github.com_docker_docker_client_client_unix.go
@@ -1,8 +1,8 @@
---- vendor/github.com/docker/docker/client/client_unix.go.orig	2020-07-19 14:22:42.028445000 +0200
-+++ vendor/github.com/docker/docker/client/client_unix.go	2020-07-19 14:22:48.268845000 +0200
+--- vendor/github.com/docker/docker/client/client_unix.go.orig	2021-01-09 22:17:26.550217000 +0100
++++ vendor/github.com/docker/docker/client/client_unix.go	2021-01-09 22:17:33.240260000 +0100
 @@ -1,4 +1,4 @@
--// +build linux freebsd solaris openbsd darwin
-+// +build linux freebsd solaris openbsd darwin dragonfly
+-// +build linux freebsd openbsd darwin
++// +build linux freebsd openbsd darwin dragonfly
  
- package client
+ package client // import "github.com/docker/docker/client"
  
