6ca0d5003afeb139bedcbfae3f6c67ef7e1754ea
DPorts Builder <nobody@home.ok>
2021-01-17 17:57:43 -0800

    Update deskutils/calendar to version 0.9

--- deskutils/calendar/files/patch-io.c
+++ deskutils/calendar/files/patch-io.c
@@ -1,20 +1,22 @@
---- io.c.orig	2020-10-18 03:01:26 UTC
+--- io.c.orig	2020-11-07 11:15:40 UTC
 +++ io.c
-@@ -42,7 +42,7 @@ static char sccsid[] = "@(#)calendar.c  8.3 (Berkeley)
- #endif
+@@ -119,8 +119,6 @@ cal_fopen(const char *file)
+ 	FILE *fp;
+ 	char *home = getenv("HOME");
+ 	unsigned int i;
+-	struct stat sb;
+-	static bool warned = false;
  
- #include <sys/cdefs.h>
--__FBSDID("$FreeBSD$");
-+__FBSDID("$FreeBSD: head/usr.bin/calendar/io.c 366962 2020-10-23 09:22:23Z se $");
+ 	if (home == NULL || *home == '\0') {
+ 		warnx("Cannot get home directory");
+@@ -145,10 +143,6 @@ cal_fopen(const char *file)
+ 	}
  
- #include <sys/param.h>
- #include <sys/stat.h>
-@@ -71,7 +71,7 @@ enum {
- };
+ 	warnx("can't open calendar file \"%s\"", file);
+-	if (!warned && stat(_PATH_INCLUDE_LOCAL, &sb) != 0) {
+-		warnx("calendar data files now provided by calendar-data pkg.");
+-		warned = true;
+-	}
  
- const char *calendarFile = "calendar";	/* default calendar file */
--static const char *calendarHomes[] = {".calendar", _PATH_INCLUDE}; /* HOME */
-+static const char *calendarHomes[] = {".calendar", _PATH_INCLUDE_LOCAL, _PATH_INCLUDE}; /* HOME */
- static const char *calendarNoMail = "nomail";/* don't sent mail if file exist */
- 
- static char path[MAXPATHLEN];
+ 	return (NULL);
+ }
