d5142c19588bd5dc4164ded2d85d27f352a01f84
DPorts Builder <nobody@home.ok>
2021-01-18 10:01:37 -0800

    Update devel/cmake to version 3.19.2

--- devel/cmake/dragonfly/patch-Source_cmSystemTools.cxx
+++ devel/cmake/dragonfly/patch-Source_cmSystemTools.cxx
@@ -1,12 +1,11 @@
---- Source/cmSystemTools.cxx.orig	2020-08-20 12:20:32 UTC
+--- Source/cmSystemTools.cxx.orig	2020-12-16 12:35:29 UTC
 +++ Source/cmSystemTools.cxx
-@@ -5,7 +5,8 @@
- // POSIX APIs are needed
+@@ -6,7 +6,7 @@
  #  define _POSIX_C_SOURCE 200809L
  #endif
--#if defined(__OpenBSD__) || defined(__FreeBSD__) || defined(__NetBSD__)
-+#if defined(__OpenBSD__) || defined(__FreeBSD__) || defined(__NetBSD__) || \
-+  defined(__DragonFly__)
+ #if defined(__OpenBSD__) || defined(__FreeBSD__) || defined(__NetBSD__) ||    \
+-  defined(__QNX__)
++  defined(__QNX__) || defined(__DragonFly__)
  // For isascii
  #  define _XOPEN_SOURCE 700
  #endif
