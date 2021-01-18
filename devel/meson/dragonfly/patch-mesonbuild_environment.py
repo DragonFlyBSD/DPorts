920a2c78f156d87ef3b1f84c020be3bc4e073593
DPorts Builder <nobody@home.ok>
2021-01-18 07:51:40 -0800

    Update devel/meson to version 0.56.2

--- devel/meson/dragonfly/patch-mesonbuild_environment.py
+++ devel/meson/dragonfly/patch-mesonbuild_environment.py
@@ -1,20 +1,20 @@
---- mesonbuild/environment.py.orig	2020-09-10 16:39:24 UTC
+--- mesonbuild/environment.py.orig	2021-01-06 10:39:48 UTC
 +++ mesonbuild/environment.py
-@@ -339,7 +339,7 @@ def detect_cpu_family(compilers: Compile
+@@ -357,7 +357,7 @@ def detect_cpu_family(compilers: Compile
      """
      if mesonlib.is_windows():
          trial = detect_windows_arch(compilers)
--    elif mesonlib.is_freebsd() or mesonlib.is_netbsd() or mesonlib.is_openbsd() or mesonlib.is_qnx():
-+    elif mesonlib.is_freebsd() or mesonlib.is_netbsd() or mesonlib.is_openbsd() or mesonlib.is_qnx() or mesonlib.is_dragonflybsd():
+-    elif mesonlib.is_freebsd() or mesonlib.is_netbsd() or mesonlib.is_openbsd() or mesonlib.is_qnx() or mesonlib.is_aix():
++    elif mesonlib.is_freebsd() or mesonlib.is_netbsd() or mesonlib.is_openbsd() or mesonlib.is_qnx() or mesonlib.is_aix() or mesonlib.is_dragonflybsd():
          trial = platform.processor().lower()
      else:
          trial = platform.machine().lower()
-@@ -390,7 +390,7 @@ def detect_cpu_family(compilers: Compile
+@@ -412,7 +412,7 @@ def detect_cpu_family(compilers: Compile
  def detect_cpu(compilers: CompilersDict):
      if mesonlib.is_windows():
          trial = detect_windows_arch(compilers)
--    elif mesonlib.is_freebsd() or mesonlib.is_netbsd() or mesonlib.is_openbsd():
-+    elif mesonlib.is_freebsd() or mesonlib.is_netbsd() or mesonlib.is_openbsd() or mesonlib.is_dragonflybsd():
+-    elif mesonlib.is_freebsd() or mesonlib.is_netbsd() or mesonlib.is_openbsd() or mesonlib.is_aix():
++    elif mesonlib.is_freebsd() or mesonlib.is_netbsd() or mesonlib.is_openbsd() or mesonlib.is_aix() or mesonlib.is_dragonflybsd():
          trial = platform.processor().lower()
      else:
          trial = platform.machine().lower()
