6d7e8e11e0d59afb8e225ecba523f138268e6cc4
DPorts Builder <nobody@home.ok>
2021-01-26 04:01:40 -0800

    Update devel/py-game to version 2.0.1

--- devel/py-game/dragonfly/patch-buildconfig_config__unix.py
+++ devel/py-game/dragonfly/patch-buildconfig_config__unix.py
@@ -1,11 +1,11 @@
---- buildconfig/config_unix.py.orig	2019-03-29 08:24:30 UTC
-+++ buildconfig/config_unix.py
-@@ -221,7 +221,7 @@ def main(sdl2=False):
+--- buildconfig/config_unix.py.orig	2021-01-25 23:19:21.471385000 +0100
++++ buildconfig/config_unix.py	2021-01-25 23:21:17.911681000 +0100
+@@ -211,7 +211,7 @@
          Dependency('SCRAP', '', 'libX11', ['X11']),
          #Dependency('GFX', 'SDL_gfxPrimitives.h', 'libSDL_gfx.so', ['SDL_gfx']),
      ])
--    is_freebsd = platform.system() == 'FreeBSD'
+-    is_freebsd = 'FreeBSD' in platform.system()
 +    is_freebsd = (platform.system() == 'FreeBSD' or platform.system() == 'DragonFly')
-     if not is_freebsd:
+     is_hurd = platform.system() == 'GNU'
+     if not is_freebsd and not is_hurd:
          porttime_dep = get_porttime_dep()
-         DEPS.append(
