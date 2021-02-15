2825b2c35babe871b779a22a7ea5f16f78440cc3
DPorts Builder <nobody@home.ok>
2021-02-15 10:53:20 -0800

    Update net/libusrsctp to version 0.9.4.0

--- net/libusrsctp/dragonfly/patch-usrsctplib_netinet_sctp__os__userspace.h
+++ net/libusrsctp/dragonfly/patch-usrsctplib_netinet_sctp__os__userspace.h
@@ -1,15 +1,10 @@
---- usrsctplib/netinet/sctp_os_userspace.h.orig	2020-04-06 14:04:26 UTC
+--- usrsctplib/netinet/sctp_os_userspace.h.orig	2021-01-10 09:44:05 UTC
 +++ usrsctplib/netinet/sctp_os_userspace.h
-@@ -1088,10 +1088,10 @@ sctp_get_mbuf_for_msg(unsigned int space
- /* with the current included files, this is defined in Linux but
-  *  in FreeBSD, it is behind a _KERNEL in sys/socket.h ...
-  */
--#if defined(__Userspace_os_DragonFly) || defined(__Userspace_os_FreeBSD) || defined(__Userspace_os_OpenBSD) || defined(__Userspace_os_NaCl)
-+#if defined(__Userspace_os_FreeBSD) || defined(__Userspace_os_OpenBSD) || defined(__Userspace_os_NaCl)
- /* stolen from /usr/include/sys/socket.h */
- #define CMSG_ALIGN(n)   _ALIGN(n)
--#elif defined(__Userspace_os_NetBSD)
-+#elif defined(__Userspace_os_NetBSD) || defined(__Userspace_os_DragonFly)
- #define CMSG_ALIGN(n)   (((n) + __ALIGNBYTES) & ~__ALIGNBYTES)
- #elif defined(__Userspace_os_Darwin)
- #if !defined(__DARWIN_ALIGNBYTES)
+@@ -409,6 +409,7 @@ struct sx {int dummy;};
+ #include <stdio.h>
+ #include <string.h>
+ #include <stdbool.h>
++#include <sys/param.h>
+ /* #include <sys/param.h>  in FreeBSD defines MSIZE */
+ /* #include <sys/ktr.h> */
+ /* #include <sys/systm.h> */
