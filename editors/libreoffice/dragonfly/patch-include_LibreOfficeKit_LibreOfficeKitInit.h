2b3decd05bb46ca9c5e33166d8bbc251acad6154
DPorts Builder <nobody@home.ok>
2021-03-01 22:38:33 -0800

    Update editors/libreoffice to version 7.1.0.3_2

--- editors/libreoffice/dragonfly/patch-include_LibreOfficeKit_LibreOfficeKitInit.h
+++ editors/libreoffice/dragonfly/patch-include_LibreOfficeKit_LibreOfficeKitInit.h
@@ -2,7 +2,7 @@
 +++ include/LibreOfficeKit/LibreOfficeKitInit.h
 @@ -19,6 +19,7 @@ extern "C"
  
- #if defined(__linux__) || defined (__FreeBSD_kernel__) || defined(_AIX) ||\
+ #if defined(__linux__) || defined (__FreeBSD__) || defined(_AIX) ||\
      defined(_WIN32) || defined(__APPLE__) || defined (__NetBSD__) ||\
 +    defined __DragonFly__ ||\
      defined (__sun) || defined(__OpenBSD__) || defined (__FreeBSD__)
---
+++ editors/libreoffice/dragonfly/patch-sw_source_core_bastyp_calc.cxx
@@ -0,0 +1,10 @@
+--- sw/source/core/bastyp/calc.cxx.orig	2021-01-27 19:33:49 UTC
++++ sw/source/core/bastyp/calc.cxx
+@@ -27,6 +27,7 @@
+ #include <comphelper/processfactory.hxx>
+ #include <comphelper/string.hxx>
+ #include <cstdlib>
++#include <cmath>
+ #include <dbmgr.hxx>
+ #include <docfld.hxx>
+ #include <docstat.hxx>
