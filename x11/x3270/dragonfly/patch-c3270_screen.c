f9edf45504166cc1101f68ac8e88fadffe53e2bb
DPorts Builder <nobody@home.ok>
2021-03-03 12:26:19 -0800

    Update x11/x3270 to version 4.0_3

--- x11/x3270/dragonfly/patch-c3270_screen.c
+++ x11/x3270/dragonfly/patch-c3270_screen.c
@@ -1,10 +1,10 @@
---- c3270/screen.c.orig	2018-02-10 22:07:45 UTC
+--- c3270/screen.c.orig	2021-01-31 20:43:17 UTC
 +++ c3270/screen.c
 @@ -31,6 +31,7 @@
   *		Screen drawing
   */
  
-+#include <wctype.h>
++#include <wctype.h> /* for wint_t */
  #include "globals.h"
  #include <signal.h>
  #include "appres.h"
