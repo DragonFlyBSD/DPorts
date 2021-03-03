52348bfd9ee05e8a0c4f8a607f3abcf410b8ae02
DPorts Builder <nobody@home.ok>
2021-03-02 21:27:32 -0800

    Update multimedia/shotcut to version 20.10.31

--- multimedia/shotcut/dragonfly/patch-src_spatialmedia_constants.h
+++ multimedia/shotcut/dragonfly/patch-src_spatialmedia_constants.h
@@ -1,11 +1,14 @@
---- src/spatialmedia/constants.h.intermediate	2020-11-14 11:52:57.000000000 +0000
+--- src/spatialmedia/constants.h.intermediate	2021-03-03 04:52:44.000000000 +0000
 +++ src/spatialmedia/constants.h
-@@ -35,7 +35,7 @@
+@@ -34,9 +34,9 @@
  #  define htole64(x) qtoLittleEndian(x)
  #  define be64toh(x) qFromBigEndian(x)
  #  define le64toh(x) qFromLittleEndian(x)
+-#elif !defined(__FreeBSD__)
++#elif !defined(__FreeBSD__) && !defined(__DragonFly__)
+ #  include <endian.h>
 -#elif defined(__FreeBSD__)
 +#elif defined(__FreeBSD__) || defined(__DragonFly__)
- # include <sys/endian.h>
- #else
- #  include <endian.h>
+ #  include <sys/endian.h>
+ #endif
+ 
--- multimedia/shotcut/files/patch-src_spatialmedia_constants.h
+++ multimedia/shotcut/files/patch-src_spatialmedia_constants.h
@@ -1,19 +1,11 @@
---- src/spatialmedia/constants.h.orig	2020-09-20 19:10:00 UTC
+--- src/spatialmedia/constants.h.orig	2020-12-28 20:21:17 UTC
 +++ src/spatialmedia/constants.h
-@@ -19,6 +19,7 @@
- 
- #include <stdint.h>
- #include <string>
-+#include <sys/param.h>
- 
- #include <QtEndian>
- #if defined(Q_OS_WIN) || defined(Q_OS_MAC)
-@@ -34,6 +35,8 @@
- #  define htole64(x) qtoLittleEndian(x)
- #  define be64toh(x) qFromBigEndian(x)
+@@ -36,6 +36,8 @@
  #  define le64toh(x) qFromLittleEndian(x)
-+#elif defined(__FreeBSD__)
-+# include <sys/endian.h>
- #else
+ #elif !defined(__FreeBSD__)
  #  include <endian.h>
++#elif defined(__FreeBSD__)
++#  include <sys/endian.h>
  #endif
+ 
+ struct AudioMetadata {
