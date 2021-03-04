6724918022c3cba9e4d27ecfa84e5b39674ea74e
DPorts Builder <nobody@home.ok>
2021-03-04 08:07:53 -0800

    Update sysutils/xfce4-cpugraph-plugin to version 1.2.2

--- sysutils/xfce4-cpugraph-plugin/dragonfly/patch-panel-plugin_os.c
+++ sysutils/xfce4-cpugraph-plugin/dragonfly/patch-panel-plugin_os.c
@@ -1,6 +1,6 @@
---- panel-plugin/os.c.orig	2019-07-02 23:18:51 UTC
+--- panel-plugin/os.c.orig	2021-02-14 17:45:27 UTC
 +++ panel-plugin/os.c
-@@ -50,6 +50,13 @@
+@@ -52,6 +52,13 @@
  #include <nlist.h>
  #endif
  
@@ -14,69 +14,29 @@
  #if defined (__NetBSD__)
  #include <sys/param.h>
  #include <sys/sched.h>
-@@ -192,6 +199,65 @@ read_cpu_data (CpuData *data, guint nb_c
-     }
- 
-     data[0].load /= nb_cpu;
-+    g_free (cp_time);
-+    return TRUE;
-+}
-+
-+#elif defined (__DragonFly__)
-+guint
-+detect_cpu_number ()
-+{
-+    static gint mib[] = {CTL_HW, HW_NCPU};
-+    gint ncpu;
-+    gsize len = sizeof (gint);
-+
-+    if (sysctl (mib, 2, &ncpu, &len, NULL, 0) < 0)
-+        return 0;
-+    else
-+        return ncpu;
-+}
-+
-+gboolean
-+read_cpu_data (CpuData *data, guint nb_cpu)
-+{
-+    glong used, total;
-+    glong *cp_time;
-+    glong *cp_time1;
-+    gint i;
-+    gint max_cpu;
-+    gsize len = sizeof (max_cpu);
-+
-+    data[0].load = 0;
-+    if (sysctlbyname ("hw.ncpu", &max_cpu, &len, NULL, 0) < 0)
-+        return FALSE;
-+
-+    if (max_cpu < nb_cpu)
-+        return FALSE; /* should not happen */
-+    len = sizeof (glong) * max_cpu * CPUSTATES;
-+    cp_time = (glong *) g_malloc (len);
-+
-+    if (sysctlbyname ("kern.cp_times", cp_time, &len, NULL, 0) < 0) {
-+        g_free (cp_time);
-+        return FALSE;
-+    }
-+
-+    for (i = 1; i <= nb_cpu; i++)
-+    {
-+        cp_time1 = &cp_time[CPUSTATES * (i - 1)];
-+        used = cp_time1[CP_USER] + cp_time1[CP_NICE] + cp_time1[CP_SYS] + cp_time1[CP_INTR];
-+        total = used + cp_time1[CP_IDLE];
-+
-+        if ((total - data[i].previous_total) != 0)
-+            data[i].load = (CPU_SCALE * (used - data[i].previous_used)) /
-+                           (total - data[i].previous_total);
-+        else
-+            data[i].load = 0;
-+            data[i].previous_used = used;
-+            data[i].previous_total = total;
-+            data[0].load += data[i].load;
-+    }
-+
-+    data[0].load /= nb_cpu;
-     g_free (cp_time);
+@@ -172,7 +179,7 @@ read_cpu_data (CpuData *data, guint nb_c
      return TRUE;
  }
+ 
+-#elif defined (__FreeBSD__)
++#elif defined (__FreeBSD__) || defined(__DragonFly__)
+ guint
+ detect_cpu_number (void)
+ {
+@@ -197,10 +204,16 @@ read_cpu_data (CpuData *data, guint nb_c
+     gsize len = sizeof (max_cpu);
+ 
+     data[0].load = 0;
++#ifdef __DragonFly__
++    if (sysctlbyname ("hw.ncpu", &max_cpu, &len, NULL, 0) < 0)
++#else
+     if (sysctlbyname ("kern.smp.maxid", &max_cpu, &len, NULL, 0) < 0)
++#endif
+         return FALSE;
+ 
++#ifndef __DragonFly__
+     max_cpu++; /* max_cpu is 0-based */
++#endif
+     if (max_cpu < nb_cpu)
+         return FALSE; /* should not happen */
+     len = sizeof (glong) * max_cpu * CPUSTATES;
