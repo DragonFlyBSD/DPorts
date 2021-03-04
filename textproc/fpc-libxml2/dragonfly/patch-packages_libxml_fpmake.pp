eb42dc531cc36d822d3162389b928e25892da522
DPorts Builder <nobody@home.ok>
2021-03-04 13:47:10 -0800

    Update textproc/fpc-libxml2 to version 3.2.0_1

--- textproc/fpc-libxml2/dragonfly/patch-packages_libxml_fpmake.pp
+++ textproc/fpc-libxml2/dragonfly/patch-packages_libxml_fpmake.pp
@@ -1,9 +1,9 @@
---- packages/libxml/fpmake.pp.orig	2015-11-13 21:15:29 UTC
+--- packages/libxml/fpmake.pp.orig	2020-05-04 16:58:27 UTC
 +++ packages/libxml/fpmake.pp
 @@ -17,7 +17,7 @@ begin
      P.Directory:=ADirectory;
  {$endif ALLPACKAGES}
-     P.Version:='3.0.4';
+     P.Version:='3.2.0';
 -    P.OSes := [freebsd,linux,win32];
 +    P.OSes := [dragonfly,freebsd,linux,win32];
      P.SourcePath.Add('src');
