0cb758dea644c1578d19c9cd7c14e05d2b6e2d88
DPorts Builder <nobody@home.ok>
2021-03-08 08:37:05 -0800

    Update audio/musescore to version 3.6.1

--- audio/musescore/files/patch-thirdparty_qt-google-analytics_ganalytics.cpp
+++ audio/musescore/files/patch-thirdparty_qt-google-analytics_ganalytics.cpp
@@ -1,11 +1,11 @@
---- thirdparty/google_analytics/ganalytics.cpp.orig	2020-02-06 21:10:58 UTC
-+++ thirdparty/google_analytics/ganalytics.cpp
-@@ -381,7 +381,7 @@ QString GAnalytics::Private::getSystemIn
+--- thirdparty/qt-google-analytics/ganalytics.cpp.orig	2021-01-27 17:12:14 UTC
++++ thirdparty/qt-google-analytics/ganalytics.cpp
+@@ -381,7 +381,7 @@ QString GAnalytics::Private::getSystemInfo()
              .arg(QAndroidJniObject::getStaticObjectField<jstring>("android/os/Build", "ID").toString())
              .arg(QAndroidJniObject::getStaticObjectField<jstring>("android/os/Build", "BRAND").toString());
  }
 -#elif defined(Q_OS_LINUX)
-+#elif defined(Q_OS_LINUX) || defined(__DragonFly__)
++#elif defined(Q_OS_UNIX)
  #include <sys/utsname.h>
  
  /**
