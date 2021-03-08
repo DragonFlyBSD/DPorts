27d8908f14fa9812857f340719e482a4ef474519
DPorts Builder <nobody@home.ok>
2021-03-08 02:31:21 -0800

    Update sysutils/pies to version 1.5

--- sysutils/pies/dragonfly/patch-src_utmp.c
+++ sysutils/pies/dragonfly/patch-src_utmp.c
@@ -1,15 +1,16 @@
---- src/utmp.c.orig	2019-05-23 10:19:15 UTC
+--- src/utmp.c.orig	2020-10-17 12:04:49 UTC
 +++ src/utmp.c
-@@ -30,6 +30,12 @@ typedef struct utmpx UTMPX;
- # define UTMP_FILE UTMPX_FILE
- # undef WTMP_FILE
- # define WTMP_FILE WTMPX_FILE
-+# ifndef WTMPX_FILE
-+#  define WTMPX_FILE _PATH_WTMPX
-+# endif
-+# ifndef UTMPX_FILE
-+#  define UTMPX_FILE _PATH_UTMPX
-+# endif
- # if defined HAVE_STRUCT_UTMPX_UT_NAME
- #  define UT_NAME(u) ((u)->ut_name)
- # elif defined HAVE_STRUCT_UTMPX_UT_USER
+@@ -48,6 +48,13 @@ typedef struct utmp UTMPX;
+ # define endutxent endutent
+ #endif
+ 
++#ifndef WTMPX_FILE
++# define WTMPX_FILE _PATH_WTMPX
++#endif
++#ifndef UTMPX_FILE
++# define UTMPX_FILE _PATH_UTMPX
++#endif
++
+ #if !defined(UTMPX_FILE) || !defined(WTMPX_FILE)
+ # if !defined(UTMP_FILE) || !defined(WTMP_FILE)
+ #  ifndef  HAVE_UTMPX_H
