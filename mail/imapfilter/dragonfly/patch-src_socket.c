ae3401ff1cb7bfc8082fede2640f2747936d4a98
DPorts Builder <nobody@home.ok>
2021-03-08 02:22:38 -0800

    Update mail/imapfilter to version 2.7.5

--- mail/imapfilter/dragonfly/patch-src_socket.c
+++ mail/imapfilter/dragonfly/patch-src_socket.c
@@ -1,9 +1,9 @@
---- src/socket.c.orig	2019-11-22 22:07:11 UTC
+--- src/socket.c.orig	2020-12-05 21:35:02 UTC
 +++ src/socket.c
 @@ -143,7 +143,7 @@ open_secure_connection(session *ssn)
  		goto fail;
  
- 	if (get_option_boolean("certificates")) {
+ 	if (get_option_boolean("hostnames")) {
 -#if OPENSSL_VERSION_NUMBER >= 0x10100000L
 +#if OPENSSL_VERSION_NUMBER >= 0x10100000L && !defined(LIBRESSL_VERSION_NUMBER)
  		SSL_set_hostflags(ssn->sslconn,
