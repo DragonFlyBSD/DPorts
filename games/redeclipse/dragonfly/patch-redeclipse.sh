01ddd0543a8860cabdebd30eceab0904835cd354
DPorts Builder <nobody@home.ok>
2021-03-08 02:34:03 -0800

    Update games/redeclipse to version 2.0.0

--- games/redeclipse/dragonfly/patch-redeclipse.sh
+++ games/redeclipse/dragonfly/patch-redeclipse.sh
@@ -1,11 +1,20 @@
---- redeclipse.sh.orig	2015-07-18 05:54:17.000000000 +0300
+--- redeclipse.sh.orig	2019-12-19 21:18:32 UTC
 +++ redeclipse.sh
-@@ -33,7 +33,7 @@ redeclipse_setup() {
+@@ -27,7 +27,7 @@ redeclipse_setup() {
+ 		        REDECLIPSE_TARGET="macos"
                  REDECLIPSE_ARCH="redeclipse.app/Contents/MacOS"
-                 REDECLIPSE_MAKE="./src/osxbuild.sh all install"
- 		;;
+                 ;;
 -            FreeBSD)
 +            FreeBSD|DragonFly)
                  REDECLIPSE_SUFFIX="_bsd"
                  REDECLIPSE_TARGET="bsd"
-                 REDECLIPSE_BRANCH="source" # we don't have binaries for bsd yet sorry
+                 ;;
+@@ -75,7 +75,7 @@ redeclipse_runit() {
+         export REDECLIPSE_PWD
+         cd "${REDECLIPSE_PATH}" || return 1
+         case "${REDECLIPSE_SYSTEM}" in
+-            Linux|FreeBSD)
++            Linux|FreeBSD|DragonFly)
+                 export LD_LIBRARY_PATH="${REDECLIPSE_PATH}/bin/${REDECLIPSE_ARCH}:${LD_LIBRARY_PATH}"
+                 ;;
+             Darwin)
