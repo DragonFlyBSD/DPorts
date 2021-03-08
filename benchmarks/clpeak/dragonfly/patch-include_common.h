1de68b5ee4abadabc51b0a3c4278cf673a27fb39
DPorts Builder <nobody@home.ok>
2021-03-08 02:38:48 -0800

    Update benchmarks/clpeak to version 1.1.0

--- benchmarks/clpeak/dragonfly/patch-include_common.h
+++ benchmarks/clpeak/dragonfly/patch-include_common.h
@@ -1,29 +1,29 @@
---- include/common.h.orig	2019-10-20 08:33:25 UTC
+--- include/common.h.intermediate	2021-03-08 06:39:53.000000000 +0000
 +++ include/common.h
-@@ -2,7 +2,7 @@
- #define COMMON_H
+@@ -7,7 +7,7 @@
  
- #include <CL/cl.hpp>
+ #include <CL/opencl.hpp>
+ 
 -#if defined(__APPLE__) || defined(__MACOSX) || defined(__FreeBSD__)
-+#if defined(__APPLE__) || defined(__MACOSX) || defined(__FreeBSD__) || defined(__DragonFly__) 
++#if defined(__APPLE__) || defined(__MACOSX) || defined(__FreeBSD__) || defined(__DragonFly__)
  #include <sys/types.h>
  #endif
  
-@@ -13,7 +13,7 @@
+@@ -18,7 +18,7 @@
  
  #define TAB             "  "
  #define NEWLINE         "\n"
 -#ifndef __FreeBSD__
-+#if ! defined __FreeBSD__ && ! defined __DragonFly__
++#if !defined(__FreeBSD__) && !defined(__DragonFly__)
  #define uint            unsigned int
  #endif
  #define ulong           unsigned long
-@@ -53,6 +53,8 @@
+@@ -56,6 +56,8 @@
+   #else
+   #define OS_NAME     "Linux unknown"
    #endif
++#elif defined(__DragonFly__)
++#define OS_NAME     "DragonFly"
  #elif defined(__FreeBSD__)
  #define OS_NAME     "FreeBSD"
-+#elif defined(__DragonFly__)
-+#define OS_NAME     "DragonFly"	
  #else
- #define OS_NAME     "Unknown"
- #endif
