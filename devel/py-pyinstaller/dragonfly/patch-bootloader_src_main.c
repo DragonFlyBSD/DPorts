cd7de212818574026321969511c4e13fe563a795
DPorts Builder <nobody@home.ok>
2021-03-08 08:09:40 -0800

    Tweak devel/py-pyinstaller version 3.5_1

--- devel/py-pyinstaller/dragonfly/patch-bootloader_src_main.c
+++ devel/py-pyinstaller/dragonfly/patch-bootloader_src_main.c
@@ -1,4 +1,4 @@
---- bootloader/src/main.c.intermediate	2019-12-30 17:27:00 UTC
+--- bootloader/src/main.c.intermediate	2021-03-08 11:48:12.000000000 +0000
 +++ bootloader/src/main.c
 @@ -16,7 +16,7 @@
   * main: For OS X and Linux
@@ -6,6 +6,6 @@
  
 -#ifdef __FreeBSD__
 +#if defined(__FreeBSD__) || defined(__DragonFly__)
+     #include <sys/cdefs.h>
      #include <floatingpoint.h>
      #include <ieeefp.h>
- #endif
