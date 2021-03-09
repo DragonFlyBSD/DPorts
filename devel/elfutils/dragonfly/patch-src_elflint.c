eb909ab8b46bc419cec701f1dbbc81b3db7d9315
DPorts Builder <nobody@home.ok>
2021-03-09 08:46:00 -0800

    Update devel/elfutils to version 0.179

--- devel/elfutils/dragonfly/patch-src_elflint.c
+++ devel/elfutils/dragonfly/patch-src_elflint.c
@@ -1,10 +1,10 @@
---- src/elflint.c.orig	2018-09-14 10:23:36 UTC
+--- src/elflint.c.orig	2020-03-30 12:17:45 UTC
 +++ src/elflint.c
-@@ -4331,6 +4331,7 @@ section [%2d] '%s': unknown core file no
- 	  case NT_GNU_HWCAP:
+@@ -4336,6 +4336,7 @@ section [%2d] '%s': unknown core file no
  	  case NT_GNU_BUILD_ID:
  	  case NT_GNU_GOLD_VERSION:
+ 	  case NT_GNU_PROPERTY_TYPE_0:
 +	  case NT_BSD_ABI_TAG:
- 	    break;
- 
- 	  case 0:
+ 	    if (nhdr.n_namesz == sizeof ELF_NOTE_GNU
+ 		&& strcmp (data->d_buf + name_offset, ELF_NOTE_GNU) == 0)
+ 	      break;
