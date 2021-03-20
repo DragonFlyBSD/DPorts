9acd9fa7f0b7048b8f5255a52a095eb2c589b513
DPorts Builder <nobody@home.ok>
2021-03-19 17:42:54 -0700

    Sync Mk with ports

--- Mk/Uses/lazarus.mk
+++ Mk/Uses/lazarus.mk
@@ -72,7 +72,7 @@ LAZARUS_DIR?=		${LOCALBASE}/share/lazarus-${LAZARUS_VER}
 ONLY_FOR_ARCHS=		i386 amd64
 ONLY_FOR_ARCHS_REASON=	not yet ported to anything other than i386 and amd64
 
-BUILD_DEPENDS+=		${LOCALBASE}/bin/as:devel/binutils \
+BUILD_DEPENDS+=		${LOCALBASE}/bin/as:devel/binutils@native \
 			fpcres:lang/fpc-utils
 
 BUILDNAME=		${LAZARUS_ARCH}-${OPSYS:tl}
