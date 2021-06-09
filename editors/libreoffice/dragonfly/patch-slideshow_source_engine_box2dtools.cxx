60111791918fd99c20671a8312362f163a78a920
DPorts Builder <nobody@home.ok>
2021-06-09 14:33:39 -0700

    Update editors/libreoffice to version 7.1.3.2

---
+++ editors/libreoffice/dragonfly/patch-slideshow_source_engine_box2dtools.cxx
@@ -0,0 +1,12 @@
+--- slideshow/source/engine/box2dtools.cxx.orig	2021-04-28 16:17:45 UTC
++++ slideshow/source/engine/box2dtools.cxx
+@@ -8,6 +8,9 @@
+  */
+ 
+ #include <box2dtools.hxx>
++
++using namespace std;
++
+ #include <config_box2d.h>
+ #include BOX2D_HEADER
+ 
