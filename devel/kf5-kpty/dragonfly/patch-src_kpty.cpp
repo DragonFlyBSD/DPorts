fda1fcaed9472e38c90dc71edc8f18f602624a18
DPorts Builder <nobody@home.ok>
2021-06-10 13:28:51 -0700

    Update devel/kf5-kpty to version 5.80.0

--- devel/kf5-kpty/dragonfly/patch-src_kpty.cpp
+++ devel/kf5-kpty/dragonfly/patch-src_kpty.cpp
@@ -1,12 +1,12 @@
---- src/kpty.cpp.orig	2019-07-07 18:37:11 UTC
-+++ src/kpty.cpp
-@@ -95,7 +95,9 @@ public:
+--- src/kpty.cpp.orig	2021-06-10 19:58:19.279111000 +0200
++++ src/kpty.cpp	2021-06-10 19:59:12.367909000 +0200
+@@ -69,7 +69,9 @@
      int cmdFd;
  };
  #else
 +#ifndef __DragonFly__
- # include <utmp.h>
+ #include <utmp.h>
 +#endif
- # if HAVE_UTMPX
- #  include <utmpx.h>
- # endif
+ #if HAVE_UTMPX
+ #include <utmpx.h>
+ #endif
