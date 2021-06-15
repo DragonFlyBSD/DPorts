d40235b79862315d6d20ef694bb6b3eeb95ced04
DPorts Builder <nobody@home.ok>
2021-06-15 12:05:50 -0700

    Update sysutils/restic to version 0.12.0

--- sysutils/restic/dragonfly/patch-internal_restic_node_dragonfly.go
+++ sysutils/restic/dragonfly/patch-internal_restic_node_dragonfly.go
@@ -1,6 +1,6 @@
 --- internal/restic/node_dragonfly.go.orig	1970-01-01 01:00:00.000000000 +0100
-+++ internal/restic/node_dragonfly.go	2021-01-20 18:59:23.226255000 +0100
-@@ -0,0 +1,17 @@
++++ internal/restic/node_dragonfly.go	2021-06-13 15:23:09.253146000 +0200
+@@ -0,0 +1,34 @@
 +// +build dragonfly
 +
 +package restic
@@ -8,13 +8,30 @@
 +import "syscall"
 +
 +func (node Node) restoreSymlinkTimestamps(path string, utimes [2]syscall.Timespec) error {
-+	return nil
++        return nil
 +}
 +
 +func (node Node) device() int {
-+	return int(node.Device)
++        return int(node.Device)
 +}
 +
-+func (s statUnix) atim() syscall.Timespec { return s.Atim }
-+func (s statUnix) mtim() syscall.Timespec { return s.Mtim }
-+func (s statUnix) ctim() syscall.Timespec { return s.Ctim }
++func (s statT) atim() syscall.Timespec { return s.Atim }
++func (s statT) mtim() syscall.Timespec { return s.Mtim }
++func (s statT) ctim() syscall.Timespec { return s.Ctim }
++
++// Getxattr retrieves extended attribute data associated with path.
++func Getxattr(path, name string) ([]byte, error) {
++        return nil, nil
++}
++
++// Listxattr retrieves a list of names of extended attributes associated with the
++// given path in the file system.
++func Listxattr(path string) ([]string, error) {
++        return nil, nil
++}
++
++// Setxattr associates name and data together as an attribute of path.
++func Setxattr(path, name string, data []byte) error {
++        return nil
++}
++
