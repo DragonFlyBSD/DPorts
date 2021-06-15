280ef1bd16223a580eaada5a413507189a5e58ea
DPorts Builder <nobody@home.ok>
2021-06-15 11:59:08 -0700

    Update graphics/mesa-devel to version 21.1.b.1978

--- graphics/mesa-devel/dragonfly/patch-src_util_u__thread.h
+++ graphics/mesa-devel/dragonfly/patch-src_util_u__thread.h
@@ -1,9 +1,9 @@
---- src/util/u_thread.h.orig	2020-10-26 21:48:36 UTC
-+++ src/util/u_thread.h
-@@ -79,7 +79,7 @@ static inline void u_thread_setname( con
- #if defined(HAVE_PTHREAD)
- #if DETECT_OS_LINUX || DETECT_OS_CYGWIN || DETECT_OS_SOLARIS
-    pthread_setname_np(pthread_self(), name);
+--- src/util/u_thread.h.orig	2021-06-15 12:49:06.371343000 +0200
++++ src/util/u_thread.h	2021-06-15 12:49:34.200714000 +0200
+@@ -113,7 +113,7 @@
+       buf[len] = '\0';
+       pthread_setname_np(pthread_self(), buf);
+    }
 -#elif DETECT_OS_FREEBSD || DETECT_OS_OPENBSD
 +#elif DETECT_OS_FREEBSD || DETECT_OS_OPENBSD || DETECT_OS_DRAGONFLY
     pthread_set_name_np(pthread_self(), name);
