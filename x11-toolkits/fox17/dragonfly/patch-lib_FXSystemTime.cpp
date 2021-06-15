b957ac3fa4d1bb39d2091d449b6c5b74309cdf23
DPorts Builder <nobody@home.ok>
2021-06-15 11:29:00 -0700

    Update x11-toolkits/fox17 to version 1.7.75

--- x11-toolkits/fox17/dragonfly/patch-lib_FXSystemTime.cpp
+++ x11-toolkits/fox17/dragonfly/patch-lib_FXSystemTime.cpp
@@ -1,20 +1,20 @@
---- lib/FXSystemTime.cpp.intermediate	2021-03-07 14:53:24.000000000 +0000
-+++ lib/FXSystemTime.cpp
-@@ -264,7 +264,7 @@ FXTime FXSystem::localTimeZoneOffset(){
+--- lib/FXSystemTime.cpp.orig	2021-06-13 14:18:02.910765000 +0200
++++ lib/FXSystemTime.cpp	2021-06-13 14:20:01.948102000 +0200
+@@ -264,7 +264,7 @@
    setuplocaltimezone();
  #if defined(_WIN32)
    return minutes*tzi.Bias;              // +minutes*tzi.StandardBias;
 -#elif defined(__FreeBSD__) || defined(__OpenBSD__)
 +#elif defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__DragonFly__)
-   return 0;     // FIXME
- #else
-   return seconds*timezone;
-@@ -278,7 +278,7 @@ FXTime FXSystem::daylightSavingsOffset()
+   struct tm tmresult;
+   time_t tmp=time(&tmp);
+   struct tm* ptm=localtime_r(&tmp,&tmresult);
+@@ -280,7 +280,7 @@
+   setuplocaltimezone();
  #if defined(_WIN32)
    return minutes*tzi.DaylightBias;      // Or difference between standard and daylight bias.
+-#elif defined(__FreeBSD__) || defined(__OpenBSD__)
++#elif defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__DragonFly__)
+   return -hours*((tzname[1][0] == ' ') ? 0 : 1);
  #else
--# if defined(__FreeBSD__)
-+# if defined(__FreeBSD__) || defined(__DragonFly__)
-    tzset();
-    int daylight = ((tzname[1][0] == ' ') ? 0 : 1);
- # endif
+   return -hours*daylight;
--- x11-toolkits/fox17/dragonfly/patch-lib_FXThread.cpp
+++ x11-toolkits/fox17/dragonfly/patch-lib_FXThread.cpp
@@ -1,31 +1,11 @@
-Explictly ignore get_name_np() for now, just return null
-because it returns temp stack pointer here.
-
---- lib/FXThread.cpp.intermediate	2021-03-07 14:53:24.000000000 +0000
-+++ lib/FXThread.cpp
-@@ -925,14 +925,14 @@ FXbool FXThread::description(const FXStr
+--- lib/FXThread.cpp.orig	2021-06-13 14:20:55.066913000 +0200
++++ lib/FXThread.cpp	2021-06-13 14:22:55.964207000 +0200
+@@ -911,7 +911,7 @@
+     return 0<=fxSetThreadDescription((HANDLE)tid,udesc);
+ #elif defined(__APPLE__)
      return pthread_setname_np(desc.text())==0;
- #elif defined(__NetBSD__)
-     return pthread_setname_np(tid,"%s",desc.text())==0;
--#elif defined(__FreeBSD__)
-+#elif defined(__FreeBSD__) || (defined(__DragonFly__) && !defined(HAVE_PTHREAD_SETNAME_NP))
+-#elif defined(__FreeBSD__) && !defined(HAVE_PTHREAD_SETNAME_NP)
++#elif (defined(__FreeBSD__) || defined(__DragonFly__)) && !defined(HAVE_PTHREAD_SETNAME_NP)
      pthread_set_name_np((pthread_t)tid,desc.text());
      return true;
- #elif defined(__OpenBSD__)
-     pthread_set_name_np(tid,desc.text());
-     return true;
- #elif defined(HAVE_PTHREAD_SETNAME_NP)
--    return pthread_setname_np(tid,desc.text())==0;
-+    return pthread_setname_np((pthread_t)tid,desc.text())==0;
- #endif
-     }
-   return false;
-@@ -967,7 +967,7 @@ FXString FXThread::description() const {
-       }
- #elif defined(HAVE_PTHREAD_GETNAME_NP)
-     FXchar desc[256];
--    if(pthread_getname_np(tid,desc,ARRAYNUMBER(desc))==0){
-+    if(pthread_getname_np((pthread_t)tid,desc,ARRAYNUMBER(desc))==0){
-       return desc;
-       }
- #endif
+ #elif defined(__NetBSD__) || defined(__OpenBSD__)
