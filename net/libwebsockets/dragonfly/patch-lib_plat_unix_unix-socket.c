65294ad4e0904afde8cf2e980b55d945f39ddb9b
DPorts Builder <nobody@home.ok>
2021-06-20 07:54:34 -0700

    Update net/libwebsockets to version 4.2.0_1

--- net/libwebsockets/dragonfly/patch-lib_plat_unix_unix-socket.c
+++ net/libwebsockets/dragonfly/patch-lib_plat_unix_unix-socket.c
@@ -1,29 +1,34 @@
---- lib/plat/unix/unix-sockets.c.orig	2020-09-21 17:52:01.688244000 +0200
-+++ lib/plat/unix/unix-sockets.c	2020-09-21 17:52:56.817904000 +0200
-@@ -84,7 +84,7 @@
+--- lib/plat/unix/unix-sockets.c.orig	2021-06-19 21:57:06.250431000 +0200
++++ lib/plat/unix/unix-sockets.c	2021-06-19 21:58:07.649016000 +0200
+@@ -105,6 +105,7 @@
  	socklen_t optlen = sizeof(optval);
  
  #if defined(__APPLE__) || \
--    defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || \
-+    defined(__DragonFly__) || defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || \
++	defined(__DragonFly__) || \
+     defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || \
      defined(__NetBSD__) || \
      defined(__OpenBSD__) || \
-     defined(__HAIKU__)
-@@ -101,7 +101,7 @@
+@@ -122,6 +123,7 @@
  			return 1;
  
  #if defined(__APPLE__) || \
--    defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || \
-+    defined(__DragonFly__) || defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || \
++	defined(__DragonFly__) || \
+     defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || \
      defined(__NetBSD__) || \
      defined(__CYGWIN__) || defined(__OpenBSD__) || defined (__sun) || \
-     defined(__HAIKU__)
-@@ -154,7 +154,7 @@
+@@ -175,6 +177,7 @@
  	if (!unix_skt && setsockopt(fd, IPPROTO_TCP, TCP_NODELAY, (const void *)&optval, optlen) < 0)
  		return 1;
  #elif !defined(__APPLE__) && \
--      !defined(__FreeBSD__) && !defined(__FreeBSD_kernel__) &&        \
-+      !defined(__DragonFly__) && !defined(__FreeBSD__) && !defined(__FreeBSD_kernel__) &&        \
++	  !defined(__DragonFly__) && \
+       !defined(__FreeBSD__) && !defined(__FreeBSD_kernel__) &&        \
        !defined(__NetBSD__) && \
        !defined(__OpenBSD__) && \
-       !defined(__HAIKU__)
+@@ -221,6 +224,7 @@
+ #endif
+ 
+ #if !defined(__APPLE__) && \
++	  !defined(__DragonFly__) && \
+       !defined(__FreeBSD__) && !defined(__FreeBSD_kernel__) &&        \
+       !defined(__NetBSD__) && \
+       !defined(__OpenBSD__) && \
--- net/libwebsockets/dragonfly/patch-lib_plat_unix_unix-spawn.c
+++ net/libwebsockets/dragonfly/patch-lib_plat_unix_unix-spawn.c
@@ -1,11 +1,11 @@
---- lib/plat/unix/unix-spawn.c.orig	1970-01-01 00:00:00 UTC
-+++ lib/plat/unix/unix-spawn.c
-@@ -444,7 +444,7 @@ lws_spawn_piped(const struct lws_spawn_p
+--- lib/plat/unix/unix-spawn.c.orig	2021-06-19 21:53:55.714819000 +0200
++++ lib/plat/unix/unix-spawn.c	2021-06-19 21:54:36.273883000 +0200
+@@ -461,7 +461,7 @@
  	if (lsp->info.disable_ctrlc)
  		/* stops non-daemonized main processess getting SIGINT
  		 * from TTY */
--#if defined(__FreeBSD__)
-+#if defined(__FreeBSD__) || defined(__DragonFly__)
+-#if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__)
++#if defined(__DragonFly__) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__)
  		setpgid(0, 0);
  #else
  		setpgrp();
