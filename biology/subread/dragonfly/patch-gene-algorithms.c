8d07951ba61dbae0d623714432ebfbd5e7d0e2dc
DPorts Builder <nobody@home.ok>
2021-06-20 07:54:38 -0700

    Update biology/subread to version 2.0.2

--- biology/subread/dragonfly/patch-gene-algorithms.c
+++ biology/subread/dragonfly/patch-gene-algorithms.c
@@ -1,11 +1,11 @@
---- gene-algorithms.c.intermediate	2020-11-27 13:54:17.000000000 +0000
-+++ gene-algorithms.c
-@@ -1563,7 +1563,7 @@ int load_offsets(gene_offset_t* offsets
+--- gene-algorithms.c.orig	2021-06-20 10:16:41.569622000 +0200
++++ gene-algorithms.c	2021-06-20 10:18:37.546979000 +0200
+@@ -1568,7 +1568,7 @@
  
  double miltime(){
  	double ret;
 -	#ifdef __FreeBSD__
 +	#if defined(__FreeBSD__) || defined(__DragonFly__)
- 	struct timeval tp;
- 	struct timezone tz;
- 	tz.tz_minuteswest=0;
+ 		struct timeval tp;
+ 		struct timezone tz;
+ 		tz.tz_minuteswest=0;
--- biology/subread/dragonfly/patch-longread-one_longread-mapping.c
+++ biology/subread/dragonfly/patch-longread-one_longread-mapping.c
@@ -1,9 +1,9 @@
---- longread-one/longread-mapping.c.intermediate	2020-11-27 13:54:17.000000000 +0000
-+++ longread-one/longread-mapping.c
-@@ -227,7 +227,7 @@ double LRMmiltime(){
+--- longread-one/longread-mapping.c.orig	2021-06-20 10:27:31.224788000 +0200
++++ longread-one/longread-mapping.c	2021-06-20 10:28:12.503849000 +0200
+@@ -227,7 +227,7 @@
+ #endif
+ double LRMmiltime(){
  	double ret;
- 
- /* Why not use gettimeofday() on all platforms? */
 -#ifdef __FreeBSD__
 +#if defined(__FreeBSD__) || defined(__DragonFly__)
  
