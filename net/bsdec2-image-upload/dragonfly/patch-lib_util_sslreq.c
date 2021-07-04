1c9cee37cd0264b9252217808747294c8597f3b7
DPorts Builder <nobody@home.ok>
2021-07-04 06:50:13 -0700

    Update net/bsdec2-image-upload to version 1.4.1

--- net/bsdec2-image-upload/dragonfly/patch-lib_util_sslreq.c
+++ net/bsdec2-image-upload/dragonfly/patch-lib_util_sslreq.c
@@ -1,6 +1,6 @@
---- lib/util/sslreq.c.orig	2020-04-10 22:55:10 UTC
-+++ lib/util/sslreq.c
-@@ -8,21 +8,13 @@
+--- lib/util/sslreq.c.orig	2021-06-22 17:55:58.499558000 +0200
++++ lib/util/sslreq.c	2021-06-22 18:00:47.482849000 +0200
+@@ -9,23 +9,13 @@
  #include <stdint.h>
  #include <string.h>
  #include <unistd.h>
@@ -12,33 +12,26 @@
  #include "sslreq.h"
  
 -/*
-- * LibreSSL claims to be OpenSSL 2.0, but (currently) has APIs compatible with
+- * LibreSSL claims to be OpenSSL 2.0, but (mostly) has APIs compatible with
 - * OpenSSL 1.0.1g.
 - */
 -#ifdef LIBRESSL_VERSION_NUMBER
 -#undef OPENSSL_VERSION_NUMBER
 -#define OPENSSL_VERSION_NUMBER 0x1000107fL
+-#if LIBRESSL_VERSION_NUMBER >= 0x2090000fL
+-#define HAVE_SSL_SET1_HOST
 -#endif
--
+-#endif
+ 
  /* Compatibility for OpenSSL pre-1.1.0 */
  #if OPENSSL_VERSION_NUMBER < 0x10100000L
- static int
-@@ -33,7 +25,9 @@ SSL_set1_host(SSL * ssl, const char * ho
- 	param = SSL_get0_param(ssl);
+@@ -39,7 +29,9 @@
  	return (X509_VERIFY_PARAM_set1_host(param, hostname, strlen(hostname)));
  }
+ #endif
 +#endif
  
 +#if OPENSSL_VERSION_NUMBER < 0x10100000L || defined(LIBRESSL_VERSION_NUMBER)
  static void
  SSL_set_hostflags(SSL * ssl, unsigned int flags)
  {
-@@ -45,7 +39,7 @@ SSL_set_hostflags(SSL * ssl, unsigned in
- #endif
- 
- /* Compatibility for OpenSSL pre-1.1.1. */
--#if OPENSSL_VERSION_NUMBER < 0x10101000L
-+#if OPENSSL_VERSION_NUMBER < 0x10101000L || defined(LIBRESSL_VERSION_NUMBER)
- static int
- SSL_write_ex(SSL * ssl, const void * buf, size_t num,
-     size_t * written)
