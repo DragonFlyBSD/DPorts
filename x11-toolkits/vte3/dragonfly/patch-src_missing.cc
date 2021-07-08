775d9d28d24d381c3624865bde16601b244105bb
DPorts Builder <nobody@home.ok>
2021-07-08 05:10:44 -0700

    Update x11-toolkits/vte3 to version 0.64.1

--- x11-toolkits/vte3/dragonfly/patch-src_missing.cc
+++ x11-toolkits/vte3/dragonfly/patch-src_missing.cc
@@ -1,11 +1,11 @@
---- src/missing.cc.orig	2020-10-08 12:30:11 UTC
-+++ src/missing.cc
-@@ -113,7 +113,7 @@ getrlimit_NOFILE_max(void)
+--- src/missing.cc.orig	2021-07-08 11:17:51.355975000 +0200
++++ src/missing.cc	2021-07-08 11:18:10.725535000 +0200
+@@ -119,7 +119,7 @@
  
  #endif /* HAVE_SYS_RESOURCE_H */
  
--#if defined(__FreeBSD__) || defined(__OpenBSD__)
-+#if defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__DragonFly__)
+-#if defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__APPLE__)
++#if defined(__DragonFly__) || defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__APPLE__)
          /* Use sysconf() function provided by the system if it is known to be
           * async-signal safe.
           */
