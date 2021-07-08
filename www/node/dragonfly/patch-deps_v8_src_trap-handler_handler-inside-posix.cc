a25a6ebc35d946065aec2ae4c95bb6315779b5fd
DPorts Builder <nobody@home.ok>
2021-07-08 14:28:39 -0700

    Update www/node to version 16.2.0

--- www/node/dragonfly/patch-deps_v8_src_trap-handler_handler-inside-posix.cc
+++ www/node/dragonfly/patch-deps_v8_src_trap-handler_handler-inside-posix.cc
@@ -1,21 +1,20 @@
---- deps/v8/src/trap-handler/handler-inside-posix.cc.orig	2020-06-02 15:09:43 UTC
-+++ deps/v8/src/trap-handler/handler-inside-posix.cc
-@@ -27,7 +27,8 @@
+--- deps/v8/src/trap-handler/handler-inside-posix.cc.orig	2021-07-08 13:39:02.013835000 +0200
++++ deps/v8/src/trap-handler/handler-inside-posix.cc	2021-07-08 19:07:18.723500000 +0200
+@@ -27,7 +27,7 @@
  
  #include <signal.h>
  
 -#if defined(V8_OS_LINUX) || defined(V8_OS_FREEBSD)
-+#if defined(V8_OS_LINUX) || defined(V8_OS_FREEBSD) || \
-+  defined(V8_OS_DRAGONFLYBSD)
++#if defined(V8_OS_LINUX) || defined(V8_OS_FREEBSD) || defined(V8_OS_DRAGONFLYBSD)
  #include <ucontext.h>
  #elif V8_OS_MACOSX
  #include <sys/ucontext.h>
-@@ -112,7 +113,7 @@ bool TryHandleSignal(int signum, siginfo
-     auto* context_rip = &uc->uc_mcontext.gregs[REG_RIP];
- #elif V8_OS_MACOSX
-     auto* context_rip = &uc->uc_mcontext->__ss.__rip;
--#elif V8_OS_FREEBSD
-+#elif V8_OS_FREEBSD || V8_OS_DRAGONFLYBSD
-     auto* context_rip = &uc->uc_mcontext.mc_rip;
+@@ -114,6 +114,8 @@
+     auto* context_ip = &uc->uc_mcontext->__ss.__rip;
+ #elif V8_OS_FREEBSD && V8_TARGET_ARCH_X64
+     auto* context_ip = &uc->uc_mcontext.mc_rip;
++#elif V8_OS_DRAGONFLYBSD && V8_TARGET_ARCH_X64
++    auto* context_ip = &uc->uc_mcontext.mc_rip;
  #else
  #error Unsupported platform
+ #endif
