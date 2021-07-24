d23234d469817abc853bdd23cd3b206e7fd9c339
DPorts Builder <nobody@home.ok>
2021-07-24 10:28:59 -0700

    Update devel/abseil to version 20210324.2

--- devel/abseil/dragonfly/patch-absl_base_config.h
+++ devel/abseil/dragonfly/patch-absl_base_config.h
@@ -1,15 +1,15 @@
---- absl/base/config.h.orig	2020-10-21 12:32:42 UTC
+--- absl/base/config.h.orig	2021-06-01 14:52:49 UTC
 +++ absl/base/config.h
-@@ -364,7 +364,7 @@ static_assert(ABSL_INTERNAL_INLINE_NAMES
+@@ -372,7 +372,7 @@ static_assert(ABSL_INTERNAL_INLINE_NAMES
  #elif defined(__linux__) || defined(__APPLE__) || defined(__FreeBSD__) ||   \
      defined(__ros__) || defined(__native_client__) || defined(__asmjs__) || \
      defined(__wasm__) || defined(__Fuchsia__) || defined(__sun) || \
--    defined(__ASYLO__)
-+	defined(__ASYLO__) || defined(__DragonFly__)
+-    defined(__ASYLO__) || defined(__myriad2__)
++    defined(__ASYLO__) || defined(__myriad2__) || defined(__DragonFly__)
  #define ABSL_HAVE_MMAP 1
  #endif
  
-@@ -375,7 +375,7 @@ static_assert(ABSL_INTERNAL_INLINE_NAMES
+@@ -383,7 +383,7 @@ static_assert(ABSL_INTERNAL_INLINE_NAMES
  #ifdef ABSL_HAVE_PTHREAD_GETSCHEDPARAM
  #error ABSL_HAVE_PTHREAD_GETSCHEDPARAM cannot be directly set
  #elif defined(__linux__) || defined(__APPLE__) || defined(__FreeBSD__) || \
