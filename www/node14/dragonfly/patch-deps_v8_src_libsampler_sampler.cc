c96f2f8a792c3771b2b3246740430e19cfb8c9e4
DPorts Builder <nobody@home.ok>
2021-07-27 18:06:39 -0700

    Update www/node14 to version 14.17.0

--- www/node14/dragonfly/patch-deps_v8_src_libsampler_sampler.cc
+++ www/node14/dragonfly/patch-deps_v8_src_libsampler_sampler.cc
@@ -1,9 +1,9 @@
---- deps/v8/src/libsampler/sampler.cc.intermediate	2021-03-05 13:40:41.000000000 +0000
-+++ deps/v8/src/libsampler/sampler.cc
-@@ -469,7 +469,7 @@ void SignalHandler::FillRegisterState(vo
-   state->sp = reinterpret_cast<void*>(mcontext->__ss.__esp);
-   state->fp = reinterpret_cast<void*>(mcontext->__ss.__ebp);
- #endif  // V8_HOST_ARCH_IA32
+--- deps/v8/src/libsampler/sampler.cc.orig	2021-07-26 22:44:32.199581000 +0200
++++ deps/v8/src/libsampler/sampler.cc	2021-07-26 22:45:01.614281000 +0200
+@@ -476,7 +476,7 @@
+   state->fp =
+       reinterpret_cast<void*>(arm_thread_state64_get_fp(mcontext->__ss));
+ #endif  // V8_HOST_ARCH_*
 -#elif V8_OS_FREEBSD
 +#elif V8_OS_FREEBSD || __DragonFly__
  #if V8_HOST_ARCH_IA32
