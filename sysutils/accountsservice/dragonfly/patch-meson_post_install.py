6b863ba69f80e1466f790611c8f3f697aa82cf80
DPorts Builder <nobody@home.ok>
2021-07-29 16:59:29 -0700

    Tweak sysutils/accountsservice version 0.6.55

--- sysutils/accountsservice/dragonfly/patch-meson_post_install.py
+++ sysutils/accountsservice/dragonfly/patch-meson_post_install.py
@@ -1,8 +1,8 @@
---- meson_post_install.py.orig	2021-06-10 09:33:16.194173000 +0200
-+++ meson_post_install.py	2021-06-10 09:33:42.433585000 +0200
+--- meson_post_install.py.orig	2021-07-29 19:16:51.295622000 +0200
++++ meson_post_install.py	2021-07-29 19:16:59.705396000 +0200
 @@ -1,4 +1,4 @@
 -#!/usr/bin/env python3
-+#!/usr/bin/env python3.7
++#!/usr/bin/env python3.8
  
  import os
  import sys
