e2c00eaa02c19aa0e639980474c9d48980fd0518
DPorts Builder <nobody@home.ok>
2021-07-29 17:01:42 -0700

    Update security/R-cran-openssl to version 1.4.4

--- security/R-cran-openssl/dragonfly/patch-src_ssl.c
+++ security/R-cran-openssl/dragonfly/patch-src_ssl.c
@@ -1,12 +1,13 @@
-Expose PF_UNSPEC
-
---- src/ssl.c.orig	2020-07-01 13:15:34 UTC
-+++ src/ssl.c
-@@ -1,10 +1,10 @@
+--- src/ssl.c.orig	2021-07-29 21:21:04.704570000 +0200
++++ src/ssl.c	2021-07-29 21:21:38.593792000 +0200
+@@ -1,5 +1,5 @@
  //getaddrinfo is an extension (not C99)
 -#if !defined(_WIN32) && !defined(__sun) && !defined(_POSIX_C_SOURCE)
 +#if !defined(_WIN32) && !defined(__sun) && !defined(_POSIX_C_SOURCE) && !defined(__DragonFly__)
  #define _POSIX_C_SOURCE 200112L
+ #endif
+ 
+@@ -9,7 +9,7 @@
  #endif
  
  //see https://github.com/jeroen/openssl/issues/41
