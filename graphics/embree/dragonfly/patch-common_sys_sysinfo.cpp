a044786128535731bcd6f097837833831f67ce08
DPorts Builder <nobody@home.ok>
2021-09-09 11:25:14 -0700

    Update graphics/embree to version 3.13.0_1

--- graphics/embree/dragonfly/patch-common_sys_sysinfo.cpp
+++ graphics/embree/dragonfly/patch-common_sys_sysinfo.cpp
@@ -1,6 +1,6 @@
---- common/sys/sysinfo.cpp.orig	2020-02-04 08:01:09 UTC
+--- common/sys/sysinfo.cpp.orig	2021-05-08 12:26:07 UTC
 +++ common/sys/sysinfo.cpp
-@@ -18,6 +18,9 @@
+@@ -5,6 +5,9 @@
  #include "intrinsics.h"
  #include "string.h"
  #include "ref.h"
@@ -10,16 +10,16 @@
  #if defined(__FREEBSD__)
  #include <sys/cpuset.h>
  #include <pthread_np.h>
-@@ -38,6 +41,8 @@ namespace embree
+@@ -25,6 +28,8 @@ namespace embree
      return "Linux (32bit)";
- #elif defined(__LINUX__) && defined(__X86_64__)
+ #elif defined(__LINUX__) && defined(__64BIT__)
      return "Linux (64bit)";
 +#elif defined(__DRAGONFLY__) && defined(__X86_64__)
 +    return "DragonFly (64bit)";
- #elif defined(__FREEBSD__) && !defined(__X86_64__)
+ #elif defined(__FREEBSD__) && !defined(__64BIT__)
      return "FreeBSD (32bit)";
- #elif defined(__FREEBSD__) && defined(__X86_64__)
-@@ -474,6 +479,37 @@ namespace embree
+ #elif defined(__FREEBSD__) && defined(__64BIT__)
+@@ -536,6 +541,37 @@ namespace embree
    }
  }
  
