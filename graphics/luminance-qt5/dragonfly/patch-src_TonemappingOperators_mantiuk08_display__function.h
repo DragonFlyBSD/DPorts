a6ed45d06c40428444c25926ffeac284e3f50974
DPorts Builder <nobody@home.ok>
2021-09-09 11:29:22 -0700

    Update graphics/luminance-qt5 to version 2.6.1.1_3

--- graphics/luminance-qt5/dragonfly/patch-src_TonemappingOperators_mantiuk08_display__function.h
+++ graphics/luminance-qt5/dragonfly/patch-src_TonemappingOperators_mantiuk08_display__function.h
@@ -1,10 +1,10 @@
---- src/TonemappingOperators/mantiuk08/display_function.h.orig	2014-01-27 20:20:46 UTC
+--- src/TonemappingOperators/mantiuk08/display_function.h.orig	2021-01-11 09:41:28 UTC
 +++ src/TonemappingOperators/mantiuk08/display_function.h
 @@ -33,6 +33,7 @@
  #define DISPLAY_FUNCTION_H
  
  #include <cstdio>
 +#include <errno.h>
+ #include "../../opthelper.h"
  
- #include "arch/math.h"
- #include "Libpfs/utils/sse.h"
+ #include <Libpfs/exception.h>
