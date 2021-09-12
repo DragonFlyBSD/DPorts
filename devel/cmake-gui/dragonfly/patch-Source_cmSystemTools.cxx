1556df46d2b23fa04663d94ee195984395fbfd47
DPorts Builder <nobody@home.ok>
2021-09-12 16:29:00 -0700

    Update devel/cmake-gui to version 3.20.4

--- devel/cmake-gui/dragonfly/patch-Source_cmSystemTools.cxx
+++ devel/cmake-gui/dragonfly/patch-Source_cmSystemTools.cxx
@@ -1,20 +1,12 @@
- Use default visibility for gettimeofday()
-
---- Source/cmSystemTools.cxx.orig	2020-10-06 12:28:17 UTC
+--- Source/cmSystemTools.cxx.orig	2021-06-14 15:05:49 UTC
 +++ Source/cmSystemTools.cxx
-@@ -1,6 +1,7 @@
- /* Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
-    file Copyright.txt or https://cmake.org/licensing for details.  */
- 
-+#ifndef __DragonFly__
- #if !defined(_WIN32) && !defined(__sun)
- // POSIX APIs are needed
+@@ -6,7 +6,8 @@
+ // NOLINTNEXTLINE(bugprone-reserved-identifier)
  #  define _POSIX_C_SOURCE 200809L
-@@ -9,6 +10,7 @@
- // For isascii
- #  define _XOPEN_SOURCE 700
  #endif
-+#endif
- 
- #include "cmSystemTools.h"
- 
+-#if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__QNX__)
++#if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__QNX__) || \
++    defined(__DragonFly__)
+ // For isascii
+ // NOLINTNEXTLINE(bugprone-reserved-identifier)
+ #  define _XOPEN_SOURCE 700
