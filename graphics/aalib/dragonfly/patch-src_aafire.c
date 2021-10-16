9a6787c1eb8a12d4c37455f26e8404dfa670038a
DPorts Builder <nobody@home.ok>
2021-10-16 10:23:05 -0700

    Tweak graphics/aalib version 1.4.r5_13

--- graphics/aalib/dragonfly/patch-src_aafire.c
+++
@@ -1,8 +0,0 @@
---- src/aafire.c.intermediate	2019-10-29 18:15:44.000000000 +0000
-+++ src/aafire.c
-@@ -1,4 +1,5 @@
- #include <stdio.h>
-+#include <stdlib.h>
- #include "aalib.h"
- 
- #define XSIZ aa_imgwidth(context)
