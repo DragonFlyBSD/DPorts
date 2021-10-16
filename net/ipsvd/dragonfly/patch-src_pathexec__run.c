89a4d551d0f5d9624a89b3061ccd1bd9480ce818
DPorts Builder <nobody@home.ok>
2021-10-16 05:40:54 -0700

    Tweak net/ipsvd version 1.0.0

--- net/ipsvd/dragonfly/patch-src_pathexec__run.c
+++
@@ -1,9 +0,0 @@
---- src/pathexec_run.c.orig	2008-06-15 20:31:14 UTC
-+++ src/pathexec_run.c
-@@ -1,5 +1,6 @@
- /* Public domain. */
- 
-+#include <unistd.h>	/* for execve() */
- #include "error.h"
- #include "stralloc.h"
- #include "str.h"
