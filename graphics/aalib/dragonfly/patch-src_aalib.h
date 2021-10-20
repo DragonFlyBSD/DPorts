2431e2ade26471f8c08aaad6525cf12cb965368a
DPorts Builder <nobody@home.ok>
2021-10-19 19:46:59 -0700

    Tweak graphics/aalib version 1.4.r5_13

---
+++ graphics/aalib/dragonfly/patch-src_aalib.h
@@ -0,0 +1,10 @@
+--- src/aalib.h.intermediate	2019-10-29 18:15:44.000000000 +0000
++++ src/aalib.h
+@@ -23,6 +23,7 @@
+ #ifndef __AALIB_INCLUDED__
+ #define __AALIB_INCLUDED__
+ #include <stdio.h>
++#include <string.h>
+ #ifdef __cplusplus
+ extern "C" {
+ #endif
