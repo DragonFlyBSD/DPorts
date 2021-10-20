e45af8dc2fcd2ee733d25fd0712e872dceb39e17
DPorts Builder <nobody@home.ok>
2021-10-19 18:38:08 -0700

    Tweak textproc/aiksaurus version 1.2.1_3

---
+++ textproc/aiksaurus/dragonfly/patch-base_aiksaurus.t.cpp
@@ -0,0 +1,10 @@
+--- base/aiksaurus.t.cpp.orig	2003-06-27 04:44:38.000000000 +0300
++++ base/aiksaurus.t.cpp
+@@ -22,6 +22,7 @@
+ #include "Aiksaurus.h"
+ #include "AsciiCompare.h"
+ #include "config.h"
++#include <cstdlib>
+ #include <iostream>
+ #include <string>
+ using namespace std;
---
+++ textproc/aiksaurus/dragonfly/patch-gtk_src_Display.h
@@ -0,0 +1,10 @@
+--- gtk/src/Display.h.orig	2002-07-11 12:09:37.000000000 +0200
++++ gtk/src/Display.h
+@@ -5,6 +5,7 @@
+ #include <gtk/gtk.h>
+ #include <vector>
+ #include <string>
++#include <cstring>
+ #include <new>
+ #include "Exception.h"
+ 
