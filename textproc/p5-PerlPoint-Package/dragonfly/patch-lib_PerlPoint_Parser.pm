17991dd638878fd3d53e4ab68e43dfdc48e4522f
DPorts Builder <nobody@home.ok>
2021-10-19 18:02:17 -0700

    Tweak textproc/p5-PerlPoint-Package version 0.45.2_2

---
+++ textproc/p5-PerlPoint-Package/dragonfly/patch-lib_PerlPoint_Parser.pm
@@ -0,0 +1,12 @@
+Should be ok.
+--- lib/PerlPoint/Parser.pm.orig	2007-10-10 21:33:00 UTC
++++ lib/PerlPoint/Parser.pm
+@@ -2122,7 +2122,7 @@ B<Example:>
+                     nonWhitespace    => qr/$patternNlbBackslash(\S)/,
+                     colon            => qr/$patternNlbBackslash(:)/,
+                     namedVarKernel   => qr/\$($patternWUmlauts)/,
+-                    symVarKernel     => qr/\$({($patternWUmlauts)})/,
++                    symVarKernel     => qr/\$(\{($patternWUmlauts)\})/,
+                    );
+   @lexerPatterns{qw(
+                     namedVar
