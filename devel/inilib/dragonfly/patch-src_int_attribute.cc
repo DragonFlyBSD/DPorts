a22ccdf2523fb3fda29df3ea9f3cb5e5fdc2b89f
DPorts Builder <nobody@home.ok>
2021-10-19 17:12:43 -0700

    Tweak devel/inilib version 1.0.7b3

---
+++ devel/inilib/dragonfly/patch-src_int_attribute.cc
@@ -0,0 +1,11 @@
+--- src/int_attribute.cc.orig	2005-03-14 19:22:50.000000000 +0200
++++ src/int_attribute.cc
+@@ -15,6 +15,8 @@
+ //           save state of program
+ 
+ #include <string>
++#include <cstring>
++#include <cstdlib>
+ 
+ #include "int_attribute.h"
+ #include "ini_functions.h"
