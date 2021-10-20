715a9141e78170f0bcec8be069226af0a3ec06b1
DPorts Builder <nobody@home.ok>
2021-10-19 17:41:53 -0700

    Tweak sysutils/ucspi-ipc version 0.67

---
+++ sysutils/ucspi-ipc/dragonfly/patch-src_seek.h
@@ -0,0 +1,9 @@
+--- src/seek.h.orig	1998-06-15 10:53:16 UTC
++++ src/seek.h
+@@ -1,5 +1,6 @@
+ #ifndef SEEK_H
+ #define SEEK_H
++#include <unistd.h>	/* for lseek() */
+ 
+ typedef unsigned long seek_pos;
+ 
