d9598636ddedb42e0361a5542816baec17ac8589
DPorts Builder <nobody@home.ok>
2021-10-19 17:13:59 -0700

    Tweak shells/ksh93 version 93.u_1,2

---
+++ shells/ksh93/dragonfly/patch-src_lib_libast_std_endian.h
@@ -0,0 +1,18 @@
+--- src/lib/libast/std/endian.h.orig	2017-11-09 19:01:26 UTC
++++ src/lib/libast/std/endian.h
+@@ -28,9 +28,15 @@
+ 
+ #include <bytesex.h>
+ 
++#if defined(__DragonFly__)
++#include <sys/endian.h>
++#endif
++
++#ifndef __LITTLE_ENDIAN
+ #define	__LITTLE_ENDIAN	1234
+ #define	__BIG_ENDIAN	4321
+ #define	__PDP_ENDIAN	3412
++#endif
+ 
+ #if defined (__USE_BSD) && !defined(__STRICT_ANSI__)
+ 
