acf6d7599dbf6cbc92b01468702310c998a0f160
DPorts Builder <nobody@home.ok>
2021-10-23 14:37:35 -0700

    Update lang/go to version 1.17.1,1

--- lang/go/dragonfly/patch-src_vendor_golang.org_x_net_route_interface__classic.go
+++ lang/go/dragonfly/patch-src_vendor_golang.org_x_net_route_interface__classic.go
@@ -1,10 +1,12 @@
---- src/vendor/golang.org/x/net/route/interface_classic.go.orig	2021-06-03 17:23:41 UTC
-+++ src/vendor/golang.org/x/net/route/interface_classic.go
-@@ -2,7 +2,7 @@
+--- src/vendor/golang.org/x/net/route/interface_classic.go.ori	2021-10-23 13:53:01.473807000 +0200
++++ src/vendor/golang.org/x/net/route/interface_classic.go	2021-10-23 13:53:15.283508000 +0200
+@@ -2,8 +2,8 @@
  // Use of this source code is governed by a BSD-style
  // license that can be found in the LICENSE file.
  
+-//go:build darwin || dragonfly || netbsd
 -// +build darwin dragonfly netbsd
++//go:build darwin || netbsd
 +// +build darwin netbsd
  
  package route
