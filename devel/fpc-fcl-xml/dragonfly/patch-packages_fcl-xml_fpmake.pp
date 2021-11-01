3ba5ff9624ddccd5ad6cc67875257f5f2f0b518e
DPorts Builder <nobody@home.ok>
2021-11-01 04:23:49 -0700

    Update devel/fpc-fcl-xml to version 3.2.2

--- devel/fpc-fcl-xml/dragonfly/patch-packages_fcl-xml_fpmake.pp
+++ devel/fpc-fcl-xml/dragonfly/patch-packages_fcl-xml_fpmake.pp
@@ -1,11 +1,11 @@
---- packages/fcl-xml/fpmake.pp.orig	2021-01-31 18:11:35.608481000 +0100
-+++ packages/fcl-xml/fpmake.pp	2021-01-31 18:12:15.078632000 +0100
+--- packages/fcl-xml/fpmake.pp.orig	2021-10-28 17:11:26.196136000 +0200
++++ packages/fcl-xml/fpmake.pp	2021-10-28 17:10:12.357806000 +0200
 @@ -21,7 +21,7 @@
-     P.Version:='3.2.0';
+     P.Version:='3.2.2';
      P.Options.Add('-S2h');
      D:=P.Dependencies.Add('fcl-base');
--    D:=P.Dependencies.Add('iconvenc',[linux,darwin,iphonesim,freebsd,haiku,beos,aix]);
-+    D:=P.Dependencies.Add('iconvenc',[linux,darwin,iphonesim,dragonfly,freebsd,haiku,beos,aix]);
+-    D:=P.Dependencies.Add('iconvenc',[linux,darwin,iphonesim,ios,freebsd,haiku,beos,aix]);
++    D:=P.Dependencies.Add('iconvenc',[linux,darwin,iphonesim,ios,dragonfly,freebsd,haiku,beos,aix]);
  
      P.Author := 'Sebastian Guenther, Sergei Gorelkin and FPC development team';
      P.License := 'LGPL with modification, ';
@@ -13,8 +13,8 @@
            AddUnit('dom');
            AddUnit('htmldefs');
          end;
--    T:=P.Targets.AddUnit('xmliconv.pas',[linux,freebsd,darwin,iphonesim,haiku,beos,aix]);
-+    T:=P.Targets.AddUnit('xmliconv.pas',[linux,dragonfly,freebsd,darwin,iphonesim,haiku,beos,aix]);
+-    T:=P.Targets.AddUnit('xmliconv.pas',[linux,freebsd,darwin,iphonesim,ios,haiku,beos,aix]);
++    T:=P.Targets.AddUnit('xmliconv.pas',[linux,dragonfly,freebsd,darwin,iphonesim,ios,haiku,beos,aix]);
        with T.Dependencies do
          begin
            AddUnit('xmlread');
