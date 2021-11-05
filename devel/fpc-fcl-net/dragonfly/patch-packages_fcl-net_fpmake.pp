86b093c2a6038a746bde55b8edcc2995aa53410c
DPorts Builder <nobody@home.ok>
2021-11-05 00:45:41 -0700

    Update devel/fpc-fcl-net to version 3.2.2

--- devel/fpc-fcl-net/dragonfly/patch-packages_fcl-net_fpmake.pp
+++ devel/fpc-fcl-net/dragonfly/patch-packages_fcl-net_fpmake.pp
@@ -1,11 +1,11 @@
---- packages/fcl-net/fpmake.pp.orig	2017-02-04 15:11:06 UTC
-+++ packages/fcl-net/fpmake.pp
-@@ -70,7 +70,7 @@ begin
+--- packages/fcl-net/fpmake.pp.orig	2021-11-02 09:43:28.142741000 +0100
++++ packages/fcl-net/fpmake.pp	2021-11-02 09:43:45.542347000 +0100
+@@ -73,7 +73,7 @@
          end;
      T.ResourceStrings := True;
  
--    T:=P.Targets.AddUnit('cnetdb.pp',[linux,freebsd,solaris]);
-+    T:=P.Targets.AddUnit('cnetdb.pp',[linux,dragonfly,freebsd,solaris]);
+-    T:=P.Targets.AddUnit('cnetdb.pp',[linux,freebsd,solaris,android]);
++    T:=P.Targets.AddUnit('cnetdb.pp',[linux,dragonfly,freebsd,solaris,android]);
  
      P.ExamplePath.Add('examples');
      P.Targets.AddExampleProgram('examples/ip6test.pp');
