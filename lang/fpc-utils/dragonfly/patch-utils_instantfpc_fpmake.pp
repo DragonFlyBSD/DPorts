5c0b0302d43f42ba74a5556db03677863d854305
DPorts Builder <nobody@home.ok>
2021-11-10 16:44:18 -0800

    Update lang/fpc-utils to version 3.2.2

--- lang/fpc-utils/dragonfly/patch-utils_instantfpc_fpmake.pp
+++ lang/fpc-utils/dragonfly/patch-utils_instantfpc_fpmake.pp
@@ -1,11 +1,11 @@
---- utils/instantfpc/fpmake.pp.orig	2015-11-13 23:15:29.000000000 +0200
-+++ utils/instantfpc/fpmake.pp
-@@ -30,7 +30,7 @@ begin
+--- utils/instantfpc/fpmake.pp.orig	2021-11-10 10:37:46.159862000 +0100
++++ utils/instantfpc/fpmake.pp	2021-11-10 10:37:58.399592000 +0100
+@@ -33,7 +33,7 @@
      P.Dependencies.Add('fcl-process');
      P.Options.Add('-S2h');
  
--    P.OSes := [win32,win64,wince,haiku,linux,freebsd,openbsd,netbsd,darwin,iphonesim,solaris,aix];
-+    P.OSes := [win32,win64,wince,haiku,linux,freebsd,openbsd,netbsd,darwin,iphonesim,solaris,aix,dragonfly];
+-    P.OSes := [win32,win64,wince,haiku,linux,freebsd,openbsd,netbsd,darwin,iphonesim,ios,solaris,aix];
++    P.OSes := [win32,win64,wince,haiku,linux,dragonfly,freebsd,openbsd,netbsd,darwin,iphonesim,ios,solaris,aix];
  
      T:=P.Targets.AddProgram('instantfpc.pas');
      T.Dependencies.AddUnit('instantfptools');
