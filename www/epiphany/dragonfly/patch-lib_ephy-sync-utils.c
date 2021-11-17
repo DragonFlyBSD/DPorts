b454138f481fe80f55cb17f5e3f04df284bf6f79
DPorts Builder <nobody@home.ok>
2021-11-17 14:44:13 -0800

    Update www/epiphany to version 41.0

--- www/epiphany/dragonfly/patch-lib_ephy-sync-utils.c
+++ www/epiphany/dragonfly/patch-lib_ephy-sync-utils.c
@@ -1,9 +1,9 @@
---- lib/ephy-sync-utils.c.intermediate	2020-11-14 05:28:17.000000000 +0000
-+++ lib/ephy-sync-utils.c
+--- lib/ephy-sync-utils.c.orig	2021-11-16 13:19:59.580937000 +0100
++++ lib/ephy-sync-utils.c	2021-11-16 13:20:12.170656000 +0100
 @@ -30,7 +30,7 @@
- #include <libsoup/soup.h>
  #include <stdio.h>
  #include <string.h>
+ #include <webkit2/webkit2.h>
 -#if defined(__linux__) || defined(__FreeBSD__)
 +#if defined(__linux__) || defined(__FreeBSD__) || defined(__DragonFly__)
  #include <sys/random.h>
