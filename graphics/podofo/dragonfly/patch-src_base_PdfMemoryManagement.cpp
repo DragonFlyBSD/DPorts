4b988acef09dcbd602a7854fc76be3e63dbba0b4
DPorts Builder <nobody@home.ok>
2021-11-17 14:56:58 -0800

    Update graphics/podofo to version 0.9.7

--- graphics/podofo/dragonfly/patch-src_base_PdfMemoryManagement.cpp
+++ graphics/podofo/dragonfly/patch-src_base_PdfMemoryManagement.cpp
@@ -1,5 +1,5 @@
---- src/base/PdfMemoryManagement.cpp.intermediate	2016-07-08 17:11:03
-+++ src/base/PdfMemoryManagement.cpp
+--- src/podofo/base/PdfMemoryManagement.cpp.intermediate	2016-07-08 17:11:03
++++ src/podofo/base/PdfMemoryManagement.cpp
 @@ -31,6 +31,7 @@
   *   files in the program, then also delete it here.                       *
   ***************************************************************************/
