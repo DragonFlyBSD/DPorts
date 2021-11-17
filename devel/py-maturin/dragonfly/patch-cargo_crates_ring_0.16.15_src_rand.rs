abd2b6a8e4b89d1d1e3a1c1b7d2000e4ee701c5a
DPorts Builder <nobody@home.ok>
2021-11-17 14:59:04 -0800

    Update devel/py-maturin to version 0.11.3

--- devel/py-maturin/dragonfly/patch-cargo_crates_ring_0.16.15_src_rand.rs
+++ devel/py-maturin/dragonfly/patch-cargo_crates_ring_0.16.15_src_rand.rs
@@ -1,5 +1,5 @@
---- cargo-crates/ring-0.16.15/src/rand.rs.orig	2021-06-16 15:05:38.469378000 +0200
-+++ cargo-crates/ring-0.16.15/src/rand.rs	2021-06-16 15:06:53.727659000 +0200
+--- cargo-crates/ring-0.16.20/src/rand.rs.orig	2021-06-16 15:05:38.469378000 +0200
++++ cargo-crates/ring-0.16.20/src/rand.rs	2021-06-16 15:06:53.727659000 +0200
 @@ -157,7 +157,7 @@
  impl sealed::SecureRandom for SystemRandom {
      #[inline(always)]
