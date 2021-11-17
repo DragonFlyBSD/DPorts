725a5bf9c0c7b3c0b7350238179f64c0f2ef4c34
DPorts Builder <nobody@home.ok>
2021-11-17 14:39:56 -0800

    Update devel/aml to version 0.2.1

--- devel/aml/dragonfly/patch-src_kqueue.c
+++ devel/aml/dragonfly/patch-src_kqueue.c
@@ -1,15 +1,18 @@
---- src/kqueue.c.intermediate	2021-08-22 14:55:30 UTC
-+++ src/kqueue.c
-@@ -17,7 +17,7 @@
+--- src/kqueue.c.orig	2021-11-16 14:10:32.570280000 +0100
++++ src/kqueue.c	2021-11-16 14:21:07.946068000 +0100
+@@ -14,6 +14,11 @@
+  * PERFORMANCE OF THIS SOFTWARE.
+  */
+ 
++#if (defined(__FreeBSD__) && __FreeBSD__ < 12) || defined(__DragonFly__)
++#define NOTE_ABSTIME 0
++typedef unsigned short	u_short;
++typedef unsigned int u_int;
++#endif
  #include "aml.h"
  #include "backend.h"
  
--#if defined(__FreeBSD__) && __FreeBSD__ < 12
-+#if (defined(__FreeBSD__) && __FreeBSD__ < 12) || defined(__DragonFly__)
- #define NOTE_ABSTIME 0
- typedef unsigned short	u_short;
- typedef unsigned int u_int;
-@@ -186,7 +186,12 @@ static int kq_set_deadline(void* state,
+@@ -181,7 +186,12 @@
  
  	struct kevent event;
  	EV_SET(&event, 0, EVFILT_TIMER, EV_ADD | EV_ONESHOT,
@@ -19,7 +22,7 @@
 +#else
 +	    0,
 +#endif
-+	    deadline, NULL);
++          deadline, NULL);
  
  	return kevent(self->fd, &event, 1, NULL, 0, NULL);
  }
