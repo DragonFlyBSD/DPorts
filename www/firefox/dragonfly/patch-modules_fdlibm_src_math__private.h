c03120dd248f6adc84e4370e039c0c05ac39b23d
DPorts Builder <nobody@home.ok>
2021-11-27 19:42:22 -0800

    Update www/firefox to version 94.0.2_3,2

--- www/firefox/dragonfly/patch-modules_fdlibm_src_math__private.h
+++ www/firefox/dragonfly/patch-modules_fdlibm_src_math__private.h
@@ -1,10 +1,12 @@
---- modules/fdlibm/src/math_private.h.intermediate	2021-11-21 00:41:32 UTC
+--- modules/fdlibm/src/math_private.h.orig	2021-11-27 00:04:29 UTC
 +++ modules/fdlibm/src/math_private.h
-@@ -32,6 +32,7 @@
+@@ -32,6 +32,9 @@
  
  #ifdef __LP64__
  typedef double __double_t;
++#ifndef __float_t
 +typedef float __float_t;
++#endif
  #else
  typedef long double __double_t;
  #endif
