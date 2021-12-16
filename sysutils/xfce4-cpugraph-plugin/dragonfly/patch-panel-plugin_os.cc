e8eb2a5fcab4d102fba6e78f0e67be83030a8083
DPorts Builder <nobody@home.ok>
2021-12-16 11:46:39 -0800

    Update sysutils/xfce4-cpugraph-plugin to version 1.2.4

--- sysutils/xfce4-cpugraph-plugin/dragonfly/patch-panel-plugin_os.cc
+++ sysutils/xfce4-cpugraph-plugin/dragonfly/patch-panel-plugin_os.cc
@@ -1,6 +1,6 @@
---- panel-plugin/os.c.orig	2021-02-14 17:45:27 UTC
-+++ panel-plugin/os.c
-@@ -52,6 +52,13 @@
+--- panel-plugin/os.cc.orig	2021-10-01 16:44:42 UTC
++++ panel-plugin/os.cc
+@@ -60,6 +60,13 @@
  #include <nlist.h>
  #endif
  
@@ -14,16 +14,16 @@
  #if defined (__NetBSD__)
  #include <sys/param.h>
  #include <sys/sched.h>
-@@ -172,7 +179,7 @@ read_cpu_data (CpuData *data, guint nb_c
-     return TRUE;
+@@ -185,7 +192,7 @@ read_cpu_data (std::vector<CpuData> &dat
+     return true;
  }
  
 -#elif defined (__FreeBSD__)
 +#elif defined (__FreeBSD__) || defined(__DragonFly__)
  guint
- detect_cpu_number (void)
+ detect_cpu_number ()
  {
-@@ -197,10 +204,16 @@ read_cpu_data (CpuData *data, guint nb_c
+@@ -214,10 +221,16 @@ read_cpu_data (std::vector<CpuData> &dat
      gsize len = sizeof (max_cpu);
  
      data[0].load = 0;
@@ -32,11 +32,11 @@
 +#else
      if (sysctlbyname ("kern.smp.maxid", &max_cpu, &len, NULL, 0) < 0)
 +#endif
-         return FALSE;
+         return false;
  
 +#ifndef __DragonFly__
      max_cpu++; /* max_cpu is 0-based */
 +#endif
      if (max_cpu < nb_cpu)
-         return FALSE; /* should not happen */
+         return false; /* should not happen */
      len = sizeof (glong) * max_cpu * CPUSTATES;
