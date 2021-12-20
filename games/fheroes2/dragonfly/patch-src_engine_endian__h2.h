a8e12b26f8775e9d24f427d8e270d478dcb0876b
DPorts Builder <nobody@home.ok>
2021-12-20 03:35:35 -0800

    Update games/fheroes2 to version 0.9.7

--- games/fheroes2/dragonfly/patch-src_engine_endian__h2.h
+++ games/fheroes2/dragonfly/patch-src_engine_endian__h2.h
@@ -1,11 +1,11 @@
---- src/engine/endian_h2.h.orig	2021-02-04 12:27:26 UTC
+--- src/engine/endian_h2.h.orig	2021-09-05 13:37:15 UTC
 +++ src/engine/endian_h2.h
 @@ -24,7 +24,7 @@
- #if defined( __linux__ ) || defined( __MINGW32__ )
+ #if defined( __linux__ )
  #include <endian.h>
  
--#elif defined( __FreeBSD__ )
-+#elif defined( __FreeBSD__ ) || defined(__DragonFly__)
+-#elif defined( __FreeBSD__ ) || defined( __OpenBSD__ )
++#elif defined( __FreeBSD__ ) || defined( __OpenBSD__ ) || defined(__DragonFly__)
  #include <sys/endian.h>
  
  #elif defined( _WIN32 ) || defined( _WIN64 )
