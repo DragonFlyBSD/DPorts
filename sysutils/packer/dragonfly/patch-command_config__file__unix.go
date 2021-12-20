3ebd4252a7a65e42d920923aea16b76fdb2dd019
DPorts Builder <nobody@home.ok>
2021-12-20 05:59:10 -0800

    Update sysutils/packer to version 1.7.4_1

---
+++ sysutils/packer/dragonfly/patch-command_config__file__unix.go
@@ -0,0 +1,8 @@
+--- command/config_file_unix.go.orig	1979-11-30 00:00:00 UTC
++++ command/config_file_unix.go
+@@ -1,4 +1,4 @@
+-// +build darwin freebsd linux netbsd openbsd solaris
++// +build darwin freebsd linux netbsd openbsd solaris dragonfly
+ 
+ package command
+ 
---
+++ sysutils/packer/dragonfly/patch-vendor_github.com_hashicorp_packer-plugin-sdk_pathing_config__file__unix.go
@@ -0,0 +1,8 @@
+--- vendor/github.com/hashicorp/packer-plugin-sdk/pathing/config_file_unix.go.orig	2021-12-20 11:25:55 UTC
++++ vendor/github.com/hashicorp/packer-plugin-sdk/pathing/config_file_unix.go
+@@ -1,4 +1,4 @@
+-// +build darwin freebsd linux netbsd openbsd solaris
++// +build darwin freebsd linux netbsd openbsd solaris dragonfly
+ 
+ package pathing
+ 
