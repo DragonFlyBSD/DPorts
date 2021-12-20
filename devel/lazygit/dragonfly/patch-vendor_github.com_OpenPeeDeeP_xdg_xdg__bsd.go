e4b85fa9d6c6491389a9c5d80ae9beff097c9415
DPorts Builder <nobody@home.ok>
2021-12-20 10:01:05 -0800

    Update devel/lazygit to version 0.29

---
+++ devel/lazygit/dragonfly/patch-vendor_github.com_OpenPeeDeeP_xdg_xdg__bsd.go
@@ -0,0 +1,8 @@
+--- vendor/github.com/OpenPeeDeeP/xdg/xdg_bsd.go.orig	2021-08-17 08:10:22 UTC
++++ vendor/github.com/OpenPeeDeeP/xdg/xdg_bsd.go
+@@ -1,4 +1,4 @@
+-// +build freebsd openbsd netbsd
++// +build freebsd openbsd netbsd dragonfly
+ 
+ // Copyright (c) 2017, OpenPeeDeeP. All rights reserved.
+ // Use of this source code is governed by a BSD-style
