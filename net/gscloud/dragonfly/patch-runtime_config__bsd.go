ff6ab90a9f7337cc3a566939ff35fbff0f3e7121
DPorts Builder <nobody@home.ok>
2021-12-20 10:00:33 -0800

    Update net/gscloud to version 0.10.0

---
+++ net/gscloud/dragonfly/patch-runtime_config__bsd.go
@@ -0,0 +1,8 @@
+--- runtime/config_bsd.go.orig	2021-04-06 13:40:32 UTC
++++ runtime/config_bsd.go
+@@ -1,4 +1,4 @@
+-// +build freebsd openbsd
++// +build freebsd openbsd dragonfly
+ 
+ package runtime
+ 
