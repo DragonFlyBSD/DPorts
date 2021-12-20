d56b90c6e80c4212010a77d7aea3c398de5e0c81
DPorts Builder <nobody@home.ok>
2021-12-20 06:09:29 -0800

    Update devel/stlink to version 1.7.0

--- devel/stlink/dragonfly/patch-cmake_modules_Findlibusb.cmake
+++ devel/stlink/dragonfly/patch-cmake_modules_Findlibusb.cmake
@@ -1,11 +1,11 @@
---- cmake/modules/Findlibusb.cmake.orig	2020-05-31 22:00:20 UTC
+--- cmake/modules/Findlibusb.cmake.orig	2021-04-24 22:24:02 UTC
 +++ cmake/modules/Findlibusb.cmake
 @@ -27,7 +27,7 @@ if (APPLE)
          message(FATAL_ERROR "No libusb library found on your system! Install libusb-1.0 from Homebrew or MacPorts")
      endif ()
  
--elseif (CMAKE_SYSTEM_NAME STREQUAL "FreeBSD")                                   # FreeBSD; libusb is integrated into the system
-+elseif (CMAKE_SYSTEM_NAME MATCHES "FreeBSD|DragonFly")                          # FreeBSD; libusb is integrated into the system
+-elseif (CMAKE_SYSTEM_NAME STREQUAL "FreeBSD")              # FreeBSD; libusb is integrated into the system
++elseif (CMAKE_SYSTEM_NAME MATCHES "FreeBSD|DragonFly")              # FreeBSD; libusb is integrated into the system
      FIND_PATH(
          LIBUSB_INCLUDE_DIR NAMES libusb.h
          HINTS /usr/include
--- devel/stlink/dragonfly/patch-src_stlink-lib_libusb__settings.h
+++ devel/stlink/dragonfly/patch-src_stlink-lib_libusb__settings.h
@@ -1,5 +1,5 @@
---- src/libusb_settings.h.orig	2020-05-31 22:00:20 UTC
-+++ src/libusb_settings.h
+--- src/stlink-lib/libusb_settings.h.orig	2021-04-24 22:24:02 UTC
++++ src/stlink-lib/libusb_settings.h
 @@ -39,8 +39,14 @@
      #define MINIMAL_API_VERSION 0x01000104 // v1.0.20
  #endif
@@ -9,9 +9,9 @@
 +#endif
 +
 +#ifndef __DragonFly__
- #if ( LIBUSB_API_VERSION < MINIMAL_API_VERSION )
+ #if (LIBUSB_API_VERSION < MINIMAL_API_VERSION)
      #error unsupported libusb version
  #endif
 +#endif
  
- #endif // STLINKUSB_H
+ #endif // LIBUSB_SETTINGS_H
