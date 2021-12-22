57b969c1f5ad0602827fdf6029bc5b2abfba034a
DPorts Builder <nobody@home.ok>
2021-12-22 08:21:42 -0800

    Update net/ipxe to version g20210727,1

--- net/ipxe/dragonfly/patch-util_elf2efi.c
+++ net/ipxe/dragonfly/patch-util_elf2efi.c
@@ -1,38 +1,32 @@
---- util/elf2efi.c.orig	2018-07-09 15:32:32 UTC
+--- util/elf2efi.c.orig	2021-07-27 12:50:36 UTC
 +++ util/elf2efi.c
-@@ -612,27 +612,35 @@ static void process_reloc ( struct elf_f
- 		 */
- 	} else {
- 		switch ( mrel ) {
-+#ifndef __DragonFly__
- 		case ELF_MREL ( EM_386, R_386_NONE ) :
- 		case ELF_MREL ( EM_ARM, R_ARM_NONE ) :
+@@ -125,6 +125,29 @@
+ #define R_ARM_V4BX 40
+ #endif
+ 
++#ifndef R_386_NONE
++#define R_386_NONE 0
 +#endif
- 		case ELF_MREL ( EM_X86_64, R_X86_64_NONE ) :
- 		case ELF_MREL ( EM_AARCH64, R_AARCH64_NONE ) :
- 		case ELF_MREL ( EM_AARCH64, R_AARCH64_NULL ) :
- 			/* Ignore dummy relocations used by REQUIRE_SYMBOL() */
- 			break;
-+#ifndef __DragonFly__
- 		case ELF_MREL ( EM_386, R_386_32 ) :
- 		case ELF_MREL ( EM_ARM, R_ARM_ABS32 ) :
- 			/* Generate a 4-byte PE relocation */
- 			generate_pe_reloc ( pe_reltab, offset, 4 );
- 			break;
++#ifndef R_386_32
++#define R_386_32 1
 +#endif
- 		case ELF_MREL ( EM_X86_64, R_X86_64_64 ) :
- 		case ELF_MREL ( EM_AARCH64, R_AARCH64_ABS64 ) :
- 			/* Generate an 8-byte PE relocation */
- 			generate_pe_reloc ( pe_reltab, offset, 8 );
- 			break;
-+#ifndef __DragonFly__
- 		case ELF_MREL ( EM_386, R_386_PC32 ) :
++#ifndef R_386_PC32
++#define R_386_PC32 2
 +#endif
- 		case ELF_MREL ( EM_ARM, R_ARM_CALL ) :
-+#ifndef __DragonFly__
- 		case ELF_MREL ( EM_ARM, R_ARM_REL32 ) :
- 		case ELF_MREL ( EM_ARM, R_ARM_THM_PC22 ) :
++
++#ifndef R_ARM_NONE
++#define R_ARM_NONE 0
 +#endif
- 		case ELF_MREL ( EM_ARM, R_ARM_THM_JUMP24 ) :
- 		case ELF_MREL ( EM_ARM, R_ARM_V4BX ):
- 		case ELF_MREL ( EM_X86_64, R_X86_64_PC32 ) :
++#ifndef R_ARM_ABS32
++#define R_ARM_ABS32 2
++#endif
++#ifndef R_ARM_REL32
++#define R_ARM_REL32 3
++#endif
++#ifndef R_ARM_THM_PC22
++#define R_ARM_THM_PC22 10
++#endif
++
+ /**
+  * Alignment of raw data of sections in the image file
+  *
