01e86cfb5fdacf226863ed9abc306d8585e31b24
DPorts Builder <nobody@home.ok>
2021-12-24 11:56:18 -0800

    Update security/softether5 to version 5.02.5180,2

---
+++ security/softether5/dragonfly/patch-src_Mayaqua_Network.c
@@ -0,0 +1,18 @@
+--- src/Mayaqua/Network.c.orig	2021-06-24 08:28:36 UTC
++++ src/Mayaqua/Network.c
+@@ -41,10 +41,14 @@
+ #include <sys/event.h>
+ #endif
+ 
++#ifdef __DragonFly__
++#include <sys/socket.h> /* for struct sockaddr */
++#endif
++
+ #ifdef UNIX
+ #ifdef UNIX_SOLARIS
+ #define USE_STATVFS
+-#include <sys/statvfs.h>'
++#include <sys/statvfs.h>
+ #else
+ #define MAYAQUA_SUPPORTS_GETIFADDRS
+ #include <ifaddrs.h>
---
+++ security/softether5/dragonfly/patch-src_Mayaqua_Network.h
@@ -0,0 +1,13 @@
+--- src/Mayaqua/Network.h.orig	2021-06-24 08:28:36 UTC
++++ src/Mayaqua/Network.h
+@@ -19,6 +19,10 @@
+ #endif
+ #endif
+ 
++#ifdef __DragonFly__
++#include <sys/socket.h> /* for struct sockaddr */
++#endif
++
+ // Dynamic Value
+ struct DYN_VALUE
+ {
