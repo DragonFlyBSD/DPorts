c3cdbcf596b97b4cbff73a9b0f466504664765b8
DPorts Builder <nobody@home.ok>
2021-12-27 05:54:57 -0800

    Update security/gvmd to version 21.4.3

--- security/gvmd/dragonfly/patch-src_utils.c
+++ security/gvmd/dragonfly/patch-src_utils.c
@@ -1,11 +1,11 @@
---- src/utils.c.intermediate	2021-03-10 15:47:49.000000000 +0000
+--- src/utils.c.intermediate	2021-12-27 15:47:49.000000000 +0000
 +++ src/utils.c
-@@ -422,7 +422,7 @@ iso_time_internal (time_t *epoch_time, c
-   tm = localtime (epoch_time);
-   if (tm == NULL)
+@@ -432,7 +432,7 @@ iso_time_internal (time_t *epoch_time, c
+ 
+   if (localtime_r (epoch_time, &tm) == NULL)
      return NULL;
 -#ifdef __FreeBSD__
 +#if defined(__FreeBSD__) || defined(__DragonFly__)
-   if (tm->tm_gmtoff == 0)
+   if (tm.tm_gmtoff == 0)
  #else
    if (timezone == 0)
