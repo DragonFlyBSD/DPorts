9051d7a1865a15804975009686e541c3e5428567
DPorts Builder <nobody@home.ok>
2021-12-27 02:31:32 -0800

    Tweak www/mod_auth_xradius version 0.4.6_2

--- www/mod_auth_xradius/dragonfly/patch-libradius_radlib.c
+++ www/mod_auth_xradius/dragonfly/patch-libradius_radlib.c
@@ -9,6 +9,71 @@
  static char *strsep(char **stringp, const char *delim)
  {
      char *s;
+@@ -166,19 +166,19 @@ insert_message_authenticator(struct xrad
+ 	u_char md[EVP_MAX_MD_SIZE];
+ 	u_int md_len;
+ 	const struct xrad_server *srvp;
+-	HMAC_CTX ctx;
++	HMAC_CTX *ctx;
+ 	srvp = &h->servers[srv];
+ 
+ 	if (h->authentic_pos != 0) {
+-		HMAC_CTX_init(&ctx);
+-		HMAC_Init(&ctx, srvp->secret, strlen(srvp->secret), EVP_md5());
+-		HMAC_Update(&ctx, &h->request[POS_CODE], POS_AUTH - POS_CODE);
+-		HMAC_Update(&ctx, &h->request[POS_AUTH], LEN_AUTH);
+-		HMAC_Update(&ctx, &h->request[POS_ATTRS],
++		ctx = HMAC_CTX_new();
++		HMAC_CTX_reset(ctx);
++		HMAC_Init(ctx, srvp->secret, strlen(srvp->secret), EVP_md5());
++		HMAC_Update(ctx, &h->request[POS_CODE], POS_AUTH - POS_CODE);
++		HMAC_Update(ctx, &h->request[POS_AUTH], LEN_AUTH);
++		HMAC_Update(ctx, &h->request[POS_ATTRS],
+ 		    h->req_len - POS_ATTRS);
+-		HMAC_Final(&ctx, md, &md_len);
+-		HMAC_CTX_cleanup(&ctx);
+-		HMAC_cleanup(&ctx);
++		HMAC_Final(ctx, md, &md_len);
++		HMAC_CTX_free(ctx);
+ 		memcpy(&h->request[h->authentic_pos + 2], md, md_len);
+ 	}
+ #endif
+@@ -197,7 +197,7 @@ is_valid_response(struct xrad_handle *h,
+ 	const struct xrad_server *srvp;
+ 	int len;
+ #ifdef WITH_SSL
+-	HMAC_CTX hctx;
++	HMAC_CTX *hctx;
+ 	u_char resp[MSGSIZE], md[EVP_MAX_MD_SIZE];
+ 	int pos, md_len;
+ #endif
+@@ -244,18 +244,18 @@ is_valid_response(struct xrad_handle *h,
+ 				/* zero fill the Message-Authenticator */
+ 				memset(&resp[pos + 2], 0, MD5_DIGEST_LENGTH);
+ 
+-				HMAC_CTX_init(&hctx);
+-				HMAC_Init(&hctx, srvp->secret,
++				hctx = HMAC_CTX_new();
++				HMAC_CTX_reset(hctx);
++				HMAC_Init(hctx, srvp->secret,
+ 				    strlen(srvp->secret), EVP_md5());
+-				HMAC_Update(&hctx, &h->response[POS_CODE],
++				HMAC_Update(hctx, &h->response[POS_CODE],
+ 				    POS_AUTH - POS_CODE);
+-				HMAC_Update(&hctx, &h->request[POS_AUTH],
++				HMAC_Update(hctx, &h->request[POS_AUTH],
+ 				    LEN_AUTH);
+-				HMAC_Update(&hctx, &resp[POS_ATTRS],
++				HMAC_Update(hctx, &resp[POS_ATTRS],
+ 				    h->resp_len - POS_ATTRS);
+-				HMAC_Final(&hctx, md, &md_len);
+-				HMAC_CTX_cleanup(&hctx);
+-				HMAC_cleanup(&hctx);
++				HMAC_Final(hctx, md, &md_len);
++				HMAC_CTX_free(hctx);
+ 				if (memcmp(md, &h->response[pos + 2],
+ 				    MD5_DIGEST_LENGTH) != 0)
+ 					return 0;
 @@ -329,7 +329,7 @@ xrad_add_server(struct xrad_handle *h, c
  	srvp = &h->servers[h->num_servers];
  
