ded0c60a7cf1c6ddfe18e6e0dd6b9a579203d176
DPorts Builder <nobody@home.ok>
2021-12-31 02:10:43 -0800

    Tweak multimedia/snapcast version 0.25.0

--- multimedia/snapcast/dragonfly/patch-common_utils.hpp
+++ multimedia/snapcast/dragonfly/patch-common_utils.hpp
@@ -1,14 +1,16 @@
 --- common/utils.hpp.orig	2021-05-15 11:16:03 UTC
 +++ common/utils.hpp
-@@ -334,7 +334,11 @@ static std::string getMacAddress(int soc
+@@ -334,7 +334,13 @@ static std::string getMacAddress(int soc
  #endif
  
  #ifdef FREEBSD
-+#ifdef __DragonFly__
-+                if (0) /* XXX */
-+#else
++# if defined(SIOCGIFMAC)
                  if (ioctl(sock, SIOCGIFMAC, &ifr) == 0)
-+#endif
++# elif defined(SIOCGHWADDR)
++                if (ioctl(sock, SIOCGHWADDR, &ifr) == 0)
++# else
++                if (0) /* XXX */
++# endif
  #else
                  if (ioctl(sock, SIOCGIFHWADDR, &ifr) == 0)
  #endif
