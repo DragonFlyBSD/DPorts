271a8a61f00d2a26c75ff08ee65e82b86d5d2eba
DPorts Builder <nobody@home.ok>
2022-01-19 18:00:07 -0800

    Update lang/rust to version 1.57.0

---
+++ lang/rust/files/patch-library_backtrace_src_backtrace_mod.rs
@@ -0,0 +1,11 @@
+--- library/backtrace/src/backtrace/mod.rs.orig	2021-11-29 19:27:28 UTC
++++ library/backtrace/src/backtrace/mod.rs
+@@ -138,6 +138,8 @@ cfg_if::cfg_if! {
+                 unix,
+                 not(target_os = "emscripten"),
+                 not(all(target_os = "ios", target_arch = "arm")),
++                not(all(target_os = "freebsd", target_arch = "arm")),
++                not(all(target_os = "freebsd", target_arch = "riscv64")),
+             ),
+             all(
+                 target_env = "sgx",
