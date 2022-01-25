39319e1f01d8c216c7c1afcb425852b61dd3f216
DPorts Builder <nobody@home.ok>
2022-01-25 04:40:20 -0800

    Update www/node to version 17.0.1

--- www/node/dragonfly/patch-deps_v8_src_trap-handler_handler-inside-posix.cc
+++ www/node/dragonfly/patch-deps_v8_src_trap-handler_handler-inside-posix.cc
@@ -1,20 +1,20 @@
---- deps/v8/src/trap-handler/handler-inside-posix.cc.orig	2021-07-08 13:39:02.013835000 +0200
-+++ deps/v8/src/trap-handler/handler-inside-posix.cc	2021-07-08 19:07:18.723500000 +0200
+--- deps/v8/src/trap-handler/handler-inside-posix.cc.orig	2021-10-26 12:00:56 UTC
++++ deps/v8/src/trap-handler/handler-inside-posix.cc
 @@ -27,7 +27,7 @@
  
  #include <signal.h>
  
 -#if defined(V8_OS_LINUX) || defined(V8_OS_FREEBSD)
-+#if defined(V8_OS_LINUX) || defined(V8_OS_FREEBSD) || defined(V8_OS_DRAGONFLYBSD)
++#if defined(V8_OS_LINUX) || defined(V8_OS_FREEBSD) || defined(__DragonFly__)
  #include <ucontext.h>
  #elif V8_OS_MACOSX
  #include <sys/ucontext.h>
-@@ -114,6 +114,8 @@
-     auto* context_ip = &uc->uc_mcontext->__ss.__rip;
- #elif V8_OS_FREEBSD && V8_TARGET_ARCH_X64
-     auto* context_ip = &uc->uc_mcontext.mc_rip;
-+#elif V8_OS_DRAGONFLYBSD && V8_TARGET_ARCH_X64
-+    auto* context_ip = &uc->uc_mcontext.mc_rip;
+@@ -51,7 +51,7 @@ namespace trap_handler {
+ #define CONTEXT_REG(reg, REG) &uc->uc_mcontext.gregs[REG_##REG]
+ #elif V8_OS_MACOSX
+ #define CONTEXT_REG(reg, REG) &uc->uc_mcontext->__ss.__##reg
+-#elif V8_OS_FREEBSD
++#elif V8_OS_FREEBSD || V8_OS_DRAGONFLYBSD
+ #define CONTEXT_REG(reg, REG) &uc->uc_mcontext.mc_##reg
  #else
- #error Unsupported platform
- #endif
+ #error "Unsupported platform."
