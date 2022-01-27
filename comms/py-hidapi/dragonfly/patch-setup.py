16bf96ea5f351d46afda6995da06759092af8b33
DPorts Builder <nobody@home.ok>
2022-01-27 01:03:46 -0800

    Update comms/py-hidapi to version 0.11.0.p2

--- comms/py-hidapi/dragonfly/patch-setup.py
+++ comms/py-hidapi/dragonfly/patch-setup.py
@@ -1,13 +1,13 @@
---- setup.py.intermediate	2020-02-17 11:05:07 UTC
+--- setup.py.intermediate	2022-01-27 06:42:07.000000000 +0000
 +++ setup.py
-@@ -81,8 +81,8 @@ if sys.platform.startswith('win') or sys
+@@ -79,8 +79,8 @@ if sys.platform.startswith("win") or sys
          )
      ]
  
--if 'bsd' in sys.platform:
--    if 'freebsd' in sys.platform:
-+if 'bsd' in sys.platform or 'dragonfly' in sys.platform:
-+    if 'freebsd' in sys.platform or 'dragonfly' in sys.platform:
-         libs = ['usb', 'hidapi']
-         include_dirs_bsd = ['/usr/local/include/hidapi']
+-if "bsd" in sys.platform:
+-    if "freebsd" in sys.platform:
++if "bsd" in sys.platform or "dragonfly" in sys.platform:
++    if "freebsd" in sys.platform or "dragonfly" in sys.platform:
+         libs = ["usb", "hidapi"]
+         include_dirs_bsd = ["/usr/local/include/hidapi"]
      else:
