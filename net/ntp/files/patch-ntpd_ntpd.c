c6b48f170237f0835eb9f4b683f4a5aabd1d97a6
DPorts Builder <nobody@home.ok>
2022-01-28 20:16:02 -0800

    Update net/ntp to version 4.2.8p15_4

--- net/ntp/files/patch-ntpd_ntpd.c
+++ net/ntp/files/patch-ntpd_ntpd.c
@@ -1,20 +1,24 @@
 --- ntpd/ntpd.c.orig	2020-06-23 02:17:48.000000000 -0700
-+++ ntpd/ntpd.c	2021-10-16 07:59:32.497688000 -0700
-@@ -145,7 +145,7 @@
++++ ntpd/ntpd.c	2022-01-26 10:14:00.828563000 -0800
+@@ -145,7 +145,9 @@
  # include <seccomp.h>
  #endif /* LIBSECCOMP and KERN_SECCOMP */
  
 -#ifdef __FreeBSD__
-+#if defined(__FreeBSD_version) && __FreeBSD_version < 1400038
++#if defined(__FreeBSD_version) && \
++    ((__FreeBSD_version > 1400000 && __FreeBSD_version < 1400038) || \
++    __FreeBSD_version < 1300524)
  #include <sys/procctl.h>
  #ifndef PROC_STACKGAP_CTL
  /*
-@@ -438,7 +438,7 @@
+@@ -438,7 +440,9 @@
  	char *argv[]
  	)
  {
 -#   ifdef __FreeBSD__
-+#   if defined(__FreeBSD_version) && __FreeBSD_version < 1400037
++#   if defined(__FreeBSD_version) && \
++       ((__FreeBSD_version > 1400000 && __FreeBSD_version < 1400038) || \
++       __FreeBSD_version < 1300524)
  	{
  		/*
  		 * We Must disable ASLR stack gap on FreeBSD to avoid a
