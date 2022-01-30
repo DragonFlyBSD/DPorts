823fa4aa9ebfa2cc9fc488313a59a2df31586921
DPorts Builder <nobody@home.ok>
2022-01-30 12:15:35 -0800

    Update multimedia/minisatip to version 1.1.52

--- multimedia/minisatip/dragonfly/patch-src_socketworks.c
+++ multimedia/minisatip/dragonfly/patch-src_socketworks.c
@@ -1,13 +1,21 @@
 Use fallback for sendmmsg()
 
---- src/socketworks.c.intermediate	2021-03-09 06:17:10.000000000 +0000
+--- src/socketworks.c.intermediate	2022-01-30 19:27:23.000000000 +0000
 +++ src/socketworks.c
-@@ -1296,7 +1296,7 @@ pthread_t get_socket_thread(int s_id)
+@@ -1217,14 +1217,14 @@ pthread_t get_socket_thread(int s_id) {
  #undef DEFAULT_LOG
  #define DEFAULT_LOG LOG_SOCKET
  
 -#ifdef __APPLE__
 +#if defined(__APPLE__) || defined(__DragonFly__)
- struct mmsghdr
- {
- 	struct msghdr msg_hdr; /* Message header */
+ struct mmsghdr {
+     struct msghdr msg_hdr; /* Message header */
+     unsigned int msg_len;  /* Number of bytes transmitted */
+ };
+ #endif
+ 
+-#if defined(__APPLE__) || defined(__SH4__)
++#if defined(__APPLE__) || defined(__SH4__) || defined(__DragonFly__)
+ int sendmmsg0(int rsock, struct mmsghdr *msg, int len, int t) {
+     int i;
+     for (i = 0; i < len; i++)
