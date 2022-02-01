771dbc98e3e673001b402d2ae65367307b6c70ab
DPorts Builder <nobody@home.ok>
2022-02-01 12:49:38 -0800

    Update dns/powerdns-recursor to version 4.6.0

--- dns/powerdns-recursor/dragonfly/patch-kqueuemplexer.cc
+++ dns/powerdns-recursor/dragonfly/patch-kqueuemplexer.cc
@@ -1,11 +1,11 @@
---- kqueuemplexer.cc.orig	2020-10-13 09:46:03 UTC
+--- kqueuemplexer.cc.orig	2021-11-23 18:39:17 UTC
 +++ kqueuemplexer.cc
 @@ -28,7 +28,7 @@
  #include <unistd.h>
  #include "misc.hh"
  #include <sys/types.h>
--#if defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || defined(__NetBSD__)
-+#if defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || defined(__NetBSD__) || defined(__DragonFly__)
+-#if defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__APPLE__)
++#if defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__APPLE__) || defined(__DragonFly__)
  #include <sys/event.h>
  #endif
  #include <sys/time.h>
