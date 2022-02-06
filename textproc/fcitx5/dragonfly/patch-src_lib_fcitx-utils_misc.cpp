f707859deaae6c49a95d545e66176baa4c3f14e5
DPorts Builder <nobody@home.ok>
2022-02-06 14:01:11 -0800

    Update textproc/fcitx5 to version 5.0.11_1

--- textproc/fcitx5/dragonfly/patch-src_lib_fcitx-utils_misc.cpp
+++ textproc/fcitx5/dragonfly/patch-src_lib_fcitx-utils_misc.cpp
@@ -1,6 +1,15 @@
---- src/lib/fcitx-utils/misc.cpp.orig	2020-12-16 20:24:12 UTC
+--- src/lib/fcitx-utils/misc.cpp.orig	2021-12-04 15:43:05 UTC
 +++ src/lib/fcitx-utils/misc.cpp
-@@ -97,6 +97,8 @@ std::string getProcessName(pid_t pid) {
+@@ -16,7 +16,7 @@
+ #include <kvm.h>
+ #include <sys/param.h>
+ #include <sys/sysctl.h>
+-#if defined(__FreeBSD__)
++#if defined(__FreeBSD__) || defined(__DragonFly__)
+ #include <sys/user.h>
+ #endif
+ #elif defined(__APPLE__)
+@@ -95,6 +95,8 @@ std::string getProcessName(pid_t pid) {
          for (i = 0; i < cnt; i++)
  #if defined(__NetBSD__) || defined(__OpenBSD__)
              if (kp->p_pid == pid)
@@ -9,7 +18,7 @@
  #else
              if (kp->ki_pid == pid)
  #endif
-@@ -104,6 +106,8 @@ std::string getProcessName(pid_t pid) {
+@@ -102,6 +104,8 @@ std::string getProcessName(pid_t pid) {
          if (i != cnt) {
  #if defined(__NetBSD__) || defined(__OpenBSD__)
              result = kp->p_comm;
