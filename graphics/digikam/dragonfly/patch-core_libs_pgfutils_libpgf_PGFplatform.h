a37a9aa04f8e1614613ed3fd131ac651bc7bf434
DPorts Builder <nobody@home.ok>
2022-02-07 07:59:54 -0800

    Update graphics/digikam to version 7.4.0_1

--- graphics/digikam/dragonfly/patch-core_libs_pgfutils_libpgf_PGFplatform.h
+++ graphics/digikam/dragonfly/patch-core_libs_pgfutils_libpgf_PGFplatform.h
@@ -1,4 +1,4 @@
---- core/libs/pgfutils/libpgf/PGFplatform.h.orig	2019-11-03 09:04:00 UTC
+--- core/libs/pgfutils/libpgf/PGFplatform.h.orig	2021-12-10 12:53:43 UTC
 +++ core/libs/pgfutils/libpgf/PGFplatform.h
 @@ -336,7 +336,7 @@ inline OSError SetFPos(HANDLE hFile, int
  //-------------------------------------------------------------------------------
@@ -6,6 +6,6 @@
  //-------------------------------------------------------------------------------
 -#if defined(__NetBSD__) || defined(__OpenBSD__) || defined(__FreeBSD__)
 +#if defined(__NetBSD__) || defined(__OpenBSD__) || defined(__FreeBSD__) || defined(__DragonFly__)
- #ifndef __POSIX__ 
- #define __POSIX__ 
- #endif 
+ #ifndef __POSIX__
+ #define __POSIX__
+ #endif
