5020aa9c592da49fb75c126798f3405c97d729b7
DPorts Builder <nobody@home.ok>
2022-02-09 11:44:50 -0800

    Tweak graphics/gegl version 0.4.34

--- graphics/gegl/dragonfly/patch-gegl_gegl-config.c
+++ graphics/gegl/dragonfly/patch-gegl_gegl-config.c
@@ -1,6 +1,15 @@
---- gegl/gegl-config.c.orig	2019-05-30 09:46:01 UTC
+--- gegl/gegl-config.c.orig	2021-12-18 23:49:45 UTC
 +++ gegl/gegl-config.c
-@@ -271,6 +271,21 @@ gegl_config_class_init (GeglConfigClass
+@@ -44,7 +44,7 @@
+ #include <mach/mach.h>
+ #endif
+ 
+-#if defined(__APPLE__) || defined(__FreeBSD__)
++#if defined(__APPLE__) || defined(__FreeBSD__) || defined(__DragonFly__)
+ #include <sys/types.h>
+ #include <sys/sysctl.h>
+ #endif
+@@ -285,6 +285,21 @@ gegl_config_class_init (GeglConfigClass
                               ) * page_size;
        mach_port_deallocate (mach_task_self (), host);
      }
@@ -20,5 +29,5 @@
 +    };
 +
  #elif defined(__FreeBSD__)
+     bool ok = true;
  
-     size_t len = sizeof(unsigned int);
