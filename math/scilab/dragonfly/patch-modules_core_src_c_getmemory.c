06e6dd8cbe30084b5e8d1d87ef9e3ca16ec58840
DPorts Builder <nobody@home.ok>
2022-02-09 12:04:10 -0800

    Tweak math/scilab version 6.1.1

--- math/scilab/dragonfly/patch-modules_core_src_c_getmemory.c
+++ math/scilab/dragonfly/patch-modules_core_src_c_getmemory.c
@@ -1,6 +1,6 @@
---- modules/core/src/c/getmemory.c.orig	2015-03-31 09:31:23 UTC
+--- modules/core/src/c/getmemory.c.orig	2021-07-15 10:04:11 UTC
 +++ modules/core/src/c/getmemory.c
-@@ -144,7 +144,7 @@ int getfreememory(void)
+@@ -154,7 +154,7 @@ int getfreememory(void)
             fall back to inaccurate sysconf() */
          return (sysconf(_SC_AVPHYS_PAGES) * sysconf(_SC_PAGESIZE)) / kooctet;
      }
@@ -9,7 +9,7 @@
      {
          /* This works on *bsd.  */
          int mib[2];
-@@ -185,7 +185,7 @@ int getfreememory(void)
+@@ -195,10 +195,10 @@ int getfreememory(void)
  
          return 0;
      }
@@ -17,4 +17,8 @@
 +#elif defined(__FreeBSD__) || defined(__DragonFly__)
      int avphys_pages;
      size_t oldlenp = sizeof (avphys_pages);
-     sysctlbyname("vm.stats.vm.v_free_count", &avphys_pages, &oldlenp, NULL, NULL);
+-    sysctlbyname("vm.stats.vm.v_free_count", &avphys_pages, &oldlenp, NULL, NULL);
++    sysctlbyname("vm.stats.vm.v_free_count", &avphys_pages, &oldlenp, NULL, 0);
+     return (avphys_pages / kooctet) * sysconf(_SC_PAGESIZE);
+ 
+ #else
