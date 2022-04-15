de55addede3f9473db9e9dbb45adc224d313cff4
DPorts Builder <nobody@home.ok>
2022-04-15 09:03:16 -0700

    Update lang/php80 to version 8.0.17_3

--- lang/php80/dragonfly/patch-TSRM_TSRM.h
+++ lang/php80/dragonfly/patch-TSRM_TSRM.h
@@ -1,11 +1,12 @@
---- TSRM/TSRM.h.orig	2022-01-18 12:16:19 UTC
+--- TSRM/TSRM.h.orig	2022-03-15 08:24:20 UTC
 +++ TSRM/TSRM.h
-@@ -147,7 +147,7 @@ TSRM_API const char *tsrm_api_name(void)
+@@ -147,7 +147,8 @@ TSRM_API const char *tsrm_api_name(void)
  # define __has_attribute(x) 0
  #endif
  
--#if !__has_attribute(tls_model) || defined(__FreeBSD__) || defined(__MUSL__)
-+#if !__has_attribute(tls_model) || defined(__FreeBSD__) || defined(__MUSL__) || defined(__DragonFly__)
+-#if !__has_attribute(tls_model) || defined(__FreeBSD__) || defined(__MUSL__) || defined(__HAIKU__)
++#if !__has_attribute(tls_model) || defined(__FreeBSD__) || defined(__MUSL__) || defined(__HAIKU__) || \
++	defined(__DragonFly__)
  # define TSRM_TLS_MODEL_ATTR
  #elif __PIC__
  # define TSRM_TLS_MODEL_ATTR __attribute__((tls_model("initial-exec")))
