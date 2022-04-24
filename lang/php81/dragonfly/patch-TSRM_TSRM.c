a576d29a1b9da13942db081cc8de0d6a1db198e5
DPorts Builder <nobody@home.ok>
2022-04-23 18:53:28 -0700

    Update lang/php81 to version 8.1.5

--- lang/php81/dragonfly/patch-TSRM_TSRM.c
+++ lang/php81/dragonfly/patch-TSRM_TSRM.c
@@ -1,11 +1,18 @@
---- TSRM/TSRM.c.orig	2021-12-15 02:00:45 UTC
+--- TSRM/TSRM.c.orig	2022-04-13 02:45:05 UTC
 +++ TSRM/TSRM.c
-@@ -729,7 +729,7 @@ TSRM_API size_t tsrm_get_ls_cache_tcb_of
+@@ -736,13 +736,13 @@ TSRM_API size_t tsrm_get_ls_cache_tcb_of
  #if defined(__APPLE__) && defined(__x86_64__)
      // TODO: Implement support for fast JIT ZTS code ???
  	return 0;
--#elif defined(__x86_64__) && defined(__GNUC__) && !defined(__FreeBSD__) && !defined(__OpenBSD__)
-+#elif defined(__x86_64__) && defined(__GNUC__) && !defined(__FreeBSD__) && !defined(__OpenBSD__) && !defined(__DragonFly__)
+-#elif defined(__x86_64__) && defined(__GNUC__) && !defined(__FreeBSD__) && !defined(__OpenBSD__) && !defined(__MUSL__)
++#elif defined(__x86_64__) && defined(__GNUC__) && !defined(__FreeBSD__) && !defined(__OpenBSD__) && !defined(__MUSL__) && !defined(__DragonFly__)
  	size_t ret;
  
  	asm ("movq _tsrm_ls_cache@gottpoff(%%rip),%0"
+           : "=r" (ret));
+ 	return ret;
+-#elif defined(__i386__) && defined(__GNUC__) && !defined(__FreeBSD__) && !defined(__OpenBSD__) && !defined(__MUSL__)
++#elif defined(__i386__) && defined(__GNUC__) && !defined(__FreeBSD__) && !defined(__OpenBSD__) && !defined(__MUSL__) && !defined(__DragonFly__)
+ 	size_t ret;
+ 
+ 	asm ("leal _tsrm_ls_cache@ntpoff,%0"
