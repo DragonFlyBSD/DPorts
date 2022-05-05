67cdbfa4cce90e5dfc003bd55d1eaeb9db02da06
DPorts Builder <nobody@home.ok>
2022-05-05 16:33:02 -0700

    Update sysutils/py-psutil to version 5.9.0

--- sysutils/py-psutil/dragonfly/patch-psutil___psbsd.py
+++ sysutils/py-psutil/dragonfly/patch-psutil___psbsd.py
@@ -1,10 +1,10 @@
---- psutil/_psbsd.py.orig	2019-06-28 13:13:13 UTC
+--- psutil/_psbsd.py.orig	2021-12-15 09:34:00 UTC
 +++ psutil/_psbsd.py
-@@ -23,6 +23,7 @@ from ._common import memoize
- from ._common import memoize_when_activated
+@@ -19,6 +19,7 @@ from . import _psutil_posix as cext_posi
+ from ._common import FREEBSD
  from ._common import NETBSD
  from ._common import OPENBSD
 +from ._common import DRAGONFLY
- from ._common import usage_percent
- from ._compat import FileNotFoundError
- from ._compat import PermissionError
+ from ._common import AccessDenied
+ from ._common import NoSuchProcess
+ from ._common import ZombieProcess
