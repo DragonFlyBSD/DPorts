04208757da76e88776a00873b8276f7c20a9f203
DPorts Builder <nobody@home.ok>
2022-05-27 09:40:13 -0700

    Update devel/ispc to version 1.17.0_2

--- devel/ispc/dragonfly/patch-src_target__enums.h
+++ devel/ispc/dragonfly/patch-src_target__enums.h
@@ -1,11 +1,11 @@
---- src/target_enums.h.orig	2020-12-19 00:48:42 UTC
+--- src/target_enums.h.orig	2022-01-15 08:35:15 UTC
 +++ src/target_enums.h
-@@ -42,7 +42,7 @@
+@@ -44,7 +44,7 @@ namespace ispc {
  
  enum class CallingConv { uninitialized, defaultcall, x86_vectorcall };
  
--enum class TargetOS { windows, linux, custom_linux, freebsd, macos, android, ios, ps4, web, error };
-+enum class TargetOS { windows, linux, custom_linux, freebsd, dragonfly, macos, android, ios, ps4, web, error };
+-enum class TargetOS { windows, linux, custom_linux, freebsd, macos, android, ios, ps4, ps5, web, error };
++enum class TargetOS { windows, linux, custom_linux, freebsd, dragonfly, macos, android, ios, ps4, ps5, web, error };
  
  TargetOS ParseOS(std::string os);
  std::string OSToString(TargetOS os);
