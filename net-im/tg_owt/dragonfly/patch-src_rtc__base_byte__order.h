a29cced8dcdb68cc773161890de5a1a16c8ec531
DPorts Builder <nobody@home.ok>
2022-06-04 15:03:15 -0700

    Update net-im/tg_owt to version 0.0.20220225

--- net-im/tg_owt/dragonfly/patch-src_rtc__base_byte__order.h
+++ net-im/tg_owt/dragonfly/patch-src_rtc__base_byte__order.h
@@ -1,11 +1,11 @@
---- src/rtc_base/byte_order.h.orig	2021-12-08 15:56:53 UTC
+--- src/rtc_base/byte_order.h.orig	2022-02-25 20:06:20 UTC
 +++ src/rtc_base/byte_order.h
 @@ -90,7 +90,7 @@
  
  #elif defined(WEBRTC_LINUX)
  #include <endian.h>
--#elif defined(WEBRTC_FREEBSD)
-+#elif defined(WEBRTC_FREEBSD) || defined(WEBRTC_DRAGONFLY)
+-#elif defined(WEBRTC_FREEBSD) || defined(WEBRTC_OPENBSD)
++#elif defined(WEBRTC_FREEBSD) || defined(WEBRTC_OPENBSD) || defined(WEBRTC_DRAGONFLY)
  #include <sys/endian.h>
  #else
  #error "Missing byte order functions for this arch."
--- net-im/tg_owt/dragonfly/patch-src_third__party_usrsctp_usrsctplib_usrsctplib_netinet_sctp__os__userspace.h
+++ net-im/tg_owt/dragonfly/patch-src_third__party_usrsctp_usrsctplib_usrsctplib_netinet_sctp__os__userspace.h
@@ -1,9 +1,9 @@
---- src/third_party/usrsctp/usrsctplib/usrsctplib/netinet/sctp_os_userspace.h.orig	2021-02-03 11:42:41 UTC
+--- src/third_party/usrsctp/usrsctplib/usrsctplib/netinet/sctp_os_userspace.h.orig	2022-02-25 20:06:20 UTC
 +++ src/third_party/usrsctp/usrsctplib/usrsctplib/netinet/sctp_os_userspace.h
-@@ -436,6 +436,7 @@ struct sx {int dummy;};
- 
+@@ -409,6 +409,7 @@ struct sx {int dummy;};
  #include <stdio.h>
  #include <string.h>
+ #include <stdbool.h>
 +#include <sys/param.h>	/* For _ALIGN */
  /* #include <sys/param.h>  in FreeBSD defines MSIZE */
  /* #include <sys/ktr.h> */
