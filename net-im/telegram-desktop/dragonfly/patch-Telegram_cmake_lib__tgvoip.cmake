bd55c17cf4f9562d8a9fdc91820853b893d1e7e7
DPorts Builder <nobody@home.ok>
2022-06-04 17:45:29 -0700

    Update net-im/telegram-desktop to version 3.6.1

--- net-im/telegram-desktop/dragonfly/patch-Telegram_cmake_lib__tgvoip.cmake
+++ net-im/telegram-desktop/dragonfly/patch-Telegram_cmake_lib__tgvoip.cmake
@@ -1,15 +1,15 @@
---- Telegram/cmake/lib_tgvoip.cmake.orig	2020-03-16 18:49:16 UTC
+--- Telegram/cmake/lib_tgvoip.cmake.orig	2022-03-16 16:14:50 UTC
 +++ Telegram/cmake/lib_tgvoip.cmake
-@@ -113,12 +113,6 @@ else()
-         os/linux/AudioInputALSA.h
-         os/linux/AudioOutputALSA.cpp
-         os/linux/AudioOutputALSA.h
--        os/linux/AudioOutputPulse.cpp
--        os/linux/AudioOutputPulse.h
--        os/linux/AudioInputPulse.cpp
--        os/linux/AudioInputPulse.h
--        os/linux/AudioPulse.cpp
--        os/linux/AudioPulse.h
+@@ -109,12 +109,6 @@ PRIVATE
+     os/linux/AudioInputALSA.h
+     os/linux/AudioOutputALSA.cpp
+     os/linux/AudioOutputALSA.h
+-    os/linux/AudioOutputPulse.cpp
+-    os/linux/AudioOutputPulse.h
+-    os/linux/AudioInputPulse.cpp
+-    os/linux/AudioInputPulse.h
+-    os/linux/AudioPulse.cpp
+-    os/linux/AudioPulse.h
  
-         # POSIX
-         os/posix/NetworkSocketPosix.cpp
+     # POSIX
+     os/posix/NetworkSocketPosix.cpp
