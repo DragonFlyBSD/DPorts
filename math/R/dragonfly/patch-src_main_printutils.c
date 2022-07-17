dc69ec774e0a03ebbd807895985b957225d37495
DPorts Builder <nobody@home.ok>
2022-07-17 08:19:15 -0700

    Update math/R to version 4.2.1

--- math/R/dragonfly/patch-src_main_printutils.c
+++ math/R/dragonfly/patch-src_main_printutils.c
@@ -1,9 +1,9 @@
---- src/main/printutils.c.ori	2021-10-23 00:05:02.091975000 +0200
-+++ src/main/printutils.c	2021-10-23 00:05:50.090906000 +0200
-@@ -70,7 +70,7 @@
-  * can use our facilities to do so in a UTF-8 locale or system
-  * facilities if the platform tells us that wchar_t is UCS-4 or we
-  * know that about the platform. */
+--- src/main/printutils.c.orig	2022-03-24 23:02:06 UTC
++++ src/main/printutils.c
+@@ -72,7 +72,7 @@
+  * know that about the platform. 
+  * Add __OpenBSD__ and  __NetBSD__ ?
+  */
 -#if !defined(__STDC_ISO_10646__) && (defined(__APPLE__) || defined(__FreeBSD__) || defined(__sun))
 +#if !defined(__STDC_ISO_10646__) && (defined(__APPLE__) || defined(__DragonFly__) || defined(__FreeBSD__) || defined(__sun))
  /* This may not be 100% true (see the comment in rlocale.h),
