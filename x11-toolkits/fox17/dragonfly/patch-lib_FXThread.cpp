e6deb6d1c871afb37bab44e081f15e8083888556
DPorts Builder <nobody@home.ok>
2022-07-29 05:39:59 -0700

    Update x11-toolkits/fox17 to version 1.7.79

--- x11-toolkits/fox17/dragonfly/patch-lib_FXThread.cpp
+++ x11-toolkits/fox17/dragonfly/patch-lib_FXThread.cpp
@@ -1,11 +1,11 @@
---- lib/FXThread.cpp.orig	2021-06-13 14:20:55.066913000 +0200
-+++ lib/FXThread.cpp	2021-06-13 14:22:55.964207000 +0200
-@@ -911,7 +911,7 @@
+--- lib/FXThread.cpp.orig	2022-02-15 03:49:40 UTC
++++ lib/FXThread.cpp
+@@ -911,7 +911,7 @@ FXbool FXThread::description(const FXStr
      return 0<=fxSetThreadDescription((HANDLE)tid,udesc);
  #elif defined(__APPLE__)
      return pthread_setname_np(desc.text())==0;
--#elif defined(__FreeBSD__) && !defined(HAVE_PTHREAD_SETNAME_NP)
-+#elif (defined(__FreeBSD__) || defined(__DragonFly__)) && !defined(HAVE_PTHREAD_SETNAME_NP)
-     pthread_set_name_np((pthread_t)tid,desc.text());
+-#elif defined(__NetBSD__) || defined(__FreeBSD__) || defined(__OpenBSD__)
++#elif defined(__NetBSD__) || defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__DragonFly__)
+     pthread_setname_np((pthread_t)tid,desc.text());
      return true;
- #elif defined(__NetBSD__) || defined(__OpenBSD__)
+ #elif defined(HAVE_PTHREAD_SETNAME_NP)
