6f0e00b0a03fe5e088cf98b1ffe850c6b9b1ce36
DPorts Builder <nobody@home.ok>
2022-08-01 09:44:08 -0700

    Tweak news/newsfetch version 1.21_2

--- news/newsfetch/dragonfly/patch-nntp.c
+++
@@ -1,8 +0,0 @@
---- nntp.c.intermediate	2020-11-24 07:04:21.000000000 +0000
-+++ nntp.c
-@@ -1,4 +1,5 @@
- #include <stdio.h>
-+#include <stdlib.h>
- #include <pwd.h>
- #include <sys/types.h>
- #include <sys/socket.h>
