137c24b117f78aa420ab51fc9f4f89232b43e9ef
DPorts Builder <nobody@home.ok>
2022-08-05 11:43:07 -0700

    Tweak devel/glrparser version 1.4_1

---
+++ devel/glrparser/dragonfly/patch-samples_basic.cc
@@ -0,0 +1,10 @@
+--- samples/basic.cc.orig	2003-03-06 17:50:24.000000000 +0200
++++ samples/basic.cc
+@@ -5,6 +5,7 @@
+ #include <iostream>
+ #include <fstream>
+ #include <strstream>
++#include <cstdlib> // for exit()
+ 
+ using namespace std;
+ 
---
+++ devel/glrparser/dragonfly/patch-samples_glrparse.cc
@@ -0,0 +1,10 @@
+--- samples/glrparse.cc.orig	2003-03-21 13:25:14.000000000 +0200
++++ samples/glrparse.cc
+@@ -7,6 +7,7 @@
+ #include <vector>
+ #include <deque>
+ #include <string>
++#include <cstdlib> // for exit()
+ 
+ #ifdef MAPM_TREES_COUNTING
+ 
