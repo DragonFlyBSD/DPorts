92cb0de32301a7e2c3938a4d19093ac61cf499a7
DPorts Builder <nobody@home.ok>
2022-08-05 11:39:12 -0700

    Tweak news/newsfetch version 1.21_2

---
+++ news/newsfetch/dragonfly/patch-opt.c
@@ -0,0 +1,9 @@
+--- opt.c.intermediate	2020-11-24 07:04:21.000000000 +0000
++++ opt.c
+@@ -1,4 +1,6 @@
+ #include <stdio.h>
++#include <stdlib.h>
++#include <string.h>
+ #include <unistd.h>
+ #include "newsfetch.h"	
+ 
---
+++ news/newsfetch/dragonfly/patch-util.c
@@ -0,0 +1,9 @@
+--- util.c.intermediate	2020-11-24 07:04:21.000000000 +0000
++++ util.c
+@@ -1,4 +1,6 @@
+ #include <stdio.h>
++#include <stdlib.h>
++#include <string.h>
+ #include <sys/types.h>
+ #include <fcntl.h>
+ #include <sys/stat.h>
