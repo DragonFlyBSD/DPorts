cde6aa6c018b0619f9f57a6264d3819dd969db1f
DPorts Builder <nobody@home.ok>
2022-08-05 11:42:07 -0700

    Tweak devel/inilib version 1.0.7b3

---
+++ devel/inilib/dragonfly/patch-src_registry.cc
@@ -0,0 +1,10 @@
+--- src/registry.cc.orig	2005-03-14 19:22:51.000000000 +0200
++++ src/registry.cc
+@@ -18,6 +18,7 @@
+ #include <iostream>
+ #include <fstream>
+ #include <string>
++#include <cstdlib>
+ 
+ #include "Debug.h"
+ #include "ini_getline.h"
