8274df44567c6c1bec2297db7129b92bdb160507
DPorts Builder <nobody@home.ok>
2022-08-15 13:26:17 -0700

    Update math/octave to version 7.1.0_2

--- math/octave/dragonfly/patch-libgnu_cdefs.h
+++ math/octave/dragonfly/patch-libgnu_cdefs.h
@@ -1,11 +1,11 @@
---- libgnu/cdefs.h.orig	2021-06-10 13:38:23.312658000 +0200
-+++ libgnu/cdefs.h	2021-06-10 13:38:56.111909000 +0200
-@@ -321,7 +321,7 @@
-    for portability when this file is used in Gnulib.  */
- #ifndef __nonnull
- # if __GNUC_PREREQ (3,3) || __glibc_clang_has_attribute (__nonnull__)
--#  define __nonnull(params) __attribute__ ((__nonnull__ params))
-+#  define __nonnull(params) __attribute__ ((__nonnull__ (params)))
+--- libgnu/cdefs.h.orig	2022-04-06 14:05:12 UTC
++++ libgnu/cdefs.h
+@@ -380,7 +380,7 @@
+    collision with a different __nonnull in DragonFlyBSD 5.9.  */
+ #ifndef __attribute_nonnull__
+ # if __GNUC_PREREQ (3,3) || __glibc_has_attribute (__nonnull__)
+-#  define __attribute_nonnull__(params) __attribute__ ((__nonnull__ params))
++#  define __attribute_nonnull__(params) __attribute__ ((__nonnull__ (params)))
  # else
- #  define __nonnull(params)
+ #  define __attribute_nonnull__(params)
  # endif
