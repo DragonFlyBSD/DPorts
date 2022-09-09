00641fbf4c3cac2d862d63c24c7a8545887371a8
DPorts Builder <nobody@home.ok>
2022-09-09 16:46:22 -0700

    Update databases/iowow to version 1.4.14

--- databases/iowow/dragonfly/patch-src_log_iwlog.c
+++ databases/iowow/dragonfly/patch-src_log_iwlog.c
@@ -1,4 +1,4 @@
---- src/log/iwlog.c.orig	2021-09-03 09:03:05 UTC
+--- src/log/iwlog.c.orig	2022-02-14 12:24:37 UTC
 +++ src/log/iwlog.c
 @@ -39,7 +39,7 @@
  #include <time.h>
@@ -9,12 +9,3 @@
  #include <libgen.h>
  #elif defined(_WIN32)
  #include <libiberty/libiberty.h>
-@@ -329,7 +329,7 @@ static iwrc _default_logfn(
-     if (!rci) {
-       errno_msg = ebuf;
-     }
--#elif defined(__APPLE__) || defined(__FreeBSD__) || defined(__ANDROID__) || ((  _POSIX_C_SOURCE >= 200112L \
-+#elif defined(__APPLE__) || defined(__FreeBSD__) || defined(__DragonFly__) || defined(__ANDROID__) || ((  _POSIX_C_SOURCE >= 200112L \
-                                                                              || _XOPEN_SOURCE >= 600) && !_GNU_SOURCE)
-     int rci = strerror_r(errno_code, ebuf, EBUF_SZ);
-     if (!rci) {
