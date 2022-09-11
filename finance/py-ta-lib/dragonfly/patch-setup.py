c4fb49b967c4176fb60c3f979e6230b1ce78327f
DPorts Builder <nobody@home.ok>
2022-09-11 12:44:50 -0700

    Update finance/py-ta-lib to version 0.4.24

--- finance/py-ta-lib/dragonfly/patch-setup.py
+++ finance/py-ta-lib/dragonfly/patch-setup.py
@@ -1,11 +1,11 @@
---- setup.py.orig	2018-01-17 21:44:53 UTC
+--- setup.py.orig	2022-01-09 17:33:10 UTC
 +++ setup.py
-@@ -25,7 +25,7 @@ lib_talib_name = 'ta_lib'  # the underly
- runtime_lib_dirs = []
+@@ -22,7 +22,7 @@ lib_talib_name = 'ta_lib'  # the underly
  
  platform_supported = False
--for prefix in ['darwin', 'linux', 'bsd', 'sunos']:
-+for prefix in ['darwin', 'linux', 'dragonfly', 'bsd', 'sunos']:
-     if prefix in sys.platform:
-         platform_supported = True
-         include_dirs = [
+ 
+-if any(s in sys.platform for s in ['darwin', 'linux', 'bsd', 'sunos']):
++if any(s in sys.platform for s in ['darwin', 'linux', 'dragonfly', 'bsd', 'sunos']):
+     platform_supported = True
+     include_dirs = [
+         '/usr/include',
