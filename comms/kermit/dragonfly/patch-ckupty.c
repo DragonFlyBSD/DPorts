1a8da0af1f345b854ddefb1b0f845109286135c7
DPorts Builder <nobody@home.ok>
2022-09-20 01:35:52 -0700

    Update comms/kermit to version 10.0.b04

--- comms/kermit/dragonfly/patch-ckupty.c
+++ comms/kermit/dragonfly/patch-ckupty.c
@@ -1,14 +1,15 @@
---- ckupty.c.orig	2017-04-26 15:57:10 UTC
+--- ckupty.c.orig	2022-06-01 11:52:12 UTC
 +++ ckupty.c
-@@ -54,13 +54,17 @@ char *ckptyv = "Pseudoterminal support,
+@@ -55,7 +55,7 @@ char *ckptyv = "Pseudoterminal support,
     . HP-UX 8.00 and earlier (no vhangup or ptsname routines)
  */
- 
--#ifndef __FreeBSD__			/* bs  20151224 */
+ #ifndef NO_PTY_XOPEN_SOURCE
+-#ifdef __FreeBSD__                      /* bs  20151224 */
 +#if !defined(__FreeBSD__) && !defined(__DragonFly__) /* bs  20151224 */
- #define _XOPEN_SOURCE 500		/* mdw 20140223 */
- #endif /* __FreeBSD__ */		/* bs  20151224 */
- #include <stdlib.h>			/* mdw 20140223 */
+ #define NO_PTY_XOPEN_SOURCE
+ #endif /* __FreeBSD__ */
+ #endif /* NO_PTY_XOPEN_SOURCE */
+@@ -68,6 +68,10 @@ char *ckptyv = "Pseudoterminal support,
  #include "ckcsym.h"
  #include "ckcdeb.h"			/* To pick up NETPTY definition */
  
