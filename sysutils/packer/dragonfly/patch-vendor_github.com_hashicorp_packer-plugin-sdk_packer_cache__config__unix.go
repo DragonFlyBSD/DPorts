8cda8a65e6a13bed300719b633a1220d96d427a3
DPorts Builder <nobody@home.ok>
2022-10-05 10:54:19 -0700

    Update sysutils/packer to version 1.7.10_2

--- sysutils/packer/dragonfly/patch-vendor_github.com_hashicorp_packer-plugin-sdk_packer_cache__config__unix.go
+++ sysutils/packer/dragonfly/patch-vendor_github.com_hashicorp_packer-plugin-sdk_packer_cache__config__unix.go
@@ -1,10 +1,10 @@
---- vendor/github.com/hashicorp/packer-plugin-sdk/pathing/config_file_unix.go.orig	2022-10-03 22:26:07.729319000 +0200
-+++ vendor/github.com/hashicorp/packer-plugin-sdk/pathing/config_file_unix.go	2022-10-03 22:30:42.213095000 +0200
+--- vendor/github.com/hashicorp/packer-plugin-sdk/packer/cache_config_unix.go.orig	2022-10-03 22:28:17.416378000 +0200
++++ vendor/github.com/hashicorp/packer-plugin-sdk/packer/cache_config_unix.go	2022-10-03 22:28:36.475944000 +0200
 @@ -1,5 +1,5 @@
 -//go:build darwin || freebsd || linux || netbsd || openbsd || solaris
 -// +build darwin freebsd linux netbsd openbsd solaris
 +//go:build darwin || dragonfly || freebsd || linux || netbsd || openbsd || solaris
 +// +build darwin dragonfly freebsd linux netbsd openbsd solaris
  
- package pathing
+ package packer
  
