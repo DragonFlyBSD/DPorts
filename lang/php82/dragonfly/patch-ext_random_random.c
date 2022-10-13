2636dc901c17f167bf43285e48b0ab33c3a2de8e
DPorts Builder <nobody@home.ok>
2022-10-13 03:42:57 -0700

    Update lang/php82 to version 8.2.0.r2

--- lang/php82/dragonfly/patch-ext_random_random.c
+++ lang/php82/dragonfly/patch-ext_random_random.c
@@ -1,7 +1,7 @@
---- ext/standard/random.c.orig	2021-12-15 02:00:45 UTC
-+++ ext/standard/random.c
-@@ -31,7 +31,7 @@
- #endif
+--- ext/random/random.c.orig	2022-10-10 00:14:01 UTC
++++ ext/random/random.c
+@@ -48,7 +48,7 @@
+ 
  #if HAVE_SYS_PARAM_H
  # include <sys/param.h>
 -# if (__FreeBSD__ && __FreeBSD_version > 1200000) || (__DragonFly__ && __DragonFly_version >= 500700) || defined(__sun)
@@ -9,12 +9,12 @@
  #  include <sys/random.h>
  # endif
  #endif
-@@ -99,7 +99,7 @@ PHPAPI int php_random_bytes(void *bytes,
+@@ -496,7 +496,7 @@ PHPAPI int php_random_bytes(void *bytes,
  #else
  	size_t read_bytes = 0;
  	ssize_t n;
--#if (defined(__linux__) && defined(SYS_getrandom)) || (defined(__FreeBSD__) && __FreeBSD_version >= 1200000) || (defined(__DragonFly__) && __DragonFly_version >= 500700) || defined(__sun)
-+#if (defined(__linux__) && defined(SYS_getrandom)) || (defined(__FreeBSD__) && __FreeBSD_version >= 1200000) || defined(__DragonFly__) || defined(__sun)
+-# if (defined(__linux__) && defined(SYS_getrandom)) || (defined(__FreeBSD__) && __FreeBSD_version >= 1200000) || (defined(__DragonFly__) && __DragonFly_version >= 500700) || defined(__sun)
++# if (defined(__linux__) && defined(SYS_getrandom)) || (defined(__FreeBSD__) && __FreeBSD_version >= 1200000) || defined(__DragonFly__) || defined(__sun)
  	/* Linux getrandom(2) syscall or FreeBSD/DragonFlyBSD getrandom(2) function*/
  	/* Keep reading until we get enough entropy */
  	while (read_bytes < size) {
