7b20483f0637e0a00c9e359ec6a6f1731e192498
DPorts Builder <nobody@home.ok>
2022-10-14 02:04:46 -0700

    Update editors/emacs to version 28.2,3

--- editors/emacs/dragonfly/extra-gls_lisp_dired.el
+++ editors/emacs/dragonfly/extra-gls_lisp_dired.el
@@ -1,11 +1,11 @@
---- lisp/files.el.orig	2015-04-02 10:23:06.000000000 +0300
+--- lisp/files.el.orig	2022-09-06 21:31:53 UTC
 +++ lisp/files.el
-@@ -6094,7 +6094,7 @@ need to be passed verbatim to shell comm
+@@ -7244,7 +7244,7 @@ need to be passed verbatim to shell comm
        pattern))))
  
  
 -(defvar insert-directory-program (purecopy "ls")
 +(defvar insert-directory-program (purecopy "gls")
-   "Absolute or relative name of the `ls' program used by `insert-directory'.")
- 
- (defcustom directory-free-space-program (purecopy "df")
+   "Absolute or relative name of the `ls'-like program.
+ This is used by `insert-directory' and `dired-insert-directory'
+ \(thus, also by `dired').")
