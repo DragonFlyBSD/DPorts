d127351727e2bf1b012907c5aa53b1cdfba9123e
DPorts Builder <nobody@home.ok>
2022-10-15 17:18:28 -0700

    Update devel/py-scikit-build to version 0.15.0

--- devel/py-scikit-build/dragonfly/patch-tests_test__platform.py
+++ devel/py-scikit-build/dragonfly/patch-tests_test__platform.py
@@ -1,18 +1,18 @@
---- tests/test_platform.py.orig	2022-04-07 21:06:30 UTC
+--- tests/test_platform.py.orig	2022-05-18 17:52:53 UTC
 +++ tests/test_platform.py
 @@ -84,7 +84,7 @@ def test_generator_cleanup():
      assert not os.path.exists(test_folder)
  
  
--@pytest.mark.parametrize("supported_platform", ["darwin", "freebsd", "openbsd", "linux", "windows", "os400"])
-+@pytest.mark.parametrize("supported_platform", ["darwin", "freebsd", "openbsd", "linux", "windows", "os400", "dragonfly"])
+-@pytest.mark.parametrize("supported_platform", ["darwin", "freebsd", "openbsd", "linux", "windows", "os400", "cygwin"])
++@pytest.mark.parametrize("supported_platform", ["darwin", "freebsd", "openbsd", "linux", "windows", "os400", "cygwin", "dragonfly"])
  def test_known_platform(supported_platform, mocker):
      mocker.patch("platform.system", return_value=supported_platform)
      platforms = {
-@@ -94,6 +94,7 @@ def test_known_platform(supported_platfo
-         "darwin": "OSX",
+@@ -95,6 +95,7 @@ def test_known_platform(supported_platfo
          "windows": "Windows",
          "os400": "BSD",
+         "cygwin": "Cygwin",
 +        "dragonfly": "BSD"
      }
      expected_platform_classname = "%sPlatform" % platforms[supported_platform]
