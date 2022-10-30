aa70d4c4c6f5ea7bb8edf1cda9057d9d05035a60
DPorts Builder <nobody@home.ok>
2022-10-30 12:32:38 -0700

    Update x11-wm/i3-gaps to version 4.21

--- x11-wm/i3-gaps/dragonfly/patch-src_log.c
+++ x11-wm/i3-gaps/dragonfly/patch-src_log.c
@@ -1,15 +1,15 @@
---- src/log.c.orig	2014-06-15 17:12:43.000000000 +0000
+--- src/log.c.orig	2022-09-21 18:12:41 UTC
 +++ src/log.c
-@@ -118,7 +118,7 @@ void open_logbuffer(void) {
-                          sysconf(_SC_PAGESIZE);
- #endif
-     logbuffer_size = min(physical_mem_bytes * 0.01, shmlog_size);
+@@ -142,7 +142,7 @@ void open_logbuffer(void) {
+         logbuffer_size = physical_mem_bytes * 0.01;
+     }
+ 
 -#if defined(__FreeBSD__)
 +#if defined(__FreeBSD__) || defined(__DragonFly__)
      sasprintf(&shmlogname, "/tmp/i3-log-%d", getpid());
  #else
      sasprintf(&shmlogname, "/i3-log-%d", getpid());
-@@ -129,7 +129,7 @@ void open_logbuffer(void) {
+@@ -153,7 +153,7 @@ void open_logbuffer(void) {
          return;
      }
  
