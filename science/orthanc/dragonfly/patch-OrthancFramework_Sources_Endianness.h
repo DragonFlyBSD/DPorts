2382e810d4ec22cdfa9ba2a0fe841660f19b0427
DPorts Builder <nobody@home.ok>
2022-10-30 12:09:38 -0700

    Update science/orthanc to version 1.11.0_3

--- science/orthanc/dragonfly/patch-OrthancFramework_Sources_Endianness.h
+++ science/orthanc/dragonfly/patch-OrthancFramework_Sources_Endianness.h
@@ -1,6 +1,6 @@
---- OrthancFramework/Sources/Endianness.h.orig	2021-11-14 16:43:16.301315000 +0100
-+++ OrthancFramework/Sources/Endianness.h	2021-11-14 16:43:43.210716000 +0100
-@@ -83,7 +83,7 @@
+--- OrthancFramework/Sources/Endianness.h.orig	2022-10-29 18:11:24.650580000 +0200
++++ OrthancFramework/Sources/Endianness.h	2022-10-29 18:11:44.680140000 +0200
+@@ -84,9 +84,10 @@
   ** FREEBSD ARCHITECTURES
   ********************************************************************/
  
@@ -8,4 +8,7 @@
 +#if defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || defined(__DragonFly__)
  #  define ORTHANC_HAS_BUILTIN_BYTE_SWAP 1
  #  include <arpa/inet.h>
++#  include <endian.h>
  #endif
+ 
+ 
