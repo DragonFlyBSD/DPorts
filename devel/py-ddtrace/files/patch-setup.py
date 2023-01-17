8170bd41772702f14a2049e7ef20100958e70405
DPorts Builder <nobody@home.ok>
2023-01-17 02:11:56 -0800

    Update devel/py-ddtrace to version 1.6.1

--- devel/py-ddtrace/files/patch-setup.py
+++ devel/py-ddtrace/files/patch-setup.py
@@ -1,7 +1,15 @@
---- setup.py.orig	2022-02-24 22:37:13 UTC
+--- setup.py.orig	2022-11-15 19:39:16 UTC
 +++ setup.py
-@@ -258,7 +257,7 @@ setup(
-         "Programming Language :: Python :: 3.10",
+@@ -260,7 +260,6 @@ setup(
+         "pathlib2; python_version<'3.5'",
+         "jsonschema",
+         "xmltodict>=0.12",
+-        "ipaddress; python_version<'3.7'",
+         "envier",
+     ]
+     + bytecode,
+@@ -296,7 +295,7 @@ setup(
+         "Programming Language :: Python :: 3.11",
      ],
      use_scm_version={"write_to": "ddtrace/_version.py"},
 -    setup_requires=["setuptools_scm[toml]>=4,<6.1", "cython", "cmake", "ninja"],
@@ -9,7 +17,7 @@
      ext_modules=ext_modules
      + cythonize(
          [
-@@ -309,14 +308,6 @@ setup(
+@@ -347,14 +346,6 @@ setup(
                  "ddtrace.profiling._build",
                  sources=["ddtrace/profiling/_build.pyx"],
                  language="c",
