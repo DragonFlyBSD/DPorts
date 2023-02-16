6d4fb12f1eeaef49d8445adfef6352ac48c0c858
DPorts Builder <nobody@home.ok>
2023-02-16 15:20:30 -0800

    Update multimedia/mlt7 to version 7.10.0

--- multimedia/mlt7/dragonfly/patch-src_framework_mlt__property.h
+++ multimedia/mlt7/dragonfly/patch-src_framework_mlt__property.h
@@ -1,16 +1,16 @@
---- src/framework/mlt_property.h.orig	2021-11-14 20:32:12.601853000 +0100
-+++ src/framework/mlt_property.h	2021-11-14 20:32:33.981380000 +0100
+--- src/framework/mlt_property.h.orig	2022-11-01 10:30:41 UTC
++++ src/framework/mlt_property.h
 @@ -25,14 +25,14 @@
  
  #include "mlt_types.h"
  
 -#if defined(__FreeBSD__)
-+#if defined(__FreeBSD__) || defined(__DragonFly__)
++#if defined(__FreeBSD__)  || defined(__DragonFly__)
  /* This header has existed since 1994 and defines __FreeBSD_version below. */
  #include <sys/param.h>
  #endif
  
- #if (defined(__GLIBC__) && !defined(__APPLE__)) || defined(HAVE_LOCALE_H)
+ #if (defined(__linux__) && !defined(__APPLE__))
  #  include <locale.h>
 -#elif defined(__APPLE__) || (defined(__FreeBSD_version) && __FreeBSD_version >= 900506)
 +#elif defined(__APPLE__) || (defined(__FreeBSD_version) && __FreeBSD_version >= 900506) || defined(__DragonFly__)
