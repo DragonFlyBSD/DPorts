09d66e1f0a2f4e9c7d19ef99a4a4e397cc39e3b4
DPorts Builder <nobody@home.ok>
2023-02-20 13:48:34 -0800

    Manual tweak audio/audacity

--- audio/audacity/dragonfly/patch-cmake-proxies_cmake-modules_conan.cmake
+++ audio/audacity/dragonfly/patch-cmake-proxies_cmake-modules_conan.cmake
@@ -1,6 +1,6 @@
---- cmake-proxies/cmake-modules/conan.cmake.orig	2021-12-22 15:35:36 UTC
+--- cmake-proxies/cmake-modules/conan.cmake.orig	2022-12-27 11:53:35 UTC
 +++ cmake-proxies/cmake-modules/conan.cmake
-@@ -91,10 +91,13 @@ macro(_conan_check_system_name)
+@@ -93,10 +93,13 @@ macro(_conan_check_system_name)
          if(${CMAKE_SYSTEM_NAME} STREQUAL "Darwin")
              set(CONAN_SYSTEM_NAME Macos)
          endif()
@@ -9,9 +9,9 @@
 +        endif()
          if(${CMAKE_SYSTEM_NAME} STREQUAL "QNX")
              set(CONAN_SYSTEM_NAME Neutrino)
-         endif()        
--        set(CONAN_SUPPORTED_PLATFORMS Windows Linux Macos Android iOS FreeBSD WindowsStore WindowsCE watchOS tvOS FreeBSD SunOS AIX Arduino Emscripten Neutrino CYGWIN)
-+        set(CONAN_SUPPORTED_PLATFORMS Windows Linux Macos Android iOS FreeBSD WindowsStore WindowsCE watchOS tvOS FreeBSD SunOS AIX Arduino Emscripten Neutrino CYGWIN DragonFly)
+         endif()
+-        set(CONAN_SUPPORTED_PLATFORMS Windows Linux Macos Android iOS FreeBSD WindowsStore WindowsCE watchOS tvOS FreeBSD SunOS AIX Arduino Emscripten Neutrino)
++        set(CONAN_SUPPORTED_PLATFORMS Windows Linux Macos Android iOS FreeBSD WindowsStore WindowsCE watchOS tvOS FreeBSD SunOS AIX Arduino Emscripten Neutrino DragonFly)
          list (FIND CONAN_SUPPORTED_PLATFORMS "${CONAN_SYSTEM_NAME}" _index)
          if (${_index} GREATER -1)
              #check if the cmake system is a conan supported one
