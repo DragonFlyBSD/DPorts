078f6bd037df94929a924e884a2051f2ee23de71
DPorts Builder <nobody@home.ok>
2023-05-05 00:00:42 -0700

    Update math/octave to version 8.1.0

--- math/octave/dragonfly/patch-libinterp_corefcn_sysdep.cc
+++ math/octave/dragonfly/patch-libinterp_corefcn_sysdep.cc
@@ -1,20 +1,20 @@
---- libinterp/corefcn/sysdep.cc.orig	2022-04-06 14:05:12 UTC
-+++ libinterp/corefcn/sysdep.cc
+--- libinterp/corefcn/sysdep.cc.orig	2023-05-03 12:41:23.424689000 +0200
++++ libinterp/corefcn/sysdep.cc	2023-05-03 12:41:48.785113000 +0200
 @@ -105,7 +105,7 @@
  
- OCTAVE_NAMESPACE_BEGIN
+ OCTAVE_BEGIN_NAMESPACE(octave)
  
 -#if defined (__386BSD__) || defined (__FreeBSD__) || defined (__NetBSD__)
-+#if defined (__386BSD__) || defined (__FreeBSD__) || defined (__NetBSD__) || defined (__DragonFly__)
++#if defined (__386BSD__) || defined(__DragonFly__) || defined (__FreeBSD__) || defined (__NetBSD__)
  
-   static void
-   BSD_init (void)
-@@ -515,7 +515,7 @@ On non-Windows platforms, this function
-     omp_get_num_threads ();
+ static void
+ BSD_init (void)
+@@ -515,7 +515,7 @@
+   omp_get_num_threads ();
  #endif
  
 -#if defined (__386BSD__) || defined (__FreeBSD__) || defined (__NetBSD__)
-+#if defined (__386BSD__) || defined (__FreeBSD__) || defined (__NetBSD__) || defined (__DragonFly__)
-     BSD_init ();
++#if defined (__386BSD__) || defined(__DragonFly__) || defined (__FreeBSD__) || defined (__NetBSD__)
+   BSD_init ();
  #elif defined (__MINGW32__)
-     MINGW_init ();
+   MINGW_init ();
