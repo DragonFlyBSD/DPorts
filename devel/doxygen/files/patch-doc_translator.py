e574d0880bbf19d5c5d4d5a10229f5c5db8a8500
DPorts Builder <nobody@home.ok>
2023-05-10 12:05:05 -0700

    Update devel/doxygen to version 1.9.6,2

--- devel/doxygen/files/patch-doc_translator.py
+++ devel/doxygen/files/patch-doc_translator.py
@@ -1,9 +1,9 @@
 Hack to avoid "UnicodeDecodeError: 'utf-8' codec can't decode byte 0xb0 in position 45: invalid start byte"
 in src/._xmlgen.cpp which is binary. This doesn't seem like it should be present at the time the docs
 are generated, but this is simpler for now.
---- doc/translator.py
+--- doc/translator.py.orig	2023-01-07 08:48:38 UTC
 +++ doc/translator.py
-@@ -1454,10 +1454,13 @@ class TrManager:
+@@ -1459,10 +1459,13 @@ class TrManager:
  
          # Read content of the file as one string.
          assert os.path.isfile(fname)
