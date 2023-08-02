db161d672148ca75a665f1dd200d293d4b2fff1e
DPorts Builder <nobody@home.ok>
2023-08-02 06:53:16 -0700

    Update math/py-pandas to version 2.0.2,1

--- math/py-pandas/dragonfly/patch-pandas___libs_window_aggregations.pyx
+++ math/py-pandas/dragonfly/patch-pandas___libs_window_aggregations.pyx
@@ -1,22 +1,16 @@
---- pandas/_libs/window/aggregations.pyx.orig	2022-08-23 17:15:49 UTC
-+++ pandas/_libs/window/aggregations.pyx
-@@ -17,11 +17,18 @@ from numpy cimport (
-     float32_t,
-     float64_t,
-     int64_t,
--    ndarray,
-+    ndarray
- )
+--- pandas/_libs/window/aggregations.pyx.orig	2023-07-31 20:24:46.329909000 +0200
++++ pandas/_libs/window/aggregations.pyx	2023-07-31 23:24:43.998652000 +0200
+@@ -23,6 +23,13 @@ cnp.import_array()
  
- cnp.import_array()
+ import cython
  
 +cdef extern from "../src/headers/cmath" namespace "std":
++
 +    bint isnan(float64_t) nogil
 +    bint notnan(float64_t) nogil
 +    int signbit(float64_t) nogil
 +    float64_t sqrt(float64_t x) nogil
 +
-+
  from pandas._libs.algos import is_monotonic
  
- from pandas._libs.dtypes cimport numeric_t
+ 
