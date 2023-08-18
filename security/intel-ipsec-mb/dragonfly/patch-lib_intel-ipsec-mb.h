ac44d21f6981a2072ca1cf1861739ee4aaf466da
DPorts Builder <nobody@home.ok>
2023-08-18 03:58:20 -0700

    Update security/intel-ipsec-mb to version 1.3

--- security/intel-ipsec-mb/dragonfly/patch-lib_intel-ipsec-mb.h
+++ security/intel-ipsec-mb/dragonfly/patch-lib_intel-ipsec-mb.h
@@ -1,11 +1,11 @@
---- lib/intel-ipsec-mb.h.orig	2020-10-21 22:25:42 UTC
-+++ lib/intel-ipsec-mb.h
+--- lib/intel-ipsec-mb.h.orig	2023-08-14 15:09:38.381071000 +0200
++++ lib/intel-ipsec-mb.h	2023-08-14 15:10:09.401615000 +0200
 @@ -45,7 +45,7 @@ typedef struct {
- /*
+ /**
   * Macros for aligning data structures and function inlines
   */
 -#if defined __linux__ || defined __FreeBSD__
 +#if defined __linux__ || defined __FreeBSD__ || defined __DragonFly__
- /* Linux/FreeBSD */
+ /**< Linux/FreeBSD */
  #define DECLARE_ALIGNED(decl, alignval) \
          decl __attribute__((aligned(alignval)))
