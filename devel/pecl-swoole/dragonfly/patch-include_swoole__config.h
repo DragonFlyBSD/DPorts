91ca624672647fbb6b0b3817d64a30ffe92789a7
DPorts Builder <nobody@home.ok>
2023-10-07 04:22:34 -0700

    Update devel/pecl-swoole to version 5.0.3

--- devel/pecl-swoole/dragonfly/patch-include_swoole__config.h
+++ devel/pecl-swoole/dragonfly/patch-include_swoole__config.h
@@ -1,9 +1,9 @@
---- include/swoole_config.h.orig	2020-08-30 04:51:37 UTC
+--- include/swoole_config.h.orig	2023-05-11 00:27:10 UTC
 +++ include/swoole_config.h
-@@ -43,7 +43,7 @@
+@@ -42,7 +42,7 @@
+ #define SW_MAX_SOCKETS_DEFAULT 1024
  
  #define SW_SOCKET_OVERFLOW_WAIT 100
- #define SW_SOCKET_MAX_DEFAULT 65536
 -#if defined(__MACH__) || defined(__FreeBSD__)
 +#if defined(__MACH__) || defined(__FreeBSD__) || defined(__DragonFly__)
  #define SW_SOCKET_BUFFER_SIZE 262144
