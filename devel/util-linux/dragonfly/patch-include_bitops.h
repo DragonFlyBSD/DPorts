7dd23acf6ef3b091eea42557a9b9853d718f98f1
DPorts Builder <nobody@home.ok>
2023-10-08 07:53:44 -0700

    Tweak devel/util-linux version 2.36_2

--- devel/util-linux/dragonfly/patch-include_bitops.h
+++ devel/util-linux/dragonfly/patch-include_bitops.h
@@ -1,4 +1,4 @@
---- include/bitops.h.orig	2020-07-23 09:26:04 UTC
+--- include/bitops.h.orig	2023-10-07 03:54:17 UTC
 +++ include/bitops.h
 @@ -16,7 +16,8 @@
  
@@ -10,12 +10,3 @@
  #  include <sys/endian.h>
  #endif
  
-@@ -25,7 +26,7 @@
- # define be16toh(x) betoh16(x)
- # define be32toh(x) betoh32(x)
- # define be64toh(x) betoh64(x)
--#elif defined(__NetBSD__) || defined(__FreeBSD__) || defined(__DragonFly__)
-+#elif !defined(bswap_32) && (defined(__NetBSD__) || defined(__FreeBSD__) || defined(__DragonFly__))
- # define bswap_16(x) bswap16(x)
- # define bswap_32(x) bswap32(x)
- # define bswap_64(x) bswap64(x)
