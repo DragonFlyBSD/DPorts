1c503f2e07b43e7b55bf14af49a9c78137933e9d
DPorts Builder <nobody@home.ok>
2023-10-09 16:19:44 -0700

    Update sysutils/kubectl to version 1.27.3_2

--- sysutils/kubectl/dragonfly/patch-vendor_github.com_moby_term_termios_bsd.go
+++ sysutils/kubectl/dragonfly/patch-vendor_github.com_moby_term_termios_bsd.go
@@ -1,7 +1,9 @@
---- vendor/github.com/moby/term/termios_bsd.go.orig	2021-08-03 21:03:03.516334000 +0200
-+++ vendor/github.com/moby/term/termios_bsd.go	2021-08-03 21:03:51.415246000 +0200
-@@ -1,4 +1,4 @@
+--- vendor/github.com/moby/term/termios_bsd.go.orig	2023-06-14 09:47:38 UTC
++++ vendor/github.com/moby/term/termios_bsd.go
+@@ -1,5 +1,5 @@
+-//go:build darwin || freebsd || openbsd || netbsd
 -// +build darwin freebsd openbsd netbsd
++//go:build darwin || freebsd || openbsd || netbsd || dragonfly
 +// +build darwin freebsd openbsd netbsd dragonfly
  
  package term
