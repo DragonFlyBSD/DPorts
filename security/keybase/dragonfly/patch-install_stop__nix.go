4c9b8ae1038df8488f175a3abf1de7b47f1a44b5
DPorts Builder <nobody@home.ok>
2023-10-14 17:04:09 -0700

    Update security/keybase to version 6.0.4_5

--- security/keybase/dragonfly/patch-install_stop__nix.go
+++ security/keybase/dragonfly/patch-install_stop__nix.go
@@ -1,10 +1,12 @@
---- install/stop_nix.go.orig	2019-10-22 18:19:13 UTC
+--- install/stop_nix.go.orig	2023-01-13 18:04:39 UTC
 +++ install/stop_nix.go
-@@ -1,7 +1,7 @@
+@@ -1,8 +1,8 @@
  // Copyright 2019 Keybase, Inc. All rights reserved. Use of
  // this source code is governed by the included BSD license.
  
+-//go:build (!windows && !darwin) || netbsd || openbsd || freebsd
 -// +build !windows,!darwin netbsd openbsd freebsd
++//go:build (!windows && !darwin) || netbsd || openbsd || freebsd || dragonfly
 +// +build !windows,!darwin netbsd openbsd freebsd dragonfly
  
  package install
--- security/keybase/dragonfly/patch-kbfs_stderrutils_dup__stderr__nix.go
+++ security/keybase/dragonfly/patch-kbfs_stderrutils_dup__stderr__nix.go
@@ -1,10 +1,12 @@
---- kbfs/stderrutils/dup_stderr_nix.go.orig	2019-07-10 18:10:08 UTC
+--- kbfs/stderrutils/dup_stderr_nix.go.orig	2023-01-13 18:04:39 UTC
 +++ kbfs/stderrutils/dup_stderr_nix.go
-@@ -1,7 +1,7 @@
+@@ -1,8 +1,8 @@
  // Copyright 2017 Keybase, Inc. All rights reserved. Use of
  // this source code is governed by the included BSD license.
  
+-//go:build linux || android || darwin || freebsd || openbsd
 -// +build linux android darwin freebsd openbsd
++//go:build linux || android || darwin || freebsd || openbsd || dragonfly
 +// +build linux android darwin freebsd openbsd dragonfly
  
  package stderrutils
