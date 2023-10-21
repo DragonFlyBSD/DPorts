1eaf43984e33f58c2017e5feeadcc5bb54e2066c
DPorts Builder <nobody@home.ok>
2023-10-20 21:13:50 -0700

    Update databases/rocksdb to version 8.5.4

--- databases/rocksdb/dragonfly/patch-env_env__test.cc
+++ databases/rocksdb/dragonfly/patch-env_env__test.cc
@@ -1,22 +1,32 @@
---- env/env_test.cc.orig	2020-04-16 23:01:15 UTC
+--- env/env_test.cc.orig	2023-09-01 20:58:39 UTC
 +++ env/env_test.cc
-@@ -1376,7 +1376,9 @@ TEST_P(EnvPosixTestWithParam, Preallocat
-   std::unique_ptr<WritableFile> srcfile;
+@@ -1568,7 +1568,8 @@ TEST_F(EnvPosixTest, NonAlignedDirectIOM
+   }
+ 
+ #if !defined(OS_MACOSX) && !defined(OS_WIN) && !defined(OS_SOLARIS) && \
+-    !defined(OS_AIX) && !defined(OS_OPENBSD) && !defined(OS_FREEBSD)
++    !defined(OS_AIX) && !defined(OS_OPENBSD) && !defined(OS_FREEBSD) \
++    !defined(OS_DRAGONFLYBSD)
+   if (soptions.use_direct_reads) {
+     ROCKSDB_NAMESPACE::SyncPoint::GetInstance()->SetCallBack(
+         "NewRandomAccessFile:O_DIRECT", [&](void* arg) {
+@@ -1934,7 +1935,8 @@ TEST_P(EnvPosixTestWithParam, Preallocat
    EnvOptions soptions;
    soptions.use_direct_reads = soptions.use_direct_writes = direct_io_;
--#if !defined(OS_MACOSX) && !defined(OS_WIN) && !defined(OS_SOLARIS) && !defined(OS_AIX) && !defined(OS_OPENBSD) && !defined(OS_FREEBSD)
-+#if !defined(OS_MACOSX) && !defined(OS_WIN) && !defined(OS_SOLARIS) && \
+ #if !defined(OS_MACOSX) && !defined(OS_WIN) && !defined(OS_SOLARIS) && \
+-    !defined(OS_AIX) && !defined(OS_OPENBSD) && !defined(OS_FREEBSD)
++    !defined(OS_AIX) && !defined(OS_OPENBSD) && !defined(OS_FREEBSD) && \
++    !defined(OS_DRAGONFLYBSD)
+   if (soptions.use_direct_writes) {
+     ROCKSDB_NAMESPACE::SyncPoint::GetInstance()->SetCallBack(
+         "NewWritableFile:O_DIRECT", [&](void* arg) {
+@@ -1997,7 +1999,8 @@ TEST_P(EnvPosixTestWithParam, Consistent
+     const std::string path = test_base_dir + "/testfile_" + std::to_string(i);
+     std::unique_ptr<WritableFile> file;
+ #if !defined(OS_MACOSX) && !defined(OS_WIN) && !defined(OS_SOLARIS) && \
+-    !defined(OS_AIX) && !defined(OS_OPENBSD) && !defined(OS_FREEBSD)
 +    !defined(OS_AIX) && !defined(OS_OPENBSD) && !defined(OS_FREEBSD) && \
 +    !defined(OS_DRAGONFLYBSD)
      if (soptions.use_direct_writes) {
        ROCKSDB_NAMESPACE::SyncPoint::GetInstance()->SetCallBack(
            "NewWritableFile:O_DIRECT", [&](void* arg) {
-@@ -1437,7 +1439,7 @@ TEST_P(EnvPosixTestWithParam, Consistent
-     const std::string path =
-         test::TmpDir(env_) + "/" + "testfile_" + std::to_string(i);
-     std::unique_ptr<WritableFile> file;
--#if !defined(OS_MACOSX) && !defined(OS_WIN) && !defined(OS_SOLARIS) && !defined(OS_AIX) && !defined(OS_OPENBSD) && !defined(OS_FREEBSD)
-+#if !defined(OS_MACOSX) && !defined(OS_WIN) && !defined(OS_SOLARIS) && !defined(OS_AIX) && !defined(OS_OPENBSD) && !defined(OS_FREEBSD) && !defined(OS_DRAGONFLYBSD)
-       if (soptions.use_direct_writes) {
-         ROCKSDB_NAMESPACE::SyncPoint::GetInstance()->SetCallBack(
-             "NewWritableFile:O_DIRECT", [&](void* arg) {
