e2a086257ca1f3ba48b35bf8a33181af7dc1516f
DPorts Builder <nobody@home.ok>
2023-10-20 18:09:13 -0700

    Update devel/rubygem-pdk to version 3.0.0

--- devel/rubygem-pdk/files/patch-lib_pdk_cli_exec_command.rb
+++ devel/rubygem-pdk/files/patch-lib_pdk_cli_exec_command.rb
@@ -1,6 +1,6 @@
---- lib/pdk/cli/exec/command.rb.orig	2022-02-13 01:06:51 UTC
+--- lib/pdk/cli/exec/command.rb.orig	2023-03-24 21:05:48 UTC
 +++ lib/pdk/cli/exec/command.rb
-@@ -187,7 +187,7 @@ module PDK
+@@ -185,7 +185,7 @@ module PDK
  
              resolved_env['GEM_HOME'] = PDK::Util::RubyVersion.gem_home
              gem_path = PDK::Util::RubyVersion.gem_path
