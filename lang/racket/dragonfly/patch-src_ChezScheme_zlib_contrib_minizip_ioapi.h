4ce533fc0e0f0449427435a62fdb91a245e1af91
DPorts Builder <nobody@home.ok>
2023-10-21 10:29:31 -0700

    Update lang/racket to version 8.10

--- lang/racket/dragonfly/patch-src_ChezScheme_zlib_contrib_minizip_ioapi.h
+++ lang/racket/dragonfly/patch-src_ChezScheme_zlib_contrib_minizip_ioapi.h
@@ -1,11 +1,11 @@
---- src/ChezScheme/zlib/contrib/minizip/ioapi.h.orig	2021-10-24 04:18:03 UTC
+--- src/ChezScheme/zlib/contrib/minizip/ioapi.h.orig	2023-04-14 10:07:55 UTC
 +++ src/ChezScheme/zlib/contrib/minizip/ioapi.h
 @@ -50,7 +50,7 @@
  #define ftello64 ftell
  #define fseeko64 fseek
  #else
--#ifdef __FreeBSD__
-+#if defined(__FreeBSD__) || defined(__DragonFly__)
+-#if defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__)
++#if defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__) || defined(__DragonFly__)
  #define fopen64 fopen
  #define ftello64 ftello
  #define fseeko64 fseeko
