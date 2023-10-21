c963f59bdc0f2856c5e864e8db995a30a058f54a
DPorts Builder <nobody@home.ok>
2023-10-21 09:16:40 -0700

    Update devel/sdl20 to version 2.28.4

--- devel/sdl20/dragonfly/patch-src_joystick_linux_SDL__sysjoystick.c
+++ devel/sdl20/dragonfly/patch-src_joystick_linux_SDL__sysjoystick.c
@@ -1,18 +1,12 @@
---- src/joystick/linux/SDL_sysjoystick.c.orig	2021-11-26 16:03:45 UTC
+--- src/joystick/linux/SDL_sysjoystick.c.orig	2023-10-01 19:12:03 UTC
 +++ src/joystick/linux/SDL_sysjoystick.c
-@@ -506,6 +506,7 @@ static int SDL_inotify_init1(void) {
+@@ -586,7 +586,9 @@ static int SDL_inotify_init1(void)
      return fd;
  }
  #endif
 +#endif
  
- static int
- StrHasPrefix(const char *string, const char *prefix)
-@@ -561,6 +562,7 @@ IsJoystickDeviceNode(const char *node)
-     }
- }
- 
 +#ifdef HAVE_INOTIFY
- static void
- LINUX_InotifyJoystickDetect(void)
+ static void LINUX_InotifyJoystickDetect(void)
  {
+     union
