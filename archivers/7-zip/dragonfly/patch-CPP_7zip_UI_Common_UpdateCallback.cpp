60eb2b7b386786d5debc223850d4dcf5d4df549e
DPorts Builder <nobody@home.ok>
2023-10-26 00:28:05 -0700

    Update archivers/7-zip to version 23.01

--- archivers/7-zip/dragonfly/patch-CPP_7zip_UI_Common_UpdateCallback.cpp
+++ archivers/7-zip/dragonfly/patch-CPP_7zip_UI_Common_UpdateCallback.cpp
@@ -1,11 +1,11 @@
---- CPP/7zip/UI/Common/UpdateCallback.cpp.orig	2023-02-04 17:04:13.468382000 +0100
-+++ CPP/7zip/UI/Common/UpdateCallback.cpp	2023-02-04 17:04:25.168587000 +0100
-@@ -9,7 +9,7 @@
- // #include <pwd.h>
- 
+--- CPP/7zip/UI/Common/UpdateCallback.cpp.orig	2023-05-03 12:00:00 UTC
++++ CPP/7zip/UI/Common/UpdateCallback.cpp
+@@ -14,7 +14,7 @@ functions: gnu_dev_makedev(), gnu_dev_ma
+ */
  // for major()/minor():
--#if defined(__FreeBSD__) || defined(BSD)
-+#if defined(__FreeBSD__) || defined(BSD) || defined(__DragonFly__)
  #include <sys/types.h>
+-#if defined(__FreeBSD__) || defined(BSD) || defined(__APPLE__)
++#if defined(__FreeBSD__) || defined(BSD) || defined(__APPLE__) || defined(__DragonFly__)
  #else
+ #ifndef major
  #include <sys/sysmacros.h>
