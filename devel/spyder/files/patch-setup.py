aa8fa7ea8a892e7015acbf6a0cad95fe72ba9953
DPorts Builder <nobody@home.ok>
2023-11-25 19:31:01 -0800

    Update devel/spyder to version 5.4.5

--- devel/spyder/files/patch-setup.py
+++ devel/spyder/files/patch-setup.py
@@ -1,4 +1,4 @@
---- setup.py.orig	2023-04-05 02:35:28 UTC
+--- setup.py.orig	2023-08-29 21:53:14 UTC
 +++ setup.py
 @@ -91,7 +91,7 @@ def get_data_files():
      """
@@ -17,16 +17,12 @@
  )
  
  
-@@ -232,10 +231,10 @@ install_requires = [
-     'python-lsp-server[all]>=1.7.2,<1.8.0',
+@@ -229,7 +228,7 @@ install_requires = [
+     'pyls-spyder>=0.4.0',
+     'pyqt5<5.16',
+     'pyqtwebengine<5.16',
+-    'python-lsp-server[all]>=1.7.4,<1.8.0',
++    'python-lsp-server[all]>=1.7.4',
      'pyxdg>=0.26;platform_system=="Linux"',
      'pyzmq>=22.1.0',
--    'qdarkstyle>=3.0.2,<3.2.0',
-+    'qdarkstyle>0',
-     'qstylizer>=0.2.2',
-     'qtawesome>=1.2.1',
--    'qtconsole>=5.4.2,<5.5.0',
-+    'qtconsole>0',
-     'qtpy>=2.1.0',
-     'rtree>=0.9.7',
-     'setuptools>=49.6.0',
+     'qdarkstyle>=3.0.2,<3.2.0',
