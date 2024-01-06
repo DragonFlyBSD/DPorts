f88b07b1db35052c5c4d67d854325e841b073de5
DPorts Builder <nobody@home.ok>
2024-01-06 10:17:49 -0800

    Update sysutils/accountsservice to version 23.13.9_1

--- sysutils/accountsservice/dragonfly/patch-src_wtmp-helper.c
+++ sysutils/accountsservice/dragonfly/patch-src_wtmp-helper.c
@@ -1,14 +1,14 @@
---- src/wtmp-helper.c.orig	2016-01-27 18:38:21 UTC
+--- src/wtmp-helper.c.orig	2023-08-12 19:01:30 UTC
 +++ src/wtmp-helper.c
-@@ -57,9 +57,11 @@ static gboolean
+@@ -54,9 +54,11 @@ static gboolean
  wtmp_helper_start (void)
  {
  #if defined(HAVE_SETUTXDB)
 +#  ifdef __FreeBSD__
-                 if (setutxdb (UTXDB_LOG, NULL) != 0) {
-                         return FALSE;
-                 }
+         if (setutxdb (UTXDB_LOG, NULL) != 0) {
+                 return FALSE;
+         }
 +#  endif
  #elif defined(PATH_WTMP)
-                 if (utmpxname (PATH_WTMP) != 0) {
-                         return FALSE;
+         if (utmpxname (PATH_WTMP) != 0) {
+                 return FALSE;
