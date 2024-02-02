6938235e226ec69da11dc9cf58aa9fc257eb3910
DPorts Builder <nobody@home.ok>
2024-02-02 15:26:53 -0800

    Tweak net/haproxy version 2.8.3

--- net/haproxy/dragonfly/patch-src_tcp__sample.c
+++ net/haproxy/dragonfly/patch-src_tcp__sample.c
@@ -1,6 +1,6 @@
---- src/tcp_sample.c.orig	2022-10-19 21:18:24.829450000 +0200
-+++ src/tcp_sample.c	2022-10-19 21:20:01.167257000 +0200
-@@ -357,7 +357,7 @@
+--- src/tcp_sample.c.orig	2023-09-07 09:32:42 UTC
++++ src/tcp_sample.c
+@@ -357,7 +357,7 @@ static inline int get_tcp_info(const str
  	case 5:  smp->data.u.sint = info.tcpi_retrans;        break;
  	case 6:  smp->data.u.sint = info.tcpi_fackets;        break;
  	case 7:  smp->data.u.sint = info.tcpi_reordering;     break;
@@ -9,16 +9,16 @@
  	/* the ones are found on FreeBSD, NetBSD and OpenBSD featuring TCP_INFO */
  	case 2:  smp->data.u.sint = info.__tcpi_unacked;      break;
  	case 3:  smp->data.u.sint = info.__tcpi_sacked;       break;
-@@ -401,7 +401,7 @@
- 	return 1;
+@@ -430,7 +430,7 @@ smp_fetch_bc_rttvar(const struct arg *ar
  }
+ 
  
 -#if defined(__linux__) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__APPLE__)
 +#if defined(__linux__) || defined(__DragonFly__) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__APPLE__)
  /* get the unacked counter on a client connection */
  static int
  smp_fetch_fc_unacked(const struct arg *args, struct sample *smp, const char *kw, void *private)
-@@ -412,7 +412,7 @@
+@@ -441,7 +441,7 @@ smp_fetch_fc_unacked(const struct arg *a
  }
  #endif
  
@@ -27,7 +27,7 @@
  /* get the sacked counter on a client connection */
  static int
  smp_fetch_fc_sacked(const struct arg *args, struct sample *smp, const char *kw, void *private)
-@@ -423,7 +423,7 @@
+@@ -452,7 +452,7 @@ smp_fetch_fc_sacked(const struct arg *ar
  }
  #endif
  
@@ -36,7 +36,7 @@
  /* get the lost counter on a client connection */
  static int
  smp_fetch_fc_lost(const struct arg *args, struct sample *smp, const char *kw, void *private)
-@@ -434,7 +434,7 @@
+@@ -463,7 +463,7 @@ smp_fetch_fc_lost(const struct arg *args
  }
  #endif
  
@@ -45,7 +45,7 @@
  /* get the retrans counter on a client connection */
  static int
  smp_fetch_fc_retrans(const struct arg *args, struct sample *smp, const char *kw, void *private)
-@@ -445,7 +445,7 @@
+@@ -474,7 +474,7 @@ smp_fetch_fc_retrans(const struct arg *a
  }
  #endif
  
@@ -54,7 +54,7 @@
  /* get the fackets counter on a client connection */
  static int
  smp_fetch_fc_fackets(const struct arg *args, struct sample *smp, const char *kw, void *private)
-@@ -456,7 +456,7 @@
+@@ -485,7 +485,7 @@ smp_fetch_fc_fackets(const struct arg *a
  }
  #endif
  
@@ -63,10 +63,10 @@
  /* get the reordering counter on a client connection */
  static int
  smp_fetch_fc_reordering(const struct arg *args, struct sample *smp, const char *kw, void *private)
-@@ -497,22 +497,22 @@
- #ifdef TCP_INFO
- 	{ "fc_rtt",           smp_fetch_fc_rtt,           ARG1(0,STR), val_fc_time_value, SMP_T_SINT, SMP_USE_L4CLI },
- 	{ "fc_rttvar",        smp_fetch_fc_rttvar,        ARG1(0,STR), val_fc_time_value, SMP_T_SINT, SMP_USE_L4CLI },
+@@ -529,22 +529,22 @@ static struct sample_fetch_kw_list sampl
+ 	{ "bc_rtt",           smp_fetch_bc_rtt,           ARG1(0,STR), val_fc_time_value, SMP_T_SINT, SMP_USE_L4CLI },
+ 	{ "bc_rttvar",        smp_fetch_bc_rttvar,        ARG1(0,STR), val_fc_time_value, SMP_T_SINT, SMP_USE_L4CLI },
+ 
 -#if defined(__linux__) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__APPLE__)
 +#if defined(__linux__) || defined(__DragonFly__) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__APPLE__)
  	{ "fc_unacked",       smp_fetch_fc_unacked,       ARG1(0,STR), var_fc_counter, SMP_T_SINT, SMP_USE_L4CLI },
