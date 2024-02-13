c4b119e2676cb30d90f5b3b3eebbfdf467244ea0
DPorts Builder <nobody@home.ok>
2024-02-12 19:28:40 -0800

    Update devel/py-aiobotocore to version 2.9.0

--- devel/py-aiobotocore/files/patch-setup.py
+++ devel/py-aiobotocore/files/patch-setup.py
@@ -1,13 +1,21 @@
---- setup.py.orig	2023-08-11 20:42:50 UTC
+--- setup.py.orig	2023-12-13 07:26:01 UTC
 +++ setup.py
-@@ -14,8 +14,8 @@ install_requires = [
+@@ -7,15 +7,15 @@ from setuptools import find_packages, setup
+ # NOTE: When updating botocore make sure to update awscli/boto3 versions below
+ install_requires = [
+     # pegged to also match items in `extras_require`
+-    'botocore>=1.33.2,<1.33.14',
++    'botocore>=1.33.2',
+     'aiohttp>=3.7.4.post0,<4.0.0',
+     'wrapt>=1.10.10, <2.0.0',
+     'aioitertools>=0.5.1,<1.0.0',
  ]
  
  extras_require = {
--    'awscli': ['awscli>=1.29.17,<1.29.18'],
--    'boto3': ['boto3>=1.28.17,<1.28.18'],
-+    'awscli': ['awscli>=1.29.17'],
-+    'boto3': ['boto3>=1.28.17'],
+-    'awscli': ['awscli>=1.31.2,<1.31.14'],
+-    'boto3': ['boto3>=1.33.2,<1.33.14'],
++    'awscli': ['awscli>=1.29.81'],
++    'boto3': ['boto3>=1.33.2'],
  }
  
  
