c98c2db63d3d3a8a2a5d51bd32ce8a98ff1e55ea
DPorts Builder <nobody@home.ok>
2024-03-29 09:30:05 -0700

    Update devel/onetbb to version 2021.11.0

--- devel/onetbb/dragonfly/patch-src_tbbmalloc_tbbmalloc.cpp
+++ devel/onetbb/dragonfly/patch-src_tbbmalloc_tbbmalloc.cpp
@@ -1,9 +1,9 @@
---- src/tbbmalloc/tbbmalloc.cpp.orig	2021-10-04 09:50:18 UTC
+--- src/tbbmalloc/tbbmalloc.cpp.orig	2023-11-17 16:23:05 UTC
 +++ src/tbbmalloc/tbbmalloc.cpp
 @@ -43,7 +43,7 @@ namespace internal {
  #define MALLOCLIB_NAME "tbbmalloc" DEBUG_SUFFIX ".dll"
  #elif __APPLE__
- #define MALLOCLIB_NAME "libtbbmalloc" DEBUG_SUFFIX ".dylib"
+ #define MALLOCLIB_NAME "libtbbmalloc" DEBUG_SUFFIX ".2.dylib"
 -#elif __FreeBSD__ || __NetBSD__ || __OpenBSD__ || __sun || _AIX || __ANDROID__
 +#elif __FreeBSD__ || __NetBSD__ || __OpenBSD__ || __sun || _AIX || __ANDROID__ || __DragonFly__
  #define MALLOCLIB_NAME "libtbbmalloc" DEBUG_SUFFIX ".so"
