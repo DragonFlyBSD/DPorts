f3db6f302b2f647db1d91432b08722029ac97750
DPorts Builder <nobody@home.ok>
2024-04-20 18:27:35 -0700

    Tweak devel/qca version 2.3.7

--- devel/qca/dragonfly/patch-src_support_console.cpp
+++ devel/qca/dragonfly/patch-src_support_console.cpp
@@ -1,11 +1,11 @@
---- src/support/console.cpp.orig	2021-02-04 10:29:44 UTC
+--- src/support/console.cpp.orig	2023-07-24 15:36:27 UTC
 +++ src/support/console.cpp
 @@ -30,7 +30,7 @@
  #ifdef Q_OS_WIN
  #include <windows.h>
  #else
 -#ifdef Q_OS_ANDROID
-+#if defined(Q_OS_ANDROID) || defined(Q_OS_FREEBSD)
++#if defined(Q_OS_ANDROID) || defined(Q_OS_DRAGONFLY)
  #include <termios.h>
  #else
  #include <sys/termios.h>
