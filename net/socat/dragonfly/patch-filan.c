dd827c828d5c8a183a39c1c0d143034fd9d6d3d9
DPorts Builder <nobody@home.ok>
2024-05-20 13:40:33 -0700

    Update net/socat to version 1.8.0.0_1

--- net/socat/dragonfly/patch-filan.c
+++ net/socat/dragonfly/patch-filan.c
@@ -1,18 +1,19 @@
---- filan.c.orig	2021-10-31 18:06:09 UTC
+--- filan.c.orig	2023-11-13 19:31:08 UTC
 +++ filan.c
-@@ -905,6 +905,7 @@ int tcpan(int fd, FILE *outfile) {
- #if WITH_TCP
+@@ -939,6 +939,7 @@ int tcpan(int fd, FILE *outfile) {
+ #if WITH_TCP && defined(TCP_INFO)
  
  int tcpan2(int fd, FILE *outfile) {
 +#if !defined(__DragonFly__)
     struct tcp_info tcpinfo;
     socklen_t tcpinfolen = sizeof(tcpinfo);
     int result;
-@@ -928,6 +929,7 @@ int tcpan2(int fd, FILE *outfile) {
-    //fprintf(outfile, "%s={%u}\t", "TCPI_DELIVERY_RATE_APP_LIMITED", tcpinfo.tcpi_delivery_rate_app_limited);
-    //fprintf(outfile, "%s={%u}\t", "TCPI_FASTOPEN_CLIENT_FAIL", tcpinfo.tcpi_fastopen_client_fail);
-    // fprintf(outfile, "%s={%u}\t", "TCPI_", tcpinfo.tcpi_);
+@@ -964,7 +965,7 @@ int tcpan2(int fd, FILE *outfile) {
+    fprintf(outfile, "%s={%u}\t", "TCPI_FASTOPEN_CLIENT_FAIL", tcpinfo.tcpi_fastopen_client_fail);
+    fprintf(outfile, "%s={%u}\t", "TCPI_", tcpinfo.tcpi_);
+ #endif
+-
 +#endif
- 
     return 0;
  }
+ 
