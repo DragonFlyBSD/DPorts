a7cb56187ac6290e59f0e37318feecca9e5b4650
DPorts Builder <nobody@home.ok>
2024-09-18 16:08:19 -0700

    Update devel/libfmt to version 10.2.1

--- devel/libfmt/dragonfly/patch-include_fmt_os.h
+++ devel/libfmt/dragonfly/patch-include_fmt_os.h
@@ -1,11 +1,11 @@
---- include/fmt/os.h.orig	2020-05-09 16:13:20 UTC
+--- include/fmt/os.h.orig	2024-01-03 19:17:28 UTC
 +++ include/fmt/os.h
-@@ -19,7 +19,7 @@
- #include <cstdio>
- #include <cstdlib>  // for strtod_l
+@@ -15,7 +15,7 @@
+ 
+ #include "format.h"
  
 -#if defined __APPLE__ || defined(__FreeBSD__)
 +#if defined __APPLE__ || defined(__FreeBSD__) || defined(__DragonFly__)
- #  include <xlocale.h>  // for LC_NUMERIC_MASK on OS X
- #endif
- 
+ #  if FMT_HAS_INCLUDE(<xlocale.h>)
+ #    include <xlocale.h>  // for LC_NUMERIC_MASK on OS X
+ #  endif
