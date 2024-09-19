1a7c096625e5a8e571628ac097a2b62f4d54a508
DPorts Builder <nobody@home.ok>
2024-09-19 12:27:25 -0700

    Update multimedia/assimp to version 5.4.1

--- multimedia/assimp/dragonfly/patch-contrib_unzip_ioapi.h
+++ multimedia/assimp/dragonfly/patch-contrib_unzip_ioapi.h
@@ -1,11 +1,12 @@
---- contrib/unzip/ioapi.h.orig	2023-10-27 17:51:52 UTC
+--- contrib/unzip/ioapi.h.orig	2024-05-13 11:45:14 UTC
 +++ contrib/unzip/ioapi.h
-@@ -50,7 +50,7 @@
+@@ -50,7 +50,8 @@
  #define ftello64 ftell
  #define fseeko64 fseek
  #else
--#if defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__)
-+#if defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__) || defined(__DragonFly__)
+-#if defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__) || defined(__HAIKU__) || defined(MINIZIP_FOPEN_NO_64)
++#if defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__) || defined(__HAIKU__) || defined(MINIZIP_FOPEN_NO_64) || \
++    defined(__DragonFly__)
  #define fopen64 fopen
  #define ftello64 ftello
  #define fseeko64 fseeko
