9c3cf9ab367b4b35a61aed9954bfcca79064111c
DPorts Builder <nobody@home.ok>
2024-09-19 12:26:14 -0700

    Update sysutils/py-psutil to version 6.0.0

--- sysutils/py-psutil/dragonfly/patch-setup.py
+++ sysutils/py-psutil/dragonfly/patch-setup.py
@@ -1,6 +1,6 @@
---- setup.py.orig	2023-10-27 08:38:43 UTC
+--- setup.py.orig	2024-06-02 23:32:26 UTC
 +++ setup.py
-@@ -48,6 +48,7 @@ sys.path.insert(0, os.path.join(HERE, "p
+@@ -49,6 +49,7 @@ sys.path.insert(0, os.path.join(HERE, "p
  from _common import AIX  # NOQA
  from _common import BSD  # NOQA
  from _common import FREEBSD  # NOQA
@@ -8,9 +8,9 @@
  from _common import LINUX  # NOQA
  from _common import MACOS  # NOQA
  from _common import NETBSD  # NOQA
-@@ -262,6 +263,20 @@ elif FREEBSD:
-         libraries=["devstat"],
-         **py_limited_api)
+@@ -291,6 +292,20 @@ elif FREEBSD:
+         # fmt: on
+     )
  
 +elif DRAGONFLY:
 +    macros.append(("PSUTIL_DRAGONFLY", 1))
@@ -29,7 +29,7 @@
  elif OPENBSD:
      macros.append(("PSUTIL_OPENBSD", 1))
      ext = Extension(
-@@ -423,6 +438,7 @@ def main():
+@@ -483,6 +498,7 @@ def main():
              'Operating System :: POSIX :: BSD :: FreeBSD',
              'Operating System :: POSIX :: BSD :: NetBSD',
              'Operating System :: POSIX :: BSD :: OpenBSD',
@@ -37,12 +37,12 @@
              'Operating System :: POSIX :: BSD',
              'Operating System :: POSIX :: Linux',
              'Operating System :: POSIX :: SunOS/Solaris',
-@@ -477,7 +493,7 @@ def main():
-             elif MACOS:
-                 print(hilite("XCode (https://developer.apple.com/xcode/) "
-                              "is not installed", color="red"), file=sys.stderr)
+@@ -549,7 +565,7 @@ def main():
+                     " is not installed"
+                 )
+                 print(hilite(msg, color="red"), file=sys.stderr)
 -            elif FREEBSD:
 +            elif FREEBSD or DRAGONFLY:
                  if which('pkg'):
                      missdeps("pkg install gcc python%s" % py3)
-                 elif which('mport'):   # MidnightBSD
+                 elif which('mport'):  # MidnightBSD
