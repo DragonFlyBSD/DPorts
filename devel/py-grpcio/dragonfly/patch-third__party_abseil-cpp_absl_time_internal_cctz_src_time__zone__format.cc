0873ac927802890976b5f52646cddc038f51ccd0
DPorts Builder <nobody@home.ok>
2024-09-26 16:20:56 -0700

    Update devel/py-grpcio to version 1.64.1

--- devel/py-grpcio/dragonfly/patch-third__party_abseil-cpp_absl_time_internal_cctz_src_time__zone__format.cc
+++ devel/py-grpcio/dragonfly/patch-third__party_abseil-cpp_absl_time_internal_cctz_src_time__zone__format.cc
@@ -1,12 +1,12 @@
---- third_party/abseil-cpp/absl/time/internal/cctz/src/time_zone_format.cc.orig	2023-10-24 08:24:24 UTC
+--- third_party/abseil-cpp/absl/time/internal/cctz/src/time_zone_format.cc.orig	2024-05-30 00:17:42 UTC
 +++ third_party/abseil-cpp/absl/time/internal/cctz/src/time_zone_format.cc
 @@ -19,7 +19,8 @@
  #endif
  
  #if defined(HAS_STRPTIME) && HAS_STRPTIME
--#if !defined(_XOPEN_SOURCE) && !defined(__OpenBSD__) && !defined(__FreeBSD__)
-+#if !defined(_XOPEN_SOURCE) && !defined(__OpenBSD__) && !defined(__FreeBSD__) && \
-+  !defined(__DragonFly__)
+-#if !defined(_XOPEN_SOURCE) && !defined(__FreeBSD__) && !defined(__OpenBSD__)
++#if !defined(_XOPEN_SOURCE) && !defined(__FreeBSD__) && !defined(__OpenBSD__) && \
++    !defined(__DragonFly__)
  #define _XOPEN_SOURCE 500  // Exposes definitions for SUSv2 (UNIX 98).
  #endif
  #endif
