5d4a18ed7d0b7175bd883b490cd619cd7b3adfac
DPorts Builder <nobody@home.ok>
2024-09-28 05:21:04 -0700

    Update security/nss to version 3.104

--- security/nss/dragonfly/patch-coreconf_config.mk
+++ security/nss/dragonfly/patch-coreconf_config.mk
@@ -1,11 +1,11 @@
---- coreconf/config.mk.orig	2022-09-15 17:27:38 UTC
+--- coreconf/config.mk.orig	2024-08-30 12:18:39 UTC
 +++ coreconf/config.mk
 @@ -31,7 +31,7 @@ endif
  #######################################################################
  
- TARGET_OSES = FreeBSD BSD_OS NetBSD OpenUNIX OS2 QNX Darwin OpenBSD \
--              AIX RISCOS WINNT WIN95 Linux Android
-+              AIX RISCOS WINNT WIN95 Linux Android DragonFly
+ TARGET_OSES = FreeBSD NetBSD OpenUNIX QNX Darwin OpenBSD \
+-              AIX WINNT Linux Android
++              AIX WINNT Linux Android DragonFly
  
  ifeq (,$(filter-out $(TARGET_OSES),$(OS_TARGET)))
  include $(CORE_DEPTH)/coreconf/$(OS_TARGET).mk
