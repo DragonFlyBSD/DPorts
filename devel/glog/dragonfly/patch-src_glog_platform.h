16f57ef37061221f5de8dd339819e146f6646ad2
DPorts Builder <nobody@home.ok>
2024-10-13 06:43:45 -0700

    Update devel/glog to version 0.7.1

--- devel/glog/dragonfly/patch-src_glog_platform.h
+++ devel/glog/dragonfly/patch-src_glog_platform.h
@@ -1,11 +1,11 @@
---- src/glog/platform.h.orig	2022-04-04 22:03:27 UTC
-+++ src/glog/platform.h
-@@ -50,6 +50,8 @@
- #define GLOG_OS_NETBSD
- #elif defined(__OpenBSD__)
- #define GLOG_OS_OPENBSD
+--- src/glog/platform.h.orig	2024-10-06 14:08:39.604460000 +0200
++++ src/glog/platform.h	2024-10-06 14:09:03.804859000 +0200
+@@ -45,6 +45,8 @@
+ #  endif
+ #elif defined(macintosh) || defined(__APPLE__) || defined(__APPLE_CC__)
+ #  define GLOG_OS_MACOSX
 +#elif defined(__DragonFly__)
-+#define GLOG_OS_DRAGONFLY
- #else
- // TODO(hamaji): Add other platforms.
- #error Platform not supported by glog. Please consider to contribute platform information by submitting a pull request on Github.
++#  define GLOG_OS_DRAGOFLY
+ #elif defined(__FreeBSD__)
+ #  define GLOG_OS_FREEBSD
+ #elif defined(__NetBSD__)
