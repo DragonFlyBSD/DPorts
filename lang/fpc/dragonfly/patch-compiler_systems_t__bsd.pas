4bfe9047d72b46bea3394b85321a2ef51aabd9e6
DPorts Builder <nobody@home.ok>
2025-02-28 04:11:20 -0800

    Update lang/fpc to version 3.2.3

--- lang/fpc/dragonfly/patch-compiler_systems_t__bsd.pas
+++ lang/fpc/dragonfly/patch-compiler_systems_t__bsd.pas
@@ -1,5 +1,5 @@
---- compiler/systems/t_bsd.pas.intermediate	2024-01-09 11:17:14 UTC
-+++ compiler/systems/t_bsd.pas
+--- compiler/systems/t_bsd.pas.orig	Thu Feb 27 21:14:17 2025
++++ compiler/systems/t_bsd.pas	Thu Feb
 @@ -116,6 +116,8 @@ begin
       LibrarySearchPath.AddLibraryPath(sysrootpath,'=/usr/lib;=$OPENBSD_X11BASE/lib;=$OPENBSD_LOCALBASE/lib',true)
     else if target_info.system in systems_freebsd then
@@ -9,12 +9,3 @@
     else
       LibrarySearchPath.AddLibraryPath(sysrootpath,'=/lib;=/usr/lib;=/usr/X11R6/lib',true);
  end;
-@@ -128,7 +130,7 @@ procedure TLinkerBSD.SetDefaultInfo;
- var
-   LdProgram: string='ld';
- begin
--  if target_info.system in systems_freebsd then
-+  if target_info.system in (systems_freebsd+[system_x86_64_dragonfly]) then
-   begin
-     LdProgram:=GetEnvPChar('FPC_LDPATH');
-     if not FileExists(LdProgram,True) then
