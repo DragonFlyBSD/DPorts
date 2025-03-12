2249415dcac49a5aed39b7bb8f693fbb55b80eff
DPorts Builder <nobody@home.ok>
2025-03-12 16:52:32 -0700

    Update devel/gitlab-runner to version 17.3.1

--- devel/gitlab-runner/dragonfly/patch-vendor_github.com_moby_term_termios_nonbsd.go
+++ devel/gitlab-runner/dragonfly/patch-vendor_github.com_moby_term_termios_nonbsd.go
@@ -1,8 +1,10 @@
---- vendor/github.com/moby/term/termios_nonbsd.go.orig	2021-06-13 15:09:42.191308000 +0200
-+++ vendor/github.com/moby/term/termios_nonbsd.go	2021-06-13 15:09:50.701117000 +0200
-@@ -1,4 +1,4 @@
--//+build !darwin,!freebsd,!netbsd,!openbsd,!windows
-+//+build !darwin,!freebsd,!netbsd,!openbsd,!windows,!dragonfly
+--- vendor/github.com/moby/term/termios_nonbsd.go.orig	Wed Mar 12 17:45:59 2025
++++ vendor/github.com/moby/term/termios_nonbsd.go	Wed Mar 12 17:47:22 2025
+@@ -1,5 +1,5 @@
+-//go:build !darwin && !freebsd && !netbsd && !openbsd && !windows
+-// +build !darwin,!freebsd,!netbsd,!openbsd,!windows
++//go:build !darwin && !freebsd && !netbsd && !openbsd && !windows && !dragonfly
++// +build !darwin,!freebsd,!netbsd,!openbsd,!windows,!dragonfly
  
  package term
  
