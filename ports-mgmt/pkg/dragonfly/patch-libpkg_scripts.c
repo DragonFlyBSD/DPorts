73537ce785573166a8379bef9ac6a2e228718435
DPorts Builder <nobody@home.ok>
2025-03-19 16:39:17 -0700

    Tweak ports-mgmt/pkg version 1.21.3

--- ports-mgmt/pkg/dragonfly/patch-libpkg_scripts.c
+++ ports-mgmt/pkg/dragonfly/patch-libpkg_scripts.c
@@ -1,19 +1,6 @@
---- libpkg/lua_scripts.c.orig	Mon Nov 13 13:48:44 2023
-+++ libpkg/lua_scripts.c	Wed Mar
-@@ -33,6 +33,12 @@
- #include <sys/types.h>
- #include <sys/wait.h>
- 
-+#ifdef __DragonFly__
-+#include <sys/types.h>
-+#include <sys/procctl.h>
-+#include <sys/wait.h>
-+#endif
-+
- #include <errno.h>
- #include <poll.h>
- #include <xstring.h>
-@@ -54,9 +60,14 @@ pkg_lua_script_run(struct pkg * const pkg, pkg_lua_scr
+--- libpkg/scripts.c.orig	Mon Nov 13 13:48:44 2023
++++ libpkg/scripts.c	Wed Mar
+@@ -71,9 +71,14 @@ pkg_script_run(struct pkg * const pkg, pkg_script type
  #ifdef PROC_REAP_KILL
  	bool do_reap;
  	pid_t mypid;
@@ -22,13 +9,13 @@
  	struct procctl_reaper_kill killemall;
 +#elif defined(__DragonFly__)
 +	struct reaper_status info;
-+	struct reaper_kill killemall;
++	struct reaper_kill killemall;	
  #endif
 +#endif
- 	int cur_pipe[2];
- 	char *line = NULL;
- 
-@@ -168,9 +179,15 @@ cleanup:
+ 	struct {
+ 		const char * const arg;
+ 		const pkg_script b;
+@@ -250,16 +255,21 @@ cleanup:
  		return (ret);
  
  	procctl(P_PID, mypid, PROC_REAP_STATUS, &info);
@@ -44,3 +31,10 @@
  		if (procctl(P_PID, mypid, PROC_REAP_KILL, &killemall) != 0) {
  			pkg_errno("%s", "Fail to kill all processes");
  		}
+ 	}
+ 	procctl(P_PID, mypid, PROC_REAP_RELEASE, NULL);
+ #endif
+-
+ 	return (ret);
+ }
+ 
