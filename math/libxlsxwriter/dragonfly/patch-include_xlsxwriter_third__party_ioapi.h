1c72df950120eac54e339f82c3f25d60f6ccf265
DPorts Builder <nobody@home.ok>
2025-03-28 16:42:23 -0700

    Update math/libxlsxwriter to version 1.1.7

--- math/libxlsxwriter/dragonfly/patch-include_xlsxwriter_third__party_ioapi.h
+++ math/libxlsxwriter/dragonfly/patch-include_xlsxwriter_third__party_ioapi.h
@@ -1,11 +1,11 @@
---- include/xlsxwriter/third_party/ioapi.h.orig	2021-08-09 20:02:21 UTC
-+++ include/xlsxwriter/third_party/ioapi.h
+--- include/xlsxwriter/third_party/ioapi.h.orig	Fri Apr  5 23:08:20 2024
++++ include/xlsxwriter/third_party/ioapi.h	Fri Mar
 @@ -56,7 +56,7 @@
  #define ftello64 ftell
  #define fseeko64 fseek
  #else
--#if defined(__FreeBSD__) || defined(__OpenBSD__)
-+#if defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__DragonFly__)
+-#if defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__) || defined(__HAIKU__) || defined(MINIZIP_FOPEN_NO_64)
++#if defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__) || defined(__HAIKU__) || defined(MINIZIP_FOPEN_NO_64) || defined(__DragonFly__)
  #define fopen64 fopen
  #define ftello64 ftello
  #define fseeko64 fseeko
