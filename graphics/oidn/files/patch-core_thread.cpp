517671ccc2b7d0863e4688c9816e4a2fb48f703e
DPorts Builder <nobody@home.ok>
2025-03-30 10:57:28 -0700

    Manual tweak graphics/oidn

--- graphics/oidn/files/patch-core_thread.cpp
+++ graphics/oidn/files/patch-core_thread.cpp
@@ -1,7 +1,7 @@
---- common/thread.cpp.orig	2020-09-11 17:36:51 UTC
-+++ common/thread.cpp
-@@ -1,6 +1,8 @@
- // Copyright 2009-2020 Intel Corporation
+--- core/thread.cpp.orig	2024-06-14 23:02:44 UTC
++++ core/thread.cpp
+@@ -1,11 +1,13 @@
+ // Copyright 2009 Intel Corporation
  // SPDX-License-Identifier: Apache-2.0
  
 +#include <pthread_np.h>
@@ -9,48 +9,54 @@
  #if defined(_MSC_VER)
    #pragma warning (disable : 4146) // unary minus operator applied to unsigned type, result still unsigned
  #endif
-@@ -120,7 +122,7 @@ namespace oidn {
-       OIDN_WARNING("SetThreadGroupAffinity failed");
+ 
+-#if defined(__linux__)
++#if defined(__linux__) || defined(__FreeBSD__)
+   #include <sched.h>
+   #include <unordered_set>
+ #elif defined(__APPLE__)
+@@ -125,7 +127,7 @@ OIDN_NAMESPACE_BEGIN
+       printWarning("SetThreadGroupAffinity failed");
    }
  
 -#elif defined(__linux__)
 +#elif defined(__linux__) || defined(__FreeBSD__)
  
-   // ---------------------------------------------------------------------------
+   // -----------------------------------------------------------------------------------------------
    // ThreadAffinity: Linux
-@@ -165,7 +167,7 @@ namespace oidn {
+@@ -179,7 +181,7 @@ OIDN_NAMESPACE_BEGIN
  
-     for (size_t i = 0; i < threadIds.size(); ++i)
+     for (size_t i = 0; i < threadIDs.size(); ++i)
      {
 -      cpu_set_t affinity;
 +      cpuset_t affinity;
        CPU_ZERO(&affinity);
-       CPU_SET(threadIds[i], &affinity);
+       CPU_SET(threadIDs[i], &affinity);
  
-@@ -182,7 +184,7 @@ namespace oidn {
+@@ -196,7 +198,7 @@ OIDN_NAMESPACE_BEGIN
      const pthread_t thread = pthread_self();
  
      // Save the current affinity
 -    if (pthread_getaffinity_np(thread, sizeof(cpu_set_t), &oldAffinities[threadIndex]) != 0)
 +    if (pthread_getaffinity_np(thread, sizeof(cpuset_t), &oldAffinities[threadIndex]) != 0)
      {
-       OIDN_WARNING("pthread_getaffinity_np failed");
+       printWarning("pthread_getaffinity_np failed");
        oldAffinities[threadIndex] = affinities[threadIndex];
-@@ -190,7 +192,7 @@ namespace oidn {
+@@ -204,7 +206,7 @@ OIDN_NAMESPACE_BEGIN
      }
  
      // Set the new affinity
 -    if (pthread_setaffinity_np(thread, sizeof(cpu_set_t), &affinities[threadIndex]) != 0)
 +    if (pthread_setaffinity_np(thread, sizeof(cpuset_t), &affinities[threadIndex]) != 0)
-       OIDN_WARNING("pthread_setaffinity_np failed");
+       printWarning("pthread_setaffinity_np failed");
    }
  
-@@ -202,7 +204,7 @@ namespace oidn {
+@@ -216,7 +218,7 @@ OIDN_NAMESPACE_BEGIN
      const pthread_t thread = pthread_self();
  
      // Restore the original affinity
 -    if (pthread_setaffinity_np(thread, sizeof(cpu_set_t), &oldAffinities[threadIndex]) != 0)
 +    if (pthread_setaffinity_np(thread, sizeof(cpuset_t), &oldAffinities[threadIndex]) != 0)
-       OIDN_WARNING("pthread_setaffinity_np failed");
+       printWarning("pthread_setaffinity_np failed");
    }
  
