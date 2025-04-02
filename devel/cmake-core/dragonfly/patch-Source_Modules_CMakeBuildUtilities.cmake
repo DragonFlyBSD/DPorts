0b53552a1565e2a096b39071d661b07584f827e1
DPorts Builder <nobody@home.ok>
2025-04-02 10:43:39 -0700

    Update devel/cmake-core to version 3.31.6

--- devel/cmake-core/dragonfly/patch-Source_Modules_CMakeBuildUtilities.cmake
+++ devel/cmake-core/dragonfly/patch-Source_Modules_CMakeBuildUtilities.cmake
@@ -1,11 +1,11 @@
---- Source/Modules/CMakeBuildUtilities.cmake.orig	2022-11-30 13:57:03 UTC
-+++ Source/Modules/CMakeBuildUtilities.cmake
-@@ -151,7 +151,7 @@ else()
+--- Source/Modules/CMakeBuildUtilities.cmake.orig	Wed Apr  2 14:01:07 2025
++++ Source/Modules/CMakeBuildUtilities.cmake	Wed Apr  2 14:01:27 2025
+@@ -150,7 +150,7 @@ else()
    endif()
    set(_CMAKE_USE_OPENSSL_DEFAULT OFF)
    if(NOT DEFINED CMAKE_USE_OPENSSL AND NOT WIN32 AND NOT APPLE
--      AND CMAKE_SYSTEM_NAME MATCHES "(Linux|FreeBSD)")
-+      AND CMAKE_SYSTEM_NAME MATCHES "(Linux|FreeBSD|DragonFly)")
+-      AND CMAKE_SYSTEM_NAME MATCHES "(Linux|FreeBSD|CYGWIN|MSYS)")
++      AND CMAKE_SYSTEM_NAME MATCHES "(Linux|FreeBSD|DragonFly|CYGWIN|MSYS)")
      set(_CMAKE_USE_OPENSSL_DEFAULT ON)
    endif()
    option(CMAKE_USE_OPENSSL "Use OpenSSL." ${_CMAKE_USE_OPENSSL_DEFAULT})
