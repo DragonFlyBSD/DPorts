f6c920ad3abc86b2eed5885a7ec5b0149246e09f
DPorts Builder <nobody@home.ok>
2025-04-02 00:51:24 -0700

    Update devel/glib20 to version 2.80.5_1,2

--- devel/glib20/dragonfly/patch-gmodule_gmodule-dl.c
+++ devel/glib20/dragonfly/patch-gmodule_gmodule-dl.c
@@ -1,11 +1,20 @@
---- gmodule/gmodule-dl.c.orig	2014-11-19 17:55:03.653329000 +0100
-+++ gmodule/gmodule-dl.c	2014-11-19 17:56:34.063455000 +0100
-@@ -105,7 +105,7 @@
- static gpointer
- _g_module_self (void)
+--- gmodule/gmodule-dl.c.orig	Mon Aug 26 10:15:22 2024
++++ gmodule/gmodule-dl.c	Wed Apr
+@@ -167,7 +167,7 @@ _g_module_self (void)
+    * NULL is given, dlsym returns an appropriate pointer.
+    */
+   lock_dlerror ();
+-#if defined(__BIONIC__) || defined(__NetBSD__) || defined(__FreeBSD__)
++#if defined(__BIONIC__) || defined(__NetBSD__) || defined(__FreeBSD__) || defined(__DragonFly__)
+   handle = RTLD_DEFAULT;
+ #else
+   handle = dlopen (NULL, RTLD_GLOBAL | RTLD_LAZY);
+@@ -182,7 +182,7 @@ _g_module_self (void)
+ static void
+ _g_module_close (gpointer handle)
  {
--#ifndef __FreeBSD__
-+#if ! defined(__FreeBSD__) && ! defined(__DragonFly__)
-   gpointer handle;
-   
-   /* to query symbols from the program itself, special link options
+-#if defined(__BIONIC__) || defined(__NetBSD__) || defined(__FreeBSD__)
++#if defined(__BIONIC__) || defined(__NetBSD__) || defined(__FreeBSD__) || defined(__DragonFly__)
+   if (handle != RTLD_DEFAULT)
+ #endif
+     {
