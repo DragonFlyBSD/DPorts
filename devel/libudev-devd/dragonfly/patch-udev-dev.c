951e3e8d8c76530604ac07ce83b513236534fd3f
DPorts Builder <nobody@home.ok>
2025-04-02 11:39:29 -0700

    Update devel/libudev-devd to version 0.6.0

--- devel/libudev-devd/dragonfly/patch-udev-dev.c
+++ devel/libudev-devd/dragonfly/patch-udev-dev.c
@@ -1,12 +1,11 @@
---- udev-utils.c.orig	2023-01-16 23:20:51 UTC
-+++ udev-utils.c
-@@ -843,7 +843,7 @@ create_drm_handler(struct udev_device *u
-
+--- udev-dev.c.orig	Fri Sep 20 07:46:46 2024
++++ udev-dev.c	Wed Apr
+@@ -626,7 +626,7 @@ create_drm_handler(struct udev_device *ud)
+ 
  	/* Get the hw.dri.<cardnum>.busid entry */
  	realpath(devpath, devbuf);
 -	if (sscanf(devbuf, "/dev/drm/%d", &cardnum) != 1)
 +	if (sscanf(devbuf, "/dev/dri/card%d", &cardnum) != 1)
  		return;
-
+ 
  	snprintf(buf, sizeof(buf), "hw.dri.%d.busid", cardnum);
-
