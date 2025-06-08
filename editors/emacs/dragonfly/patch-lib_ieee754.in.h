713e4dea526c84d0046d92ca29129af2abb946f7
DPorts Builder <nobody@home.ok>
2025-06-08 14:32:02 -0700

    Update editors/emacs to version 30.1_2,3

--- editors/emacs/dragonfly/patch-lib_ieee754.in.h
+++ editors/emacs/dragonfly/patch-lib_ieee754.in.h
@@ -1,28 +1,12 @@
---- lib/ieee754.in.h.orig	2020-07-29 21:40:41 UTC
-+++ lib/ieee754.in.h
-@@ -25,6 +25,9 @@
- # include <endian.h>
+--- lib/ieee754.in.h.orig	2025-05-13 11:41:33.818289000 +0200
++++ lib/ieee754.in.h	2025-05-13 11:42:33.829047000 +0200
+@@ -28,6 +28,9 @@
+ # define _IEEE754_FLOAT_WORD_ORDER __FLOAT_WORD_ORDER
  #else
  /* Gnulib usage.  */
 +#if defined(__DragonFLy__)
 +#include <machine/endian.h>
 +#endif
- # ifndef __BEGIN_DECLS
- #  ifdef __cplusplus
- #   define __BEGIN_DECLS	extern "C" {
-@@ -35,6 +38,7 @@
- #  endif
- # endif
- # ifndef __FLOAT_WORD_ORDER
-+#ifndef __BYTE_ORDER
- #  define __LITTLE_ENDIAN	1234
- #  define __BIG_ENDIAN		4321
- #  ifdef WORDS_BIGENDIAN
-@@ -42,6 +46,7 @@
- #  else
- #   define __BYTE_ORDER __LITTLE_ENDIAN
- #  endif
-+#endif
- #  define __FLOAT_WORD_ORDER __BYTE_ORDER
- # endif
- #endif
+ # include <endian.h>
+ # define _IEEE754_BYTE_ORDER BYTE_ORDER
+ # define _IEEE754_BIG_ENDIAN BIG_ENDIAN
