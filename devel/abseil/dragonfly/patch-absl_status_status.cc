7b321c15736ffacf67f3b5bc2145a52e63420bce
DPorts Builder <nobody@home.ok>
2025-06-09 11:32:01 -0700

    Update devel/abseil to version 20250127.0

--- devel/abseil/dragonfly/patch-absl_status_status.cc
+++ devel/abseil/dragonfly/patch-absl_status_status.cc
@@ -1,5 +1,5 @@
---- absl/status/status.cc.orig	2022-10-15 18:56:03.346469000 +0200
-+++ absl/status/status.cc	2022-10-15 19:10:10.267657000 +0200
+--- absl/status/status.cc.orig	Mon Feb  3 15:50:45 2025
++++ absl/status/status.cc	Mon Jun
 @@ -13,7 +13,7 @@
  // limitations under the License.
  #include "absl/status/status.h"
@@ -7,39 +7,5 @@
 -#include <errno.h>
 +#include <sys/errno.h>
  
- #include <cassert>
- 
-@@ -460,15 +460,19 @@
-     case EFAULT:        // Bad address
-     case EILSEQ:        // Illegal byte sequence
-     case ENOPROTOOPT:   // Protocol not available
-+#ifdef ENOSTR
-     case ENOSTR:        // Not a STREAM
-+#endif
-     case ENOTSOCK:      // Not a socket
-     case ENOTTY:        // Inappropriate I/O control operation
-     case EPROTOTYPE:    // Protocol wrong type for socket
-     case ESPIPE:        // Invalid seek
-       return StatusCode::kInvalidArgument;
-     case ETIMEDOUT:  // Connection timed out
-+#ifdef ETIME
-     case ETIME:      // Timer expired
-       return StatusCode::kDeadlineExceeded;
-+#endif
-     case ENODEV:  // No such device
-     case ENOENT:  // No such file or directory
- #ifdef ENOMEDIUM
-@@ -526,9 +530,13 @@
-     case EMLINK:   // Too many links
-     case ENFILE:   // Too many open files in system
-     case ENOBUFS:  // No buffer space available
-+#ifdef ENODATA
-     case ENODATA:  // No message is available on the STREAM read queue
-+#endif
-     case ENOMEM:   // Not enough space
-+#ifdef ENOSR
-     case ENOSR:    // No STREAM resources
-+#endif
- #ifdef EUSERS
-     case EUSERS:  // Too many users
- #endif
+ #include <atomic>
+ #include <cstddef>
