ee69985fa82a06771fa747d92704ebfc4019aaf5
DPorts Builder <nobody@home.ok>
2025-06-14 02:55:39 -0700

    Import www/node22 version 22.16.0

---
+++ www/node22/dragonfly/patch-deps_v8_third__party_abseil-cpp_absl_time_internal_cctz_src_time__zone__lookup__test.cc
@@ -0,0 +1,11 @@
+--- deps/v8/third_party/abseil-cpp/absl/time/internal/cctz/src/time_zone_lookup_test.cc.orig	Mon Feb  3 15:50:45 2025
++++ deps/v8/third_party/abseil-cpp/absl/time/internal/cctz/src/time_zone_lookup_test.cc	Mon Jun
+@@ -927,7 +927,7 @@ TEST(MakeTime, SysSecondsLimits) {
+     const time_zone cut = LoadZone("libc:UTC");
+     const year_t max_tm_year = year_t{std::numeric_limits<int>::max()} + 1900;
+     tp = convert(civil_second(max_tm_year, 12, 31, 23, 59, 59), cut);
+-#if defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__EMSCRIPTEN__)
++#if defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__EMSCRIPTEN__) || defined(__DragonFly__)
+     // Some gmtime_r() impls fail on extreme positive values.
+ #else
+     EXPECT_EQ("2147485547-12-31T23:59:59+00:00",
