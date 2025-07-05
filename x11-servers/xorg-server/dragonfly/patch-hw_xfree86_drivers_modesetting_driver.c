5eeb4e0841d6f3af42e6bda4b7769ea7c5cb86c1
DPorts Builder <nobody@home.ok>
2025-07-05 09:50:30 -0700

    Update x11-servers/xorg-server to version 21.1.16,1

--- x11-servers/xorg-server/dragonfly/patch-hw_xfree86_drivers_modesetting_driver.c
+++ x11-servers/xorg-server/dragonfly/patch-hw_xfree86_drivers_modesetting_driver.c
@@ -1,12 +1,11 @@
---- hw/xfree86/drivers/modesetting/driver.c.orig	2020-01-13 22:57:05 UTC
+--- hw/xfree86/drivers/modesetting/driver.c.orig	2025-06-10 06:55:30 UTC
 +++ hw/xfree86/drivers/modesetting/driver.c
-@@ -749,8 +749,7 @@ try_enable_glamor(ScrnInfoPtr pScrn)
+@@ -925,7 +925,7 @@ try_enable_glamor(ScrnInfoPtr pScrn)
      modesettingPtr ms = modesettingPTR(pScrn);
      const char *accel_method_str = xf86GetOptValString(ms->drmmode.Options,
                                                         OPTION_ACCEL_METHOD);
 -    Bool do_glamor = (!accel_method_str ||
--                      strcmp(accel_method_str, "glamor") == 0);
-+    Bool do_glamor = FALSE;
++    Bool do_glamor = (accel_method_str &&
+                       strcmp(accel_method_str, "glamor") == 0);
  
      ms->drmmode.glamor = FALSE;
- 
