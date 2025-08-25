a06f33268f26d4786982a52a2195089490f99d3e
DPorts Builder <nobody@home.ok>
2025-08-25 04:39:43 -0700

    Update devel/grpc to version 1.72.0,2

--- devel/grpc/dragonfly/patch-src_core_util_posix_directory__reader.cc
+++ devel/grpc/dragonfly/patch-src_core_util_posix_directory__reader.cc
@@ -1,10 +1,10 @@
---- src/core/lib/gprpp/posix/directory_reader.cc.orig	Wed Oct 23 16:57:09 2024
-+++ src/core/lib/gprpp/posix/directory_reader.cc	Wed Oct
+--- src/core/util/posix/directory_reader.cc.orig	2025-08-25 08:59:24 UTC
++++ src/core/util/posix/directory_reader.cc
 @@ -25,6 +25,7 @@
  #include "absl/strings/string_view.h"
  
  #if defined(GPR_LINUX) || defined(GPR_ANDROID) || defined(GPR_FREEBSD) || \
 +    defined(GPR_DRAGONFLY) || \
-     defined(GPR_APPLE)
+     defined(GPR_APPLE) || defined(GPR_NETBSD)
  
  #include <dirent.h>
