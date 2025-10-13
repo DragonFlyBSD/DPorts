f6a6148de06fcbdacf0bd9e1b8898e6b67ceb594
DPorts Builder <nobody@home.ok>
2025-10-13 11:29:40 -0700

    Update devel/onetbb to version 2022.0.0

--- devel/onetbb/dragonfly/patch-test_common_utils__dynamic__libs.h
+++ devel/onetbb/dragonfly/patch-test_common_utils__dynamic__libs.h
@@ -1,11 +1,11 @@
---- test/common/utils_dynamic_libs.h.orig	2021-10-04 09:50:18 UTC
-+++ test/common/utils_dynamic_libs.h
-@@ -50,7 +50,7 @@ namespace utils {
- #if __APPLE__
+--- test/common/utils_dynamic_libs.h.orig	Thu Oct 31 17:23:11 2024
++++ test/common/utils_dynamic_libs.h	Mon Oct
+@@ -58,7 +58,7 @@ namespace utils {
  #define EXT ".dylib"
+ #endif
  // Android SDK build system does not support .so file name versioning
--#elif __FreeBSD__ || __NetBSD__ || __sun || _AIX || __ANDROID__
-+#elif __FreeBSD__ || __NetBSD__ || __sun || _AIX || __ANDROID__ || __DragonFly__
+-#elif __FreeBSD__ || __NetBSD__ || __OpenBSD__ || __sun || _AIX || __ANDROID__
++#elif __FreeBSD__ || __NetBSD__ || __OpenBSD__ || __sun || _AIX || __ANDROID__ || __DragonFly__
  #define EXT ".so"
  #elif __unix__  // Order of these elif's matters!
  #define EXT __TBB_STRING(.so.2)
