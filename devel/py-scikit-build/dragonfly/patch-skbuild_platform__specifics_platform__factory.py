86dc02f09fa0e49213f891276d393e21296fe3cc
DPorts Builder <nobody@home.ok>
2025-11-02 05:07:21 -0800

    Update devel/py-scikit-build to version 0.18.1

--- devel/py-scikit-build/dragonfly/patch-skbuild_platform__specifics_platform__factory.py
+++ devel/py-scikit-build/dragonfly/patch-skbuild_platform__specifics_platform__factory.py
@@ -1,11 +1,11 @@
---- skbuild/platform_specifics/platform_factory.py.orig	2022-04-07 21:06:30 UTC
-+++ skbuild/platform_specifics/platform_factory.py
-@@ -26,7 +26,7 @@ def get_platform():
+--- skbuild/platform_specifics/platform_factory.py.orig	Sun Feb  2 00:00:00 2020
++++ skbuild/platform_specifics/platform_factory.py	Sun Nov
+@@ -35,7 +35,7 @@ def get_platform() -> abstract.CMakePlatform:
  
          return osx.OSXPlatform()
  
--    if this_platform in {"freebsd", "os400", "openbsd"}:
-+    if this_platform in {"freebsd", "os400", "openbsd", "dragonfly"}:
+-    if this_platform in {"freebsd", "netbsd", "os400", "openbsd"}:
++    if this_platform in {"freebsd", "netbsd", "os400", "openbsd", "dragonfly"}:
          from . import bsd
  
          return bsd.BSDPlatform()
