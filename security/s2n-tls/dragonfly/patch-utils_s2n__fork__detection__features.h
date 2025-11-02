c9179512589e1bda24598f987661fd264f101553
DPorts Builder <nobody@home.ok>
2025-11-02 05:07:26 -0800

    Update security/s2n-tls to version 1.5.15,1

--- security/s2n-tls/dragonfly/patch-utils_s2n__fork__detection__features.h
+++ security/s2n-tls/dragonfly/patch-utils_s2n__fork__detection__features.h
@@ -1,11 +1,11 @@
---- utils/s2n_fork_detection.c.orig	2023-04-17 21:07:42.270230000 +0200
-+++ utils/s2n_fork_detection.c	2023-04-17 21:08:04.160559000 +0200
-@@ -20,7 +20,7 @@
+--- utils/s2n_fork_detection_features.h.orig	Tue Mar 18 22:43:41 2025
++++ utils/s2n_fork_detection_features.h	Sun Nov
+@@ -22,7 +22,7 @@
  typedef struct _opaque_pthread_once_t __darwin_pthread_once_t;
  typedef __darwin_pthread_once_t pthread_once_t;
      #define _DARWIN_C_SOURCE
 -#elif defined(__FreeBSD__) || defined(__OpenBSD__)
-+#elif defined(__DragonFly__) || defined(__FreeBSD__) || defined(__OpenBSD__)
++#elif defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__DragonFly__)
      /* FreeBSD requires POSIX compatibility off for its syscalls (enables __BSD_VISIBLE)
       * Without the below line, <sys/mman.h> cannot be imported (it requires __BSD_VISIBLE) */
      #undef _POSIX_C_SOURCE
